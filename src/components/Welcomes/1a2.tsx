d65f67e307f6305eddbf9c8bb9919303e72d222f
Jack Works <zjwpeter@gmail.com>
2019-02-27 12:16:15 +0800

    Welcome 1a-2

---
+++ src/components/Welcomes/1a2.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import Paper from '@material-ui/core/Paper/Paper'
+import Typography from '@material-ui/core/Typography/Typography'
+import { withStylesTyped } from '../../utils/theme'
+import { Theme } from '@material-ui/core/styles/createMuiTheme'
+import createStyles from '@material-ui/core/styles/createStyles'
+import Button from '@material-ui/core/Button/Button'
+
+interface Props {}
+export default withStylesTyped((theme: Theme) =>
+    createStyles({
+        paper: {
+            padding: '2rem 1rem 1rem 1rem',
+            textAlign: 'center',
+            maxWidth: '35rem',
+            '& > *': {
+                marginBottom: theme.spacing.unit * 3,
+            },
+        },
+        button: {
+            minWidth: 180,
+        },
+    }),
+)<{}>(function Welcome({ classes }) {
+    return (
+        <Paper className={classes.paper}>
+            <Typography variant="h5">Check this box when you post</Typography>
+            <img src={require('./1a2.jpg')} width="100%" />
+            <Typography variant="subtitle1">Then only your friends with Maskbook can see the post content</Typography>
+            <Button variant="raised" color="primary" className={classes.button}>
+                Nice!
+            </Button>
+        </Paper>
+    )
+})
--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Welcome0 from '../components/Welcomes/0'
 import Welcome1a1 from '../components/Welcomes/1a1'
+import Welcome1a2 from '../components/Welcomes/1a2'
 
 storiesOf('Welcome', module)
     .add('Step 0', () => <Welcome0 />)
     .add('Step 1a-1', () => <Welcome1a1 />)
+    .add('Step 1a-2', () => <Welcome1a2 />)
