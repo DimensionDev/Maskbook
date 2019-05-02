9eb9357c1add0389588952fc0a6e7ec66688e30b
Jack Works <zjwpeter@gmail.com>
2019-05-02 10:20:20 +0800

    Close #19, Fix hijack race

--- src/background-service.ts
+++ src/background-service.ts
@@ -1,3 +1,15 @@
 import './extension/service'
 import { backgroundSetup } from './setup'
 backgroundSetup()
+
+browser.webNavigation.onCommitted.addListener(
+    arg =>
+        browser.tabs
+            .executeScript(arg.tabId, {
+                frameId: arg.frameId,
+                runAt: 'document_start',
+                file: browser.runtime.getURL('js/injectedscript.js'),
+            })
+            .then(console.log, console.log),
+    { url: [{ hostSuffix: 'facebook.com' }] },
+)
--- src/extension/injected-script/index.ts
+++ src/extension/injected-script/index.ts
@@ -1,15 +1,2 @@
-import { GetContext } from '@holoflows/kit/es'
-switch (GetContext()) {
-    case 'content':
-        const script = document.createElement('script')
-        script.src = browser.runtime.getURL('/static/js/injectedscript.js')
-        script.dataset.chrome = 'true'
-        document.querySelector('html')!.appendChild(script)
-        break
-    case 'webpage':
-        require('./addEventListener')
-        const e = document.querySelector('script[data-chrome=true]')
-        e && e.remove()
-        console.log('Injected script loaded')
-        break
-}
+import './addEventListener'
+console.log('Injected script loaded')
