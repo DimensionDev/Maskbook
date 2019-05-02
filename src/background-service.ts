f473e3b2df817db2b8e7abc0169bf60400b87eb7
Jack Works <zjwpeter@gmail.com>
2019-05-02 16:19:32 +0800

    Fix #19 again (maybe)

--- src/background-service.ts
+++ src/background-service.ts
@@ -2,13 +2,17 @@ import './extension/service'
 import { backgroundSetup } from './setup'
 backgroundSetup()
 
+const script = `{
+const script = document.createElement('script')
+script.src = "${browser.runtime.getURL('js/injectedscript.js')}"
+document.documentElement.appendChild(script)
+}`
 browser.webNavigation.onCommitted.addListener(
     async arg => {
         try {
             await browser.tabs.executeScript(arg.tabId, {
-                frameId: arg.frameId,
                 runAt: 'document_start',
-                file: 'js/injectedscript.js',
+                code: script,
             })
         } catch (e) {
             console.error('Inject error', e, arg, browser.runtime.getURL('js/injectedscript.js'))
