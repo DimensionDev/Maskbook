4e7a7f99d04e56e3466bd1545154f46c27f64828
Jack Works <zjwpeter@gmail.com>
2019-05-05 13:41:02 +0800

    Enable memorize for AutomatedTabTask

--- src/extension/content-script/tasks.ts
+++ src/extension/content-script/tasks.ts
@@ -1,21 +1,24 @@
 import { AutomatedTabTask, LiveSelector, MutationObserverWatcher } from '@holoflows/kit'
-export default AutomatedTabTask({
-    /**
-     * Access: https://www.facebook.com/${username}/posts/${postId}
-     * Or: https://www.facebook.com/permalink.php?story_fbid=${postId}&id=${userId}
-     */
-    async getPostContent() {
-        const post = new LiveSelector().querySelector<HTMLParagraphElement>('#contentArea p')
-        const [data] = await new MutationObserverWatcher(post).once(node => node.innerText)
-        return data
+export default AutomatedTabTask(
+    {
+        /**
+         * Access: https://www.facebook.com/${username}/posts/${postId}
+         * Or: https://www.facebook.com/permalink.php?story_fbid=${postId}&id=${userId}
+         */
+        async getPostContent() {
+            const post = new LiveSelector().querySelector<HTMLParagraphElement>('#contentArea p')
+            const [data] = await new MutationObserverWatcher(post).once(node => node.innerText)
+            return data
+        },
+        /**
+         * Access: https://www.facebook.com/profile.php?id=${userId}
+         * Or: https://www.facebook.com/${username}?fref=pymk
+         */
+        async getBioContent() {
+            const bio = new LiveSelector().querySelector<HTMLDivElement>('#profile_timeline_intro_card')
+            const [data] = await new MutationObserverWatcher(bio).once(node => node.innerText)
+            return data
+        },
     },
-    /**
-     * Access: https://www.facebook.com/profile.php?id=${userId}
-     * Or: https://www.facebook.com/${username}?fref=pymk
-     */
-    async getBioContent() {
-        const bio = new LiveSelector().querySelector<HTMLDivElement>('#profile_timeline_intro_card')
-        const [data] = await new MutationObserverWatcher(bio).once(node => node.innerText)
-        return data
-    },
-})!
+    { memorable: true },
+)!
