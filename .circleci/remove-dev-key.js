85d73e9ad1654d6a5d4a316c5e85a70fb84719be
Jack Works <zjwpeter@gmail.com>
2019-05-05 15:04:08 +0800

    Revert "Add key in manifest, close #28"
    This reverts commit c0bbb0b7ed26048ff3aa74c2e9ed3b0c6c75f5c4.

--- .circleci/remove-dev-key.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const url = path.join(__dirname, '../dist/manifest.json')
-const json = require(url)
-delete json.key
-delete json.$schema
-fs.writeFileSync(url, JSON.stringify(json, undefined, 4), { encoding: 'utf8' })
