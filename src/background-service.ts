92fd9162a4d8491b1401d5213402101ec4142552
Jack Works <zjwpeter@gmail.com>
2019-05-13 21:55:33 +0800

    Try to support Firefox related #25
    Signed-off-by: Jack Works <zjwpeter@gmail.com>

--- src/background-service.ts
+++ src/background-service.ts
@@ -5,19 +5,24 @@ backgroundSetup()
 
 if (GetContext() === 'background') {
     const script = `{
-const script = document.createElement('script')
-script.src = "${browser.runtime.getURL('js/injectedscript.js')}"
-document.documentElement.appendChild(script)
-}`
+        const script = document.createElement('script')
+        script.src = "${browser.runtime.getURL('js/injectedscript.js')}"
+        document.documentElement.appendChild(script)
+        }`
     browser.webNavigation.onCommitted.addListener(
         async arg => {
             try {
                 await browser.tabs.executeScript(arg.tabId, {
                     runAt: 'document_start',
+                    frameId: arg.frameId,
                     code: script,
                 })
             } catch (e) {
-                console.error('Inject error', e, arg, browser.runtime.getURL('js/injectedscript.js'))
+                if (e.message.match('non-structured-clonable data')) {
+                    // It's okay we don't need the result, happened on Firefox
+                } else if (e.message.match('Frame not found, or missing host permission')) {
+                    // It's maybe okay, happened on Firefox
+                } else console.error('Inject error', e, arg, browser.runtime.getURL('js/injectedscript.js'))
             }
         },
         { url: [{ hostEquals: 'www.facebook.com' }] },
