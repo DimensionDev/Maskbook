9b887d04fb4640e7d8a09c15fc5383c89936e72f
Jack Works <zjwpeter@gmail.com>
2019-07-23 12:02:45 +0800

    feat: draft for version 2 gun

--- src/network/gun/version.2.ts
+++ src/network/gun/version.2.ts
@@ -1,12 +1,16 @@
 export const Version2Person = '1edc22d2-ca62-4d0a-b842-148f6faa4269'
 export const Version2Post = 'd7cc9636-be26-447b-a1bb-798b126f7ace'
-
+import Gun from 'gun/gun'
+import 'gun/sea'
+import { PostIdentifier } from '../../database/type'
+import { PublishedAESKey } from '../../crypto/crypto-alpha-40'
+import { queryPostAESKey } from '../../key-management/posts-gun'
 interface PersonOnGun {
     proofPostId: string
 }
 interface PostOnGun {
-    /** Keys is a set of PublishedAESKeys without user identifier */
-    keys: string[]
+    /** NO identifier here! */
+    keys: { encryptedKey: string; salt: string }[]
 }
 export interface ApplicationStateInGunVersion2 {
     /**
@@ -20,4 +24,42 @@ export interface ApplicationStateInGunVersion2 {
      * ! If this network is dangerous, then users expect secret distribution, don't write data into this field
      */
     [key: string]: PersonOnGun | PostOnGun
+}
+const gun = new Gun<ApplicationStateInGunVersion2>()
+function isPersonOnGun(x: any): x is PersonOnGun {
+    return typeof x.proofPostId === 'string'
+}
+function isPostOnGun(x: any): x is PostOnGun {
+    return Array.isArray(x.keys)
+}
+async function postIdToKey(post: PostIdentifier) {
+    const hash = await Gun.SEA.work(post, Version2Post, undefined, {
+        name: 'SHA-256',
+        encode: 'base64',
+        // TODO: How many iterations is better?
+        iterations: 5,
+    })
+    return hash!
+}
+export async function queryPostAESKeyV2(postIdentifier: PostIdentifier) {}
+
+export async function publishPostAESKey(
+    postIdentifier: string,
+    receiversKeys: {
+        key: PublishedAESKey
+        name: string
+    }[],
+) {
+    // Store AES key to gun
+    const stored: {
+        [postIdentifier: string]: PublishedAESKey
+    } = {}
+    for (const k of receiversKeys) {
+        stored[k.name] = k.key
+    }
+    console.log('Save to gun', postIdentifier, receiversKeys)
+    // await gun
+    //     .get('posts')
+    //     .get(postIdentifier)
+    //     .put(stored).then!()
 }
