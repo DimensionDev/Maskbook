0a44e41cdbd8467038526d9d1e84784573bc884e
Jack Works <zjwpeter@gmail.com>
2019-07-26 17:14:18 +0800

    feat: gen qr to transfer key

--- src/components/InjectedComponents/NotSetupYetPrompt.tsx
+++ src/components/InjectedComponents/NotSetupYetPrompt.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import Services from '../../extension/service'
 import { geti18nString } from '../../utils/i18n'
-import { SnackbarContent, Button } from '@material-ui/core'
+import { SnackbarContent, Button, makeStyles } from '@material-ui/core'
 import { myUsernameRef } from '../../extension/content-script/injections/MyUsername'
 import { isMobile } from '../../social-network/facebook.com/isMobile'
 import { useValueRef } from '../../utils/hooks/useValueRef'
-import { useNotSetUpYetStyles } from './DecryptedPost'
+import { GetContext } from '@holoflows/kit/es'
+
+const useNotSetUpYetStyles = makeStyles({
+    root: {
+        marginBottom: '2em',
+        maxWidth: '50em',
+    },
+})
 export function NotSetupYetPrompt() {
     const id = useValueRef(myUsernameRef)
     const styles = useNotSetUpYetStyles()
     const button = (
-        <Button onClick={() => Services.Welcome.openWelcomePage(id, isMobile)} color="primary" size="small">
+        <Button
+            onClick={() => {
+                if (GetContext() === 'options') {
+                    location.hash = '/welcome'
+                } else Services.Welcome.openWelcomePage(id, isMobile)
+            }}
+            color="primary"
+            size="small">
             {geti18nString('click_to_setup')}
         </Button>
     )
--- src/stories/OptionsPage.tsx
+++ src/stories/OptionsPage.tsx
@@ -7,10 +7,12 @@ import Dashboard from '../components/Dashboard/Dashboard'
 import Privacy from '../extension/options-page/Privacy'
 import { QrCode } from '../components/MobileImportExport/qrcode'
 import { text } from '@storybook/addon-knobs'
+import { ExportData } from '../components/MobileImportExport/Export'
 
 storiesOf('Options Page', module)
     .add('Privacy', () => Privacy)
     .add('QrCode', () => <QrCode text={text('QrCode', 'QrCode')} />)
+    .add('ExportData', () => <ExportData />)
 
 storiesOf('Dashboard (unused)', module)
     .add('Identity Component (unused)', () => <Identity person={demoPeople[0]} onClick={action('Click')} />)
