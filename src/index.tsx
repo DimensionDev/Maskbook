7ccf1eff129292fab3bc8c87af899c49edacba57
Jack Works <zjwpeter@gmail.com>
2019-07-26 15:03:57 +0800

    feat: new options page

--- src/index.tsx
+++ src/index.tsx
@@ -4,42 +4,176 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 
 import Welcome from './extension/options-page/Welcome'
-import { ThemeProvider } from '@material-ui/styles'
-import { MaskbookLightTheme } from './utils/theme'
-import { OptionsPage } from './components/OptionsPage'
-import { geti18nString } from './utils/i18n'
 import Privacy from './extension/options-page/Privacy'
 
+import { ThemeProvider } from '@material-ui/styles'
+import { MaskbookLightTheme } from './utils/theme'
+import { geti18nString } from './utils/i18n'
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles'
+import {
+    Divider,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    CssBaseline,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Hidden,
+    Drawer,
+    Link as MuiLink,
+} from '@material-ui/core'
+import Menu from '@material-ui/icons/Menu'
+import NearMe from '@material-ui/icons/NearMe'
+import Assignment from '@material-ui/icons/Assignment'
+
 uiSetup()
-function App() {
+const drawerWidth = 240
+const empty = (
+    <div
+        style={{
+            background: '#232D36 url(https://maskbook.com/img/bg--warai-kamen.svg) fixed repeat',
+            width: '100%',
+            height: 'calc(100vh - 64px)',
+        }}
+    />
+)
+const OptionsPageRouters = (
+    <>
+        <Route exact path="/" component={() => empty} />
+        <Route path="/welcome" component={Welcome} />
+        <Route path="/privacy" component={() => Privacy} />
+    </>
+)
+const Links1st = (
+    <>
+        <LinkItem icon={<NearMe />} title={geti18nString('options_index_setup')} to="/welcome" />
+    </>
+)
+const Links2rd = (
+    <>
+        <LinkItem icon={<Assignment />} title="Privacy policy" to="/privacy" />
+    </>
+)
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+        },
+        logo: {
+            height: 54,
+            marginBottom: -10,
+        },
+        drawer: {
+            [theme.breakpoints.up('sm')]: {
+                width: drawerWidth,
+                flexShrink: 0,
+            },
+        },
+        appBar: {
+            marginLeft: drawerWidth,
+            zIndex: theme.zIndex.drawer + 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+            [theme.breakpoints.up('sm')]: {
+                display: 'none',
+            },
+        },
+        toolbar: theme.mixins.toolbar,
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        content: {
+            flexGrow: 1,
+            // paddingLeft: theme.spacing(3),
+            // paddingRight: theme.spacing(3),
+        },
+    }),
+)
+ReactDOM.render(<ResponsiveDrawer />, document.getElementById('root')!)
+function ResponsiveDrawer() {
+    const classes = useStyles()
+    const theme = useTheme()
+    const [mobileOpen, setMobileOpen] = React.useState(false)
+
+    function handleDrawerToggle() {
+        setMobileOpen(!mobileOpen)
+    }
+
+    const drawer = (
+        <div>
+            <div className={classes.toolbar} />
+            <Divider />
+            <List onClick={handleDrawerToggle}>{Links1st}</List>
+            <Divider />
+            <List onClick={handleDrawerToggle}>{Links2rd}</List>
+        </div>
+    )
+
     return (
         <ThemeProvider theme={MaskbookLightTheme}>
+            <style>{'body {overflow-x: hidden;}'}</style>
             <Router>
-                <Route
-                    exact
-                    path="/"
-                    component={() => (
-                        <>
-                            <OptionsPage
-                                buttonTitle={geti18nString('options_index_welcome')}
-                                title={geti18nString('options_index_setup')}
-                                to="/welcome"
-                                linkComponent={Link}
-                            />
-                            <OptionsPage
-                                buttonTitle="View our privacy policy"
-                                title="How we collect and use your data"
-                                to="/privacy"
-                                linkComponent={Link}
-                            />
-                        </>
-                    )}
-                />
-                <Route path="/welcome" component={Welcome} />
-                <Route path="/privacy" component={() => Privacy} />
+                <div className={classes.root}>
+                    <CssBaseline />
+                    <AppBar position="fixed" className={classes.appBar}>
+                        <Toolbar>
+                            <IconButton
+                                color="inherit"
+                                // Todo: i18n
+                                aria-label="open drawer"
+                                edge="start"
+                                onClick={handleDrawerToggle}
+                                className={classes.menuButton}>
+                                <Menu />
+                            </IconButton>
+                            <Typography variant="h6" noWrap>
+                                <img
+                                    className={classes.logo}
+                                    src="https://maskbook.com/img/maskbook--logotype-white.png"
+                                    alt="Maskbook"
+                                />
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                    <nav className={classes.drawer}>
+                        <Hidden smUp>
+                            <Drawer
+                                variant="temporary"
+                                anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                                open={mobileOpen}
+                                onClose={handleDrawerToggle}
+                                classes={{ paper: classes.drawerPaper }}
+                                ModalProps={{ keepMounted: true }}>
+                                {drawer}
+                            </Drawer>
+                        </Hidden>
+                        <Hidden xsDown>
+                            <Drawer classes={{ paper: classes.drawerPaper }} variant="permanent" open>
+                                {drawer}
+                            </Drawer>
+                        </Hidden>
+                    </nav>
+                    <main className={classes.content}>
+                        <div className={classes.toolbar} />
+                        {OptionsPageRouters}
+                    </main>
+                </div>
             </Router>
         </ThemeProvider>
     )
 }
-
-ReactDOM.render(<App />, document.getElementById('root')!)
+function LinkItem(props: { to: string; icon: React.ReactElement; title: string }) {
+    return (
+        <MuiLink component={Link} to={props.to}>
+            <ListItem button>
+                <ListItemIcon>{props.icon}</ListItemIcon>
+                <ListItemText primary={props.title} />
+            </ListItem>
+        </MuiLink>
+    )
+}
--- src/stories/OptionsPage.tsx
+++ src/stories/OptionsPage.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { OptionsPage } from '../components/OptionsPage'
 import Identity from '../components/Dashboard/Identity'
 import { demoPeople } from './demoPeople'
 import { action } from '@storybook/addon-actions'
 import Dashboard from '../components/Dashboard/Dashboard'
 
-storiesOf('Options page', module).add('Index', () => <OptionsPage buttonTitle="Title 2" title="Title" to="/" />)
 storiesOf('Dashboard (unused)', module)
     .add('Identity Component (unused)', () => <Identity person={demoPeople[0]} onClick={action('Click')} />)
     .add('Dashboard (unused)', () => (
