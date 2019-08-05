3e00eed134dec754ada76029dd4ebcb38b0b61f0
Misaka <447f.misaka@outlook.com>
2019-08-05 20:28:48 +0800

    fix: works in env that permission api is partly available

--- src/utils/hooks/useRequestCamera.ts
+++ src/utils/hooks/useRequestCamera.ts
@@ -1,17 +1,42 @@
 /** This file is published under MIT License */
 import { useEffect, useState } from 'react'
 import { useAsync } from '../components/AsyncComponent'
+import { has } from 'lodash-es'
+
+const q = <const>["query", "request", "revoke"];
+
+export function checkPermissionApiUsability(type?: typeof q[number]) {
+    const r: Partial<{[T in typeof q[number]]: boolean}> = {}
+    for (const v of q) {
+        r[v] = has(navigator, `permissions.${v}`);
+    }
+    if (type) {
+        return r[type];
+    }
+    return r as Required<typeof r>;
+}
 
 export function useRequestCamera(needRequest: boolean) {
     const [permission, updatePermission] = useState<PermissionState>('prompt')
     useEffect(() => {
         let permissionStatus: PermissionStatus | undefined = undefined
         const update = () => updatePermission(permissionStatus!.state)
-        navigator.permissions.query({ name: 'camera' }).then(p => {
-            permissionStatus = p
-            p.onchange = update
+        if (checkPermissionApiUsability("query")) {
+            navigator.permissions.query({ name: 'camera' }).then(p => {
+                permissionStatus = p
+                p.onchange = update
+                update()
+            })
+        } else {
+            permissionStatus = {
+                onchange: null,
+                state: "granted",
+                addEventListener: () => {},
+                removeEventListener: () => {},
+                dispatchEvent: () => true
+            }
             update()
-        })
+        }
         return () => {
             if (permissionStatus) permissionStatus.onchange = null
         }
@@ -23,16 +48,19 @@ export function useRequestCamera(needRequest: boolean) {
     return permission
 }
 async function requestPermission(): Promise<unknown> {
-    // @ts-ignore
-    if (navigator.permissions.request) return navigator.permissions.request({ name: 'camera' })
-    const t = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-            width: { ideal: 1920 },
-            height: { ideal: 1080 }
-        },
-    })
-    t.getTracks()[0].stop()
+    if (checkPermissionApiUsability("request")) {
+        // @ts-ignore
+        if (navigator.permissions.request) return navigator.permissions.request({ name: 'camera' })
+    } else {
+        const t = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                width: { ideal: 1920 },
+                height: { ideal: 1080 }
+            },
+        })
+        t.getTracks()[0].stop()
+    }
 }
 export async function getFrontVideoDevices() {
     const devices = (await navigator.mediaDevices.enumerateDevices()).filter(devices => devices.kind === 'videoinput')
