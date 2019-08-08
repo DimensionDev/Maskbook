d98fb242fdf3713da3f374b32d51d730650c62b5
Jack Works <zjwpeter@gmail.com>
2019-08-08 14:20:19 +0800

    Revert "fix: type in worker"
    This reverts commit 6a80fe7763c65312ec93fd956ef6a2e4148fe46c.

--- src/web-workers/QRCode.ts
+++ src/web-workers/QRCode.ts
@@ -1,10 +1,12 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="webworker" />
-
+// ! TODO:
+// ! find a easy way for using DOM context and worker context separeatly
+// ! then we can remove @ts-ignore here and make worker type safe
 import jsQR from 'jsqr'
 
+// @ts-ignore
 addEventListener('message', ev => {
     const [data, width, height] = ev.data as Parameters<typeof jsQR>
 
+    // @ts-ignore
     postMessage(jsQR(data, width, height))
 })
