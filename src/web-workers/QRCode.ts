6a80fe7763c65312ec93fd956ef6a2e4148fe46c
Jack Works <zjwpeter@gmail.com>
2019-08-08 14:06:28 +0800

    fix: type in worker

--- src/web-workers/QRCode.ts
+++ src/web-workers/QRCode.ts
@@ -1,12 +1,10 @@
-// ! TODO:
-// ! find a easy way for using DOM context and worker context separeatly
-// ! then we can remove @ts-ignore here and make worker type safe
+/// <reference no-default-lib="true"/>
+/// <reference lib="webworker" />
+
 import jsQR from 'jsqr'
 
-// @ts-ignore
 addEventListener('message', ev => {
     const [data, width, height] = ev.data as Parameters<typeof jsQR>
 
-    // @ts-ignore
     postMessage(jsQR(data, width, height))
 })
