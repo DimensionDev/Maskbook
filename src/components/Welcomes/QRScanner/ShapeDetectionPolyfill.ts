245718038580f08f1f29d3de33d4842453397ce0
guanbinrui <guanbinrui@dimension.im>
2019-08-08 09:49:32 +0800

    feat: decode qrcode in web worker

--- src/components/Welcomes/QRScanner/ShapeDetectionPolyfill.ts
+++ src/components/Welcomes/QRScanner/ShapeDetectionPolyfill.ts
@@ -1,9 +1,17 @@
-import jsQR from 'jsqr'
-import { isNull } from 'lodash-es'
 /// <reference path="./ShapeDetectionSpec.d.ts" />
+import { isNull } from 'lodash-es'
+
+const noop = () => {}
 
 class BarcodeDetectorPolyfill implements BarcodeDetector {
-    constructor() {}
+    private worker: Worker = new Worker(browser.runtime.getURL('js/qrcode.js'))
+    private onWorkerMessage: (ev: MessageEvent) => void = noop
+    private onWorkerError: (err: ErrorEvent) => void = noop
+
+    constructor() {
+        this.worker.addEventListener('message', ev => this.onWorkerMessage.call(this.worker, ev))
+        this.worker.addEventListener('error', err => this.onWorkerError.call(this.worker, err))
+    }
     // noinspection JSMethodCanBeStatic
     public async detect(mediaSource: CanvasImageSource) {
         const canvas = document.createElement('canvas')
@@ -14,13 +22,25 @@ class BarcodeDetectorPolyfill implements BarcodeDetector {
         }
         ctx.drawImage(mediaSource, 0, 0)
         const d = ctx.getImageData(0, 0, canvas.width, canvas.height)
-        const res = jsQR(d.data, canvas.width, canvas.height)
-        if (isNull(res)) {
-            return []
-        }
-        const result = new DetectedBarcodePolyfill()
-        result.rawValue = res.data
-        return [result]
+        return new Promise<DetectedBarcode[]>(resolve => {
+            this.worker.postMessage([d.data, canvas.width, canvas.height])
+            this.onWorkerMessage = (ev: MessageEvent) => {
+                if (isNull(ev.data)) {
+                    resolve([])
+                    return
+                }
+                const result = new DetectedBarcodePolyfill()
+                result.rawValue = ev.data.data
+                resolve([result])
+            }
+            this.onWorkerError = (err: ErrorEvent) => {
+                resolve([])
+            }
+        }).then(detected => {
+            this.onWorkerMessage = noop
+            this.onWorkerError = noop
+            return detected
+        })
     }
 }
 class DetectedBarcodePolyfill implements DetectedBarcode {
