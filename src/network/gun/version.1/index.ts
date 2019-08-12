d89b8a35c565e638e3c3ba802b52130d18fa00a1
Jack Works <zjwpeter@gmail.com>
2019-08-12 11:52:16 +0800

    chore: deprecates v1

---
+++ src/network/gun/version.1/index.ts
@@ -0,0 +1,29 @@
+import Gun from 'gun'
+import 'gun/lib/then'
+import { OnlyRunInContext } from '@holoflows/kit/es'
+import { PublishedAESKey } from '../../../crypto/crypto-alpha-40'
+OnlyRunInContext('background', 'Gun')
+
+/**
+ * @deprecated // ! This version will leak post targets ! //
+ *
+ * Use exchange v2 instead!
+ */
+export interface ApplicationStateInGunVersion1 {
+    maskbook: {
+        users: {
+            // User ID
+            [userID: string]: {
+                provePostId: string
+            }
+        }
+        posts: {
+            // Post salt
+            [salt: string]: {
+                // ! This will leak post targets. (But keys are safe)
+                [receiversUsername: string]: PublishedAESKey
+            }
+        }
+    }
+}
+export const gun = new Gun<ApplicationStateInGunVersion1>(['http://172.104.123.119:8765/gun']).get('maskbook')
