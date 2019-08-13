375cb74cecb0414868522405458049194429b1b1
guanbinrui <guanbinrui@dimension.im>
2019-08-13 15:25:15 +0800

    fix(firefox): window.close not working

--- src/setup.ts
+++ src/setup.ts
@@ -5,21 +5,32 @@ export function backgroundSetup() {
     Object.assign(window, {
         elliptic: require('elliptic'),
     })
+    MessageCenter.on('closeActiveTab', async () => {
+        const tabs = await browser.tabs.query({
+            active: true,
+        })
+        if (tabs[0]) {
+            await browser.tabs.remove(tabs[0].id!)
+        }
+    })
 }
 import { activateSocialNetworkUI, definedSocialNetworkUIs } from './social-network/ui'
 import { definedSocialNetworkWorkers } from './social-network/worker'
+import { MessageCenter } from './utils/messages'
 Object.assign(window, { definedSocialNetworkWorkers, definedSocialNetworkUIs })
 export function uiSetup() {
     activateSocialNetworkUI()
 
-    if (typeof browser !== 'undefined' && browser.tabs && browser.tabs.query && browser.tabs.remove) {
-        const close = window.close
-        window.close = () => {
-            Reflect.apply(close, window, [])
-            setTimeout(async () => {
+    const close = window.close
+    window.close = () => {
+        Reflect.apply(close, window, [])
+        setTimeout(async () => {
+            if (typeof browser !== 'undefined' && browser.tabs && browser.tabs.query && browser.tabs.remove) {
                 const { id } = await browser.tabs.getCurrent()
                 id && (await browser.tabs.remove(id))
-            }, 400)
-        }
+            } else {
+                MessageCenter.emit('closeActiveTab', undefined)
+            }
+        }, 400)
     }
 }
