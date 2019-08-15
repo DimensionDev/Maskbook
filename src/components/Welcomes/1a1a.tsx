c418aea5a7d562b1060be432adaec6438c4334cd
Sun Yi-Ran <sunrisefox@qq.com>
2019-08-15 22:44:16 +0800

    fix(webpack): prevent other chunks from being injected to index.html (#108)
    * fix(webpack): prevent other chunks from being injected to index.html
    
    * fix(welcome): script error on no identity

--- src/components/Welcomes/1a1a.tsx
+++ src/components/Welcomes/1a1a.tsx
@@ -28,7 +28,7 @@ const useStyles = makeStyles(theme => ({
 }))
 export default function Welcome({ next, identities, didntFindAccount }: Props) {
     const classes = useStyles()
-    const [selected, setSelect] = useState<Person[]>([identities[0]])
+    const [selected, setSelect] = useState<Person[]>(identities[0] ? identities : [])
     return (
         <WelcomeContainer className={classes.paper}>
             <Typography variant="h5">{geti18nString('welcome_1a1_title')}</Typography>
