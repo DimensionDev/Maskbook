6b1323713b750698177bf83273735ecdb9c243a6
Misaka imoto 17535 <447f.misaka@outlook.com>
2019-08-22 14:46:11 +0800

    chore: start-up script maintaince
    * feat: allow extension to be auto reloaded
    
    * feat: remove the plugin, update script, update document.

--- just.config.js
+++ just.config.js
@@ -2,34 +2,42 @@ const { task, parallel } = require('just-task')
 const { spawn } = require('child_process')
 const path = require('path')
 
-task('install/holoflows', async () => {
-    const base = path.join(process.cwd(), 'node_modules/@holoflows')
-    await step(`cd ${path.join(base, 'kit')} && yarn && yarn build`)
-})
-
 const lintCommand = async (str, level = 'log') => {
     const listen = 'onchange "./src/**/*" -i --'
     await step(`${listen} prettier --${str} "./src/**/*.{ts,tsx}" --loglevel ${level}`)
 }
 
-task('lint', () => lintCommand('check'))
-task('lint/fix', () => lintCommand('write', 'warn'))
+task('watch', () => parallel('react', 'watch/hot-reload-firefox'))
+task('watch/hot-reload-firefox', () => step('web-ext run --source-dir ./dist/'))
 
-task('storybook/serve', () => step('start-storybook -p 9009 -s public --quiet', true))
-
-task('storybook', parallel('lint/fix', 'storybook/serve'))
-task('storybook/build', () => step('build-storybook -s public --quiet', true))
-
+task('react', () => parallel('lint/fix', 'react/start'))
 task('react/start', () => step('react-app-rewired start'))
 task('react/build', () => step('react-app-rewired build'))
 task('react/test', () => step('react-app-rewired test'))
 
-task('react', parallel('lint/fix', 'react/start'))
+task('lint', () => lintCommand('check'))
+task('lint/fix', () => lintCommand('write', 'warn'))
 
-const step = (cmd, withWarn = false) => {
+task('storybook', () => parallel('lint/fix', 'storybook/serve'))
+task('storybook/serve', () => step('start-storybook -p 9009 -s public --quiet', { withWarn: true }))
+task('storybook/build', () => step('build-storybook -s public --quiet', { withWarn: true }))
+
+task('install/holoflows', async () => {
+    const base = path.join(process.cwd(), 'node_modules/@holoflows')
+    await step(`cd ${path.join(base, 'kit')} && yarn && yarn build`)
+})
+
+/**
+ * @param cmd {string} The command you want to run
+ * @param opt {object} Options
+ * @param opt.withWarn {boolean} Show warn in stdio
+ * @param opt.env {NodeJS.ProcessEnv} Environment key-value pairs
+ */
+const step = (cmd, opt = { withWarn: false }) => {
     const child = spawn(cmd, [], {
         shell: true,
-        stdio: ['inherit', 'inherit', withWarn ? 'inherit' : 'ignore'],
+        stdio: ['inherit', 'inherit', opt.withWarn ? 'inherit' : 'ignore'],
+        env: opt.env,
     })
 
     return new Promise((resolve, reject) => {
