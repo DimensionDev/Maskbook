fab12eded87f7a6819ee7d15b50b78d58e14babc
Misaka <447f.misaka@outlook.com>
2019-08-26 16:48:23 +0800

    update: entry point

--- src/provider.worker.ts
+++ src/provider.worker.ts
@@ -1,2 +1,4 @@
 import './social-network-provider/facebook.com/worker-provider'
+import './social-network-provider/twitter.com/worker'
+
 import './social-network-provider/options-page/index'
--- src/social-network-provider/index.ts
+++
@@ -1,6 +0,0 @@
-import './twitter.com/worker'
-import './twitter.com/ui'
-import './facebook.com/worker-provider'
-import './facebook.com/ui-provider'
-
-import './options-page'
