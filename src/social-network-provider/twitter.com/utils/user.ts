fcaccab88efef60a3af9a12efdb931726d4f93f1
Misaka <447f.misaka@outlook.com>
2019-08-26 19:38:11 +0800

    chore: add document for username validator

--- src/social-network-provider/twitter.com/utils/user.ts
+++ src/social-network-provider/twitter.com/utils/user.ts
@@ -1,6 +1,9 @@
 import { SocialNetworkWorkerAndUI } from '../../../social-network/shared'
 import { isNull } from 'lodash-es'
 
+/**
+ * @link https://help.twitter.com/en/managing-your-account/twitter-username-rules
+ */
 export const usernameValidator: SocialNetworkWorkerAndUI['isValidUsername'] = (name: string) => {
     console.log(name)
     for (const v of [/(twitter|admin)/i, /.{16,}/, /[^A-Za-z0-9_]/]) {
