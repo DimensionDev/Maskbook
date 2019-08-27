8ab333558e251c947324a7c2b9969024e47beb65
Jack Works <zjwpeter@gmail.com>
2019-08-27 14:23:38 +0800

    fix: options_index_devel -> options_index_dev

--- src/index.tsx
+++ src/index.tsx
@@ -59,7 +59,7 @@ const Links1st = (
     <>
         <LinkItem icon={<NearMe />} title={geti18nString('options_index_setup')} to="/welcome" />
         <LinkItem icon={<Phonelink />} title={geti18nString('options_index_mobile_export')} to="/mobile-setup" />
-        <LinkItem icon={<Code />} title={geti18nString('options_index_devel')} to="/developer" />
+        <LinkItem icon={<Code />} title={geti18nString('options_index_dev')} to="/developer" />
     </>
 )
 const Links2rd = (
