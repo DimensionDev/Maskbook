DimensionDev/Maskbook

commit 51a544b10dacc7c7ecb185b8c6ffce7e494f6238

fix(deps): set @material-ui to newest working version; fix build
See mui-org/material-ui#17158.

src/components/Welcomes/1b1.tsx
@@ -13,7 +13,7 @@ import QRScanner from './QRScanner'
 import { isWKWebkit, iOSHost } from '../../utils/iOS-RPC'
 import { useAsync } from '../../utils/components/AsyncComponent'
 
-const RestoreBox = styled('div')(({ theme }: { theme: Theme }) => ({
+const RestoreBox = styled('div')(({ theme }) => ({
     color: theme.palette.text.hint,
     border: `2px dashed ${theme.palette.divider}`,
     whiteSpace: 'pre-line',

src/database/helpers/person.ts
@@ -28,7 +28,7 @@ export async function personRecordToPerson(record: PersonRecord): Promise<Person
  * Query a person even it is not stored in the database.
  * @param identifier - Identifier for people want to query
  */
-export async function queryPerson(identifier: PersonIdentifier): Promise<PersonUI> {
+export async function queryPerson(identifier: PersonIdentifier): Promise<Person> {
     const person = await queryPersonDB(identifier)
     if (!person)
         return {

src/utils/hooks/useDragAndDrop.ts
@@ -25,14 +25,14 @@ export function useDragAndDrop() {
         e.preventDefault()
         setStatus('drag-enter')
     }, [])
+    const onLeave = React.useCallback((e: React.DragEvent) => {
+        setStatus(undefined)
+    }, [])
     const onCapture = React.useCallback((e: React.DragEvent) => {
         e.preventDefault()
         onChange(e)
         setTimeout(onLeave, 200)
     }, [onChange, onLeave])
-    const onLeave = React.useCallback((e: React.DragEvent) => {
-        setStatus(undefined)
-    }, [])
     return {
         dragEvents: {
             onDragEnterCapture: onEnter,

commit afff8d50ed3783ad861246e3c01b5998332baa7a

fix: style broken (regressed by 51a544b)

src/components/Dashboard/Dashboard.tsx
@@ -28,9 +28,8 @@ const Main = styled('div')(({ theme }: { theme: Theme }) => ({
     },
 }))
 export default function Dashboard(props: Props) {
-    const theme = useTheme()
     return (
-        <Main theme={theme}>
+        <Main>
             <Typography variant="h5">Maskbook Identity Management</Typography>
             <main>
                 {props.identities.map(x => (

src/components/Welcomes/0.tsx
@@ -66,7 +66,7 @@ export default function Welcome({ create, restore, close }: Props) {
                 <Typography variant="subtitle1" className={classes.subtitle}>
                     {geti18nString('welcome_0_description')}
                 </Typography>
-                <LinedBox theme={theme}>
+                <LinedBox>
                     <Box flex={1}>
                         <Typography variant="body1">{geti18nString('welcome_0_new_user')}</Typography>
                         <Typography variant="h6">{geti18nString('welcome_0_connect_maskbook')}</Typography>
@@ -77,7 +77,7 @@ export default function Welcome({ create, restore, close }: Props) {
                         </Button>
                     </VerticalCenter>
                 </LinedBox>
-                <LinedBox theme={theme}>
+                <LinedBox>
                     <Box flex={1}>
                         <Typography variant="body1">{geti18nString('welcome_0_old_user')}</Typography>
                         <Typography variant="h6">{geti18nString('welcome_0_restore_key')}</Typography>

src/components/Welcomes/1a4.manual.tsx
@@ -38,7 +38,6 @@ export default function Manual({ provePost }: Props) {
     }, [])
     return (
         <TextField
-            theme={theme}
             ref={ref}
             readOnly
             onClick={onFocus}

src/components/Welcomes/1b1.tsx
@@ -13,7 +13,7 @@ import QRScanner from './QRScanner'
 import { isWKWebkit, iOSHost } from '../../utils/iOS-RPC'
 import { useAsync } from '../../utils/components/AsyncComponent'
 
-const RestoreBox = styled('div')(({ theme }) => ({
+const RestoreBox = styled('div')(({ theme }: { theme: Theme }) => ({
     color: theme.palette.text.hint,
     border: `2px dashed ${theme.palette.divider}`,
     whiteSpace: 'pre-line',