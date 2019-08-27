df03a0b7dfca74f56330843adc4b168dc412f244
Mingye Wang <arthur200126@gmail.com>
2019-08-27 14:38:31 +0800

    chore(just): cleanup lint tasks

--- just.config.js
+++ just.config.js
@@ -2,9 +2,8 @@ const { task, series, parallel } = require('just-task')
 const { spawn } = require('child_process')
 const path = require('path')
 
-const prettierCommand = async (str, level = 'log') => {
-    const listen = 'onchange "./src/**/*" -i --'
-    await step(`${listen} prettier --${str} "./src/**/*.{ts,tsx}" --loglevel ${level}`)
+const prettierCommand = async (str, level = 'log', watch = false) => {
+    await step(`prettier --${str} "./src/**/*.{ts,tsx}" --loglevel ${level}`, { watch: watch })
 }
 const eslintCommand = 'eslint --ext tsx,ts ./src/ --cache'
 
@@ -16,19 +15,13 @@ task('react/start', () => step('react-app-rewired start'))
 task('react/build', () => step('react-app-rewired build'))
 task('react/test', () => step('react-app-rewired test'))
 
-task('lint', () => prettierCommand('check'))
+task('prettier', () => prettierCommand('check'))
 task('prettier/fix', () => prettierCommand('write', 'warn'))
+task('prettier/autofix', () => prettierCommand('write', 'warn', true))
+task('eslint', () => step(eslintCommand, { withWarn: true }))
 task('eslint/fix', () => step(eslintCommand + ' --fix'))
-task('lint/strictonce', async () => {
-    const p1 = step(eslintCommand, { withWarn: true }).catch(e => e)
-    const p2 = step('prettier --check "./src/**/*.{ts,tsx}" ', { withWarn: true }).catch(e => e)
-    const eslint = await p1
-    const prettier = await p2
-    if (!eslint && !eslint) return
-    if (eslint && prettier) throw new Error('Both ESLint and Prettier failed!\n' + eslint + '\n' + prettier)
-    if (eslint) throw new Error('ESLint check failed!\n' + eslint)
-    if (prettier) throw new Error('Prettier check failed!\n' + prettier)
-})
+task('lint', () => parallel('prettier', 'eslint'))
+task('lint/fix', () => series('prettier/fix', 'eslint/fix'))
 
 task('storybook', () => parallel('lint/fix', 'storybook/serve'))
 task('storybook/serve', () => step('start-storybook -p 9009 -s public --quiet', { withWarn: true }))
@@ -47,10 +40,12 @@ task('install/holoflows', async () => {
  * @param cmd {string} The command you want to run
  * @param [opt] {object} Options
  * @param [opt.withWarn] {boolean} Show warn in stdio
+ * @param [opt.watch] {boolean} Watch continuously
  * @param [opt.env] {NodeJS.ProcessEnv} Environment key-value pairs
  */
-const step = (cmd, opt = { withWarn: process.env.CI === 'true' }) => {
-    const child = spawn(cmd, [], {
+const step = (cmd, opt = { withWarn: process.env.CI === 'true', watch: false }) => {
+    const spawn_cmd = opt.watch ? 'onchange "./src/**/*" -i -- ' + cmd : cmd
+    const child = spawn(spawn_cmd, [], {
         shell: true,
         stdio: ['inherit', 'inherit', opt.withWarn ? 'inherit' : 'ignore'],
         env: opt.env,
