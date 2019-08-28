c5fbe325586312d525b6144ab0cf6bf8f9740588
Misaka <447f.misaka@outlook.com>
2019-08-28 22:29:46 +0800

    feat: auto adb list, exit on browser exit

--- just.config.js
+++ just.config.js
@@ -1,6 +1,7 @@
 const { task, series, parallel } = require('just-task')
 const { spawn } = require('child_process')
 const { watch } = require('chokidar')
+const adb = require('adbkit').createClient()
 const path = require('path')
 const fs = require('fs-extra')
 
@@ -12,7 +13,7 @@ const eslintCommand = 'eslint --ext tsx,ts ./src/ --cache'
 
 task('watch', () => series('react'))
 task('watch/firefox', () => parallel('react', 'load/firefox'))
-task('watch/android', () => parallel('react', 'load/android-firefox'))
+task('watch/android', () => parallel('react', 'load/firefox/android'))
 
 task('react', () => parallel('lint/fix', 'react/start'))
 task('react/start', () => step('react-app-rewired start'))
@@ -20,28 +21,38 @@ task('react/build', () => step('react-app-rewired build'))
 task('react/test', () => step('react-app-rewired test'))
 
 task('storybook', () => parallel('lint/fix', 'storybook/serve'))
-task('storybook/serve', () => step('start-storybook -p 9009 -s public --quiet', { withWarn: true }))
-task('storybook/build', () => step('build-storybook -s public --quiet', { withWarn: true }))
+task('storybook/serve', () => step('start-storybook -p 9009 -s public --quiet'))
+task('storybook/build', () => step('build-storybook -s public --quiet'))
 
-
-const prompt = () => console.log('dir has been changed, starting hot reload service')
+const prompt = () => console.log('[web-ext] assuming built, starting hot reload service')
 
 task('load/firefox', async () => {
     await fs.remove('./dist')
     await untilDirChanged('./dist')
     prompt()
     await step('web-ext run --source-dir ./dist/')
+    process.exit(0);
 })
-task('load/android-firefox', async () => {
+task('load/firefox/android', async () => {
     await fs.remove('./dist')
     await untilDirChanged('./dist')
     prompt()
-    const i = process.argv.findIndex(v => v === '--android-device')
-    if (i > -1) {
-        await step(`web-ext run --target=firefox-android --source-dir ./dist/ --android-device=${process.argv[i + 1]}`)
+    const list = adb.listDevices()
+    const device = (() => {
+        if (list.length === 1) {
+            return list[0]['id']
+        }
+        const i = process.argv.findIndex(v => v === '--android-device')
+        if (i > -1) {
+            return process.argv[i + 1]
+        }
+    })()
+    if (device) {
+        await step(`web-ext run --target=firefox-android --source-dir ./dist/ --android-device=${device}`)
     } else {
-        await step(`web-ext run --target=firefox-android --source-dir ./dist/`)
+        throw new Error('[web-ext] no device specified, exiting')
     }
+    process.exit(0);
 })
 
 task('lint', () => prettierCommand('check'))
@@ -49,8 +60,8 @@ task('lint/fix', () => parallel('lint/prettier/fix', 'lint/eslint/fix'))
 task('lint/prettier/fix', () => prettierCommand('write', 'warn'))
 task('lint/eslint/fix', () => step(eslintCommand + ' --fix'))
 task('lint/strict-once', async () => {
-    const p1 = step(eslintCommand, { withWarn: true }).catch(e => e)
-    const p2 = step('prettier --check "./src/**/*.{ts,tsx}" ', { withWarn: true }).catch(e => e)
+    const p1 = step(eslintCommand).catch(e => e)
+    const p2 = step('prettier --check "./src/**/*.{ts,tsx}"').catch(e => e)
     const eslint = await p1
     const prettier = await p2
     if (!eslint && !eslint) return
@@ -74,7 +85,7 @@ task('install/holoflows', async () => {
  * @param [opt.withWarn] {boolean} Show warn in stdio
  * @param [opt.env] {NodeJS.ProcessEnv} Environment key-value pairs
  */
-const step = (cmd, opt = { withWarn: process.env.CI === 'true' }) => {
+const step = (cmd, opt = { withWarn: true }) => {
     const child = spawn(cmd, [], {
         shell: true,
         stdio: ['inherit', 'inherit', opt.withWarn ? 'inherit' : 'ignore'],
@@ -95,14 +106,13 @@ const step = (cmd, opt = { withWarn: process.env.CI === 'true' }) => {
     })
 }
 
-const untilDirChanged = (dir = '.', timeout = 3000) => {
+const untilDirChanged = (dir = '.', timeout = 4000) => {
     return new Promise((resolve => {
         let last = undefined
-        watch(dir).on('all', (event, path) => {
+        watch(dir).on('all', () => {
             if (last) {
                 clearTimeout(last)
             }
-            console.log(`${event} ${path}`)
             last = setTimeout(resolve, timeout)
         })
     }))
