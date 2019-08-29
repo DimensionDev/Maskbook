8f53c2640375e27afbb94a36dbb5a1fc78735fa8
Misaka <447f.misaka@outlook.com>
2019-08-29 17:11:49 +0800

    feat: firefox now can have saved profile.

--- just.config.js
+++ just.config.js
@@ -1,9 +1,12 @@
 const { task, series, parallel } = require('just-task')
+const args = require('just-task').argv()
 const { spawn } = require('child_process')
 const { watch } = require('chokidar')
 const adb = require('adbkit').createClient()
 const path = require('path')
 const fs = require('fs-extra')
+
+const profile = path.join(process.cwd(), `.firefox`)
 
 const prettierCommand = async (str, level = 'log') => {
     const listen = 'onchange "./src/**/*" -i --'
@@ -12,6 +15,9 @@ const prettierCommand = async (str, level = 'log') => {
 const eslintCommand = 'eslint --ext tsx,ts ./src/ --cache'
 
 task('watch', () => series('react'))
+/**
+ * @cli-argument fresh {boolean} use a new profile to start over.
+ */
 task('watch/firefox', () => parallel('react', 'load/firefox'))
 task('watch/android', () => parallel('react', 'load/firefox/android'))
 
@@ -27,11 +33,23 @@ task('storybook/build', () => step('build-storybook -s public --quiet'))
 const prompt = () => console.log('[web-ext] assuming built, starting hot reload service')
 
 task('load/firefox', async () => {
+    if (!await fs.pathExists(profile) || args.fresh) {
+        try {
+            const timestamp = Date.now().toString()
+            await step(`firefox -CreateProfile "${timestamp} ${path.join(profile, timestamp)}"`)
+        } catch {
+            throw new Error('Cannot locate or create a profile for firefox. Add firefox to your PATH.')
+        }
+        if (args.fresh) {
+            console.warn('new profile generated. old firefox profile cleanable by command "firefox -P".')
+        }
+    }
     await fs.remove('./dist')
     await untilDirChanged('./dist')
     prompt()
-    await step('web-ext run --source-dir ./dist/')
-    process.exit(0);
+    const latestProfile = path.join(profile, last(await fs.readdir(profile)))
+    await step(`web-ext run --firefox-profile=${latestProfile} --keep-profile-changes --source-dir ./dist/`)
+    process.exit(0)
 })
 task('load/firefox/android', async () => {
     await fs.remove('./dist')
@@ -42,17 +60,14 @@ task('load/firefox/android', async () => {
         if (list.length === 1) {
             return list[0]['id']
         }
-        const i = process.argv.findIndex(v => v === '--android-device')
-        if (i > -1) {
-            return process.argv[i + 1]
-        }
+        return args['android-device']
     })()
     if (device) {
-        await step(`web-ext run --target=firefox-android --source-dir ./dist/ --android-device=${device}`)
+        await step(`web-ext run ${args.refresh ? '' : '--keep-profile-changes'} --target=firefox-android --source-dir ./dist/ --android-device=${device}`)
     } else {
         throw new Error('[web-ext] no device specified, exiting')
     }
-    process.exit(0);
+    process.exit(0)
 })
 
 task('lint', () => prettierCommand('check'))
@@ -73,7 +88,7 @@ task('lint/strict-once', async () => {
 task('install', () => series('install/holoflows'))
 task('install/holoflows', async () => {
     const base = path.join(process.cwd(), 'node_modules/@holoflows')
-    if (process.argv.includes('--upgrade')) {
+    if (args.upgrade) {
         await step('yarn upgrade @holoflows/kit')
     }
     await step(`cd ${path.join(base, 'kit')} && yarn && yarn build`)
@@ -116,4 +131,9 @@ const untilDirChanged = (dir = '.', timeout = 4000) => {
             last = setTimeout(resolve, timeout)
         })
     }))
+}
+
+const last = (array) => {
+    const length = array == null ? 0 : array.length
+    return length ? array[length - 1] : undefined
 }
