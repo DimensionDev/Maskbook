7481a31ae8fa7ccba4887521868c15d76a1ec3d6
Jack Works <zjwpeter@gmail.com>
2019-08-29 16:49:18 +0800

    feat: let MessageCenter use Serialization

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,5 +1,7 @@
 import { MessageCenter as MC } from '@holoflows/kit/es'
 import { Person } from '../database'
+import { PostIdentifier } from '../database/type'
+import Serialization from './type-transform/Serialization'
 
 interface UIEvent {}
 interface KeyStoreEvent {
@@ -7,8 +9,27 @@ interface KeyStoreEvent {
     newPerson: Person
     generateKeyPair: undefined
     identityUpdated: undefined
+    decryptionStatusUpdated: {
+        changedPost: PostIdentifier
+    }
 }
 interface TypedMessages extends UIEvent, KeyStoreEvent {}
 
-export const MessageCenter = new MC<TypedMessages>('maskbook-events')
-MessageCenter.writeToConsole = true
+class Channel<T> extends MC<T> {
+    emit<Key extends keyof T>(key: Key, data: T[Key]) {
+        console.log('Emit broadcast', key, data)
+        Serialization.serialization(data).then(data => {
+            super.emit(key, data as any)
+        })
+    }
+    on<Key extends keyof T>(event: Key, handler: (data: T[Key]) => void): void {
+        const handlerWrapped = (data: any) =>
+            Serialization.deserialization(data).then(data => {
+                console.log('Receive broadcast', event, data)
+                handler(data)
+            })
+        handlerWrapped.raw = handler
+        super.on(event, handlerWrapped)
+    }
+}
+export const MessageCenter = new Channel<TypedMessages>('maskbook-events')
