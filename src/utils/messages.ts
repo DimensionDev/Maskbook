c766e3c18b59d435c7d0f39e7d05c1deb95f299d
Jack Works <zjwpeter@gmail.com>
2019-09-03 23:19:53 +0800

    chore: use MessageCenter new built-in serialization

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -16,22 +16,5 @@ interface KeyStoreEvent {
     identityUpdated: undefined
 }
 export interface TypedMessages extends UIEvent, KeyStoreEvent {}
-
-class Channel<T> extends MC<T> {
-    emit<Key extends keyof T>(key: Key, data: T[Key]) {
-        console.log('Emit broadcast', key, data)
-        Serialization.serialization(data).then(data => {
-            super.emit(key, data as any)
-        })
-    }
-    on<Key extends keyof T>(event: Key, handler: (data: T[Key]) => void): void {
-        const handlerWrapped = (data: any) =>
-            Serialization.deserialization(data).then(data => {
-                console.log('Receive broadcast', event, data)
-                handler(data)
-            })
-        handlerWrapped.raw = handler
-        super.on(event, handlerWrapped)
-    }
-}
-export const MessageCenter = new Channel<TypedMessages>('maskbook-events')
+export const MessageCenter = new MC<TypedMessages>('maskbook-events')
+MessageCenter.serialization = Serialization
