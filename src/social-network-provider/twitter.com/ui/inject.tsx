adf6ce0f9a6a2f292431089470e556d3099d8b00
Misaka <447f.misaka@outlook.com>
2019-09-06 20:08:24 +0800

    fix: logic error on getting user screen name

--- src/social-network-provider/twitter.com/ui/inject.tsx
+++ src/social-network-provider/twitter.com/ui/inject.tsx
@@ -10,7 +10,10 @@ import { nop } from '../../../utils/utils'
  * using a closed shadow root can prevent access from other script.
  */
 const newMOW = (i: LiveSelector<HTMLElement, true>) =>
-    new MOW(i).setDomProxyOption({ afterShadowRootInit: { mode: 'closed' } }).startWatch()
+    new MOW(i)
+        .setDomProxyOption({ afterShadowRootInit: { mode: 'closed' } })
+        .enableSingleMode()
+        .startWatch()
 
 const injectPostBox = () => {
     const target = newMOW(newPostEditorBelow())
