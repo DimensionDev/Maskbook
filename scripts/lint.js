5d7cffbdc666604177f8c89817e0f7ba43381ff8
SunriseFox <sunrisefox@qq.com>
2019-09-07 14:04:09 +0800

    fix: adjust configurations to make eslint and tsc work better

--- scripts/lint.js
+++ scripts/lint.js
@@ -7,6 +7,6 @@ process.chdir(base)
 const reportOnly = process.argv.indexOf('--reportOnly') !== -1
 
 ;(async () => {
-  // await spawn('eslint', ['--ext', 'tsx,ts', './src/', ...reportOnly ? [] : ['--cache', '--fix'] ])
+  await spawn('eslint', ['--ignore-path', '.prettierignore', '--ext', 'tsx,ts,jsx,js', './src/', ...reportOnly ? [] : ['--cache', '--fix'] ])
   await spawn('prettier', ['./src/**/*.{ts,tsx}', ...reportOnly ? [ '--check', '--loglevel', 'log'] : ['--write', '--loglevel', 'warn'] ])
 })()
