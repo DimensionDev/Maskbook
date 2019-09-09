b1638b0ab33c1aa134a9e22e26138b481d4f892d
Jack Works <zjwpeter@gmail.com>
2019-09-09 19:33:47 +0800

    feat: add Group preview in Chip and ListItem

--- src/stories/demoPeople.tsx
+++ src/stories/demoPeople.tsx
@@ -1,5 +1,5 @@
-import { Person } from '../database'
-import { PersonIdentifier } from '../database/type'
+import { Person, Group } from '../database'
+import { PersonIdentifier, GroupIdentifier, GroupType } from '../database/type'
 export const demoPeople: Person[] = [
     {
         fingerprint: 'FDFE333CE20ED446AD88F3C8BA3AD1AA5ECAF521',
@@ -29,5 +29,33 @@ export const demoPeople: Person[] = [
         nickname: '温斯顿·史密斯',
         identifier: new PersonIdentifier('gnu-social@example.org', 'test4'),
         groups: [],
+    },
+]
+
+export const demoGroup: Group[] = [
+    {
+        groupName: 'Group A',
+        identifier: new GroupIdentifier('localhost', '12345', GroupType.real),
+        members: demoPeople.map(x => x.identifier),
+    },
+    {
+        groupName: 'Group B',
+        identifier: new GroupIdentifier('localhost', '2345', GroupType.virtual, demoPeople[0].identifier.userId),
+        members: demoPeople.map(x => x.identifier).slice(1),
+    },
+    {
+        groupName: 'Group C',
+        identifier: new GroupIdentifier('localhost', '3456', GroupType.real),
+        members: demoPeople.map(x => x.identifier).slice(2),
+    },
+    {
+        groupName: 'Group D',
+        identifier: new GroupIdentifier('localhost', '4567', GroupType.real),
+        members: demoPeople.map(x => x.identifier).slice(3),
+    },
+    {
+        groupName: 'Group E',
+        identifier: new GroupIdentifier('localhost', '4567', GroupType.real),
+        members: [],
     },
 ]
--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,5 +1,6 @@
 import en from '../_locales/en/messages.json'
 import zh from '../_locales/zh/messages.json'
+import React from 'react'
 
 type I18NStrings = typeof en
 
@@ -7,8 +8,38 @@ const langs: Record<string, I18NStrings> = {
     en,
     zh,
 }
+export function useIntlListFormat() {
+    const formatter = React.useMemo(() => {
+        return new Intl.ListFormat({ type: 'conjunction' })
+    }, [])
+    return React.useCallback((list: string[]) => formatter.format(list), [formatter])
+}
+// See: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/ListFormat
+declare namespace Intl {
+    interface ListFormatOptions {
+        localeMatcher: 'lookup' | 'best fit'
+        type: 'conjunction' | 'disjunction' | 'unit'
+        style: 'long' | 'short' | 'narrow'
+    }
+    class ListFormat {
+        constructor(options?: Partial<ListFormatOptions>)
+        constructor(locales?: string, options?: Partial<ListFormatOptions>)
+        format(str: string[]): string
+    }
+}
+// A simple polyfill. Enough for us.
+if (!Intl.ListFormat) {
+    Intl.ListFormat = class {
+        constructor(public locales?: string | Partial<Intl.ListFormatOptions>, options?: Intl.ListFormatOptions) {}
+        format(string: string[]) {
+            const locale = typeof this.locales === 'string' ? this.locales : getCurrentLanguageFull()
+            if (locale.startsWith('zh')) return string.join('、')
+            return string.join(', ')
+        }
+    }
+}
 export function geti18nString(key: keyof I18NStrings, substitutions: string | string[] = '') {
-    const lang = langs[navigator.language.split('-')[0]] || langs.en
+    const lang = langs[getCurrentScript()] || langs.en
     const string = lang[key] || langs.en[key]
     if (typeof substitutions === 'string') substitutions = [substitutions]
     if (substitutions.length > 3) console.error('Implement this please')
@@ -16,4 +47,11 @@ export function geti18nString(key: keyof I18NStrings, substitutions: string | st
         .replace('$1', substitutions[0])
         .replace('$2', substitutions[1])
         .replace('$3', substitutions[2])
+}
+function getCurrentScript() {
+    return navigator.language.split('-')[0]
+}
+
+function getCurrentLanguageFull() {
+    return navigator.language
 }
