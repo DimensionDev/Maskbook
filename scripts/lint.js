34871833243210e7526d083e8fde52b90b193a2d
Mingye Wang <arthur200126@gmail.com>
2019-09-10 17:22:08 +0800

    chore(husky): cut away the middleman yarn

--- scripts/lint.js
+++ scripts/lint.js
@@ -8,16 +8,20 @@ process.chdir(base)
 const reportOnly = argv.reportOnly
 
 ;(async () => {
-    !argv.noEslint ? await spawn('eslint', [
-        '--ignore-path',
-        '.prettierignore',
-        '--ext',
-        'tsx,ts,jsx,js',
-        './src/',
-        ...(reportOnly ? [] : ['--cache', '--fix']),
-    ]) : null
-    !argv.noPrettier ? await spawn('prettier', [
-        './src/**/*.{ts,tsx,jsx,js}',
-        ...(reportOnly ? ['--check', '--loglevel', 'log'] : ['--write', '--loglevel', 'warn']),
-    ]) : null
+    !argv.noEslint
+        ? await spawn('eslint', [
+              '--ignore-path',
+              '.prettierignore',
+              '--ext',
+              'tsx,ts,jsx,js',
+              './src/',
+              ...(reportOnly ? [] : ['--cache', '--fix']),
+          ])
+        : null
+    !argv.noPrettier
+        ? await spawn('prettier', [
+              './**/*.{ts,tsx,jsx,js}',
+              ...(reportOnly ? ['--check', '--loglevel', 'log'] : ['--write', '--loglevel', 'warn']),
+          ])
+        : null
 })()
