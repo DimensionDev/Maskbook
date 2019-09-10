248acdd67014c15c68ed22dc21f390692349fe3e
Mingye Wang <arthur200126@gmail.com>
2019-09-10 17:20:56 +0800

    chore: enable prettier on configs

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,11 +3,9 @@ const path = require('path')
 
 const base = path.join(__dirname, '../')
 process.chdir(base)
-
 ;(async () => {
-  if (process.argv.indexOf('--upgrade') !== -1)
-    await spawn('yarn', ['upgrade', '@holoflows/kit'])
-  process.chdir('node_modules/@holoflows/kit')
-  await spawn('yarn', ['install'])
-  await spawn('yarn', ['build'])
+    if (process.argv.indexOf('--upgrade') !== -1) await spawn('yarn', ['upgrade', '@holoflows/kit'])
+    process.chdir('node_modules/@holoflows/kit')
+    await spawn('yarn', ['install'])
+    await spawn('yarn', ['build'])
 })()
