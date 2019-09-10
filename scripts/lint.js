ca5c752df3b1849c996a655fadae1a6006b91cac
Mingye Wang <arthur200126@gmail.com>
2019-09-10 17:08:10 +0800

    fix(toolchain): keep the profile and do argparse right
    did someone forget to run prettier?

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,10 +1,11 @@
 const { spawn } = require('./spawn')
 const path = require('path')
+const argv = require('yargs').argv
 
 const base = path.join(__dirname, '../')
 process.chdir(base)
 
-const reportOnly = process.argv.indexOf('--reportOnly') !== -1
+const reportOnly = argv.reportOnly
 
 ;(async () => {
   await spawn('eslint', ['--ignore-path', '.prettierignore', '--ext', 'tsx,ts,jsx,js', './src/', ...reportOnly ? [] : ['--cache', '--fix'] ])
