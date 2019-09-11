ee1423d82eb74b3f93a47763758c30eb23e0703f
Jack Works <zjwpeter@gmail.com>
2019-09-11 12:09:58 +0800

    feat: create user group for old users

--- src/database/migrate/index.ts
+++ src/database/migrate/index.ts
@@ -1,7 +1,9 @@
 import avatar from './old.avatar.1'
 import localKeys from './old.localKeys.1'
 import keys from './old.keystore.1'
+import createUserGroupForOldUsers from './create.user.group.for.old.users'
 
+createUserGroupForOldUsers()
 avatar()
     .finally(() => localKeys())
     .finally(() => keys())
