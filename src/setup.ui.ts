296c96b6a5c7448357cf12623c09725cc13fa32c
Jack Works <zjwpeter@gmail.com>
2019-09-22 15:23:50 +0800

    feat: upgrade @kit and add a DOMProxy debugger

--- src/setup.ui.ts
+++ src/setup.ui.ts
@@ -1,10 +1,11 @@
 import { MessageCenter } from './utils/messages'
 import { definedSocialNetworkUIs, activateSocialNetworkUI } from './social-network/ui'
 import './provider.ui'
-import { LiveSelector, Watcher } from '@holoflows/kit/es'
+import { LiveSelector, Watcher, DOMProxy } from '@holoflows/kit/es'
 
 LiveSelector.enhanceDebugger()
 Watcher.enhanceDebugger()
+DOMProxy.enhanceDebugger()
 Object.assign(globalThis, {
     definedSocialNetworkUIs: definedSocialNetworkUIs,
 })
