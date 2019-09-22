1f8be98e86444eefb9d04ec30d203d59e920021c
Jack Works <zjwpeter@gmail.com>
2019-09-22 13:20:43 +0800

    fix: upgrade react-devtools to 4+

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,10 +1,5 @@
 import './setup.ui'
 import { GetContext } from '@holoflows/kit/es'
-if (process.env.NODE_ENV === 'development') {
-    try {
-        require('react-devtools')
-    } catch {}
-}
 if (GetContext() === 'content') {
     console.log('Maskbook content script loaded')
     require('./extension/content-script/index')
