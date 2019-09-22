680a2fc1d243a7847fa75e0c5af257bebb802401
Jack Works <zjwpeter@gmail.com>
2019-09-22 15:23:50 +0800

    feat: upgrade @kit and add a DOMProxy debugger

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,5 +7,21 @@ process.chdir(base)
     if (process.argv.indexOf('--upgrade') !== -1) await spawn('yarn', ['upgrade', '@holoflows/kit'])
     process.chdir('node_modules/@holoflows/kit')
     await spawn('yarn', ['install'])
-    await spawn('yarn', ['build:tsc'])
+    try {
+        /**
+         * For unknown reason, first time build will raise an exception. But if we build it twice, problem will be fixed
+         *
+         * src/Extension/AutomatedTabTask.ts:119:17 -
+         * error TS2742: The inferred type of 'AutomatedTabTask' cannot be named
+         * without a reference to '@holoflows/kit/node_modules/csstype'.
+         * This is likely not portable.
+         * A type annotation is necessary.
+         * 119 export function AutomatedTabTask<T extends Record<string, (...args: any[]) => PromiseLike<any>>>(
+         *                     ~~~~~~~~~~~~~~~~
+         */
+        await spawn('yarn', ['build:tsc'])
+    } catch (e) {
+        console.log('Build failed, retry one more time.')
+        await spawn('yarn', ['build:tsc'])
+    }
 })()
