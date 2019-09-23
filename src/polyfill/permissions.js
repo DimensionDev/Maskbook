9d454482c675dfc8ab658278b21526be704a8ecb
SunriseFox <sunrisefox@qq.com>
2019-09-24 07:05:33 +0800

    fix: store requested hosts to localStorage

--- src/polyfill/permissions.js
+++ src/polyfill/permissions.js
@@ -1,12 +1,14 @@
 ;(()=>{
-  if (!browser) return
+  if (typeof browser === 'undefined' || !browser) return
   if (browser.permissions) return
   browser.permissions = {}
-  const requestedUrls = []
+  const item = localStorage.getItem('requestedUrls')
+  const requestedUrls = JSON.parse(item) || []
   browser.permissions.request = ({origins}) => {
     for (let i of origins) {
-      if (requestedUrls.indexOf(i) === -1) requestedUrls.push(i)
+      if (!requestedUrls.includes(i)) requestedUrls.push(i)
     }
+    localStorage.setItem('requestedUrls', JSON.stringify(requestedUrls))
     return Promise.resolve(true)
   }
   browser.permissions.getAll = () => {
