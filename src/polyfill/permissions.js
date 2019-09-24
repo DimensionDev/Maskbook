ab8efe87f1c24a282dca1c311a6314b3f7fe3894
SunriseFox <sunrisefox@qq.com>
2019-09-24 11:14:35 +0800

    fix: also patch background page

--- src/polyfill/permissions.js
+++ src/polyfill/permissions.js
@@ -1,17 +1,29 @@
 ;(()=>{
   if (typeof browser === 'undefined' || !browser) return
-  if (browser.permissions) return
-  browser.permissions = {}
-  const item = localStorage.getItem('requestedUrls')
-  const requestedUrls = JSON.parse(item) || []
-  browser.permissions.request = ({origins}) => {
-    for (let i of origins) {
-      if (!requestedUrls.includes(i)) requestedUrls.push(i)
+  const _permissions = browser.permissions || {}
+  browser.permissions = new Proxy(_permissions, {
+    get (target, prop, receiver) {
+      if (prop === 'request') {
+        return ({origins}) => {
+          const item = localStorage.getItem('requestedUrls')
+          let requestedUrls = JSON.parse(item) || []
+          for (let i of origins) {
+            if (!requestedUrls.includes(i)) requestedUrls.push(i)
+          }
+          localStorage.setItem('requestedUrls', JSON.stringify(requestedUrls))
+          return Promise.resolve(true)
+        }
+      } else if (prop === 'getAll') {
+        return () => {
+          const item = localStorage.getItem('requestedUrls')
+          return Promise.resolve({origins: JSON.parse(item) || []})
+        }
+      } else {
+        return Reflect.get(target, prop, receiver)
+      }
+    },
+    set () {
+      return false
     }
-    localStorage.setItem('requestedUrls', JSON.stringify(requestedUrls))
-    return Promise.resolve(true)
-  }
-  browser.permissions.getAll = () => {
-    return Promise.resolve({origins: requestedUrls})
-  }
+  })
 })()
