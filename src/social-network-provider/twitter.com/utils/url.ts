a77ed5390e492d89f6a25167e924481803d58f93
Misaka <447f.misaka@outlook.com>
2019-09-28 13:06:37 +0800

    chore: hostURL

--- src/social-network-provider/twitter.com/utils/url.ts
+++ src/social-network-provider/twitter.com/utils/url.ts
@@ -1,15 +1,15 @@
 import { PersonIdentifier, PostIdentifier } from '../../../database/type'
 import { usernameValidator } from './user'
 import { geti18nString } from '../../../utils/i18n'
-import { host } from '../index'
+import { hostURL } from '../index'
 
 export const toPostUrl = (post: PostIdentifier<PersonIdentifier>) => {
     if (!usernameValidator(post.identifier.userId)) {
         throw new Error(geti18nString('service_username_invalid'))
     }
-    return `https://${host}/${post.identifier.userId}/status/${post.postId}`
+    return `${hostURL}/${post.identifier.userId}/status/${post.postId}`
 }
 
 export const toProfileUrl = (self: PersonIdentifier) => {
-    return `https://${host}/${self.userId}`
+    return `${hostURL}/${self.userId}`
 }
