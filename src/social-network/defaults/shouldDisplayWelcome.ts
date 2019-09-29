e84d6b4b4c013720e40b731227e71649b7afc48e
Misaka <447f.misaka@outlook.com>
2019-09-29 09:15:14 +0800

    chore: clean up type check for networkIdentifier

--- src/social-network/defaults/shouldDisplayWelcome.ts
+++ src/social-network/defaults/shouldDisplayWelcome.ts
@@ -4,10 +4,6 @@ import { getActivatedUI } from '../ui'
 
 export async function shouldDisplayWelcomeDefault() {
     const netId = getActivatedUI().networkIdentifier
-    if (typeof netId === 'function')
-        throw new TypeError(
-            'You cannot use shouldDisplayWelcomeDefault when networkIdentifier is a function. Please implement this function by yourself.',
-        )
     const storage = (await getStorage(netId)) || {}
     if (storage.forceDisplayWelcome) return true
 
