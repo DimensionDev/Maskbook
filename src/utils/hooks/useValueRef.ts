7b6ba10a2437351084db21115f39139bf6f71ff3
Jack Works <zjwpeter@gmail.com>
2019-10-08 16:03:25 +0800

    fix: a race condition in useValueRef

--- src/utils/hooks/useValueRef.ts
+++ src/utils/hooks/useValueRef.ts
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react'
 
 export function useValueRef<T>(ref: ValueRef<T>) {
     const [value, setValue] = useState<T>(ref.value)
-    useEffect(() => ref.addListener(newValue => setValue(newValue)), [ref])
+    useEffect(() => {
+        if (value !== ref.value) setValue(ref.value)
+        return ref.addListener(v => setValue(v))
+    }, [ref, value])
     return value
 }
