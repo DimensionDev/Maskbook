4c9ecfffc94cf7debe48e29b25d22c987dc2dbfe
Jack Works <zjwpeter@gmail.com>
2019-10-11 15:13:25 +0800

    chore: better message for post hash dialog

--- src/extension/background-script/CryptoServices/debugShowAllPossibleHashForPost.ts
+++ src/extension/background-script/CryptoServices/debugShowAllPossibleHashForPost.ts
@@ -1,6 +1,7 @@
 import { PostIVIdentifier } from '../../../database/type'
 import { queryPeopleDB } from '../../../database/people'
 import { hashPostSalt, hashCryptoKey, hashCryptoKeyUnstable } from '../../../network/gun/version.2/hash'
+import { calculateFingerprint } from '../../../database'
 
 export async function debugShowAllPossibleHashForPost(post: PostIVIdentifier, payloadVersion: -38 | -39 | -40) {
     const friends = await queryPeopleDB(x => x.network === post.network)
@@ -13,8 +14,9 @@ export async function debugShowAllPossibleHashForPost(post: PostIVIdentifier, pa
                         x.identifier.toText(),
                         (await hashPostSalt(post.postIV)) +
                             '-' +
-                            (await (payloadVersion >= -38 ? hashCryptoKey : hashCryptoKeyUnstable)(x.publicKey!)),
-                    ] as [string, string],
+                            (await (payloadVersion <= -39 ? hashCryptoKeyUnstable : hashCryptoKey)(x.publicKey!)),
+                        await calculateFingerprint(x.publicKey!),
+                    ] as [string, string, string],
             ),
     )
 }
