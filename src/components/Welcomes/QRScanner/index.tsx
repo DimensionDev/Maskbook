cd45c0c06e9938af4e1deeee796aee6b382efec9
Jack Works <zjwpeter@gmail.com>
2019-10-15 17:51:57 +0800

    fix: wrong call in QRScanner

--- src/components/Welcomes/QRScanner/index.tsx
+++ src/components/Welcomes/QRScanner/index.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react'
 import { useRef } from 'react'
 import { useQRCodeScan } from '../../../utils/hooks/useQRCodeScan'
-import { decompressBackupFile, compressBackupFile } from '../../../utils/type-transform/BackupFileShortRepresentation'
+import { decompressBackupFile } from '../../../utils/type-transform/BackupFileShortRepresentation'
 
 interface Props {
     scanning: boolean
@@ -18,7 +18,7 @@ export default function QRScanner(
     const { scanning, onResult, onError, ...videoProps } = props
     const video = useRef<HTMLVideoElement | null>(null)
 
-    useQRCodeScan(video, scanning, x => compressBackupFile(decompressBackupFile(x)), onError)
+    useQRCodeScan(video, scanning, x => onResult(JSON.stringify(decompressBackupFile(x))), onError)
     return (
         <div style={{ position: 'relative' }}>
             <video ref={video} aria-label="QR Code scanner" {...videoProps} />
