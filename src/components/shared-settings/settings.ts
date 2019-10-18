b95bc7b22aa44986851c8cbad24b40c41d896190
SunriseFox <sunrisefox@qq.com>
2019-10-18 09:04:35 +0800

    feat: use createSettings for network specific synchronized settings

--- src/components/shared-settings/settings.ts
+++ src/components/shared-settings/settings.ts
@@ -1,5 +1,7 @@
-import { createNewSettings } from './createSettings'
-import { PersonIdentifier } from '../../database/type'
+import { createNewSettings, createNetworkSpecificSettings } from './createSettings'
+import { ValueRef } from '@holoflows/kit/es'
+import { MessageCenter } from '../../utils/messages'
+
 /**
  * Does the debug mode on
  */
@@ -20,7 +22,27 @@ export const disableOpenNewTabInBackgroundSettings = createNewSettings<boolean>(
     },
 )
 
-export const currentSelectedIdentity = createNewSettings<any>('currentSelectedIdentity', null, {
-    primary: 'Synchronized current selected identity',
-    secondary: 'Store the serialized identifier',
-})
+const createProxiedSettings = (key: string) => {
+    const target: { [key: string]: ValueRef<string> | string } = {}
+    MessageCenter.on('settingsCreated', p => {
+        if (!(p in target)) {
+            target[p] = createNetworkSpecificSettings<string>(p, key, '')
+        }
+    })
+    return new Proxy(target, {
+        get(target, p: string) {
+            if (!(p in target)) {
+                MessageCenter.emit('settingsCreated', p)
+                target[p] = createNetworkSpecificSettings<string>(p, key, '')
+            }
+            return target[p]
+        },
+        set(target, p: string, value: string) {
+            // @ts-ignore
+            target[p].value = value
+            return true
+        },
+    })
+}
+
+export const currentSelectedIdentity = createProxiedSettings('currentSelectedIdentity')
