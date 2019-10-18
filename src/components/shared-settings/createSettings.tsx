6eff43f5135a7ffdc5f01d18b9646324d38d6b6d
SunriseFox <sunrisefox@qq.com>
2019-10-18 09:04:35 +0800

    feat: use createSettings for network specific synchronized settings

--- src/components/shared-settings/createSettings.tsx
+++ src/components/shared-settings/createSettings.tsx
@@ -9,34 +9,55 @@ interface SettingsTexts {
     secondary?: string
 }
 const texts = new WeakMap<ValueRef<any>, SettingsTexts>()
-export function createNewSettings<T extends browser.storage.StorageValue>(
+
+function createInternalSettings<T extends browser.storage.StorageValue>(
+    storage: string,
     key: string,
     initialValue: T,
-    UITexts: SettingsTexts,
     comparer: (a: T, b: T) => boolean = (a, b) => a === b,
 ) {
     const settings = new ValueRef(initialValue, comparer)
-    texts.set(settings, UITexts)
-
-    update()
+    const instanceKey = `${storage}+${key}`
+    update(instanceKey)
     settings.addListener(async newVal => {
-        const stored = ((await browser.storage.local.get()).settings as object) || {}
+        const stored = ((await browser.storage.local.get())[storage] as object) || {}
         browser.storage.local.set({
-            settings: { ...stored, [key]: newVal },
+            [storage]: { ...stored, [key]: newVal },
         })
-        MessageCenter.emit('settingsUpdated', undefined)
+        MessageCenter.emit('settingsUpdated', instanceKey)
     })
     MessageCenter.on('settingsUpdated', update)
-    async function update() {
+    async function update(receivedKey: string) {
+        if (receivedKey !== instanceKey) return
         if (typeof browser === 'object') {
             const value = await browser.storage.local.get()
-            const stored = value.settings
+            const stored = value[storage]
             if (typeof stored === 'object' && stored !== null && key in (stored as any)) {
                 settings.value = Reflect.get(stored, key)
             }
         }
     }
     return settings
+}
+
+export function createNewSettings<T extends browser.storage.StorageValue>(
+    key: string,
+    initialValue: T,
+    UITexts: SettingsTexts,
+    comparer: (a: T, b: T) => boolean = (a, b) => a === b,
+) {
+    const settings = createInternalSettings('settings', key, initialValue, comparer)
+    texts.set(settings, UITexts)
+    return settings
+}
+
+export function createNetworkSpecificSettings<T extends browser.storage.StorageValue>(
+    network: string,
+    key: string,
+    initialValue: T,
+    comparer: (a: T, b: T) => boolean = (a, b) => a === b,
+) {
+    return createInternalSettings(network, key, initialValue, comparer)
 }
 
 export function useSettingsUI<T>(settingsRef: ValueRef<T>) {
