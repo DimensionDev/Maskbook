bc43c0d9c5b7645f9ce951985d0095acd0f38688
Jack Works <zjwpeter@gmail.com>
2019-10-23 16:46:11 +0800

    chore: remove some useless comments

--- src/web-workers/QRCode.ts
+++ src/web-workers/QRCode.ts
@@ -1,12 +1,8 @@
-// ! TODO:
-// ! find a easy way for using DOM context and worker context separeatly
-// ! then we can remove @ts-ignore here and make worker type safe
 import jsQR from 'jsqr'
 
-// @ts-ignore
-addEventListener('message', ev => {
+const self = (globalThis as unknown) as Worker & WindowOrWorkerGlobalScope
+self.addEventListener('message', ev => {
     const [data, width, height] = ev.data as Parameters<typeof jsQR>
 
-    // @ts-ignore
-    postMessage(jsQR(data, width, height))
+    self.postMessage(jsQR(data, width, height))
 })
