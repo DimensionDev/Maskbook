f0f53a4fa2e3bc511fbed7d3a462ff6564da29e8
Misaka Kumomi <447f.misaka@outlook.com>
2019-10-23 19:42:06 +0800

    fix: add timeout, rewrite type

--- src/social-network-provider/twitter.com/utils/postBox.ts
+++ src/social-network-provider/twitter.com/utils/postBox.ts
@@ -1,7 +1,7 @@
 import { sleep, untilDocumentReady } from '../../../utils/utils'
 import { newPostEditorFocusAnchor, newPostEditorSelector } from './selector'
 import { equal } from '../../../utils/assert'
-import { untilElementAvailable } from '../../../utils/holoflowKits'
+import { untilElementAvailable } from '../../../utils/holoflowKits/untilElementAvailable'
 
 export const postBoxInPopup = () => {
     return window.location.pathname.includes('compose')
