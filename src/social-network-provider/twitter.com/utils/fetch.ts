64e2a95e24984ad5689717560e67758591d44f35
Misaka Kumomi <447f.misaka@outlook.com>
2019-10-25 17:57:53 +0800

    fix: collecting user name from biocard

--- src/social-network-provider/twitter.com/utils/fetch.ts
+++ src/social-network-provider/twitter.com/utils/fetch.ts
@@ -1,7 +1,23 @@
 import { bioCard } from './selector'
 import { regexMatch } from '../../../utils/utils'
 import { notNullable } from '../../../utils/assert'
-import { defaultTo, join, trim } from 'lodash-es'
+import { defaultTo, join } from 'lodash-es'
+
+/**
+ * @example
+ * parseNameArea("TheMirror\n(●'◡'●)@1\n@MisakaMirror")
+ * >>> {
+ *      name: "TheMirror(●'◡'●)@1",
+ *      handle: "MisakaMirror"
+ * }
+ */
+const parseNameArea = (t: string) => {
+    const r = regexMatch(t, /^((.+\s*)*)@(.+)$/, null)!
+    return {
+        name: r[1].replace(/\n+/g, ''),
+        handle: r[3],
+    }
+}
 
 export const bioCardParser = () => {
     const avatar = notNullable(
@@ -10,20 +26,23 @@ export const bioCardParser = () => {
             .map(x => x.src)
             .evaluate(),
     )
-    const userNames = notNullable(
-        bioCard()
-            .map(x => (x.children[1] as HTMLElement).innerText.split('\n'))
-            .evaluate(),
+    const nameArea = parseNameArea(
+        notNullable(
+            bioCard()
+                .map(x => (x.children[1] as HTMLElement).innerText)
+                .evaluate(),
+        ),
     )
     const bio = notNullable(
         bioCard()
             .map(x => (x.children[2] as HTMLElement).innerHTML)
             .evaluate(),
     )
+    debugger
     return {
         avatar,
-        name: userNames[0],
-        handle: notNullable(regexMatch(userNames[1], /@(.+)/)),
+        name: nameArea.name,
+        handle: nameArea.handle,
         bio,
     }
 }
@@ -43,19 +62,15 @@ export const postContentParser = (node: HTMLElement) => {
  * @return          link to avatar.
  */
 export const postParser = async (node: HTMLElement) => {
-    const nameArea = regexMatch(
-        notNullable(node.children[1].querySelector<HTMLAnchorElement>('a')).innerText,
-        /^((.+\s*)*)@(.+)$/,
-        null,
-    )!
+    const nameArea = parseNameArea(notNullable(node.children[1].querySelector<HTMLAnchorElement>('a')).innerText)
     const avatarElement = node.children[0].querySelector<HTMLImageElement>(`img[src*="twimg.com"]`)
     const pidLocation = defaultTo(
         node.children[1].querySelector<HTMLAnchorElement>('a[href*="status"]'),
         node.parentElement!.querySelector<HTMLAnchorElement>('a[href*="status"]'),
     )
     return {
-        name: trim(nameArea[1], '\n'),
-        handle: nameArea[3],
+        name: nameArea.name,
+        handle: nameArea.handle,
         // pid may not available at promoted tweet
         pid: pidLocation ? regexMatch(pidLocation!.href, /status\/(\d+)/, 1)! : undefined,
         avatar: avatarElement ? avatarElement.src : undefined,
