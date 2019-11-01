54fdf901e8889df4ce75a254c033c60b55896c40
Jack Works <zjwpeter@gmail.com>
2019-11-01 15:30:38 +0800

    chore: better warning block

--- src/extension/options-page/Backup.tsx
+++ src/extension/options-page/Backup.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router'
-import { Dialog, DialogTitle, DialogContent, Button, ButtonGroup } from '@material-ui/core'
+import { Dialog, DialogTitle, DialogContent, Button, ButtonGroup, SnackbarContent } from '@material-ui/core'
 
 import CloudDownloadIcon from '@material-ui/icons/CloudDownload'
 import CenterFocusWeakIcon from '@material-ui/icons/CenterFocusWeak'
@@ -12,6 +12,7 @@ import { compressBackupFile } from '../../utils/type-transform/BackupFileShortRe
 import { BackupJSONFileLatest } from '../../utils/type-transform/BackupFile'
 import { PersonIdentifier } from '../../database/type'
 import { QrCode } from '../../components/shared/qrcode'
+import { amber } from '@material-ui/core/colors'
 
 const useStyles = makeStyles(theme =>
     createStyles({
@@ -40,6 +41,13 @@ const useStyles = makeStyles(theme =>
             ...(theme.palette.type === 'dark'
                 ? {}
                 : { backgroundColor: 'rgb(238,238,238)', color: 'rgb(118,118,118)' }),
+        },
+        warning: {
+            backgroundColor: amber[600],
+            color: theme.palette.getContrastText(amber[600]),
+            '& > div': {
+                margin: 'auto',
+            },
         },
     }),
 )
@@ -102,13 +110,13 @@ export default function BackupDialog() {
                                 </Typography>
                             )}
                         </div>
-                        <Typography color="error" variant="h6" style={{ textAlign: 'center' }}>
-                            {geti18nString('options_mobile_export_subtitle')}
-                        </Typography>
+                        <SnackbarContent
+                            classes={{ root: classes.warning }}
+                            message={geti18nString('options_mobile_export_subtitle')}
+                        />
                     </>
-                ) : (
-                    <div style={{ height: '5vh' }}></div>
-                )}
+                ) : null}
+                <div style={{ height: '5vh' }}></div>
             </DialogContent>
         </Dialog>
     )
