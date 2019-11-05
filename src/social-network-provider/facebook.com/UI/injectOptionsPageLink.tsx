b2b57ba4c5cd1d1a377bbecb932920942c06b4c1
SunriseFox <sunrisefox@qq.com>
2019-11-05 21:50:27 +0800

    feat: injectOptionsPageLink use same style of facebook

--- src/social-network-provider/facebook.com/UI/injectOptionsPageLink.tsx
+++ src/social-network-provider/facebook.com/UI/injectOptionsPageLink.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { getUrl } from '../../../utils/utils'
 import { LiveSelector, MutationObserverWatcher } from '@holoflows/kit'
 import { renderInShadowRoot } from '../../../utils/jss/renderInShadowRoot'
 import { makeStyles } from '@material-ui/core'
@@ -17,21 +18,24 @@ export function injectOptionsPageLinkAtFacebook() {
 
 const useStyle = makeStyles({
     root: {
-        display: 'block',
+        display: 'flex',
         minHeight: 42,
         lineHeight: '42px',
+        fontSize: '1rem',
         borderBottom: '1px solid rgb(233, 234, 237)',
-        paddingLeft: '1em',
     },
+    icon: { transform: 'translate(-1px, 1px)', margin: '4px 5px 0 7px' },
 })
 function Link() {
     const style = useStyle()
+    const icon = getUrl('/maskbook-icon-padded.png')
     return (
         <a
             className={style.root}
             onClick={() => {
                 Services.Welcome.openOptionsPage('/')
             }}>
+            <img alt="" width={34} height={34} src={icon} className={style.icon} />
             Maskbook Settings
         </a>
     )
