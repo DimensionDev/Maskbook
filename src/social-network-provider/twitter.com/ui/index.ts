540fbf235236a0d947d893c1a24fc78c243769b9
Jack Works <jackworks@protonmail.com>
2019-11-22 17:20:07 +0800

    feat: add dark mode for twitter

--- src/social-network-provider/twitter.com/ui/index.ts
+++ src/social-network-provider/twitter.com/ui/index.ts
@@ -9,6 +9,9 @@ import { twitterUIFetch } from './fetch'
 import { twitterUIInjections } from './inject'
 import { InitGroupsValueRef } from '../../../social-network/defaults/GroupsValueRef'
 import { twitterUrl } from '../utils/url'
+import React from 'react'
+import { createMuiTheme } from '@material-ui/core'
+import { MaskbookDarkTheme } from '../../../utils/theme'
 
 export const instanceOfTwitterUI = defineSocialNetworkUI({
     ...sharedSettings,
@@ -41,4 +44,48 @@ export const instanceOfTwitterUI = defineSocialNetworkUI({
         setStorage(twitterUrl.hostIdentifier, { userIgnoredWelcome: true, forceDisplayWelcome: false }).then()
     },
     shouldDisplayWelcome: shouldDisplayWelcomeDefault,
+    useColorScheme() {
+        const [currentScheme, setScheme] = React.useState<'light' | 'dark'>('light')
+        React.useLayoutEffect(() => {
+            const id = setInterval(() => setScheme(isDarkMode()), 2000)
+            return () => clearInterval(id)
+        })
+        return currentScheme
+    },
+    darkTheme: createMuiTheme({
+        ...MaskbookDarkTheme,
+        palette: {
+            ...MaskbookDarkTheme.palette,
+            background: {
+                ...MaskbookDarkTheme.palette.background,
+                get paper() {
+                    return `rgb(${getBackgroundColor().join(',')})`
+                },
+            },
+        },
+    }),
 })
+
+function isDarkMode(): 'dark' | 'light' {
+    const [r, g, b] = getBackgroundColor()
+    if (r < 68 && g < 68 && b < 68) return 'dark'
+    return 'light'
+}
+
+function getBackgroundColor(): [number, number, number] {
+    const background = String(
+        // @ts-ignore CSSOM
+        document.body?.computedStyleMap?.()?.get?.('background-color') ??
+            // Old CSSOM
+            document.body.style.backgroundColor,
+    )
+    const match = background.match(/rgb\((\d+?), +(\d+?), +(\d+?)\)/)
+    if (match) {
+        const [_, r, g, b] = match
+        const nr = parseInt(r)
+        const ng = parseInt(g)
+        const nb = parseInt(b)
+        return [nr, ng, nb]
+    }
+    return [255, 255, 255]
+}
