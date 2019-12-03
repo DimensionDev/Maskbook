0e624f5b8132fcadfdda468964c3fdafecac3f0b
guanbinrui <guanbinrui@dimension.im>
2019-12-03 16:25:51 +0800

    feat(ui): add SelectRecipients component

---
+++ src/components/shared/SelectRecipients/ClickableChip.tsx
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core'
+import Chip, { ChipProps } from '@material-ui/core/Chip'
+
+export interface ClickableChipProps {
+    ChipProps?: ChipProps
+}
+
+const useStyles = makeStyles({
+    root: {
+        marginRight: 6,
+        marginBottom: 6,
+        cursor: 'pointer',
+    },
+})
+
+export function ClickableChip(props: ClickableChipProps) {
+    const classes = useStyles()
+
+    return <Chip className={classes.root} {...props.ChipProps} />
+}
