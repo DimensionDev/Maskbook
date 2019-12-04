52a94c5dbdea1835c7508e14658046b1481ca89c
guanbinrui <guanbinrui@dimension.im>
2019-12-04 15:35:28 +0800

    fix: update user groups immediately
    listening joinGroup event close #461

--- src/social-network/defaults/GroupsValueRef.ts
+++ src/social-network/defaults/GroupsValueRef.ts
@@ -14,14 +14,24 @@ export function InitGroupsValueRef(
     groupIDs: string[] = [PreDefinedVirtualGroupNames.friends],
 ) {
     create(network, self.groupsRef, groupIDs)
+    MessageCenter.on('joinGroup', ({ group, newMembers }) => join(group, self.groupsRef, newMembers))
+    MessageCenter.on(
+        'groupsChanged',
+        createDataWithIdentifierChangedListener(self.groupsRef, x => x.of.identifier.network === network),
+    )
+}
+
+function join(groupIdentifier: GroupIdentifier, ref: ValueRef<Group[]>, members: ProfileIdentifier[]) {
+    const group = ref.value.find(g => g.identifier.equals(groupIdentifier))
+    if (!group) {
+        return
+    }
+    group.members = [...group.members, ...members.filter(member => !group.members.some(m => m.equals(member)))]
+    ref.value = [...ref.value]
 }
 
 async function query(network: string, ref: ValueRef<Group[]>) {
     Services.UserGroup.queryUserGroups(network).then(p => (ref.value = p))
-    MessageCenter.on(
-        'groupsChanged',
-        createDataWithIdentifierChangedListener(ref, x => x.of.identifier.network === network),
-    )
 }
 
 async function create(network: string, ref: ValueRef<Group[]>, groupIDs: string[]) {
