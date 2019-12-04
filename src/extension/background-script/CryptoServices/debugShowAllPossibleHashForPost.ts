69c36002c7c5cf4442b07c25501b9a5087324514
Jack Works <jackworks@protonmail.com>
2019-12-04 13:41:49 +0800

    fix: accidental call on UI path

--- src/extension/background-script/CryptoServices/debugShowAllPossibleHashForPost.ts
+++ src/extension/background-script/CryptoServices/debugShowAllPossibleHashForPost.ts
@@ -1,7 +1,7 @@
 import { PostIVIdentifier } from '../../../database/type'
 import { hashPostSalt, hashCryptoKey, hashCryptoKeyUnstable } from '../../../network/gun/version.2/hash'
-import { getActivatedUI } from '../../../social-network/ui'
 import { queryProfilesWithQuery, queryPublicKey } from '../../../database'
+import { getNetworkWorker } from '../../../social-network/worker'
 
 export async function debugShowAllPossibleHashForPost(post: PostIVIdentifier, payloadVersion: -38 | -39 | -40) {
     const friends = await queryProfilesWithQuery(x => x.identifier.network === post.network)
@@ -12,7 +12,7 @@ export async function debugShowAllPossibleHashForPost(post: PostIVIdentifier, pa
                 async x =>
                     [
                         x.identifier.toText(),
-                        (await hashPostSalt(post.postIV, getActivatedUI().gunNetworkHint)) +
+                        (await hashPostSalt(post.postIV, getNetworkWorker(post).gunNetworkHint)) +
                             '-' +
                             (await (payloadVersion <= -39 ? hashCryptoKeyUnstable : hashCryptoKey)(
                                 (await queryPublicKey(x.identifier))!,
