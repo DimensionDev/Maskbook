dbda45d386133bd9274104bacbf45a00150d7449
guanbinrui <guanbinrui@dimension.im>
2019-12-04 18:52:41 +0800

    fix: unable to locate profile node when header photo absent
    close #465

--- src/social-network-provider/twitter.com/utils/selector.ts
+++ src/social-network-provider/twitter.com/utils/selector.ts
@@ -16,6 +16,7 @@ export const bioCard = () =>
         [
             '.profile', // legacy twitter
             'a[href*="header_photo"] ~ div', // new twitter
+            'div[data-testid="primaryColumn"] > div > div:last-child > div > div > div > div ~ div', // new twitter without header photo
         ].join(),
     )
 
