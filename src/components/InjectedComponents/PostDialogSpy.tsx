b14bde2ea732084ebd298529e3f74e662c3606ad
guanbinrui <guanbinrui@dimension.im>
2019-12-09 15:56:40 +0800

    feat: inject PostDialogSpy

--- src/components/InjectedComponents/PostDialogSpy.tsx
+++
@@ -1,8 +0,0 @@
-import { MessageCenter } from '../../utils/messages'
-
-export interface PostDialogSpyProps {}
-
-export function PostDialogSpy(props: PostDialogSpyProps) {
-    MessageCenter.emit('startCompose', undefined)
-    return <div style={{ display: 'none' }}></div>
-}
--- src/social-network-provider/twitter.com/ui/injectPostDialogSpy.tsx
+++ src/social-network-provider/twitter.com/ui/injectPostDialogSpy.tsx
@@ -2,7 +2,7 @@ import { twitterUrl } from '../utils/url'
 import { MutationObserverWatcher } from '@holoflows/kit/es'
 import { postPopupInjectPointSelector } from '../utils/selector'
 import { renderInShadowRoot } from '../../../utils/jss/renderInShadowRoot'
-import { PostDialogSpy } from '../../../components/InjectedComponents/PostDialogSpy'
+import { MessageCenter } from '../../../utils/messages'
 
 export function injectPostDialogSpyAtTwitter() {
     if (location.hostname.indexOf(twitterUrl.hostIdentifier) === -1) return
@@ -15,9 +15,11 @@ export function injectPostDialogSpyAtTwitter() {
             subtree: true,
         })
 
+    watcher.addListener('onAdd', () => MessageCenter.emit('startCompose', undefined, true))
+    watcher.addListener('onRemove', () => MessageCenter.emit('cancelCompose', undefined, true))
     renderInShadowRoot(<PostDialogSpyAtTwitter />, watcher.firstDOMProxy.afterShadow)
 }
 
 function PostDialogSpyAtTwitter() {
-    return <PostDialogSpy />
+    return <div style={{ display: 'none' }}></div>
 }
