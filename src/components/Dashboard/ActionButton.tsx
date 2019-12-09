77344ff1a8f29d7581c1422e28ad82bb36f4202b
SunriseFox <sunrisefox@qq.com>
2019-12-09 18:26:19 +0800

    chore: backup database

--- src/components/Dashboard/ActionButton.tsx
+++ src/components/Dashboard/ActionButton.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import { Button, Typography } from '@material-ui/core'
+import { Button, Typography, CircularProgress } from '@material-ui/core'
 import { ButtonProps } from '@material-ui/core/Button'
 
 interface ButtoneProps extends ButtonProps {
     width?: number | string
+    loading?: boolean
     // TODO: figure out type
     [key: string]: any
 }
 
 export default function ActionButton(props: ButtoneProps) {
-    const { width, children, className, ...p } = props
+    const { width, loading, children, className, ...p } = props
     return (
-        <Button className={'actionButton ' + className} style={{ width }} {...p}>
+        <Button
+            disabled={loading}
+            startIcon={loading && <CircularProgress size={24} />}
+            className={'actionButton ' + className}
+            style={{ width }}
+            {...p}>
             <Typography variant="button" children={children}></Typography>
         </Button>
     )
