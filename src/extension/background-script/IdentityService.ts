b79390bc4a0cac15eb14c2d78cb6f64bc121f2f5
SunriseFox <sunrisefox@qq.com>
2019-12-09 11:17:17 +0800

    chore: export createPersonaByMnemonic from identityService

--- src/extension/background-script/IdentityService.ts
+++ src/extension/background-script/IdentityService.ts
@@ -53,7 +53,7 @@ export function removeProfile(id: ProfileIdentifier): Promise<void> {
 //#endregion
 
 //#region Persona
-export { queryPersona } from '../../database'
+export { queryPersona, createPersonaByMnemonic } from '../../database'
 export async function queryPersonas(identifier?: PersonaIdentifier, requirePrivateKey = false): Promise<Persona[]> {
     if (typeof identifier === 'undefined')
         return (await queryPersonasDB(k => (requirePrivateKey ? !!k.privateKey : true))).map(personaRecordToPersona)
