2e2f2c4c6d2e9afae21d2ae8ece0415141cac3f2
SunriseFox <sunrisefox@qq.com>
2019-12-09 13:42:02 +0800

    chore: make getMyProveBio accept personaIdentifier

--- src/extension/background-script/CryptoServices/getMyProveBio.ts
+++ src/extension/background-script/CryptoServices/getMyProveBio.ts
@@ -1,13 +1,15 @@
 import { compressSecp256k1Key } from '../../../utils/type-transform/SECP256k1-Compression'
-import { ProfileIdentifier } from '../../../database/type'
+import { ProfileIdentifier, PersonaIdentifier } from '../../../database/type'
 import { getNetworkWorker } from '../../../social-network/worker'
 import { queryPublicKey } from '../../../database'
 //#endregion
 //#region ProvePost, create & verify
-export async function getMyProveBio(whoAmI: ProfileIdentifier): Promise<string | null> {
+export async function getMyProveBio(whoAmI: ProfileIdentifier | PersonaIdentifier): Promise<string | null> {
     const myIdentity = await queryPublicKey(whoAmI)
     if (!myIdentity) return null
     const pub = await crypto.subtle.exportKey('jwk', myIdentity)
     const compressed = compressSecp256k1Key(pub, 'public')
-    return getNetworkWorker(whoAmI.network).publicKeyEncoder(compressed)
+    // FIXME: wait for #191
+    return compressed
+    // return getNetworkWorker(whoAmI.network).publicKeyEncoder(compressed)
 }
