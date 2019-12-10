cad94414ee2c976a6aee49034d028c81dbd6b9f0
SunriseFox <sunrisefox@qq.com>
2019-12-10 15:25:48 +0800

    chore: persona card setting icon cursor

--- src/extension/options-page/DashboardComponents/PersonaCard.tsx
+++ src/extension/options-page/DashboardComponents/PersonaCard.tsx
@@ -46,6 +46,9 @@ const useStyles = makeStyles(theme =>
                 },
             },
         },
+        cursor: {
+            cursor: 'pointer',
+        },
     }),
 )
 
@@ -120,7 +123,7 @@ export default function PersonaCard({ persona }: Props) {
                                 {persona.nickname}
                             </span>
                             <Typography className="fullWidth" variant="body1" component="span" color="textSecondary">
-                                <SettingsIcon fontSize="small" onClick={handleClick} />
+                                <SettingsIcon className={classes.cursor} fontSize="small" onClick={handleClick} />
                                 <Menu
                                     anchorEl={anchorEl}
                                     keepMounted
