7d4aeaa8ba691110036a68db89ce6075ff655111
guanbinrui <guanbinrui@dimension.im>
2019-12-12 13:33:16 +0800

    fix: distinguish current groups by current identity #494

--- src/components/DataSource/useActivatedUI.ts
+++ src/components/DataSource/useActivatedUI.ts
@@ -20,3 +20,9 @@ export function useCurrentIdentity(noDefault?: boolean): Profile | null {
     const current = useValueRef(currentSelectedIdentity[getActivatedUI().networkIdentifier])
     return all.find(i => i.identifier.toText() === current) || (noDefault ? null : all[0])
 }
+export function useCurrentGroupsList() {
+    const groups = useGroupsList()
+    const currentIdentity = useCurrentIdentity()
+    if (!currentIdentity) return []
+    return groups.filter(x => x.identifier.ownerIdentifier.equals(currentIdentity.identifier))
+}
