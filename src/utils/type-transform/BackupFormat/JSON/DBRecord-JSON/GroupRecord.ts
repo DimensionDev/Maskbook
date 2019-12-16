dccce1f46968e2d52ee6276ca4bea68141a35567
Jack Works <jackworks@protonmail.com>
2019-12-16 13:45:17 +0800

    chore: add transform function for db records and its inverse

--- src/utils/type-transform/BackupFormat/JSON/DBRecord-JSON/GroupRecord.ts
+++ src/utils/type-transform/BackupFormat/JSON/DBRecord-JSON/GroupRecord.ts
@@ -1,5 +1,6 @@
 import { GroupRecord } from '../../../../../database/group'
 import { BackupJSONFileLatest } from '../latest'
+import { Identifier, GroupIdentifier, ProfileIdentifier } from '../../../../../database/type'
 
 export function GroupRecordToJSONFormat(group: GroupRecord): BackupJSONFileLatest['userGroups'][0] {
     return {
@@ -7,5 +8,14 @@ export function GroupRecordToJSONFormat(group: GroupRecord): BackupJSONFileLates
         identifier: group.identifier.toText(),
         members: group.members.map(x => x.toText()),
         banned: group.banned?.map(x => x.toText()),
+    }
+}
+
+export function GroupRecordFromJSONFormat(group: BackupJSONFileLatest['userGroups'][0]): GroupRecord {
+    return {
+        groupName: group.groupName,
+        identifier: Identifier.fromString(group.identifier, GroupIdentifier).unwrap('Cast failed'),
+        members: group.members.map(x => Identifier.fromString(x, ProfileIdentifier).unwrap('Cast failed')),
+        banned: group.banned?.map(x => Identifier.fromString(x, ProfileIdentifier).unwrap('Cast failed')),
     }
 }
