64b80488b18e7b0e4a2af8c07f1c68ad8e4c690d
SunriseFox <sunrisefox@qq.com>
2019-12-17 16:21:27 +0800

    chore: enforce QR code to 260px

--- src/components/shared/qrcode.tsx
+++ src/components/shared/qrcode.tsx
@@ -4,12 +4,13 @@ import { useRef, useEffect } from 'react'
 
 export function QrCode(props: {
     text: string
+    options?: qr.QRCodeRenderersOptions
     canvasProps?: React.DetailedHTMLProps<React.CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement>
 }) {
     const ref = useRef<HTMLCanvasElement | null>(null)
     useEffect(() => {
         if (!ref.current) return
-        qr.toCanvas(ref.current, props.text)
-    }, [props.text])
+        qr.toCanvas(ref.current, props.text, props.options)
+    }, [props.options, props.text])
     return <canvas {...props.canvasProps} ref={ref} />
 }
