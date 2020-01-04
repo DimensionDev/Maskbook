8905dd74f2f24ca2677c115d82040e5acb4596b3
guanbinrui <guanbinrui@dimension.im>
2020-01-04 13:40:58 +0800

    fix(ui): additional post box is over wide in twitter

--- src/social-network-provider/twitter.com/ui/inject.tsx
+++ src/social-network-provider/twitter.com/ui/inject.tsx
@@ -7,6 +7,14 @@ import { PostInfo, SocialNetworkUIInjections } from '../../../social-network/ui'
 import { injectPostInspectorDefault } from '../../../social-network/defaults/injectPostInspector'
 import { injectKnownIdentityAtTwitter } from './injectKnownIdentity'
 
+function AdditionalPostBoxInTwitter() {
+    return (
+        <div style={{ maxWidth: 600 }}>
+            <AdditionalPostBox />
+        </div>
+    )
+}
+
 // Closing these shadowRoot prevents external access to them.
 const newMOW = (i: LiveSelector<HTMLElement, true>) =>
     new MOW(i)
@@ -23,9 +31,9 @@ const emptyNode = document.createElement('div')
 
 const injectPostBox = () => {
     const target = newMOW(newPostEditorBelow().map(x => (hasDraftEditor(x) ? x : emptyNode)))
-    renderInShadowRoot(<AdditionalPostBox />, target.firstDOMProxy.afterShadow)
+    renderInShadowRoot(<AdditionalPostBoxInTwitter />, target.firstDOMProxy.afterShadow)
     const popUpTarget = newMOW(postPopupInjectPointSelector())
-    renderInShadowRoot(<AdditionalPostBox />, popUpTarget.firstDOMProxy.afterShadow)
+    renderInShadowRoot(<AdditionalPostBoxInTwitter />, popUpTarget.firstDOMProxy.afterShadow)
 }
 
 const injectPostInspector = (current: PostInfo) => {
