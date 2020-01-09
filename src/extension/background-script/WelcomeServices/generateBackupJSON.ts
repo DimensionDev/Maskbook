f73f778c61577a7010e662dba4d791f124241e65
Jack Works <jackworks@protonmail.com>
2020-01-09 14:16:09 +0800

    fix: some post key is not exportable

--- src/extension/background-script/WelcomeServices/generateBackupJSON.ts
+++ src/extension/background-script/WelcomeServices/generateBackupJSON.ts
@@ -56,10 +56,16 @@ export async function generateBackupJSON(opts: Partial<BackupOptions> = {}): Pro
         const data = await queryPostsDB(() => true)
         await Promise.all(
             data.map(
-                async x => x.postCryptoKey && map.set(x.postCryptoKey, await CryptoKeyToJsonWebKey(x.postCryptoKey)),
+                async x =>
+                    x.postCryptoKey &&
+                    // ? Some old post key is not exportable. Skip them if export failed.
+                    CryptoKeyToJsonWebKey(x.postCryptoKey).then(
+                        y => map.set(x.postCryptoKey!, y),
+                        () => {},
+                    ),
             ),
         )
-        data.forEach(x => posts.push(PostRecordToJSONFormat(x, map)))
+        data.filter(x => !!map.get(x.postCryptoKey!)).forEach(x => posts.push(PostRecordToJSONFormat(x, map)))
     }
 
     async function backupAllUserGroups() {
