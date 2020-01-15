6a069a37b95b2782fec35802ca6aceed14dd82ec
SunriseFox <sunrisefox@qq.com>
2020-01-15 18:35:16 +0800

    chore: red packet state

--- src/components/InjectedComponents/StructuredMessage/RedPacket.tsx
+++ src/components/InjectedComponents/StructuredMessage/RedPacket.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 
-import { Button } from '@material-ui/core'
+import { Button, Typography, makeStyles, createStyles } from '@material-ui/core'
 import { RedPacketSimplified } from '../../../extension/options-page/DashboardComponents/RedPacket'
 
+const useStyles = makeStyles(theme =>
+    createStyles({
+        button: {
+            width: 190,
+            margin: 'auto',
+            display: 'block',
+        },
+        detail: {
+            textAlign: 'center',
+            marginTop: theme.spacing(1),
+        },
+    }),
+)
+
 export default function PluginRedPacket() {
+    const classes = useStyles()
     return (
         <div>
-            <RedPacketSimplified />
-            <Button variant="contained" color="primary" style={{ width: 190, marginLeft: 'auto', display: 'block' }}>
+            <RedPacketSimplified state="pending" />
+            <RedPacketSimplified state="opening" />
+            <RedPacketSimplified state="opened" />
+            <Button variant="contained" color="primary" className={classes.button}>
                 Open Red Packet
             </Button>
+            <Typography color="primary" variant="body1" className={classes.detail}>
+                View Details
+            </Typography>
         </div>
     )
 }
