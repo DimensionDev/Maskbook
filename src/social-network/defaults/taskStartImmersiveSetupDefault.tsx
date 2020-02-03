38cb8cdf7b97922ef01fe8652cbaddbf2be56bfc
Jack Works <zjwpeter@gmail.com>
2020-02-03 19:35:58 +0800

    chore: add some comments

--- src/social-network/defaults/taskStartImmersiveSetupDefault.tsx
+++ src/social-network/defaults/taskStartImmersiveSetupDefault.tsx
@@ -20,9 +20,6 @@ function UI({
 }: { unmount: () => void; post: ValueRef<string>; persona: PersonaIdentifier } & Partial<
     ImmersiveSetupStepperUIProps
 >) {
-    // TODO: This should working now. Please verify if it works
-    // TODO: <del>workaround. add serialization support for tasks</del>
-    restorePrototype(persona, ECKeyIdentifier.prototype)
     const provePost = useValueRef(post)
     return (
         <DraggablePaper>
