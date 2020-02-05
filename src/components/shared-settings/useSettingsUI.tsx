a3c9b402a4160360a1c68d0f6a44efb668b3f5cb
Jack Works <zjwpeter@gmail.com>
2020-02-05 19:18:38 +0800

    refactor: refactor useSettingsUI

---
+++ src/components/shared-settings/useSettingsUI.tsx
@@ -0,0 +1,103 @@
+import { ValueRef } from '@holoflows/kit/es'
+import { useValueRef } from '../../utils/hooks/useValueRef'
+import { texts, SettingsTexts } from './createSettings'
+import {
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+    Switch,
+    Select,
+    MenuItem,
+    makeStyles,
+} from '@material-ui/core'
+import React from 'react'
+
+const useStyles = makeStyles({
+    container: { listStyleType: 'none', width: '100%' },
+})
+export type SettingsMode = (
+    | {
+          type: 'auto'
+      }
+    | {
+          type: 'enum'
+          enum: any
+      }
+) &
+    Partial<SettingsTexts>
+export function SettingsUI<T>(props: { value: ValueRef<T>; mode?: SettingsMode }) {
+    const { value: valueRef, mode = { type: 'auto' } as SettingsMode } = props
+    // This file is share between context. prevent loading in the background.
+    const currentValue = useValueRef(valueRef)
+    const text: Partial<SettingsTexts> = mode || texts.get(valueRef) || {}
+
+    const _props: Props = {
+        primaryText: text.primary ?? '_unknown_setting_',
+        settingsRef: valueRef,
+        secondaryText: text.secondary,
+    }
+
+    if (mode.type === 'enum') return <EnumUI {..._props} enumObject={mode.enum} />
+    else return <AutoUI {..._props} currentValue={currentValue} />
+}
+interface Props {
+    primaryText: string
+    secondaryText?: string
+    settingsRef: ValueRef<any>
+}
+function EnumUI(props: Props & { enumObject: any }) {
+    const classes = useStyles()
+    const { enumObject, settingsRef, primaryText, secondaryText } = props
+    const enum_ = Object.keys(enumObject)
+        .filter(x => Number.isNaN(parseInt(x)))
+        .map(key => ({ key, value: enumObject[key] }))
+    return (
+        <ListItem component="div" classes={classes}>
+            <ListItemText id={primaryText} primary={primaryText} secondary={secondaryText} />
+            <ListItemSecondaryAction>
+                <Select
+                    value={settingsRef.value}
+                    onChange={(event: React.ChangeEvent<any>) => {
+                        let value = event.target.value
+                        if (!Number.isNaN(parseInt(value))) {
+                            value = parseInt(value)
+                        }
+                        if (!enum_.some(x => x.value === value)) {
+                            console.log(value)
+                            throw new Error('Invalid state')
+                        }
+                        settingsRef.value = value
+                    }}>
+                    {enum_.map(({ key, value }) => (
+                        <MenuItem value={String(value)} key={String(key)}>
+                            {String(key)}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </ListItemSecondaryAction>
+        </ListItem>
+    )
+}
+
+function AutoUI(props: Props & { currentValue: unknown }) {
+    const { currentValue, primaryText, settingsRef, secondaryText } = props
+    switch (typeof currentValue) {
+        case 'boolean':
+            const ref = (settingsRef as ValueRef<unknown>) as ValueRef<boolean>
+            return (
+                <ListItem button onClick={() => (ref.value = !ref.value)}>
+                    <ListItemText id={primaryText} primary={primaryText} secondary={secondaryText} />
+                    <ListItemSecondaryAction>
+                        <Switch
+                            inputProps={{ 'aria-labelledby': primaryText }}
+                            edge="end"
+                            checked={currentValue}
+                            onClick={() => (ref.value = !ref.value)}
+                        />
+                    </ListItemSecondaryAction>
+                </ListItem>
+            )
+        default:
+            throw new Error('Not implemented yet')
+    }
+}
--- src/stories/shared.tsx
+++ src/stories/shared.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { ChooseIdentity } from '../components/shared/ChooseIdentity'
-import { boolean } from '@storybook/addon-knobs'
-import { action } from '@storybook/addon-actions'
+import { SettingsUI } from '../components/shared-settings/useSettingsUI'
+import { ValueRef } from '@holoflows/kit/es'
+import { List, Paper } from '@material-ui/core'
+import { useValueRef } from '../utils/hooks/useValueRef'
 
-storiesOf('Shared Components', module).add('ChooseIdentity', () => {
-    return <ChooseIdentity />
-})
+storiesOf('Shared Components', module)
+    .add('ChooseIdentity', () => {
+        return <ChooseIdentity />
+    })
+    .add('useSettingsUI', () => {
+        function D(props: { x: ValueRef<any> }) {
+            const x = useValueRef(props.x)
+            return (
+                <div>
+                    {String(x)} (type: {typeof x})
+                </div>
+            )
+        }
+        return (
+            <Paper>
+                <D x={booleanRef} />
+                <D x={syntaxKind} />
+                <D x={numberKind} />
+                <List>
+                    <SettingsUI value={booleanRef} mode={{ type: 'auto', primary: 'boolean setting' }} />
+                    <SettingsUI value={syntaxKind} mode={{ type: 'enum', enum: SyntaxKind, primary: 'String enum' }} />
+                    <SettingsUI value={numberKind} mode={{ type: 'enum', enum: NumberKind, primary: 'Number enum' }} />
+                </List>
+            </Paper>
+        )
+    })
+enum SyntaxKind {
+    ImportDeclaration = 'import',
+    ExportDeclaration = 'export',
+}
+enum NumberKind {
+    Minus100 = -100,
+    Zero = 0,
+    One = 1,
+}
+const syntaxKind = new ValueRef<SyntaxKind>(SyntaxKind.ImportDeclaration)
+const numberKind = new ValueRef<NumberKind>(NumberKind.Zero)
+const booleanRef = new ValueRef<boolean>(false)
