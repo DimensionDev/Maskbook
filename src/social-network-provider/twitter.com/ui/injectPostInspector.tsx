0c1595fc2c7d060c2f4153439666cfcf9318d10b
guanbinrui <guanbinrui@dimension.im>
2020-02-12 12:21:56 +0800

    feat: zip tweet

---
+++ src/social-network-provider/twitter.com/ui/injectPostInspector.tsx
@@ -0,0 +1,17 @@
+import { PostInfo } from '../../../social-network/ui'
+import { injectPostInspectorDefault } from '../../../social-network/defaults/injectPostInspector'
+
+export function injectPostInspectorAtTwitter(current: PostInfo) {
+    return injectPostInspectorDefault({
+        zipPost(node) {
+            const content = node.current.parentElement?.querySelector<HTMLDivElement>('[lang]')
+
+            if (content) {
+                content.style.maxHeight = '1.5em'
+                content.style.whiteSpace = 'nowrap'
+                content.style.overflow = 'hidden'
+                content.style.textOverflow = 'ellipsis'
+            }
+        },
+    })(current)
+}
