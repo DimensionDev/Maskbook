fd9ff9ed24bba050fee0f3a240e54b75d9f0ff3a
SunriseFox <sunrisefox@qq.com>
2020-02-13 17:29:31 +0800

    chore: remove settings from debug page

--- src/extension/options-page/Debug.tsx
+++ src/extension/options-page/Debug.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { Grid, makeStyles, List } from '@material-ui/core'
+import { Grid, makeStyles } from '@material-ui/core'
 import { AddProve } from './DeveloperComponents/AddProve'
 import { DecryptPostDeveloperMode } from './DeveloperComponents/DecryptPost'
 import { SeeMyProvePost } from './DeveloperComponents/SeeMyProvePost'
-import { SelectEthereumNetwork } from '../../plugins/Wallet/UI/Developer/SelectEthereumNetwork'
 import { FriendsDeveloperMode } from './DeveloperComponents/Friends'
-import {
-    debugModeSetting,
-    steganographyModeSetting,
-    disableOpenNewTabInBackgroundSettings,
-    languageSettings,
-    Language,
-} from '../../components/shared-settings/settings'
-import { SettingsUI } from '../../components/shared-settings/useSettingsUI'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -24,16 +15,7 @@ const DashboardDebugPage = () => {
     return (
         <>
             <div className={classes.root}>
-                <List>
-                    <SettingsUI value={languageSettings} mode={{ enum: Language, type: 'enum' }} />
-                    <SettingsUI value={debugModeSetting} />
-                    <SettingsUI value={steganographyModeSetting} />
-                    <SettingsUI value={disableOpenNewTabInBackgroundSettings} />
-                </List>
                 <Grid container item spacing={2} direction="column" alignItems="stretch" wrap="nowrap">
-                    <Grid item>
-                        <SelectEthereumNetwork />
-                    </Grid>
                     <Grid item>
                         <SeeMyProvePost />
                     </Grid>
