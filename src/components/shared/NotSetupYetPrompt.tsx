ec52ef907371717c8da8f4bbb35ebf25db526341
SunriseFox <sunrisefox@qq.com>
2020-02-13 13:43:31 +0800

    chore: update NotSetupYetPrompt ui

--- src/components/shared/NotSetupYetPrompt.tsx
+++ src/components/shared/NotSetupYetPrompt.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { useI18N } from '../../utils/i18n-next-ui'
 import { Banner, BannerProps } from '../Welcomes/Banner'
 
 export function NotSetupYetPrompt(props: Partial<BannerProps>) {
-    const { t } = useI18N()
-    return <Banner title={t('service_not_setup_yet')} close="hidden" {...props} />
+    return <Banner close="hidden" {...props} />
 }
