66f64ff89890012f9e427f2fa0e7b6c60028120b
Septs <github@septs.pw>
2020-03-03 21:53:00 +0800

    Revert "fix: windows compatibility"
    This reverts commit b4126302a7b723c9e2c379a463deb2db12c6b86b.

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -3,7 +3,7 @@ import path from 'path'
 
 const stdio = [process.stdin, process.stdout, process.stderr]
 const cwd = path.join(__dirname, '..', 'node_modules', '@holoflows', 'kit')
-const yarn = (...args: string[]) => execFileSync('yarn', args, { cwd, stdio, shell: true })
+const yarn = (...args: string[]) => execFileSync('yarn', args, { cwd, stdio })
 
 yarn('install')
 yarn('build:tsc')
