49bd889f61dfbb324300940527247e0e96840476
SunriseFox <sunrisefox@qq.com>
2020-03-04 10:11:40 +0800

    chore: minimizable stepper

--- src/stories/Immersive-Setup.tsx
+++ src/stories/Immersive-Setup.tsx
@@ -8,19 +8,22 @@ import { text } from '@storybook/addon-knobs'
 import { sleep } from '@holoflows/kit/es/util/sleep'
 import { action } from '@storybook/addon-actions'
 import { ECKeyIdentifier } from '../database/type'
+import { DraggablePaper } from '../components/InjectedComponents/ImmersiveSetup/DraggablePaper'
 
 storiesOf('Immersive Setup', module).add('Stepper', () => (
-    <ImmersiveSetupStepper
-        persona={new ECKeyIdentifier('secp256k1', 'test_key')}
-        loadProfile={async () => {
-            action('loadProfile')()
-            await sleep(700)
-        }}
-        provePost={text('Prove post', '🎭A81Kg7HVsITcftN/0IBp2q6+IyfZCYHntkVsMTRl741L0🎭')}
-        onClose={action('close')}
-        autoPasteProvePost={async () => {
-            action('autoPasteProvePost')()
-            await sleep(700)
-        }}
-    />
+    <DraggablePaper>
+        <ImmersiveSetupStepper
+            persona={new ECKeyIdentifier('secp256k1', 'test_key')}
+            loadProfile={async () => {
+                action('loadProfile')()
+                await sleep(700)
+            }}
+            provePost={text('Prove post', '🎭A81Kg7HVsITcftN/0IBp2q6+IyfZCYHntkVsMTRl741L0🎭')}
+            onClose={action('close')}
+            autoPasteProvePost={async () => {
+                action('autoPasteProvePost')()
+                await sleep(700)
+            }}
+        />
+    </DraggablePaper>
 ))
