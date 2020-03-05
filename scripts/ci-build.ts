56107ab8bb878196ee8b74d9e10db0a90f55dc08
Septs <github@septs.pw>
2020-03-05 20:14:20 +0800

    chore: add logging

--- scripts/ci-build.ts
+++ scripts/ci-build.ts
@@ -8,8 +8,10 @@ const BUILD_PATH = path.join(cwd, 'build')
 const stdio = [process.stdin, process.stdout, process.stderr]
 const shell = os.platform() === 'win32'
 
-const exec = (command: string, args?: string[], options?: ExecFileSyncOptions) =>
-    execFileSync(command, args, { cwd, stdio, shell, ...options })
+const exec = (command: string, args: string[], options?: ExecFileSyncOptions) => {
+    console.log('$', command, args.join(' '), '# cwd:', options?.cwd ?? cwd)
+    return execFileSync(command, args, { cwd, stdio, shell, ...options })
+}
 
 function buildTypes(name: string): string[] {
     if (/full/.test(name) || name === 'master') {
@@ -27,13 +29,14 @@ const branch = git.branchName()
 const types = buildTypes(branch.toLowerCase())
 console.log(`Branch: ${branch}`)
 for (const type of types) {
+    console.log('#', 'Building for target:', type)
     if (type === 'chromium' && types.includes('base')) {
         // chromium doesn't have it's own changes yet.
         // just copying base version is acceptable
-        exec('cp', ['Maskbook.base.zip', 'Maskbook.chromium.zip'])
+        exec('cp', ['-v', 'Maskbook.base.zip', 'Maskbook.chromium.zip'])
+    } else {
+        exec('yarn', [`build:${type.toLowerCase()}`])
+        exec('zip', ['-FS', '-r', `../Maskbook.${type}.zip`, '.'], { cwd: BUILD_PATH })
+        exec('rm', ['-rfv', 'build'])
     }
-    console.log(`Building for target: ${type}`)
-    exec('yarn', [`build:${type.toLowerCase()}`])
-    exec('zip', ['-r', `../Maskbook.${type}.zip`, '.'], { cwd: BUILD_PATH })
-    exec('rm', ['-rf', BUILD_PATH])
 }
