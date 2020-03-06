0211c361b69e49b08a6730231c6fab7c8e1fd5e9
guanbinrui <guanbinrui@dimension.im>
2020-03-06 17:07:30 +0800

    test: setup e2e

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const extPath = path.join(__dirname, './build')
+module.exports = {
+    launch: {
+        dumpio: true,
+        headless: false,
+        args: [
+            '--no-sandbox',
+            '--disable-infobars',
+            `--disable-extensions-except=${extPath}`,
+            `--load-extension=${extPath}`,
+        ],
+    },
+    browser: 'chromium',
+    browserContext: 'default',
+}
