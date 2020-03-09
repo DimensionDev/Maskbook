66abf865a19b59007032f0da0b4a3f08016df660
guanbinrui <guanbinrui@dimension.im>
2020-03-09 17:25:12 +0800

    chore: inject maskbook icon for mobile twitter
    close #867

--- src/social-network-provider/twitter.com/ui/inject.tsx
+++ src/social-network-provider/twitter.com/ui/inject.tsx
@@ -3,10 +3,12 @@ import { injectKnownIdentityAtTwitter } from './injectKnownIdentity'
 import { injectPostDialogAtTwitter } from './injectPostDialog'
 import { injectPostDialogHintAtTwitter } from './injectPostDialogHint'
 import { injectPostInspectorAtTwitter } from './injectPostInspector'
+import { injectPostDialogIconAtTwitter } from './injectPostDialogIcon'
 
 const injectPostBox = () => {
     injectPostDialogAtTwitter()
     injectPostDialogHintAtTwitter()
+    injectPostDialogIconAtTwitter()
 }
 
 export const twitterUIInjections: SocialNetworkUIInjections = {
