167615987e50d2200477ce6629a213a72febdf72
guanbinrui <guanbinrui@dimension.im>
2020-03-16 11:23:01 +0800

    fix: can not decrypt image payload in ff

--- src/social-network-provider/twitter.com/utils/fetch.ts
+++ src/social-network-provider/twitter.com/utils/fetch.ts
@@ -181,12 +181,9 @@ export const postImageParser = async (node: HTMLElement) => {
             await Promise.all(
                 imgUrls.map(async (url) => {
                     try {
-                        const content = await Services.Steganography.decodeImage(
-                            new Uint8Array(await downloadUrl(url)),
-                            {
-                                pass: posterIdentity.toText(),
-                            },
-                        )
+                        const content = await Services.Steganography.decodeImageUrl(url, {
+                            pass: posterIdentity.toText(),
+                        })
                         return /https:\/\/.+\..+\/%20(.+)%40/.test(content) ? content : ''
                     } catch {
                         // for twitter image url maybe absent
