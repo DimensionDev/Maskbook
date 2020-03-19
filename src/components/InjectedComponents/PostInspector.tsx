1bfb9c5b4fdfaba6452c68db4d150c9bb294951c
Jack Works <jackworks@protonmail.com>
2020-03-19 13:57:10 +0800

    fix: type error

--- src/components/InjectedComponents/PostInspector.tsx
+++ src/components/InjectedComponents/PostInspector.tsx
@@ -102,6 +102,7 @@ export function PostInspector(props: PostInspectorProps) {
         )
     } else if (type.provePost.length) {
         const AddToKeyStoreX = props.AddToKeyStoreComponent || AddToKeyStore
+        if (!AddToKeyStoreX) return null
         return (
             <>
                 <AddToKeyStoreX postBy={postBy} provePost={post} {...props.AddToKeyStoreProps} />
