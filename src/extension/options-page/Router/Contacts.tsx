3d6eca28fb96ca442ab277e31e930f5f41f71130
SunriseFox <sunrisefox@qq.com>
2020-03-20 09:35:51 +0800

    chore: contacts page

--- src/extension/options-page/Router/Contacts.tsx
+++ src/extension/options-page/Router/Contacts.tsx
@@ -1,7 +1,18 @@
 import React, { useMemo } from 'react'
 import DashboardRouterContainer from './Container'
-import { TextField, InputAdornment } from '@material-ui/core'
+import { TextField, InputAdornment, Typography } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
 import SearchIcon from '@material-ui/icons/Search'
+import { useFriendsList } from '../../../components/DataSource/useActivatedUI'
+import { ContactLine } from '../DashboardComponents/ContactLine'
+
+const useStyles = makeStyles(theme =>
+    createStyles({
+        title: {
+            margin: theme.spacing(3, 0),
+        },
+    }),
+)
 
 export default function DashboardContactsRouter() {
     const actions = useMemo(
@@ -21,9 +32,20 @@ export default function DashboardContactsRouter() {
         ],
         [],
     )
+
+    const classes = useStyles()
+    const contacts = useFriendsList()
+
     return (
         <DashboardRouterContainer title="Contacts" actions={actions}>
-            <div></div>
+            <Typography className={classes.title} variant="body2" color="textSecondary">
+                All people recorded in the Maskbook database.
+            </Typography>
+            <section>
+                {contacts.map(contact => (
+                    <ContactLine key={contact.identifier.toText()} contact={contact}></ContactLine>
+                ))}
+            </section>
         </DashboardRouterContainer>
     )
 }
