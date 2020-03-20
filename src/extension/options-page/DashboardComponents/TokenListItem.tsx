82eaddac48a9b68f65104d16d111affdc5b1b65d
SunriseFox <sunrisefox@qq.com>
2020-03-20 17:36:08 +0800

    chore: dummy wallet page

---
+++ src/extension/options-page/DashboardComponents/TokenListItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    ListItemSecondaryAction,
+    makeStyles,
+    createStyles,
+    Avatar,
+} from '@material-ui/core'
+
+const useStyles = makeStyles(theme =>
+    createStyles({
+        coin: {
+            width: '24px',
+            height: '24px',
+        },
+    }),
+)
+
+export function TokenListItem() {
+    const classes = useStyles()
+    return (
+        <ListItem divider disableGutters>
+            <ListItemIcon>
+                <Avatar
+                    className={classes.coin}
+                    src="https://github.com/trustwallet/assets/raw/master/blockchains/ethereum/assets/0x00000100F2A2bd000715001920eB70D229700085/logo.png"></Avatar>
+            </ListItemIcon>
+            <ListItemText primary={'DAI'} secondary={'Dai Stablecoin'} />
+            <ListItemSecondaryAction>0.124645998323285029</ListItemSecondaryAction>
+        </ListItem>
+    )
+}
--- src/extension/options-page/Router/Wallets.tsx
+++ src/extension/options-page/Router/Wallets.tsx
@@ -1,7 +1,67 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import DashboardRouterContainer from './Container'
-import { Button } from '@material-ui/core'
+import { Button, Typography, Box, IconButton, List } from '@material-ui/core'
+import { makeStyles, createStyles, Theme, ThemeProvider } from '@material-ui/core/styles'
+
 import AddIcon from '@material-ui/icons/Add'
+import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined'
+
+import { WalletItem } from '../DashboardComponents/WalletItem'
+import { WalletRecord } from '../../../plugins/Wallet/database/types'
+import { TokenListItem } from '../DashboardComponents/TokenListItem'
+
+const useStyles = makeStyles(theme =>
+    createStyles({
+        wrapper: {
+            display: 'flex',
+            height: '100%',
+        },
+        list: {
+            width: '251px',
+            flex: '0 0 auto',
+            height: '100%',
+            overflow: 'auto',
+            borderRight: `1px solid ${theme.palette.divider}`,
+        },
+        content: {
+            width: '100%',
+            overflow: 'auto',
+            flex: '1 1 auto',
+        },
+        title: {
+            flex: '1',
+        },
+    }),
+)
+
+const fakeWallet = {
+    name: 'Wallet A',
+    address: '0x668281324fd6cB5F205F0764D114D6681A8AC6d1',
+}
+
+const walletTheme = (theme: Theme): Theme => ({
+    ...theme,
+    overrides: {
+        ...theme.overrides,
+        MuiList: {
+            root: {
+                margin: theme.spacing(0, 3),
+            },
+        },
+        MuiListItemIcon: {
+            root: {
+                justifyContent: 'center',
+                minWidth: 'unset',
+                marginRight: theme.spacing(2),
+            },
+        },
+        MuiListItemSecondaryAction: {
+            root: {
+                ...theme.typography.body1,
+            },
+        },
+    },
+})
 
 export default function DashboardWalletsRouter() {
     const actions = useMemo(
@@ -15,9 +75,42 @@ export default function DashboardWalletsRouter() {
         ],
         [],
     )
+    const classes = useStyles()
+    const wallets: Partial<WalletRecord>[] = [fakeWallet, fakeWallet]
+    const [current, setCurrent] = useState(0)
+
     return (
-        <DashboardRouterContainer padded={false} title="My Wallets" actions={actions}>
-            <div></div>
-        </DashboardRouterContainer>
+        <ThemeProvider theme={walletTheme}>
+            <DashboardRouterContainer padded={false} title="My Wallets" actions={actions}>
+                <div className={classes.wrapper}>
+                    <div className={classes.list}>
+                        {wallets.map((wallet, index) => (
+                            <WalletItem
+                                onClick={() => setCurrent(index)}
+                                wallet={wallet}
+                                selected={index === current}></WalletItem>
+                        ))}
+                    </div>
+                    <div className={classes.content}>
+                        <Box pt={3} pb={2} pl={3} pr={2} display="flex" alignItems="center">
+                            <Typography className={classes.title} variant="h5">
+                                Details
+                            </Typography>
+                            <Button variant="text" color="primary" startIcon={<AddIcon />}>
+                                Add Token
+                            </Button>
+                            <IconButton>
+                                <MoreVertOutlinedIcon />
+                            </IconButton>
+                        </Box>
+                        <List disablePadding>
+                            <TokenListItem></TokenListItem>
+                            <TokenListItem></TokenListItem>
+                            <TokenListItem></TokenListItem>
+                        </List>
+                    </div>
+                </div>
+            </DashboardRouterContainer>
+        </ThemeProvider>
     )
 }
