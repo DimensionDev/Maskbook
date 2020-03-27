f9b04d5ebeca778da750d309c3f2d1827cf95bff
Septs <github@septs.pw>
2020-03-27 11:27:49 +0800

    fix: workaround test in china

--- scripts/jest-global-setup.js
+++ scripts/jest-global-setup.js
@@ -2,6 +2,9 @@ module.exports = async () => {
     await require('util').promisify(addAsmCryptoForTest)()
 }
 
+// orignal url: https://peculiarventures.github.io/pv-webcrypto-tests/src/asmcrypto.js
+const ASM_CRYPTO_URL = 'https://gitcdn.xyz/repo/PeculiarVentures/pv-webcrypto-tests/gh-pages/src/asmcrypto.js'
+
 function addAsmCryptoForTest(cb) {
     var download = function (url, dest, cb) {
         var file = fs.createWriteStream(dest)
@@ -20,7 +23,7 @@ function addAsmCryptoForTest(cb) {
     if (fs.existsSync(dist) && fs.readFileSync(dist, 'utf-8').length > 200) return cb()
     // Jesus, but at least it will only run it test env
     // See https://github.com/PeculiarVentures/webcrypto-liner/issues/47
-    download('https://peculiarventures.github.io/pv-webcrypto-tests/src/asmcrypto.js', dist, () => {
+    download(ASM_CRYPTO_URL, dist, () => {
         fs.writeFileSync(dist, fs.readFileSync(dist, 'utf-8').replace('({},this);', '({},globalThis);'))
         load()
     })
