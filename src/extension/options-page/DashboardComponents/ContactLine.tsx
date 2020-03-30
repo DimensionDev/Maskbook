2353a964f8f0edda75fda7b422e1c6a7dfb41a9a
SunriseFox <sunrisefox@qq.com>
2020-03-30 16:30:28 +0800

    chore: update type and lint files

--- src/extension/options-page/DashboardComponents/ContactLine.tsx
+++ src/extension/options-page/DashboardComponents/ContactLine.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Typography, ButtonBase, ButtonBaseProps } from '@material-ui/core'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
-import { Profile } from '../../../database'
+import type { Profile } from '../../../database'
 import { Avatar } from '../../../utils/components/Avatar'
 import { useModal } from '../Dialog/Base'
 import { DashboardContactDialog } from '../Dialog/Contact'
