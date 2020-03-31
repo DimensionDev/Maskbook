d084fc598f19229cb22585f46e05e67529c3f83f
SunriseFox <sunrisefox@qq.com>
2020-03-31 11:21:33 +0800

    chore: create import delete wallet

---
+++ src/extension/options-page/DashboardComponents/DashboardMenu.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import type { WrappedDialogProps } from '../Dialog/Base'
+import { Menu, Popper, Grow, Paper, ClickAwayListener, MenuList } from '@material-ui/core'
+
+export default function DashboardMenu(props: WrappedDialogProps<{ anchorEl?: Element; menus: JSX.Element[] }>) {
+    const { anchorEl, menus } = props.ComponentProps!
+
+    return (
+        <Popper open={props.open} anchorEl={anchorEl} transition>
+            {({ TransitionProps, placement }) => (
+                <Grow
+                    {...TransitionProps}
+                    style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}>
+                    <Paper>
+                        <ClickAwayListener onClickAway={props.onClose}>
+                            <MenuList autoFocusItem={props.open} onClick={props.onClose}>
+                                {menus}
+                            </MenuList>
+                        </ClickAwayListener>
+                    </Paper>
+                </Grow>
+            )}
+        </Popper>
+    )
+}
--- src/extension/options-page/Dialog/Wallet.tsx
+++ src/extension/options-page/Dialog/Wallet.tsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react'
-import { DashboardDialogCore, DashboardDialogWrapper, WrappedDialogProps } from './Base'
+import { DashboardDialogCore, DashboardDialogWrapper, WrappedDialogProps, useSnackbarCallback } from './Base'
 import { CreditCard as CreditCardIcon, Hexagon as HexagonIcon, Clock as ClockIcon } from 'react-feather'
 import { TextField, Typography } from '@material-ui/core'
 import AbstractTab, { AbstractTabProps } from '../DashboardComponents/AbstractTab'
 import { useI18N } from '../../../utils/i18n-next-ui'
 import { ThrottledButton } from '../DashboardComponents/ActionButton'
+import SpacedButtonGroup from '../DashboardComponents/SpacedButtonGroup'
+import Services from '../../service'
+import type { WalletRecord } from '../../../plugins/Wallet/database/types'
 
 export function DashboardWalletImportDialog(props: WrappedDialogProps) {
     const state = useState(0)
 
-    const [word, setWord] = useState('')
-    const [pass, setPass] = useState('')
+    const [mnemonic, setMnemonic] = useState('')
+    const [passphrase, setPassphrase] = useState('')
     const [name, setName] = useState('')
 
     const [privKey, setPrivKey] = useState('')
@@ -23,10 +26,14 @@ export function DashboardWalletImportDialog(props: WrappedDialogProps) {
                     <form>
                         <TextField
                             placeholder="Mnemonic Word*"
-                            value={word}
-                            onChange={(e) => setWord(e.target.value)}
+                            value={mnemonic}
+                            onChange={(e) => setMnemonic(e.target.value)}
                         />
-                        <TextField placeholder="Password*" value={pass} onChange={(e) => setPass(e.target.value)} />
+                        <TextField
+                            placeholder="Password*"
+                            value={passphrase}
+                            onChange={(e) => setPassphrase(e.target.value)}
+                        />
                         <TextField placeholder="Wallet Name*" value={name} onChange={(e) => setName(e.target.value)} />
                     </form>
                 ),
@@ -54,6 +61,21 @@ export function DashboardWalletImportDialog(props: WrappedDialogProps) {
         height: 240,
     }
 
+    const onSubmit = useSnackbarCallback(
+        () => {
+            if (state[0] === 1)
+                return Services.Plugin.invokePlugin('maskbook.wallet', 'importNewWallet', {
+                    name,
+                    mnemonic: mnemonic.split(' '),
+                    passphrase,
+                })
+            alert('// TODO!: import private key')
+            return Promise.reject()
+        },
+        [name, mnemonic, passphrase],
+        props.onClose,
+    )
+
     return (
         <DashboardDialogCore {...props}>
             <DashboardDialogWrapper
@@ -62,7 +84,7 @@ export function DashboardWalletImportDialog(props: WrappedDialogProps) {
                 primary="Import Wallet"
                 secondary="Import a wallet with mnemonic words and password.">
                 <AbstractTab {...tabProps}></AbstractTab>
-                <ThrottledButton variant="contained" color="primary">
+                <ThrottledButton variant="contained" color="primary" onClick={onSubmit}>
                     Import
                 </ThrottledButton>
             </DashboardDialogWrapper>
@@ -72,24 +94,38 @@ export function DashboardWalletImportDialog(props: WrappedDialogProps) {
 
 export function DashboardWalletCreateDialog(props: WrappedDialogProps) {
     const [name, setName] = useState('')
-    const [pass, setPass] = useState('')
+    const [passphrase, setPassphrase] = useState('')
+
+    const onSubmit = useSnackbarCallback(
+        () => Services.Plugin.invokePlugin('maskbook.wallet', 'createNewWallet', { name, passphrase }),
+        [name, passphrase],
+        props.onClose,
+    )
 
     return (
         <DashboardDialogCore {...props}>
             <DashboardDialogWrapper icon={<CreditCardIcon />} iconColor="#4EE0BC" primary="Create a Wallet">
                 <form>
                     <TextField placeholder="Wallet Name*" value={name} onChange={(e) => setName(e.target.value)} />
-                    <TextField placeholder="Password*" value={pass} onChange={(e) => setPass(e.target.value)} />
+                    <TextField
+                        placeholder="Password*"
+                        value={passphrase}
+                        onChange={(e) => setPassphrase(e.target.value)}
+                    />
                 </form>
                 <Typography variant="body2" color="textSecondary">
                     Set a password to improve the security level
                 </Typography>
-                <ThrottledButton variant="contained" color="primary">
+                <ThrottledButton variant="contained" color="primary" onClick={onSubmit}>
                     Create
                 </ThrottledButton>
             </DashboardDialogWrapper>
         </DashboardDialogCore>
     )
+}
+
+interface WalletProps {
+    wallet: WalletRecord
 }
 
 export function DashboardWalletAddTokenDialog(props: WrappedDialogProps) {
@@ -184,6 +220,35 @@ export function DashboardWalletBackupDialog(props: WrappedDialogProps) {
                     <Typography variant="body2">Private key</Typography>
                     <blockquote>0x1a3car70f81c1f812b9b84711a99c62fe7c986a9db174e3ffc180cfdcbbf9933</blockquote>
                 </section>
+            </DashboardDialogWrapper>
+        </DashboardDialogCore>
+    )
+}
+
+export function DashboardWalletDeleteConfirmDialog(props: WrappedDialogProps<WalletProps>) {
+    const { t } = useI18N()
+    const { wallet } = props.ComponentProps!
+    const deletePersona = useSnackbarCallback(
+        () => Services.Plugin.invokePlugin('maskbook.wallet', 'removeWallet', wallet.address),
+        [wallet.address],
+        props.onClose,
+    )
+    return (
+        <DashboardDialogCore {...props}>
+            <DashboardDialogWrapper
+                size="small"
+                icon={<CreditCardIcon />}
+                iconColor="#F4637D"
+                primary={'Delete Wallet'}
+                secondary={'Are you sure? If you do not have backup, you will lose ALL YOUR MONEY of it.'}>
+                <SpacedButtonGroup>
+                    <ThrottledButton variant="contained" color="danger" onClick={deletePersona}>
+                        OK
+                    </ThrottledButton>
+                    <ThrottledButton variant="outlined" color="primary" onClick={props.onClose}>
+                        Cancel
+                    </ThrottledButton>
+                </SpacedButtonGroup>
             </DashboardDialogWrapper>
         </DashboardDialogCore>
     )
