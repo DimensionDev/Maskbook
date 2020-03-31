e9a08ab81baced0ed33991f7295bd9c5ee45dd05
SunriseFox <sunrisefox@qq.com>
2020-03-31 11:21:33 +0800

    chore: create import delete wallet

--- src/extension/options-page/DashboardComponents/TokenListItem.tsx
+++ src/extension/options-page/DashboardComponents/TokenListItem.tsx
@@ -8,6 +8,7 @@ import {
     createStyles,
     Avatar,
 } from '@material-ui/core'
+import type { ERC20TokenRecord } from '../../../plugins/Wallet/database/types'
 
 const useStyles = makeStyles((theme) =>
     createStyles({
@@ -18,17 +19,22 @@ const useStyles = makeStyles((theme) =>
     }),
 )
 
-export function TokenListItem() {
+interface TokenListItemProps {
+    token: ERC20TokenRecord
+}
+
+export function TokenListItem(props: TokenListItemProps) {
     const classes = useStyles()
+    const { token } = props
     return (
         <ListItem divider disableGutters>
             <ListItemIcon>
                 <Avatar
                     className={classes.coin}
-                    src="https://github.com/trustwallet/assets/raw/master/blockchains/ethereum/assets/0x00000100F2A2bd000715001920eB70D229700085/logo.png"></Avatar>
+                    src={`https://rawcdn.githack.com/trustwallet/assets/257c82b25e6f27ede7a2b309aadc0ed17bca45ae/blockchains/ethereum/assets/${token.address}/logo.png`}></Avatar>
             </ListItemIcon>
-            <ListItemText primary={'DAI'} secondary={'Dai Stablecoin'} />
-            <ListItemSecondaryAction>0.124645998323285029</ListItemSecondaryAction>
+            <ListItemText primary={token.symbol} secondary={token.name} />
+            <ListItemSecondaryAction>{token.decimals}</ListItemSecondaryAction>
         </ListItem>
     )
 }
--- src/extension/options-page/Router/Wallets.tsx
+++ src/extension/options-page/Router/Wallets.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from 'react'
 import DashboardRouterContainer from './Container'
-import { Button, Typography, Box, IconButton, List } from '@material-ui/core'
+import { Button, Typography, Box, IconButton, List, MenuItem } from '@material-ui/core'
 import { makeStyles, createStyles, Theme, ThemeProvider } from '@material-ui/core/styles'
 
 import AddCircleIcon from '@material-ui/icons/AddCircle'
@@ -8,7 +8,6 @@ import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined'
 import HistoryIcon from '@material-ui/icons/History'
 
 import { WalletItem } from '../DashboardComponents/WalletItem'
-import type { WalletRecord } from '../../../plugins/Wallet/database/types'
 import { TokenListItem } from '../DashboardComponents/TokenListItem'
 import { useModal } from '../Dialog/Base'
 import {
@@ -16,13 +15,20 @@ import {
     DashboardWalletCreateDialog,
     DashboardWalletAddTokenDialog,
     DashboardWalletHistoryDialog,
+    DashboardWalletBackupDialog,
+    DashboardWalletDeleteConfirmDialog,
 } from '../Dialog/Wallet'
+import { useMyWallets } from '../../../components/DataSource/independent'
+import DashboardMenu from '../DashboardComponents/DashboardMenu'
+import { useI18N } from '../../../utils/i18n-next-ui'
+import { useColorProvider } from '../../../utils/theme'
 
 const useStyles = makeStyles((theme) =>
     createStyles({
         wrapper: {
             display: 'flex',
-            flex: 1,
+            flex: '0 0 100%',
+            height: '100%',
         },
         list: {
             width: '251px',
@@ -81,10 +87,16 @@ const walletTheme = (theme: Theme): Theme => ({
 })
 
 export default function DashboardWalletsRouter() {
+    const classes = useStyles()
+    const color = useColorProvider()
+    const { t } = useI18N()
+
     const [walletImport, openWalletImport] = useModal(DashboardWalletImportDialog)
     const [walletCreate, openWalletCreate] = useModal(DashboardWalletCreateDialog)
     const [addToken, openAddToken] = useModal(DashboardWalletAddTokenDialog)
     const [walletHistory, oepnWalletHistory] = useModal(DashboardWalletHistoryDialog)
+    const [walletBackup, , oepnWalletBackup] = useModal(DashboardWalletBackupDialog)
+    const [walletDelete, , oepnWalletDelete] = useModal(DashboardWalletDeleteConfirmDialog)
 
     const actions = useMemo(
         () => [
@@ -97,55 +109,69 @@ export default function DashboardWalletsRouter() {
         ],
         [openWalletCreate, openWalletImport],
     )
-    const classes = useStyles()
-    const wallets: Partial<WalletRecord>[] = [fakeWallet, fakeWallet]
+
+    const [wallets, tokens] = useMyWallets()
     const [current, setCurrent] = useState(0)
 
+    const menus = useMemo(
+        () => [
+            // <MenuItem onClick={openRename}>{t('rename')}</MenuItem>,
+            <MenuItem onClick={() => oepnWalletBackup({ wallet: wallets[current] })}>
+                {t('dashboard_create_backup')}
+            </MenuItem>,
+            <MenuItem onClick={() => oepnWalletDelete({ wallet: wallets[current] })} className={color.error}>
+                {t('dashboard_delete_persona')}
+            </MenuItem>,
+        ],
+        [wallets, current],
+    )
+
+    const [menu, , openMenu] = useModal(DashboardMenu, { menus })
+
     return (
-        <ThemeProvider theme={walletTheme}>
-            <DashboardRouterContainer padded={false} title="My Wallets" actions={actions}>
-                <div className={classes.wrapper}>
-                    <div className={classes.list}>
-                        {wallets.map((wallet, index) => (
-                            <WalletItem
-                                onClick={() => setCurrent(index)}
-                                wallet={wallet}
-                                selected={index === current}></WalletItem>
-                        ))}
-                    </div>
-                    <div className={classes.content}>
-                        <Box pt={3} pb={2} pl={3} pr={2} display="flex" alignItems="center">
-                            <Typography className={classes.title} variant="h5">
-                                Details
-                            </Typography>
-                            <Button variant="text" color="primary" onClick={openAddToken} startIcon={<AddCircleIcon />}>
-                                Add Token
-                            </Button>
-                            <IconButton>
-                                <MoreVertOutlinedIcon />
-                            </IconButton>
-                        </Box>
+        <DashboardRouterContainer padded={false} title="My Wallets" actions={actions}>
+            <div className={classes.wrapper}>
+                <div className={classes.list}>
+                    {wallets.map((wallet, index) => (
+                        <WalletItem
+                            onClick={() => setCurrent(index)}
+                            wallet={wallet}
+                            selected={index === current}></WalletItem>
+                    ))}
+                </div>
+                <div className={classes.content}>
+                    <Box pt={3} pb={2} pl={3} pr={2} display="flex" alignItems="center">
+                        <Typography className={classes.title} variant="h5">
+                            Details
+                        </Typography>
+                        <Button variant="text" color="primary" onClick={openAddToken} startIcon={<AddCircleIcon />}>
+                            Add Token
+                        </Button>
+                        <IconButton onClick={(e) => openMenu({ anchorEl: e.currentTarget })}>
+                            <MoreVertOutlinedIcon />
+                        </IconButton>
+                        {menu}
+                    </Box>
+                    <ThemeProvider theme={walletTheme}>
                         <List className={classes.tokenList} disablePadding>
-                            <TokenListItem></TokenListItem>
-                            <TokenListItem></TokenListItem>
-                            <TokenListItem></TokenListItem>
+                            {tokens.map((token) => (
+                                <TokenListItem token={token} key={token.address} />
+                            ))}
                         </List>
-                        <div className={classes.footer}>
-                            <Button
-                                onClick={oepnWalletHistory}
-                                startIcon={<HistoryIcon />}
-                                variant="text"
-                                color="primary">
-                                History
-                            </Button>
-                        </div>
+                    </ThemeProvider>
+                    <div className={classes.footer}>
+                        <Button onClick={oepnWalletHistory} startIcon={<HistoryIcon />} variant="text" color="primary">
+                            History
+                        </Button>
                     </div>
                 </div>
-                {walletImport}
-                {walletCreate}
-                {addToken}
-                {walletHistory}
-            </DashboardRouterContainer>
-        </ThemeProvider>
+            </div>
+            {walletBackup}
+            {walletDelete}
+            {walletImport}
+            {walletCreate}
+            {addToken}
+            {walletHistory}
+        </DashboardRouterContainer>
     )
 }
