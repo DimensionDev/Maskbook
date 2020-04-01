2bc2335db0353d75ecd0f8511eff8b9abea2951e
SunriseFox <sunrisefox@qq.com>
2020-04-01 16:55:09 +0800

    chore: dialog i18n

--- src/extension/options-page/Dialog/Feedback.tsx
+++ src/extension/options-page/Dialog/Feedback.tsx
@@ -3,29 +3,31 @@ import { DashboardDialogCore, DashboardDialogWrapper, WrappedDialogProps } from 
 import { Smile as SmileIcon } from 'react-feather'
 import { TextField } from '@material-ui/core'
 import { ThrottledButton } from '../DashboardComponents/ActionButton'
+import { useI18N } from '../../../utils/i18n-next-ui'
 
 export function DashboardFeedbackDialog(props: WrappedDialogProps) {
+    const { t } = useI18N()
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
 
     return (
         <DashboardDialogCore {...props}>
-            <DashboardDialogWrapper icon={<SmileIcon />} iconColor="#F8B03E" primary="Feedback">
+            <DashboardDialogWrapper icon={<SmileIcon />} iconColor="#F8B03E" primary={t('feedback')}>
                 <form>
-                    <TextField required label="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <TextField required label={t('name')} value={name} onChange={(e) => setName(e.target.value)} />
                     <TextField required label="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <TextField
                         multiline
                         rows={4}
                         required
-                        label="Your message..."
+                        label={t('your_message')}
                         value={message}
                         onChange={(e) => setMessage(e.target.value)}
                     />
                 </form>
                 <ThrottledButton variant="contained" color="primary">
-                    Submit
+                    {t('submit')}
                 </ThrottledButton>
             </DashboardDialogWrapper>
         </DashboardDialogCore>
