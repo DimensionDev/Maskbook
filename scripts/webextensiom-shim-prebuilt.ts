6353b75b9dbee259ea5532c08b106412b60765f4
Jack Works <jackworks@protonmail.com>
2020-04-16 21:51:41 +0800

    chore: reduce iOS build size, close #988

--- scripts/webextensiom-shim-prebuilt.ts
+++ scripts/webextensiom-shim-prebuilt.ts
@@ -11,3 +11,17 @@ run(
     'node',
     ...'../ts-node/dist/bin.js -T ./src/bin/prebuilts.ts eofkdgkhfoebecmamljfaepckoecjhib ../../build/'.split(' '),
 )
+import fs from 'fs'
+function readDir(x: string) {
+    const _ = path.join(__dirname, x)
+    return fs.readdirSync(_).map((x) => path.join(_, x))
+}
+readDir('../build/js').filter(unused).forEach(fs.unlinkSync)
+// .forEach((x) => console.log(x))
+function unused(x: string): boolean {
+    if (x.includes('prebuilt-0-module')) return true
+    if (x.includes('prebuilt') && !x.includes('content-script')) return true
+    // Since content script has it prebuilt version, we can remove the original version?
+    if (x.match(/content-script\.js$/)) return true
+    return false
+}
