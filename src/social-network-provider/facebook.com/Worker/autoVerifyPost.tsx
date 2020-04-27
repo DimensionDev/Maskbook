46ebe98b5e31b47ac6afc56431b9ee1a037cb97d
Jack Works <jackworks@protonmail.com>
2020-04-27 15:06:49 +0800

    fix: ban pinned property cause ff doesnt support it

--- src/social-network-provider/facebook.com/Worker/autoVerifyPost.tsx
+++ src/social-network-provider/facebook.com/Worker/autoVerifyPost.tsx
@@ -6,7 +6,6 @@ export function autoVerifyPostFacebook(user: ProfileIdentifier, prove: string) {
         active: true,
         autoClose: false,
         memorable: false,
-        pinned: false,
         timeout: Infinity,
     }).pasteIntoPostBox(prove, {
         warningText: i18n.t('automation_request_paste_into_post_box'),
