42b0fcdeb72611a374f2bc8caed477d615be5feb
guanbinrui <guanbinrui@dimension.im>
2020-04-28 18:10:10 +0800

    chore: adjust layout buttons

--- src/extension/options-page/Dialog/Base.tsx
+++ src/extension/options-page/Dialog/Base.tsx
@@ -8,10 +8,13 @@ import {
     createStyles,
     makeStyles,
     IconButtonProps,
+    DialogContent,
+    Typography,
 } from '@material-ui/core'
 import CloseIcon from '@material-ui/icons/Close'
-import { TransitionProps } from '@material-ui/core/transitions'
+import type { TransitionProps } from '@material-ui/core/transitions'
 import { useBlurContext } from '..'
+import classNames from 'classnames'
 
 const Transition = React.forwardRef<unknown, TransitionProps>(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />
@@ -30,6 +33,63 @@ export function DashboardDialogCore(props: DialogProps) {
             hideBackdrop
             {...dialogProps}
         />
+    )
+}
+
+interface DashboardDialogWrapperProps {
+    icon: React.ReactElement
+    iconColor?: string
+    primary: string
+    secondary?: string
+    size?: 'small' | 'medium'
+    children: React.ReactNode
+}
+
+const useDashboardDialogWrapperStyles = makeStyles((theme) =>
+    createStyles({
+        wrapper: {
+            width: '440px',
+            padding: theme.spacing(4),
+        },
+        wrapperSizedSmall: {
+            width: '280px',
+            padding: theme.spacing(3),
+        },
+        header: {
+            marginTop: theme.spacing(1),
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '280px',
+        },
+        primary: {
+            margin: theme.spacing(1, 0),
+        },
+        secondary: {
+            lineHeight: 1.75,
+            textAlign: 'center',
+        },
+    }),
+)
+
+export function DashboardDialogWrapper(props: DashboardDialogWrapperProps) {
+    const { icon, iconColor, primary, secondary, children, size } = props
+    const classes = useDashboardDialogWrapperStyles()
+    return (
+        <DialogContent className={classNames(classes.wrapper, { [classes.wrapperSizedSmall]: size === 'small' })}>
+            <section className={classes.header}>
+                {React.cloneElement(icon, { width: 64, height: 64, stroke: iconColor })}
+                <Typography className={classes.primary} variant="h5">
+                    {primary}
+                </Typography>
+                <Typography className={classes.secondary} color="textSecondary" variant="body2">
+                    {secondary}
+                </Typography>
+            </section>
+            {children}
+        </DialogContent>
     )
 }
 
--- src/extension/options-page/Router/Personas.tsx
+++ src/extension/options-page/Router/Personas.tsx
@@ -1,18 +1,31 @@
 import React, { useMemo } from 'react'
 import DashboardRouterContainer from './Container'
 import { Button, makeStyles, createStyles } from '@material-ui/core'
-import AddIcon from '@material-ui/icons/Add'
+import AddCircleIcon from '@material-ui/icons/AddCircle'
 import { useMyPersonas } from '../../../components/DataSource/useActivatedUI'
 import PersonaCard from '../DashboardComponents/PersonaCard'
 import { DashboardPersonaDialog } from '../Dialog/Persona'
 import { useDialog } from '../Dialog/Base'
+import { Database as DatabaseIcon } from 'react-feather'
 
 const useStyles = makeStyles((theme) =>
     createStyles({
         container: {
             display: 'flex',
             flexWrap: 'wrap',
-            margin: theme.spacing(3, 0),
+            flex: 1,
+            alignItems: 'baseline',
+            overflow: 'auto',
+            marginTop: theme.spacing(1),
+            paddingTop: theme.spacing(2),
+        },
+        footer: {
+            flexGrow: 0,
+            flexShrink: 0,
+            margin: theme.spacing(2, 0),
+            '& > *:not(:last-child)': {
+                marginRight: theme.spacing(1),
+            },
         },
     }),
 )
@@ -25,17 +38,14 @@ export default function DashboardPersonasRouter() {
 
     const actions = useMemo(
         () => [
-            <Button color="primary" variant="outlined" onClick={() => openCreatePersona()}>
-                Restore
-            </Button>,
             <Button color="primary" variant="outlined">
-                Backup
+                Import
             </Button>,
-            <Button color="primary" variant="contained" endIcon={<AddIcon />}>
-                New Persona
+            <Button color="primary" variant="contained" endIcon={<AddCircleIcon />}>
+                Create Persona
             </Button>,
         ],
-        [openCreatePersona],
+        [],
     )
 
     return (
@@ -44,6 +54,14 @@ export default function DashboardPersonasRouter() {
                 {personas.map((persona) => (
                     <PersonaCard key={persona.identifier.toText()} persona={persona} />
                 ))}
+            </section>
+            <section className={classes.footer}>
+                <Button startIcon={<DatabaseIcon size={18} />} color="primary" variant="text">
+                    Restore Datebase
+                </Button>
+                <Button color="primary" variant="text">
+                    Backup Datebase
+                </Button>
             </section>
             {createPersona}
         </DashboardRouterContainer>
