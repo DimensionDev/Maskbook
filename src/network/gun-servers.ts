a9fd6119ad34fcf099631169ad9bd7267c01030b
Septs <github@septs.pw>
2020-05-18 11:21:23 +0800

    feat: add gun server

--- src/network/gun-servers.ts
+++ src/network/gun-servers.ts
@@ -1,2 +1,2 @@
-export const gunServers = ['https://gun.maskbook.com/gun'] as const
+export const gunServers = ['https://hk.gun.dimension.chat/gun', 'https://gun.maskbook.com/gun'] as const
 export const gun2Servers = gunServers
