ee6b561a6b3982b2e3c220940678b1c5595686d2
Jack Works <jackworks@protonmail.com>
2020-05-20 16:12:45 +0800

    fix: double render in popup

--- src/utils/SSRRenderer.tsx
+++ src/utils/SSRRenderer.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 
 export function SSRRenderer(jsx: JSX.Element, container?: HTMLElement) {
     if (typeof window === 'object') {
-        if (!container) container = document.getElementById('root')!
+        if (!container) container = document.getElementById('root') ?? void 0
         if (!container) {
             container = document.createElement('div')
             document.body.appendChild(container)
         }
-        ReactDOM.createRoot(container).render(React.createElement(React.StrictMode, {}, jsx))
+        const oldChildren = [...container.children]
+        ReactDOM.createRoot(container).render(<React.StrictMode children={jsx} />)
+        oldChildren.forEach((x) => x.remove())
     } else {
         async function render() {
             const Server = await import('react-dom/server')
