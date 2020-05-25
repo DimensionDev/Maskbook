56a588bfdb62a1e6bbb3d335a038ac65f0978057
guanbinrui <guanbinrui@dimension.im>
2020-05-25 11:53:46 +0800

    fix: the backdrop of dialog not close when location changed

--- src/extension/options-page/DashboardContexts/BlurContext.tsx
+++ src/extension/options-page/DashboardContexts/BlurContext.tsx
@@ -1,5 +1,6 @@
-import React, { useMemo, useContext, useEffect, useRef } from 'react'
+import React, { useMemo, useContext, useEffect, useRef, useCallback } from 'react'
 import { makeStyles, createStyles, useTheme } from '@material-ui/core'
+import { useLocation } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) =>
     createStyles({
@@ -32,22 +33,26 @@ export interface DashboardBlurContextUIProps {
 
 export function DashboardBlurContextUI({ children }: DashboardBlurContextUIProps) {
     const classes = useStyles()
+    const location = useLocation()
     const ref = useRef<HTMLDivElement>(null!)
+    const blur = useCallback(() => {
+        blurRequest += 1
+        ref.current.classList.add(classes.blur)
+    }, [classes.blur])
+    const unblur = useCallback(() => {
+        blurRequest -= 1
+        if (blurRequest <= 0) ref.current.classList.remove(classes.blur)
+    }, [classes.blur])
     const toggle = useMemo(
         () => ({
-            blur: () => {
-                blurRequest += 1
-                ref.current.classList.add(classes.blur)
-            },
-            unblur: () => {
-                blurRequest -= 1
-                if (blurRequest <= 0) ref.current.classList.remove(classes.blur)
-            },
+            blur,
+            unblur,
         }),
-        [classes.blur],
+        [blur, unblur],
     )
     const theme = useTheme()
 
+    useEffect(unblur, [location])
     useEffect(() => {
         // global filter blur color fix
         document.body.style.backgroundColor = theme.palette.background.paper
