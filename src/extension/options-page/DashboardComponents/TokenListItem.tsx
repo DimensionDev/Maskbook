acaba9178dc8a3b6134b9553dbb7dde8613e48dc
guanbinrui <guanbinrui@dimension.im>
2020-05-27 13:30:34 +0800

    refactor: add token icon component

--- src/extension/options-page/DashboardComponents/TokenListItem.tsx
+++ src/extension/options-page/DashboardComponents/TokenListItem.tsx
@@ -6,12 +6,11 @@ import {
     ListItemSecondaryAction,
     makeStyles,
     createStyles,
-    Avatar,
 } from '@material-ui/core'
 import type { ERC20TokenRecord } from '../../../plugins/Wallet/database/types'
 import type BigNumber from 'bignumber.js'
 import { formatBalance } from '../../../plugins/Wallet/formatter'
-import { ETH_ADDRESS } from '../../../plugins/Wallet/token'
+import { TokenIcon } from './TokenIcon'
 
 const useStyles = makeStyles((theme) =>
     createStyles({
@@ -36,19 +35,13 @@ interface TokenListItemProps {
 export function TokenListItem(props: TokenListItemProps) {
     const classes = useStyles()
     const { balance, token } = props
-    const name = token.name?.substr(0, 1).toLocaleUpperCase()
     return (
         <ListItem divider disableGutters>
             <ListItemIcon>
-                <Avatar
-                    className={classes.coin}
-                    src={
-                        token.address === ETH_ADDRESS
-                            ? 'https://rawcdn.githack.com/trustwallet/assets/257c82b25e6f27ede7a2b309aadc0ed17bca45ae/blockchains/ethereum/info/logo.png'
-                            : `https://rawcdn.githack.com/trustwallet/assets/257c82b25e6f27ede7a2b309aadc0ed17bca45ae/blockchains/ethereum/assets/${token.address}/logo.png`
-                    }>
-                    {name}
-                </Avatar>
+                <TokenIcon
+                    classes={{ coin: classes.coin }}
+                    name={token.name?.substr(0, 1).toLocaleUpperCase()}
+                    address={token.address}></TokenIcon>
             </ListItemIcon>
             <ListItemText className={classes.name} primary={token.symbol} secondary={token.name} />
             <ListItemSecondaryAction className={classes.amount}>
