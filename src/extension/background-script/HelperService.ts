c56d375fc1dc31568ac7859f2b947539b986290a
Jack Works <jackworks@protonmail.com>
2020-06-10 15:19:27 +0800

    feat: add a fun to resolve t.co link

--- src/extension/background-script/HelperService.ts
+++ src/extension/background-script/HelperService.ts
@@ -1,3 +1,19 @@
+export async function resolveTCOLink(u: string) {
+    if (!u.startsWith('https://t.co/')) return null
+    const req = await globalThis.fetch(u, {
+        redirect: 'error',
+        credentials: 'omit',
+        referrerPolicy: 'no-referrer',
+    })
+    const text = await req.text()
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(text, 'text/html')
+    const dom = doc.querySelector('noscript > meta') as HTMLMetaElement
+    if (!dom) return null
+    const [, url] = dom.content.split('URL=')
+    return url ?? null
+}
+
 export function fetch(url: string) {
     return globalThis.fetch(url).then((x) => x.arrayBuffer())
 }
