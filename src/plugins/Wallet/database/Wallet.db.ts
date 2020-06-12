cef12c52d55bda94a541caa99a83127d6a99ed9b
guanbinrui <guanbinrui@dimension.im>
2020-06-12 17:30:04 +0800

    chore: db migration

--- src/plugins/Wallet/database/Wallet.db.ts
+++ src/plugins/Wallet/database/Wallet.db.ts
@@ -11,18 +11,21 @@ function path<T>(x: T) {
     return x
 }
 export const createWalletDBAccess = createDBAccess(() => {
-    return openDB<WalletDB>('maskbook-plugin-wallet', 1, {
+    return openDB<WalletDB>('maskbook-plugin-wallet', 2, {
         upgrade(db, oldVersion, newVersion, tx) {
             function v0_v1() {
-                db.createObjectStore('GitcoinDonation', {
-                    keyPath: path<keyof GitcoinDonationRecordInDatabase>('donation_transaction_hash'),
-                })
                 db.createObjectStore('RedPacket', { keyPath: path<keyof RedPacketRecordInDatabase>('id') })
                 db.createObjectStore('ERC20Token', { keyPath: path<keyof WalletRecordInDatabase>('address') })
                 db.createObjectStore('Wallet', { keyPath: path<keyof WalletRecordInDatabase>('address') })
                 tx.objectStore('RedPacket').createIndex('red_packet_id', 'red_packet_id', { unique: true })
             }
+            function v1_v2() {
+                db.createObjectStore('GitcoinDonation', {
+                    keyPath: path<keyof GitcoinDonationRecordInDatabase>('donation_transaction_hash'),
+                })
+            }
             if (oldVersion < 1) v0_v1()
+            if (oldversion < 2) v1_v2()
         },
     })
 })
