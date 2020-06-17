f8ba2affa2f7e02b6b944c0d755eee149a1f8b05
guanbinrui <guanbinrui@dimension.im>
2020-06-17 12:28:39 +0800

    refactor: build different contracts from different folders

--- scripts/contracts.sh
+++ scripts/contracts.sh
@@ -3,11 +3,19 @@
 # TODO:
 # solc is not listed in the deps of project
 # replace it with solcjs
-solc ./contracts/*.sol --abi --overwrite -o ./contracts/ --allow-paths $(pwd)/node_modules/
-solc ./node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol --abi --overwrite -o ./contracts/ --allow-paths $(pwd)/node_modules/
-npx typechain $(./contracts/*.abi) --target=web3-v1 --outDir ./src/plugins/Wallet/contract/
+declare -a contracts=("happy-red-packet" "bulk-checkout")
 
-# copy abi to source folder
-for f in ./contracts/*.abi;
-    do cp "$f" ./src/plugins/Wallet/contract/"$(echo "${f##*/}" | sed s/abi/json/)";
-done;
+for i in "${contracts[@]}"
+do
+    # compole contracts...
+    solc ./contracts/$i/*.sol --abi --overwrite -o ./contracts/$i/ --allow-paths $(pwd)/node_modules/
+
+    # generate typings...
+    npx typechain $(./contracts/$i/*.abi) --target=web3-v1 --outDir ./src/plugins/Wallet/contracts/$i
+
+    # copy abi to source folder...
+    for f in $(./contracts/$i/*.abi)
+    do
+        cp "$f" ./src/plugins/Wallet/contracts/$i/"$(echo "${f##*/}" | sed s/abi/json/)";
+    done;
+done
