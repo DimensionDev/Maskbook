8dc2abc34547b6400b25d7cabe631f61b675afb1
guanbinrui <guanbinrui@dimension.im>
2020-06-19 13:08:02 +0800

    test: comment & post

--- e2e/integration/post/create.ts
+++ e2e/integration/post/create.ts
@@ -1,3 +1,4 @@
+import { join } from 'path'
 import * as dashboard from '../../commands/dashboard'
 import * as restore from '../../commands/restore'
 import { CREATE_POST_STORY_URL } from '../../support/constants'
@@ -11,7 +12,7 @@ beforeAll(async () => {
 
     // restore alice's db backup
     await dashboard.openInitializeRestore(page)
-    await restore.fromFile(page, '../fixtures/post/db_backup_alice.json')
+    await restore.fromFile(page, join(__dirname, '../../fixtures/persona/persona_backup_alice.json'))
 })
 
 afterAll(async () => {
@@ -21,56 +22,57 @@ afterAll(async () => {
 describe(`${CREATE_POST_STORY_URL}#Story:CreatePost(?br=wip)-BasicWorkflow`, () => {
     for (const enableImageMode of [false, true]) {
         for (const sns of [
-            // new Twitter(
-            //     process.env.E2E_ALICE_TWITTER_ID!,
-            //     process.env.E2E_ALICE_TWITTER_USERNAME!,
-            //     process.env.E2E_ALICE_TWITTER_PASSWORD!,
-            // ),
+            new Twitter(
+                process.env.E2E_ALICE_TWITTER_ID!,
+                process.env.E2E_ALICE_TWITTER_USERNAME!,
+                process.env.E2E_ALICE_TWITTER_PASSWORD!,
+            ),
             new Facebook(
                 process.env.E2E_ALICE_FACEBOOK_ID!,
                 process.env.E2E_ALICE_FACEBOOK_USERNAME!,
                 process.env.E2E_ALICE_FACEBOOK_PASSWORD!,
             ),
         ]) {
-            it(`${sns.name} with ${enableImageMode ? 'image based payload' : 'generic payload'}`, async () => {
+            it(`create ${enableImageMode ? 'image-based payload' : 'generic payload'} on ${sns.name}`, async () => {
                 // login sns account
                 const loginPage = await helpers.newPage(page)
                 await sns.login(loginPage)
                 await loginPage.close()
 
                 // setup a new page
-                const snsPage = await helpers.newPage(page)
+                const snsFeedPage = await helpers.newPage(page)
 
                 // open sns news feed
-                await sns.openNewsFeed(snsPage)
+                await sns.openNewsFeed(snsFeedPage)
 
                 // click compose button if needed
                 if (sns.composeButtonSelector) {
-                    const composeButton = await snsPage.waitFor(sns.composeButtonSelector)
+                    const composeButton = await snsFeedPage.waitFor(sns.composeButtonSelector)
                     await composeButton.click()
-                    await snsPage.waitFor(500)
+                    await snsFeedPage.waitFor(500)
                 }
 
                 // wait maskbook inject post dialog hint
-                await snsPage.waitFor(sns.postDialogHintSelector)
+                await snsFeedPage.waitFor(sns.postDialogHintSelector)
 
                 // click the hint button open maskbook post composing view
-                await snsPage.waitFor(500)
-                const hintButton = await snsPage.evaluateHandle(
+                const hintButton = await snsFeedPage.waitForFunction(
                     `document.querySelector('${sns.postDialogHintSelector}').shadowRoot.querySelector('[data-testid="hint_button"]')`,
                 )
                 await (hintButton as any).click()
 
+                // wait maskbook inject post dialog modal
+                await snsFeedPage.waitFor(sns.postDialogModalSelector)
+
                 // type plain text
-                await snsPage.waitFor(500)
-                const textTextarea = await snsPage.evaluateHandle(
+                const textTextarea = await snsFeedPage.waitForFunction(
                     `document.querySelector('${sns.postDialogModalSelector}').shadowRoot.querySelector('[data-testid="text_textarea"]')`,
                 )
                 await (textTextarea as any).type('maskbook')
 
                 // designates recipients
-                await snsPage.waitFor(500)
-                const defaultGroupChip = await snsPage.evaluateHandle(
+                await snsFeedPage.waitFor(500)
+                const defaultGroupChip = await snsFeedPage.waitForFunction(
                     `document.querySelector('${sns.postDialogModalSelector}').shadowRoot.querySelector('[data-testid="_default_friends_group_"]')`,
                 )
                 await (defaultGroupChip as any).click()
@@ -81,27 +83,26 @@ describe(`${CREATE_POST_STORY_URL}#Story:CreatePost(?br=wip)-BasicWorkflow`, () 
                 await dashboardPage.close()
 
                 // click the finish
-                await snsPage.waitFor(500)
-                const finishButton = await snsPage.evaluateHandle(
+                const finishButton = await snsFeedPage.waitForFunction(
                     `document.querySelector('${sns.postDialogModalSelector}').shadowRoot.querySelector('[data-testid="finish_button"]')`,
                 )
                 await (finishButton as any).click()
 
                 // validate text
-                await snsPage.waitFor(2000)
-                const payloadTextarea = await snsPage.waitFor(sns.composeEditorSelector)
+                await snsFeedPage.waitFor(2000)
+                const payloadTextarea = await snsFeedPage.waitFor(sns.composeEditorSelector)
                 const cipherText = await payloadTextarea.evaluate(e => e.textContent)
                 expect(cipherText?.includes('Maskbook')).toBe(true)
 
                 // valdiate attachment
                 if (enableImageMode) {
-                    const payloadImage = await snsPage.waitFor(sns.composeImageSelector)
+                    const payloadImage = await snsFeedPage.waitFor(sns.composeImageSelector)
                     const imageUrl = await payloadImage.evaluate(e => e.getAttribute('src'))
                     expect(imageUrl).toBeTruthy()
                 }
 
                 // close the page
-                await snsPage.close()
+                await snsFeedPage.close()
             })
         }
     }
