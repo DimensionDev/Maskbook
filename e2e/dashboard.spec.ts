d56d80678300fbf57e754395288dfc9f0ed667f7
guanbinrui <guanbinrui@dimension.im>
2020-06-23 12:27:34 +0800

    chore: create red packet improvements
    close #1096
    close #1270
    
    + add loading UI when creating red packet
    + add error dialog if creating failed
    + sort existing red packets by create nonce

--- e2e/dashboard.spec.ts
+++ e2e/dashboard.spec.ts
@@ -3,11 +3,6 @@ describe('dashboard - setup a account', () => {
         await page.goto('chrome-extension://jkoeaghipilijlahjplgbfiocjhldnap/index.html')
     })
 
-    it('render welcome page', async () => {
-        const setupButton = await page.waitFor('a[href="#/setup/restore-database"]')
-        expect(await setupButton.evaluate((e) => e.textContent.toLowerCase())).toBe('set up')
-    })
-
     it('take a screenshot', async () => {
         await page.screenshot({
             path: './screenshots/welcome.png',
