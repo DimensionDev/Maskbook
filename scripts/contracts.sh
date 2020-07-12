3122353bcaea6fcae2a2dbb4cea6180474b0e379
guanbinrui <guanbinrui@dimension.im>
2020-07-12 10:54:46 +0800

    refactor: compile script

--- scripts/contracts.sh
+++ scripts/contracts.sh
@@ -5,46 +5,38 @@
 # replace it with solcjs
 
 # self hosted contracts
-declare -a selfHostedContracts=("happy-red-packet" "bulk-checkout" "splitter")
 
-for i in "${selfHostedContracts[@]}"
+declare -a contract_names=("happy-red-packet" "bulk-checkout" "splitter" "balance-checker")
+declare -a contract_hosted=(true true true false)
+declare size=${#contract_names[@]}
+
+for ((i=0; i < $size; i++));
 do
-    for f in ./contracts/$i/*.sol
-    do         
-        # compole contracts
-        solc $f --abi --overwrite -o ./contracts/$i/ --allow-paths $(pwd)/node_modules/
-    done
+    declare contract_name=${contract_names[$i]}
 
-    for f in ./contracts/$i/*.abi
+    if [ ${contract_hosted[$i]} == true ]; then
+        for f in ./contracts/$contract_name/*.sol
+        do         
+            # compile contracts
+            solc $f --abi --overwrite -o ./contracts/$contract_name/ --allow-paths $(pwd)/node_modules/
+        done
+    fi
+
+    for f in ./contracts/$contract_name/*.abi
     do 
         # generate typings
-        npx typechain $f --target=web3-v1 --outDir ./src/plugins/Wallet/contracts/$i
+        npx typechain $f --target=web3-v1 --outDir ./src/plugins/Wallet/contracts/$contract_name
 
         # copy abi to source folder
-        cp $f ./src/plugins/Wallet/contracts/$i/"$(echo "${f##*/}" | sed s/abi/json/)"
+        cp $f ./src/plugins/Wallet/contracts/$contract_name/"$(echo "${f##*/}" | sed s/abi/json/)"
 
         # rm duplicate type.d.ts
-        mv ./src/plugins/Wallet/contracts/$i/types.d.ts ./src/plugins/Wallet/contracts/
+        mv ./src/plugins/Wallet/contracts/$contract_name/types.d.ts ./src/plugins/Wallet/contracts/types.d.ts
     done
 done
 
-# external contracts
-declare -a externalContracts=("balance-checker")
-
-for i in "${externalContracts[@]}"
-do
-    for f in ./contracts/$i/*.abi
-    do 
-        # generate typings
-        npx typechain $f --target=web3-v1 --outDir ./src/plugins/Wallet/contracts/$i
-
-        # copy abi to source folder
-        cp $f ./src/plugins/Wallet/contracts/$i/"$(echo "${f##*/}" | sed s/abi/json/)"
-
-        # rm duplicate type.d.ts
-        mv ./src/plugins/Wallet/contracts/$i/types.d.ts ./src/plugins/Wallet/contracts/
-    done
-done
+# fix the import path of type.d.ts
+sed -i '' "s/.\/types/..\/types/" ./src/plugins/Wallet/contracts/**/*.d.ts
 
 # format code
 npx prettier ./src/plugins/Wallet/contracts/* --write
