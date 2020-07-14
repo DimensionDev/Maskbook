59fd6aa15cdd42b5928be13d036c5cf6e3044858
guanbinrui <guanbinrui@dimension.im>
2020-07-14 11:07:35 +0800

    chore(ui): mimic button style

--- src/components/InjectedComponents/TransferButton.tsx
+++ src/components/InjectedComponents/TransferButton.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney'
 import { IconButton, IconButtonProps } from '@material-ui/core'
 
 export interface TransferButtonProps extends IconButtonProps {}
 
 export function TransferButton({ ...props }: TransferButtonProps) {
+    const onTransfer = useCallback(() => {
+        console.log("DEBUG: let's transfer")
+    }, [])
     return (
-        <IconButton {...props}>
+        <IconButton color="default" onClick={onTransfer} {...props}>
             <AttachMoneyIcon />
         </IconButton>
     )
