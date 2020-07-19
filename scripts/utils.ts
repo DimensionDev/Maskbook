536a0c2d911057562f9bcb67ec63f03acf097d03
Septs <github@septs.pw>
2020-07-19 14:35:53 +0800

    chore: use github packages

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -7,6 +7,7 @@ export const NODE_MODULES_PATH = path.resolve(ROOT_PATH, 'node_modules')
 export const BUILD_PATH = path.resolve(ROOT_PATH, 'build')
 
 export function run(cwd: string | undefined, cmd: string, ...args: string[]) {
+    console.log('$', cmd, args.join(' '), '# cwd:', cwd)
     return execFileSync(cmd, args, {
         cwd,
         stdio: [process.stdin, process.stdout, process.stderr],
