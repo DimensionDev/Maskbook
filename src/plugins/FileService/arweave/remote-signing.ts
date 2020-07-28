6879aa7f49bf419e83eeb02eccb2ac8d1c434658
Septs <github@septs.pw>
2020-07-28 23:29:42 +0800

    feat: new remote signing method

--- src/plugins/FileService/arweave/remote-signing.ts
+++ src/plugins/FileService/arweave/remote-signing.ts
@@ -1,11 +1,30 @@
+import { encode } from '@msgpack/msgpack'
 import type Transaction from 'arweave/web/lib/transaction'
+import { stringToBuffer as toBuffer } from 'arweave/web/lib/utils'
 import { signing } from '../constants'
 
 export async function sign(transaction: Transaction) {
     const response = await fetch(signing, {
         method: 'POST',
-        body: await transaction.getSignatureData(),
+        body: await makeRequest(transaction),
     })
-    const signed = await response.json()
-    transaction.setSignature(signed)
+    transaction.setSignature(await response.json())
+}
+
+async function makeRequest(transaction: Transaction) {
+    await transaction.prepareChunks(transaction.data)
+    return encode([
+        toBuffer(transaction.format.toString()),
+        transaction.get('owner', { decode: true, string: false }),
+        transaction.get('target', { decode: true, string: false }),
+        toBuffer(transaction.quantity),
+        toBuffer(transaction.reward),
+        transaction.get('last_tx', { decode: true, string: false }),
+        transaction.tags.map((tag) => [
+            tag.get('name', { decode: true, string: false }),
+            tag.get('value', { decode: true, string: false }),
+        ]),
+        toBuffer(transaction.data_size),
+        transaction.get('data_root', { decode: true, string: false }),
+    ])
 }
--- src/plugins/FileService/constants.ts
+++ src/plugins/FileService/constants.ts
@@ -7,6 +7,6 @@ export const META_KEY_1 = 'com.maskbook.fileservice:1'
 export const MAX_FILE_SIZE = 0xa00000 // = 10 MiB
 
 export const landing = 'https://files.maskbook.com/partner/arweave/landing-page.html'
-export const signing = 'https://tfuhgly4y0.execute-api.ap-east-1.amazonaws.com/arweave-submit-agent'
+export const signing = 'https://37oxcroa15.execute-api.ap-east-1.amazonaws.com'
 export const legalTerms = 'https://legal.maskbook.com/arweave/file-service/plugin-terms.html'
 export const legalPolicy = 'https://legal.maskbook.com/arweave/file-service/privacy--uploader.html'
