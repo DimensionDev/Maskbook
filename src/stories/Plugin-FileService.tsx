5f2aa984cc8917a2f0f28bcf3f1dced47d0b79d2
Jack Works <jackworks@protonmail.com>
2020-08-04 14:46:24 +0800

    chore: add story for file service preview

--- src/stories/Plugin-FileService.tsx
+++ src/stories/Plugin-FileService.tsx
@@ -3,13 +3,30 @@ import { storiesOf } from '@storybook/react'
 import React from 'react'
 import MainDialog from '../plugins/FileService/MainDialog'
 import { figmaLink } from './utils'
+import { Preview } from '../plugins/FileService/Preview'
+import { text, boolean, number } from '@storybook/addon-knobs'
 
-storiesOf('Plugin: File Service', module).add(
-    'Main Dialog',
-    () => (
-        <div style={{ padding: 16, background: '#fff' }}>
-            <MainDialog open onConfirm={action('onConfirm')} onDecline={action('onDecline')} />
-        </div>
-    ),
-    figmaLink('https://www.figma.com/file/jcrvENZmip6PUR66Jz6ENb/Plugin%3A-File-Service'),
-)
+storiesOf('Plugin: File Service', module)
+    .add(
+        'Main Dialog',
+        () => <MainDialog open onConfirm={action('onConfirm')} onDecline={action('onDecline')} />,
+        figmaLink('https://www.figma.com/file/jcrvENZmip6PUR66Jz6ENb/Plugin%3A-File-Service'),
+    )
+    .add('Preview', () => {
+        const decrypted = boolean('Decrypted', true)
+        const key = text('Key', 'Decrypted key')
+        return (
+            <Preview
+                info={{
+                    createdAt: new Date(),
+                    id: text('id', 'id'),
+                    key: decrypted ? key : void 0,
+                    landingTxID: text('Transaction ID', 'Transaction ID'),
+                    name: text('File name', 'file.png'),
+                    payloadTxID: text('Payload TxID', 'Payload TxID'),
+                    size: number('File Size', 2333),
+                    type: 'arweave',
+                }}
+            />
+        )
+    })
