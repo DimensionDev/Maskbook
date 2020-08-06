1441737ff5671ddfe20baf916948a05dedb66076
Septs <github@septs.pw>
2020-08-06 13:14:42 +0800

    feat: dump database

---
+++ src/extension/debug-page/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Entry } from './Entry'
+
+const container = document.createElement('main')
+
+ReactDOM.render(<Entry />, container, () => {
+    document.body.appendChild(container)
+})
