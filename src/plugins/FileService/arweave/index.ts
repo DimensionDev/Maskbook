a160c5ffc6799aee7eb1358611a503c2f2445c79
Jack Works <jackworks@protonmail.com>
2020-08-10 15:49:19 +0800

    fix: build error

--- src/plugins/FileService/arweave/index.ts
+++ src/plugins/FileService/arweave/index.ts
@@ -14,15 +14,6 @@ const instance = Arweave.init({
     protocol: 'https',
 })
 
-export function makeFileKey(length = 16) {
-    let key = ''
-    const table = 'ABDEFGHJKMNPQRTWXYadefhijkmnprstuvwxyz03478'
-    for (let i = 0; i < length; i += 1) {
-        key += table.charAt(Math.floor(Math.random() * table.length))
-    }
-    return key
-}
-
 export interface AttachmentOptions {
     key?: string | null
     type: string
