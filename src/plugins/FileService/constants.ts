0df4b641d51b0743dfc3a37e046e8ea37a9746cf
Septs <github@septs.pw>
2020-08-13 15:50:37 +0800

    chore: enable production key

--- src/plugins/FileService/constants.ts
+++ src/plugins/FileService/constants.ts
@@ -6,9 +6,7 @@ export const META_KEY_1 = 'com.maskbook.fileservice:1'
 export const MAX_FILE_SIZE = 0xa00000 // = 10 MiB
 
 export const landing = 'https://files.maskbook.com/partner/arweave/landing-page.html'
-// in production
-// https://service.maskbook.com/arweave-remote-signing
-export const signing = 'https://f8149vcdfi.execute-api.ap-east-1.amazonaws.com/arweave-remote-signing'
+export const signing = 'https://service.maskbook.com/arweave-remote-signing'
 
 export const enum FileRouter {
     upload = '/upload',
