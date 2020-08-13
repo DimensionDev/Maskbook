f6b78ea8e7f03d2a8b4fc1682234af1de6d99f3c
guanbinrui <guanbinrui@dimension.im>
2020-08-13 15:36:12 +0800

    chore: restore wallets from backup

--- src/utils/type-transform/BackupFormat/JSON/DBRecord-JSON/WalletRecord.ts
+++ src/utils/type-transform/BackupFormat/JSON/DBRecord-JSON/WalletRecord.ts
@@ -1,12 +1,12 @@
 import type { WalletRecord, ManagedWalletRecord } from '../../../../../plugins/Wallet/database/types'
 import type { BackupJSONFileLatest } from '../latest'
-import { keyToJWK, keyToAddr } from '../../../SECP256k1-ETH'
+import { keyToJWK, keyToAddr, JWKToKey } from '../../../SECP256k1-ETH'
 import { isSameAddr } from '../../../../../plugins/Wallet/token'
 
 type WalletBackup = BackupJSONFileLatest['wallets'][0]
 
 export function WalletRecordToJSONFormat(wallet: WalletRecord): WalletBackup {
-    const json: Partial<WalletBackup> = {
+    const backup: Partial<WalletBackup> = {
         name: wallet.name ?? '',
         address: wallet.address,
         createdAt: wallet.createdAt.getTime(),
@@ -16,9 +16,9 @@ export function WalletRecordToJSONFormat(wallet: WalletRecord): WalletBackup {
     // generate keys for managed wallet
     try {
         const wallet_ = wallet as ManagedWalletRecord
-        json.passphrase = wallet_.passphrase
+        backup.passphrase = wallet_.passphrase
         if (wallet_.mnemonic?.length)
-            json.mnemonic = {
+            backup.mnemonic = {
                 words: wallet_.mnemonic.join(' '),
                 parameter: {
                     path: "m/44'/60'/0'/0/0",
@@ -26,22 +26,37 @@ export function WalletRecordToJSONFormat(wallet: WalletRecord): WalletBackup {
                 },
             }
         if (wallet_._public_key_ && isSameAddr(keyToAddr(wallet_._public_key_, 'public'), wallet.address))
-            json.publicKey = keyToJWK(wallet_._public_key_, 'public')
+            backup.publicKey = keyToJWK(wallet_._public_key_, 'public')
         if (wallet_._private_key_ && isSameAddr(keyToAddr(wallet_._private_key_, 'private'), wallet.address))
-            json.privateKey = keyToJWK(wallet_._private_key_, 'private')
+            backup.privateKey = keyToJWK(wallet_._private_key_, 'private')
     } catch (e) {
         console.error(e)
     }
-    return json as WalletBackup
+    return backup as WalletBackup
 }
 
 export function WalletRecordFromJSONFormat(wallet: WalletBackup): WalletRecord {
     const record: Partial<WalletRecord> = {
+        type: 'exotic',
         name: wallet.name,
         address: wallet.address,
         createdAt: new Date(wallet.createdAt),
         updatedAt: new Date(wallet.updatedAt),
     }
-    const record_ = record as ManagedWalletRecord
-    return record_
+    if (wallet.mnemonic?.words) {
+        const record_ = record as ManagedWalletRecord
+        record_.type = 'managed'
+        record_.passphrase = wallet.passphrase ?? ''
+        record_.mnemonic = wallet.mnemonic.words.split(' ')
+    }
+    if (wallet.privateKey) {
+        const record_ = record as ManagedWalletRecord
+        record_.type = 'managed'
+        record_._private_key_ = JWKToKey(wallet.privateKey, 'private')
+    }
+    if (wallet.publicKey) {
+        const record_ = record as ManagedWalletRecord
+        record_._public_key_ = JWKToKey(wallet.publicKey, 'public')
+    }
+    return record as WalletRecord
 }
--- src/utils/type-transform/SECP256k1-ETH.ts
+++ src/utils/type-transform/SECP256k1-ETH.ts
@@ -1,3 +1,4 @@
+import secp256k1 from 'tiny-secp256k1'
 import { ec as EC } from 'elliptic'
 import { EthereumAddress } from 'wallet.ts'
 import type {
@@ -5,7 +6,7 @@ import type {
     EC_Private_JsonWebKey,
     EC_JsonWebKey,
 } from '../../modules/CryptoAlgorithm/interfaces/utils'
-import { Convert } from 'pvtsutils'
+import { Convert, combine } from 'pvtsutils'
 
 export function keyToJWK(key: string, type: 'public'): EC_Public_JsonWebKey
 export function keyToJWK(key: string, type: 'private'): EC_Private_JsonWebKey
@@ -30,7 +31,21 @@ export function keyToJWK(key: string, type: 'public' | 'private'): JsonWebKey {
 }
 
 export function JWKToKey(jwk: EC_JsonWebKey, type: 'public' | 'private'): string {
-    return ''
+    const ec = new EC('secp256k1')
+    if (type === 'public' && jwk.x && jwk.y) {
+        const xb = ab(jwk.x)
+        const yb = ab(jwk.y)
+        const point = Buffer.from(combine(new Uint8Array([0x04]), xb, yb))
+        if (secp256k1.isPoint(point)) return `0x${ec.keyFromPublic(point).getPublic(false, 'hex')}`
+    }
+    if (type === 'private' && jwk.d) {
+        const db = Buffer.from(ab(jwk.d))
+        if (secp256k1.isPrivate(db)) return `0x${ec.keyFromPrivate(db).getPrivate('hex')}`
+    }
+    throw new Error('invalid private key')
+    function ab(base64: string) {
+        return Convert.FromBase64Url(base64)
+    }
 }
 
 export function keyToAddr(key: string, type: 'public' | 'private'): string {
