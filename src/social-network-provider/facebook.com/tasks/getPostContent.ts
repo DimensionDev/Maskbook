f4d7cd9e407fbb3504df6fe264085514a695afc4
Jack Works <jackworks@protonmail.com>
2020-08-24 15:02:33 +0800

    feat: support new fb post content

--- src/social-network-provider/facebook.com/tasks/getPostContent.ts
+++ src/social-network-provider/facebook.com/tasks/getPostContent.ts
@@ -1,8 +1,11 @@
 import { LiveSelector, MutationObserverWatcher } from '@holoflows/kit/es'
 import { timeout } from '../../../utils/utils'
 
+const postContent = new LiveSelector().querySelector<HTMLElement>('[data-ad-preview="message"]')
 export async function getPostContentFacebook(): Promise<string> {
-    const post = new LiveSelector().querySelector('#contentArea').getElementsByTagName('p')
+    return get(postContent)
+}
+async function get(post: LiveSelector<HTMLElement>) {
     const [data] = await timeout(new MutationObserverWatcher(post), 10000)
     return data.innerText
 }
