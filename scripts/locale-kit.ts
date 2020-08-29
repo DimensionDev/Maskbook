6572f5732994bcd17be0a35e3240a665c4415f70
Septs <github@septs.pw>
2020-08-29 11:02:31 +0800

    chore: simplify locale-kit

--- scripts/locale-kit.ts
+++ scripts/locale-kit.ts
@@ -4,7 +4,6 @@ import path from 'path'
 
 const SOURCE_PATH = path.join(__dirname, '..', 'src')
 const LOCALE_PATH = path.join(SOURCE_PATH, '_locales')
-const EN_LOCALE_PATH = path.join(LOCALE_PATH, 'en', 'messages.json')
 
 async function* walk(dir: string): AsyncIterableIterator<string> {
     for await (const dirent of await fs.opendir(dir)) {
@@ -17,9 +16,19 @@ async function* walk(dir: string): AsyncIterableIterator<string> {
     }
 }
 
+async function readMessages(name: string) {
+    const target = path.join(LOCALE_PATH, name, 'messages.json')
+    return JSON.parse(await fs.readFile(target, 'utf-8'))
+}
+
+async function writeMessages(name: string, messages: unknown) {
+    const target = path.join(LOCALE_PATH, name, 'messages.json')
+    await fs.writeFile(target, JSON.stringify(messages, null, 4), 'utf-8')
+}
+
 async function findAllUnusedKeys() {
     const usedKeys: string[] = []
-    const keys = _.keys(JSON.parse(await fs.readFile(EN_LOCALE_PATH, 'utf-8')))
+    const keys = _.keys(await readMessages('en'))
     for await (const file of walk(SOURCE_PATH)) {
         const content = await fs.readFile(file, 'utf-8')
         const filteredKeys = keys.filter(
@@ -33,22 +42,21 @@ async function findAllUnusedKeys() {
 async function removeAllUnusedKeys(keys: string[]) {
     const locales = ['en', 'zh', 'ja']
     for (const name of locales) {
-        const localePath = path.join(LOCALE_PATH, name, 'messages.json')
-        const messages = JSON.parse(await fs.readFile(localePath, 'utf-8'))
-        const modifedMessages = _.omit(messages, keys)
-        await fs.writeFile(localePath, JSON.stringify(modifedMessages, null, 4), 'utf-8')
+        const modifedMessages = _.omit(await readMessages(name), keys)
+        await writeMessages(name, modifedMessages)
     }
 }
 
 async function syncKeyOrder() {
     const locales = ['zh', 'ja']
-    const keys = _.keys(JSON.parse(await fs.readFile(EN_LOCALE_PATH, 'utf-8')))
+    const keys = _.keys(await readMessages('en'))
     for (const name of locales) {
-        const localePath = path.join(LOCALE_PATH, name, 'messages.json')
-        let pairs = _.toPairs(JSON.parse(await fs.readFile(localePath, 'utf-8')))
-        pairs = _.sortBy(pairs, ([key]) => keys.indexOf(key))
-        const modifedMessages = _.fromPairs(pairs)
-        await fs.writeFile(localePath, JSON.stringify(modifedMessages, null, 4), 'utf-8')
+        const modifedMessages = _.chain(await readMessages(name))
+            .toPairs()
+            .sortBy(([key]) => keys.indexOf(key))
+            .fromPairs()
+            .value()
+        await writeMessages(name, modifedMessages)
     }
 }
 
