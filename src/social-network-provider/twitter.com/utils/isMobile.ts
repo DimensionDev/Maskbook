d607c43cacf77f31f8906b833f3a1c83b4e9cfe6
Jack Works <jackworks@protonmail.com>
2020-08-31 15:50:57 +0800

    feat: add a link from dashboard to sns

--- src/social-network-provider/twitter.com/utils/isMobile.ts
+++ src/social-network-provider/twitter.com/utils/isMobile.ts
@@ -4,3 +4,4 @@ import { twitterUrl } from './url'
 export const isMobileTwitter = OnlyRunInContext('content', false)
     ? location.hostname === twitterUrl.hostLeadingUrlMobile.substr(8)
     : !!navigator.userAgent.match(/Mobile|mobile/)
+export const twitterDomain = isMobileTwitter ? 'https://m.twitter.com/' : 'https://twitter.com/'
