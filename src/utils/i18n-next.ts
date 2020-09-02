69f4cde6013b8b0559195ced98ddb1914882f314
Jack Works <jackworks@protonmail.com>
2020-09-02 10:14:23 +0800

    chore: ensure i18n fallback

--- src/utils/i18n-next.ts
+++ src/utils/i18n-next.ts
@@ -4,11 +4,15 @@ import zh from '../_locales/zh/messages.json'
 import ja from '../_locales/ja/messages.json'
 import type { I18NFunction } from './i18n-next-ui'
 export type I18NStrings = typeof en
+
+function removeEmpty(lang: Record<string, string>) {
+    return Object.fromEntries(Object.entries(lang).filter((x) => x[1].length))
+}
 i18nNextInstance.init({
     resources: {
         en: { translation: en },
-        zh: { translation: zh },
-        ja: { translation: ja },
+        zh: { translation: removeEmpty(zh) },
+        ja: { translation: removeEmpty(ja) },
     },
     keySeparator: false,
     interpolation: {
