ed0e9f0e2d27b90fd0ccdc588a470dcf66fc12c7
Hancheng Zhou <z308114274@gmail.com>
2020-09-02 13:54:51 +0800

    chore: improve locale kit
    chore: improve locale kit
    
    chore: improve locale kit
    
    chore: improve locale kit
    
    chore: improve locale kit
    
    chore: improve locale kit
    
    chore: improve locale kit
    
    chore: improve locale kit
    
    chore: improve locale kit

--- scripts/locale-kit.ts
+++ scripts/locale-kit.ts
@@ -41,22 +41,43 @@ function getUsedKeys(content: string) {
         }
         return undefined
     }
-    const addKey = (node: ts.Node) => {
-        if (ts.isStringLiteralLike(node)) {
-            keys.add(node.text)
-        }
-    }
     const transformer = (context: ts.TransformationContext) => (rootNode: ts.Node) => {
+        const setFromVariableWrapper = (variableValue: string): ((node: ts.Node) => ts.Node) => {
+            const setFromVariable = (node: ts.Node): ts.Node => {
+                if (
+                    ts.isVariableDeclaration(node) &&
+                    ts.isVariableDeclarationList(node.parent) &&
+                    !(node.parent.flags ^ ts.NodeFlags.Const) &&
+                    ts.isIdentifier(node.name) &&
+                    node.name.text === variableValue &&
+                    node.initializer &&
+                    ts.isStringLiteralLike(node.initializer)
+                ) {
+                    keys.add(node.initializer.text)
+                }
+                return ts.visitEachChild(node, setFromVariable, context)
+            }
+            return setFromVariable
+        }
+        const addKey = (node: ts.Node) => {
+            if (ts.isStringLiteralLike(node)) {
+                keys.add(node.text)
+            } else if (ts.isIdentifier(node)) {
+                setFromVariableWrapper(node.text)(rootNode)
+            } else if (ts.isJsxExpression(node) && node.expression) {
+                setFromVariableWrapper(node.expression.getText())(rootNode)
+            }
+        }
         const visit: ts.Visitor = (node) => {
             if (ts.isIdentifier(node) && node.text === 't') {
                 const localeKey = closest(node, ts.isCallExpression)?.arguments[0]
                 if (localeKey === undefined) {
                     return node
-                } else if (ts.isStringLiteralLike(localeKey)) {
-                    addKey(localeKey)
                 } else if (ts.isConditionalExpression(localeKey)) {
                     addKey(localeKey.whenTrue)
                     addKey(localeKey.whenFalse)
+                } else {
+                    addKey(localeKey)
                 }
             } else if (ts.isJsxAttribute(node) && node.name.escapedText === 'i18nKey' && node.initializer) {
                 addKey(node.initializer)
