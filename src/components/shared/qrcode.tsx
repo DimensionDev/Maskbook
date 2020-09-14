49b9c16437a25bb25c651f4bd0646d52d6203681
Hancheng Zhou <z308114274@gmail.com>
2020-09-14 15:02:23 +0800

    fix: cache qrcode

--- src/components/shared/qrcode.tsx
+++ src/components/shared/qrcode.tsx
@@ -3,6 +3,15 @@ import { useAsync } from 'react-use'
 import qr from 'qrcode'
 import { iOSHost } from '../../utils/iOS-RPC'
 
+interface QRProps {
+    text: string
+    options?: qr.QRCodeRenderersOptions
+    canvasProps?: React.DetailedHTMLProps<
+        React.CanvasHTMLAttributes<HTMLCanvasElement & HTMLImageElement>,
+        HTMLCanvasElement & HTMLImageElement
+    >
+}
+
 const cache = new Proxy(sessionStorage, {
     get(t, p: 'get' | 'set') {
         return {
@@ -16,26 +25,19 @@ const cache = new Proxy(sessionStorage, {
     },
 })
 
-export function QRCode(props: {
-    text: string
-    options?: qr.QRCodeRenderersOptions
-    canvasProps?: React.DetailedHTMLProps<
-        React.CanvasHTMLAttributes<HTMLCanvasElement & HTMLImageElement>,
-        HTMLCanvasElement & HTMLImageElement
-    >
-}) {
+export function QRCode({ text, options = {}, canvasProps }: QRProps) {
     const ref = useRef<HTMLCanvasElement | null>(null)
-    const image = cache.get(props.text)
+    const image = cache.get(text)
     useEffect(() => {
-        if (cache.get(props.text) || !ref.current) return
-        qr.toCanvas(ref.current, props.text, props.options)
-        return () => {
-            // if already rendered canvas, do not re-render img
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-            cache.set(props.text, ref.current?.toDataURL())
-        }
-    }, [props.options, props.text])
-    return image ? <img src={image} {...props.canvasProps} /> : <canvas {...props.canvasProps} ref={ref} />
+        if (!ref.current) return
+
+        qr.toCanvas(ref.current, text, options, () => {
+            if (ref.current?.toDataURL()) {
+                cache.set(text, ref.current?.toDataURL())
+            }
+        })
+    }, [options, text])
+    return image ? <img src={image} {...canvasProps} /> : <canvas {...canvasProps} ref={ref} />
 }
 
 export function WKWebkitQRScanner(props: { onScan?: (val: string) => void; onQuit?: () => void }) {
