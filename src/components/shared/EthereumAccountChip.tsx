0e5ba4e96ec3b74e1188149e6d90b3445a7cf4de
guanbinrui <guanbinrui@dimension.im>
2020-09-21 21:09:25 +0800

    chore: add EthereumAccountChip

--- src/components/shared/EthereumAccountChip.tsx
+++ src/components/shared/EthereumAccountChip.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Chip } from '@material-ui/core'
+import { Chip, ChipProps, makeStyles, Theme, createStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) => {
+    return createStyles({
+        root: {
+            borderRadius: 12,
+            lineHeight: 1,
+        },
+        balance: {
+            fontSize: 12,
+            marginRight: theme.spacing(1),
+        },
+    })
+})
 
 export interface EthereumAccountChipProps {
-    address: string
+    address?: string
+    ChipProps?: Partial<ChipProps>
 }
 
-export function EthereumAccountChip({ address }: EthereumAccountChipProps) {
+export function EthereumAccountChip({ address = '', ChipProps }: EthereumAccountChipProps) {
+    const classes = useStyles()
     const address_ = address.replace(/^0x/i, '')
-    return <Chip size="small" label={`0x${address_.slice(0, 4)}...${address_.slice(-4)}`}></Chip>
+    if (!address_) return null
+    return <Chip className={classes.root} size="medium" label={`0x${address_.slice(0, 4)}...${address_.slice(-4)}`} />
 }
