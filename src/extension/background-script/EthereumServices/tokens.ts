f4680306402d87e28bc85bdad6e338e446cae2f4
guanbinrui <guanbinrui@dimension.im>
2020-09-21 19:06:42 +0800

    refactor: useTokensList

--- src/extension/background-script/EthereumServices/tokens.ts
+++ src/extension/background-script/EthereumServices/tokens.ts
@@ -1,4 +1,4 @@
-import { Token, EthereumTokenType } from '../../../web3/types'
+import { Token, EthereumTokenType, ChainId } from '../../../web3/types'
 import { flatMap, uniq } from 'lodash-es'
 
 interface TokenList {
@@ -25,17 +25,21 @@ export async function fetchTokenList(url: string) {
     return response.json() as Promise<TokenList>
 }
 
-export async function fetchTokensFromTokenList(url: string): Promise<Token[]> {
+export async function fetchTokensFromTokenList(url: string, chainId: ChainId = ChainId.Mainnet): Promise<Token[]> {
     const { tokens } = await fetchTokenList(url)
-    return tokens.map((x) => ({
-        type: EthereumTokenType.ERC20,
-        ...x,
-    }))
+    return tokens
+        .filter((x) => x.chainId === chainId)
+        .map((x) => ({
+            type: EthereumTokenType.ERC20,
+            ...x,
+        }))
 }
 
-export async function fetchTokensFromTokenLists(urls: string[]): Promise<Token[]> {
+export async function fetchTokensFromTokenLists(urls: string[], chainId: ChainId = ChainId.Mainnet): Promise<Token[]> {
     const uniqueSet = new Set<string>()
-    const tokens = flatMap(await Promise.all(urls.map((x) => fetchTokensFromTokenList(x))))
+    const tokens = (await Promise.allSettled(urls.map((x) => fetchTokensFromTokenList(x, chainId)))).flatMap((x) =>
+        x.status === 'fulfilled' ? x.value : [],
+    )
     return tokens.filter((x) => {
         const key = x.address.toLowerCase()
         if (uniqueSet.has(key)) return false
