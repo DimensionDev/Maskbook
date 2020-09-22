42d7d9212f2d8ef521a65e4eb76788e19c85f435
guanbinrui <guanbinrui@dimension.im>
2020-09-23 00:39:21 +0800

    fix: the block number doesn't react to visibility changes

--- src/web3/hooks/useBlockNumber.ts
+++ src/web3/hooks/useBlockNumber.ts
@@ -1,5 +1,5 @@
 import { useAsyncTimes } from '../../utils/hooks/useAsyncTimes'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import Services from '../../extension/service'
 import { useIsWindowVisible } from '../../utils/hooks/useIsWindowVisible'
 
@@ -7,16 +7,20 @@ import { useIsWindowVisible } from '../../utils/hooks/useIsWindowVisible'
  * Polling block number
  */
 export function useBlockNumber() {
+    const [blockNumber, setBlockNumber] = useState(0)
     const isWindowVisible = useIsWindowVisible()
     const callback = useCallback(async () => {
+        console.log('DEBUG: isWindowVisible')
+        console.log(isWindowVisible)
         if (!isWindowVisible) return
-        return Services.Ethereum.getBlockNumber()
+        setBlockNumber(await Services.Ethereum.getBlockNumber())
     }, [isWindowVisible])
 
     // TODO:
     // add listener on web3 provider
-    return useAsyncTimes(callback, {
+    useAsyncTimes(callback, {
         times: Number.MAX_SAFE_INTEGER,
-        delay: 30 /* seconds */ * 1000 /* milliseconds */,
+        delay: 15 /* seconds */ * 1000 /* milliseconds */,
     })
+    return blockNumber
 }
