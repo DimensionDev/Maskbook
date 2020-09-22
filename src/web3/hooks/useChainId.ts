48f3ec99fb300f9f2fda41741567723502401908
guanbinrui <guanbinrui@dimension.im>
2020-09-22 12:10:51 +0800

    chore: remove useMemo

--- src/web3/hooks/useChainId.ts
+++ src/web3/hooks/useChainId.ts
@@ -17,10 +17,8 @@ export function useChainId() {
     const maskbookChainId = useValueRef(currentMaskbookChainIdSettings)
     const metamaskChainId = useValueRef(currentMetaMaskChainIdSettings)
     const walletconnectChainId = useValueRef(currentWalletConnectChainIdSettings)
-    return useMemo(() => {
-        if (wallet?.provider === ProviderType.Maskbook) return maskbookChainId
-        if (wallet?.provider === ProviderType.MetaMask) return metamaskChainId
-        if (wallet?.provider === ProviderType.WalletConnect) return walletconnectChainId
-        return ChainId.Mainnet
-    }, [wallet?.provider, maskbookChainId, metamaskChainId, walletconnectChainId])
+    if (wallet?.provider === ProviderType.Maskbook) return maskbookChainId
+    if (wallet?.provider === ProviderType.MetaMask) return metamaskChainId
+    if (wallet?.provider === ProviderType.WalletConnect) return walletconnectChainId
+    return ChainId.Mainnet
 }
