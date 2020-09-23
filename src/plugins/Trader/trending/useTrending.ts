3b982f04ff148535ad386ed3bfa8e33be4c24b24
guanbinrui <guanbinrui@dimension.im>
2020-09-23 18:07:22 +0800

    fix: trending view should not crush by network error

--- src/plugins/Trader/trending/useTrending.ts
+++ src/plugins/Trader/trending/useTrending.ts
@@ -5,7 +5,17 @@ import type { DataProvider, Currency } from '../types'
 export function useTrending(keyword: string, platform: DataProvider, currency: Currency | null) {
     const { value: trending, loading: loadingTrending, error: errorTrending } = useAsync(async () => {
         if (!currency) return null
-        return Services.Plugin.invokePlugin('maskbook.trader', 'getCoinTrendingByKeyword', keyword, platform, currency)
+        try {
+            return Services.Plugin.invokePlugin(
+                'maskbook.trader',
+                'getCoinTrendingByKeyword',
+                keyword,
+                platform,
+                currency,
+            )
+        } catch (e) {
+            return null
+        }
     }, [platform, currency, keyword])
     return {
         value: trending,
