df87c617d726d330628f47da210160b25fdada5f
Jack Works <jackworks@protonmail.com>
2020-10-16 17:02:09 +0800

    fix: iOS scanning

--- src/extension/options-page/DashboardComponents/RestoreFromQRCodeCameraBox.tsx
+++ src/extension/options-page/DashboardComponents/RestoreFromQRCodeCameraBox.tsx
@@ -6,6 +6,8 @@ import { QRCodeVideoScannerDialog } from '../DashboardDialogs/Setup'
 import { PortalShadowRoot } from '../../../utils/shadow-root/ShadowRootPortal'
 import { useStylesExtends } from '../../../components/custom-ui-helper'
 import { useVideoDevices } from '../../../utils/hooks/useVideoDevices'
+import { hasWKWebkitRPCHandlers, iOSHost } from '../../../utils/iOS-RPC'
+import { useAsync } from 'react-use'
 
 const useStyles = makeStyles((theme) =>
     createStyles({
@@ -32,53 +34,60 @@ export interface RestoreFromQRCodeCameraBoxProps extends withClasses<KeysInferFr
     onError?: () => void
 }
 
-export function RestoreFromQRCodeCameraBox(props: RestoreFromQRCodeCameraBoxProps) {
-    const { onScan, onError } = props
+export const RestoreFromQRCodeCameraBox = hasWKWebkitRPCHandlers
+    ? (props: RestoreFromQRCodeCameraBoxProps) => {
+          useAsync(async () => {
+              props.onScan?.(await iOSHost.scanQRCode())
+          })
+          return null
+      }
+    : (props: RestoreFromQRCodeCameraBoxProps) => {
+          const { onScan, onError } = props
 
-    const classes = useStylesExtends(useStyles(), props)
-    const [qrCodeVideoScannerDialog, , openQRCodeVideoScannerDialog] = useModal(QRCodeVideoScannerDialog)
+          const classes = useStylesExtends(useStyles(), props)
+          const [qrCodeVideoScannerDialog, , openQRCodeVideoScannerDialog] = useModal(QRCodeVideoScannerDialog)
 
-    const devices = useVideoDevices()
-    const filteredDevices = devices.filter((d) => !!d.deviceId)
-    const [selectedDeviceId, setSelectedDeviceId] = useState('')
+          const devices = useVideoDevices()
+          const filteredDevices = devices.filter((d) => !!d.deviceId)
+          const [selectedDeviceId, setSelectedDeviceId] = useState('')
 
-    // set default device id
-    useEffect(() => {
-        if (!selectedDeviceId && filteredDevices[0]?.deviceId) setSelectedDeviceId(filteredDevices[0]?.deviceId)
-    }, [filteredDevices, selectedDeviceId])
+          // set default device id
+          useEffect(() => {
+              if (!selectedDeviceId && filteredDevices[0]?.deviceId) setSelectedDeviceId(filteredDevices[0]?.deviceId)
+          }, [filteredDevices, selectedDeviceId])
 
-    return (
-        <Box className={classes.root} display="flex" justifyContent="space-between">
-            <FormControl className={classes.formControl} variant="filled">
-                <Select
-                    value={selectedDeviceId}
-                    variant="outlined"
-                    MenuProps={{
-                        container: PortalShadowRoot,
-                        classes: { paper: classes.menuPaper },
-                    }}
-                    onChange={(e) => setSelectedDeviceId(e.target.value as string)}>
-                    {filteredDevices.map(({ deviceId, label }) => (
-                        <MenuItem key={deviceId} value={deviceId}>
-                            {label}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <Button
-                className={classes.button}
-                variant="outlined"
-                disabled={!selectedDeviceId}
-                onClick={() =>
-                    openQRCodeVideoScannerDialog({
-                        deviceId: selectedDeviceId,
-                        onScan,
-                        onError,
-                    })
-                }>
-                <CropFreeIcon />
-            </Button>
-            {qrCodeVideoScannerDialog}
-        </Box>
-    )
-}
+          return (
+              <Box className={classes.root} display="flex" justifyContent="space-between">
+                  <FormControl className={classes.formControl} variant="filled">
+                      <Select
+                          value={selectedDeviceId}
+                          variant="outlined"
+                          MenuProps={{
+                              container: PortalShadowRoot,
+                              classes: { paper: classes.menuPaper },
+                          }}
+                          onChange={(e) => setSelectedDeviceId(e.target.value as string)}>
+                          {filteredDevices.map(({ deviceId, label }) => (
+                              <MenuItem key={deviceId} value={deviceId}>
+                                  {label}
+                              </MenuItem>
+                          ))}
+                      </Select>
+                  </FormControl>
+                  <Button
+                      className={classes.button}
+                      variant="outlined"
+                      disabled={!selectedDeviceId}
+                      onClick={() =>
+                          openQRCodeVideoScannerDialog({
+                              deviceId: selectedDeviceId,
+                              onScan,
+                              onError,
+                          })
+                      }>
+                      <CropFreeIcon />
+                  </Button>
+                  {qrCodeVideoScannerDialog}
+              </Box>
+          )
+      }
