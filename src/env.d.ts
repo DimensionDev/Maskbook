dfee2efc1771ecc561f8b8fff66e661f702b301a
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2020-10-23 11:05:39 +0800

    chore: add blockie for token icon (#1758)

--- src/env.d.ts
+++ src/env.d.ts
@@ -87,3 +87,15 @@ declare module 'react-middle-ellipsis' {
     const component: (props: ComponentProps) => JSX.Element
     export default component
 }
+
+declare module 'ethereum-blockies' {
+    export interface BlockieOptions {
+        seed?: string // seed used to generate icon data, default: random
+        color?: string // to manually specify the icon color, default: random
+        bgcolor?: string // choose a different background color, default: white
+        size?: number // width/height of the icon in blocks, default: 10
+        scale?: number // width/height of each block in pixels, default: 5
+    }
+
+    export function create(options?: BlockieOptions): HTMLCanvasElement
+}
