309e35d06459a8c1b8dfb85d47afcdb90784b259
Jack Works <jackworks@protonmail.com>
2020-10-26 15:33:30 +0800

    fix: build fail

--- packages/maskbook/src/setup.ssr.js
+++ packages/maskbook/src/setup.ssr.js
@@ -29,58 +29,17 @@ globalThis.matchMedia = () => {
     return { matches: false, ...EventTarget }
 }
 
-const { join } = require('path')
-const { writeFileSync, readFileSync, unlinkSync } = require('fs')
-
-const restoreLodash = modifyPackage('lodash-es', (x) => (x.main = '../lodash'))
-const restoreKit = modifyPackage('@dimensiondev/holoflows-kit', (x) => {
-    x.exports = {
-        '.': './umd/index.js',
-        './es/util/sleep': './umd/index.js',
-        './es': './umd/index.js',
-        './package.json': './package.json',
-    }
-})
 process.on('uncaughtException', function (err) {
-    cleanup()
     throw err
 })
 process.on('unhandledRejection', (err) => {
-    cleanup()
     throw err
 })
-function cleanup() {
-    restoreLodash()
-    restoreKit()
-}
-try {
-    require('ts-node').register({
-        project: require.resolve(__dirname + '/../../../tsconfig.json'),
-        transpileOnly: true,
-        // ignore: [],
-    })
-    globalThis.window = globalThis
-    require('./polyfill/index')
-    delete globalThis.window
-    module.exports = require(process.argv[process.argv.length - 1])
-} finally {
-    cleanup()
-}
-
-function modifyPackage(packageName, modifier) {
-    const path = join(require.resolve(packageName + '/package.json'))
-    const [x, orig] = [readJSON(path), readJSON(path)]
-    modifier(x)
-    writeJSON(path, x)
-    return () => {
-        writeJSON(path, orig)
-    }
-
-    function readJSON(path) {
-        return JSON.parse(readFileSync(path))
-    }
-
-    function writeJSON(path, object) {
-        return writeFileSync(path, JSON.stringify(object, undefined, 4))
-    }
-}
+require('ts-node').register({
+    project: require.resolve(__dirname + '/../../../tsconfig.json'),
+    transpileOnly: true,
+})
+globalThis.window = globalThis
+require('./polyfill/index')
+delete globalThis.window
+module.exports = require(process.argv[process.argv.length - 1])
