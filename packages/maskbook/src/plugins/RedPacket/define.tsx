e15b51eb6bc466d7a729c37d4f7047d1bc12fc26
Hancheng Zhou <hancheng.zhou@compass.com>
2020-10-27 13:49:14 +0800

    fix: missing variable update (#1793)

--- packages/maskbook/src/plugins/RedPacket/define.tsx
+++ packages/maskbook/src/plugins/RedPacket/define.tsx
@@ -48,6 +48,6 @@ export const RedPacketPluginDefine: PluginConfig = {
             },
         ],
     ]),
-    pageInspector: RedPacketCompositionUI,
+    PageComponent: RedPacketCompositionUI,
     postDialogEntries: [RedPacketCompositionEntry],
 }
