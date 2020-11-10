02c648753e1f86d2652084e230638febdf1edd53
Jack Works <jackworks@protonmail.com>
2020-11-10 14:57:27 +0800

    refactor: cleaner register of services

---
+++ packages/maskbook/src/extension/service-generator.ts
@@ -0,0 +1,3 @@
+export { upload as pluginArweaveUpload } from '../plugins/FileService/arweave/index'
+export { decryptFromText, decryptFromImageUrl } from './background-script/CryptoServices/decryptFrom'
+export { sendTransaction } from './background-script/EthereumServices/transaction'
