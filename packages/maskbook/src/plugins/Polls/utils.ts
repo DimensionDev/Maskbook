a379cadc07941ce726c8bb7fe5bb8008125cdebf
Jack Works <jackworks@protonmail.com>
2020-11-12 13:21:09 +0800

    refactor: migrate poll to new api

--- packages/maskbook/src/plugins/Polls/utils.ts
+++ packages/maskbook/src/plugins/Polls/utils.ts
@@ -1,7 +1,11 @@
 import { createTypedMessageMetadataReader, createRenderWithMetadata } from '../../protocols/typed-message/metadata'
 import type { PollMetaData } from './types'
-import { POLL_META_KEY_1 } from './constants'
+import { identifier, POLL_META_KEY_1 } from './constants'
 import schema from './schema.json'
+import { createPluginMessage } from '../utils/createPluginMessage'
+import { createPluginRPC } from '../utils/createPluginRPC'
 
 export const PollMetadataReader = createTypedMessageMetadataReader<PollMetaData>(POLL_META_KEY_1, schema)
 export const renderWithPollMetadata = createRenderWithMetadata(PollMetadataReader)
+const PollMessage = createPluginMessage<{ _: unknown }>(identifier)
+export const PluginPollRPC = createPluginRPC(() => import('./Services'), PollMessage.events._)
