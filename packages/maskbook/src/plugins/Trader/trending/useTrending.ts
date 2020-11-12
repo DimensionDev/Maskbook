91f8282284df0a87ad959a6a3d985b5f810872eb
Jack Works <jackworks@protonmail.com>
2020-11-12 13:31:25 +0800

    refactor: migrate trader plugin to new api

--- packages/maskbook/src/plugins/Trader/trending/useTrending.ts
+++ packages/maskbook/src/plugins/Trader/trending/useTrending.ts
@@ -1,18 +1,13 @@
 import { useAsync } from 'react-use'
 import Services from '../../../extension/service'
+import { PluginTraderRPC } from '../messages'
 import type { DataProvider, Currency } from '../types'
 
 export function useTrending(keyword: string, platform: DataProvider, currency: Currency | null) {
     const { value: trending, loading: loadingTrending, error: errorTrending } = useAsync(async () => {
         if (!currency) return null
         try {
-            return Services.Plugin.invokePlugin(
-                'maskbook.trader',
-                'getCoinTrendingByKeyword',
-                keyword,
-                platform,
-                currency,
-            )
+            return PluginTraderRPC.getCoinTrendingByKeyword(keyword, platform, currency)
         } catch (e) {
             return null
         }
