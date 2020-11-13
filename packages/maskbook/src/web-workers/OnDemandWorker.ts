a269a6e3b128f5fade9ff2f0187156613b2d5952
Jack Works <jackworks@protonmail.com>
2020-11-13 14:16:14 +0800

    fix: onDemandWorker crashes

--- packages/maskbook/src/web-workers/OnDemandWorker.ts
+++ packages/maskbook/src/web-workers/OnDemandWorker.ts
@@ -1,15 +1,33 @@
 type ctor = [stringUrl: string | URL, options?: WorkerOptions]
+const DelayAfterWorkerCreated = 50
+const WorkerShouldTerminateInterval = 60 * 1000
+const InactiveTimeToTerminateDefault = 10 * 1000
 export class OnDemandWorker extends Worker {
     protected readonly init: ctor
     protected worker: Worker | undefined = this
     protected readonly id = Math.random().toString(16).slice(2)
+    public inactiveTimeToTerminate = InactiveTimeToTerminateDefault
     constructor(...init: ctor) {
         super(...init)
         this.init = init
         this.log('created with', ...init)
+        this.watchUsage()
+    }
+    protected watchUsage() {
+        const i = setInterval(() => {
+            if (!this.worker) {
+                clearInterval(i)
+                return
+            }
+            if (Date.now() - this.lastUsed > this.inactiveTimeToTerminate) {
+                this.log('inactive for ', this.inactiveTimeToTerminate / 1000, 'sec')
+                this.terminate()
+                clearInterval(i)
+            }
+        }, WorkerShouldTerminateInterval)
     }
     protected log(...args: any[]) {
-        console.log(`OnDemandWorker ${this.id}`, ...args)
+        console.log(`OnDemandWorker ${this.id}`, this, ...args)
     }
     protected lastUsed = Date.now()
     protected use(onReady: () => void) {
@@ -18,17 +36,23 @@ export class OnDemandWorker extends Worker {
 
         if (this.worker) return onReady()
         this.worker = new Worker(...this.init)
-        this.worker.addEventListener('error', (e) => this.dispatchEvent(e))
-        this.worker.addEventListener('message', (e) => this.dispatchEvent(e))
-        this.worker.addEventListener('messageerror', (e) => this.dispatchEvent(e))
-        setTimeout(onReady, 50)
+        this.worker.addEventListener('error', (e) => this.dispatchEvent(cloneEvent(e)))
+        this.worker.addEventListener('message', (e) => this.dispatchEvent(cloneEvent(e)))
+        this.worker.addEventListener('messageerror', (e) => this.dispatchEvent(cloneEvent(e)))
+        this.watchUsage()
+        setTimeout(onReady, DelayAfterWorkerCreated)
     }
     terminate() {
-        this.worker?.terminate()
+        this.worker && Worker.prototype.terminate.call(this.worker)
         this.worker = undefined
         this.log('terminated')
     }
 }
 OnDemandWorker.prototype.postMessage = function (this: OnDemandWorker, ...args: [any, any]) {
-    this.use(() => this.worker?.postMessage(...args))
+    this.use(() => this.worker && Worker.prototype.postMessage.call(this.worker, ...args))
+}
+
+function cloneEvent(e: MessageEvent | ErrorEvent) {
+    // @ts-ignore
+    return new e.constructor(e.type, e)
 }
