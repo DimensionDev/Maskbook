50ca2c098faba8d784c75fc392ac26aa54d5a8d3
Jack Works <jackworks@protonmail.com>
2020-11-13 16:51:29 +0800

    feat: migrate poll service to worker

--- packages/maskbook/src/plugins/Polls/utils.ts
+++ packages/maskbook/src/plugins/Polls/utils.ts
@@ -4,8 +4,18 @@ import { identifier, POLL_META_KEY_1 } from './constants'
 import schema from './schema.json'
 import { createPluginMessage } from '../utils/createPluginMessage'
 import { createPluginRPC } from '../utils/createPluginRPC'
+import { OnDemandWorker } from '../../web-workers/OnDemandWorker'
+import { AsyncCall } from 'async-call-rpc'
+import { WorkerChannel } from 'async-call-rpc/utils/web/worker'
 
 export const PollMetadataReader = createTypedMessageMetadataReader<PollMetaData>(POLL_META_KEY_1, schema)
 export const renderWithPollMetadata = createRenderWithMetadata(PollMetadataReader)
 const PollMessage = createPluginMessage<{ _: unknown }>(identifier)
-export const PluginPollRPC = createPluginRPC(identifier, () => import('./Services'), PollMessage.events._)
+export const PluginPollRPC = createPluginRPC(
+    identifier,
+    async () => {
+        const PollWorker = new OnDemandWorker(new URL('./Services.ts', import.meta.url))
+        return AsyncCall<typeof import('./Services')>({}, { channel: new WorkerChannel(PollWorker) })
+    },
+    PollMessage.events._,
+)
