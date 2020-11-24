a8604d475fe2a7d003bc5c3b36edc64c2eff9f30
Septs <github@septs.pw>
2020-11-24 12:49:39 +0800

    chore: redesign firefox logic

--- packages/maskbook/src/plugins/Wallet/UI/WalletConnectQRCodeDialog/FirefoxPlatform.tsx
+++ packages/maskbook/src/plugins/Wallet/UI/WalletConnectQRCodeDialog/FirefoxPlatform.tsx
@@ -21,21 +21,16 @@ const useStyles = makeStyles(() =>
 export const FirefoxPlatform: React.FC<{ uri: string }> = ({ uri }) => {
     const { t } = useI18N()
     const classes = useStyles()
-    const onConnect = () => open(uri)
     const [qrMode, setQRMode] = useState(false)
-    const ViewArea = () => {
-        if (qrMode) {
-            return <QRCodeModel uri={uri} />
-        }
-        return (
-            <div className={classNames(classes.container, classes.content)}>
-                <Button onClick={onConnect}>{t('plugin_wallet_on_connect_in_firefox')}</Button>
-            </div>
-        )
-    }
+    const CallLocalApp = () => (
+        <div className={classNames(classes.container, classes.content)}>
+            <Button onClick={() => open(uri)}>{t('plugin_wallet_on_connect_in_firefox')}</Button>
+        </div>
+    )
     return (
         <Grid className={classes.container}>
-            <ViewArea />
+            <CallLocalApp />
+            {qrMode ? <QRCodeModel uri={uri} /> : <CallLocalApp />}
             <Button onClick={() => setQRMode(!qrMode)}>
                 {t(qrMode ? 'plugin_wallet_return_mobile_wallet_options' : 'plugin_wallet_view_qr_code')}
             </Button>
