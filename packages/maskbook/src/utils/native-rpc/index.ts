3ec51c3dfe6c0c48820755e3848126c5a4e1f696
jk234ert <ugling88@hotmail.com>
2020-11-27 13:58:45 +0800

    chore: remove test postmessage code

--- packages/maskbook/src/utils/native-rpc/index.ts
+++ packages/maskbook/src/utils/native-rpc/index.ts
@@ -3,8 +3,6 @@ import { AndroidGeckoViewChannel } from './Android.channel'
 import { iOSWebkitChannel } from './iOS.channel'
 import { WebviewAPI } from './Web'
 import type { AndroidNativeAPIs, iOSNativeAPIs } from './types'
-import { timeout } from '../utils'
-
 export const hasNativeAPI = process.env.architecture === 'app'
 export let nativeAPI:
     | { type: 'iOS'; api: _AsyncVersionOf<iOSNativeAPIs> }
@@ -26,13 +24,5 @@ if (process.env.architecture === 'app') {
             channel: new AndroidGeckoViewChannel(),
         }))
         nativeAPI = { type: 'Android', api }
-        const data = 'hello! android should echo this message as-is'
-        timeout(api.android_echo(data), 1000).then(
-            (x: string) =>
-                x === data
-                    ? console.log('Android implemented android_echo correctly')
-                    : console.error('Android did not implemented android_echo correctly, received', x),
-            (err) => console.error('api.android_echo reports an error:', err),
-        )
     }
 }
