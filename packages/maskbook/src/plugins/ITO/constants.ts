0528e0dd24319594128b202cba62ae551dfec73c
guanbinrui <guanbinrui@dimension.im>
2020-12-01 14:49:00 +0800

    chore: compile contracts

--- packages/maskbook/src/plugins/ITO/constants.ts
+++ packages/maskbook/src/plugins/ITO/constants.ts
@@ -7,7 +7,7 @@ export const ITO_CONSTANTS = {
     ITO_ADDRESS: {
         [ChainId.Mainnet]: '',
         [ChainId.Ropsten]: '',
-        [ChainId.Rinkeby]: '',
+        [ChainId.Rinkeby]: '0xad329807b1dd9cca742d0a935d04e86d6405f8ae',
         [ChainId.Kovan]: '',
     },
 }
