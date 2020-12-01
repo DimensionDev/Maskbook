746093c9f09f713096afef4ef1256e0d921ac3c3
guanbinrui <guanbinrui@dimension.im>
2020-12-01 15:17:41 +0800

    chore: refine ITO_JSONPayload

---
+++ packages/maskbook/src/plugins/ITO/UI/ITO_CompositionDialog.tsx
@@ -0,0 +1,60 @@
+import { useCallback } from 'react'
+import { v4 as uuid } from 'uuid'
+import { Button, DialogContent, DialogProps } from '@material-ui/core'
+import { InjectedDialog } from '../../../components/shared/InjectedDialog'
+import { getActivatedUI } from '../../../social-network/ui'
+import { useChainId } from '../../../web3/hooks/useChainState'
+import { resolveChainName } from '../../../web3/pipes'
+import { EthereumNetwork, EthereumTokenType } from '../../../web3/types'
+import { ITO_MetaKey } from '../constants'
+import type { ITO_JSONPayload } from '../types'
+import BigNumber from 'bignumber.js'
+
+export interface ITO_CompositionDialogProps {
+    open: boolean
+    onClose: () => void
+    DialogProps?: Partial<DialogProps>
+}
+
+export function ITO_CompositionDialog(props: ITO_CompositionDialogProps) {
+    const chainId = useChainId()
+    const onCreatePayload = useCallback(() => {
+        const payload: ITO_JSONPayload = {
+            pid: uuid(),
+            password: uuid(),
+            total: new BigNumber('1000000000').toFixed(),
+            sender: {
+                address: '0x',
+                name: 'Maskbook',
+                message: 'ITO',
+            },
+            creation_time: new Date().getTime(),
+            duration: 60 /* seconds */ * 60 /* mins */ * 24 /* hours */,
+            network: resolveChainName(chainId) as EthereumNetwork,
+            token_type: EthereumTokenType.ERC20,
+            token: {
+                address: '0x',
+                name: 'MASKBOOK TEST TOKEN',
+                decimals: 18,
+                symbol: 'MAK',
+            },
+        }
+
+        // update the composition dialog
+        const ref = getActivatedUI().typedMessageMetadata
+        const next = new Map(ref.value.entries())
+        payload ? next.set(ITO_MetaKey, payload) : next.delete(ITO_MetaKey)
+        ref.value = next
+
+        // close the dialog
+        props.onClose()
+    }, [chainId, props.onClose])
+    return (
+        <InjectedDialog open={props.open} title="ITO Composition Dialog" onClose={props.onClose}>
+            <DialogContent>
+                <h1>Form</h1>
+                <Button onClick={onCreatePayload}>Create a ITO payload</Button>
+            </DialogContent>
+        </InjectedDialog>
+    )
+}
