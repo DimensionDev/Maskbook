0405b0ccba7f61cf897c5334343dd94002a4e406
Hancheng Zhou <z308114274@gmail.com>
2020-12-03 17:12:22 +0800

    fix: delete contact vague bg

--- packages/maskbook/src/extension/options-page/DashboardDialogs/Contact.tsx
+++ packages/maskbook/src/extension/options-page/DashboardDialogs/Contact.tsx
@@ -35,7 +35,7 @@ export function DashboardContactDeleteConfirmDialog(
         [contact],
         () => {
             props.onClose()
-            onDeleted()
+            setTimeout(onDeleted, 0)
         },
     )
     return (
