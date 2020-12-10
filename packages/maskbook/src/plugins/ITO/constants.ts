7b9713c854085b5447de53b70e4f618ef601c564
guanbinrui <guanbinrui@dimension.im>
2020-12-10 14:14:18 +0800

    chore: blocking ito packet

--- packages/maskbook/src/plugins/ITO/constants.ts
+++ packages/maskbook/src/plugins/ITO/constants.ts
@@ -4,7 +4,7 @@ export const ITO_MetaKey = 'com.maskbook.ito:1'
 export const ITO_PluginID = 'com.maskbook.ito'
 
 export const ITO_CONSTANTS = {
-    ITO_ADDRESS: {
+    ITO_CONTRACT_ADDRESS: {
         [ChainId.Mainnet]: '',
         [ChainId.Ropsten]: '',
         [ChainId.Rinkeby]: '0x643d411da56744950c6f9f47d75d58bc42623c11',
@@ -12,3 +12,5 @@ export const ITO_CONSTANTS = {
         [ChainId.Gorli]: '',
     },
 }
+
+export const ITO_CONTRACT_BASE_DATE = new Date(1606780800000)
