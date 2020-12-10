cadf90123d5512c56cd3629d8d40b9c136c7599b
guanbinrui <guanbinrui@dimension.im>
2020-12-10 17:34:44 +0800

    chore: get reserves from subgraph (close #2048)

--- packages/maskbook/src/plugins/Trader/apis/uniswap/index.ts
+++ packages/maskbook/src/plugins/Trader/apis/uniswap/index.ts
@@ -1,3 +1,5 @@
+import BigNumber from 'bignumber.js'
+import stringify from 'json-stable-stringify'
 import { THEGRAPH_UNISWAP_V2 } from '../../constants'
 
 interface QueryPairsResponse {
@@ -6,26 +8,44 @@ interface QueryPairsResponse {
             id: string
             reserve0: string
             reserve1: string
+            token0: {
+                decimals: number
+            }
+            token1: {
+                decimals: number
+            }
         }[]
     }
 }
 
 export async function queryPairs(ids: string[]) {
+    if (!ids.length) return []
     const response = await fetch(THEGRAPH_UNISWAP_V2, {
         method: 'POST',
         mode: 'cors',
         body: JSON.stringify({
             query: `
             {
-                pairs (where: { id_in: [${ids.join()}] }) {
-                  id
-                  reserve0
-                  reserve1
+                pairs (where: { id_in: ${stringify(ids.map((x) => x.toLowerCase()))} }) {
+                    id
+                    reserve0
+                    reserve1
+                    token0 {
+                        decimals
+                    }
+                    token1 {
+                        decimals
+                    }
                 }
             }
             `,
             variables: null,
         }),
     })
-    return response.json() as Promise<QueryPairsResponse>
+    const { data } = (await response.json()) as QueryPairsResponse
+    return data.pairs.map((x) => ({
+        id: x.id,
+        reserve0: new BigNumber(x.reserve0).multipliedBy(new BigNumber(10).pow(x.token0.decimals)).toFixed(),
+        reserve1: new BigNumber(x.reserve1).multipliedBy(new BigNumber(10).pow(x.token1.decimals)).toFixed(),
+    }))
 }
