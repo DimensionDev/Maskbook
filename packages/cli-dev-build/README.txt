13c01a3b78c944318536347606865553651db14f
Jack Works <jackworks@protonmail.com>
2020-12-10 17:20:29 +0800

    feat: add manifest v3 to the build flags (#2070)
    * feat: add manifest v3 to the build flags
    
    * fix: build error
    
    * fix: build error
    
    * Apply suggestions from code review
    
    Co-authored-by: Septs <github@septs.pw>
    
    * fix: build error
    
    Co-authored-by: Septs <github@septs.pw>

---
+++ packages/cli-dev-build/README.txt
@@ -0,0 +1,8 @@
+# Example:
+> npm run start chromium
+> yarn build firefox
+
+# Available flags
+Presets, one of 'chromium' | 'e2e' | 'firefox' | 'android' | 'iOS'
+Stages, one of 'beta' | 'insider'
+Others, any of 'reproducible' | 'profile' | 'manifest-v3'
---
+++ packages/cli-dev-build/main.js
@@ -0,0 +1,40 @@
+const { spawn } = require('child_process')
+
+const preset = ['chromium', 'E2E', 'firefox', 'android', 'iOS', 'base']
+const otherFlags = ['beta', 'insider', 'reproducible', 'profile', 'manifest-v3']
+const knownFlags = ['-h', '--help', ...preset, ...otherFlags]
+/** @param {'dev' | 'build'} mode */
+async function main(mode) {
+    let args = process.argv.slice(2)
+
+    if (args.includes('-h') || args.includes('--help')) {
+        const inquirer = require('inquirer')
+        const { preset } = await inquirer.prompt({
+            type: 'list',
+            name: 'preset',
+            message: 'Choose preset',
+            choices: preset,
+        })
+        const { flags } = await inquirer.prompt({
+            type: 'checkbox',
+            name: 'flags',
+            choices: otherFlags,
+        })
+        args = [...flags, preset]
+        const { confirm } = await inquirer.prompt({
+            type: 'confirm',
+            name: 'confirm',
+            message: `Command is: "npx ${mode === 'dev' ? 'dev' : 'build'} ${args.join(' ')}". Is that OK?`,
+        })
+        if (!confirm) return
+    }
+
+    const command = ['--mode', mode === 'dev' ? 'development' : 'production']
+    if (mode === 'dev') command.unshift('serve')
+    args.forEach((flag) => {
+        command.push('--env', flag)
+        if (!knownFlags.includes(flag)) throw new TypeError('Unknown flag ' + flag)
+    })
+    spawn('webpack', command, { stdio: 'inherit', shell: true })
+}
+module.exports = main
