4ec3d81bb4782c42a41f1e5da69f2b58737406b9
Septs <github@septs.pw>
2020-12-11 18:09:41 +0800

    chore: update build command

--- packages/cli-dev-build/main.js
+++ packages/cli-dev-build/main.js
@@ -1,14 +1,15 @@
-const { spawn } = require('child_process')
+const { exec } = require('child_process')
+const inquirer = require('inquirer')
 
 const presets = ['chromium', 'E2E', 'firefox', 'android', 'iOS', 'base']
 const otherFlags = ['beta', 'insider', 'reproducible', 'profile', 'manifest-v3']
 const knownFlags = ['-h', '--help', ...presets, ...otherFlags]
+
 /** @param {'dev' | 'build'} mode */
 async function main(mode) {
     let args = process.argv.slice(2)
 
     if (args.includes('-h') || args.includes('--help')) {
-        const inquirer = require('inquirer')
         const { preset } = await inquirer.prompt({
             type: 'list',
             name: 'preset',
@@ -35,6 +36,7 @@ async function main(mode) {
         command.push('--env', flag)
         if (!knownFlags.includes(flag)) throw new TypeError('Unknown flag ' + flag)
     })
-    spawn('webpack', command, { stdio: 'inherit', shell: true })
+    return exec('webpack', command, { stdio: 'inherit', shell: true })
 }
+
 module.exports = main
