b6d11ab73ba57d7bfceb78bb51295ed238fc22ba
guanbinrui <guanbinrui@dimension.im>
2020-12-14 16:58:50 +0800

    chore: add remaining fields

--- packages/maskbook/src/plugins/ITO/hooks/usePoolPayload.ts
+++ packages/maskbook/src/plugins/ITO/hooks/usePoolPayload.ts
@@ -5,8 +5,9 @@ export function usePoolPayload(id: string) {
     return useMemo(() => {
         return {
             contract_address: '0x52ceb31d6c197b5c039786fbefd6a82df70fdfd6',
-            creation_time: 1607669042000,
             total: '100',
+            total_remaining: '100',
+            claim_remaining: '10',
             pid: '0x91abb4660d1925c3c0a5bfb9d0481e80a558d86b3fe446764cadc2cd9505f1b4',
             sender: {
                 address: '0x66b57885E8E9D84742faBda0cE6E3496055b012d',
@@ -24,6 +25,7 @@ export function usePoolPayload(id: string) {
             },
             limit: '10',
             password: 'd928b4b2-0a7e-4c08-aaa9-6d02d7a40391',
+            creation_time: 1607669042000,
             start_time: 1607668871197,
             end_time: 1607755271197,
             exchange_tokens: [
