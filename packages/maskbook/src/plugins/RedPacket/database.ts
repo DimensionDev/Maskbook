6cbce8d71f490c1c0630ef432a4ef1bb86876709
Septs <github@septs.pw>
2020-12-14 13:26:03 +0800

    fix: duplicate RedPacketRecord into db

--- packages/maskbook/src/plugins/RedPacket/database.ts
+++ packages/maskbook/src/plugins/RedPacket/database.ts
@@ -2,7 +2,7 @@ import type { RedPacketRecord, RedPacketRecordInDatabase } from './types'
 import { RedPacketPluginID } from './constants'
 import { createPluginDatabase } from '../../database/Plugin/wrap-plugin-database'
 import { asyncIteratorToArray } from '../../utils/type-transform/asyncIteratorHelpers'
-import { omit } from 'lodash-es'
+import { isNil, omit } from 'lodash-es'
 
 export const RedPacketDatabase = createPluginDatabase<RedPacketRecordInDatabase>(RedPacketPluginID)
 
@@ -15,7 +15,10 @@ export async function getRedPacket(rpid: string) {
     return record ? RedPacketRecordOutDB(record) : undefined
 }
 
-export function addRedPacket(record: RedPacketRecord) {
+export async function addRedPacket(record: RedPacketRecord) {
+    if (!isNil(await RedPacketDatabase.get('red-packet', record.id))) {
+        return
+    }
     return RedPacketDatabase.add(RedPacketRecordIntoDB(record))
 }
 
