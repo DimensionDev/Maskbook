58d026bd5595f82864e383d730bfda5f9c6ec429
lelenei <zwpsky@gmail.com>
2021-01-04 23:00:18 +0800

    feat: create getcoin ui

--- packages/maskbook/src/plugins/Gitcoin/UI/PreviewCard.tsx
+++ packages/maskbook/src/plugins/Gitcoin/UI/PreviewCard.tsx
@@ -1,8 +1,75 @@
-import { makeStyles, createStyles } from '@material-ui/core'
+import { makeStyles, createStyles, Card, Typography, Button, ButtonGroup, Grid, Avatar, Icon } from '@material-ui/core'
+import React from 'react'
 import { useI18N } from '../../../utils/i18n-next-ui'
 import { useGrant } from '../hooks/useGrant'
+import QueryBuilderIcon from '@material-ui/icons/QueryBuilder'
+import CheckIcon from '@material-ui/icons/Check'
+const useStyles = makeStyles((theme) => createStyles({
+    root: {
+        padding: theme.spacing(1),
+    },
+    logo: {
+        padding: theme.spacing(4),
+        textAlign: 'center',
+        "& > *": {
+            width: 100,
+            height: 100,
+        },
+    },
+    title: {
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    description: {
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+    },
+    update: {
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    avatar: {
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
+        display: 'flex',
+        flexDirection: 'row',
+        "& > * ": {
+            marginLeft: theme.spacing(1),
+        },
+    },
+    small: {
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+      },
+    buttons: {
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
+        "& > *": {
+            justifyContent: 'center',
+            textAlign: 'center',
+            "& > *": {
+                width: 150,
+            },
+        },
 
-const useStyles = makeStyles((theme) => createStyles({}))
+    },
+    verified: {
+        borderRadius: 50,
+    },
+    text: {
+        overflow : 'hidden',
+        textOverflow: 'ellipsis',
+        display: '-webkit-box',
+        '-webkit-line-clamp': '4',
+        '-webkit-box-orient': 'vertical',
+    },
+}))
 
 interface PreviewCardProps {
     id: string
@@ -18,5 +85,53 @@ export function PreviewCard(props: PreviewCardProps) {
     if (error) return <h1>ERROR</h1>
     if (!grant) return null
 
-    return <h1>Grant</h1>
+    return (
+        <Card variant="outlined" className={classes.root} elevation={0}>
+            <div className={classes.logo}>
+                <img src={grant.logo_url} />
+            </div>
+
+            <div className={classes.title}>
+                <Typography variant='h6' color='textPrimary'>
+                    {grant.title}
+                </Typography>
+                {/*
+                <Icon color="primary" fontSize="small">check_circle</Icon>
+                */}
+                {grant.verified ? (<CheckIcon fontSize="small" color='secondary' />): null}
+            </div>
+            <div className={classes.description}>
+                <Typography variant='body1' color='textSecondary' className={classes.text}>
+                    {grant.description}
+                </Typography>
+            </div>
+            <div className={classes.update}>
+                <QueryBuilderIcon fontSize='small' color='disabled' />
+                <Typography variant='body1' color='textSecondary'>
+                   Last update: {grant.last_update_natural}
+                </Typography>
+            </div>
+
+            <div className={classes.avatar}>
+                <Typography variant='body2'>
+                    By
+                </Typography>
+                <Avatar alt={grant.admin_profile.handle} src={grant.admin_profile.avatar_url} className={classes.small}/>
+                <Typography variant='body2'>
+                    {grant.admin_profile.handle}
+                </Typography>
+            </div>
+
+
+            <Grid container className={classes.buttons} spacing={2}>
+                <Grid item xs={6} >
+                    <Button variant="outlined" color="primary">View on Gitcoin</Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <Button variant="contained" color="primary">Donate</Button>
+                </Grid>
+            </Grid>
+
+        </Card>
+    )
 }
