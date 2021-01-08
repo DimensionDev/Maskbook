2996f8f90a27d78410edb210b48e2a1090c7e5bf
guanbinrui <guanbinrui@dimension.im>
2021-01-08 16:38:33 +0800

    fix: subgraph url

--- packages/maskbook/src/plugins/ITO/constants.ts
+++ packages/maskbook/src/plugins/ITO/constants.ts
@@ -99,8 +99,8 @@ export const ITO_CONSTANTS = {
         [ChainId.Gorli]: '',
     },
     SUBGRAPH_URL: {
-        [ChainId.Mainnet]: 'https://api.thegraph.com/subgraphs/dimensiondev/mask-ito-mainnet',
-        [ChainId.Ropsten]: 'https://api.thegraph.com/subgraphs/dimensiondev/mask-ito',
+        [ChainId.Mainnet]: 'https://api.thegraph.com/subgraphs/name/dimensiondev/mask-ito-mainnet',
+        [ChainId.Ropsten]: 'https://api.thegraph.com/subgraphs/name/dimensiondev/mask-ito',
         [ChainId.Rinkeby]: '',
         [ChainId.Kovan]: '',
         [ChainId.Gorli]: '',
