10efb12de3401a5e0bbe12f5f6ba4da659e8cc92
Septs <github@septs.pw>
2021-01-08 18:54:16 +0800

    chore: compress svg

--- packages/maskbook/src/extension/options-page/DashboardDialogs/About.tsx
+++ packages/maskbook/src/extension/options-page/DashboardDialogs/About.tsx
@@ -86,7 +86,6 @@ const useStyles = makeStyles((theme) =>
 const DiscordIcon: React.FC<SvgIconProps> = (props) => (
     <SvgIcon {...props}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 512 512">
-            <defs />
             <path
                 fill="#000"
                 stroke="null"
