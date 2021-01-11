fa11fb57faae3833f828bbf818df3f5b0d5bf94d
lelenei <zwpsky@gmail.com>
2021-01-11 14:14:42 +0800

    fix: transak should only support Ethereum cryptocurrencies #2189      * recover: 64115b218d7bdaf25cf294999cd98a179764eec6

--- packages/maskbook/src/plugins/Transak/hooks/useTransakAllowanceCoin.ts
+++ packages/maskbook/src/plugins/Transak/hooks/useTransakAllowanceCoin.ts
@@ -11,9 +11,12 @@ const URL = ENV[process.env.NODE_ENV]
 
 export function useTransakAllowanceCoin(coin: Coin): boolean {
     return useAsync(async () => {
-        const allowanceList = await fetch(URL, { method: 'GET' })
-            .then((res) => res.json())
-            .then((res) => res.response)
-        return allowanceList.map((val: Coin) => val.symbol).includes(coin.symbol)
+        if (coin.eth_address || coin.symbol.toLowerCase() === 'eth') {
+            const allowanceList = await fetch(URL, { method: 'GET' })
+                .then((res) => res.json())
+                .then((res) => res.response)
+            return allowanceList.map((val: Coin) => val.symbol).includes(coin.symbol)
+        }
+        return false
     }).value
 }
