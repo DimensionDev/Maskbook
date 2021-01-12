feded7f66825ab26b6dd7965cd3a7fd6b05fad5f
Hancheng Zhou <z308114274@gmail.com>
2021-01-12 10:31:39 +0800

    fix: show actual swap amount (#2223)

--- packages/maskbook/src/plugins/ITO/UI/ShareDialog.tsx
+++ packages/maskbook/src/plugins/ITO/UI/ShareDialog.tsx
@@ -58,7 +58,7 @@ const useStyles = makeStyles((theme) =>
 
 export interface ShareDialogProps extends withClasses<'root'> {
     token: EtherTokenDetailed | ERC20TokenDetailed
-    tokenAmount: BigNumber
+    actualSwapAmount: BigNumber
     poolName: string
     onClose: () => void
 }
@@ -67,9 +67,9 @@ export function ShareDialog(props: ShareDialogProps) {
     const ShareBackground = getAssetAsBlobURL(new URL('../assets/share-background.jpg', import.meta.url))
     const { t } = useI18N()
     const classes = useStylesExtends(useStyles(), {})
-    const { token, tokenAmount } = props
+    const { token, actualSwapAmount } = props
     const postLink = usePostLink()
-    const amount = formatBalance(tokenAmount, token.decimals ?? 0)
+    const amount = formatBalance(actualSwapAmount, token.decimals ?? 0)
     const shareLink = useShareLink(
         t('plugin_ito_claim_success_share', {
             link: postLink,
