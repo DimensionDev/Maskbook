eab366eb1cd8e32fedb00802380bcb4b9fc8412a
Sherlock <shh_name@hotmail.com>
2021-01-14 17:44:30 +0800

    Add box

--- packages/maskbook/src/plugins/FileService/constants.ts
+++ packages/maskbook/src/plugins/FileService/constants.ts
@@ -7,6 +7,7 @@ export const MAX_FILE_SIZE = 0xa00000 // = 10 MiB
 
 export const landing = 'https://files.maskbook.com/partner/arweave/landing-page.html'
 export const signing = 'https://service.maskbook.com/arweave-remote-signing'
+export const mesonPrefix = 'https://coldcdn.com/api/cdn/9m5pde'
 
 export const enum FileRouter {
     upload = '/upload',
