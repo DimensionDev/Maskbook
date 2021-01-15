caf9a6293d96f7ad4f3e10ceb931b233f73bc053
Hancheng Zhou <z308114274@gmail.com>
2021-01-15 17:35:43 +0800

    feat: token icon source failover (#2241)
    Co-authored-by: Septs <github@septs.pw>

--- packages/maskbook/src/extension/options-page/DashboardComponents/TokenIcon.tsx
+++ packages/maskbook/src/extension/options-page/DashboardComponents/TokenIcon.tsx
@@ -4,23 +4,29 @@ import { isSameAddress, getConstant } from '../../../web3/helpers'
 import { CONSTANTS } from '../../../web3/constants'
 import { useBlockie } from '../../../web3/hooks/useBlockie'
 import { formatChecksumAddress } from '../../../plugins/Wallet/formatter'
+import { useImageFailover } from '../../../utils/hooks/useImageFailover'
 
 //#region fix icon image
-const TRUST_WALLET_ASSETS = 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum'
 const ETH_ADDRESS = getConstant(CONSTANTS, 'ETH_ADDRESS')
-const ICON_MAP = {
-    [ETH_ADDRESS]: `${TRUST_WALLET_ASSETS}/info/logo.png`,
-    '0x32a7C02e79c4ea1008dD6564b35F131428673c41': 'https://s2.coinmarketcap.com/static/img/coins/64x64/6747.png', // CRUST
-    '0x04abEdA201850aC0124161F037Efd70c74ddC74C': 'https://s2.coinmarketcap.com/static/img/coins/64x64/5841.png', // NEST
-    '0x14de81C71B3F73874659082b971433514E201B27': 'https://etherscan.io/token/images/ykyctoken_32.png', // Yes KYC
-}
 
-function resolveTokenIconURL(address: string) {
+const iconSourceList = [
+    'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum',
+    'https://rawcdn.githack.com/trustwallet/assets/master/blockchains/ethereum',
+]
+const IMG_SUFFIX = '/info/logo.png'
+
+function resolveTokenIconURL(address: string, trustWalletAssets: string) {
+    const iconMap = {
+        [ETH_ADDRESS]: `${trustWalletAssets}/info/logo.png`,
+        '0x32a7C02e79c4ea1008dD6564b35F131428673c41': 'https://s2.coinmarketcap.com/static/img/coins/64x64/6747.png', // CRUST
+        '0x04abEdA201850aC0124161F037Efd70c74ddC74C': 'https://s2.coinmarketcap.com/static/img/coins/64x64/5841.png', // NEST
+        '0x14de81C71B3F73874659082b971433514E201B27': 'https://etherscan.io/token/images/ykyctoken_32.png', // Yes KYC
+    }
     const checksummedAddress = formatChecksumAddress(address)
     if (isSameAddress(checksummedAddress, getConstant(CONSTANTS, 'ETH_ADDRESS')))
         return 'https://rawcdn.githack.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png'
-    if (ICON_MAP[checksummedAddress]) return ICON_MAP[checksummedAddress]
-    return `${TRUST_WALLET_ASSETS}/assets/${checksummedAddress}/logo.png`
+    if (iconMap[checksummedAddress]) return iconMap[checksummedAddress]
+    return `${trustWalletAssets}/assets/${checksummedAddress}/logo.png`
 }
 //#endregion
 
@@ -46,8 +52,12 @@ export function TokenIcon(props: TokenIconProps) {
     const classes = useStylesExtends(useStyles(), props)
     const tokenBlockie = useBlockie(props.address)
 
+    const { value: trustWalletAssets, loading } = useImageFailover(iconSourceList, IMG_SUFFIX)
     return (
-        <Avatar className={classes.icon} src={resolveTokenIconURL(address)} {...props.AvatarProps}>
+        <Avatar
+            className={classes.icon}
+            src={loading ? '' : resolveTokenIconURL(address, trustWalletAssets as string)}
+            {...props.AvatarProps}>
             <Avatar className={classes.icon} src={tokenBlockie}>
                 {name?.substr(0, 1).toLocaleUpperCase()}
             </Avatar>
