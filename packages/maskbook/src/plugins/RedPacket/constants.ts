9d6630c21cd81825c07364edd0f8073b7a27a4ff
Septs <github@septs.pw>
2021-01-21 11:27:06 +0800

    feat: update api expose domain (#2260)

--- packages/maskbook/src/plugins/RedPacket/constants.ts
+++ packages/maskbook/src/plugins/RedPacket/constants.ts
@@ -20,7 +20,7 @@ export const RED_PACKET_DEFAULT_SHARES = 5
 export const RED_PACKET_MIN_SHARES = 1
 export const RED_PACKET_MAX_SHARES = 999
 
-export const RED_PACKET_HISTORY_URL = 'https://service.maskbook.com/red-packet-history'
+export const RED_PACKET_HISTORY_URL = 'https://service.r2d2.to/red-packet-history'
 export const RED_PACKET_HISTROY_MAX_BLOCK_SIZE = Math.floor(
     (30 /* days */ * 24 /* hours */ * 60 /* minutes */ * 60) /* seconds */ / 15 /* each block in seconds */,
 )
