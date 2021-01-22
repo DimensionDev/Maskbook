2685701335b67d884f2bcbf5aee83d6bf557b2a0
Jack Works <jackworks@protonmail.com>
2021-01-22 13:14:31 +0800

    feat: introduce i18n codegen

--- packages/cli-dev-build/ts.js
+++ packages/cli-dev-build/ts.js
@@ -1,18 +1,22 @@
 // start a one-time "tsc -b" or parallel "tsc -b -w"
 
 const { spawn } = require('child_process')
-const { join } = require('path')
+const { join, resolve } = require('path')
 const Lock = require('./process-lock')
+const { runCli } = require('@magic-works/i18n-codegen')
 
+const configFile = resolve(__dirname, '../../.i18n-codegen.json')
 const args = { stdio: 'inherit', shell: true, cwd: join(__dirname, '../..') }
 module.exports.dev = async function () {
     lock: for await (const lock of Lock()) {
         if (await lock()) break lock
     }
+    runCli({ config: configFile, watch: true })
     spawn('tsc', ['-b', '-w'], args)
 }
 module.exports.build = function () {
     return new Promise((resolve, reject) => {
+        runCli({ config: configFile })
         const p = spawn('tsc', ['-b'], args)
         p.on('close', resolve)
         p.on('error', reject)
