41e05cd6dae9b06de52e9d04d244d52981498457
lelenei <zwpsky@gmail.com>
2021-01-29 16:54:00 +0800

    feat: add unsubscribe

--- packages/maskbook/src/plugins/Trader/apis/coinmarketcap/getPriceStat.ts
+++ packages/maskbook/src/plugins/Trader/apis/coinmarketcap/getPriceStat.ts
@@ -16,16 +16,13 @@ export interface PriceState {
     v: number
 }
 
-
 const WATCHED_COINS: number[] = []
-const WATCHED_PRICES = new Map<string,PriceState>()
+const WATCHED_PRICES = new Map<string, PriceState>()
 
 enum ERROR_TYPE {
     WS_CONNECT_ERROR,
     NOT_SUBSCRIBE,
 }
-
-
 
 function connectWebSocket() {
     return new Promise<WebSocket>((resolve, reject) => {
@@ -46,11 +43,22 @@ const msgData = {
     },
 }
 
-
 function syncPricesFromWS(coinIds: number[]) {
+    Event.addListener('unsubscribe', (msg) => {
+        WATCHED_COINS.splice(WATCHED_COINS.indexOf(msg), 1)
+        if (WATCHED_COINS.length > 0) {
+            webSocket?.send(JSON.stringify(msgData))
+            return
+        }
+
+        if (webSocket) {
+            webSocket.close()
+            webSocket = null
+        }
+    })
     Event.addListener('subscribe', (msg) => {
         if (webSocket && webSocket.readyState === WebSocket.OPEN) {
-            if (WATCHED_COINS.some(x => x === msg)) {
+            if (WATCHED_COINS.some((x) => x === msg)) {
                 return
             }
 
@@ -58,51 +66,51 @@ function syncPricesFromWS(coinIds: number[]) {
             webSocket.send(JSON.stringify(msgData))
             return
         }
-        connectWebSocket().then((client) => {
-            client.onopen = () => {
-                webSocket = client
-                coinIds.map((x) => {
-                    if (WATCHED_COINS.indexOf(x) === -1) {
-                        WATCHED_COINS.push(x)
+        connectWebSocket()
+            .then((client) => {
+                client.onopen = () => {
+                    webSocket = client
+                    coinIds.map((x) => {
+                        if (WATCHED_COINS.indexOf(x) === -1) {
+                            WATCHED_COINS.push(x)
+                        }
+                    })
+
+                    if (WATCHED_COINS.length > 0) {
+                        client.send(JSON.stringify(msgData))
                     }
-                })
+                }
+                client.onerror = (error) => {
+                    client.close()
+                    webSocket = null
+                    console.log(error)
+                }
+                client.onclose = () => {
+                    console.log('close')
+                    webSocket = null
+                }
 
-                if (WATCHED_COINS.length > 0) {
-                    client.send(JSON.stringify(msgData))
+                client.onmessage = (ev) => {
+                    if (typeof ev.data !== 'string') {
+                        return
+                    }
+                    try {
+                        const jsondata = JSON.parse(ev.data)
+                        Event.emit(jsondata.id, jsondata)
+                    } catch (e) {
+                        console.error(e)
+                    }
                 }
-            }
-            client.onerror = (error) => {
-                client.close()
-                webSocket = null
-                console.log(error)
-            }
-            client.onclose = () => {
-                console.log('close')
-                webSocket = null
-            }
-
-            client.onmessage = (ev) => {
-                if (typeof ev.data!== 'string') {
-                    return
-                }
-                try {
-                    const jsondata = JSON.parse(ev.data)
-                    Event.emit(jsondata.id, jsondata)
-                } catch (e) {
-                    console.error(e)
-                }
-            }
-        })
-        .catch(error => console.log(error))
+            })
+            .catch((error) => console.log(error))
     })
-
 }
 
 export function getCoinPrice(coinId: number) {
-    if (!WATCHED_COINS.some(x => x === coinId)) {
+    if (!WATCHED_COINS.some((x) => x === coinId)) {
         throw new Error('coin not subscribe!')
     }
-    if (!webSocket)  {
+    if (!webSocket) {
         throw new Error('websocket connect error')
     }
     if (webSocket && webSocket.readyState === WebSocket.CLOSED) {
@@ -112,7 +120,6 @@ export function getCoinPrice(coinId: number) {
 }
 
 export function subscribeCoinPrce(coinId: number) {
-
     syncPricesFromWS([coinId])
     Event.addListener(coinId.toString(), (data) => {
         WATCHED_PRICES.set(coinId.toString(), data)
@@ -120,7 +127,6 @@ export function subscribeCoinPrce(coinId: number) {
 
     Event.emit('subscribe', coinId)
 }
-
 
 export function unsubscribeCoinPrce(coinId: number) {
     Event.emit('unsubscribe', coinId)
