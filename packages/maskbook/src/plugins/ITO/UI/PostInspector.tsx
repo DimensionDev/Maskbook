3451e8e7e470bc1e37a930051653059cc0cd4376
Hancheng Zhou <z308114274@gmail.com>
2021-01-29 17:10:30 +0800

    chore: more general way for suspense error (#2302)
    * chore: more general way for suspense error
    
    Co-authored-by: Jack Works <jackworks@protonmail.com>

--- packages/maskbook/src/plugins/ITO/UI/PostInspector.tsx
+++ packages/maskbook/src/plugins/ITO/UI/PostInspector.tsx
@@ -1,15 +1,14 @@
+import { poolPayloadErrorRetry } from '../hooks/usePoolPayload'
 import { ITO, ITO_LoadingFail } from './ITO'
-import { usePoolPayload } from '../hooks/usePoolPayload'
 
 export interface PostInspectorProps {
     pid: string
 }
 
 export function PostInspector(props: PostInspectorProps) {
-    const { payload, retry: retryPoolPayload } = usePoolPayload(props.pid)
     return (
-        <ITO_LoadingFail retryPoolPayload={retryPoolPayload}>
-            <ITO pid={props.pid} payload={payload} retryPoolPayload={retryPoolPayload} />
+        <ITO_LoadingFail retryPoolPayload={poolPayloadErrorRetry}>
+            <ITO pid={props.pid} />
         </ITO_LoadingFail>
     )
 }
