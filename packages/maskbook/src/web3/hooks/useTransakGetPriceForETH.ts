71576268a61d6dcbdf640c943822b02fd74cb656
lelenei <zwpsky@gmail.com>
2021-02-01 14:50:34 +0800

    fix: remove debug log

--- packages/maskbook/src/web3/hooks/useTransakGetPriceForETH.ts
+++ packages/maskbook/src/web3/hooks/useTransakGetPriceForETH.ts
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useAsync } from 'react-use'
 import { currentGasPriceEthToUSD } from '../../settings/settings'
 import { getTransakGetPriceForETH } from '../apis/getTransakGetPriceForETH'
@@ -7,17 +6,13 @@ const timeout = 300
 let getTransakGetPriceForETH_Time = 0
 
 export function useTransakGetPriceForETH() {
-    console.log('time: ', getTransakGetPriceForETH_Time)
     return useAsync(async () => {
-        console.log('time: ', getTransakGetPriceForETH_Time)
         const now = Date.now()
         if (getTransakGetPriceForETH_Time === 0 || now - getTransakGetPriceForETH_Time > timeout * 1000) {
             const usd = await getTransakGetPriceForETH()
             getTransakGetPriceForETH_Time = now
             currentGasPriceEthToUSD.value = usd.toFixed(2)
-            console.log('usd:', usd)
         }
-        console.log('use1:', currentGasPriceEthToUSD.value)
         return currentGasPriceEthToUSD.value
     }, [])
 }
