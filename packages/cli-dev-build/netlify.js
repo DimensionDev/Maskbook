85d5b0183ff118ffe55dd69e9b7dd98f0fc6256b
Jack Works <jackworks@protonmail.com>
2021-02-02 20:18:25 +0800

    chore: try netlify

--- packages/cli-dev-build/netlify.js
+++ packages/cli-dev-build/netlify.js
@@ -1,17 +1,17 @@
 const { series, parallel } = require('gulp')
 const { exec } = require('child_process')
 
-const { join } = require('path')
+const { join, relative } = require('path')
 const root = join(__dirname, '../../')
 const dashboard = join(__dirname, '../dashboard')
 const theme = join(__dirname, '../theme')
-const output = join(__dirname, '../netlify')
+const netlify = join(__dirname, '../netlify')
 
 const createBuildStorybook6 = (basePath, output, name) => {
     const f = () =>
         // Storybook breaks if we use absolute path, maybe see
         // https://github.com/storybookjs/storybook-deployer/issues/56
-        exec(`npx build-storybook --output-dir ../netlify/${output}`, {
+        exec(`npx build-storybook --output-dir ${relative(basePath, output)}`, {
             cwd: basePath,
             shell: true,
         })
@@ -21,7 +21,7 @@ const createBuildStorybook6 = (basePath, output, name) => {
 }
 
 const { build } = require('./ts')
-const a = createBuildStorybook6(dashboard, join(output, 'dashboard-storybook'), 'dashboard')
-const b = createBuildStorybook6(theme, join(output, 'theme-storybook'), 'theme')
+const a = createBuildStorybook6(dashboard, join(netlify, 'dashboard-storybook'), 'dashboard')
+const b = createBuildStorybook6(theme, join(netlify, 'theme-storybook'), 'theme')
 
 exports.buildNetlify = series(build, parallel(a, b))
