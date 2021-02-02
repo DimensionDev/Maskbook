5794d0fb1f8c0aeabd61fbede80d6181be45c717
Jack Works <jackworks@protonmail.com>
2021-02-02 19:46:53 +0800

    chore: try

--- packages/cli-dev-build/netlify.js
+++ packages/cli-dev-build/netlify.js
@@ -1,5 +1,5 @@
 const { series, parallel } = require('gulp')
-const { exec } = require('child_process')
+const { spawn } = require('child_process')
 
 const { join } = require('path')
 const root = join(__dirname, '../../')
@@ -8,7 +8,11 @@ const theme = join(__dirname, '../theme')
 const output = join(__dirname, '../netlify/')
 
 const createBuildStorybook6 = (basePath, output, name) => {
-    const f = () => exec('npx build-storybook --output-dir ' + output, { cwd: basePath })
+    const f = () =>
+        spawn(`npx build-storybook --output-dir ${output}`, {
+            cwd: basePath,
+            shell: true,
+        })
     f.displayName = name + '-storybook'
     f.description = `Build storybook of ${name} to ${output}`
     return f
