45b466dd185e2cf62abc6d343e8951ec018338dc
Jack Works <jackworks@protonmail.com>
2021-02-02 22:22:11 +0800

    fix: final

--- packages/cli-dev-build/netlify.js
+++ packages/cli-dev-build/netlify.js
@@ -2,7 +2,6 @@ const { series, parallel } = require('gulp')
 const { spawn } = require('child_process')
 
 const { join, relative } = require('path')
-const { existsSync } = require('fs')
 const root = join(__dirname, '../../')
 const dashboard = join(__dirname, '../dashboard')
 const theme = join(__dirname, '../theme')
@@ -28,15 +27,4 @@ const addrB = join(netlify, 'storybook-static/theme')
 const taskA = createBuildStorybook6(dashboard, addrA, 'dashboard')
 const taskB = createBuildStorybook6(theme, addrB, 'theme')
 
-const sleep = () => new Promise((resolve) => setTimeout(resolve, 1000))
-const check = () => {
-    const l = join(addrA, './iframe.html')
-    const j = join(addrB, './iframe.html')
-    // npx build-storybook will quite before the build process is done
-    const a = existsSync(l)
-    const b = existsSync(j)
-    if (a && b) return Promise.resolve()
-    console.log(l, a, j, b)
-    return sleep().then(check)
-}
-exports.buildNetlify = series(build, parallel(taskA, taskB), check)
+exports.buildNetlify = series(build, parallel(taskA, taskB))
