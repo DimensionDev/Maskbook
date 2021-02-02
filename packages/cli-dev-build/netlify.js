d73d60a47df79cf4f56847ec58c90c2c29c0a94e
Jack Works <jackworks@protonmail.com>
2021-02-02 20:47:28 +0800

    chore: try netlify

--- packages/cli-dev-build/netlify.js
+++ packages/cli-dev-build/netlify.js
@@ -1,27 +1,32 @@
-const { series, parallel } = require('gulp')
+const { series, parallel, src, dest } = require('gulp')
 const { exec } = require('child_process')
 
-const { join, relative } = require('path')
+const { join } = require('path')
 const root = join(__dirname, '../../')
 const dashboard = join(__dirname, '../dashboard')
 const theme = join(__dirname, '../theme')
 const netlify = join(__dirname, '../netlify')
 
-const createBuildStorybook6 = (basePath, output, name) => {
-    const f = () =>
-        // Storybook breaks if we use absolute path, maybe see
-        // https://github.com/storybookjs/storybook-deployer/issues/56
-        exec(`npx build-storybook --output-dir ${relative(basePath, output)}`, {
-            cwd: basePath,
-            shell: true,
-        })
+const createBuildStorybook6 = (basePath, name) => {
+    const f = () => exec(`npx build-storybook`, { cwd: basePath, shell: true })
     f.displayName = name + '-storybook'
-    f.description = `Build storybook of ${name} to ${output}`
+    f.description = `Build storybook of ${name}`
+    return f
+}
+
+function copy(path, out) {
+    const f = () => src(join(path, './storybook-static/*')).pipe(dest(join(netlify, out)))
+    f.displayName = 'Copy assets to ' + out
     return f
 }
 
 const { build } = require('./ts')
-const a = createBuildStorybook6(dashboard, join(netlify, 'dashboard-storybook'), 'dashboard')
-const b = createBuildStorybook6(theme, join(netlify, 'theme-storybook'), 'theme')
+const a = createBuildStorybook6(dashboard, 'dashboard')
+const b = createBuildStorybook6(theme, 'theme')
 
-exports.buildNetlify = series(build, parallel(a, b))
+exports.buildNetlify = series(
+    build,
+    parallel(a, b),
+    copy(dashboard, './dashboard-storybook'),
+    copy(theme, './theme-storybook'),
+)
