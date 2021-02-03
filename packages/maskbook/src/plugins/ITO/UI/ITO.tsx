98d74e21f30164cbbad71ee3ef555feaa1d93881
Hancheng Zhou <z308114274@gmail.com>
2021-02-03 14:52:39 +0800

    fix: bug only buyer can withdraw

--- packages/maskbook/src/plugins/ITO/UI/ITO.tsx
+++ packages/maskbook/src/plugins/ITO/UI/ITO.tsx
@@ -260,6 +260,7 @@ export function ITO(props: ITO_Props) {
     )
     const canWithdraw =
         isAccountSeller && !tradeInfo?.destructInfo && (listOfStatus.includes(ITO_Status.expired) || noRemain)
+
     const refundAmount = tradeInfo?.buyInfo
         ? new BigNumber(tradeInfo?.buyInfo.amount).minus(new BigNumber(tradeInfo?.buyInfo.amount_sold))
         : new BigNumber(0)
@@ -491,7 +492,7 @@ export function ITO(props: ITO_Props) {
             </Card>
 
             <Box className={classes.actionFooter}>
-                {(total_remaining.isZero() && !isBuyer) || loadingTradeInfo || loadingAvailability ? null : !account ||
+                {(total_remaining.isZero() && !isBuyer && !canWithdraw) || loadingTradeInfo || loadingAvailability ? null : !account ||
                   !chainIdValid ? (
                     <ActionButton onClick={onConnect} variant="contained" size="large" className={classes.actionButton}>
                         {t('plugin_wallet_connect_a_wallet')}
