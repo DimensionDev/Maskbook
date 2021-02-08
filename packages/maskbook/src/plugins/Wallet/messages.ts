2199ed38af3ef1efaecbb6b9b6e0d8db01470328
guanbinrui <guanbinrui@dimension.im>
2021-02-08 16:50:28 +0800

    chore: define SelectERC20TOkenDialogEvent

--- packages/maskbook/src/plugins/Wallet/messages.ts
+++ packages/maskbook/src/plugins/Wallet/messages.ts
@@ -1,3 +1,4 @@
+import type { ERC20TokenDetailed, EtherTokenDetailed } from '../../web3/types'
 import { createPluginMessage } from '../utils/createPluginMessage'
 import { createPluginRPC } from '../utils/createPluginRPC'
 import { PLUGIN_IDENTIFIER } from './constants'
@@ -32,6 +33,41 @@ type WalletConnectQRCodeDialogEvent =
           open: false
       }
 
+type SelectERC20TOkenDialogEvent =
+    | {
+          open: true
+
+          /**
+           * Specific an array of detailed tokens that must be included in the final token list.
+           */
+          tokens?: (EtherTokenDetailed | ERC20TokenDetailed)[]
+
+          /**
+           *  Specific an array of token that only be included in the final token list.
+           */
+          whitelist?: string[]
+
+          /**
+           * Specific an array of token that must be excluded in the final token list.
+           */
+          backlist?: string[]
+
+          /**
+           * Specific an array of token address that selected in the final token list.
+           */
+          selectedTokens?: string[]
+
+          disableSearchBar?: boolean
+      }
+    | {
+          open: false
+
+          /**
+           * The selected detailed token.
+           */
+          token?: EtherTokenDetailed | ERC20TokenDetailed
+      }
+
 interface WalletMessage {
     /**
      * Select wallet dialog
@@ -49,15 +85,20 @@ interface WalletMessage {
     walletStatusDialogUpdated: WalletStatusDialogEvent
 
     /**
+     * Select ERC20 token dialog
+     */
+    selectERC20TokenDialogUpdated: SelectERC20TOkenDialogEvent
+
+    /**
      * WalletConnect QR Code dialog
      */
     walletConnectQRCodeDialogUpdated: WalletConnectQRCodeDialogEvent
 
     walletsUpdated: void
-
     tokensUpdated: void
     rpc: unknown
 }
+
 if (module.hot) module.hot.accept()
 export const WalletMessages = createPluginMessage<WalletMessage>(PLUGIN_IDENTIFIER)
 export const WalletRPC = createPluginRPC(PLUGIN_IDENTIFIER, () => import('./services'), WalletMessages.events.rpc)
