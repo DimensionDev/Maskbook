84dc44229ca956de63d71ebde11a271130234b36
guanbinrui <guanbinrui@dimension.im>
2021-02-13 10:41:59 +0800

    chore: revamp getSwaps with cache management

--- packages/maskbook/src/plugins/Trader/apis/balancer/index.ts
+++ packages/maskbook/src/plugins/Trader/apis/balancer/index.ts
@@ -1,4 +1,5 @@
 import BigNumber from 'bignumber.js'
+import { memoize } from 'lodash-es'
 import { SOR } from '@balancer-labs/sor'
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { getChainId } from '../../../../extension/background-script/EthereumService'
@@ -7,26 +8,26 @@ import type { ChainId } from '../../../../web3/types'
 import { BALANCER_MAX_NO_POOLS, BALANCER_SOR_GAS_PRICE, BALANCER_SWAP_TYPE, TRADE_CONSTANTS } from '../../constants'
 import { CONSTANTS } from '../../../../web3/constants'
 
-async function createProvider(chainId: ChainId) {
-    return new JsonRpcProvider(getConstant(CONSTANTS, 'INFURA_ADDRESS', chainId))
+//#region the pools cache management
+const fetchedCache = new Map<ChainId, number>()
+
+function isFetchedCacheExpired(chainId: ChainId) {
+    return (fetchedCache.get(chainId) ?? 0) !== new Date().getHours()
 }
 
-async function createSOR(tokenOut: string, chainId?: ChainId) {
-    const chainId_ = chainId ?? (await getChainId())
-    const provider = await createProvider(chainId_)
-    const poolsUrl = getConstant(TRADE_CONSTANTS, 'BALANCER_EXCHANGE_PROXY_ADDRESS', chainId_)
-    const sor = new SOR(provider, BALANCER_SOR_GAS_PRICE, BALANCER_MAX_NO_POOLS, chainId_, poolsUrl)
-
-    // this fetches all pools list from URL in constructor then onChain balances using Multicall
-    await sor.fetchPools()
-
-    // this calculates the cost to make a swap which is used as an input to sor to allow it to make gas efficient recommendations.
-    // can be set once and will be used for further swap calculations.
-    // defaults to 0 if not called or can be set manually using: await sor.setCostOutputToken(tokenOut, manualPriceBn)
-    await sor.setCostOutputToken(tokenOut)
-
-    return sor
+function updateFetchedCache(chainId: ChainId) {
+    fetchedCache.set(chainId, new Date().getHours())
 }
+//#endregion
+
+const createSOR = memoize(
+    (chainId: ChainId) => {
+        const provider = new JsonRpcProvider(getConstant(CONSTANTS, 'INFURA_ADDRESS', chainId))
+        const poolsUrl = getConstant(TRADE_CONSTANTS, 'BALANCER_EXCHANGE_PROXY_ADDRESS', chainId)
+        return new SOR(provider, BALANCER_SOR_GAS_PRICE, BALANCER_MAX_NO_POOLS, chainId, poolsUrl)
+    },
+    (chainId: ChainId) => String(chainId),
+)
 
 export async function getBalancerSwaps(
     tokenIn: string,
@@ -34,6 +35,22 @@ export async function getBalancerSwaps(
     swapType: BALANCER_SWAP_TYPE,
     amount: string,
 ) {
-    const sor = await createSOR(tokenOut)
+    const chainId = await getChainId()
+    const sor = createSOR(chainId)
+
+    // this fetches all pools list from URL in constructor then onChain balances using Multicall
+    if (!sor.isAllFetched) {
+        await sor.fetchPools()
+        updateFetchedCache(chainId)
+    }
+
+    // if sor is expired then update in the non-blocking way
+    if (sor.isAllFetched && isFetchedCacheExpired(chainId)) sor.fetchPools().then(() => updateFetchedCache(chainId))
+
+    // this calculates the cost to make a swap which is used as an input to sor to allow it to make gas efficient recommendations.
+    // can be set once and will be used for further swap calculations.
+    // defaults to 0 if not called or can be set manually using: await sor.setCostOutputToken(tokenOut, manualPriceBn)
+    await sor.setCostOutputToken(tokenOut)
+
     return sor.getSwaps(tokenIn, tokenOut, swapType, new BigNumber(amount))
 }
