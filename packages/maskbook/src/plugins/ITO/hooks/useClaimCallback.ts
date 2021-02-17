e8cfb2071fcdea07d1dc3e2e082d190d69629eea
guanbinrui <guanbinrui@dimension.im>
2021-02-17 23:01:51 +0800

    chore: add useClaimCallback

---
+++ packages/maskbook/src/plugins/ITO/hooks/useClaimCallback.ts
@@ -0,0 +1,87 @@
+import { useCallback } from 'react'
+import BigNumber from 'bignumber.js'
+import type { TransactionReceipt } from 'web3-core'
+import { TransactionStateType, useTransactionState } from '../../../web3/hooks/useTransactionState'
+import { useAccount } from '../../../web3/hooks/useAccount'
+import { TransactionEventType } from '../../../web3/types'
+import type { Tx } from '../../../contracts/types'
+import { addGasMargin } from '../../../web3/helpers'
+import { useChainId } from '../../../web3/hooks/useChainState'
+import { useMaskITO_Contract } from '../contracts/useMaskITO_Contarct'
+
+export function useClaimCallback() {
+    const account = useAccount()
+    const chainId = useChainId()
+    const MaskITO_Contract = useMaskITO_Contract()
+    const [claimState, setClaimState] = useTransactionState()
+
+    const claimCallback = useCallback(async () => {
+        if (!MaskITO_Contract) {
+            setClaimState({
+                type: TransactionStateType.UNKNOWN,
+            })
+            return
+        }
+
+        // pre-step: start waiting for provider to confirm tx
+        setClaimState({
+            type: TransactionStateType.WAIT_FOR_CONFIRMING,
+        })
+
+        const config: Tx = {
+            from: account,
+            to: MaskITO_Contract.options.address,
+            value: '0',
+        }
+
+        // step 1: estimate gas
+        const estimatedGas = await MaskITO_Contract.methods
+            .claim()
+            .estimateGas(config)
+            .catch((error: Error) => {
+                setClaimState({
+                    type: TransactionStateType.FAILED,
+                    error,
+                })
+                throw error
+            })
+
+        // step 2: blocking
+        return new Promise<void>(async (resolve, reject) => {
+            const promiEvent = MaskITO_Contract.methods.claim().send({
+                gas: addGasMargin(new BigNumber(estimatedGas)).toFixed(),
+                ...config,
+            })
+            promiEvent.on(TransactionEventType.RECEIPT, (receipt: TransactionReceipt) => {
+                setClaimState({
+                    type: TransactionStateType.CONFIRMED,
+                    no: 0,
+                    receipt,
+                })
+            })
+            promiEvent.on(TransactionEventType.CONFIRMATION, (no: number, receipt: TransactionReceipt) => {
+                setClaimState({
+                    type: TransactionStateType.CONFIRMED,
+                    no,
+                    receipt,
+                })
+                resolve()
+            })
+            promiEvent.on(TransactionEventType.ERROR, (error: Error) => {
+                setClaimState({
+                    type: TransactionStateType.FAILED,
+                    error,
+                })
+                reject(error)
+            })
+        })
+    }, [account, chainId, MaskITO_Contract])
+
+    const resetCallback = useCallback(() => {
+        setClaimState({
+            type: TransactionStateType.UNKNOWN,
+        })
+    }, [])
+
+    return [claimState, claimCallback, resetCallback] as const
+}
