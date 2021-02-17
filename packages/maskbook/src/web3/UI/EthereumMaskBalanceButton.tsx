91f3978b77e4cb1516011e03ffd3773d050a0940
guanbinrui <guanbinrui@dimension.im>
2021-02-17 17:22:32 +0800

    chore(ui): tweat the mask button

--- packages/maskbook/src/web3/UI/EthereumMaskBalanceButton.tsx
+++ packages/maskbook/src/web3/UI/EthereumMaskBalanceButton.tsx
@@ -1,10 +1,18 @@
-import { Button, createStyles, makeStyles } from '@material-ui/core'
+import { Button, createStyles, makeStyles, Typography } from '@material-ui/core'
 import { useStylesExtends } from '../../components/custom-ui-helper'
+import { MaskbookIcon } from '../../resources/MaskbookIcon'
 
 const useStyles = makeStyles((theme) => {
     return createStyles({
         root: {
             borderRadius: 16,
+            fontWeight: 300,
+            backgroundColor: '#1C68F3',
+        },
+        icon: {
+            border: `solid 1px ${theme.palette.common.white}`,
+            borderRadius: '50%',
+            marginRight: theme.spacing(0.5),
         },
     })
 })
@@ -16,7 +24,8 @@ export function EthereumMaskBalanceButton(props: EthereumMaskBalanceButtonProps)
 
     return (
         <Button className={classes.root} variant="contained" color="primary">
-            0 MASK
+            <MaskbookIcon className={classes.icon} />
+            <Typography>0 MASK</Typography>
         </Button>
     )
 }
