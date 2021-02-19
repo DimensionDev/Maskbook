fe5d6b8331c231054b3b7145ed3bcdb914c9a19c
Jack Works <jackworks@protonmail.com>
2021-02-19 21:07:40 +0800

    chore: add to storybook i18n

--- packages/theme/.storybook/preview.js
+++ packages/theme/.storybook/preview.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { ThemeProvider, StylesProvider } from '@material-ui/core'
 import { MaskDarkTheme, MaskLightTheme } from '../src/theme'
 import { withMatrix } from 'storybook-addon-matrix'
+import { addMaskThemeI18N } from '../src/locales'
+import i18n from 'i18next'
+import { I18nextProvider } from 'react-i18next'
+
+addMaskThemeI18N(i18n)
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -11,7 +16,9 @@ export const decorators = [
     (Story) => (
         <StylesProvider injectFirst>
             <ThemeProvider theme={MaskLightTheme}>
-                <Story />
+                <I18nextProvider i18n={i18n}>
+                    <Story />
+                </I18nextProvider>
             </ThemeProvider>
         </StylesProvider>
     ),
