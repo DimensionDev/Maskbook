1f86bec13864a217dcda6854f89c931bb6f53204
Jack Works <jackworks@protonmail.com>
2021-02-19 22:32:27 +0800

    fix: storybook

--- packages/theme/.storybook/preview.js
+++ packages/theme/.storybook/preview.js
@@ -3,11 +3,17 @@ import { ThemeProvider, StylesProvider } from '@material-ui/core'
 import { MaskDarkTheme, MaskLightTheme } from '../src/theme'
 import { withMatrix } from 'storybook-addon-matrix'
 import { addMaskThemeI18N } from '../src/locales'
-import i18n from 'i18next'
 import { I18nextProvider } from 'react-i18next'
-
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+i18n.init({
+    resources: {},
+    keySeparator: false,
+    interpolation: { escapeValue: false },
+    fallbackLng: 'en',
+})
+i18n.use(initReactI18next)
 addMaskThemeI18N(i18n)
-
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
 }
