81b6b6e0a4db51d505cc875c253073ad4f63fd3b
guanbinrui <guanbinrui@dimension.im>
2021-02-20 15:45:28 +0800

    chore: deploy contracts

--- packages/maskbook/src/plugins/Airdrop/constants.ts
+++ packages/maskbook/src/plugins/Airdrop/constants.ts
@@ -6,8 +6,8 @@ export const AirdropPluginID = 'com.maskbook.airdrop'
 export const AIRDROP_CONSTANTS = {
     AIRDROP_CONTRACT_ADDRESS: {
         [ChainId.Mainnet]: '',
-        [ChainId.Ropsten]: '',
-        [ChainId.Rinkeby]: '',
+        [ChainId.Ropsten]: '0x59c087d85B5baC37d6cCA7B343BBA5718f385347',
+        [ChainId.Rinkeby]: '0x91fdf9171b2f6dd10bf1E405C5fEf66A21de3bE5',
         [ChainId.Kovan]: '',
         [ChainId.Gorli]: '',
     },
