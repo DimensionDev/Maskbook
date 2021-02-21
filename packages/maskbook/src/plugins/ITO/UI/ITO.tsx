e844d300401b2053f06bbc56254be9a4cd8192f5
guanbinrui <guanbinrui@dimension.im>
2021-02-22 03:50:45 +0800

    fix: withdraw contract address

--- packages/maskbook/src/plugins/ITO/UI/ITO.tsx
+++ packages/maskbook/src/plugins/ITO/UI/ITO.tsx
@@ -191,7 +191,7 @@ export function ITO(props: ITO_Props) {
     const postLink = usePostLink()
     const chainId = useChainId()
     const chainIdValid = useChainIdValid()
-    const [destructState, destructCallback, resetDestructCallback] = useDestructCallback(false)
+    const [destructState, destructCallback, resetDestructCallback] = useDestructCallback(props.isMask ?? false)
     const [openClaimDialog, setOpenClaimDialog] = useState(false)
 
     // assets
