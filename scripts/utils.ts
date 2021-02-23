ba086e3bfe79c423f01fda44ee4a65ace9f8f420
Septs <github@septs.pw>
2021-02-23 19:39:26 +0800

    fix: ci build (#2468)

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,4 +1,4 @@
-import { execFileSync } from 'child_process'
+import { spawnSync } from 'child_process'
 import os from 'os'
 import path from 'path'
 
@@ -7,9 +7,9 @@ export const BUILD_PATH = path.resolve(ROOT_PATH, 'build')
 
 export function run(cwd = ROOT_PATH, cmd: string, ...args: string[]) {
     console.log('$', cmd, args.join(' '), '# cwd:', path.relative(ROOT_PATH, cwd))
-    return execFileSync(cmd, args, {
+    return spawnSync(cmd, args, {
         cwd,
-        stdio: [process.stdin, process.stdout, process.stderr],
+        stdio: 'inherit',
         shell: os.platform() === 'win32',
     })
 }
