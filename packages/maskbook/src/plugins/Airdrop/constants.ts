85dec882d3630a290d0778bddf5671624a9f4366
guanbinrui <guanbinrui@dimension.im>
2021-02-26 20:05:06 +0800

    chore: deploy contract

--- packages/maskbook/src/plugins/Airdrop/constants.ts
+++ packages/maskbook/src/plugins/Airdrop/constants.ts
@@ -8,10 +8,10 @@ export const AIRDROP_CONSTANTS = {
         [ChainId.Mainnet]: '0x0000000000000000000000000000000000000000',
 
         // token: 0x5B966f3a32Db9C180843bCb40267A66b73E4f022
-        // start: 1613900000
-        // end: 1663900000
-        // proof root: 0x9fbb7d9cdcfc16dde42d74f08a2debc2d2466a9ac424a34ccea9528b7ce7db80
-        [ChainId.Ropsten]: '0x5cb2Fcc577bf1F3C2a530691277254329225e3e2',
+        // start: 1614342224
+        // end: 1614614400
+        // proof root: 0x127fbceb4f7fe6dd37769da52ef29ac2b322584d369bcb89df39092b3eced979
+        [ChainId.Ropsten]: '0x022433708964fB27493BaFF4570dBD5A4a9dfd03',
 
         // token: 0xFD9Eb54f6aC885079e7bB3E207922Bb7256E3fcb
         // start: 1613900000
