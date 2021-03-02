702b4d2d33d17d770a7a31cc80d3c889471c4f1e
Hancheng Zhou <z308114274@gmail.com>
2021-03-02 15:31:06 +0800

    chore: remove unused history code

--- packages/maskbook/src/plugins/RedPacket/UI/RedPacketList.tsx
+++ packages/maskbook/src/plugins/RedPacket/UI/RedPacketList.tsx
@@ -1,14 +1,9 @@
 import { makeStyles, Typography, List } from '@material-ui/core'
 import type { RedPacketJSONPayload, RedPacket_InMask_Record } from '../types'
-import { FixedSizeList, FixedSizeListProps } from 'react-window'
-import { RedPacketInList, RedPacketInHistoryList } from './RedPacketInList'
-import { useRedPacketsFromChain } from '../hooks/useRedPacket'
-import { usePayloadsComputed } from '../hooks/usePayloadComputed'
+import { useAccount } from '@dimensiondev/web3-shared'
+import { RedPacketInHistoryList } from './RedPacketInList'
 import { useAllRedPackets } from '../hooks/useAllRedPackets'
-import { useChainIdValid, useAccount } from '@dimensiondev/web3-shared'
-import { useI18N } from '../../../utils'
 
-//#region red packet list UI
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
@@ -29,47 +24,6 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'center',
     },
 }))
-
-interface RedPacketListProps {
-    loading?: boolean
-    payloads: RedPacketJSONPayload[]
-    FixedSizeListProps?: Partial<FixedSizeListProps>
-    onSelect?: (payload: RedPacketJSONPayload) => void
-}
-
-function RedPacketList(props: RedPacketListProps) {
-    const { t } = useI18N()
-    const { loading = false, payloads, FixedSizeListProps, onSelect } = props
-    const classes = useStyles()
-    return (
-        <div className={classes.root}>
-            {loading ? (
-                <Typography className={classes.placeholder} color="textSecondary">
-                    {t('plugin_dhedge_loading')}
-                </Typography>
-            ) : payloads.length === 0 ? (
-                <Typography className={classes.placeholder} color="textSecondary">
-                    {t('plugin_dhedge_no_data')}
-                </Typography>
-            ) : (
-                <FixedSizeList
-                    className={classes.list}
-                    width="100%"
-                    height={350}
-                    overscanCount={4}
-                    itemSize={60}
-                    itemData={{
-                        payloads,
-                        onClick: onSelect,
-                    }}
-                    itemCount={payloads.length}
-                    {...FixedSizeListProps}>
-                    {RedPacketInList}
-                </FixedSizeList>
-            )}
-        </div>
-    )
-}
 
 interface RedPacketHistoryListProps {
     payloads: RedPacket_InMask_Record[]
@@ -119,33 +73,5 @@ export function RedPacketBacklogList(props: RedPacketBacklogListProps) {
         )
     }
     return <RedPacketHistoryList payloads={payloads!} onSelect={onSelect} onClose={onClose} />
-}
-//#endregion
-
-//#region inbound list
-export interface RedPacketInboundListProps extends withClasses<never> {
-    onSelect?: (payload: RedPacketJSONPayload) => void
-}
-
-export function RedPacketInboundList(props: RedPacketInboundListProps) {
-    const { onSelect } = props
-    const { value: records = [], loading } = useRedPacketsFromChain()
-    const payloads = usePayloadsComputed('claim', records)
-    const chainIdValid = useChainIdValid()
-    return <RedPacketList loading={loading} payloads={chainIdValid ? payloads : []} onSelect={onSelect} />
-}
-//#endregion
-
-//#region outbound list
-export interface RedPacketOutboundListProps extends withClasses<never> {
-    onSelect?: (payload: RedPacketJSONPayload) => void
-}
-
-export function RedPacketOutboundList(props: RedPacketOutboundListProps) {
-    const { onSelect } = props
-    const { value: records = [], loading } = useRedPacketsFromChain()
-    const payloads = usePayloadsComputed('create', records)
-    const chainIdValid = useChainIdValid()
-    return <RedPacketList loading={loading} payloads={chainIdValid ? payloads : []} onSelect={onSelect} />
 }
 //#endregion
