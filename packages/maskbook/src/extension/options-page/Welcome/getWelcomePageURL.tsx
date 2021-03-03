025910c9cfef98294e9d23bdc63f613fa6ed41ad
Jack Works <jackworks@protonmail.com>
2021-03-03 15:31:26 +0800

    refactor: remove some usage of getUrl

--- packages/maskbook/src/extension/options-page/Welcome/getWelcomePageURL.tsx
+++ packages/maskbook/src/extension/options-page/Welcome/getWelcomePageURL.tsx
@@ -1,14 +1,13 @@
 import { Flags } from '../../../utils/flags'
-import { getUrl } from '../../../utils/utils'
 import { DashboardRoute } from '../Route'
 import { SetupStep } from '../SetupStep'
 
 export function getWelcomePageURL() {
     if (process.env.target === 'E2E') {
-        return getUrl(`index.html#${DashboardRoute.Setup}`)
+        return browser.runtime.getURL(`index.html#${DashboardRoute.Setup}`)
     } else if (Flags.has_no_browser_tab_ui) {
-        return getUrl(`index.html#${DashboardRoute.Nav}`)
+        return browser.runtime.getURL(`index.html#${DashboardRoute.Nav}`)
     } else {
-        return getUrl(`index.html#${DashboardRoute.Setup}/${SetupStep.ConsentDataCollection}`)
+        return browser.runtime.getURL(`index.html#${DashboardRoute.Setup}/${SetupStep.ConsentDataCollection}`)
     }
 }
