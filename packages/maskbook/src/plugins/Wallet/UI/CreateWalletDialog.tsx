ace06a35fb7056f4e69af7410168120b0ccbd51c
guanbinrui <guanbinrui@dimension.im>
2021-03-10 11:39:23 +0800

    chore: add description fields

--- packages/maskbook/src/plugins/Wallet/UI/CreateWalletDialog.tsx
+++ packages/maskbook/src/plugins/Wallet/UI/CreateWalletDialog.tsx
@@ -1,5 +1,15 @@
 import { useState, useCallback } from 'react'
-import { Button, Box, Card, createStyles, DialogContent, makeStyles, useTheme, TextField } from '@material-ui/core'
+import {
+    Button,
+    Box,
+    Card,
+    createStyles,
+    DialogContent,
+    makeStyles,
+    useTheme,
+    TextField,
+    Typography,
+} from '@material-ui/core'
 import classNames from 'classnames'
 import { useI18N } from '../../../utils/i18n-next-ui'
 import { useStylesExtends } from '../../../components/custom-ui-helper'
@@ -22,14 +32,13 @@ enum CreateWalletStep {
 const useStyles = makeStyles((theme) =>
     createStyles({
         content: {
-            minHeight: 300,
             padding: theme.spacing(6, 7.6),
         },
         top: {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'flex-end',
-            padding: theme.spacing(2, 0),
+            padding: theme.spacing(0, 0, 2),
         },
         bottom: {
             display: 'flex',
@@ -37,6 +46,7 @@ const useStyles = makeStyles((theme) =>
             justifyContent: 'center',
             padding: theme.spacing(4, 0, 0),
         },
+        description: {},
         input: {
             width: '100%',
         },
@@ -87,7 +97,8 @@ export function CreateWalletDialog(props: CreateWalletDialogProps) {
             open: false,
         })
         await sleep(300)
-        setStep(CreateWalletStep.Words)
+        setName('')
+        setStep(CreateWalletStep.Name)
     }, [setOpen])
     //#endregion
 
@@ -103,15 +114,6 @@ export function CreateWalletDialog(props: CreateWalletDialogProps) {
                 break
         }
     }, [step])
-    const onSubmit = useSnackbarCallback(
-        () =>
-            WalletRPC.importNewWallet({
-                name,
-                mnemonic: words,
-            }),
-        [name, words],
-        onClose,
-    )
     const onBack = useCallback(() => {
         switch (step) {
             case CreateWalletStep.Words:
@@ -123,6 +125,15 @@ export function CreateWalletDialog(props: CreateWalletDialogProps) {
                 break
         }
     }, [step, resetCallback])
+    const onSubmit = useSnackbarCallback(
+        () =>
+            WalletRPC.importNewWallet({
+                name,
+                mnemonic: words,
+            }),
+        [name, words],
+        onClose,
+    )
     const onWordChange = useCallback((word: string, index: number) => answerCallback(word, index), [answerCallback])
     //#endregion
 
@@ -130,11 +141,27 @@ export function CreateWalletDialog(props: CreateWalletDialogProps) {
         <InjectedDialog
             open={open}
             onClose={onClose}
-            title={t('plugin_wallet_create_a_wallet')}
+            title={t(
+                step === CreateWalletStep.Name
+                    ? 'plugin_wallet_setup_title_create'
+                    : step === CreateWalletStep.Words
+                    ? 'plugin_wallet_setup_title_words'
+                    : 'plugin_wallet_setup_title_verify',
+            )}
             DialogProps={{
                 maxWidth: step === CreateWalletStep.Name ? 'xs' : 'sm',
             }}>
             <DialogContent className={classes.content}>
+                {step !== CreateWalletStep.Name ? (
+                    <Typography className={classes.description}>
+                        {t(
+                            step === CreateWalletStep.Words
+                                ? 'plugin_wallet_setup_description_words'
+                                : 'plugin_wallet_setup_description_verify',
+                        )}
+                    </Typography>
+                ) : null}
+
                 {step === CreateWalletStep.Name ? (
                     <Box>
                         <TextField
@@ -201,7 +228,7 @@ export function CreateWalletDialog(props: CreateWalletDialogProps) {
                             style={{
                                 marginRight: 16,
                             }}>
-                            {t('plugin_wallet_verification_back')}
+                            {t('plugin_wallet_setup_back')}
                         </ActionButton>
                     ) : null}
                     <ActionButton
@@ -212,9 +239,11 @@ export function CreateWalletDialog(props: CreateWalletDialogProps) {
                         }
                         onClick={step === CreateWalletStep.Name || step === CreateWalletStep.Words ? onNext : onSubmit}>
                         {t(
-                            step === CreateWalletStep.Name || step === CreateWalletStep.Words
-                                ? 'plugin_wallet_verification_next'
-                                : 'plugin_wallet_verification_verify',
+                            step === CreateWalletStep.Name
+                                ? 'plugin_wallet_setup_create'
+                                : step === CreateWalletStep.Words
+                                ? 'plugin_wallet_setup_next'
+                                : 'plugin_wallet_setup_verify',
                         )}
                     </ActionButton>
                 </Box>
