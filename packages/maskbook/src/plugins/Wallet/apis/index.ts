ce40028b102cc2822e5ad34eab71ded5033b2169
Septs <github@septs.pw>
2021-03-12 20:20:05 +0800

    fix: from debank logic

--- packages/maskbook/src/plugins/Wallet/apis/index.ts
+++ packages/maskbook/src/plugins/Wallet/apis/index.ts
@@ -16,45 +16,47 @@ export async function getTransactionList(address: string, provider: TransactionP
 }
 
 function fromDeBank({ cate_dict, history_list, token_dict }: DeBankAPI.HISTORY_RESPONSE['data']) {
-    return history_list.map((transaction) => {
-        let type = transaction.tx?.name
-        if (isNil(type) && !isNil(transaction.cate_id)) {
-            type = cate_dict[transaction.cate_id].en
-        } else if (type === '') {
-            type = 'contract interaction'
-        }
-        return {
-            type,
-            id: transaction.id,
-            timeAt: new Date(transaction.time_at * 1000),
-            toAddress: transaction.other_addr,
-            failed: transaction.tx?.status === 0,
-            pairs: [
-                ...transaction.sends
-                    .filter(({ token_id }) => token_dict[token_id].is_verified)
-                    .map(({ amount, token_id }) => ({
-                        name: token_dict[token_id].name,
-                        symbol: token_dict[token_id].optimized_symbol,
-                        address: token_id,
-                        direction: 'send',
-                        amount,
-                    })),
-                ...transaction.receives
-                    .filter(({ token_id }) => token_dict[token_id].is_verified)
-                    .map(({ amount, token_id }) => ({
-                        name: token_dict[token_id].name,
-                        symbol: token_dict[token_id].optimized_symbol,
-                        address: token_id,
-                        direction: 'receive',
-                        amount,
-                    })),
-            ],
-            gasFee: transaction.tx
-                ? {
-                      eth: transaction.tx.eth_gas_fee,
-                      usd: transaction.tx.usd_gas_fee,
-                  }
-                : undefined,
-        }
-    })
+    return history_list
+        .filter((transaction) => transaction.tx?.name ?? transaction.cate_id)
+        .map((transaction) => {
+            let type = transaction.tx?.name
+            if (isNil(type) && !isNil(transaction.cate_id)) {
+                type = cate_dict[transaction.cate_id].en
+            } else if (type === '') {
+                type = 'contract interaction'
+            }
+            return {
+                type,
+                id: transaction.id,
+                timeAt: new Date(transaction.time_at * 1000),
+                toAddress: transaction.other_addr,
+                failed: transaction.tx?.status === 0,
+                pairs: [
+                    ...transaction.sends
+                        .filter(({ token_id }) => token_dict[token_id].is_verified)
+                        .map(({ amount, token_id }) => ({
+                            name: token_dict[token_id].name,
+                            symbol: token_dict[token_id].optimized_symbol,
+                            address: token_id,
+                            direction: 'send',
+                            amount,
+                        })),
+                    ...transaction.receives
+                        .filter(({ token_id }) => token_dict[token_id].is_verified)
+                        .map(({ amount, token_id }) => ({
+                            name: token_dict[token_id].name,
+                            symbol: token_dict[token_id].optimized_symbol,
+                            address: token_id,
+                            direction: 'receive',
+                            amount,
+                        })),
+                ],
+                gasFee: transaction.tx
+                    ? {
+                          eth: transaction.tx.eth_gas_fee,
+                          usd: transaction.tx.usd_gas_fee,
+                      }
+                    : undefined,
+            }
+        })
 }
