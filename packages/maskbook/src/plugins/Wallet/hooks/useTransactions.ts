deaeddb2fb30a335129b9d3603c56eba74510879
guanbinrui <guanbinrui@dimension.im>
2021-03-12 19:51:12 +0800

    fix: api parameter error

--- packages/maskbook/src/plugins/Wallet/hooks/useTransactions.ts
+++ packages/maskbook/src/plugins/Wallet/hooks/useTransactions.ts
@@ -5,6 +5,6 @@ import type { TransactionProvider } from '../types'
 export function useTransactions(address: string, provider: TransactionProvider) {
     return useAsyncRetry(async () => {
         if (!address) return []
-        return WalletRPC.getTransactionList('0x9972d940c9a23f84fcf92867d18f28d75d010e5e'.toLowerCase(), provider)
+        return WalletRPC.getTransactionList(address.toLowerCase(), provider)
     }, [address, provider])
 }
