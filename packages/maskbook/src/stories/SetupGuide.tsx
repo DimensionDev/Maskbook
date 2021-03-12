afae80915012a8bba17eadf6a38258eadd72e793
Jack Works <jackworks@protonmail.com>
2021-03-12 15:03:59 +0800

    chore: remove storybook

--- packages/maskbook/src/stories/SetupGuide.tsx
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { ECKeyIdentifier } from '../database/type'
-import { SetupGuide } from '../components/InjectedComponents/SetupGuide'
-
-storiesOf('Setup Guide', module).add('Setup Guide', () => (
-    <SetupGuide persona={new ECKeyIdentifier('secp256k1', 'test_key')} onClose={action('close')} />
-))
--- packages/maskbook/src/stories/utils.ts
+++
@@ -1,9 +0,0 @@
-export function figmaLink(link: string) {
-    return {
-        design: config({
-            type: 'figma',
-            url: link,
-        }),
-    }
-}
-import { config } from 'storybook-addon-designs'
