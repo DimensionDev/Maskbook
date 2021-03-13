0786116954d1c2d657ab394659c5b26dc3b41edd
guanbinrui <guanbinrui@dimension.im>
2021-03-13 11:26:09 +0800

    chore(ui): add list UI

---
+++ packages/maskbook/src/extension/options-page/DashboardComponents/CollectibleList/Card.tsx
@@ -0,0 +1,39 @@
+import Tilt from 'react-tilt'
+import { Card, createStyles, Link, makeStyles } from '@material-ui/core'
+import { Image } from '../../../../components/shared/Image'
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        root: {
+            borderRadius: 4,
+            position: 'relative',
+            backgroundColor: theme.palette.background.paper,
+        },
+    }),
+)
+
+export interface CollectibleCardProps {
+    url: string
+    link: string
+    canViewOnEtherscan?: boolean
+}
+
+export function CollectibleCard(props: CollectibleCardProps) {
+    const classes = useStyles(props)
+
+    return <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href={props.link}>
+        <Tilt options={{ scale: 1, max: 30, glare: true, 'max-glare': 1, speed: 1000 }}>
+            <Card
+                className={classes.root}
+                style={{
+                    width: 160,
+                    height: 220,
+                }}>
+                <Image component="img" width={160} height={220} src={props.url} />
+            </Card>
+        </Tilt>
+    </Link>
+}
--- packages/maskbook/src/extension/options-page/DashboardComponents/CollectibleList/index.tsx
+++ packages/maskbook/src/extension/options-page/DashboardComponents/CollectibleList/index.tsx
@@ -1,21 +1,32 @@
 import {
     Box,
     Button,
-    createStyles,
     makeStyles,
-    Skeleton,
-    Table,
-    TableBody,
-    TableCell,
-    TableRow,
     Typography,
 } from '@material-ui/core'
+import { CollectibleCard } from './Card'
 import { useCollectibles } from '../../../../plugins/Wallet/hooks/useCollectibles'
 import { AssetProvider } from '../../../../plugins/Wallet/types'
 import { useAccount } from '../../../../web3/hooks/useAccount'
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        padding: theme.spacing(1),
+    },
+    card: {
+        padding: theme.spacing(1),
+    },
+    description: {
+        textAlign: 'center',
+        marginTop: theme.spacing(0.5),
+    },
+}))
+
 export function CollectibleList() {
     const account = useAccount()
+    const classes = useStyles()
     const {
         value: collectibles = [],
         loading: collectiblesLoading,
@@ -48,10 +59,15 @@ export function CollectibleList() {
         )
 
     return (
-        <>
+        <Box className={classes.root}>
             {collectibles.map((x) => (
-                <Typography>{x.id}</Typography>
+                <div className={classes.card} key={x.id}>
+                    <CollectibleCard key={x.id} url={x.image_url ?? x.image_preview_url ?? ''} link={x.permalink} />
+                    <div className={classes.description}>
+                        <Typography color="textSecondary" variant="body2">{x.name}</Typography>
+                    </div>
+                </div>
             ))}
-        </>
+        </Box>
     )
 }
