510804c2a966058a3e6a22a878597b90603fce4b
guanbinrui <guanbinrui@dimension.im>
2021-03-13 15:48:55 +0800

    chore: remove test addr

--- packages/maskbook/src/plugins/Wallet/hooks/useCollectibles.ts
+++ packages/maskbook/src/plugins/Wallet/hooks/useCollectibles.ts
@@ -5,7 +5,6 @@ import type { AssetProvider } from '../types'
 export function useCollectibles(address: string, provider: AssetProvider) {
     return useAsyncRetry(async () => {
         if (!address) return []
-        // return WalletRPC.getAssetsListNFT(address.toLowerCase(), provider)
-        return WalletRPC.getAssetsListNFT('0x0d09dC9a840B1b4ea25194998fD90bB50fC2008A'.toLowerCase(), provider)
+        return WalletRPC.getAssetsListNFT(address.toLowerCase(), provider)
     }, [address, provider])
 }
