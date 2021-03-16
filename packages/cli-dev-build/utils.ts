f32d569cb66c4421ce3f64751b7c46b93df352a0
Septs <github@septs.pw>
2021-03-16 17:49:49 +0800

    chore: move scripts to packages/cli-dev-build

--- packages/cli-dev-build/utils.ts
+++ packages/cli-dev-build/utils.ts
@@ -2,7 +2,7 @@ import { spawnSync } from 'child_process'
 import os from 'os'
 import path from 'path'
 
-export const ROOT_PATH = path.resolve(__dirname, '..')
+export const ROOT_PATH = path.resolve(__dirname, '..', '..')
 export const BUILD_PATH = path.resolve(ROOT_PATH, 'build')
 
 export function run(cwd = ROOT_PATH, cmd: string, ...args: string[]) {
