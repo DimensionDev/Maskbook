c4c1c4d6bc0313c71e6a6befd019ec89cfbf8e29
Septs <github@septs.pw>
2021-03-17 16:38:47 +0800

    fix: typo

--- packages/cli/utils.ts
+++ packages/cli/utils.ts
@@ -19,7 +19,7 @@ export function run(cwd = ROOT_PATH, cmd: string, ...args: string[]) {
 export const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
 export const awaitChildProcess = (child: ChildProcess) => {
-    return new Promise((resolve, reject) => {
+    return new Promise<number>((resolve, reject) => {
         child.on('error', () => reject(child.exitCode))
         child.on('exit', (code) => (code === 0 ? resolve : reject)(code))
     })
