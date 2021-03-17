097a8b72c0deca9e066537e2b4ec4a8e7174e5a2
Hancheng Zhou <z308114274@gmail.com>
2021-03-17 10:56:58 +0800

    fix: nft assets ui (#2610)

--- packages/maskbook/src/plugins/Wallet/apis/opensea.ts
+++ packages/maskbook/src/plugins/Wallet/apis/opensea.ts
@@ -123,7 +123,6 @@ export async function getAssetsList(from: string, size = 50, page = 0) {
     const response = await fetch(`https://api.opensea.io/api/v1/assets?${params.toString()}`, {
         method: 'GET',
         mode: 'cors',
-        cache: 'force-cache',
     })
     return (await response.json()) as AssetsListResponse
 }
