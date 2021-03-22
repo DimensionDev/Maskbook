ba153eb6907760966a47219f90ff999c818e3f8b
guanbinrui <guanbinrui@dimension.im>
2021-03-22 19:23:53 +0800

    chore: remove useless db logic

--- packages/maskbook/src/plugins/Wallet/database/Wallet.db.ts
+++ packages/maskbook/src/plugins/Wallet/database/Wallet.db.ts
@@ -18,26 +18,9 @@ export const createWalletDBAccess = createDBAccess(() => {
     return openDB<WalletDB>('maskbook-plugin-wallet', 7, {
         async upgrade(db, oldVersion, newVersion, tx) {
             function v0_v1() {
-                // @ts-expect-error
-                db.createObjectStore('RedPacket', { keyPath: path<keyof RedPacketRecordInDatabase>('id') })
                 db.createObjectStore('ERC20Token', { keyPath: path<keyof ERC20TokenRecordInDatabase>('address') })
                 db.createObjectStore('Wallet', { keyPath: path<keyof WalletRecordInDatabase>('address') })
             }
-            function v1_v2() {
-                // @ts-expect-error
-                db.createObjectStore('GitcoinDonation', { keyPath: 'donation_transaction_hash' })
-            }
-            /**
-             * The following store has been removed from v3
-             * GitcoinDonation: { % data dropped % }
-             * RedPacket: {
-             *     value: RedPacketRecordInDatabase
-             *     key: string
-             *     indexes: {
-             *         red_packet_id: string
-             *     }
-             * }
-             */
             function v2_v3() {
                 const os = db.createObjectStore('PluginStore', { keyPath: 'record_id' })
                 // @ts-ignore
@@ -47,29 +30,16 @@ export const createWalletDBAccess = createDBAccess(() => {
                 // @ts-ignore
                 os.createIndex('2', 'index2')
                 os.createIndex('plugin_id', 'plugin_id')
-                // @ts-ignore
-                db.deleteObjectStore('GitcoinDonation')
-                // @ts-ignore
-                db.deleteObjectStore('RedPacket')
             }
-
-            /**
-             * Add ERC721Token and ERC1155Token records in DB
-             */
             function v5_v6() {
                 db.createObjectStore('ERC721Token', { keyPath: path<keyof ERC721TokenRecordInDatabase>('record_id') })
                 db.createObjectStore('ERC1155Token', { keyPath: path<keyof ERC1155TokenRecordInDatabase>('record_id') })
             }
-
-            /**
-             * Add Phrase records in DB
-             */
             function v6_v7() {
                 db.createObjectStore('Phrase', { keyPath: path<keyof PhraseRecordInDatabase>('id') })
             }
 
             if (oldVersion < 1) v0_v1()
-            if (oldVersion < 2) v1_v2()
             if (oldVersion < 3) v2_v3()
             if (oldVersion < 6) v5_v6()
             if (oldVersion < 7) v6_v7()
