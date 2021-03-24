01769725b5ab0dfefcec5b51135d163d2b7e7e2d
guanbinrui <guanbinrui@dimension.im>
2021-03-24 14:14:47 +0800

    chore: remove test accounts

--- packages/maskbook/src/plugins/Wallet/hooks/useCollectibles.ts
+++ packages/maskbook/src/plugins/Wallet/hooks/useCollectibles.ts
@@ -27,11 +27,6 @@ export function useCollectiblesFromDB() {
 export function useCollectiblesFromNetwork(address: string, provider: AssetProvider) {
     return useAsyncRetry(async () => {
         if (!address) return []
-        return await WalletRPC.getAssetsListNFT('0x265b95a94d0de9643fb06765b7b2a6df9910aca1'.toLowerCase(), provider)
-        // return WalletRPC.getAssetsListNFT(address.toLowerCase(), provider)
-        // return WalletRPC.getAssetsListNFT(address, provider)
-        // return WalletRPC.getAssetsListNFT('0x3c6137504c38215fea30605b3e364a23c1d3e14f'.toLowerCase(), provider)
-        // return WalletRPC.getAssetsListNFT('0x65c1b9ae4e4d8dcccfd3dc41b940840fe8570f2a'.toLowerCase(), provider)
-        // return WalletRPC.getAssetsListNFT('0xa357a589a37cf7b6edb31b707e8ed3219c8249ac'.toLowerCase(), provider)
+        return WalletRPC.getAssetsListNFT(address.toLowerCase(), provider)
     }, [address, provider])
 }
