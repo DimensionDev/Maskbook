eaf9be4e2398a0bafab17c13f03a695d961e18b7
Jack Works <jackworks@protonmail.com>
2021-03-24 16:17:01 +0800

    feat: limit the condition of composition button appears

--- packages/maskbook/src/social-network-adaptor/instagram.com/injection/Entry.tsx
+++ packages/maskbook/src/social-network-adaptor/instagram.com/injection/Entry.tsx
@@ -1,16 +1,30 @@
 import { Fab, experimentalStyled as styled } from '@material-ui/core'
 import { Create } from '@material-ui/icons'
 import { PostDialog } from '../../../components/InjectedComponents/PostDialog'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { LiveSelector, MutationObserverWatcher } from '@dimensiondev/holoflows-kit'
 
 const Container = styled('div')`
     position: fixed;
     right: 2.5em;
     bottom: 5em;
 `
+const appearsWith = '[data-testid="new-post-button"]'
 export function Entry() {
     const open = useState(false)
-    console.log(open)
+    const [display, setDisplay] = useState(!!document.querySelector(appearsWith))
+    useEffect(() => {
+        const watch = new MutationObserverWatcher(
+            new LiveSelector().querySelector(appearsWith).enableSingleMode(),
+        ).startWatch({
+            childList: true,
+            subtree: true,
+        })
+        watch.on('onAdd', () => setDisplay(true))
+        watch.on('onRemove', () => setDisplay(false))
+        return () => watch.stopWatch()
+    })
+    if (!display) return null
     return (
         <Container>
             <Fab variant="extended" onClick={() => open[1](true)}>
