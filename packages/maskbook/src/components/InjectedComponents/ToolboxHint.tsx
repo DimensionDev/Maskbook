75716f46ef741fad52bf0722c1a8c112e3fae284
Hancheng Zhou <z308114274@gmail.com>
2021-03-24 12:26:32 +0800

    chore: hint ui

--- packages/maskbook/src/components/InjectedComponents/ToolboxHint.tsx
+++ packages/maskbook/src/components/InjectedComponents/ToolboxHint.tsx
@@ -1,4 +1,4 @@
-import { Box, makeStyles, Typography } from '@material-ui/core'
+import { Button, makeStyles, Typography } from '@material-ui/core'
 import { MaskbookSharpIcon } from '../../resources/MaskbookIcon'
 
 const useStyles = makeStyles((theme) => ({
@@ -6,7 +6,32 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         alignItems: 'center',
         width: '100%',
-        padding: '8px 10px',
+        padding: '12px 26px 12px 12px',
+        cursor: 'pointer',
+        [theme.breakpoints.down('lg')]: {
+            transform: 'translateX(-6px)',
+            padding: 12,
+        },
+        '&:hover': {
+            "& $text": {
+                color: theme.palette.primary.main,
+            },
+            "& $icon": {
+                color: theme.palette.primary.main,
+            }
+        },
+    },
+    text: {
+        color: theme.palette.mode === 'dark' ? 'rgb(255, 255, 255)' : 'rgb(15, 20, 25)',
+        fontWeight: 700,
+        fontSize: 20,
+        marginLeft: 22,
+        [theme.breakpoints.down('lg')]: {
+            display: 'none'
+        }
+    },
+    icon: {
+        color: theme.palette.mode === 'dark' ? 'rgb(255, 255, 255)' : 'rgb(15, 20, 25)',
     },
 }))
 
@@ -14,9 +39,9 @@ export function ToolboxHint() {
     const classes = useStyles()
 
     return (
-        <Box className={classes.wrapper}>
-            <MaskbookSharpIcon />
-            <Typography>Mask</Typography>
-        </Box>
+        <Button className={classes.wrapper}>
+            <MaskbookSharpIcon classes={{ root: classes.icon }} />
+            <Typography className={classes.text}>Mask</Typography>
+        </Button>
     )
 }
