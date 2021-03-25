916330a4c3d58f02b61533374d65404a3fd43e6a
guanbinrui <guanbinrui@dimension.im>
2021-03-25 16:25:19 +0800

    chore: add db paged methods

--- packages/maskbook/src/plugins/Wallet/services/erc721.ts
+++ packages/maskbook/src/plugins/Wallet/services/erc721.ts
@@ -1,14 +1,43 @@
+import Fuse from 'fuse.js'
 import { createTransaction } from '../../../database/helpers/openDB'
 import { createWalletDBAccess } from '../database/Wallet.db'
 import { WalletMessages } from '../messages'
 import { assert } from '../../../utils/utils'
 import { formatChecksumAddress } from '../formatter'
-import { WalletRecordIntoDB, ERC721TokenRecordIntoDB, getWalletByAddress } from './helpers'
+import { WalletRecordIntoDB, ERC721TokenRecordIntoDB, getWalletByAddress, ERC721TokenRecordOutDB } from './helpers'
 import type { ERC721TokenDetailed } from '../../../web3/types'
+import type { ERC721TokenRecord } from '../database/types'
+import { EthereumAddress } from 'wallet.ts'
+import { isSameAddress } from '../../../web3/helpers'
+import { queryTransactionPaged } from '../../../database/helpers/pagination'
 
 export async function getERC721Tokens() {
     const t = createTransaction(await createWalletDBAccess(), 'readonly')('ERC721Token', 'Wallet')
     return t.objectStore('ERC721Token').getAll()
+}
+
+const fuse = new Fuse([] as ERC721TokenRecord[], {
+    shouldSort: true,
+    threshold: 0.45,
+    minMatchCharLength: 1,
+    keys: [
+        { name: 'name', weight: 0.8 },
+        { name: 'symbol', weight: 0.2 },
+    ],
+})
+
+export async function getERC721TokensPaged(index: number, count: number, query?: string) {
+    const t = createTransaction(await createWalletDBAccess(), 'readonly')('ERC721Token')
+    return queryTransactionPaged(t, 'ERC721Token', {
+        skip: index * count,
+        count,
+        predicate: (record) => {
+            if (!query) return true
+            if (EthereumAddress.isValid(query) && !isSameAddress(query, record.address)) return false
+            fuse.setCollection([record])
+            return !!fuse.search(query).length
+        },
+    })
 }
 
 export async function addERC721Token(token: ERC721TokenDetailed) {
