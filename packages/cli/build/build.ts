2d33ee80270135415c41d6bf2ebb5a6ec2da21aa
Jack Works <jackworks@protonmail.com>
2021-03-29 14:32:38 +0800

    fix: cli build not working

--- packages/cli/build/build.ts
+++ packages/cli/build/build.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env ts-node
 import { spawn } from 'child_process'
-import { awaitChildProcess, ROOT_PATH } from '../utils'
+import { awaitChildProcess } from '../utils'
 import onMain from './main'
 import { build } from './typescript'
 
@@ -8,7 +8,6 @@ async function main() {
     await build()
     if (process.argv[2] === '--') {
         return spawn(process.argv[3], process.argv.slice(4), {
-            cwd: ROOT_PATH,
             stdio: 'inherit',
             shell: true,
         })
