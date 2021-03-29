ca4ab02f7146ee9db338d901713f46ca614f6a34
Jack Works <jackworks@protonmail.com>
2021-03-29 13:33:25 +0800

    Update packages/maskbook/src/plugins/Valuables/constants.ts
    Co-authored-by: Septs <github@septs.pw>

--- packages/maskbook/src/plugins/Valuables/constants.ts
+++ packages/maskbook/src/plugins/Valuables/constants.ts
@@ -1,3 +1,4 @@
 // proxy: https://v.cent.co/data/tweet-txn
-export const VCENT_BASE_URL = 'https://v.cent.co/data/tweet-txn?tweetID='
+// proxy: https://v.cent.co/data/tweet-txn
+export const VCENT_BASE_URL = 'https://vcent-agent.r2d2.to/data/tweet-txn?tweetID='
 export const VCENT_PLUGIN_ID = 'v.cent.co'
