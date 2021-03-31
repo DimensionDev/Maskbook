07606e81985ecc1291188b71bb4ceaee1094ef4b
Hancheng Zhou <z308114274@gmail.com>
2021-03-31 18:07:16 +0800

    chore: inspect nft post

--- packages/maskbook/src/plugins/NFT/define.tsx
+++ packages/maskbook/src/plugins/NFT/define.tsx
@@ -1,5 +1,6 @@
 import { PluginConfig, PluginStage, PluginScope } from '../types'
 import { getTypedMessageContent } from '../../protocols/typed-message/types'
+import { usePostInfoDetails } from '../../components/DataSource/usePostInfo'
 import { pluginName, identifier } from './constants'
 import NFTInPost from './UI/NFTInPost'
 import { getRelevantUrl } from './utils'
@@ -10,7 +11,16 @@ export const NFTPluginsDefine: PluginConfig = {
     stage: PluginStage.Beta,
     scope: PluginScope.Internal,
     successDecryptionInspector: function Comp(props) {
-        let nftUrl = getRelevantUrl(getTypedMessageContent(props.message))
+        const nftUrl = getRelevantUrl(getTypedMessageContent(props.message))
+
+        return nftUrl ? <NFTInPost nftUrl={nftUrl} /> : null
+    },
+    postInspector: function Component(): JSX.Element | null {
+        const nftUrl = usePostInfoDetails('postMetadataMentionedLinks')
+            .concat(usePostInfoDetails('postMentionedLinks'))
+            .map(getRelevantUrl)
+            .find((url) => Boolean(url))
+
         return nftUrl ? <NFTInPost nftUrl={nftUrl} /> : null
     },
 }
--- packages/maskbook/src/social-network-adaptor/twitter.com/utils/selector.ts
+++ packages/maskbook/src/social-network-adaptor/twitter.com/utils/selector.ts
@@ -95,12 +95,20 @@ export const postsContentSelector = () =>
             '[data-testid="tweet"] > div:last-child div[role="link"] div[lang]', // quoted tweet in timeline page for new twitter
         ].join(),
     ).concat(
-        querySelectorAll('[data-testid="tweet"] > div:last-child').map(
-            (x) =>
-                x.querySelector('[role="group"]')?.parentElement?.querySelector('div[lang]') as
-                    | HTMLDivElement
-                    | undefined,
-        ), // timeline page for new twitter
+        querySelectorAll('[data-testid="tweet"] > div:last-child').map((x) => {
+            const textElement = x.querySelector('[role="group"]')?.parentElement?.querySelector('div[lang]') as
+                | HTMLDivElement
+                | undefined
+
+            if (textElement) return textElement
+
+            // There's no textElement as there's only a twitter summary card parsed by a single url.
+            const summaryCardElement = x
+                .querySelector('[role="group"]')
+                ?.parentElement?.querySelector('[data-testid="card.wrapper"]') as HTMLDivElement | undefined
+
+            return summaryCardElement
+        }), // timeline page for new twitter
     )
 
 const base = querySelector<HTMLScriptElement>('#react-root + script')
