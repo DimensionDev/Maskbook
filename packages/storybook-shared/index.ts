252c5aea958ff457a69492c87661fb8710e62734
Jack Works <jackworks@protonmail.com>
2021-04-01 14:49:37 +0800

    chore: fix ci

--- packages/storybook-shared/index.ts
+++ packages/storybook-shared/index.ts
@@ -1,2 +1,2 @@
 export * from './story-of'
-// test
+// test1
--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xeuo pipefail
 test "$CI" = true || exit 1
-npx pnpm install -r --store=node_modules/.pnpm-store || echo skiping pnpm install
-node ./node_modules/.pnpm/esbuild@*/install.js
+npx pnpm install -r --store=node_modules/.pnpm-store
+node ./node_modules/.pnpm/esbuild@0.9.3/install.js || echo esbuild might failed
 cd packages/cli/build || exit 2
 npx gulp buildNetlify
