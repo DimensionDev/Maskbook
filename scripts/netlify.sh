d45f9d353e84b94d33ab017f5ae95857588b9b1f
Jack Works <jackworks@protonmail.com>
2021-04-01 14:52:57 +0800

    chore: fix ci

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xeuo pipefail
 test "$CI" = true || exit 1
-npx pnpm install -r --store=node_modules/.pnpm-store
-node ./node_modules/.pnpm/esbuild@0.9.3/install.js || echo esbuild might failed
+npx pnpm install -r --store-dir=node_modules/.pnpm-store
+# node ./node_modules/.pnpm/esbuild@0.9.7/install.js || echo esbuild might failed
 cd packages/cli/build || exit 2
 npx gulp buildNetlify
