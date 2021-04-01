0251cb51687b517d4704d92544ef39482a737395
Jack Works <jackworks@protonmail.com>
2021-04-01 14:45:44 +0800

    fix: netlify

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -2,5 +2,6 @@
 set -xeuo pipefail
 test "$CI" = true || exit 1
 npx pnpm install -r --store=node_modules/.pnpm-store || echo skiping pnpm install
+node ./node_modules/.pnpm/esbuild@*/install.js
 cd packages/cli/build || exit 2
 npx gulp buildNetlify
