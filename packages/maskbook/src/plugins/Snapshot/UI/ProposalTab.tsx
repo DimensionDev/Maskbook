8f016cd066a0d2f55b21eb79640bc271506fc43d
guanbinrui <guanbinrui@dimension.im>
2021-04-09 14:38:46 +0800

    chore: results card wip

--- packages/maskbook/src/plugins/Snapshot/UI/ProposalTab.tsx
+++ packages/maskbook/src/plugins/Snapshot/UI/ProposalTab.tsx
@@ -3,13 +3,14 @@ import { VotingCard } from './VotingCard'
 import { SnapshotTab } from './SnapshotTab'
 import { Markdown } from './Markdown'
 import { useProposal } from '../hooks/useProposal'
-import type { ProposalMessage } from '../types'
 import { SnapshotContext } from '../context'
 
 export function ProposalTab() {
     const identifier = useContext(SnapshotContext)
-    const { payload: proposal } = useProposal(identifier.id)
-    const message: ProposalMessage = JSON.parse(proposal.msg)
+    const {
+        payload: { message },
+    } = useProposal(identifier.id)
+
     return (
         <SnapshotTab>
             <Markdown content={message.payload.body} />
--- packages/maskbook/src/plugins/Snapshot/hooks/useResults.ts
+++ packages/maskbook/src/plugins/Snapshot/hooks/useResults.ts
@@ -1,33 +1,50 @@
-import type { ProposalIdentifier, ProposalResult, ProposalMessage, Votes } from '../types'
+import type { ProposalIdentifier, ProposalResult, Votes } from '../types'
 import { useSuspense } from '../../../utils/hooks/useSuspense'
 import { useProposal } from './useProposal'
 import { useVotes } from './useVotes'
 
-const cache = new Map<string, [0, Promise<void>] | [1, ProposalResult] | [2, Error]>()
+const cache = new Map<
+    string,
+    [0, Promise<void>] | [1, { results: ProposalResult[]; totalPower: number }] | [2, Error]
+>()
 export function resultsErrorRetry() {
     cache.forEach(([status], id) => status === 2 && cache.delete(id))
 }
 export function useResults(identifier: ProposalIdentifier) {
-    return useSuspense<ProposalResult, [ProposalIdentifier]>(identifier.id, [identifier], cache, Suspender)
+    return useSuspense<{ results: ProposalResult[]; totalPower: number }, [ProposalIdentifier]>(
+        identifier.id,
+        [identifier],
+        cache,
+        Suspender,
+    )
 }
 
 async function Suspender(identifier: ProposalIdentifier) {
-    const { payload: proposal } = useProposal(identifier.id)
+    const {
+        payload: { message },
+    } = useProposal(identifier.id)
     const { payload: votes } = useVotes(identifier)
-    const message: ProposalMessage = JSON.parse(proposal.msg)
     const strategies = message.payload.metadata.strategies
-    const results: ProposalResult = {
-        voteNumberOfChoices: message.payload.choices.map((_choice, i) => voteForChoice(votes, i).length),
-        powerOfChoices: message.payload.choices.map((_choice, i) =>
-            voteForChoice(votes, i).reduce((a, b) => a + b.balance, 0),
-        ),
-        powerDetailOfChoices: message.payload.choices.map((_choice, i) =>
-            strategies.map((_strategy, sI) => voteForChoice(votes, i).reduce((a, b) => a + b.scores[sI], 0)),
-        ),
-        totalPower: Object.values(votes).reduce((a, b) => a + b.balance, 0),
-    }
+    const voteNumberOfChoices = message.payload.choices.map((_choice, i) => voteForChoice(votes, i).length)
+    const powerOfChoices = message.payload.choices.map((_choice, i) =>
+        voteForChoice(votes, i).reduce((a, b) => a + b.balance, 0),
+    )
+    const powerDetailOfChoices = message.payload.choices.map((_choice, i) =>
+        strategies.map((_strategy, sI) => voteForChoice(votes, i).reduce((a, b) => a + b.scores[sI], 0)),
+    )
+    const totalPower = Object.values(votes).reduce((a, b) => a + b.balance, 0)
 
-    return results
+    const results: ProposalResult[] = powerOfChoices
+        .map((p, i) => ({
+            choice: message.payload.choices[i],
+            power: p,
+            voteNumber: voteNumberOfChoices[i],
+            powerDetail: powerDetailOfChoices[i],
+            percentage: (p / totalPower) * 100,
+        }))
+        .sort((a, b) => b.power - a.power)
+
+    return { results, totalPower }
 }
 
 function voteForChoice(votes: Votes, i: number) {
