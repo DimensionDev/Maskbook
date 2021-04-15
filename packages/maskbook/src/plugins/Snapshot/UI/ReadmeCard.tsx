7639efe8f8266e007a714512cbd6dd02ad842f85
guanbinrui <guanbinrui@dimension.im>
2021-04-16 00:14:41 +0800

    chore(ui): remove readme card

--- packages/maskbook/src/plugins/Snapshot/UI/ReadmeCard.tsx
+++ packages/maskbook/src/plugins/Snapshot/UI/ReadmeCard.tsx
@@ -1,7 +1,5 @@
 import { useContext } from 'react'
 import { createStyles, makeStyles } from '@material-ui/core'
-import { useI18N } from '../../../utils/i18n-next-ui'
-import { SnapshotCard } from './SnapshotCard'
 import { Markdown } from './Markdown'
 import { useProposal } from '../hooks/useProposal'
 import { SnapshotContext } from '../context'
@@ -13,16 +11,10 @@ const useStyles = makeStyles((theme) => {
 export interface ReadMeCardProps {}
 
 export function ReadMeCard(props: ReadMeCardProps) {
-    const { t } = useI18N()
-    const classes = useStyles()
     const identifier = useContext(SnapshotContext)
     const {
         payload: { message },
     } = useProposal(identifier.id)
 
-    return (
-        <SnapshotCard title={t('plugin_snapshot_readme_title')}>
-            <Markdown content={message.payload.body} />
-        </SnapshotCard>
-    )
+    return <Markdown content={message.payload.body} />
 }
