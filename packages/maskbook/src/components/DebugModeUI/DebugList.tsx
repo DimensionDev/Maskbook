30aa613dd952a9c1fecede4beae698be9ab0c38d
Jack Works <jackworks@protonmail.com>
2021-04-16 15:55:42 +0800

    chore: fix key not set jsx warnings

--- packages/maskbook/src/components/DebugModeUI/DebugList.tsx
+++ packages/maskbook/src/components/DebugModeUI/DebugList.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react'
+
 type AcceptableDebuggerType = string | number | undefined | boolean
 function F({ content, hint }: { hint: string; content: AcceptableDebuggerType }) {
     return String(content).length > 50 ? (
@@ -15,7 +17,11 @@ function F({ content, hint }: { hint: string; content: AcceptableDebuggerType })
 export function DebugList(props: { items: readonly (readonly [string, AcceptableDebuggerType] | JSX.Element)[] }) {
     return (
         <div style={{ wordBreak: 'break-all', padding: '0 1em', margin: 0, background: 'black', color: 'white' }}>
-            {props.items.map((x) => (Array.isArray(x) ? <F key={x[0]} hint={x[0]} content={x[1]} /> : x))}
+            {props.items.map((x) => (
+                <Fragment key={x.toString()}>
+                    {Array.isArray(x) ? <F key={x[0]} hint={x[0]} content={x[1]} /> : x}
+                </Fragment>
+            ))}
         </div>
     )
 }
