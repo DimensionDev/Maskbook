39d5a3b9d1dcf1f130593fc0faa4601461c05c31
Jack Works <jackworks@protonmail.com>
2021-04-16 18:22:57 +0800

    fix: netlify close gh-2876

---
+++ packages/icons/utils/ssr.tsx
@@ -0,0 +1,46 @@
+import { IconPreview } from './previewer'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { ServerStyleSheets } from '@material-ui/core'
+import fs from 'fs'
+import { resolve } from 'path'
+
+import * as General from '../general'
+import * as Brands from '../brands'
+
+const CSS = `
+body {
+    font-family: system-ui;
+}
+svg {
+    outline: 1px solid red;
+    margin: 0 6px;
+}
+.black {
+    background: black;
+}
+`
+fs.writeFileSync(resolve(__dirname, '../build.html'), render())
+function render() {
+    const css = new ServerStyleSheets()
+    const html = renderToStaticMarkup(
+        css.collect(
+            <html lang="en">
+                <head>
+                    <meta charSet="UTF-8" />
+                    <title>Icons preview</title>
+                    <style>$$style_here$$</style>
+                    <style>{CSS}</style>
+                </head>
+                <body>
+                    <main id="brands">
+                        <IconPreview icons={Brands} title="Brands" />
+                    </main>
+                    <main id="general">
+                        <IconPreview icons={General} title="General" />
+                    </main>
+                </body>
+            </html>,
+        ),
+    )
+    return '<!DOCTYPE html>' + html.replace('$$style__here$$', css.toString())
+}
