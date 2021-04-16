66274638405dca4e6efe73bfa515e90ab1a940b2
Jack Works <jackworks@protonmail.com>
2021-04-16 11:51:52 +0800

    feat: hmr no longer requires https now

---
+++ packages/maskbook/src/extension/content-script/hmr.ts
@@ -0,0 +1,14 @@
+// If the content script runs in https, webpack will connect https://localhost:HMR_PORT
+if (module.hot) {
+    globalThis.WebSocket = new Proxy(WebSocket, {
+        construct(target, args, newTarget) {
+            args[0] = removeWss(args[0])
+            return Reflect.construct(target, args, newTarget)
+        },
+    })
+}
+function removeWss(x: string) {
+    if (x.startsWith('wss://localhost')) return x.replace(/^wss/, 'ws')
+    return x
+}
+export {}
