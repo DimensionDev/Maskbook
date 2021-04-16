a5c618c34675ae908009f2d6c2f3076d9072fc54
Jack Works <jackworks@protonmail.com>
2021-04-16 15:46:15 +0800

    fix: add migration of missing permission close gh-2680 (#2864)
    * feat: add declarative permission in SNS adaptor
    
    * fix: add migration of missing permission close gh-2680
    
    * fix: popup page doesnot clone declarative permissions
    
    * fix: remove usage to m.twitter.com
    
    * fix: remove usage to www.twitter.com

--- packages/maskbook/src/social-network-adaptor/twitter.com/utils/isMobile.ts
+++ packages/maskbook/src/social-network-adaptor/twitter.com/utils/isMobile.ts
@@ -4,4 +4,4 @@ import { twitterUrl } from './url'
 export const isMobileTwitter = isEnvironment(Environment.ContentScript)
     ? location.hostname === twitterUrl.hostLeadingUrlMobile.substr(8)
     : !!navigator.userAgent.match(/Mobile|mobile/)
-export const twitterDomain = isMobileTwitter ? 'https://m.twitter.com/' : 'https://twitter.com/'
+export const twitterDomain = isMobileTwitter ? 'https://mobile.twitter.com/' : 'https://twitter.com/'
