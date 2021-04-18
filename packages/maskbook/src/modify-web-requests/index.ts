63d6cd9184f7a749617581d9cb9f881c8c3054c8
Septs <github@septs.pw>
2021-04-18 17:38:32 +0800

    chore: update structure

--- packages/maskbook/src/modify-web-requests/index.ts
+++ packages/maskbook/src/modify-web-requests/index.ts
@@ -1,38 +1,25 @@
-const crossDomainHeaders: browser.webRequest.HttpHeaders = [
-    { name: 'access-control-allow-origin', value: '*' },
-    { name: 'access-control-allow-methods', value: 'GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS' },
-    { name: 'access-control-allow-headers', value: '*' },
-    { name: 'access-control-expose-headers', value: '*' },
-]
-
-browser.webRequest.onBeforeSendHeaders.addListener(
-    (event) => {
-        const requestHeaders = event.requestHeaders ?? []
-        const { hostname } = new URL(event.url)
-        if (hostname === 'api-v4.zerion.io') {
-            modifyHeaders(requestHeaders, [{ name: 'origin', value: 'http://mask.io' }])
-        }
-        return
-    },
-    { urls: ['<all_urls>'] },
-    ['blocking', 'requestHeaders'],
-)
+const urls = ['*://gitcoin.co/*', '*://web-api.coinmarketcap.com/*', '*://v.cent.co/*']
 
 browser.webRequest.onHeadersReceived.addListener(
     (event) => {
         const responseHeaders = event.responseHeaders ?? []
-        modifyHeaders(responseHeaders, crossDomainHeaders)
+        modifyHeaders(responseHeaders, [
+            { name: 'access-control-allow-origin', value: '*' },
+            { name: 'access-control-allow-methods', value: 'GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS' },
+            { name: 'access-control-allow-headers', value: '*' },
+            { name: 'access-control-expose-headers', value: '*' },
+        ])
         return { responseHeaders }
     },
-    { urls: ['<all_urls>'] },
+    { urls },
     ['blocking', 'responseHeaders'],
 )
 
 function modifyHeaders(baseHeaders: browser.webRequest.HttpHeaders, newHeaders: browser.webRequest.HttpHeaders) {
     newHeaders.forEach((header) => {
-        const position = baseHeaders.findIndex(({ name }) => name.toLowerCase() === header.name)
-        if (position !== -1) {
-            baseHeaders[position] = header
+        const index = baseHeaders.findIndex(({ name }) => name.toLowerCase() === header.name)
+        if (index !== -1) {
+            baseHeaders[index] = header
         } else {
             baseHeaders.push(header)
         }
