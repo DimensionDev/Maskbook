a566b18cb0a03d6f072f61ae702575d75beae06b
relaxgo <patientzhe@gmail.com>
2021-04-19 14:01:39 +0800

    refactor: split Wallet dialogs

---
+++ packages/maskbook/src/extension/options-page/DashboardDialogs/Wallet/Backup.tsx
@@ -0,0 +1,56 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core'
+import { CreditCard as CreditCardIcon } from 'react-feather'
+import { useAsync } from 'react-use'
+import { WalletRPC } from '../../../../plugins/Wallet/messages'
+import { Flags } from '../../../../utils/flags'
+import { useI18N } from '../../../../utils/i18n-next-ui'
+import ShowcaseBox from '../../DashboardComponents/ShowcaseBox'
+import { DashboardDialogCore, DashboardDialogWrapper, WrappedDialogProps } from '../Base'
+import type { WalletProps } from './types'
+
+const useBackupDialogStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        section: {
+            textAlign: 'left',
+        },
+    }),
+)
+
+export function DashboardWalletBackupDialog(props: WrappedDialogProps<WalletProps>) {
+    const { t } = useI18N()
+    const { wallet } = props.ComponentProps!
+    const classes = useBackupDialogStyles()
+    const { value: privateKeyInHex } = useAsync(async () => {
+        if (!wallet) return
+        const { privateKeyInHex } = wallet._private_key_
+            ? await WalletRPC.recoverWalletFromPrivateKey(wallet._private_key_)
+            : await WalletRPC.recoverWallet(wallet.mnemonic, wallet.passphrase)
+        return privateKeyInHex
+    }, [wallet])
+
+    return (
+        <DashboardDialogCore {...props}>
+            <DashboardDialogWrapper
+                icon={<CreditCardIcon />}
+                iconColor="#4EE0BC"
+                primary={t('backup_wallet')}
+                secondary={t('backup_wallet_hint')}
+                constraintSecondary={false}
+                content={
+                    <>
+                        {Flags.wallet_mnemonic_words_backup_enabled && wallet?.mnemonic.length ? (
+                            <section className={classes.section}>
+                                <ShowcaseBox title={t('mnemonic_words')}>{wallet.mnemonic.join(' ')}</ShowcaseBox>
+                            </section>
+                        ) : null}
+                        {Flags.wallet_private_key_backup_enabled ? (
+                            <section className={classes.section}>
+                                <ShowcaseBox title={t('private_key')}>{privateKeyInHex}</ShowcaseBox>
+                            </section>
+                        ) : null}
+                    </>
+                }
+            />
+        </DashboardDialogCore>
+    )
+}
---
+++ packages/maskbook/src/extension/options-page/DashboardDialogs/Wallet/Error.tsx
@@ -0,0 +1,54 @@
+import { Button } from '@material-ui/core'
+import { Info as InfoIcon } from 'react-feather'
+import { useHistory } from 'react-router-dom'
+import useQueryParams from '../../../../utils/hooks/useQueryParams'
+import { useI18N } from '../../../../utils/i18n-next-ui'
+import { delay } from '../../../../utils/utils'
+import { DashboardRoute } from '../../Route'
+import { DashboardDialogCore, DashboardDialogWrapper, WrappedDialogProps } from '../Base'
+
+export function DashboardWalletErrorDialog(props: WrappedDialogProps<object>) {
+    const { t } = useI18N()
+    const history = useHistory<unknown>()
+    const { error } = useQueryParams(['error'])
+
+    let message = ''
+    switch (error) {
+        case 'nowallet':
+            message = t('error_no_wallet')
+            break
+        case 'Returned error: gas required exceeds allowance (10000000) or always failing transaction':
+            message = t('error_gas_feed_exceeds')
+            break
+        case 'Returned error: insufficient funds for gas * price value':
+            message = t('error_insufficient_balance')
+            break
+        default:
+            message = t('error_unknown')
+            break
+    }
+
+    const onClose = async () => {
+        props.onClose()
+        // prevent UI updating before dialog disappearing
+        await delay(300)
+        history.replace(DashboardRoute.Wallets)
+    }
+
+    return (
+        <DashboardDialogCore {...props} onClose={onClose}>
+            <DashboardDialogWrapper
+                size="small"
+                icon={<InfoIcon />}
+                iconColor="#F4637D"
+                primary={t('error_wallet')}
+                secondary={message}
+                footer={
+                    <Button variant="contained" onClick={onClose}>
+                        {t('ok')}
+                    </Button>
+                }
+            />
+        </DashboardDialogCore>
+    )
+}
