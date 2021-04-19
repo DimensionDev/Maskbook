3c4f9f176bc55ba2df1a969211fc1c549c1c48ae
relaxgo <patientzhe@gmail.com>
2021-04-19 11:49:43 +0800

    fix: remove String.fromCodePoint detect
    Co-authored-by: Jack Works <jackworks@protonmail.com>

--- packages/maskbook/src/plugins/ITO/UI/RegionSelect.tsx
+++ packages/maskbook/src/plugins/ITO/UI/RegionSelect.tsx
@@ -223,7 +223,5 @@ function countryToFlag(isoCode: string) {
     if (!Flags.using_emoji_flag) {
         return ''
     }
-    return typeof String.fromCodePoint !== 'undefined'
-        ? isoCode.toUpperCase().replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + 127397))
-        : isoCode
+    return isoCode.toUpperCase().replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + 127397))
 }
