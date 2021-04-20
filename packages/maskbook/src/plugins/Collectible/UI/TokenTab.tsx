bd926608dd4241483f8f3fd0cc7736bb7168f943
guanbinrui <guanbinrui@dimension.im>
2021-04-20 12:07:57 +0800

    chore(ui): adjust token tab

--- packages/maskbook/src/plugins/Collectible/UI/TokenTab.tsx
+++ packages/maskbook/src/plugins/Collectible/UI/TokenTab.tsx
@@ -1,4 +1,4 @@
-import { Box, createStyles, Link, makeStyles, Typography } from '@material-ui/core'
+import { Box, Paper, createStyles, Link, makeStyles, Typography } from '@material-ui/core'
 import { CollectibleTab } from './CollectibleTab'
 import { CollectibleState } from '../hooks/useCollectibleState'
 import { useI18N } from '../../../utils/i18n-next-ui'
@@ -8,8 +8,12 @@ import { ChainId } from '../../../web3/types'
 
 const useStyles = makeStyles((theme) => {
     return createStyles({
+        content: {
+            paddingTop: 0,
+            paddingBottom: '0 !important',
+        },
         container: {
-            padding: theme.spacing(2),
+            padding: theme.spacing(1),
         },
         description: {
             fontSize: 14,
@@ -27,11 +31,14 @@ const useStyles = makeStyles((theme) => {
             alignItems: 'center',
             fontSize: 14,
             fontWeight: 600,
-            // color: '#15181B',
         },
         chain_row: {
             display: 'flex',
             justifyContent: 'space-between',
+            marginBottom: theme.spacing(0.5),
+            '&:last-child': {
+                marginBottom: 0,
+            },
         },
         tokenId: {
             maxWidth: 112,
@@ -51,41 +58,47 @@ export function TokenTab(props: TokenTabProps) {
 
     if (!asset.value) return null
     return (
-        <CollectibleTab>
+        <CollectibleTab classes={{ content: classes.content }}>
             <Box className={classes.container}>
-                <Typography variant="h6">{t('plugin_collectible_base')}</Typography>
+                <Typography variant="body1" sx={{ marginBottom: 1 }}>
+                    {t('plugin_collectible_base')}
+                </Typography>
 
-                <Typography>
-                    {asset.value.creator ? (
-                        <Typography variant="subtitle2">
-                            {t('plugin_collectible_create_by')}{' '}
-                            <Link href={asset.value.creator.link} target="_blank" rel="noopener noreferrer">
-                                {asset.value.creator.user?.username ?? asset.value.creator.address?.slice(2, 8)}
-                            </Link>
-                        </Typography>
-                    ) : asset.value.owner ? (
-                        <Typography variant="subtitle2">
-                            {t('plugin_collectible_owned_by')}{' '}
-                            <Link href={asset.value.owner.link} target="_blank" rel="noopener noreferrer">
-                                {asset.value.owner?.user?.username ?? asset.value.owner?.address?.slice(2, 8) ?? ''}
-                            </Link>
-                        </Typography>
-                    ) : null}
-                    <Typography className={classes.description}>{asset.value.description}</Typography>
+                {asset.value.creator ? (
+                    <Typography variant="body2">
+                        {t('plugin_collectible_create_by')}{' '}
+                        <Link href={asset.value.creator.link} target="_blank" rel="noopener noreferrer">
+                            {asset.value.creator.user?.username ?? asset.value.creator.address?.slice(2, 8)}
+                        </Link>
+                    </Typography>
+                ) : asset.value.owner ? (
+                    <Typography variant="body2">
+                        {t('plugin_collectible_owned_by')}{' '}
+                        <Link href={asset.value.owner.link} target="_blank" rel="noopener noreferrer">
+                            {asset.value.owner?.user?.username ?? asset.value.owner?.address?.slice(2, 8) ?? ''}
+                        </Link>
+                    </Typography>
+                ) : null}
+                <Typography className={classes.description} variant="body2">
+                    {asset.value.description}
                 </Typography>
             </Box>
 
             {asset.value.traits && asset.value.traits.length ? (
                 <Box className={classes.container}>
-                    <Typography variant="h6">{t('plugin_collectible_properties')}</Typography>
+                    <Typography variant="body1" sx={{ marginBottom: 1 }}>
+                        {t('plugin_collectible_properties')}
+                    </Typography>
 
                     <Box className={classes.trait_content}>
                         {asset.value.traits.map(({ trait_type, value }) => {
                             return (
-                                <Box className={classes.trait} key={trait_type + value}>
-                                    <Typography sx={{ color: '#1C68F3' }}>{trait_type}</Typography>
-                                    <Typography>{value}</Typography>
-                                </Box>
+                                <Paper className={classes.trait} key={trait_type + value} variant="outlined">
+                                    <Typography variant="body2" color="primary">
+                                        {trait_type}
+                                    </Typography>
+                                    <Typography variant="body2">{value}</Typography>
+                                </Paper>
                             )
                         })}
                     </Box>
@@ -94,33 +107,40 @@ export function TokenTab(props: TokenTabProps) {
 
             {asset.value.assetContract.name && asset.value.assetContract?.description ? (
                 <Box className={classes.container}>
-                    <Typography variant="h6">
+                    <Typography variant="body1" sx={{ marginBottom: 1 }}>
                         {t('plugin_collectible_about')} {asset.value.assetContract.name}
                     </Typography>
-
-                    <Typography className={classes.description}>{asset.value.assetContract?.description}</Typography>
+                    <Typography className={classes.description} variant="body2">
+                        {asset.value.assetContract?.description}
+                    </Typography>
                 </Box>
             ) : null}
 
             <Box className={classes.container}>
-                <Typography variant="h6">{t('plugin_collectible_chain_info')}</Typography>
+                <Typography variant="body1" sx={{ marginBottom: 1 }}>
+                    {t('plugin_collectible_chain_info')}
+                </Typography>
 
                 <Box className={classes.chain_row}>
-                    <Typography>{t('plugin_collectible_contract_address')}</Typography>
+                    <Typography variant="body2">{t('plugin_collectible_contract_address')}</Typography>
                     <Link
                         href={resolveAddressLinkOnEtherscan(ChainId.Mainnet, token?.contractAddress ?? '')}
                         target="_blank"
                         rel="noopener noreferrer">
-                        <Typography>{formatEthereumAddress(token?.contractAddress ?? '', 4)}</Typography>
+                        <Typography variant="body2">
+                            {formatEthereumAddress(token?.contractAddress ?? '', 4)}
+                        </Typography>
                     </Link>
                 </Box>
                 <Box className={classes.chain_row}>
-                    <Typography>{t('plugin_collectible_token_id')}</Typography>
-                    <Typography className={classes.tokenId}>{token?.tokenId}</Typography>
+                    <Typography variant="body2">{t('plugin_collectible_token_id')}</Typography>
+                    <Typography className={classes.tokenId} variant="body2">
+                        {token?.tokenId}
+                    </Typography>
                 </Box>
                 <Box className={classes.chain_row}>
-                    <Typography>{t('plugin_collectible_block_chain')}</Typography>
-                    <Typography>Ethereum</Typography>
+                    <Typography variant="body2">{t('plugin_collectible_block_chain')}</Typography>
+                    <Typography variant="body2">Ethereum</Typography>
                 </Box>
             </Box>
         </CollectibleTab>
