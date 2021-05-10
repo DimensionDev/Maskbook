4fdbfa665fb724574b65c48342a20a893c81ac30
cmy <nuanyang233@gmail.com>
2021-05-10 19:27:46 +0800

    fix: useMenu anchorEl

--- packages/shared/src/hooks/useMenu.tsx
+++ packages/shared/src/hooks/useMenu.tsx
@@ -11,7 +11,11 @@ export const useMenuContext = createContext<React.ComponentType<MenuProps>>(Menu
 // ! Do not change signature of this. Change useMenuConfig instead.
 export function useMenu(
     ...elements: Array<JSX.Element | null>
-): [menu: React.ReactElement, openDialog: () => void, closeDialog: () => void] {
+): [
+    menu: React.ReactElement,
+    openDialog: (anchorElOrEvent: HTMLElement | SyntheticEvent<HTMLElement>) => void,
+    closeDialog: () => void,
+] {
     return useMenuConfig(elements, {})
 }
 
@@ -21,15 +25,25 @@ export function useMenuConfig(
     config: useMenuConfig,
 ): [
     menu: React.ReactElement,
-    openDialog: (anchorElOrEvent?: HTMLElement | SyntheticEvent<HTMLElement>) => void,
+    openDialog: (anchorElOrEvent: HTMLElement | SyntheticEvent<HTMLElement>) => void,
     closeDialog: () => void,
 ] {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>()
     const [status, setOpen] = useState(false)
-    const open = useCallback(() => setOpen(true), [])
+    const open = useCallback((anchorElOrEvent: HTMLElement | SyntheticEvent<HTMLElement>) => {
+        let element: HTMLElement
+        if (anchorElOrEvent instanceof HTMLElement) {
+            element = anchorElOrEvent
+        } else {
+            element = anchorElOrEvent.currentTarget
+        }
+        setAnchorEl(element)
+        setOpen(true)
+    }, [])
     const close = useCallback(() => setOpen(false), [])
     const Menu = useContext(useMenuContext)
     return [
-        <Menu open={status} onClose={close} onClick={close}>
+        <Menu open={status} onClose={close} onClick={close} anchorEl={anchorEl}>
             {elements?.map((element, key) =>
                 isValidElement<object>(element) ? cloneElement(element, { ...element.props, key }) : element,
             )}
