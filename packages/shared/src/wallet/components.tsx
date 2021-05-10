dd8f32c438f349b8f2a510896bb31a2008d1a13d
guanbinrui <guanbinrui@dimension.im>
2021-05-10 18:52:59 +0800

    refactor: remove format token

--- packages/shared/src/wallet/components.tsx
+++ packages/shared/src/wallet/components.tsx
@@ -1,6 +1,6 @@
 import type { BigNumber } from 'bignumber.js'
 import { FC, Fragment } from 'react'
-import { formatBalance, formatCurrency, formatToken, formatEthereumAddress } from './formatter'
+import { formatBalance, formatCurrency, formatEthereumAddress } from './formatter'
 
 export interface FormattedBalanceProps {
     value: BigNumber.Value | undefined
@@ -10,47 +10,29 @@ export interface FormattedBalanceProps {
 }
 
 export const FormattedBalance: FC<FormattedBalanceProps> = (props) => {
-    let formatted = formatBalance(props.value, props.decimals, props.significant)
-    if (props.symbol) {
-        formatted += ` ${props.symbol}`
-    }
+    const formatted = formatBalance(props.value, props.decimals, props.significant)
+    if (props.symbol) return <Fragment>{`${formatted} ${props.symbol}`}</Fragment>
     return <Fragment>{formatted}</Fragment>
 }
 
 export interface FormattedCurrencyProps {
-    type: 'currency'
-    value: BigNumber.Value
     sign?: string
     symbol?: string
-}
-
-export interface FormattedTokenProps {
-    type: 'token'
     value: BigNumber.Value
-    symbol?: string
 }
 
-export const FormattedCurrency: FC<FormattedCurrencyProps | FormattedTokenProps> = (props) => {
-    let formatted: string
-    if (props.type === 'currency') {
-        formatted = formatCurrency(props.value, props.sign)
-    } else if (props.type === 'token') {
-        formatted = formatToken(props.value)
-    } else {
-        throw new Error('unsupported currency type')
-    }
-    if (props.symbol) {
-        formatted += ` ${props.symbol}`
-    }
+export const FormattedCurrency: FC<FormattedCurrencyProps> = (props) => {
+    const formatted = formatCurrency(props.value, props.sign)
+    if (props.symbol) return <Fragment>{`${formatted} ${props.symbol}`}</Fragment>
     return <Fragment>{formatted}</Fragment>
 }
 
 interface FormattedEthereumAddressProps {
-    type: 'ethereum'
     address: string
     size?: number
 }
 
 export const FormattedAddress: FC<FormattedEthereumAddressProps> = (props) => {
-    return <Fragment>{formatEthereumAddress(props.address, props.size)}</Fragment>
+    const formatted = formatEthereumAddress(props.address, props.size)
+    return <Fragment>{formatted}</Fragment>
 }
