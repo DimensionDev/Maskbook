dce18f1d7bfdfb78ef60cfdeebcfcc40638398f5
cmy <nuanyang233@gmail.com>
2021-05-10 11:10:54 +0800

    chore: prettier

--- packages/dashboard/src/pages/Personas/api.ts
+++ packages/dashboard/src/pages/Personas/api.ts
@@ -1,11 +1,10 @@
 import { createGlobalState } from '@dimensiondev/maskbook-shared'
 import { Services, Messages } from '../../API'
 
-export const [
-    useDefinedSocialNetworkUIs,
-    revalidateSocialNetworkUIs,
-    definedSocialNetworkUIs,
-] = createGlobalState(Services.SocialNetwork.getDefinedSocialNetworkUIs, () => () => {})
+export const [useDefinedSocialNetworkUIs, revalidateSocialNetworkUIs, definedSocialNetworkUIs] = createGlobalState(
+    Services.SocialNetwork.getDefinedSocialNetworkUIs,
+    () => () => {},
+)
 
 export const [useMyPersonas, revalidateMyPersonas, myPersonas] = createGlobalState(
     Services.Identity.queryMyPersonas,
