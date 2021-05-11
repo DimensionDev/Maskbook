4af36090fd65c6c8f69e135c45b21d84cdcff5ce
Jack Works <jackworks@protonmail.com>
2021-05-11 13:27:50 +0800

    feat: add 6 new icons in the icons package

---
+++ packages/icons/general/ArrowDownRound.tsx
@@ -0,0 +1,8 @@
+import { createIcon } from '../utils'
+export const ArrowDownRound = createIcon(
+    'ArrowDown',
+    <>
+        <path d="M10 11.6l4 4 4-4" stroke="inherit" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+    </>,
+    '0 0 28 28',
+)
---
+++ packages/icons/general/Settings.tsx
@@ -0,0 +1,9 @@
+import { createIcon } from '../utils'
+
+export const SettingsIcon = createIcon(
+    'Settings',
+    <>
+        <path d="M12 23l-9.5-5.5v-11L12 1l9.5 5.5v11L12 23zm0-19.688L4.5 7.653v8.694l7.5 4.342 7.5-4.342V7.653L12 3.311v.001zM12 16a4 4 0 112.828-1.172A4.027 4.027 0 0112 16zm0-6a2 2 0 10-.001 4A2 2 0 0012 10z" />
+    </>,
+    '0 0 24 24',
+)
