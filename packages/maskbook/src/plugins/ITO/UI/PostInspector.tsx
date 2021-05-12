2b695c32d0968906801a09e7fc57de7ec2870163
guanbinrui <guanbinrui@dimension.im>
2021-05-13 01:25:42 +0800

    fix: post inspector

--- packages/maskbook/src/plugins/ITO/UI/PostInspector.tsx
+++ packages/maskbook/src/plugins/ITO/UI/PostInspector.tsx
@@ -1,3 +1,4 @@
+import { useChainId } from '../../../web3/hooks/useBlockNumber'
 import { EthereumChainBoundary } from '../../../web3/UI/EthereumChainBoundary'
 import { isCompactPayload } from '../helpers'
 import { usePoolPayload } from '../hooks/usePoolPayload'
@@ -11,14 +12,21 @@ export interface PostInspectorProps {
 export function PostInspector(props: PostInspectorProps) {
     const { chain_id, pid } = props.payload
     const isCompactPayload_ = isCompactPayload(props.payload)
-    const { value: payload, error, loading, retry } = usePoolPayload(isCompactPayload_ ? pid : '')
 
-    const Content = () => {
+    const chainId = useChainId()
+    const {
+        value: payload,
+        error,
+        loading,
+        retry,
+    } = usePoolPayload(isCompactPayload_ && chainId === chain_id ? pid : '')
+
+    const renderITO = () => {
         if (isCompactPayload_) {
             if (loading) return <ITO_Loading />
             if (error) return <ITO_Error retryPoolPayload={retry} />
         }
         return <ITO pid={pid} payload={payload ?? props.payload} />
     }
-    return <EthereumChainBoundary chainId={chain_id}>{Content}</EthereumChainBoundary>
+    return <EthereumChainBoundary chainId={chain_id}>{renderITO()}</EthereumChainBoundary>
 }
