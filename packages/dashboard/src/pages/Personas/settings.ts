31901ba57bb92405d32fb744decc6f6c444a885e
cmy <nuanyang233@gmail.com>
2021-05-12 13:57:16 +0800

    chore: add todo

--- packages/dashboard/src/pages/Personas/settings.ts
+++ packages/dashboard/src/pages/Personas/settings.ts
@@ -1,3 +1,4 @@
+//TODO: replace to new settings util
 import { createInternalSettings } from '../../../../maskbook/src/settings/createSettings'
 
 export const currentPersonaIdentifier = createInternalSettings<string>('currentPersonaIdentifier', '')
