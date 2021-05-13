538f7a39dd0e6eef30ad0612b961b4470bb0e8c8
Jack Works <jackworks@protonmail.com>
2021-05-13 13:36:38 +0800

    chore: change queryOwnedPersonaInformation

--- packages/shared/src/Persona/type.ts
+++ packages/shared/src/Persona/type.ts
@@ -1,4 +1,4 @@
-import type { PersonaIdentifier, ProfileIdentifier, ReadonlyIdentifierMap } from '..'
+import type { PersonaIdentifier, ProfileIdentifier } from '..'
 
 /**
  * This interface contains the minimal information for UI display
@@ -7,9 +7,10 @@ export interface PersonaInformation {
     /** The nickname of the persona. Should use profile.nickname if it presents. */
     nickname?: string
     identifier: PersonaIdentifier
-    linkedProfiles: ReadonlyIdentifierMap<ProfileIdentifier, ProfileInformation>
+    linkedProfiles: ProfileInformation[]
 }
 export interface ProfileInformation {
     /** The nickname of the profile. Should be used in prior. */
     nickname?: string
+    identifier: ProfileIdentifier
 }
