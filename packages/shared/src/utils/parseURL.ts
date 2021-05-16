f781141f0127b6e9043be30ea9874df99ddb5de6
unclebill <unclebill@mindhand.io>
2021-05-16 11:35:27 +0800

    fix: some invalid url wrongly detected by anchorme
    url like `https://…a.com`. see alexcorvi/anchorme.js/issues/109
    
    closes #3185

--- packages/shared/src/utils/parseURL.ts
+++ packages/shared/src/utils/parseURL.ts
@@ -1,4 +1,14 @@
 import parser from 'anchorme'
-export function parseURL(string: string) {
-    return parser.list(string).map((x) => x.string)
+export function parseURL(text: string) {
+    return parser
+        .list(text)
+        .map((x) => x.string)
+        .filter((y) => {
+            try {
+                new URL(y)
+                return true
+            } catch {
+                return false
+            }
+        })
 }
