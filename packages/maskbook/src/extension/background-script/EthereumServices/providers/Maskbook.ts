e50b8ebc67d6fdcf409f83e15bc74e1337289509
guanbinrui <guanbinrui@dimension.im>
2021-05-17 11:28:24 +0800

    refactor: seed

--- packages/maskbook/src/extension/background-script/EthereumServices/providers/Maskbook.ts
+++ packages/maskbook/src/extension/background-script/EthereumServices/providers/Maskbook.ts
@@ -5,9 +5,6 @@ import { getConstant } from '../../../../web3/helpers'
 import { CONSTANTS } from '../../../../web3/constants'
 import type { ChainId } from '../../../../web3/types'
 
-// 5 is the length of weights
-const SEED = Math.floor(Math.random() * 4)
-
 //#region providers
 const providerPool = new Map<string, HttpProvider>()
 
@@ -53,7 +50,8 @@ export function createWeb3({
     if (!url) {
         const urls = getConstant(CONSTANTS, 'PROVIDER_ADDRESS_LIST', chainId)
         const weights = getConstant(CONSTANTS, 'PROVIDER_WEIGHT_LIST', chainId)
-        url = urls[weights[SEED]]
+        const seed = getConstant(CONSTANTS, 'PROVIDER_WEIGHT_SEED', chainId)
+        url = urls[weights[seed]]
     }
     const provider = createProvider(url)
     const web3 = createWeb3Instance(provider)
