d60faba32fd714d123beb5af0c90561dd1dd01a1
Jack Works <jackworks@protonmail.com>
2021-05-22 15:05:39 +0800

    fix: wrong web3 context position

--- packages/maskbook/src/UIRoot.tsx
+++ packages/maskbook/src/UIRoot.tsx
@@ -13,15 +13,17 @@ import { Web3Context } from './web3/context'
 export function MaskUIRootWithinShadow(JSX: JSX.Element) {
     return (
         <StrictMode>
-            <I18nextProvider i18n={i18nNextInstance}>
-                <ErrorBoundaryBuildInfoContext.Provider value={buildInfoMarkdown}>
-                    <ErrorBoundary>
-                        <SnackbarProvider maxSnack={30} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
-                            <StrictMode>{JSX}</StrictMode>
-                        </SnackbarProvider>
-                    </ErrorBoundary>
-                </ErrorBoundaryBuildInfoContext.Provider>
-            </I18nextProvider>
+            <Web3Provider value={Web3Context}>
+                <I18nextProvider i18n={i18nNextInstance}>
+                    <ErrorBoundaryBuildInfoContext.Provider value={buildInfoMarkdown}>
+                        <ErrorBoundary>
+                            <SnackbarProvider maxSnack={30} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+                                <StrictMode>{JSX}</StrictMode>
+                            </SnackbarProvider>
+                        </ErrorBoundary>
+                    </ErrorBoundaryBuildInfoContext.Provider>
+                </I18nextProvider>
+            </Web3Provider>
         </StrictMode>
     )
 }
@@ -33,7 +35,7 @@ export function MaskUIRoot(JSX: JSX.Element) {
             <StylesProvider>
                 <ThemeProvider theme={useClassicMaskTheme()}>
                     <CssBaseline />
-                    <Web3Provider value={Web3Context}>{JSX}</Web3Provider>
+                    {JSX}
                 </ThemeProvider>
             </StylesProvider>
         </StyledEngineProvider>,
