4164db12dcb2b5acf6dc4e8ecda1d88132179bef
Manishoo <maanishooshtari@gmail.com>
2021-05-26 16:58:16 +0430

    fix: handle being undefined if extension loads faster than site

--- packages/maskbook/src/social-network-adaptor/minds.com/collecting/identity.ts
+++ packages/maskbook/src/social-network-adaptor/minds.com/collecting/identity.ts
@@ -1,14 +1,15 @@
 import { ProfileIdentifier } from '../../../database/type'
 import { creator, SocialNetworkUI as Next } from '../../../social-network'
-import { untilDocumentReady } from '../../../utils/dom'
+import { untilDocumentReady, untilElementAvailable } from '../../../utils/dom'
 import { mindsBase } from '../base'
-import { selfInfoSelectors } from '../utils/selector'
+import { handleSelector, selfInfoSelectors } from '../utils/selector'
 
 async function resolveLastRecognizedIdentityInner(
     ref: Next.CollectingCapabilities.IdentityResolveProvider['lastRecognized'],
     cancel: AbortSignal,
 ) {
     await untilDocumentReady()
+    await untilElementAvailable(handleSelector())
     const handle = selfInfoSelectors().handle.evaluate()
     const avatar = selfInfoSelectors().avatar.evaluate()
 
@@ -19,18 +20,18 @@ async function resolveLastRecognizedIdentityInner(
             nickname: undefined,
             avatar,
         }
-    }
 
-    // call the API to get the nickname
-    fetch('/api/v1/channel/' + handle, { signal: cancel })
-        .then((res) => res.json())
-        .then(({ channel }) => {
-            ref.value = {
-                identifier: new ProfileIdentifier(mindsBase.networkIdentifier, channel.username),
-                nickname: channel.name,
-                avatar: channel.avatar_url?.medium,
-            }
-        })
+        // call the API to get the nickname
+        fetch('/api/v1/channel/' + handle, { signal: cancel })
+          .then((res) => res.json())
+          .then(({ channel }) => {
+              ref.value = {
+                  identifier: new ProfileIdentifier(mindsBase.networkIdentifier, channel.username),
+                  nickname: channel.name,
+                  avatar: channel.avatar_url?.medium,
+              }
+          })
+    }
 }
 
 export const IdentityProviderMinds: Next.CollectingCapabilities.IdentityResolveProvider = {
--- packages/maskbook/src/social-network-adaptor/minds.com/utils/selector.ts
+++ packages/maskbook/src/social-network-adaptor/minds.com/utils/selector.ts
@@ -38,8 +38,10 @@ export const postEditorDraftContentSelector = () => {
     return querySelector<HTMLElement>('m-composer__modal m-composer__textarea textarea.m-composerTextarea__message')
 }
 
+export const handleSelector = () => querySelector<HTMLScriptElement>('.m-user-menu ul li a:first-child')
+
 export const selfInfoSelectors = () => ({
-    handle: querySelector<HTMLScriptElement>('.m-user-menu ul li a:first-child').map((x) =>
+    handle: handleSelector().map((x) =>
         x.innerText.replace(/@/, '').trim(),
     ),
     avatar: querySelector<HTMLScriptElement>('.m-user-menu .minds-avatar').map((x) =>
