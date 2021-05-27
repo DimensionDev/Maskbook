c310ee6ed871224d786c334f6c7589fdfd68fb43
Jack Works <jackworks@protonmail.com>
2021-05-27 13:43:50 +0800

    Revert "fix: ci"
    This reverts commit 1550d14e2282059ef2c1835607ac4c0344ad6397.

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -xeuo pipefail
 test "$CI" = true || exit 1
-sudo npm i -g typescript
 npx pnpm install -r --store-dir=node_modules/.pnpm-store
 # Hard coded...
 node ./node_modules/.pnpm/esbuild@0.9.7/node_modules/esbuild/install.js
