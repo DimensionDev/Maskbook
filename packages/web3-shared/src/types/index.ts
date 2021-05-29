fb114d22a407157d045f2034db38c37a920557c5
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2021-05-29 15:41:16 +0800

    refactor: update comment
    Co-authored-by: Jack Works <jackworks@protonmail.com>

--- packages/web3-shared/src/types/index.ts
+++ packages/web3-shared/src/types/index.ts
@@ -163,8 +163,9 @@ export type EthereumTokenDetailedType<T extends EthereumTokenType> = TokenDetail
 export type TokenAssetDetailedType<T extends EthereumTokenType.ERC721 | EthereumTokenType.ERC1155> =
     TokenAssetDetailedMap[T]
 
-// Please don't use this enum but use ChainId instead
-// this exists for back backward compatible
+/**
+ * @deprecated Please don't use this enum but use ChainId instead this exists for back backward compatible
+ */
 export enum EthereumNetwork {
     Mainnet = 'Mainnet',
     Ropsten = 'Ropsten',
