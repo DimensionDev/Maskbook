cf44976209c07ac3f16ac1c56ddc68b2ee645f0c
guanbinrui <guanbinrui@dimension.im>
2021-06-04 14:04:39 +0800

    chore: trader only available on ethereum

--- packages/maskbook/src/plugins/Trader/apis/trader/index.ts
+++ packages/maskbook/src/plugins/Trader/apis/trader/index.ts
@@ -1,11 +1,16 @@
+import { getChainDetailed } from '@dimensiondev/web3-shared'
 import { TagType, TradeProvider } from '../../types'
 
 export async function getAvailableTraderProviders(type: TagType, keyword: string) {
-    return [
-        TradeProvider.UNISWAP,
-        TradeProvider.SUSHISWAP,
-        TradeProvider.ZRX,
-        TradeProvider.BALANCER,
-        TradeProvider.SASHIMISWAP,
-    ]
+    const chainDetailed = getChainDetailed()
+
+    if (chainDetailed?.chain === 'ETH')
+        return [
+            TradeProvider.UNISWAP,
+            TradeProvider.SUSHISWAP,
+            TradeProvider.ZRX,
+            TradeProvider.BALANCER,
+            TradeProvider.SASHIMISWAP,
+        ]
+    return []
 }
