77c91b6ba8567fbb3738ada97c49a601fd4062e0
unclebill <billbill290@gmail.com>
2021-06-04 12:18:46 +0800

    fix: locale-kit should only resolve directory as locale name

--- packages/cli/locale-kit/utils.ts
+++ packages/cli/locale-kit/utils.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-imports */
-import { promises as fs, readdirSync } from 'fs'
+import { promises as fs, readdirSync, lstatSync } from 'fs'
 import { difference, keys, uniq, without } from 'lodash'
 import { resolve, relative } from 'path'
 import { getUsedKeys } from './ast'
@@ -7,7 +7,10 @@ import { ROOT_PATH, PKG_PATH } from '../utils'
 
 const SOURCE_PATH = resolve(PKG_PATH, 'maskbook', 'src')
 export const LOCALE_PATH = resolve(SOURCE_PATH, '_locales')
-export const LOCALE_NAMES = readdirSync(LOCALE_PATH)
+export const LOCALE_NAMES = readdirSync(LOCALE_PATH).filter((locale) => {
+    const fpath = resolve(LOCALE_PATH, locale)
+    return lstatSync(fpath).isDirectory()
+})
 
 export function getMessagePath(name: string) {
     return resolve(LOCALE_PATH, name, 'messages.json')
