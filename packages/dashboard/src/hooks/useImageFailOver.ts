9b6868d22a2dd31ba608d02e99cea3080bffc7d6
cmy <nuanyang233@gmail.com>
2021-06-12 21:04:18 +0800

    chore: add comment for useImageFailOver

--- packages/dashboard/src/hooks/useImageFailOver.ts
+++ packages/dashboard/src/hooks/useImageFailOver.ts
@@ -1,6 +1,11 @@
 import { useAsync } from 'react-use'
 import { findAvailableImageURL } from '@dimensiondev/kit'
 
+/**
+ * Request images from different sources and get the image's url of the loaded
+ * @param urls the url of different image sources
+ * @param suffix the image file path suffix
+ */
 export function useImageFailOver(urls: readonly string[], suffix: string) {
     return useAsync(async () => {
         try {
