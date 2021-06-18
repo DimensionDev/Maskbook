a24922b586d75b9eca2d85a4ac12b8d87521eec7
guanbinrui <guanbinrui@dimension.im>
2021-06-18 17:05:07 +0800

    fix: ts error

--- packages/maskbook/src/plugins/RedPacket/UI/RedPacket.tsx
+++ packages/maskbook/src/plugins/RedPacket/UI/RedPacket.tsx
@@ -281,7 +281,7 @@ export function RedPacket(props: RedPacketProps) {
                                 })
                             if (listOfStatus.includes(RedPacketStatus.claimed))
                                 return t('plugin_red_packet_description_claimed', {
-                                    amount: formatBalance(availability.claimed_amount, tokenDetailed.decimals),
+                                    amount: formatBalance(availability.claimed, tokenDetailed.decimals),
                                     symbol: tokenDetailed.symbol,
                                 })
                             if (listOfStatus.includes(RedPacketStatus.refunded))
