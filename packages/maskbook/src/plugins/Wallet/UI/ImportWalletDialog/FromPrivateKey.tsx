8f05dc7acb3999f81b957963bbe4813a061c8429
unclebill <billbill290@gmail.com>
2021-06-20 15:37:51 +0800

    fixup! fixup! fix: follow up reviews

--- packages/maskbook/src/plugins/Wallet/UI/ImportWalletDialog/FromPrivateKey.tsx
+++ packages/maskbook/src/plugins/Wallet/UI/ImportWalletDialog/FromPrivateKey.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, FC, useCallback, useEffect, useState } from 'react'
+import { ChangeEvent, FC, useCallback, useState } from 'react'
 import { Box, TextField, makeStyles } from '@material-ui/core'
-import { debounce } from 'lodash-es'
+import { useDebounce } from 'react-use'
 import { useI18N } from '../../../../utils'
 import { WalletRPC } from '../../messages'
 
@@ -28,34 +28,29 @@ export const FromPrivateKey: FC<FromPrivateKeyProps> = ({ onRecover }) => {
     const classes = useStyles()
     const { t } = useI18N()
     const [valid, setValid] = useState(true)
-    const [recovering, setRecovering] = useState(false)
     const [privateKey, setPrivateKey] = useState('')
     const handleChange = useCallback((evt: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         const text = evt.target.value
         setPrivateKey(text)
     }, [])
-    useEffect(() => {
-        debounce(() => {
+    useDebounce(
+        () => {
             if (!privateKey) {
-                setRecovering(false)
                 setValid(true)
                 return
             }
-            setRecovering(true)
-            WalletRPC.recoverWalletFromPrivateKey(privateKey)
-                .then(({ address, privateKeyValid }) => {
-                    setValid(privateKeyValid)
-                    onRecover({
-                        address,
-                        privateKey,
-                        privateKeyValid,
-                    })
+            WalletRPC.recoverWalletFromPrivateKey(privateKey).then(({ address, privateKeyValid }) => {
+                setValid(privateKeyValid)
+                onRecover({
+                    address,
+                    privateKey,
+                    privateKeyValid,
                 })
-                .finally(() => {
-                    setRecovering(false)
-                })
-        }, 500)()
-    }, [privateKey])
+            })
+        },
+        500,
+        [privateKey],
+    )
 
     return (
         <Box>
