2f6b2ebd6985559ec7d13da23b654c0ab31e6bc1
Jack Works <jackworks@protonmail.com>
2021-06-22 12:04:25 +0800

    feat: add cleanup

--- packages/maskbook/src/settings/listener.ts
+++ packages/maskbook/src/settings/listener.ts
@@ -1,5 +1,5 @@
 import { Environment, isEnvironment, ValueRef } from '@dimensiondev/holoflows-kit'
-import { MaskMessage, sideEffect } from '../utils'
+import { MaskMessage, sideEffect, startEffect } from '../utils'
 import { allPostReplacementSettings, appearanceSettings, languageSettings, debugModeSetting } from './settings'
 
 export function ToBeListened() {
@@ -15,11 +15,17 @@ type SettingsEventName = ReturnType<typeof ToBeListened>
 export type SettingsEvents = {
     [key in keyof SettingsEventName]: SettingsEventName[key] extends ValueRef<infer T> ? T : void
 }
-sideEffect.then(() => {
-    if (!isEnvironment(Environment.ManifestBackground)) return
-    const obj = ToBeListened()
-    for (const _key in obj) {
-        const key = _key as keyof SettingsEventName
-        obj[key].addListener((data) => MaskMessage.events[key].sendToAll(data as never))
-    }
+startEffect(import.meta.webpackHot, (abort) => {
+    sideEffect.then(() => {
+        if (!isEnvironment(Environment.ManifestBackground)) return
+        const obj = ToBeListened()
+        for (const _key in obj) {
+            const key = _key as keyof SettingsEventName
+            abort.signal.addEventListener(
+                'abort',
+                obj[key].addListener((data) => MaskMessage.events[key].sendToAll(data as never)),
+            )
+        }
+    })
+    return () => {}
 })
