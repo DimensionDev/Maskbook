1db0fcf3d3dbb29ebec963fd2dcb052e93f7678e
Septs <github@septs.pw>
2021-06-22 12:13:03 +0800

    chore: done poc

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -1,15 +1,26 @@
-import { ChainId } from '@dimensiondev/web3-shared'
-import { memoize } from 'lodash-es'
+import { ChainId, useChainId } from '@dimensiondev/web3-shared'
+import { useMemo } from 'react'
 import trade from './trade-constants.json'
 
-export const getTradeConstants = memoize((chainId = ChainId.Mainnet) => {
+export const getTradeConstants = (chainId = ChainId.Mainnet) => {
     type Constants = typeof trade
     type Table = { [key in keyof Constants]: Constants[key]['Mainnet'] }
 
     const table = {} as Table
-    const chainName = ChainId[chainId]
+    const chainName = ChainId[chainId] as keyof typeof ChainId
     for (const name in trade) {
-        table[name] = trade[name][chainName]
+        const key = name as keyof Constants
+        table[key] = trade[key][chainName]
     }
     return Object.freeze(table)
-})
+}
+
+export const useTradeConstants = hookTransform(getTradeConstants)
+
+function hookTransform<T>(getConstants: (chainId: ChainId) => T) {
+    return function useConstants(chainId = ChainId.Mainnet) {
+        const current = useChainId()
+        const finalChain = chainId ?? current
+        return useMemo(() => getConstants(finalChain), [finalChain])
+    }
+}
