c3aac6009e8e132c063496aca45a10752b4bcfd1
Hom Yan <yanzhihong23@qq.com>
2021-06-23 16:23:22 +0800

    refactor: optimize svg icons

--- packages/icons/plugins/Swap.tsx
+++ packages/icons/plugins/Swap.tsx
@@ -4,40 +4,15 @@ import type { SvgIcon } from '@material-ui/core'
 export const SwapIcon: typeof SvgIcon = createIcon(
     'Swap',
     <>
-        <circle cx="14.0002" cy="14" r="14" fill="#FFB915" fill-opacity="0.15" />
-        <g clipPath="url(#clip0)">
-            <path
-                d="M17.2411 6.87012L19.8337 9.46271L17.2411 12.0553"
-                stroke="#FFB915"
-                strokeWidth="1.25"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path
-                d="M8.16687 13.3515V12.0552C8.16687 11.3676 8.44002 10.7082 8.92622 10.222C9.41243 9.73579 10.0719 9.46265 10.7595 9.46265H19.8335"
-                stroke="#FFB915"
-                strokeWidth="1.25"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path
-                d="M10.7595 21.1293L8.16687 18.5367L10.7595 15.9441"
-                stroke="#FFB915"
-                strokeWidth="1.25"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path
-                d="M19.8335 14.6478V15.9441C19.8335 16.6317 19.5604 17.2912 19.0742 17.7774C18.588 18.2636 17.9285 18.5367 17.2409 18.5367H8.16687"
-                stroke="#FFB915"
-                strokeWidth="1.25"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
+        <circle cx="14" cy="14" r="14" fill="#FFB915" fillOpacity=".15" />
+        <g clipPath="url(#clip0)" stroke="#FFB915" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M17.241 6.87l2.593 2.593-2.593 2.592" />
+            <path d="M8.167 13.351v-1.296a2.593 2.593 0 012.592-2.592h9.075M10.76 21.13l-2.593-2.593 2.592-2.593" />
+            <path d="M19.834 14.648v1.296a2.593 2.593 0 01-2.593 2.593H8.167" />
         </g>
         <defs>
             <clipPath id="clip0">
-                <rect width="15.5556" height="15.5556" fill="white" transform="translate(6.22241 6.22192)" />
+                <path fill="#fff" transform="translate(6.222 6.222)" d="M0 0h15.556v15.556H0z" />
             </clipPath>
         </defs>
     </>,
