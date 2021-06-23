3c0348bbb55963473b2164d19a9a54da889c69da
Lantt <laanfor@mask.io>
2021-06-23 14:56:30 +0800

    feat: add searchable list component in theme package

---
+++ packages/theme/stories/components/SearchableListExample.tsx
@@ -0,0 +1,41 @@
+import { SearchableList } from '../../src/Components/SearchableList'
+import { DialogContent } from '@material-ui/core'
+import { MaskDialog } from '../../src/Components'
+import { experimentalStyled as styled } from '@material-ui/core/styles'
+
+const demoData: IDemoData[] = [
+    { address: 'ETH' },
+    { address: 'MASK' },
+    { address: 'ETH' },
+    { address: 'UMI' },
+    { address: 'AMP' },
+    { address: 'ANT' },
+    { address: 'BAL' },
+    { address: 'YFI' },
+]
+
+interface IItemProps {
+    data: IDemoData
+}
+
+interface IDemoData {
+    address: string
+}
+
+const ListItem = styled('div')`
+    line-height: 60px;
+`
+
+export function SearchableListItemExample({ data }: IItemProps) {
+    return <ListItem>{data.address}</ListItem>
+}
+
+export function SearchableListExample() {
+    return (
+        <MaskDialog open title="Searchable List">
+            <DialogContent>
+                <SearchableList<IDemoData> data={demoData} onSelect={() => {}} itemRender={SearchableListItemExample} />
+            </DialogContent>
+        </MaskDialog>
+    )
+}
