e5a69a416f2485de21c1ac6609a0f31c36eff01a
guanbinrui <guanbinrui@dimension.im>
2021-06-23 11:35:52 +0800

    refactor: code style

--- packages/maskbook/src/plugins/RedPacket/index.ts
+++ packages/maskbook/src/plugins/RedPacket/index.ts
@@ -10,7 +10,6 @@ registerPlugin({
     },
     Worker: {
         load: () => import('./Worker'),
-        hotModuleReload: (hot) =>
-            import.meta.webpackHot && import.meta.webpackHot.accept('./Worker/index', () => hot(import('./Worker'))),
+        hotModuleReload: (hot) => import.meta.webpackHot?.accept('./Worker/index', () => hot(import('./Worker'))),
     },
 })
