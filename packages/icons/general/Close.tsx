a48a5548cad26b97693b45c7101b361426e42e46
cmy <nuanyang233@gmail.com>
2021-06-24 21:06:08 +0800

    chore: pick icons

---
+++ packages/icons/general/Close.tsx
@@ -0,0 +1,9 @@
+import { createIcon } from '../utils'
+
+export const CloseIcon = createIcon(
+    'CloseIcon',
+    <>
+        <path d="M15 5L5 15M5 5l10 10" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+    </>,
+    '0 0 20 20',
+)
