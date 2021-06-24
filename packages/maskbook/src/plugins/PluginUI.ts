31c07450f47fd02c391234d67df24543d85686a7
unclebill <billbill290@gmail.com>
2021-06-24 17:03:46 +0800

    feat: migrate VCent plugin to new plugin infra

--- packages/maskbook/src/plugins/PluginUI.ts
+++ packages/maskbook/src/plugins/PluginUI.ts
@@ -17,7 +17,6 @@ import { TraderPluginDefine } from './Trader/define'
 import { TransakPluginDefine } from './Transak/define'
 import { ITO_PluginDefine } from './ITO/define'
 import { sideEffect } from '../utils/side-effects'
-import { VCentPluginDefine } from './VCent/define'
 import { DHedgePluginDefine } from './dHEDGE/define'
 
 sideEffect.then(() => {
@@ -25,7 +24,6 @@ sideEffect.then(() => {
     plugins.add(WalletPluginDefine)
     plugins.add(FileServicePluginDefine)
     if (Flags.ito_enabled) plugins.add(ITO_PluginDefine)
-    if (Flags.vcent_enabled) plugins.add(VCentPluginDefine)
     if (Flags.gitcoin_enabled) plugins.add(GitcoinPluginDefine)
     if (Flags.poll_enabled) plugins.add(PollsPluginDefine)
     if (Flags.trader_enabled) plugins.add(TraderPluginDefine)
