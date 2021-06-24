11f2b5255af098999f0c48a4818e2a692217de29
nuanyang233 <nuanyang233@gmail.com>
2021-06-24 14:41:23 +0800

    Update packages/dashboard/src/hooks/useRedPacketAddress.ts
    Co-authored-by: Septs <github@septs.pw>

--- packages/dashboard/src/hooks/useRedPacketAddress.ts
+++ packages/dashboard/src/hooks/useRedPacketAddress.ts
@@ -2,8 +2,7 @@ import { useConstant } from '@masknet/web3-shared'
 import { RED_PACKET_CONSTANTS } from '../constants'
 
 export function useRedPacketAddress() {
-    const addressV2 = useConstant(RED_PACKET_CONSTANTS).HAPPY_RED_PACKET_ADDRESS_V2
-    const addressV3 = useConstant(RED_PACKET_CONSTANTS).HAPPY_RED_PACKET_ADDRESS_V3
+    const { HAPPY_RED_PACKET_ADDRESS_V2, HAPPY_RED_PACKET_ADDRESS_V3 } = useConstant(RED_PACKET_CONSTANTS)
 
-    return addressV2 ?? addressV3
+    return HAPPY_RED_PACKET_ADDRESS_V2 ?? HAPPY_RED_PACKET_ADDRESS_V3
 }
