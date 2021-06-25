f55bfcab80685a95035d8e80ca0521bd1442bcaa
Jack Works <jackworks@protonmail.com>
2021-06-25 15:58:19 +0800

    chore: change publisher

--- packages/maskbook/src/plugins/Polls/base.ts
+++ packages/maskbook/src/plugins/Polls/base.ts
@@ -6,7 +6,7 @@ export const base: Plugin.Shared.Definition = {
     icon: PLUGIN_ICON,
     name: { fallback: PLUGIN_NAME },
     description: { fallback: PLUGIN_DESCRIPTION },
-    publisher: { name: { fallback: 'Mask Network' }, link: 'https://mask.io/' },
+    publisher: { name: { fallback: 'xichi' }, link: 'https://github.com/xichi' },
     enableRequirement: {
         architecture: { app: false, web: true },
         networks: { type: 'opt-out', networks: {} },
