5a8238be5f99034aff3a91f29d6bb3ca0325b003
unclebill <billbill290@gmail.com>
2021-06-25 14:20:42 +0800

    fixup! feat: migrate Transak plugin to new plugin infra

--- packages/maskbook/src/plugins/Transak/SNSAdaptor/index.tsx
+++ packages/maskbook/src/plugins/Transak/SNSAdaptor/index.tsx
@@ -1,12 +1,19 @@
 import type { Plugin } from '@masknet/plugin-infra'
 import { base } from '../base'
 import { BuyTokenDialog } from './BuyTokenDialog'
+import { PLUGIN_ICON, PLUGIN_NAME } from '../constants'
 
 const sns: Plugin.SNSAdaptor.Definition = {
     ...base,
     init(signal) {},
     GlobalInjection() {
         return <BuyTokenDialog />
+    },
+    CompositionDialogEntry: {
+        dialog: () => {
+            return <BuyTokenDialog />
+        },
+        label: { fallback: `${PLUGIN_ICON} ${PLUGIN_NAME}` },
     },
 }
 
