d6cec6d4d1821cce339ad1266a0083566a2e3f53
Septs <github@septs.pw>
2021-06-25 12:28:41 +0800

    chore: move to web3-shared

--- packages/constants/compile-constants.ts
+++ packages/constants/compile-constants.ts
@@ -1,100 +1,45 @@
-import fs from 'fs'
+import { promises as fs } from 'fs'
 import path from 'path'
 import { run } from '../cli/utils'
 
 const DATA_PATH = path.join(__dirname, 'data')
-const INDEX_PAYH = path.join(__dirname, 'src', 'index.ts')
 
-const files = fs.readdirSync(DATA_PATH)
-
-const captialized = (x: string) => `${x.charAt(0).toUpperCase()}${x.substr(1)}`
+const chainNames = ['Mainnet', 'Ropsten', 'Rinkeby', 'Kovan', 'Gorli', 'BSC', 'BSCT', 'Matic', 'Mumbai']
 
 async function main() {
-    // complete constants
-    files.forEach((x) => {
-        const filePaths = path.resolve(DATA_PATH, x)
-        const data = fs.readFileSync(filePaths).toString()
-
-        try {
-            const constants = JSON.parse(data)
-            const validatedConstants = Object.keys(constants).reduce((accumulator, key) => {
-                const value = Object.values(constants[key])[0]
-                const typeOfValue = Array.isArray(value) ? 'array' : typeof value
-                const defaultOfValue = (() => {
-                    switch (typeOfValue) {
-                        case 'string':
-                            return ''
-                        case 'number':
-                            return 0
-                        case 'boolean':
-                            return false
-                        case 'array':
-                            return []
-                        default:
-                            return null
-                    }
-                })()
-                accumulator[key] = {
-                    Mainnet: constants[key]['Mainnet'] ?? defaultOfValue,
-                    Ropsten: constants[key]['Ropsten'] ?? defaultOfValue,
-                    Rinkeby: constants[key]['Rinkeby'] ?? defaultOfValue,
-                    Kovan: constants[key]['Kovan'] ?? defaultOfValue,
-                    Gorli: constants[key]['Gorli'] ?? defaultOfValue,
-                    BSC: constants[key]['BSC'] ?? defaultOfValue,
-                    BSCT: constants[key]['BSCT'] ?? defaultOfValue,
-                    Matic: constants[key]['Matic'] ?? defaultOfValue,
-                    Mumbai: constants[key]['Mumbai'] ?? defaultOfValue,
-                }
-                return accumulator
-            }, {} as { [key: string]: any })
-
-            fs.writeFileSync(filePaths, JSON.stringify(validatedConstants, null, 4))
-        } catch (e) {
-            console.log(`Error: failed to parse ${filePaths}. It's not a valid JSON file.`)
-        }
-    })
-
-    // generate the index file
-    const code = files
-        .map((x) => {
-            const filePath = path.resolve(DATA_PATH, x)
-            const { name: fileName, ext: fileExt } = path.parse(filePath)
-
-            return {
-                path: filePath,
-                name: fileName,
-                ext: fileExt,
-            }
-        })
-        .reduce(
-            (code, data) => {
-                if (data.ext.toLowerCase() !== '.json') return code
-                const name = data.name
-                    .split(/[_-\s]+/)
-                    .map(captialized)
-                    .join('')
-                const constantName = `${name.toUpperCase()}_CONSTANTS`
-                const vallinaName = `get${captialized(name)}Constants`
-                const hookName = `use${captialized(name)}Constants`
-                code.push(`import ${constantName} from '../data/${data.name}${data.ext}'`)
-                code.push(`export const ${vallinaName} = transform(${constantName})`)
-                code.push(`export const ${hookName} = hookTransform(${vallinaName})`)
-                code.push('')
-                return code
-            },
-            [
-                '/* THIS FILE WAS GENERATED AUTOMATICALLY. PLEASE DO NOT MODIFY IT DIRECTLY. */',
-                '',
-                `import { hookTransform, transform } from './utils'`,
-                '',
-            ],
-        )
-
-    fs.writeFileSync(INDEX_PAYH, code.join('\n'))
-
     // format files
     run(DATA_PATH, 'npx', 'prettier', '--write', '*')
-    run(INDEX_PAYH, 'npx', 'prettier', '--write', '*')
+
+    // fix constants
+    for (const file of await fs.readdir(DATA_PATH)) {
+        const filePath = path.resolve(DATA_PATH, file)
+        const data = await fs.readFile(filePath, 'utf-8')
+        const constants: Record<string, Record<string, any>> = JSON.parse(data)
+        for (const name of Object.keys(constants)) {
+            const values = constants[name]
+            const defaultValue = getDefaultValue(Object.values(values)[0])
+            constants[name] = {}
+            for (const chainName of chainNames) {
+                constants[name][chainName] = values[chainName] ?? defaultValue
+            }
+        }
+        await fs.writeFile(filePath, JSON.stringify(constants, null, 4), 'utf-8')
+    }
 }
 
 main()
+
+function getDefaultValue(value: unknown) {
+    if (Array.isArray(value)) {
+        return []
+    }
+    switch (typeof value) {
+        case 'string':
+            return ''
+        case 'number':
+            return 0
+        case 'boolean':
+            return false
+    }
+    return null
+}
--- packages/web3-shared/src/constants/index.ts
+++ packages/web3-shared/src/constants/index.ts
@@ -1,32 +1,41 @@
-import * as constants from '@masknet/constants'
+import Airdrop from '@masknet/constants/data/Airdrop.json'
+import Ethereum from '@masknet/constants/data/ethereum.json'
+import Gitcoin from '@masknet/constants/data/gitcoin.json'
+import ITO from '@masknet/constants/data/ITO.json'
+import LBP from '@masknet/constants/data/LBP.json'
+import RedPacket from '@masknet/constants/data/red-packet.json'
+import Tokens from '@masknet/constants/data/tokens.json'
+import Trader from '@masknet/constants/data/trader.json'
+import Trending from '@masknet/constants/data/trending.json'
+import URL from '@masknet/constants/data/URL.json'
 import { hookTransform, transform } from './utils'
 
-export const getITOConstants = transform(constants.ITO)
+export const getITOConstants = transform(ITO)
 export const useITOConstants = hookTransform(getITOConstants)
 
-export const getLBPConstants = transform(constants.LBP)
+export const getLBPConstants = transform(LBP)
 export const useLBPConstants = hookTransform(getLBPConstants)
 
-export const getURLConstants = transform(constants.URL)
+export const getURLConstants = transform(URL)
 export const useURLConstants = hookTransform(getURLConstants)
 
-export const getAirdropConstants = transform(constants.Airdrop)
+export const getAirdropConstants = transform(Airdrop)
 export const useAirdropConstants = hookTransform(getAirdropConstants)
 
-export const getEthereumConstants = transform(constants.Ethereum)
+export const getEthereumConstants = transform(Ethereum)
 export const useEthereumConstants = hookTransform(getEthereumConstants)
 
-export const getGitcoinConstants = transform(constants.Gitcoin)
+export const getGitcoinConstants = transform(Gitcoin)
 export const useGitcoinConstants = hookTransform(getGitcoinConstants)
 
-export const getRedPacketConstants = transform(constants.RedPacket)
+export const getRedPacketConstants = transform(RedPacket)
 export const useRedPacketConstants = hookTransform(getRedPacketConstants)
 
-export const getTokensConstants = transform(constants.Tokens)
+export const getTokensConstants = transform(Tokens)
 export const useTokensConstants = hookTransform(getTokensConstants)
 
-export const getTraderConstants = transform(constants.Trader)
+export const getTraderConstants = transform(Trader)
 export const useTraderConstants = hookTransform(getTraderConstants)
 
-export const getTrendingConstants = transform(constants.Trending)
+export const getTrendingConstants = transform(Trending)
 export const useTrendingConstants = hookTransform(getTrendingConstants)
