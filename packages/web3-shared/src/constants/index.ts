45bf5420a7d4691e8632ca9511de5926d5ee8ae9
Septs <github@septs.pw>
2021-06-25 12:48:33 +0800

    chore: reorder expose

--- packages/web3-shared/src/constants/index.ts
+++ packages/web3-shared/src/constants/index.ts
@@ -10,15 +10,6 @@ import Trending from '@masknet/constants/data/trending.json'
 import URL from '@masknet/constants/data/URL.json'
 import { hookTransform, transform } from './utils'
 
-export const getITOConstants = transform(ITO)
-export const useITOConstants = hookTransform(getITOConstants)
-
-export const getLBPConstants = transform(LBP)
-export const useLBPConstants = hookTransform(getLBPConstants)
-
-export const getURLConstants = transform(URL)
-export const useURLConstants = hookTransform(getURLConstants)
-
 export const getAirdropConstants = transform(Airdrop)
 export const useAirdropConstants = hookTransform(getAirdropConstants)
 
@@ -27,6 +18,12 @@ export const useEthereumConstants = hookTransform(getEthereumConstants)
 
 export const getGitcoinConstants = transform(Gitcoin)
 export const useGitcoinConstants = hookTransform(getGitcoinConstants)
+
+export const getITOConstants = transform(ITO)
+export const useITOConstants = hookTransform(getITOConstants)
+
+export const getLBPConstants = transform(LBP)
+export const useLBPConstants = hookTransform(getLBPConstants)
 
 export const getRedPacketConstants = transform(RedPacket)
 export const useRedPacketConstants = hookTransform(getRedPacketConstants)
@@ -39,3 +36,6 @@ export const useTraderConstants = hookTransform(getTraderConstants)
 
 export const getTrendingConstants = transform(Trending)
 export const useTrendingConstants = hookTransform(getTrendingConstants)
+
+export const getURLConstants = transform(URL)
+export const useURLConstants = hookTransform(getURLConstants)
