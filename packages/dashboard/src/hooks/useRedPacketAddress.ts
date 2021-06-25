889d613800cdde949247881dffcf7571a82a9020
cmy <nuanyang233@gmail.com>
2021-06-25 14:13:03 +0800

    fix: import path

--- packages/dashboard/src/hooks/useRedPacketAddress.ts
+++ packages/dashboard/src/hooks/useRedPacketAddress.ts
@@ -1,4 +1,4 @@
-import { useRedPacketConstants } from '@masknet/constants'
+import { useRedPacketConstants } from '@masknet/web3-shared'
 
 export function useRedPacketAddress() {
     const { HAPPY_RED_PACKET_ADDRESS_V2, HAPPY_RED_PACKET_ADDRESS_V3 } = useRedPacketConstants()
