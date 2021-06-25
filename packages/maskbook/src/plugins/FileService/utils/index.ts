6f77516bc416c41ea85a88880eb3b73863733cf2
Septs <github@septs.pw>
2021-06-26 03:23:05 +0800

    chore: simplify logic

--- packages/maskbook/src/plugins/FileService/utils/index.ts
+++ packages/maskbook/src/plugins/FileService/utils/index.ts
@@ -1,19 +1,19 @@
 import { createPluginMessage } from '../../utils/createPluginMessage'
 import { createPluginRPC, createPluginRPCGenerator } from '../../utils/createPluginRPC'
-import { pluginId } from '../constants'
+import { FileServicePluginID } from '../constants'
 
 import.meta.webpackHot?.accept()
 
-const PluginFileServiceMessage = createPluginMessage<{ _: unknown; _2: unknown }>(pluginId)
+const PluginFileServiceMessage = createPluginMessage<{ _: unknown; _2: unknown }>(FileServicePluginID)
 
 export const PluginFileServiceRPC = createPluginRPC(
-    pluginId,
+    FileServicePluginID,
     () => import('../Worker/service'),
     PluginFileServiceMessage.events._,
 )
 
 export const PluginFileServiceRPCGenerator = createPluginRPCGenerator(
-    pluginId,
+    FileServicePluginID,
     () => import('../Worker/service').then(({ upload }) => ({ upload })),
     PluginFileServiceMessage.events._2,
 )
