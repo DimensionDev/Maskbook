cfea5c755f63211457025b376a87c94c967f94e2
Septs <github@septs.pw>
2021-06-29 11:08:45 +0800

    chore: clean event bound

--- packages/maskbook/src/utils/hooks/useQRCodeImageScan.tsx
+++ packages/maskbook/src/utils/hooks/useQRCodeImageScan.tsx
@@ -1,33 +1,23 @@
+import { loadImage } from '@dimensiondev/kit'
 import { useRef, useState, useEffect } from 'react'
 import { useAsync } from 'react-use'
 
 export function useQRCodeImageScan(image: React.MutableRefObject<HTMLImageElement | null>) {
     const scanner = useRef(new BarcodeDetector({ formats: ['qr_code'] }))
-    const [src, setSrc] = useState('')
+    const [source, setSource] = useState('')
 
     useEffect(() => {
         const node = image.current
         if (node) {
-            node.onload = () => setSrc(node?.getAttribute('src') ?? '')
-            node.onerror = () => setSrc('')
+            node.addEventListener('load', () => setSource(node?.getAttribute('src') ?? ''))
+            node.addEventListener('error', () => setSource(''))
         } else {
-            setSrc('')
+            setSource('')
         }
     }, [image])
-    return useAsync(
-        () =>
-            new Promise<string>((resolve, reject) => {
-                const fakeImage = new Image()
-                fakeImage.onload = () =>
-                    scanner.current
-                        .detect(fakeImage)
-                        .then(([result] = []) => resolve(result?.rawValue))
-                        .catch(reject)
-                fakeImage.onerror = reject
-                if (src && fakeImage.src !== src) {
-                    fakeImage.src = src
-                }
-            }),
-        [src, scanner.current],
-    )
+    return useAsync(async () => {
+        const image = await loadImage(source)
+        const [{ rawValue }] = await scanner.current.detect(image)
+        return rawValue
+    }, [source, scanner.current])
 }
