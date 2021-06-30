9190825a81ffcfca28d756edf07741500645261f
Hancheng Zhou <z308114274@gmail.com>
2021-06-30 15:22:49 +0800

    fix: bug

--- packages/shared/src/hooks/useScrollBottomEvent.ts
+++ packages/shared/src/hooks/useScrollBottomEvent.ts
@@ -7,12 +7,8 @@ export function useScrollBottomEvent(ref: RefObject<HTMLDivElement>, cb: () => v
             // ev.currentTarget is always null when applies debounce().
             const ev = _ev as Event & { path: HTMLDivElement[] }
             const element = ev.path[0]
-            const isBottomArrived = element.scrollTop === element.scrollHeight - element.offsetHeight
-            console.log({
-                scrollTop: element.scrollTop,
-                scrollHeight: element.scrollHeight,
-                offsetHeight: element.offsetHeight,
-            })
+            // On some device, there's a slight deviation between `scrollHeight` and `offsetHeight + scrollTop`
+            const isBottomArrived = Math.abs(element.scrollHeight - element.offsetHeight - element.scrollTop) < 2
             if (isBottomArrived) cb()
         }, 300),
         [cb],
