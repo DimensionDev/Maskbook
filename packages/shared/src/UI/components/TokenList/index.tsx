dd161c7ed0631957183dc971ac8794bdac0e820a
Lantt <lanyixing@live.com>
2021-06-30 17:43:57 +0800

    refactor: rename token list props

--- packages/shared/src/UI/components/TokenList/index.tsx
+++ packages/shared/src/UI/components/TokenList/index.tsx
@@ -5,7 +5,7 @@ import type { Asset, FungibleTokenDetailed } from '@masknet/web3-shared'
 import { EthereumTokenType, isSameAddress, useERC20Tokens } from '@masknet/web3-shared'
 import { SearchableList } from '@masknet/theme'
 
-interface IProps {
+export interface TokenListProps {
     onSelect(asset: Asset): void
     placeholder?: string
     assets: Asset[]
@@ -15,7 +15,7 @@ interface IProps {
 const checkAddedToken = (token: FungibleTokenDetailed, tokens: FungibleTokenDetailed[]) =>
     token.type === EthereumTokenType.Native || some(tokens, (t: any) => isSameAddress(token.address, t.address))
 
-export const TokenList: React.FC<IProps> = memo(({ onSelect, placeholder, assets, loading }) => {
+export const TokenList: React.FC<TokenListProps> = memo(({ onSelect, placeholder, assets, loading }) => {
     const [loadingPlaceholder, setLoadingPlaceholder] = useState(placeholder)
     const tokens = useERC20Tokens()
 
