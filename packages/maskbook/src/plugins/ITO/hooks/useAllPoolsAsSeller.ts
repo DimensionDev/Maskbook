119ee53e587bf23f48dc22cb74c7a9a7202c5f35
Hancheng Zhou <z308114274@gmail.com>
2021-06-30 17:58:35 +0800

    feat: ITO lazy load history list (#3524)

--- packages/maskbook/src/plugins/ITO/hooks/useAllPoolsAsSeller.ts
+++ packages/maskbook/src/plugins/ITO/hooks/useAllPoolsAsSeller.ts
@@ -1,9 +1,13 @@
 import { useAsyncRetry } from 'react-use'
 import { PluginITO_RPC } from '../messages'
+import type { PoolSubgraph } from '../types'
+import { useRef } from 'react'
 
-export function useAllPoolsAsSeller(address: string) {
+export function useAllPoolsAsSeller(address: string, page: number) {
+    const allPoolsRef = useRef<PoolSubgraph[]>([])
     return useAsyncRetry(async () => {
-        const pools = await PluginITO_RPC.getAllPoolsAsSeller(address)
-        return pools.reverse()
-    }, [address])
+        const pools = await PluginITO_RPC.getAllPoolsAsSeller(address, page)
+        allPoolsRef.current = allPoolsRef.current.concat(pools)
+        return allPoolsRef.current
+    }, [address, page])
 }
--- packages/shared/src/hooks/index.ts
+++ packages/shared/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useValueRef'
 export * from './useObservableMapSet'
 export * from './useMenu'
+export * from './useScrollBottomEvent'
