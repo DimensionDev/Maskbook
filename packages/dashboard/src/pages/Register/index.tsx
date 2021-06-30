4c0c3cb6d06f4093ac8c5ba85eae16963017cf98
Lantt <lanyixing@live.com>
2021-06-30 23:18:10 +0800

    refactor: remove unused code

--- packages/dashboard/src/pages/Register/index.tsx
+++
@@ -1,9 +0,0 @@
-import { RowLayout } from '../../components/RegisterFrame/RowLayout'
-
-export default function Register() {
-    return (
-        <RowLayout>
-            <div>content</div>
-        </RowLayout>
-    )
-}
--- packages/dashboard/src/pages/routes.tsx
+++ packages/dashboard/src/pages/routes.tsx
@@ -2,18 +2,11 @@ import { Route, Routes, Navigate } from 'react-router'
 import React, { lazy, Suspense } from 'react'
 import { DashboardFrame } from '../components/DashboardFrame'
 import { RoutePaths } from '../type'
-import { RoutePaths as ResetPasswordRoutePaths } from './PasswordReset'
-
 const Wallets = lazy(() => import('./Wallets'))
 const Setup = lazy(() => import('./Setup'))
 const SignUp = lazy(() => import('./SignUp'))
 const Login = lazy(() => import('./Login'))
 const PrivacyPolicy = lazy(() => import('./PrivacyPolicy'))
-const PasswordReset = lazy(() => import('./PasswordReset'))
-const InputAccountEmail = lazy(() => import('./PasswordReset/InputAccountEmail'))
-const VerificationCodeSending = lazy(() => import('./PasswordReset/VerificationCodeSending'))
-const NewPassword = lazy(() => import('./PasswordReset/NewPassword'))
-const Register = lazy(() => import('./Register'))
 const Welcome = lazy(() => import('./Welcome'))
 const Personas = lazy(() => import('./Personas'))
 const Settings = lazy(() => import('./Settings'))
@@ -28,15 +21,6 @@ export function Pages() {
                 <Route path={RoutePaths.SignUp} element={<SignUp />} />
                 <Route path={RoutePaths.Login} element={<Login />} />
                 <Route path={RoutePaths.PrivacyPolicy} element={<PrivacyPolicy />} />
-                <Route path={RoutePaths.PasswordReset} element={<PasswordReset />}>
-                    <Route path={ResetPasswordRoutePaths.InputAccountEmail} element={<InputAccountEmail />} />
-                    <Route
-                        path={ResetPasswordRoutePaths.VerificationCodeSending}
-                        element={<VerificationCodeSending />}
-                    />
-                    <Route path={ResetPasswordRoutePaths.NewPassword} element={<NewPassword />} />
-                </Route>
-                <Route path={RoutePaths.Register} element={<Register />} />
                 <Route path={RoutePaths.Personas} element={frame(<Personas />)} />
                 <Route path={`${RoutePaths.Wallets}/*`} element={frame(<Wallets />)} />
                 <Route path={RoutePaths.Settings} element={frame(<Settings />)} />
