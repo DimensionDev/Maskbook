69b840c82fe6fcea086530d5cbd049ef1a4e374d
guanbinrui <guanbinrui@dimension.im>
2021-07-01 17:32:56 +0800

    refactor: use erc20 token detailed

--- packages/web3-shared/src/utils/token.ts
+++ packages/web3-shared/src/utils/token.ts
@@ -1,7 +1,7 @@
 import { getEnumAsArray, safeUnreachable } from '@dimensiondev/kit'
 import BigNumber from 'bignumber.js'
 import type Web3 from 'web3'
-import type { AbiOutput } from 'web3-utils'
+import { AbiOutput, hexToBytes, toAscii } from 'web3-utils'
 import CHAINS from '../assets/chains.json'
 import { getTokenConstants } from '../constants'
 import {
@@ -207,6 +207,22 @@ export function decodeOutputString(web3: Web3, abis: AbiOutput[], output: string
     if (abis.length === 1) return web3.eth.abi.decodeParameter(abis[0], output)
     if (abis.length > 1) return web3.eth.abi.decodeParameters(abis, output)
     return
+}
+
+// parse a name or symbol from a token response
+const BYTES32_REGEX = /^0x[a-fA-F0-9]{64}$/
+
+export function parseStringOrBytes32(
+    str: string | undefined,
+    bytes32: string | undefined,
+    defaultValue: string,
+): string {
+    return str && str.length > 0
+        ? str
+        : // need to check for proper bytes string and valid terminator
+        bytes32 && BYTES32_REGEX.test(bytes32) && hexToBytes(bytes32)[31] === 0
+        ? toAscii(bytes32)
+        : defaultValue
 }
 
 export const getTokenUSDValue = (token: Asset) => (token.value ? Number.parseFloat(token.value[CurrencyType.USD]) : 0)
