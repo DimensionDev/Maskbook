bd631f497be0f61a84ea556bc6430e5712bc1f87
guanbinrui <guanbinrui@dimension.im>
2021-07-05 15:59:57 +0800

    fix: typed message on trader

--- packages/maskbook/src/plugins/Trader/SNSAdaptor/index.tsx
+++ packages/maskbook/src/plugins/Trader/SNSAdaptor/index.tsx
@@ -1,9 +1,14 @@
 import type { Plugin } from '@masknet/plugin-infra'
+import { isTypedMessageAnchor, TypedMessage, TypedMessageAnchor, TypedMessageTuple } from '@masknet/shared'
 import { base } from '../base'
+import { makeTypedMessageCashTrending } from '../messages/TypedMessageCashTrending'
 import { SettingsDialog } from './trader/SettingsDialog'
 import { TraderDialog } from './trader/TraderDialog'
 import { SearchResultInspector } from './trending/SearchResultInspector'
 import { TagInspector } from './trending/TagInspector'
+
+const isCashTagMessage = (m: TypedMessage): m is TypedMessageAnchor =>
+    isTypedMessageAnchor(m) && ['cash', 'hash'].includes(m.category) && !/#[\w\d]+lbp$/i.test(m.content)
 
 const sns: Plugin.SNSAdaptor.Definition = {
     ...base,
@@ -17,6 +22,12 @@ const sns: Plugin.SNSAdaptor.Definition = {
                 <TraderDialog />
             </>
         )
+    },
+    typedMessageTransformer(message: TypedMessageTuple) {
+        return {
+            ...message,
+            items: message.items.map((m: TypedMessage) => (isCashTagMessage(m) ? makeTypedMessageCashTrending(m) : m)),
+        } as TypedMessageTuple
     },
 }
 
