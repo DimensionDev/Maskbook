642308226122f2b2d4bee041ef3034797ae645e0
nuanyang233 <nuanyang233@gmail.com>
2021-07-07 17:08:24 +0800

    feat: pagination

--- packages/dashboard/src/pages/Personas/components/ContactsTable/index.tsx
+++ packages/dashboard/src/pages/Personas/components/ContactsTable/index.tsx
@@ -1,25 +1,56 @@
-import { memo, useMemo } from 'react'
+import { Dispatch, memo, SetStateAction, useMemo, useState } from 'react'
 import { PersonaContext } from '../../hooks/usePersonaContext'
 import { useContacts } from '../../hooks/useContacts'
 import type { Contact } from '@masknet/shared'
-import { Table, TableCell, TableContainer, TableHead, TableRow, makeStyles, TableBody, Box } from '@material-ui/core'
+import {
+    Table,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    makeStyles,
+    TableBody,
+    Box,
+    Pagination,
+    PaginationItem,
+} from '@material-ui/core'
 import { MaskColorVar } from '@masknet/theme'
 import { ContactTableRow } from '../ContactTableRow'
 import { EmptyContactPlaceholder } from '../EmptyContactPlaceholder'
 import { LoadingPlaceholder } from '../../../../components/LoadingPlacholder'
-import { sortBy } from 'lodash-es'
+import { ceil, sortBy } from 'lodash-es'
 
 const useStyles = makeStyles((theme) => ({
     container: {
         height: '100%',
         display: 'flex',
         flexDirection: 'column',
+        maxHeight: 'calc(100% - 58px)',
     },
     header: {
         color: MaskColorVar.normalText,
         fontWeight: theme.typography.fontWeightRegular,
         padding: '24px 28px',
         backgroundColor: MaskColorVar.primaryBackground,
+    },
+    footer: {
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    paginationItem: {
+        borderRadius: 4,
+        border: `1px solid ${MaskColorVar.lineLight}`,
+        color: MaskColorVar.textPrimary,
+        '&.Mui-selected': {
+            backgroundColor: MaskColorVar.blue,
+            color: '#ffffff',
+            border: 'none',
+            '&:hover': {
+                backgroundColor: MaskColorVar.blue,
+            },
+        },
     },
 }))
 
@@ -28,6 +59,7 @@ export interface ContactsTableProps {
 }
 
 export const ContactsTable = memo<ContactsTableProps>(({ network }) => {
+    const [page, setPage] = useState(1)
     const { currentPersona } = PersonaContext.useContainer()
 
     const { value, error, loading } = useContacts(network, currentPersona)
@@ -43,7 +75,7 @@ export const ContactsTable = memo<ContactsTableProps>(({ network }) => {
                 avatar: profile.avatar,
             })),
             (item) => item.favorite,
-        )
+        ).slice((page - 1) * 20, page * 20)
     }, [value])
 
     return (
@@ -52,6 +84,10 @@ export const ContactsTable = memo<ContactsTableProps>(({ network }) => {
             isLoading={loading}
             network={network}
             dataSource={dataSource}
+            page={page}
+            onPageChange={setPage}
+            count={ceil((value?.length ?? 0) / 20) ?? 1}
+            showPagination={!loading && !error && !!value?.length}
         />
     )
 })
@@ -60,35 +96,64 @@ export interface ContactsTableUIProps extends ContactsTableProps {
     dataSource: Contact[]
     isEmpty: boolean
     isLoading: boolean
+    page: number
+    onPageChange: Dispatch<SetStateAction<number>>
+    showPagination: boolean
+    count: number
 }
 
-export const ContactsTableUI = memo<ContactsTableUIProps>(({ network, dataSource, isEmpty, isLoading }) => {
-    const classes = useStyles()
-    return (
-        <TableContainer className={classes.container}>
-            {isEmpty || isLoading ? (
-                <Box flex={1}>{isLoading ? <LoadingPlaceholder /> : isEmpty ? <EmptyContactPlaceholder /> : null}</Box>
-            ) : (
-                <Table stickyHeader>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell variant="head" align="center" className={classes.header}>
-                                Name
-                            </TableCell>
-                            <TableCell variant="head" align="center" className={classes.header}>
-                                Operation
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    {dataSource.length ? (
-                        <TableBody>
-                            {dataSource.map((item, index) => (
-                                <ContactTableRow key={index} contact={item} index={index + 1} network={network} />
-                            ))}
-                        </TableBody>
-                    ) : null}
-                </Table>
-            )}
-        </TableContainer>
-    )
-})
+export const ContactsTableUI = memo<ContactsTableUIProps>(
+    ({ showPagination, page, count, onPageChange, network, dataSource, isEmpty, isLoading }) => {
+        const classes = useStyles()
+        return (
+            <>
+                <TableContainer className={classes.container}>
+                    {isEmpty || isLoading ? (
+                        <Box flex={1}>
+                            {isLoading ? <LoadingPlaceholder /> : isEmpty ? <EmptyContactPlaceholder /> : null}
+                        </Box>
+                    ) : (
+                        <Table stickyHeader>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell variant="head" align="center" className={classes.header}>
+                                        Name
+                                    </TableCell>
+                                    <TableCell variant="head" align="center" className={classes.header}>
+                                        Operation
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            {dataSource.length ? (
+                                <TableBody>
+                                    {dataSource.map((item, index) => (
+                                        <ContactTableRow
+                                            key={index}
+                                            contact={item}
+                                            index={index + 1}
+                                            network={network}
+                                        />
+                                    ))}
+                                </TableBody>
+                            ) : null}
+                        </Table>
+                    )}
+                </TableContainer>
+                {showPagination && !isEmpty ? (
+                    <Box className={classes.footer}>
+                        <Pagination
+                            variant="outlined"
+                            shape="rounded"
+                            count={count}
+                            page={page}
+                            onChange={(event, page) => onPageChange(page)}
+                            renderItem={(item) => (
+                                <PaginationItem {...item} classes={{ root: classes.paginationItem }} />
+                            )}
+                        />
+                    </Box>
+                ) : null}
+            </>
+        )
+    },
+)
