aefbb076170b6081ff4460069fa9837d03f7a01e
guanbinrui <guanbinrui@dimension.im>
2021-07-12 16:34:02 +0800

    refactor: move dhedge to new plugin infra

---
+++ packages/maskbook/src/plugins/dHEDGE/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import type { Plugin } from '@masknet/plugin-infra'
+import { base } from '../base'
+import { InvestDialog } from '../UI/InvestDialog'
+
+const dashboard: Plugin.Dashboard.Definition = {
+    ...base,
+    init(signal) {},
+    GlobalInjection: function Component() {
+        return (
+            <>
+                <InvestDialog />
+            </>
+        )
+    },
+}
+
+export default dashboard
