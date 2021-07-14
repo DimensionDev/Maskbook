0c51b588be45e885146729ec93437cf04ce1caf3
nuanyang233 <nuanyang233@gmail.com>
2021-07-14 14:02:04 +0800

    feat: introduce react-hook-form (#3632)
    * feat: hook-form demo
    
    * chore: add story of add token form
    
    * fix: code style
    
    * docs: add guide
    
    * chore: reply review
    
    * docs: fix demo
    
    * docs: reply review
    
    * Update docs/form-guide.md
    
    * docs: reply review
    
    Co-authored-by: Jack Works <jackworks@protonmail.com>

--- packages/dashboard/src/pages/Wallets/components/AddTokenFormUI/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/AddTokenFormUI/index.tsx
@@ -1,16 +1,10 @@
-import { memo, useEffect, useMemo, useState } from 'react'
+import { memo } from 'react'
 import { useDashboardI18N } from '../../../../locales'
-import { EthereumAddress } from 'wallet.ts'
-import { useUpdateEffect } from 'react-use'
 import { Button, DialogActions, DialogContent, makeStyles, TextField } from '@material-ui/core'
 import type { ERC20TokenDetailed } from '@masknet/web3-shared'
-import { isSameAddress } from '@masknet/web3-shared'
+import { useFormContext, Controller } from 'react-hook-form'
 
 export interface AddTokenFormUIProps {
-    open: boolean
-    address: string
-    exclude: string[]
-    onAddressChange: (address: string) => void
     onNext: () => void
     onClose: () => void
     token?: ERC20TokenDetailed
@@ -40,107 +34,80 @@ const useStyles = makeStyles((theme) => ({
     },
 }))
 
-export const AddTokenFormUI = memo<AddTokenFormUIProps>(
-    ({ address, onAddressChange, token, open, exclude, onClose, onNext }) => {
-        const t = useDashboardI18N()
-        const classes = useStyles()
-        const [symbol, setSymbol] = useState('')
-        const [decimals, setDecimals] = useState('')
+type AddTokenFormData = {
+    address: string
+    symbol: string
+    decimals: number
+}
 
-        const validateAddressMessage = useMemo(() => {
-            if (address.length && !EthereumAddress.isValid(address)) return t.wallets_incorrect_address()
-            if (exclude.find((item) => isSameAddress(item, address))) return t.wallets_token_been_added()
-            return ''
-        }, [address])
+export const AddTokenFormUI = memo<AddTokenFormUIProps>(({ token, onClose, onNext }) => {
+    const t = useDashboardI18N()
+    const classes = useStyles()
 
-        const validateSymbolMessage = useMemo(() => {
-            if (symbol.length && symbol.length > 11) return t.wallets_token_symbol_tips()
-            return ''
-        }, [])
+    const {
+        formState: { errors, isValid },
+    } = useFormContext<AddTokenFormData>()
 
-        const validateDecimalsMessage = useMemo(() => {
-            if (decimals.length && (Number(decimals) < 0 || Number(decimals) > 18))
-                return t.wallets_token_decimals_tips()
-            return ''
-        }, [decimals])
-
-        const disableButton = useMemo(
-            () =>
-                !address ||
-                !symbol ||
-                !decimals ||
-                !!validateAddressMessage ||
-                !!validateSymbolMessage ||
-                !!validateDecimalsMessage,
-            [address, symbol, decimals, validateAddressMessage, validateSymbolMessage, validateDecimalsMessage],
-        )
-
-        useEffect(() => {
-            if (token) {
-                setSymbol(token.symbol ?? '')
-                setDecimals(String(token.decimals))
-            }
-        }, [token])
-
-        //#region when dialog be closed, clear input
-        useUpdateEffect(() => {
-            if (!open) {
-                setSymbol('')
-                setDecimals('')
-            }
-        }, [open])
-
-        return (
-            <>
-                <DialogContent className={classes.content}>
-                    <form>
-                        <div className={classes.item}>
+    return (
+        <>
+            <DialogContent className={classes.content}>
+                <div className={classes.item}>
+                    <Controller
+                        render={({ field }) => (
                             <TextField
+                                {...field}
                                 variant="filled"
-                                value={address}
                                 label={t.wallets_add_token_contract_address()}
                                 InputProps={{ disableUnderline: true }}
-                                error={!!validateAddressMessage}
-                                helperText={validateAddressMessage}
-                                onChange={(e) => onAddressChange(e.target.value)}
+                                error={!!errors.address?.message}
+                                helperText={errors.address?.message}
                             />
-                        </div>
-                        <div className={classes.item}>
+                        )}
+                        name="address"
+                    />
+                </div>
+                <div className={classes.item}>
+                    <Controller
+                        render={({ field }) => (
                             <TextField
+                                {...field}
                                 variant="filled"
-                                value={symbol}
                                 label={t.wallets_add_token_symbol()}
                                 InputProps={{ disableUnderline: true }}
-                                error={!!validateSymbolMessage}
-                                onChange={(e) => setSymbol(e.target.value)}
-                                helperText={validateSymbolMessage}
+                                error={!!errors.symbol?.message}
+                                helperText={errors.symbol?.message}
                             />
-                        </div>
-                        <div className={classes.item}>
+                        )}
+                        name="symbol"
+                    />
+                </div>
+                <div className={classes.item}>
+                    <Controller
+                        render={({ field }) => (
                             <TextField
+                                {...field}
+                                onChange={(e) => field.onChange(Number(e.target.value))}
                                 variant="filled"
-                                type="number"
-                                value={decimals}
                                 label={t.wallets_add_token_decimals()}
                                 disabled={!!token?.decimals}
                                 inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
                                 InputProps={{ disableUnderline: true }}
-                                error={!!validateDecimalsMessage}
-                                helperText={validateDecimalsMessage}
-                                onChange={(e) => setDecimals(e.target.value)}
+                                error={!!errors.decimals?.message}
+                                helperText={errors.decimals?.message}
                             />
-                        </div>
-                    </form>
-                </DialogContent>
-                <DialogActions className={classes.actions}>
-                    <Button color="secondary" className={classes.button} onClick={onClose}>
-                        {t.wallets_add_token_cancel()}
-                    </Button>
-                    <Button color="primary" className={classes.button} onClick={onNext} disabled={disableButton}>
-                        {t.wallets_add_token_next()}
-                    </Button>
-                </DialogActions>
-            </>
-        )
-    },
-)
+                        )}
+                        name="decimals"
+                    />
+                </div>
+            </DialogContent>
+            <DialogActions className={classes.actions}>
+                <Button color="secondary" className={classes.button} onClick={onClose}>
+                    {t.wallets_add_token_cancel()}
+                </Button>
+                <Button color="primary" className={classes.button} onClick={onNext} disabled={!isValid}>
+                    {t.wallets_add_token_next()}
+                </Button>
+            </DialogActions>
+        </>
+    )
+})
