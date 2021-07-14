1cd694e6590800c2fbc9236645102068c5272a72
Lantt <lanyixing@live.com>
2021-07-14 11:59:44 +0800

    fix: should should token list when not connect wallet

--- packages/maskbook/src/extension/options-page/DashboardComponents/FixedTokenList.tsx
+++ packages/maskbook/src/extension/options-page/DashboardComponents/FixedTokenList.tsx
@@ -4,6 +4,7 @@ import {
     FungibleTokenDetailed,
     isSameAddress,
     TokenListsState,
+    useAccount,
     useAssetsFromChain,
     useERC20TokensDetailedFromTokenLists,
     useEthereumConstants,
@@ -34,6 +35,8 @@ export interface FixedTokenListProps extends withClasses<never> {
 
 export function FixedTokenList(props: FixedTokenListProps) {
     const classes = useStylesExtends(useStyles(), props)
+    const account = useAccount()
+
     const {
         keyword,
         whitelist: includeTokens = [],
@@ -47,24 +50,27 @@ export function FixedTokenList(props: FixedTokenListProps) {
     const { ERC20_TOKEN_LISTS } = useEthereumConstants()
     const [address, setAddress] = useState('')
     const { MASK_ADDRESS } = useTokenConstants()
-    const { state, tokensDetailed: erc20TokensDetailed } = useERC20TokensDetailedFromTokenLists(
-        ERC20_TOKEN_LISTS,
+
+    const filteredTokensAddress = ERC20_TOKEN_LISTS.filter(
+        (address) =>
+            (!includeTokens.length || includeTokens.some(currySameAddress(address))) &&
+            (!excludeTokens.length || !excludeTokens.some(currySameAddress(address))),
+    )
+
+    const { state, tokensDetailed: filteredERC20TokensDetailed } = useERC20TokensDetailedFromTokenLists(
+        filteredTokensAddress,
         keyword,
     )
 
-    const filteredTokens = erc20TokensDetailed.filter(
-        (x) =>
-            (!includeTokens.length || includeTokens.some(currySameAddress(x.address))) &&
-            (!excludeTokens.length || !excludeTokens.some(currySameAddress(x.address))),
+    const renderTokens = uniqBy([...tokens, ...filteredERC20TokensDetailed], (x) => x.address.toLowerCase()).sort(
+        (a, z) => {
+            if (a.type === EthereumTokenType.Native) return -1
+            if (z.type === EthereumTokenType.Native) return 1
+            if (isSameAddress(a.address, MASK_ADDRESS)) return -1
+            if (isSameAddress(z.address, MASK_ADDRESS)) return 1
+            return 0
+        },
     )
-
-    const renderTokens = uniqBy([...tokens, ...filteredTokens], (x) => x.address.toLowerCase()).sort((a, z) => {
-        if (a.type === EthereumTokenType.Native) return -1
-        if (z.type === EthereumTokenType.Native) return 1
-        if (isSameAddress(a.address, MASK_ADDRESS)) return -1
-        if (isSameAddress(z.address, MASK_ADDRESS)) return 1
-        return 0
-    })
 
     const {
         loading: loadingBalance,
@@ -72,13 +78,14 @@ export function FixedTokenList(props: FixedTokenListProps) {
         error,
     } = useAssetsFromChain(renderTokens.filter((x) => EthereumAddress.isValid(x.address)))
 
-    const renderAssets = error
-        ? renderTokens.map((token) => ({ token: token, balance: null }))
-        : assets.sort((a, b) => {
-              if (a.balance > b.balance) return -1
-              if (a.balance < b.balance) return 1
-              return 0
-          })
+    const renderAssets =
+        error || !account
+            ? renderTokens.map((token) => ({ token: token, balance: null }))
+            : assets.sort((a, b) => {
+                  if (a.balance > b.balance) return -1
+                  if (a.balance < b.balance) return 1
+                  return 0
+              })
 
     //#region UI helpers
     const renderPlaceholder = (message: string) => (
@@ -91,14 +98,14 @@ export function FixedTokenList(props: FixedTokenListProps) {
     if (state === TokenListsState.LOADING_TOKEN_LISTS || loadingBalance)
         return renderPlaceholder('Loading token lists...')
     if (state === TokenListsState.LOADING_SEARCHED_TOKEN) return renderPlaceholder('Loading token...')
-    if (!erc20TokensDetailed.length) return renderPlaceholder('No token found')
+    if (!filteredERC20TokensDetailed.length) return renderPlaceholder('No token found')
 
     return (
         <FixedSizeList
             className={classes.list}
             width="100%"
             height={100}
-            overscanCount={4}
+            overscanCount={8}
             itemSize={50}
             itemData={{
                 assets: renderAssets,
