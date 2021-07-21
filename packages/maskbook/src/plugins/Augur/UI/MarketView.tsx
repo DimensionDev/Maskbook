2a4695ca06c5edf19ecc29098fe1ec133b71f178
Elhamne <elham.ne94@gmail.com>
2021-07-21 21:18:53 +0430

    feat: missing message name

--- packages/maskbook/src/plugins/Augur/UI/MarketView.tsx
+++ packages/maskbook/src/plugins/Augur/UI/MarketView.tsx
@@ -101,7 +101,7 @@ export function MarketView(props: MarketViewProps) {
     const [tabIndex, setTabIndex] = useState(0)
     const tabs = [
         <Tab className={classes.tab} key="description" label={t('plugin_augur_tab_description')} />,
-        <Tab className={classes.tab} key="chart" label={t('plugin_augur_tab_chart')} />,
+        <Tab className={classes.tab} key="chart" label={t('plugin_augur_tab_odds')} />,
         <Tab className={classes.tab} key="buysell" label={t('plugin_augur_tab_buysell')} />,
     ].filter(Boolean)
     //#endregion
