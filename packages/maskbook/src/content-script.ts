ee15f3332789ef02354a8391f89a5e48d08caef3
himself65 <himself65@outlook.com>
2021-07-29 17:30:00 +0800

    fix: dont inject masklogin to globalThis

--- packages/maskbook/src/content-script.ts
+++ packages/maskbook/src/content-script.ts
@@ -3,9 +3,16 @@ import Services from './extension/service'
 import './setup.ui'
 
 // The scope should be the ./ of the web page
-const rootHref = new URL('./', location.href).href
-console.log('rootHref:', rootHref)
-Services.ThirdPartyPlugin.isSDKEnabled(rootHref).then((result) => {
-    console.log('result', result)
+const currentLocation = new URL('./', location.href).href
+Services.ThirdPartyPlugin.isSDKEnabled(currentLocation).then((result) => {
     result && import('./extension/external-sdk')
 })
+
+// we cannot inject to the globalThis, because this function will loaded after the third party javascript loaded
+// so globalThis.masklogin would be `undefined` for a long time
+// Object.defineProperty(globalThis, 'masklogin',{
+//     enumerable: true,
+//     configurable: true,
+//     value: Services.WebAuthn,
+//     writable: false
+// })
