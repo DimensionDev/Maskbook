7f16e06b9e2a2487174463ced4531aa9c6f27e48
Elhamne <elham.ne94@gmail.com>
2021-07-31 16:11:56 +0430

    feat: buy/sell button added

--- packages/maskbook/src/plugins/Augur/UI/MarketBuySell.tsx
+++ packages/maskbook/src/plugins/Augur/UI/MarketBuySell.tsx
@@ -1,3 +1,150 @@
+import { useState } from 'react'
+import { makeStyles, Typography, Grid, Switch, SwitchClassKey, SwitchProps, withStyles } from '@material-ui/core'
+import { Trans } from 'react-i18next'
+
+interface Styles extends Partial<Record<SwitchClassKey, string>> {
+    focusVisible?: string
+}
+
+interface Props extends SwitchProps {
+    classes: Styles
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        alignItems: 'center',
+    },
+    spacing: {
+        padding: theme.spacing(2),
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+    },
+    card: {
+        border: `solid .0625rem ${theme.palette.divider}`,
+        borderRadius: '1rem',
+        gridGap: '1.1rem',
+    },
+    head: {
+        borderBottom: `solid .0625rem ${theme.palette.divider}`,
+        paddingLeft: theme.spacing(3),
+        paddingRight: theme.spacing(3),
+    },
+}))
+
+const AugurSwitch = withStyles((theme) => ({
+    root: {
+        width: 170,
+        height: 50,
+        padding: 0,
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+        borderRadius: '.5rem',
+        border: `solid 1px ${theme.palette.divider}`,
+        fontSize: '1.25rem',
+    },
+    switchBase: {
+        padding: 0,
+        color: '#15171a',
+        borderColor: 'transparent',
+        marginLeft: 1,
+        marginTop: 1,
+        '&:after': {
+            content: "'Buy'",
+            color: '#fff',
+            position: 'absolute',
+        },
+        '&$checked': {
+            '&:after': {
+                content: "'Sell'",
+                color: '#fff',
+                position: 'absolute',
+            },
+            color: '#15171a',
+            transform: 'translateX(100%)',
+            marginLeft: -1,
+            '& + $track': {
+                backgroundColor: 'transparent',
+            },
+        },
+    },
+    thumb: {
+        width: 84,
+        height: 46,
+        borderRadius: '.5rem',
+    },
+    track: {
+        borderRadius: '.5rem',
+        backgroundColor: 'transparent',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        opacity: 0.5,
+        '&:before': {
+            content: "'Buy'",
+        },
+        '&:after': {
+            content: "'Sell'",
+        },
+    },
+    checked: {},
+    focusVisible: {},
+}))(({ classes, ...props }: Props) => {
+    return (
+        <Switch
+            focusVisibleClassName={classes.focusVisible}
+            disableRipple
+            classes={{
+                root: classes.root,
+                switchBase: classes.switchBase,
+                thumb: classes.thumb,
+                track: classes.track,
+                checked: classes.checked,
+            }}
+            {...props}
+        />
+    )
+})
+
 export const MarketBuySell = () => {
-    return <div>Market Buy / Sell</div>
+    const classes = useStyles()
+    const [buySell, setBuySell] = useState(false)
+    const handleChange = () => {
+        setBuySell(!buySell)
+    }
+
+    return (
+        <div className={`${classes.root} ${classes.spacing}`}>
+            <Grid container direction="column" className={classes.card}>
+                <Grid
+                    item
+                    container
+                    direction="row"
+                    wrap="nowrap"
+                    justifyContent="space-between"
+                    alignItems="center"
+                    className={`${classes.head} ${classes.spacing}`}>
+                    <Grid item>
+                        <Typography variant="h6" color="textPrimary">
+                            <AugurSwitch checked={buySell} onChange={handleChange} name="buySell" />
+                        </Typography>
+                    </Grid>
+                    <Grid item container direction="column" alignItems="flex-end">
+                        <Grid item>
+                            <Typography variant="body2" color="textSecondary">
+                                <Trans i18nKey="plugin_augur_fee" />
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="body2" color="textPrimary">
+                                1.50%
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item container className={classes.spacing}>
+                    Predictions
+                </Grid>
+            </Grid>
+        </div>
+    )
 }
