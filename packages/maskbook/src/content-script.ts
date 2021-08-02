8580454bd0c71c1f4a771d50035f9640b3fa6f0e
himself65 <himself65@outlook.com>
2021-08-02 14:12:47 +0800

    fix: remove console

--- packages/maskbook/src/content-script.ts
+++ packages/maskbook/src/content-script.ts
@@ -4,17 +4,6 @@ import './setup.ui'
 
 // The scope should be the ./ of the web page
 const currentLocation = new URL('./', location.href).href
-console.log('currentLocation', currentLocation)
 Services.ThirdPartyPlugin.isSDKEnabled(currentLocation).then((result) => {
-    console.log('should import external-sdk:', result)
     result && import('./extension/external-sdk')
 })
-
-// we cannot inject to the globalThis, because this function will loaded after the third party javascript loaded
-// so globalThis.masklogin would be `undefined` for a long time
-// Object.defineProperty(globalThis, 'masklogin',{
-//     enumerable: true,
-//     configurable: true,
-//     value: Services.WebAuthn,
-//     writable: false
-// })
