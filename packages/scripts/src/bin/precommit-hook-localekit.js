bc2fece91f9ef9611ad106fff489f0d13d2da16c
Jack Works <jackworks@protonmail.com>
2021-08-02 12:41:20 +0800

    fix: review

--- packages/scripts/src/bin/precommit-hook-localekit.js
+++ packages/scripts/src/bin/precommit-hook-localekit.js
@@ -1,8 +1,16 @@
 const { spawnSync } = require('child_process')
 const { resolve } = require('path')
 
-spawnSync('npx', ['gulp', 'locale-kit', '--sync-keys', '--remove-unused-keys'], {
+const { status, error } = spawnSync('npx', ['gulp', 'locale-kit', '--sync-keys', '--remove-unused-keys'], {
     cwd: resolve(__dirname, '../../../../'),
     stdio: 'inherit',
     shell: true,
 })
+if (error) {
+    console.error(error)
+}
+if (status === null || status === 0) {
+    process.exit(0)
+} else {
+    process.exit(status)
+}
