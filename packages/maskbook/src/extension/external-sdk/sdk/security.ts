29cf8f56b28fce3700c3b6d8164001fff843ea75
himself65 <himself65@outlook.com>
2021-08-03 15:03:23 +0800

    fix: update `isRegistrableDomain`

--- packages/maskbook/src/extension/external-sdk/sdk/security.ts
+++ packages/maskbook/src/extension/external-sdk/sdk/security.ts
@@ -1,1 +1,6 @@
-export { isRegistrableDomain } from '@dimensiondev/mask-webauthn/backend'
+import { currentBaseURL } from '../constant'
+import { isRegistrableDomain as _isRegistrableDomain } from '@dimensiondev/mask-webauthn/backend'
+
+export function isRegistrableDomain(domain: string) {
+    return _isRegistrableDomain(domain, currentBaseURL)
+}
