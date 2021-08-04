97b1be478bd07318daf95de7279e69e63e53f3fa
Jack Works <jackworks@protonmail.com>
2021-08-04 21:48:32 +0800

    chore: resolve review

--- packages/dashboard/src/pages/Settings/api.ts
+++ packages/dashboard/src/pages/Settings/api.ts
@@ -1,23 +1,24 @@
 import { createGlobalState } from '@masknet/shared'
 import { Services, Messages } from '../../API'
 import type { DataProvider } from '@masknet/public-api'
-export const [useLanguage] = createGlobalState(Services.Settings.getLanguage, (x) =>
-    Messages.events.languageSettings.on(x),
+export const [useLanguage] = createGlobalState(Services.Settings.getLanguage, Messages.events.languageSettings.on)
+
+export const [useTrendingDataSource] = createGlobalState<DataProvider>(
+    Services.Settings.getTrendingDataSource,
+    Messages.events.currentTrendingDataProviderSettings.on,
 )
 
-export const [useTrendingDataSource] = createGlobalState<DataProvider>(Services.Settings.getTrendingDataSource, (x) =>
-    Messages.events.currentTrendingDataProviderSettings.on(x),
-)
-
-export const [useEthNetworkTradeProvider] = createGlobalState(Services.Settings.getEthNetworkTradeProvider, (x) =>
-    Messages.events.ethNetworkTradeProviderSettings.on(x),
+export const [useEthNetworkTradeProvider] = createGlobalState(
+    Services.Settings.getEthNetworkTradeProvider,
+    Messages.events.ethNetworkTradeProviderSettings.on,
 )
 
 export const [usePolygonNetworkTradeProvider] = createGlobalState(
     Services.Settings.getPolygonNetworkTradeProvider,
-    (x) => Messages.events.polygonNetworkTradeProviderSettings.on(x),
+    Messages.events.polygonNetworkTradeProviderSettings.on,
 )
 
-export const [useBscNetworkTradeProvider] = createGlobalState(Services.Settings.getBscNetworkTradeProvider, (x) =>
-    Messages.events.bscNetworkTradeProviderSettings.on(x),
+export const [useBscNetworkTradeProvider] = createGlobalState(
+    Services.Settings.getBscNetworkTradeProvider,
+    Messages.events.bscNetworkTradeProviderSettings.on,
 )
