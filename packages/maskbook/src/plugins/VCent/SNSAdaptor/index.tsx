ac55571ea3d7b9ef1d7b848ad58c7d687e5b6011
guanbinrui <guanbinrui@dimension.im>
2021-08-05 22:57:54 +0800

    refactor: vcent only available on the detailed page

--- packages/maskbook/src/plugins/VCent/SNSAdaptor/index.tsx
+++ packages/maskbook/src/plugins/VCent/SNSAdaptor/index.tsx
@@ -7,15 +7,19 @@ const sns: Plugin.SNSAdaptor.Definition = {
     ...base,
     init(signal) {},
     DecryptedInspector: function Comp() {
-        const tweetAddress = usePostInfoDetails.postID()
-        if (!tweetAddress) return null
-        return <VCentDialog tweetAddress={tweetAddress} />
+        return <Renderer />
     },
     PostInspector() {
-        const tweetAddress = usePostInfoDetails.postID()
-        if (!tweetAddress) return null
-        return <VCentDialog tweetAddress={tweetAddress} />
+        return <Renderer />
     },
 }
 
 export default sns
+
+function Renderer() {
+    const tweetAddress = usePostInfoDetails.postID()
+    if (!tweetAddress) return null
+    // only for detailed page
+    if (!/\/status\/\d+$/.test(location.pathname)) return null
+    return <VCentDialog tweetAddress={tweetAddress} />
+}
