1077831513e651fb5c9b7a54c2ea17ac92637968
AxeAfk <gonghe@dodoex.io>
2021-08-05 23:14:57 +0800

    refactor: use urlcat

--- packages/maskbook/src/plugins/Trader/apis/dodo/index.ts
+++ packages/maskbook/src/plugins/Trader/apis/dodo/index.ts
@@ -8,24 +8,22 @@ import type {
 import { DODO_BASE_URL } from '../../constants/dodo'
 import BigNumber from 'bignumber.js'
 import { pow10 } from '@masknet/web3-shared'
+import urlcat from 'urlcat'
 
 export async function swapRoute(request: SwapRouteRequest) {
-    const params = new URLSearchParams()
-    Object.entries(request).map(([key, value]) => {
-        if (typeof value === 'string') params.set(key, value)
-    })
-    if (request.chainId) params.set('chainId', String(request.chainId))
-    if (request.slippage) params.set('slippage', String(request.slippage))
-    if (request.fromToken) {
-        params.set('fromTokenAddress', request.fromToken.address)
-        params.set('fromTokenDecimals', String(request.fromToken.decimals))
-    }
-    if (request.toToken) {
-        params.set('toTokenAddress', request.toToken.address)
-        params.set('toTokenDecimals', String(request.toToken.decimals))
-    }
-
-    const response = await fetch(`${DODO_BASE_URL}/dodoapi/getdodoroute?${params.toString()}`)
+    const response = await fetch(
+        urlcat(`${DODO_BASE_URL}/dodoapi/getdodoroute`, {
+            chainId: request.chainId,
+            slippage: request.slippage,
+            fromTokenAddress: request.fromToken?.address,
+            fromTokenDecimals: request.fromToken.decimals,
+            toTokenAddress: request.toToken?.address,
+            toTokenDecimals: request.toToken.decimals,
+            fromAmount: request.fromAmount,
+            userAddr: request.userAddr,
+            rpc: request.rpc,
+        }),
+    )
     const payload: SwapRouteResponse = await response.json()
 
     if (payload.status !== 200) {
