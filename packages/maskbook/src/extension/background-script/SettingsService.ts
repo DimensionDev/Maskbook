a9c73af4cc2b7f8bc0e79f5fe2d1b499b99cd9a1
Hom <yanzhihong23@qq.com>
2021-08-06 15:07:54 +0800

    feat: add labs page (#3480)
    * feat: add labs page
    
    * refactor: optimize svg icons
    
    * fix: typo
    
    * fix: storybook package name
    
    * fix: svg property
    
    * fix: snapshot icon
    
    * feat: add setting dialogs
    
    * refactor: remove plugin icon namespace
    
    * fix: radio button selection
    
    * feat: handle Swap & Transak explore (test)
    
    * chore: setup rpc properly
    
    * fix: duplicated import
    
    * fix: duplicated import
    
    * fix: open Swap dialog in dashboard
    
    * feat: plugin status switch
    
    * feat: hide swtich for Swap
    
    * fix: import type
    
    * feat: add transak plugin status check in trending view
    
    * feat: add plugin status check for ToolboxHint
    
    * feat: swap setting dialog
    
    * feat: remove zh.json for labs page
    
    * fix: merge conflict
    
    * feat: add webview apis
    
    * feat: hide action buttons if plugin is disabled
    
    * feat: auto open plugin in SNS
    
    * fix: typo
    
    * refactor: use NetworkType
    
    * fix: eslint
    
    * fix: build error
    
    * refactor: clean up toolbox hint
    
    * refactor: stable order in toolbar
    
    * chore: add new entry for toolbar
    
    * refactor: let file service to use new entry
    
    * refactor: let 2 plugin to use new entry
    
    * refactor: rewrite how to open remote plugins
    
    * refactor: cleanup
    
    * refactor: cleanup
    
    * fix: build error
    
    * chore: reexport a type
    
    * fix: eslint
    
    Co-authored-by: Jack Works <jackworks@protonmail.com>

--- packages/maskbook/src/extension/background-script/SettingsService.ts
+++ packages/maskbook/src/extension/background-script/SettingsService.ts
@@ -98,3 +98,18 @@ export async function isPluginEnabled(id: string) {
 export async function setPluginStatus(id: string, enabled: boolean) {
     currentPluginEnabledStatus['plugin:' + id].value = enabled
 }
+const key = 'openSNSAndActivatePlugin'
+/**
+ * This function will open a new web page, then open the composition dialog and activate the composition entry of the given plugin.
+ * @param url URL to open
+ * @param pluginID Plugin to activate
+ */
+export async function openSNSAndActivatePlugin(url: string, pluginID: string) {
+    await browser.tabs.create({ active: true, url })
+    sessionStorage.setItem(key, pluginID)
+}
+export async function shouldActivatePluginOnSNSStart() {
+    const val = sessionStorage.getItem(key)
+    sessionStorage.removeItem(key)
+    return val
+}
