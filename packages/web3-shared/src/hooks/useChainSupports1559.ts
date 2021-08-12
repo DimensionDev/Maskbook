97b5a8aaba05723480299619c3ee08cea57595f0
guanbinrui <guanbinrui@dimension.im>
2021-08-12 11:18:00 +0800

    chore: distinguish eip1599 txs

--- packages/web3-shared/src/hooks/useChainSupports1559.ts
+++
@@ -1,6 +0,0 @@
-import { useWeb3StateContext } from "../context";
-
-export function useChainSupports1159() {
-    const features: string[] = useWeb3StateContext().chainDetailed?.features ?? []
-    return features.includes('EIP1159')
-}
