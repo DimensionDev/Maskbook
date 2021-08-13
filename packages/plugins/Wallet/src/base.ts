ba867bb86a3536c6fdc52d4972bb3b9de06503e0
Jack Works <jackworks@protonmail.com>
2021-08-13 16:42:24 +0800

    docs(i18n): sync i18n files from Crowdin (#3946)
    * New translations messages.json (Japanese)
    docs(i18n): sync translation messages.json for Japanese
    
    * New translations messages.json (Korean)
    docs(i18n): sync translation messages.json for Korean
    
    * New translations messages.json (Chinese Simplified)
    docs(i18n): sync translation messages.json for Chinese Simplified
    
    * New translations messages.json (Chinese Traditional)
    docs(i18n): sync translation messages.json for Chinese Traditional
    
    * New translations en.json (Japanese)
    docs(i18n): sync translation en.json for Japanese
    
    * New translations en.json (Korean)
    docs(i18n): sync translation en.json for Korean
    
    * New translations en.json (Chinese Simplified)
    docs(i18n): sync translation en.json for Chinese Simplified
    
    * New translations en.json (Chinese Traditional)
    docs(i18n): sync translation en.json for Chinese Traditional
    
    * New translations en.json (Japanese)
    docs(i18n): sync translation en.json for Japanese
    
    * New translations en.json (Korean)
    docs(i18n): sync translation en.json for Korean
    
    * New translations en.json (Chinese Simplified)
    docs(i18n): sync translation en.json for Chinese Simplified
    
    * New translations en.json (Japanese)
    docs(i18n): sync translation en.json for Japanese
    
    * New translations en.json (Korean)
    docs(i18n): sync translation en.json for Korean
    
    * New translations en.json (Chinese Simplified)
    docs(i18n): sync translation en.json for Chinese Simplified
    
    * New translations en.json (Chinese Traditional)
    docs(i18n): sync translation en.json for Chinese Traditional
    
    * chore: disable part of locale-kit
    
    * feat: add new langs in def
    
    * fix: typo

--- packages/plugins/Wallet/src/base.ts
+++ packages/plugins/Wallet/src/base.ts
@@ -1,6 +1,9 @@
 import type { Plugin } from '@masknet/plugin-infra'
 import { PLUGIN_DESCRIPTION, PLUGIN_ICON, PLUGIN_IDENTIFIER, PLUGIN_NAME } from './constants'
 import en from './locales/en.json'
+import zh from './locales/zh.json'
+import ko from './locales/ko.json'
+import jp from './locales/ja.json'
 
 export const base: Plugin.Shared.Definition = {
     ID: PLUGIN_IDENTIFIER,
@@ -14,5 +17,5 @@ export const base: Plugin.Shared.Definition = {
         target: 'stable',
     },
     management: { alwaysOn: true },
-    i18n: { en },
+    i18n: { en, zh, ko, jp },
 }
