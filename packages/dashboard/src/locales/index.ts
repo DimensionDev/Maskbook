ae5beec560106f97792915f1119a55d162890957
Jack Works <jackworks@protonmail.com>
2021-08-13 15:35:38 +0800

    fix: typo

--- packages/dashboard/src/locales/index.ts
+++ packages/dashboard/src/locales/index.ts
@@ -2,7 +2,7 @@ export * from './i18n_generated'
 import { createI18NBundle } from '@masknet/shared'
 import en from './en.json'
 import zh from './zh.json'
-import jp from './jp.json'
+import jp from './ja.json'
 import ko from './ko.json'
 
 export const addDashboardI18N = createI18NBundle('dashboard', {
--- packages/plugins/Wallet/src/base.ts
+++ packages/plugins/Wallet/src/base.ts
@@ -3,7 +3,7 @@ import { PLUGIN_DESCRIPTION, PLUGIN_ICON, PLUGIN_IDENTIFIER, PLUGIN_NAME } from 
 import en from './locales/en.json'
 import zh from './locales/zh.json'
 import ko from './locales/ko.json'
-import jp from './locales/jp.json'
+import jp from './locales/ja.json'
 
 export const base: Plugin.Shared.Definition = {
     ID: PLUGIN_IDENTIFIER,
