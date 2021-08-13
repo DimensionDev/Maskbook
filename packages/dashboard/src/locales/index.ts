758bcbc1ae886a48a72611d1fc2eb6bef76b37bb
Jack Works <jackworks@protonmail.com>
2021-08-13 15:32:12 +0800

    feat: add new langs in def

--- packages/dashboard/src/locales/index.ts
+++ packages/dashboard/src/locales/index.ts
@@ -2,7 +2,12 @@ export * from './i18n_generated'
 import { createI18NBundle } from '@masknet/shared'
 import en from './en.json'
 import zh from './zh.json'
+import jp from './jp.json'
+import ko from './ko.json'
+
 export const addDashboardI18N = createI18NBundle('dashboard', {
     en,
     zh,
+    jp,
+    ko,
 })
