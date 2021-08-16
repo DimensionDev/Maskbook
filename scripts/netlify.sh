766611865a7b75c986f03ec742ae009e06809b81
Jack Works <jackworks@protonmail.com>
2021-08-16 23:02:48 +0800

    fix: clean netlify cache

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -xeuo pipefail
 test "$CI" = true || exit 1
+rm -rf node_modules/.pnpm
 npx pnpm install -r --store-dir=node_modules/.pnpm-store
 # Hard coded...
 node ./node_modules/.pnpm/esbuild@0.9.7/node_modules/esbuild/install.js
