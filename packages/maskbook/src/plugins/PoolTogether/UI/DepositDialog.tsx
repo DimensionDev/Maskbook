57ef852f0295c09a1e61294270921ca9822ae825
Arash Fatahzade <fatahzade@gmail.com>
2021-08-17 19:07:34 +0430

    fix: undo unwanted changes

--- packages/maskbook/src/plugins/PoolTogether/UI/DepositDialog.tsx
+++ packages/maskbook/src/plugins/PoolTogether/UI/DepositDialog.tsx
@@ -253,6 +253,7 @@ export function DepositDialog() {
     const prizePeriodSeconds = Number.parseInt(pool?.config.prizePeriodSeconds ?? '', 10)
 
     if (!token || !pool) return null
+
     return (
         <div className={classes.root}>
             <InjectedDialog
