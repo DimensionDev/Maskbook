68f2a76f47665b9d7e54905e5a1b9eae460e6f3e
Lantt <lanyixing@live.com>
2021-08-19 14:45:10 +0800

    feat: remove custom token tab

--- packages/dashboard/src/pages/Wallets/components/AddTokenDialog/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/AddTokenDialog/index.tsx
@@ -1,10 +1,8 @@
 import { memo } from 'react'
-import { MaskDialog, useTabs } from '@masknet/theme'
+import { MaskDialog } from '@masknet/theme'
 import { useDashboardI18N } from '../../../../locales'
-import { TabContext, TabList, TabPanel } from '@material-ui/lab'
-import { Tab } from '@material-ui/core'
-import { AddCustomToken } from './AddCustomToken'
-import { AddTokenFromList } from './AddTokenFromList'
+import { ERC20TokenList } from '@masknet/shared'
+import { DialogContent } from '@material-ui/core'
 
 export interface AddTokenDialogProps {
     open: boolean
@@ -13,22 +11,12 @@ export interface AddTokenDialogProps {
 
 export const AddTokenDialog = memo<AddTokenDialogProps>(({ open, onClose }) => {
     const t = useDashboardI18N()
-    const [currentTab, onChange, tabs] = useTabs('searchToken', 'customToken')
 
     return (
         <MaskDialog maxWidth="md" open={open} title={t.wallets_add_token()} onClose={onClose}>
-            <TabContext value={currentTab}>
-                <TabList onChange={onChange}>
-                    <Tab sx={{ width: '50%' }} label="SearchToken" value={tabs.searchToken} />
-                    <Tab sx={{ width: '50%' }} label="Custom Token" value={tabs.customToken} />
-                </TabList>
-                <TabPanel value={tabs.searchToken}>
-                    <AddTokenFromList />
-                </TabPanel>
-                <TabPanel value={tabs.customToken}>
-                    <AddCustomToken onClose={onClose} />
-                </TabPanel>
-            </TabContext>
+            <DialogContent>
+                <ERC20TokenList />
+            </DialogContent>
         </MaskDialog>
     )
 })
