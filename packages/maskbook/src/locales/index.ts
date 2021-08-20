0f8afbc41e15fedec557e72c33fa548a0af7f142
Jack Works <jackworks@protonmail.com>
2021-08-20 18:57:33 +0800

    fix: language detect close #3446

--- packages/maskbook/src/locales/index.ts
+++ packages/maskbook/src/locales/index.ts
@@ -1,5 +1,7 @@
 // This file is auto generated. DO NOT EDIT
 // Run `npx gulp sync-languages` to regenerate.
+// Default fallback langauge in a family of langauges are chosen by the alphabet order
+// To overwrite this, please overwrite packages/scripts/src/locale-kit-next/index.ts
 import en_US from './en-US.json'
 import es_ES from './es-ES.json'
 import fa_IR from './fa-IR.json'
@@ -14,17 +16,17 @@ import zh_TW from './zh-TW.json'
 
 export * from './i18n_generated'
 export const languages = {
-    'en-US': en_US,
-    'es-ES': es_ES,
-    'fa-IR': fa_IR,
-    'fr-FR': fr_FR,
-    'it-IT': it_IT,
-    'ja-JP': ja_JP,
-    'ko-KR': ko_KR,
-    'qya-AA': qya_AA,
-    'ru-RU': ru_RU,
+    en: en_US,
+    es: es_ES,
+    fa: fa_IR,
+    fr: fr_FR,
+    it: it_IT,
+    ja: ja_JP,
+    ko: ko_KR,
+    qy: qya_AA,
+    ru: ru_RU,
     'zh-CN': zh_CN,
-    'zh-TW': zh_TW,
+    zh: zh_TW,
 }
 import { createI18NBundle } from '@masknet/shared'
 export const addMaskI18N = createI18NBundle('mask', languages)
--- packages/plugins/example/src/locales/index.ts
+++ packages/plugins/example/src/locales/index.ts
@@ -1,10 +1,12 @@
 // This file is auto generated. DO NOT EDIT
 // Run `npx gulp sync-languages` to regenerate.
+// Default fallback langauge in a family of langauges are chosen by the alphabet order
+// To overwrite this, please overwrite packages/scripts/src/locale-kit-next/index.ts
 import en_US from './en-US.json'
 import zh_TW from './zh-TW.json'
 
 export * from './i18n_generated'
 export const languages = {
-    'en-US': en_US,
-    'zh-TW': zh_TW,
+    en: en_US,
+    zh: zh_TW,
 }
