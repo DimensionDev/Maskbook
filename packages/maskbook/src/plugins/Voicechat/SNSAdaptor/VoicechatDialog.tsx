cb273e40245a1b31a0bcf026b9fc6d9a242c5390
Jack Works <jackworks@protonmail.com>
2021-08-20 18:10:49 +0800

    refactor: migrate to new makeStyles API

--- packages/maskbook/src/plugins/Voicechat/SNSAdaptor/VoicechatDialog.tsx
+++ packages/maskbook/src/plugins/Voicechat/SNSAdaptor/VoicechatDialog.tsx
@@ -1,22 +1,13 @@
-import {
-    Box,
-    Button,
-    Checkbox,
-    DialogContent,
-    DialogProps,
-    FormControlLabel,
-    makeStyles,
-    TextField,
-} from '@material-ui/core'
-import { useEffect, useState } from 'react'
+import { Box, Button, Checkbox, DialogContent, DialogProps, FormControlLabel, TextField } from '@material-ui/core'
+import { makeStyles } from '@masknet/theme'
+import { useState } from 'react'
 import { InjectedDialog } from '../../../components/shared/InjectedDialog'
-import { editActivatedPostMetadata, globalTypedMessageMetadata } from '../../../protocols/typed-message/global-state'
-import { useValueRef } from '../../../utils/hooks/useValueRef'
 import { useI18N } from '../../../utils/i18n-next-ui'
 import { VOICECHAT_META_KEY_1 } from '../constants'
 import type { VoiceChatMetadata } from '../types'
+import { useCompositionContext } from '../../../components/CompositionDialog/CompositionContext'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles()((theme) => ({
     line: {
         display: 'flex',
         margin: theme.spacing(1),
@@ -33,9 +24,9 @@ interface VoicechatDialogProps extends withClasses<'wrapper'> {
 }
 
 export const VoicechatDialog = (props: VoicechatDialogProps) => {
-    const classes = useStyles()
-
+    const { classes } = useStyles()
     const { t } = useI18N()
+    const { attachMetadata, dropMetadata } = useCompositionContext()
 
     const [voicechatEnabled, setVoicechatEnabled] = useState(false)
     const [customServerURL, setCustomServerURL] = useState('')
@@ -43,23 +34,15 @@ export const VoicechatDialog = (props: VoicechatDialogProps) => {
     const submit = () => {
         const set: VoiceChatMetadata = { customServer: customServerURL }
 
-        editActivatedPostMetadata((next) => {
-            if (voicechatEnabled) {
-                next.set(VOICECHAT_META_KEY_1, JSON.parse(JSON.stringify(set)))
-            } else {
-                next.delete(VOICECHAT_META_KEY_1)
-            }
-        })
+        if (voicechatEnabled) {
+            attachMetadata(VOICECHAT_META_KEY_1, JSON.parse(JSON.stringify(set)))
+            props.open && setVoicechatEnabled(true)
+        } else {
+            dropMetadata(VOICECHAT_META_KEY_1)
+            props.open && setVoicechatEnabled(false)
+        }
         props.onClose()
     }
-
-    const currentMeta = useValueRef(globalTypedMessageMetadata)
-    useEffect(() => {
-        if (props.open) {
-            const enabled = Array.from(currentMeta.keys()).includes(VOICECHAT_META_KEY_1)
-            setVoicechatEnabled(enabled)
-        }
-    }, [props.open])
 
     return (
         <InjectedDialog open={props.open} onClose={props.onClose} title={t('plugin_voicechat_title')}>
