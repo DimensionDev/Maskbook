f795899c90c3063f664f860c73a8cbd378ffac26
Jack Works <jackworks@protonmail.com>
2021-08-20 16:17:51 +0800

    refactor: migrate plugin dHEDGE to use new makeStyles API

--- packages/maskbook/src/plugins/Collectible/SNSAdaptor/CollectibleCard.tsx
+++ packages/maskbook/src/plugins/Collectible/SNSAdaptor/CollectibleCard.tsx
@@ -1,18 +1,11 @@
-import { makeStyles } from '@material-ui/core/styles'
-import { useStylesExtends } from '@masknet/shared'
 import { Card } from '@material-ui/core'
-
-const useStyles = makeStyles((theme) => {
-    return {}
-})
-
 export interface CollectibleCardProps extends withClasses<'root'> {
     children?: React.ReactNode
 }
 
 export function CollectibleCard(props: CollectibleCardProps) {
     const { children } = props
-    const classes = useStylesExtends(useStyles(), props)
+    const classes = props.classes
     return (
         <Card className={classes.root} elevation={0}>
             {children}
