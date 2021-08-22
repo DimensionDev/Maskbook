00887fd556e100d2dcbb71143d31d038df42eda8
Osama El Hariri <osama.hariri.dev@gmail.com>
2021-08-22 11:54:03 +0300

    refactor: remove contract that is no longer used

--- packages/maskbook/src/plugins/GoodGhosting/contracts/useAaveLendingPoolContract.ts
+++
@@ -1,8 +0,0 @@
-import type { AbiItem } from 'web3-utils'
-import AaveLendingPoolABI from '@masknet/web3-contracts/abis/AaveLendingPool.json'
-import type { AaveLendingPool } from '@masknet/web3-contracts/types/AaveLendingPool'
-import { useContract } from '@masknet/web3-shared'
-
-export function useAaveLendingPoolContract(address: string) {
-    return useContract<AaveLendingPool>(address, AaveLendingPoolABI as AbiItem[])
-}
