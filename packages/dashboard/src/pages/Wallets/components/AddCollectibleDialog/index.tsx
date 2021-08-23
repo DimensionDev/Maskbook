c07b6dd191e5222a01139b57f27597b5765870e3
Lantt <lanyixing@live.com>
2021-08-23 13:47:18 +0800

    feat: user can add nft in dashboard 2.0

--- packages/dashboard/src/pages/Wallets/components/AddCollectibleDialog/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/AddCollectibleDialog/index.tsx
@@ -1,13 +1,18 @@
-import { memo, useEffect, useState } from 'react'
+import { FormEvent, memo, useEffect, useState } from 'react'
 import { MaskDialog, MaskTextField } from '@masknet/theme'
-import { useSnackbarCallback } from '@masknet/shared'
 import { Box, Button, DialogActions, DialogContent } from '@material-ui/core'
-import { useWallet } from '@masknet/web3-shared'
+import {
+    isSameAddress,
+    useERC721ContractDetailed,
+    useERC721TokenDetailedCallback,
+    useWallet,
+} from '@masknet/web3-shared'
 import { EthereumAddress } from 'wallet.ts'
 import { useDashboardI18N } from '../../../../locales'
 import { z } from 'zod'
-import { Controller, useForm, ErrorOption } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { PluginServices } from '../../../../API'
 
 export interface AddCollectibleDialogProps {
     open: boolean
@@ -16,42 +21,39 @@ export interface AddCollectibleDialogProps {
 
 type FormInputs = {
     address: string
-    //  all the tokenId is number?
     tokenId: string
 }
-type FormError = {
-    name: keyof FormInputs
-    option: ErrorOption
+
+enum FormErrorType {
+    Added = 'ADDED',
+    NotExist = 'NOT_EXIST',
 }
 
 export const AddCollectibleDialog = memo<AddCollectibleDialogProps>(({ open, onClose }) => {
-    const t = useDashboardI18N()
-    const [address, setAddress] = useState('')
-    const [error, setError] = useState<FormError>()
-
     const wallet = useWallet()
-    // Todo: The current implement does not work, please refactor this component according to dashboard 1.0 Add Asset:
-    // Todo: maskbook/src/extension/options-page/DashboardDialogs/Wallet/AddERC721Token.tsx
-    // const tokenDetailed = useERC721TokenDetailed(address)
-    // const assetDetailed = useERC721TokenAssetDetailed(tokenDetailed.value)
+    const [address, setAddress] = useState('')
+    const { value: contractDetailed, loading: contractDetailLoading } = useERC721ContractDetailed(address)
+    const [tokenId, setTokenId, erc721TokenDetailedCallback] = useERC721TokenDetailedCallback(contractDetailed)
 
-    const onSubmit = useSnackbarCallback({
-        executor: async () => {
-            // if (!tokenDetailed.value || !assetDetailed.value || !wallet) return
-            // await Promise.all([
-            //     PluginServices.Wallet.addERC721Token({
-            //         ...tokenDetailed.value,
-            //         asset: assetDetailed.value,
-            //     }),
-            //     PluginServices.Wallet.trustERC721Token(wallet.address, tokenDetailed.value),
-            // ])
-        },
-        deps: [wallet],
-        onSuccess: onClose,
-        // todo: handle error
-        onError: () =>
-            setError({ name: 'tokenId', option: { type: 'value', message: t.wallets_collectible_error_not_exist() } }),
-    })
+    const onSubmit = async () => {
+        if (contractDetailLoading || !wallet) return
+
+        const tokenInDB = await PluginServices.Wallet.getERC721Token(address, tokenId)
+        if (tokenInDB) throw new Error(FormErrorType.Added)
+
+        const _tokenDetailed = await erc721TokenDetailedCallback()
+
+        if (
+            (_tokenDetailed && !isSameAddress(_tokenDetailed.info.owner, wallet.address)) ||
+            !_tokenDetailed ||
+            !_tokenDetailed.info.owner
+        ) {
+            throw new Error(FormErrorType.NotExist)
+        } else {
+            await PluginServices.Wallet.addERC721Token(_tokenDetailed)
+            onClose()
+        }
+    }
 
     return (
         <AddCollectibleDialogUI
@@ -59,9 +61,8 @@ export const AddCollectibleDialog = memo<AddCollectibleDialogProps>(({ open, onC
             onClose={onClose}
             address={address}
             onAddressChange={setAddress}
+            onTokenIdChange={setTokenId}
             onSubmit={onSubmit}
-            error={error}
-            exclude={Array.from(wallet?.erc721_token_whitelist ?? [])}
         />
     )
 })
@@ -70,48 +71,57 @@ export interface AddCollectibleDialogUIProps {
     open: boolean
     onClose: () => void
     address: string
-    exclude: string[]
-    error?: FormError
     onAddressChange: (address: string) => void
+    onTokenIdChange: (tokenId: string) => void
     onSubmit: () => void
 }
 
 export const AddCollectibleDialogUI = memo<AddCollectibleDialogUIProps>(
-    ({ open, error, onClose, address, exclude, onAddressChange, onSubmit }) => {
+    ({ open, onClose, onAddressChange, onTokenIdChange, onSubmit }) => {
         const t = useDashboardI18N()
-
-        useEffect(() => {
-            if (!error) return
-            setError(error.name, error.option)
-        })
 
         const schema = z.object({
             address: z
                 .string()
                 .min(1)
                 .refine((address) => EthereumAddress.isValid(address), t.wallets_incorrect_address()),
-            tokenId: z
-                .string()
-                .min(1)
-                .refine((address) => !exclude.find((item) => item === address), t.wallets_collectible_been_added()),
+            tokenId: z.string().min(1),
         })
 
         const {
             control,
             handleSubmit,
             setError,
+            watch,
             formState: { errors },
         } = useForm<FormInputs>({
             resolver: zodResolver(schema),
-            defaultValues: {
-                address: '',
-                tokenId: '',
-            },
+            defaultValues: { address: '', tokenId: '' },
         })
+
+        useEffect(() => {
+            const subscription = watch((value) => {
+                onAddressChange(value.address)
+                onTokenIdChange(value.tokenId)
+            })
+            return () => subscription.unsubscribe()
+        }, [watch])
+
+        const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+            handleSubmit(onSubmit)(event).catch((error) => {
+                setError('tokenId', {
+                    type: 'value',
+                    message:
+                        error.message === FormErrorType.Added
+                            ? t.wallets_collectible_been_added()
+                            : t.wallets_collectible_error_not_exist(),
+                })
+            })
+        }
 
         return (
             <MaskDialog open={open} title={t.wallets_add_collectible()} onClose={onClose}>
-                <form onSubmit={handleSubmit(onSubmit)}>
+                <form onSubmit={handleFormSubmit}>
                     <DialogContent>
                         <Box>
                             <Controller
