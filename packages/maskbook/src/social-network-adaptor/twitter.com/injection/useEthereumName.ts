d90409e2489ad51c4f7d2e8720b17086a44e434a
UncleBill <billbill290@gmail.com>
2021-08-24 20:12:59 +0800

    fix(twitter): to avoid matching regular domain, ens regexp match common ens only (#4085)
    * fix(twitter): to avoid matching regular domain, ens regexp match common ens only
    
    * fixup! fix(twitter): to avoid matching regular domain, ens regexp match common ens only

--- packages/maskbook/src/social-network-adaptor/twitter.com/injection/useEthereumName.ts
+++ packages/maskbook/src/social-network-adaptor/twitter.com/injection/useEthereumName.ts
@@ -1,7 +1,7 @@
 import { useResolveEns } from '@masknet/web3-shared'
 import { useEffect, useMemo, useState } from 'react'
 
-const ENS_RE = /[\w#%+.:=@~-]{1,256}\.[\d()A-Za-z]{1,6}\b([\w#%&()+./:=?@~-]*)?/
+const ENS_RE = /[\w#%+.:=@~-]{1,256}\.(eth|kred|xyz|luxe)\b/
 const ENS_RE_FULL = new RegExp(`^${ENS_RE.source}$`)
 const ADDRESS = /0x[\dA-Fa-f]{40}/
 
