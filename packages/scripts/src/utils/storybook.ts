66310de81cdb33e90d942afddfcec997363aff3e
Jack Works <jackworks@protonmail.com>
2021-08-24 12:27:54 +0800

    fix: build

--- packages/scripts/src/utils/storybook.ts
+++ packages/scripts/src/utils/storybook.ts
@@ -1,7 +1,7 @@
 import { shell } from './run'
 export function createBuildStorybook6(cwd: string, output: string, name: string) {
     const fn = () => {
-        return shell.cwd(cwd)`npx build-storybook -o ${output} --quite`
+        return shell.cwd(cwd)`npx build-storybook -o ${output} --quiet`
     }
     fn.displayName = `${name}-storybook`
     fn.description = `Build storybook of ${name} to ${output}`
