5cf4ca097b3c666a28278383f97f6a4a5052f60c
Jack Works <jackworks@protonmail.com>
2021-08-24 13:25:50 +0800

    fix: build

--- packages/scripts/src/netlify/index.ts
+++ packages/scripts/src/netlify/index.ts
@@ -1,6 +1,7 @@
 import { series, src, dest } from 'gulp'
 import { createBuildStorybook6, NETLIFY_PATH, PKG_PATH, shell, task } from '../utils'
 import { resolve } from 'path'
+import { i18nCodegen } from '../codegen'
 const SNOWPACK_PATH = resolve(NETLIFY_PATH, 'snowpack')
 const STATIC_PATH = resolve(NETLIFY_PATH, 'storybook-static')
 
@@ -35,5 +36,5 @@ const icons = series(
         return from.pipe(to)
     },
 )
-export const buildNetlify = series(icons, dashboardSB, themeSB)
+export const buildNetlify = series(i18nCodegen, icons, dashboardSB, themeSB)
 task(buildNetlify, 'build-ci-netlify', 'Build for Netlify')
