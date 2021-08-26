bd82b89f4cee486f7e1df92508504d8a37e13467
Lantt <lanyixing@live.com>
2021-08-26 11:27:25 +0800

    refactor: use ui component for state bar

--- packages/dashboard/src/pages/Wallets/components/WalletStateBar/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/WalletStateBar/index.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react'
 import { Box, Button, Stack, Typography } from '@material-ui/core'
 import {
+    ProviderType,
     TransactionStatusType,
     useChainColor,
     useChainDetailed,
@@ -36,12 +37,12 @@ const useStyles = makeStyles()((theme) => ({
 
 export const WalletStateBar = memo(() => {
     const t = useDashboardI18N()
-    const { classes } = useStyles()
 
     const wallet = useWallet()
-    const chainColor = useChainColor()
     const { providerType } = useWeb3StateContext()
     const chainDetailed = useChainDetailed()
+    const chainColor = useChainColor()
+
     const { value: pendingTransactions = [] } = useRecentTransactions(TransactionStatusType.NOT_DEPEND)
 
     const { openDialog: openConnectWalletDialog } = useRemoteControlledDialog(
@@ -52,42 +53,71 @@ export const WalletStateBar = memo(() => {
         return <Button onClick={openConnectWalletDialog}>{t.wallets_connect_wallet_connect()}</Button>
     }
     return (
-        <Stack justifyContent="center" direction="row" alignItems="center">
-            <Stack
-                direction="row"
-                alignItems="center"
-                justifyContent="center"
-                sx={{ background: chainColor.replace(')', ', 0.1)'), px: 2, mr: 1 }}
-                color={chainColor}
-                className={classes.bar}
-                onClick={openConnectWalletDialog}>
-                <Typography component="span" sx={{ background: chainColor }} className={classes.dot} />
-                <Typography component="span" fontSize={12}>
-                    {chainDetailed?.chain}
-                </Typography>
-            </Stack>
-            {!!pendingTransactions.length && (
+        <WalletStateBarUI
+            isPending={!!pendingTransactions.length}
+            chain={chainDetailed?.chain}
+            chainColor={chainColor}
+            providerType={providerType}
+            openConnectWalletDialog={openConnectWalletDialog}
+            walletName={wallet.name ?? ''}
+            walletAddress={wallet.address}
+        />
+    )
+})
+
+interface WalletStateBarUIProps {
+    chain?: string
+    chainColor: string
+    isPending: boolean
+    providerType: ProviderType
+    walletName: string
+    walletAddress: string
+    openConnectWalletDialog(): void
+}
+
+const WalletStateBarUI = memo<WalletStateBarUIProps>(
+    ({ chain, isPending, providerType, chainColor, walletAddress, walletName, openConnectWalletDialog }) => {
+        const t = useDashboardI18N()
+        const { classes } = useStyles()
+
+        return (
+            <Stack justifyContent="center" direction="row" alignItems="center">
                 <Stack
                     direction="row"
                     alignItems="center"
                     justifyContent="center"
-                    sx={{ px: 2, background: MaskColorVar.orangeMain.alpha(0.1), color: MaskColorVar.orangeMain }}
-                    className={classes.bar}>
-                    <LoadingIcon sx={{ fontSize: 12, mr: 0.8, color: MaskColorVar.orangeMain }} />
-                    <Typography component="span" fontSize={12} display="inline-block">
-                        {t.wallet_transactions_pending()}
+                    sx={{ background: chainColor.replace(')', ', 0.1)'), px: 2, mr: 1 }}
+                    color={chainColor}
+                    className={classes.bar}
+                    onClick={openConnectWalletDialog}>
+                    <Typography component="span" sx={{ background: chainColor }} className={classes.dot} />
+                    <Typography component="span" fontSize={12}>
+                        {chain}
                     </Typography>
                 </Stack>
-            )}
-            <Stack mx={1} justifyContent="center">
-                <ProviderIcon providerType={providerType} />
-            </Stack>
-            <Box sx={{ userSelect: 'none' }}>
-                <Box fontSize={16}>{wallet.name}</Box>
-                <Box fontSize={12}>
-                    <FormattedAddress address={wallet.address} size={10} />
+                {isPending && (
+                    <Stack
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ px: 2, background: MaskColorVar.orangeMain.alpha(0.1), color: MaskColorVar.orangeMain }}
+                        className={classes.bar}>
+                        <LoadingIcon sx={{ fontSize: 12, mr: 0.8, color: MaskColorVar.orangeMain }} />
+                        <Typography component="span" fontSize={12} display="inline-block">
+                            {t.wallet_transactions_pending()}
+                        </Typography>
+                    </Stack>
+                )}
+                <Stack mx={1} justifyContent="center">
+                    <ProviderIcon providerType={providerType} />
+                </Stack>
+                <Box sx={{ userSelect: 'none' }}>
+                    <Box fontSize={16}>{walletName}</Box>
+                    <Box fontSize={12}>
+                        <FormattedAddress address={walletAddress} size={10} />
+                    </Box>
                 </Box>
-            </Box>
-        </Stack>
-    )
-})
+            </Stack>
+        )
+    },
+)
