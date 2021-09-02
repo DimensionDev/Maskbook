775c8943c7ec3e37564c9d1efa00685976f19a50
Lantt <lanyixing@live.com>
2021-09-02 18:34:22 +0800

    feat: email and phone format

--- packages/dashboard/src/components/Restore/steps/EmailField.tsx
+++ packages/dashboard/src/components/Restore/steps/EmailField.tsx
@@ -6,17 +6,30 @@ import { Button } from '@material-ui/core'
 import { Label, ValidationCodeStep } from './common'
 import type { StepCommonProps } from '../../Stepper'
 import { AccountType } from '../../../pages/Settings/type'
+import { emailRegexp } from '../../../pages/Settings/regexp'
 
 export const EmailField = memo(({ toStep }: StepCommonProps) => {
     const t = useDashboardI18N()
     const [account, setAccount] = useState<string>('')
+    const [invalidEmail, setInvalidEmail] = useState(false)
+
+    const validCheck = () => {
+        if (!account) return
+
+        const isValid = emailRegexp.test(account)
+        setInvalidEmail(!isValid)
+    }
+
     return (
         <>
             <MaskTextField
                 label={<Label onModeChange={() => toStep(ValidationCodeStep.PhoneInput)} mode={AccountType.email} />}
                 fullWidth
                 value={account}
+                onBlur={validCheck}
                 onChange={(event) => setAccount(event.target.value)}
+                error={invalidEmail}
+                helperText={invalidEmail ? t.sign_in_account_cloud_backup_email_format_error() : ''}
                 type="email"
             />
             <ButtonContainer>
@@ -26,7 +39,7 @@ export const EmailField = memo(({ toStep }: StepCommonProps) => {
                     onClick={() =>
                         toStep(ValidationCodeStep.AccountValidation, { account: account, type: AccountType.email })
                     }
-                    disabled={!account}>
+                    disabled={!account || invalidEmail}>
                     {t.next()}
                 </Button>
             </ButtonContainer>
