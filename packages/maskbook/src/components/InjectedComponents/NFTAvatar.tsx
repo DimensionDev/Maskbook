91935f449ef378b284f11f7f830b3316bd3997f7
lelenei <72531217+lelenei@users.noreply.github.com>
2021-09-08 14:26:36 +0800

    fix: account error

--- packages/maskbook/src/components/InjectedComponents/NFTAvatar.tsx
+++ packages/maskbook/src/components/InjectedComponents/NFTAvatar.tsx
@@ -94,7 +94,7 @@ export function NFTAvatar(props: NFTAvatarProps) {
         loading,
         retry,
         error,
-    } = useCollectibles('0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045', chainId, provider, page, 50)
+    } = useCollectibles(account, chainId, provider, page, 50)
     //0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
 
     const { collectibles, hasNextPage } = value
