879886523f662cea6aeac548ed8007500b68295f
Lantt <lanyixing@live.com>
2021-09-08 17:15:34 +0800

    fix: merge error

--- packages/dashboard/src/pages/Personas/components/PersonaDrawer/index.tsx
+++ packages/dashboard/src/pages/Personas/components/PersonaDrawer/index.tsx
@@ -64,7 +64,6 @@ export interface PersonaDrawerUIProps extends PersonaDrawer {
 
 export const PersonaDrawerUI = memo<PersonaDrawerUIProps>(
     ({ open, currentPersonaIdentifier, toggleDrawer, personas, onChangeCurrentPersona, onCreatePersona }) => {
-        const navigate = useNavigate()
         const { classes } = useStyles()
         const t = useDashboardI18N()
         const navigate = useNavigate()
