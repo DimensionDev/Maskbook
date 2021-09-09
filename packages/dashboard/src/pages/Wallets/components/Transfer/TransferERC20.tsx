178b64260aced54adba0a303280dbb377c1027d7
Lantt <lanyixing@live.com>
2021-09-09 19:26:32 +0800

    feat: dashboard-transfer (#4285)
    * feat: transfer token in dashboard
    
    * feat: improve
    
    * refactor: add todo
    
    * refactor: code style
    
    * chore: revert lock file
    
    Co-authored-by: guanbinrui <guanbinrui@dimension.im>

---
+++ packages/dashboard/src/pages/Wallets/components/Transfer/TransferERC20.tsx
@@ -0,0 +1,143 @@
+import { MaskTextField } from '@masknet/theme'
+import { Box, Button, Stack } from '@material-ui/core'
+import { memo, useCallback, useEffect, useMemo, useState } from 'react'
+import {
+    EthereumTokenType,
+    FungibleTokenDetailed,
+    isGreaterThan,
+    isZero,
+    pow10,
+    TransactionStateType,
+    useFungibleTokenBalance,
+    useGasPrice,
+    useTokenTransferCallback,
+} from '@masknet/web3-shared'
+import BigNumber from 'bignumber.js'
+import { TokenAmountPanel } from '@masknet/shared'
+import { SelectTokenDialog } from '../SelectTokenDialog'
+import { useDashboardI18N } from '../../../../locales'
+import { EthereumAddress } from 'wallet.ts'
+
+interface TransferERC20Props {
+    token: FungibleTokenDetailed
+}
+
+export const TransferERC20 = memo<TransferERC20Props>(({ token }) => {
+    const t = useDashboardI18N()
+    const [amount, setAmount] = useState('')
+    const [address, setAddress] = useState('')
+    const [memo, setMemo] = useState('')
+    const [selectedToken, setToken] = useState<FungibleTokenDetailed>(token)
+    const [isOpenSelectTokenDialog, openSelectTokenDialog] = useState(false)
+
+    // gas price
+    const { value: gasPrice = '0' } = useGasPrice()
+
+    // balance
+    const { value: tokenBalance = '0', retry: tokenBalanceRetry } = useFungibleTokenBalance(
+        selectedToken?.type ?? EthereumTokenType.Native,
+        selectedToken?.address ?? '',
+    )
+
+    // transfer amount
+    const transferAmount = new BigNumber(amount || '0').multipliedBy(pow10(token.decimals)).toFixed()
+    const maxAmount = useMemo(() => {
+        let amount_ = new BigNumber(tokenBalance || '0')
+        amount_ =
+            selectedToken.type === EthereumTokenType.Native
+                ? amount_.minus(new BigNumber(30000).multipliedBy(gasPrice))
+                : amount_
+        return amount_.toFixed()
+    }, [tokenBalance, gasPrice, selectedToken?.type, amount])
+
+    const [transferState, transferCallback, resetTransferCallback] = useTokenTransferCallback(
+        selectedToken.type,
+        selectedToken.address,
+        new BigNumber(amount).multipliedBy(pow10(selectedToken.decimals)).toFixed(),
+        address,
+        memo,
+    )
+
+    const onTransfer = useCallback(async () => {
+        await transferCallback()
+    }, [transferCallback])
+
+    //#region validation
+    const validationMessage = useMemo(() => {
+        if (!transferAmount || isZero(transferAmount)) return t.wallets_transfer_error_amount_absence()
+        if (isGreaterThan(new BigNumber(amount).multipliedBy(pow10(selectedToken.decimals)).toFixed(), transferAmount))
+            return t.wallets_transfer_error_insufficient_balance({ symbol: selectedToken.symbol ?? '' })
+        if (!address) return t.wallets_transfer_error_address_absence()
+        if (!EthereumAddress.isValid(address)) return t.wallets_transfer_error_invalid_address()
+        return ''
+    }, [transferAmount, address, tokenBalance, selectedToken, amount])
+    //#endregion
+
+    useEffect(() => {
+        if (transferState.type === TransactionStateType.FAILED || transferState.type === TransactionStateType.HASH) {
+            setMemo('')
+            setAddress('')
+            setAmount('')
+            resetTransferCallback()
+        }
+    }, [transferState])
+
+    return (
+        <Stack direction="row" justifyContent="center" mt={4}>
+            <Stack maxWidth={640} minWidth={500} alignItems="center">
+                <Box width="100%">
+                    <MaskTextField
+                        value={address}
+                        onChange={(e) => setAddress(e.currentTarget.value)}
+                        label={t.wallets_transfer_to_address()}
+                    />
+                </Box>
+                <Box mt={2} width="100%">
+                    <TokenAmountPanel
+                        amount={amount}
+                        maxAmount={maxAmount}
+                        balance={tokenBalance}
+                        label={t.wallets_transfer_amount()}
+                        token={selectedToken}
+                        onAmountChange={setAmount}
+                        SelectTokenChip={{
+                            loading: false,
+                            ChipProps: {
+                                onClick: () => openSelectTokenDialog(true),
+                            },
+                        }}
+                    />
+                </Box>
+                <Box mt={2} width="100%">
+                    <MaskTextField
+                        value={memo}
+                        placeholder={t.wallets_transfer_memo_placeholder()}
+                        disabled={selectedToken.type === EthereumTokenType.ERC20}
+                        onChange={(e) => setMemo(e.currentTarget.value)}
+                        label={t.wallets_transfer_memo()}
+                    />
+                </Box>
+                <Box mt={4}>
+                    <Button
+                        sx={{ width: 240 }}
+                        disabled={
+                            !!validationMessage || transferState.type === TransactionStateType.WAIT_FOR_CONFIRMING
+                        }
+                        onClick={onTransfer}>
+                        {validationMessage || t.wallets_transfer_send()}
+                    </Button>
+                </Box>
+            </Stack>
+            {isOpenSelectTokenDialog && (
+                <SelectTokenDialog
+                    onSelect={(token) => {
+                        setToken(token!)
+                        openSelectTokenDialog(false)
+                    }}
+                    open={isOpenSelectTokenDialog}
+                    onClose={() => openSelectTokenDialog(false)}
+                />
+            )}
+        </Stack>
+    )
+})
---
+++ packages/shared/src/UI/components/SelectTokenChip/index.tsx
@@ -0,0 +1,100 @@
+import classNames from 'classnames'
+import { Chip, ChipProps, CircularProgress } from '@material-ui/core'
+import { makeStyles } from '@masknet/theme'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import ErrorIcon from '@material-ui/icons/Error'
+import { noop } from 'lodash-es'
+import type { FungibleTokenDetailed } from '@masknet/web3-shared'
+import { useSharedI18N } from '../../../locales'
+import { TokenIcon } from '../TokenIcon'
+
+const useStyles = makeStyles()((theme) => {
+    return {
+        chip: {
+            border: 'none',
+            borderRadius: 8,
+            paddingLeft: theme.spacing(0.5),
+            fontSize: 12,
+        },
+        loadingChip: {
+            marginRight: theme.spacing(-0.5),
+            fontSize: 12,
+        },
+        icon: {
+            color: theme.palette.text.primary,
+            pointerEvents: 'none',
+        },
+        tokenIcon: {
+            width: 16,
+            height: 16,
+        },
+    }
+})
+
+export interface SelectTokenChipProps {
+    token?: FungibleTokenDetailed | null
+    error?: Error
+    loading?: boolean
+    readonly?: boolean
+    ChipProps?: Partial<ChipProps>
+}
+
+// todo: merge into one with maskbook SelectTokenChip
+export function SelectTokenChip(props: SelectTokenChipProps) {
+    const t = useSharedI18N()
+    const { token, error, loading = false, readonly = false, ChipProps } = props
+    const { classes } = useStyles()
+    if (loading)
+        return (
+            <Chip
+                className={classNames(classes.chip, classes.loadingChip)}
+                icon={<CircularProgress size={16} />}
+                size="small"
+                clickable={false}
+                variant="outlined"
+            />
+        )
+    if (!token)
+        return (
+            <Chip className={classes.chip} label={t.select_token()} size="small" clickable={!readonly} {...ChipProps} />
+        )
+
+    if (token && error)
+        return (
+            <Chip
+                className={classes.chip}
+                icon={<TokenIcon address={token.address} name={token.name} logoURI={token.logoURI} />}
+                deleteIcon={<ErrorIcon className={classes.icon} />}
+                label={token.symbol}
+                color="default"
+                size="small"
+                variant="outlined"
+                clickable={!readonly}
+                // the delete icon only visible when this callback provided
+                onDelete={noop}
+                {...ChipProps}
+            />
+        )
+    return (
+        <Chip
+            className={classes.chip}
+            icon={
+                <TokenIcon
+                    classes={{ icon: classes.tokenIcon }}
+                    address={token.address}
+                    name={token.name}
+                    logoURI={token.logoURI}
+                />
+            }
+            deleteIcon={readonly ? undefined : <ExpandMoreIcon className={classes.icon} />}
+            color="default"
+            size="small"
+            variant="outlined"
+            clickable={!readonly}
+            label={token.symbol}
+            // the delete icon only visible when this callback provided
+            onDelete={readonly ? undefined : noop}
+            {...ChipProps}
+        />
+    )
+}
