8d8b1f35a7fd69447a37b636fc43663c76ae317a
Jack Works <jackworks@protonmail.com>
2021-09-10 18:00:53 +0800

    fix: typo

--- packages/mask-sdk/main/persona.ts
+++ packages/mask-sdk/main/persona.ts
@@ -1,7 +1,7 @@
 import { contentScript } from './bridge'
 export const persona: Mask.Persona = {
     __experimental__sign__(message, style) {
-        if (style !== 'web3') throw new TypeError('Unsupported persona signautre style. Supported style: web3')
+        if (style !== 'web3') throw new TypeError('Unsupported persona sign style. Supported style: web3')
 
         return contentScript.persona_sign_web3(message)
     },
