a0b0b2418c0c4e74d72373d1b97899be0c2b06da
unclebill <billbill290@gmail.com>
2021-09-10 13:52:49 +0800

    fix: fix wrong fn name

--- packages/maskbook/src/plugins/RedPacket/Worker/servicesForNft.ts
+++ packages/maskbook/src/plugins/RedPacket/Worker/servicesForNft.ts
@@ -1,10 +1,10 @@
-import type { RedPacketNftRecord } from '../types'
+import type { RedPacketNftRecord, RedPacketNftRecordInDatabase } from '../types'
 import * as database from './databaseForNft'
 
 export async function addRedPacketNft(record: RedPacketNftRecord) {
     database.addRedPacketNft(record)
 }
 
-export async function updateRedPacketNftPassword(id: string, password: string) {
-    database.updateRedPacketNftPassword(id, password)
+export async function updateRedPacketNft(newRecord: RedPacketNftRecordInDatabase) {
+    database.updateRedPacketNft(newRecord)
 }
