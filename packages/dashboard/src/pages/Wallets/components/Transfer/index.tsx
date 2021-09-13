060e05bcd72665cfbf9a1c2d7e165c2f769cacdd
Lantt <lanyixing@live.com>
2021-09-13 11:23:00 +0800

    fix: transfer type error (#4329)

--- packages/dashboard/src/pages/Wallets/components/Transfer/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/Transfer/index.tsx
@@ -9,11 +9,11 @@ import { useLocation } from 'react-router-dom'
 
 export const Transfer = memo(() => {
     // todo: token and chain
-    const { state } = useLocation() as { state: { token: FungibleTokenDetailed } }
+    const { state } = useLocation() as { state: { token: FungibleTokenDetailed } | null }
     const { value: nativeToken } = useNativeTokenDetailed()
     const [currentTab, onChange, tabs] = useTabs('tokens', 'collections')
 
-    if (!nativeToken && !state.token) return null
+    if (!nativeToken && !state?.token) return null
 
     return (
         <ContentContainer sx={{ marginTop: 3, display: 'flex', flexDirection: 'column' }}>
@@ -24,7 +24,7 @@ export const Transfer = memo(() => {
                         <Tab label="Collections" value={tabs.collections} />
                     </TabList>
                     <TabPanel value={tabs.tokens}>
-                        <TransferERC20 token={state.token || nativeToken} />
+                        <TransferERC20 token={state?.token! || nativeToken} />
                     </TabPanel>
                     <TabPanel value={tabs.collections}>todo</TabPanel>
                 </TabContext>
