bfb0acaaccdf057c395e92719a5de0aef96ec2c7
Lantt <lanyixing@live.com>
2021-09-13 15:31:08 +0800

    refactor: apply feedback

--- packages/theme/src/Components/TextField/PasswordField.tsx
+++ packages/theme/src/Components/TextField/PasswordField.tsx
@@ -4,20 +4,20 @@ import { Visibility, VisibilityOff } from '@material-ui/icons'
 import { IconButton, InputAdornment } from '@material-ui/core'
 
 export const MaskPasswordTextField = memo<MaskTextFieldProps>((props) => {
-    const [isShowPassword, setIsShowPassword] = useState(false)
+    const [hidden, setHidden] = useState(false)
 
     return (
         <MaskTextField
             {...props}
-            type={isShowPassword ? 'text' : 'password'}
+            type={hidden ? 'text' : 'password'}
             InputProps={{
                 endAdornment: (
                     <InputAdornment position="end" sx={{ mr: 1 }}>
                         <IconButton
-                            onClick={() => setIsShowPassword(!isShowPassword)}
-                            onMouseDown={() => setIsShowPassword(!isShowPassword)}
+                            onClick={() => setHidden(!hidden)}
+                            onMouseDown={() => setHidden(!hidden)}
                             edge="end">
-                            {isShowPassword ? <VisibilityOff fontSize="small" /> : <Visibility fontSize="small" />}
+                            {hidden ? <VisibilityOff fontSize="small" /> : <Visibility fontSize="small" />}
                         </IconButton>
                     </InputAdornment>
                 ),
