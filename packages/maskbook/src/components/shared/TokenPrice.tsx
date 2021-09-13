267e2df71073c85b7a9d3bceaa4b669bb16abe83
unclebill <billbill290@gmail.com>
2021-09-13 15:07:29 +0800

    fix: let TokenPrice inherit HTMLSpanElement

--- packages/maskbook/src/components/shared/TokenPrice.tsx
+++ packages/maskbook/src/components/shared/TokenPrice.tsx
@@ -1,14 +1,21 @@
+import type { FC, HTMLProps } from 'react'
 import { ChainId, CurrencyType } from '@masknet/web3-shared'
 import { useTokenPrice } from '../../plugins/Wallet/hooks/useTokenPrice'
 
-interface TokenPriceProps {
+interface TokenPriceProps extends Omit<HTMLProps<HTMLSpanElement>, 'children'> {
     chainId: ChainId
     contractAddress: string | undefined
     amount: number
     currencyType?: CurrencyType
 }
 
-export const TokenPrice = ({ chainId, contractAddress, amount, currencyType = CurrencyType.USD }: TokenPriceProps) => {
-    const price = useTokenPrice(chainId, contractAddress, currencyType) * amount
-    return <span>${price.toFixed(2)}</span>
+export const TokenPrice: FC<TokenPriceProps> = ({
+    chainId,
+    contractAddress,
+    amount,
+    currencyType = CurrencyType.USD,
+    ...rest
+}) => {
+    const price = useTokenPrice(chainId, contractAddress, currencyType)
+    return <span {...rest}>${(price * amount).toFixed(2)}</span>
 }
