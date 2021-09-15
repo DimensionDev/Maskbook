a9371add6715e9bf51626006d02b0f75025ade85
Hom Yan <yanzhihong23@qq.com>
2021-09-15 15:34:23 +0800

    feat: arbitrum & xDai swap setting

--- packages/dashboard/src/pages/Labs/components/SwapSettingDialog.tsx
+++ packages/dashboard/src/pages/Labs/components/SwapSettingDialog.tsx
@@ -7,6 +7,8 @@ import {
     useBinanceNetworkTradeProvider,
     useEthereumNetworkTradeProvider,
     usePolygonNetworkTradeProvider,
+    useArbitrumNetworkTradeProvider,
+    usexDaiNetworkTradeProvider,
 } from '../../Settings/api'
 
 import SettingItem from './SettingItem'
@@ -29,42 +31,68 @@ export default function SwapSettingDialog({ open, onClose }: SettingDialogProps)
     const polygonOptions = [
         { label: 'QuickSwap', value: TradeProvider.QUICKSWAP },
         { label: 'SushiSwap', value: TradeProvider.SUSHISWAP },
+        { label: '0x', value: TradeProvider.ZRX },
         { label: 'DODO', value: TradeProvider.DODO },
     ]
 
     const bscOptions = [
         { label: 'PancakeSwap', value: TradeProvider.PANCAKESWAP },
         { label: 'SushiSwap', value: TradeProvider.SUSHISWAP },
+        { label: '0x', value: TradeProvider.ZRX },
         { label: 'DODO', value: TradeProvider.DODO },
     ]
 
+    const arbitrumOptions = [{ label: 'UniSwap V3', value: TradeProvider.UNISWAP_V3 }]
+
+    const xDaiOptions = [{ label: 'SushiSwap', value: TradeProvider.SUSHISWAP }]
+
     const t = useDashboardI18N()
 
-    const ethNetworkTradeProvider = useEthereumNetworkTradeProvider()
-    const polygonNetworkTradeProvider = usePolygonNetworkTradeProvider()
-    const bscNetworkTradeProvider = useBinanceNetworkTradeProvider()
+    const items = [
+        {
+            legend: t.labs_settings_swap_network({ network: 'ETH' }),
+            value: useEthereumNetworkTradeProvider(),
+            options: ethOptions,
+            onChange: (value: any) => Services.Settings.setEthNetworkTradeProvider(+value),
+        },
+        {
+            legend: t.labs_settings_swap_network({ network: 'Polygon/Matic' }),
+            value: usePolygonNetworkTradeProvider(),
+            options: polygonOptions,
+            onChange: (value: any) => Services.Settings.setPolygonNetworkTradeProvider(+value),
+        },
+        {
+            legend: t.labs_settings_swap_network({ network: 'BSC' }),
+            value: useBinanceNetworkTradeProvider(),
+            options: bscOptions,
+            onChange: (value: any) => Services.Settings.setBinanceNetworkTradeProvider(+value),
+        },
+        {
+            legend: t.labs_settings_swap_network({ network: 'Arbitrum' }),
+            value: useArbitrumNetworkTradeProvider(),
+            options: arbitrumOptions,
+            onChange: (value: any) => Services.Settings.setArbitrumNetworkTradeProvider(+value),
+        },
+        {
+            legend: t.labs_settings_swap_network({ network: 'xDai' }),
+            value: usexDaiNetworkTradeProvider(),
+            options: xDaiOptions,
+            onChange: (value: any) => Services.Settings.setxDaiNetworkTradeProvider(+value),
+        },
+    ]
 
     return (
         <MaskDialog title={t.labs_settings_swap()} open={open} onClose={onClose}>
             <DialogContent sx={{ padding: '16px 40px 24px' }}>
-                <SettingItem
-                    legend={t.labs_settings_swap_eth()}
-                    value={ethNetworkTradeProvider}
-                    options={ethOptions}
-                    onChange={(value) => Services.Settings.setEthNetworkTradeProvider(+value)}
-                />
-                <SettingItem
-                    legend={t.labs_settings_swap_polygon()}
-                    value={polygonNetworkTradeProvider}
-                    options={polygonOptions}
-                    onChange={(value) => Services.Settings.setPolygonNetworkTradeProvider(+value)}
-                />
-                <SettingItem
-                    legend={t.labs_settings_swap_bsc()}
-                    value={bscNetworkTradeProvider}
-                    options={bscOptions}
-                    onChange={(value) => Services.Settings.setBinanceNetworkTradeProvider(+value)}
-                />
+                {items.map((i, idx) => (
+                    <SettingItem
+                        legend={i.legend}
+                        value={i.value}
+                        options={i.options}
+                        onChange={i.onChange}
+                        key={idx}
+                    />
+                ))}
             </DialogContent>
         </MaskDialog>
     )
