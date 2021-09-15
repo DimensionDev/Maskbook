33de0b8eb1dcf221e669f9543c1a71fee8a28aad
Lantt <lanyixing@live.com>
2021-09-15 11:41:28 +0800

    feat: show asset in dashboard wallet by network

--- packages/dashboard/src/pages/Wallets/components/TokenTable/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/TokenTable/index.tsx
@@ -1,23 +1,19 @@
-import { Dispatch, memo, SetStateAction, useCallback, useState } from 'react'
-import {
-    Box,
-    Pagination,
-    PaginationItem,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-} from '@material-ui/core'
+import { memo, useCallback } from 'react'
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
 import { makeStyles, MaskColorVar } from '@masknet/theme'
 import { useDashboardI18N } from '../../../../locales'
 import { EmptyPlaceholder } from '../EmptyPlaceholder'
 import { LoadingPlaceholder } from '../../../../components/LoadingPlaceholder'
 import { TokenTableRow } from '../TokenTableRow'
-import { Asset, formatBalance, FungibleTokenDetailed, useAssets, useERC20TokensPaged } from '@masknet/web3-shared'
+import {
+    Asset,
+    formatBalance,
+    FungibleTokenDetailed,
+    useAssets,
+    useChainId,
+    useTrustedERC20Tokens,
+} from '@masknet/web3-shared'
 import BigNumber from 'bignumber.js'
-import { ceil } from 'lodash-es'
 import { useRemoteControlledDialog } from '@masknet/shared'
 import { PluginMessages } from '../../../../API'
 import { RoutePaths } from '../../../../type'
@@ -58,19 +54,18 @@ const useStyles = makeStyles()((theme) => ({
     },
 }))
 
-// todo: show asset by chain
 export const TokenTable = memo(() => {
     const navigate = useNavigate()
+    const chainId = useChainId()
 
-    const [page, setPage] = useState(1)
-    const { value } = useERC20TokensPaged(page - 1, 50)
+    const trustedERC20Tokens = useTrustedERC20Tokens()
     const { setDialog: openSwapDialog } = useRemoteControlledDialog(PluginMessages.Swap.swapDialogUpdated)
 
     const {
         error: detailedTokensError,
         loading: detailedTokensLoading,
         value: detailedTokens,
-    } = useAssets(value?.tokens || [])
+    } = useAssets(trustedERC20Tokens || [])
 
     const onSwap = useCallback((token: FungibleTokenDetailed) => {
         openSwapDialog({
@@ -94,13 +89,9 @@ export const TokenTable = memo(() => {
 
     return (
         <TokenTableUI
-            page={page}
-            onPageChange={setPage}
             isLoading={detailedTokensLoading}
             isEmpty={!!detailedTokensError || !detailedTokens.length}
-            showPagination={!detailedTokensLoading && !detailedTokensError && !!detailedTokens.length}
             dataSource={detailedTokens}
-            count={ceil((!!value?.count ? value.count : 1) / 50) ?? 1}
             onSwap={onSwap}
             onSend={onSend}
         />
@@ -108,94 +99,74 @@ export const TokenTable = memo(() => {
 })
 
 export interface TokenTableUIProps {
-    page: number
-    onPageChange: Dispatch<SetStateAction<number>>
     isLoading: boolean
     isEmpty: boolean
-    showPagination: boolean
     dataSource: Asset[]
-    count: number
     onSwap(token: FungibleTokenDetailed): void
     onSend(token: FungibleTokenDetailed): void
 }
 
-export const TokenTableUI = memo<TokenTableUIProps>(
-    ({ page, onSwap, onSend, onPageChange, isLoading, isEmpty, showPagination, dataSource, count }) => {
-        const t = useDashboardI18N()
-        const { classes } = useStyles()
-        return (
-            <>
-                <TableContainer className={classes.container}>
-                    {isLoading || isEmpty ? (
-                        <Box flex={1}>
-                            {isLoading ? <LoadingPlaceholder /> : null}
-                            {isEmpty ? <EmptyPlaceholder children={t.wallets_empty_tokens_tip()} /> : null}
-                        </Box>
-                    ) : (
-                        <Table stickyHeader sx={{ padding: '0 44px' }}>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell key="Asset" align="center" variant="head" className={classes.header}>
-                                        {t.wallets_assets_asset()}
-                                    </TableCell>
-                                    <TableCell key="Balance" align="center" variant="head" className={classes.header}>
-                                        {t.wallets_assets_balance()}
-                                    </TableCell>
-                                    <TableCell key="Price" align="center" variant="head" className={classes.header}>
-                                        {t.wallets_assets_price()}
-                                    </TableCell>
-                                    <TableCell key="Value" align="center" variant="head" className={classes.header}>
-                                        {t.wallets_assets_value()}
-                                    </TableCell>
-                                    <TableCell key="Operation" align="center" variant="head" className={classes.header}>
-                                        {t.wallets_assets_operation()}
-                                    </TableCell>
-                                </TableRow>
-                            </TableHead>
-
-                            {dataSource.length ? (
-                                <TableBody>
-                                    {dataSource
-                                        .sort((first, second) => {
-                                            const firstValue = new BigNumber(
-                                                formatBalance(first.balance, first.token.decimals),
-                                            )
-                                            const secondValue = new BigNumber(
-                                                formatBalance(second.balance, second.token.decimals),
-                                            )
-
-                                            if (firstValue.eq(secondValue)) return 0
-
-                                            return Number(firstValue.lt(secondValue))
-                                        })
-                                        .map((asset, index) => (
-                                            <TokenTableRow
-                                                onSend={() => onSend(asset.token)}
-                                                onSwap={() => onSwap(asset.token)}
-                                                asset={asset}
-                                                key={index}
-                                            />
-                                        ))}
-                                </TableBody>
-                            ) : null}
-                        </Table>
-                    )}
-                </TableContainer>
-                {showPagination && !isEmpty ? (
-                    <Box className={classes.footer}>
-                        <Pagination
-                            variant="outlined"
-                            shape="rounded"
-                            count={count}
-                            page={page}
-                            onChange={(event, page) => onPageChange(page)}
-                            renderItem={(item) => (
-                                <PaginationItem {...item} classes={{ root: classes.paginationItem }} />
-                            )}
-                        />
+export const TokenTableUI = memo<TokenTableUIProps>(({ onSwap, onSend, isLoading, isEmpty, dataSource }) => {
+    const t = useDashboardI18N()
+    const { classes } = useStyles()
+    return (
+        <>
+            <TableContainer className={classes.container}>
+                {isLoading || isEmpty ? (
+                    <Box flex={1}>
+                        {isLoading ? <LoadingPlaceholder /> : null}
+                        {isEmpty ? <EmptyPlaceholder children={t.wallets_empty_tokens_tip()} /> : null}
                     </Box>
-                ) : null}
-            </>
-        )
-    },
-)
+                ) : (
+                    <Table stickyHeader sx={{ padding: '0 44px' }}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell key="Asset" align="center" variant="head" className={classes.header}>
+                                    {t.wallets_assets_asset()}
+                                </TableCell>
+                                <TableCell key="Balance" align="center" variant="head" className={classes.header}>
+                                    {t.wallets_assets_balance()}
+                                </TableCell>
+                                <TableCell key="Price" align="center" variant="head" className={classes.header}>
+                                    {t.wallets_assets_price()}
+                                </TableCell>
+                                <TableCell key="Value" align="center" variant="head" className={classes.header}>
+                                    {t.wallets_assets_value()}
+                                </TableCell>
+                                <TableCell key="Operation" align="center" variant="head" className={classes.header}>
+                                    {t.wallets_assets_operation()}
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+
+                        {dataSource.length ? (
+                            <TableBody>
+                                {dataSource
+                                    .sort((first, second) => {
+                                        const firstValue = new BigNumber(
+                                            formatBalance(first.balance, first.token.decimals),
+                                        )
+                                        const secondValue = new BigNumber(
+                                            formatBalance(second.balance, second.token.decimals),
+                                        )
+
+                                        if (firstValue.eq(secondValue)) return 0
+
+                                        return Number(firstValue.lt(secondValue))
+                                    })
+                                    .map((asset, index) => (
+                                        <TokenTableRow
+                                            onSend={() => onSend(asset.token)}
+                                            onSwap={() => onSwap(asset.token)}
+                                            asset={asset}
+                                            key={index}
+                                        />
+                                    ))}
+                            </TableBody>
+                        ) : null}
+                    </Table>
+                )}
+            </TableContainer>
+        </>
+    )
+})
