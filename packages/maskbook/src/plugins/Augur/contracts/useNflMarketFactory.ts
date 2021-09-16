aaa55247fb199b5d9ce5ef10bb009811177b3f54
Arash Fatahzade <fatahzade@gmail.com>
2021-09-16 16:26:14 +0430

    feat: nfl contracts

---
+++ packages/maskbook/src/plugins/Augur/contracts/useNflMarketFactory.ts
@@ -0,0 +1,8 @@
+import type { AbiItem } from 'web3-utils'
+import NflMarketFactoryABI from '@masknet/web3-contracts/abis/AugurMmaLinkMarketFactory.json'
+import type { AugurNflMarketFactory } from '@masknet/web3-contracts/types/AugurNflMarketFactory'
+import { useContract } from '@masknet/web3-shared'
+
+export function useNflkMarketFactory(address: string) {
+    return useContract<AugurNflMarketFactory>(address, NflMarketFactoryABI as AbiItem[])
+}
