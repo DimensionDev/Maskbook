fd0b67197e63e49b5fea7f59440ff59e018bd886
UncleBill <billbill290@gmail.com>
2021-09-17 13:03:45 +0800

    fix: typo of EIP1559 (#4376)

--- packages/maskbook/src/utils/flags.ts
+++ packages/maskbook/src/utils/flags.ts
@@ -46,7 +46,7 @@ export const Flags = {
     plugin_switch_enabled: betaOrInsiderOnly,
     //#endregion
 
-    EIP1159_enabled: false,
+    EIP1559_enabled: false,
 
     bsc_enabled: true,
     polygon_enabled: true,
