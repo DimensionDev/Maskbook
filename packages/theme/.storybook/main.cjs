c0ae6f16dd96a1312d785b6c3ac6b73d43a69d91
Jack Works <jackworks@protonmail.com>
2021-09-18 11:01:09 +0800

    feat: upgrade sb to webpack 5 (#4383)
    * feat: upgrade sb to webpack 5
    
    * fix: storybook

--- packages/theme/.storybook/main.cjs
+++ packages/theme/.storybook/main.cjs
@@ -1,17 +1,30 @@
 module.exports = {
     stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    addons: [
+        '@storybook/addon-links',
+        {
+            name: '@storybook/addon-essentials',
+            options: {
+                // https://github.com/storybookjs/storybook/issues/15901
+                // docs not compatible with webpack 5.
+                docs: false,
+            },
+        },
+    ],
     reactOptions: {
         fastRefresh: true,
     },
     webpackFinal: async (config) => {
-        config.module.rules.push(
-            {
-                test: /\.m?js$/,
-                type: 'javascript/auto',
+        config.module.rules.push({
+            test: /\.m?js$/,
+            type: 'javascript/auto',
+            resolve: {
+                fullySpecified: false, // disable the behaviour
             },
-            {},
-        )
+        })
         return config
+    },
+    core: {
+        builder: 'webpack5',
     },
 }
