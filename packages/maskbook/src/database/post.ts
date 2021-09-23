1840d95a5efc47ad2238d29a7189d5a18a1484e4
Lantt <lanyixing@live.com>
2021-09-23 14:17:55 +0800

    fix: filter by user ids in post history

--- packages/maskbook/src/database/post.ts
+++ packages/maskbook/src/database/post.ts
@@ -271,6 +271,7 @@ export async function queryPostPagedDB(
     linked: PersonaIdentifier,
     options: {
         network: string
+        userIds: string[]
         after?: PostIVIdentifier
     },
     count: number,
@@ -282,6 +283,7 @@ export async function queryPostPagedDB(
 
     for await (const cursor of t.objectStore('post').iterate()) {
         if (cursor.value.encryptBy !== linked.toText()) continue
+        if (!options.userIds.includes(cursor.value.postBy.userId)) continue
 
         const postIdentifier = Identifier.fromString(cursor.value.identifier, PostIVIdentifier).unwrap()
         if (postIdentifier.network !== options.network) continue
