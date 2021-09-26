5ec60d02b4bc6d19c783f828e794c59c184b1064
Lantt <lanyixing@live.com>
2021-09-26 15:37:24 +0800

    feat: improve the token list (#4381)
    * feat: improve the token list
    
    * feat: add remote select erc20 token dialog
    
    * fix: search result include in list
    
    * fix: rename erc20 dialog name
    
    * fix: revert fuse config for token list

--- packages/maskbook/src/plugins/Wallet/SNSAdaptor/index.tsx
+++ packages/maskbook/src/plugins/Wallet/SNSAdaptor/index.tsx
@@ -15,6 +15,7 @@ import { ConnectWalletDialog } from './ConnectWalletDialog'
 import { useStartWatchChainState } from '../hooks/useStartWatchChainState'
 import { WalletRiskWarningDialog } from './RiskWarningDialog'
 import { GasSettingDialog } from './GasSettingDialog'
+import { ERC20TokenListDialog } from './RRC20TokenListDialog'
 
 const sns: Plugin.SNSAdaptor.Definition = {
     ...base,
@@ -37,6 +38,7 @@ const sns: Plugin.SNSAdaptor.Definition = {
                 <WalletRenameWalletDialog />
                 <WalletRiskWarningDialog />
                 <GasSettingDialog />
+                <ERC20TokenListDialog />
             </>
         )
     },
