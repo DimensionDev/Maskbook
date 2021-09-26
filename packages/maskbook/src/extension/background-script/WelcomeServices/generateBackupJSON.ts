6486c313fc75fc2edaf96bfc168e8f324eccee5b
guanbinrui <guanbinrui@dimension.im>
2021-09-26 10:34:04 +0800

    fix: backup wallet

--- packages/maskbook/src/extension/background-script/WelcomeServices/generateBackupJSON.ts
+++ packages/maskbook/src/extension/background-script/WelcomeServices/generateBackupJSON.ts
@@ -108,9 +108,8 @@ export async function generateBackupJSON(opts: Partial<BackupOptions> = {}): Pro
             ).map(async (wallet) => {
                 return {
                     ...wallet,
-                    mnemonic: wallet.derivationPath
-                        ? await exportMnemonic(wallet.address)
-                        : await exportPrivateKey(wallet.address),
+                    mnemonic: wallet.derivationPath ? await exportMnemonic(wallet.address) : undefined,
+                    privateKey: wallet.derivationPath ? undefined : await exportPrivateKey(wallet.address),
                 }
             }),
         )
