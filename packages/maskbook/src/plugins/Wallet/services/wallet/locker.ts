b40926a8368e376b1af0ecccb2e83518b18b4bcf
nuanyang233 <nuanyang233@gmail.com>
2021-09-27 04:18:56 +0800

    fix: lock status

--- packages/maskbook/src/plugins/Wallet/services/wallet/locker.ts
+++ packages/maskbook/src/plugins/Wallet/services/wallet/locker.ts
@@ -1,6 +1,7 @@
 import { currentMaskWalletLockStatusSettings } from '../../settings'
 import * as password from './password'
 import { LockStatus } from '@masknet/web3-shared'
+import { WalletMessages } from '../../messages'
 
 export async function isLocked() {
     return (await password.hasPassword()) && !(await password.INTERNAL_getPassword())
@@ -9,6 +10,7 @@ export async function isLocked() {
 export async function lockWallet() {
     password.clearPassword()
     currentMaskWalletLockStatusSettings.value = LockStatus.LOCKED
+    WalletMessages.events.walletLockStatusUpdated.sendToAll(true)
 }
 
 export async function unlockWallet(unverifiedPassword: string) {
@@ -17,8 +19,10 @@ export async function unlockWallet(unverifiedPassword: string) {
         await password.verifyPasswordRequired(unverifiedPassword)
         password.INTERNAL_setPassword(unverifiedPassword)
         currentMaskWalletLockStatusSettings.value = LockStatus.UNLOCK
+        WalletMessages.events.walletLockStatusUpdated.sendToAll(false)
         return true
     } catch {
+        WalletMessages.events.walletLockStatusUpdated.sendToAll(false)
         return false
     }
 }
