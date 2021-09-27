82251d43b7c99c63de87c1cec384907ebc50d75b
Jack Works <jackworks@protonmail.com>
2021-09-27 13:40:12 +0800

    fix: permission request on firefox, close #4479, close #4478

--- packages/maskbook/src/extension/background-script/WelcomeService.ts
+++ packages/maskbook/src/extension/background-script/WelcomeService.ts
@@ -7,7 +7,7 @@ import { deriveLocalKeyFromECDHKey } from '../../utils/mnemonic-code/localKeyGen
 import type { PersonaIdentifier, ProfileIdentifier } from '../../database/type'
 import { BackupOptions, generateBackupJSON } from './WelcomeServices/generateBackupJSON'
 import type { AESJsonWebKey } from '../../modules/CryptoAlgorithm/interfaces/utils'
-import { saveAsFileFromBuffer } from './HelperService'
+import { requestBrowserPermission, saveAsFileFromBuffer } from './HelperService'
 import type { DashboardRoute } from '../options-page/Route'
 import {
     BackupJSONFileLatest,
@@ -16,7 +16,7 @@ import {
 } from '../../utils/type-transform/BackupFormat/JSON/latest'
 
 import { assertEnvironment, Environment } from '@dimensiondev/holoflows-kit'
-import { decompressBackupFile, extraPermissions, requestPermissions } from '../../utils'
+import { decompressBackupFile, extraPermissions } from '../../utils'
 import { v4 as uuid } from 'uuid'
 import { getUnconfirmedBackup, restoreBackup, setUnconfirmedBackup } from './WelcomeServices/restoreBackup'
 
@@ -134,7 +134,7 @@ export async function checkPermissionsAndRestore(id: string) {
     if (json) {
         const permissions = await extraPermissions(json.grantedHostPermissions)
         if (permissions.length) {
-            const granted = await requestPermissions(permissions)
+            const granted = await requestBrowserPermission({origins: permissions})
             if (!granted) return
         }
 
