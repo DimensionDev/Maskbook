2d5c16d1174a51e36f1568b3f7cf1ab4346c1b7b
Elhamne <elham.ne94@gmail.com>
2021-09-28 18:44:50 +0330

    feat: pnltooltip and positions info added

--- packages/maskbook/src/plugins/Augur/UI/PositionsView.tsx
+++ packages/maskbook/src/plugins/Augur/UI/PositionsView.tsx
@@ -1,7 +1,21 @@
 import type { Market } from '../types'
 import { makeStyles } from '@masknet/theme'
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@material-ui/core'
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Typography,
+    Grid,
+    IconButton,
+    Tooltip,
+} from '@material-ui/core'
 import { useI18N } from '../../../utils'
+import ErrorIcon from '@material-ui/icons/Error'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
 
 const useStyles = makeStyles()((theme) => ({
     root: {
@@ -15,10 +29,11 @@ const useStyles = makeStyles()((theme) => ({
         borderRadius: '.5rem',
     },
     success: {
-        color: theme.palette.success.main,
+        color: theme.palette.success.light,
+        borderColor: theme.palette.success.light,
     },
     fail: {
-        color: theme.palette.error.main,
+        color: theme.palette.error.light,
     },
 }))
 
@@ -41,6 +56,7 @@ export function PositionsView(props: PositionsProps) {
                             {t('plugin_augur_no_positions')}
                         </Typography>
                     </caption>
+
                     <TableHead>
                         <TableRow>
                             <TableCell align="center">{t('plugin_augur_tab_positions_column_outcome')}</TableCell>
@@ -52,7 +68,19 @@ export function PositionsView(props: PositionsProps) {
                             </TableCell>
                             <TableCell align="center">{t('plugin_augur_tab_positions_column_init_value')}</TableCell>
                             <TableCell align="center">{t('plugin_augur_tab_positions_column_cur_value')}</TableCell>
-                            <TableCell align="center">{t('plugin_augur_tab_positions_column_p_l')}</TableCell>
+                            <TableCell align="center">
+                                {t('plugin_augur_tab_positions_column_p_l')}{' '}
+                                <Tooltip
+                                    title={t('plugin_augur_tab_positions_column_p_l_tooltip')}
+                                    arrow
+                                    PopperProps={{
+                                        disablePortal: true,
+                                    }}>
+                                    <IconButton>
+                                        <HelpOutlineIcon fontSize="small" color="info" />
+                                    </IconButton>
+                                </Tooltip>
+                            </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -80,6 +108,24 @@ export function PositionsView(props: PositionsProps) {
                     </TableBody>
                 </Table>
             </TableContainer>
+            <br />
+            <Grid container wrap="nowrap" className={`${classes.info} ${classes.success}`} sx={{ padding: 2 }}>
+                <Grid item>
+                    <ErrorIcon />
+                </Grid>
+                <Grid item container direction="column" sx={{ paddingX: 1 }}>
+                    <Grid item>
+                        <Typography variant="body1" color="textPrimary">
+                            {t('plugin_augur_tab_positions_info_title')}
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="body2" color="textSecondary">
+                            {t('plugin_augur_tab_positions_info_desc')}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
         </div>
     )
 }
