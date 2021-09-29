db37ce88c1056a0790fa3f9a238957c3f00928d6
Lantt <lanyixing@live.com>
2021-09-29 14:35:09 +0800

    style: collectible card

--- packages/dashboard/src/pages/Wallets/components/CollectibleCard/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/CollectibleCard/index.tsx
@@ -1,15 +1,17 @@
 import { ERC721TokenDetailed, ChainId, CollectibleProvider, resolveCollectibleLink } from '@masknet/web3-shared'
-import { memo } from 'react'
-import { Box, Link, Typography } from '@material-ui/core'
+import { memo, useRef } from 'react'
+import { Box, Button, Link, Typography } from '@material-ui/core'
 import { makeStyles } from '@masknet/theme'
 import { MaskColorVar } from '@masknet/theme'
 import { CollectiblePlaceholder } from '../CollectiblePlaceHolder'
+import { useHoverDirty } from 'react-use'
+import { useDashboardI18N } from '../../../../locales'
 
 const useStyles = makeStyles()({
     card: {
         borderRadius: 8,
         width: 140,
-        height: 215,
+        minHeight: 215,
         backgroundColor: MaskColorVar.lightestBackground,
         display: 'flex',
         flexDirection: 'column',
@@ -20,13 +22,19 @@ const useStyles = makeStyles()({
     },
     description: {
         flex: 1,
-        padding: '6.5px 7.5px',
+        flexGrow: 1,
     },
     name: {
         whiteSpace: 'nowrap',
         textOverflow: 'ellipsis',
         overflow: 'hidden',
         fontSize: 12,
+    },
+    hover: {
+        '&:hover': {
+            transform: 'scale(1.1)',
+            filter: 'drop-shadow(0px 12px 28px rgba(0, 0, 0, 0.1))',
+        },
     },
 })
 
@@ -38,23 +46,51 @@ export interface CollectibleCardProps {
 }
 
 export const CollectibleCard = memo<CollectibleCardProps>(({ chainId, provider, token, onSend }) => {
+    const t = useDashboardI18N()
     const { classes } = useStyles()
-    return token.info.image ? (
-        <div className={classes.card}>
-            <Link target="_blank" rel="noopener noreferrer" href={resolveCollectibleLink(chainId, provider, token)}>
-                <div className={classes.imgContainer}>
-                    <img src={token.info.image} style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
+    const ref = useRef(null)
+    const isHovering = useHoverDirty(ref)
+
+    return (
+        <Box className={classes.hover} ref={ref}>
+            {token.info.image ? (
+                <div className={classes.card}>
+                    <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={resolveCollectibleLink(chainId, provider, token)}>
+                        <div className={classes.imgContainer}>
+                            <img
+                                src={token.info.image}
+                                style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+                            />
+                        </div>
+                    </Link>
+                    <Box className={classes.description} py={1} px={3}>
+                        {isHovering ? (
+                            <Box>
+                                <Button
+                                    size="small"
+                                    fullWidth
+                                    onClick={onSend}
+                                    variant="rounded"
+                                    style={{ boxShadow: 'none' }}
+                                    sx={{ fontWeight: 'bolder', height: '28px' }}>
+                                    {t.send()}
+                                </Button>
+                            </Box>
+                        ) : (
+                            <Typography className={classes.name} color="textPrimary" variant="body2" onClick={onSend}>
+                                {token.info.name}
+                            </Typography>
+                        )}
+                    </Box>
                 </div>
-            </Link>
-            <div className={classes.description}>
-                <Typography className={classes.name} color="textPrimary" variant="body2" onClick={onSend}>
-                    {token.info.name}
-                </Typography>
-            </div>
-        </div>
-    ) : (
-        <Box onClick={onSend}>
-            <CollectiblePlaceholder />
+            ) : (
+                <Box>
+                    <CollectiblePlaceholder onSend={onSend} isHovering={isHovering} />
+                </Box>
+            )}
         </Box>
     )
 })
--- packages/dashboard/src/pages/Wallets/components/CollectiblePlaceHolder/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/CollectiblePlaceHolder/index.tsx
@@ -2,13 +2,16 @@ import { memo } from 'react'
 import { MiniMaskIcon } from '@masknet/icons'
 import { makeStyles } from '@masknet/theme'
 import { MaskColorVar } from '@masknet/theme'
+import { Button } from '@mui/material'
+import { useDashboardI18N } from '../../../../locales'
+import { Box } from '@mui/system'
 
 const useStyles = makeStyles()({
     container: {
         borderRadius: 8,
         width: 140,
         height: 215,
-        backgroundColor: MaskColorVar.lightBackground,
+        backgroundColor: MaskColorVar.lineLight,
         display: 'flex',
         flexDirection: 'column',
     },
@@ -21,18 +24,44 @@ const useStyles = makeStyles()({
     },
     description: {
         flex: 1,
-        backgroundColor: MaskColorVar.infoBackground,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
     },
 })
 
-export const CollectiblePlaceholder = memo(() => {
+interface CollectiblePlaceHolderProps {
+    onSend(): void
+    isHovering: boolean
+}
+
+export const CollectiblePlaceholder = memo<CollectiblePlaceHolderProps>(({ onSend, isHovering }) => {
     const { classes } = useStyles()
+    const t = useDashboardI18N()
+
     return (
         <div className={classes.container}>
             <div className={classes.placeholder}>
                 <MiniMaskIcon viewBox="0 0 48 48" sx={{ fontSize: 48, opacity: 0.5 }} />
             </div>
-            <div className={classes.description} />
+            <Box
+                className={classes.description}
+                sx={{ backgroundColor: isHovering ? MaskColorVar.primaryBackground : '' }}>
+                {isHovering ? (
+                    <Box py={1} px={3}>
+                        <Button
+                            size="small"
+                            fullWidth
+                            onClick={onSend}
+                            variant="rounded"
+                            style={{ boxShadow: 'none' }}
+                            sx={{ fontWeight: 'bolder', height: '28px' }}>
+                            {t.send()}
+                        </Button>
+                    </Box>
+                ) : (
+                    <div className={classes.description} />
+                )}
+            </Box>
         </div>
     )
 })
