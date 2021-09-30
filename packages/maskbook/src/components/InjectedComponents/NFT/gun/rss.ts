9a65e983c2faf29cf6b75112956b524bf6ef0e0a
lelenei <72531217+lelenei@users.noreply.github.com>
2021-09-30 19:20:55 +0800

    refactor: code style

--- packages/maskbook/src/components/InjectedComponents/NFT/gun/rss.ts
+++ packages/maskbook/src/components/InjectedComponents/NFT/gun/rss.ts
@@ -3,7 +3,6 @@ import RSS3 from 'rss3-next'
 import type Web3 from 'web3'
 import type { Persona } from '../../../../database'
 import Services from '../../../../extension/service'
-import { currentAccountSettings } from '../../../../plugins/Wallet/settings'
 import { AvatarMetaDB, RSS3_APP } from '../types'
 import { ethAddrFrom } from '../utils/address'
 
@@ -13,42 +12,37 @@ interface NFTRSSNode {
     nft: AvatarMetaDB
 }
 
-async function createRSS(address?: string) {
-    const rss3 = new RSS3({
-        endpoint: RSS3_APP,
-        address: address ?? currentAccountSettings.value,
-    })
+export async function createRSS(persona: Persona) {
+    const address = await ethAddrFrom(persona.identifier.toText())
+    if (!address) return
 
-    return rss3
+    return new RSS3({
+        endpoint: RSS3_APP,
+        address,
+    })
 }
 
 export async function getNFTAvatarFromRSS(persona: Persona, web3: Web3) {
-    const _address = await ethAddrFrom(persona.identifier.toText())
-    if (!_address) return
-
-    const rss = await createRSS(_address)
-    const res = await rss?.files.get(rss.account.address)
-    const nft = Object.getOwnPropertyDescriptor(res, '_nft') as NFTRSSNode
+    const rss = await createRSS(persona)
+    if (!rss) return
+    const file = await rss.files.get(rss.account.address)
+    const nft = Object.getOwnPropertyDescriptor(file, '_nft')
     if (!nft) return
-
-    const wallet = web3.eth.accounts.recover(_address, nft.signature)
-    if (wallet !== nft.address) return null
-    return nft.nft
+    const wallet = web3.eth.accounts.recover(rss.account.address, nft.value.signature)
+    if (wallet !== nft.value.address) return null
+    return nft.value.nft
 }
 
 export async function saveNFTAvatarFromRSS(persona: Persona, wallet: Wallet, nft: AvatarMetaDB) {
-    const _address = await ethAddrFrom(persona.identifier.toText())
-    if (!_address) return
+    const rss = await createRSS(persona)
+    if (!rss) return
 
-    const rss = await createRSS(_address)
     const file = await rss.files.get(rss.account.address)
     if (!file) {
-        console.log('save error')
-        return
+        throw new Error('Not Found')
     }
 
     const result = await Services.Identity.signWithPersona({ message: wallet.address, method: 'eth' })
-    console.log(result)
     const f = Object.assign(file, {
         _nft: {
             wallet: wallet.address,
@@ -59,6 +53,6 @@ export async function saveNFTAvatarFromRSS(persona: Persona, wallet: Wallet, nft
 
     rss.files.set(file)
     await rss.files.sync()
-    const res = await rss.files.get(rss.account.address)
-    return Object.getOwnPropertyDescriptor(res, '_nft') as AvatarMetaDB
+
+    return nft
 }
