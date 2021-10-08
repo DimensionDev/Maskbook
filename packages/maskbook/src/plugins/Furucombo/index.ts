38fba6fa3a05ed4f939e8687161aea737b78884e
adz <slf.pro@gmail.com>
2021-10-08 16:46:26 +0300

    feat(furucombo): remove Dashboard and Worker

--- packages/maskbook/src/plugins/Furucombo/index.ts
+++ packages/maskbook/src/plugins/Furucombo/index.ts
@@ -8,14 +8,4 @@ registerPlugin({
         hotModuleReload: (hot) =>
             import.meta.webpackHot && import.meta.webpackHot.accept('./SNSAdaptor', () => hot(import('./SNSAdaptor'))),
     },
-    Dashboard: {
-        load: () => import('./Dashboard'),
-        hotModuleReload: (hot) =>
-            import.meta.webpackHot && import.meta.webpackHot.accept('./Dashboard', () => hot(import('./Dashboard'))),
-    },
-    Worker: {
-        load: () => import('./Worker'),
-        hotModuleReload: (hot) =>
-            import.meta.webpackHot && import.meta.webpackHot.accept('./Worker', () => hot(import('./Worker'))),
-    },
 })
