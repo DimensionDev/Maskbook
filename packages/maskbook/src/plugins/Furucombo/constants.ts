a9af59807a616647a28ea61fe25b4edd130197c0
slfweb <slf.pro@gmail.com>
2021-10-12 12:31:55 +0200

    feat: furucombo plugin (#4501)
    * feat(furucombo): initial config
    
    * feat(Furucombo): implement investable UI
    
    * feat(furucombo): implement investments UI
    
    * feat(furucombo): update plugin README
    
    * feat(furucombo): update furucombo README
    
    * feat(furucombo): fix build errors
    
    * feat(furucombo): review suggestions
    
    * feat(Furucombo): fix reviews
    
    * feat(furucombo): make changes from reviews
    
    * feat(furucombo): refactor rewardIcons displaying
    
    * feat(furucombo): fix check spells
    
    * feat(furucombo): refactor rewards icons
    
    * feat(furucombo): cache fetching pools
    
    * feat(furucombo): remove Dashboard and Worker
    
    * feat(furucombo): set fallback to default font

---
+++ packages/maskbook/src/plugins/Furucombo/constants.ts
@@ -0,0 +1,5 @@
+export const PLUGIN_ID = 'app.furucombo'
+
+export const API_KEY = 'EryftBXrpN4ZG6dViNQtM3uNomPQpeyvgW6El9h0'
+export const API_URL = 'https://api.furucombo.app/v1/investables'
+export const BASE_URL = 'https://furucombo.app/invest'
