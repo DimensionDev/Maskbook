d5566aff9ef0e9235e8df6da534143b9f36dc2cd
Hom Yan <yanzhihong23@qq.com>
2021-10-20 14:36:06 +0800

    fix: camel

--- packages/icons/settings/SettingsEmail.tsx
+++ packages/icons/settings/SettingsEmail.tsx
@@ -7,21 +7,21 @@ export const SettingsEmailIcon: typeof SvgIcon = createPaletteAwareIcon(
         <path
             d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2Z"
             stroke="#1C68F3"
-            stroke-width="1.25"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
-        <path d="m22 6-10 7L2 6" stroke="#1C68F3" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="m22 6-10 7L2 6" stroke="#1C68F3" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
     </g>,
     <g>
         <path
             d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2Z"
             stroke="#fff"
-            stroke-width="1.25"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
-        <path d="m22 6-10 7L2 6" stroke="#fff" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="m22 6-10 7L2 6" stroke="#fff" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
     </g>,
     '0 0 24 24',
 )
