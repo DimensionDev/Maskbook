c8758e3da4f4a0e98e14dd9385a2dfc54a7f0ed5
Jack Works <jackworks@protonmail.com>
2021-10-25 23:48:14 +0800

    fix: popups breaks

--- packages/maskbook/src/extension/popups/render.tsx
+++ packages/maskbook/src/extension/popups/render.tsx
@@ -1,63 +1,10 @@
-import { lazy } from 'react'
-import { Redirect, Route, Switch } from 'react-router-dom'
-import { HashRouter } from 'react-router-dom'
-import { createInjectHooksRenderer, startPluginDashboard, useActivatedPluginsDashboard } from '@masknet/plugin-infra'
-import { PopupRoutes } from '.'
-import { createNormalReactRoot, useClassicMaskFullPageTheme } from '../../utils'
-import '../../social-network-adaptor/browser-action'
-import { Web3Provider } from '@masknet/web3-shared-evm'
-import { PopupWeb3Context } from '../../web3/context'
-import { PopupFrame } from './components/PopupFrame'
-import { Appearance } from '@masknet/theme'
+import { startPluginDashboard } from '@masknet/plugin-infra'
+import { createNormalReactRoot } from '../../utils'
 import { createPluginHost } from '../../../src/plugin-infra/host'
-import { MaskUIRoot } from '../../UIRoot'
 import { status } from '../../setup.ui'
+import Popups from './UI'
 
-function useAlwaysLightTheme() {
-    return useClassicMaskFullPageTheme({ forcePalette: Appearance.light })
-}
-const Wallet = lazy(() => import('./pages/Wallet'))
-const Personas = lazy(() => import('./pages/Personas'))
-const SwapPage = lazy(() => import('./pages/Swap'))
-const RequestPermissionPage = lazy(() => import('./RequestPermission'))
-const PermissionAwareRedirect = lazy(() => import('./PermissionAwareRedirect'))
-const ThirdPartyRequestPermission = lazy(() => import('./ThirdPartyRequestPermission'))
-const SignRequest = lazy(() => import('./SignRequest'))
+status.then(() => createNormalReactRoot(<Popups />))
 
-const PluginRender = createInjectHooksRenderer(useActivatedPluginsDashboard, (x) => x.GlobalInjection)
-
-function Dialogs() {
-    return (
-        <MaskUIRoot useTheme={useAlwaysLightTheme} kind="fullpage">
-            <Web3Provider value={PopupWeb3Context}>
-                <HashRouter>
-                    <Switch>
-                        <Route path={PopupRoutes.Wallet} children={frame(<Wallet />)} />
-                        <Route path={PopupRoutes.Personas} children={frame(<Personas />)} />
-                        <Route path={PopupRoutes.Swap} children={<SwapPage />} />
-                        <Route path={PopupRoutes.RequestPermission} exact>
-                            <RequestPermissionPage />
-                        </Route>
-                        <Route path={PopupRoutes.PermissionAwareRedirect} exact>
-                            <PermissionAwareRedirect />
-                        </Route>
-                        <Route path={PopupRoutes.ThirdPartyRequestPermission} exact>
-                            <ThirdPartyRequestPermission />
-                        </Route>
-                        <Route path={PopupRoutes.SignRequest} exact>
-                            <SignRequest />
-                        </Route>
-                        <Route children={<Redirect to={PopupRoutes.Wallet} />} />
-                    </Switch>
-                </HashRouter>
-                <PluginRender />
-            </Web3Provider>
-        </MaskUIRoot>
-    )
-}
-status.then(() => createNormalReactRoot(<Dialogs />))
+// TODO: Should only load plugins when the page is plugin-aware.
 startPluginDashboard(createPluginHost(undefined, () => undefined))
-
-function frame(x: React.ReactNode) {
-    return <PopupFrame children={x} />
-}
