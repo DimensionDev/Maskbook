ce5e0eed2e48890dd95f534b8b8dceed031eb9e6
Lantt <laanfor@mask.io>
2021-10-27 18:59:03 +0800

    feat: transaction snackbar ui (#4693)
    * feat: snackbar style for transaction
    
    * refactor: remove luck drop transaction snackbar
    
    * refactor: remove unused key
    
    * fix: typo
    
    * refactor: use create look up table to replace constant

--- packages/maskbook/src/plugins/Wallet/SNSAdaptor/TransactionSnackbar/index.tsx
+++ packages/maskbook/src/plugins/Wallet/SNSAdaptor/TransactionSnackbar/index.tsx
@@ -1,9 +1,68 @@
-import { useRef, useCallback, useEffect } from 'react'
-import { TransactionStateType } from '@masknet/web3-shared-evm'
-import { ShowSnackbarOptions, SnackbarKey, SnackbarMessage, useCustomSnackbar } from '@masknet/theme'
+import React, { ReactNode, useCallback, useEffect, useRef } from 'react'
+import {
+    createLookupTableResolver,
+    resolveTransactionLinkOnExplorer,
+    TransactionState,
+    TransactionStateType,
+    useChainId,
+} from '@masknet/web3-shared-evm'
+import { makeStyles, ShowSnackbarOptions, SnackbarKey, SnackbarMessage, useCustomSnackbar } from '@masknet/theme'
 import { WalletMessages } from '../../messages'
+import { getSendTransactionComputedPayload } from '../../../../extension/background-script/EthereumService'
+import { RecentTransactionDescription } from '../WalletStatusDialog/TransactionDescription'
+import { Link } from '@mui/material'
+import LaunchIcon from '@mui/icons-material/Launch'
+import type { TransactionReceipt } from 'web3-core'
+
+export const resolveSnackbarConfig = createLookupTableResolver<
+    TransactionStateType,
+    Pick<ShowSnackbarOptions, 'message' | 'processing' | 'variant'>
+>(
+    {
+        [TransactionStateType.WAIT_FOR_CONFIRMING]: {
+            processing: true,
+            variant: 'success',
+            message: 'Confirm this transaction in your wallet',
+        },
+        [TransactionStateType.HASH]: {
+            processing: true,
+            variant: 'success',
+            message: 'Transaction Submitted',
+        },
+        [TransactionStateType.CONFIRMED]: {
+            processing: false,
+            variant: 'success',
+            message: 'Transaction Successfully',
+        },
+        [TransactionStateType.RECEIPT]: {
+            processing: false,
+            variant: 'error',
+            message: 'Transaction rejected',
+        },
+        [TransactionStateType.FAILED]: {
+            processing: false,
+            variant: 'error',
+            message: 'Transaction rejected',
+        },
+        [TransactionStateType.UNKNOWN]: {
+            processing: false,
+            variant: 'error',
+            message: '',
+        },
+    },
+    {},
+)
+
+const useStyles = makeStyles()({
+    link: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+})
 
 export function TransactionSnackbar() {
+    const chainId = useChainId()
+    const { classes } = useStyles()
     const { showSnackbar, closeSnackbar } = useCustomSnackbar()
     const snackbarKeyRef = useRef<SnackbarKey>()
 
@@ -18,13 +77,43 @@ export function TransactionSnackbar() {
         [showSnackbar, closeSnackbar],
     )
 
+    const getTitle = useCallback((state: TransactionState, payload: any, hash?: string) => {
+        return (
+            <RecentTransactionDescription
+                hash={hash ?? ''}
+                computedPayload={payload}
+                receipt={(state as { receipt: TransactionReceipt }).receipt}
+            />
+        )
+    }, [])
+
+    const getFullMessage = useCallback(
+        (message: ReactNode, hash?: string) => {
+            if (!hash) return message
+            const link = resolveTransactionLinkOnExplorer(chainId, hash)
+            return (
+                <Link className={classes.link} color="inherit" href={link} target="_blank" rel="noopener noreferrer">
+                    {message} <LaunchIcon sx={{ ml: 1 }} fontSize="inherit" />
+                </Link>
+            )
+        },
+        [chainId],
+    )
+
     useEffect(() => {
-        return WalletMessages.events.transactionProgressUpdated.on((progress) => {
-            console.log('DEBUG: progress updated')
-            console.log(progress)
-            showSingletonSnackbar(TransactionStateType[progress.state.type], {
-                variant: 'success',
-            })
+        return WalletMessages.events.transactionProgressUpdated.on(async (progress) => {
+            if (progress.state.type === TransactionStateType.UNKNOWN) return
+
+            const payload = await getSendTransactionComputedPayload(progress.payload)
+            const config = resolveSnackbarConfig(progress.state.type)
+            const hash =
+                (progress.state as { hash?: string }).hash ??
+                (progress.state as { receipt?: TransactionReceipt }).receipt?.transactionHash
+
+            showSingletonSnackbar(getTitle(progress.state, payload, hash), {
+                ...config,
+                ...{ message: getFullMessage(config.message, hash) },
+            } as ShowSnackbarOptions)
         })
     }, [])
 
