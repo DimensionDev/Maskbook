30289c94e32902f4332b1ae06c137e98bf38a2c8
lelenei <72531217+lelenei@users.noreply.github.com>
2021-11-04 14:30:57 +0800

    refactor: remove unused code

--- packages/mask/src/social-network-adaptor/twitter.com/injection/NFT/Avatar.tsx
+++ packages/mask/src/social-network-adaptor/twitter.com/injection/NFT/Avatar.tsx
@@ -37,11 +37,6 @@ function _(main: () => LiveSelector<HTMLElement, false>, signal: AbortSignal) {
                 const imgNode = avatarImageNodeParent.querySelector('div') as HTMLImageElement
                 if (imgNode) imgNode.style.borderRadius = '100%'
 
-                const onClick = () => {
-                    window.open(`https://twitter.com/${twitterId}`, '_blank')
-                }
-                avatarNodeParent.firstChild.addEventListener('click', onClick)
-
                 const root = createReactRootShadowed(proxy.afterShadow, { signal })
                 root.render(
                     <div
@@ -54,10 +49,7 @@ function _(main: () => LiveSelector<HTMLElement, false>, signal: AbortSignal) {
                         <NFTBadgeTimeline userId={twitterId} avatarId={avatarId} width={width} height={height} />
                     </div>,
                 )
-                remover = () => {
-                    root.destory()
-                    avatarNodeParent.firstChild?.removeEventListener('click', onClick)
-                }
+                remover = root.destory
             }
 
             run()
