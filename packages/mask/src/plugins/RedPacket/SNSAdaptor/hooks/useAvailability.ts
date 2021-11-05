637a6ae7db36bae42db447211b6ef31f264d8873
zhouhanseng <z308114274@gmail.com>
2021-11-05 14:51:31 +0800

    fix: remove useless rpc call

--- packages/mask/src/plugins/RedPacket/SNSAdaptor/hooks/useAvailability.ts
+++ packages/mask/src/plugins/RedPacket/SNSAdaptor/hooks/useAvailability.ts
@@ -1,10 +1,12 @@
 import { useAsyncRetry } from 'react-use'
+import { ChainId, useChainId } from '@masknet/web3-shared-evm'
 import { useRedPacketContract } from './useRedPacketContract'
 
-export function useAvailability(version: number, from: string, id?: string) {
+export function useAvailability(version: number, from: string, id: string, redpacketChainId: ChainId) {
     const redPacketContract = useRedPacketContract(version)
+    const currentChainId = useChainId()
     return useAsyncRetry(async () => {
-        if (!id) return null
+        if (!id || redpacketChainId !== currentChainId) return null
         if (!redPacketContract) return null
         return redPacketContract.methods.check_availability(id).call({
             // check availability is ok w/o account
