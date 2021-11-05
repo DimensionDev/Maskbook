7e9046b1530599b779266ba907f737a50e623955
lelenei <72531217+lelenei@users.noreply.github.com>
2021-11-05 10:29:21 +0800

    fix: add cache expired time

--- packages/mask/src/plugins/Avatar/hooks/useNFTAvatar.ts
+++ packages/mask/src/plugins/Avatar/hooks/useNFTAvatar.ts
@@ -2,16 +2,25 @@ import { useAsync } from 'react-use'
 import { PluginNFTAvatarRPC } from '../messages'
 import type { AvatarMetaDB } from '../types'
 
-const cache = new Map<string, Promise<AvatarMetaDB | undefined>>()
+const EXPIRED_TIME = 60 * 1000
+const cache = new Map<string, [number, Promise<AvatarMetaDB | undefined>]>()
 export function useNFTAvatar(userId: string) {
     return useAsync(async () => {
         if (!userId) return
         if (userId === '$unknown') return
-        let f = cache.get(userId)
-        if (!f) {
+        const c = cache.get(userId)
+        let f
+        if (c) {
+            f = c[1]
+            if (!f || Date.now() - c[0] >= EXPIRED_TIME) {
+                f = PluginNFTAvatarRPC.getNFTAvatar(userId)
+                cache.set(userId, [Date.now(), f])
+            }
+        } else {
             f = PluginNFTAvatarRPC.getNFTAvatar(userId)
-            cache.set(userId, f)
+            cache.set(userId, [Date.now(), f])
         }
+
         const avatar = await f
         return avatar
     }, [userId, cache])
