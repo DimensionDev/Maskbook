1f33a1131059d9980535ef7ad1deeb35b351ad2e
Jack Works <jackworks@protonmail.com>
2021-11-09 00:19:04 +0800

    refactor: move avatar db out

--- packages/mask/background/database/avatar-cache/db.ts
+++ packages/mask/background/database/avatar-cache/db.ts
@@ -1,11 +1,13 @@
-/// <reference path="./global.d.ts" />
 import { openDB, DBSchema } from 'idb/with-async-ittr-cjs'
-import { Identifier, PersonaIdentifier, ProfileIdentifier } from './type'
-import { createDBAccess, IDBPSafeTransaction, createTransaction } from '../../background/database/utils/openDB'
+import { Identifier, IdentifierMap, PersonaIdentifier, ProfileIdentifier } from '@masknet/shared-base'
+import { createDBAccess, createTransaction, IDBPSafeTransaction } from '../utils/openDB'
+
+const pendingUpdate = new IdentifierMap<IdentifierWithAvatar, Partial<AvatarMetadataRecord>>(new Map())
+let pendingUpdateTimer: ReturnType<typeof setTimeout> | null
 
 //#region Schema
-export type IdentityWithAvatar = ProfileIdentifier | PersonaIdentifier
-export type AvatarRecord = ArrayBuffer
+export type IdentifierWithAvatar = ProfileIdentifier | PersonaIdentifier
+type AvatarRecord = ArrayBuffer
 interface AvatarMetadataRecord {
     identifier: string
     lastUpdateTime: Date
@@ -24,8 +26,7 @@ export interface AvatarDBSchema extends DBSchema {
     }
 }
 //#endregion
-
-const db = createDBAccess(() => {
+export const createAvatarDBAccess = createDBAccess(() => {
     return openDB<AvatarDBSchema>('maskbook-avatar-cache', 1, {
         upgrade(db, oldVersion, newVersion, transaction) {
             // Out line keys
@@ -34,62 +35,71 @@ const db = createDBAccess(() => {
         },
     })
 })
-export const createAvatarDBAccess = db
 /**
  * Store avatar into database
  */
-export async function storeAvatarDB(id: IdentityWithAvatar, avatar: ArrayBuffer) {
+export async function storeAvatarDB(
+    t: IDBPSafeTransaction<AvatarDBSchema, ['metadata', 'avatars'], 'readwrite'>,
+    id: IdentifierWithAvatar,
+    avatar: ArrayBuffer,
+): Promise<void> {
     const meta: AvatarMetadataRecord = {
         identifier: id.toText(),
         lastUpdateTime: new Date(),
         lastAccessTime: new Date(),
     }
-    const t = (await db()).transaction(['avatars', 'metadata'], 'readwrite')
     await t.objectStore('avatars').put(avatar, id.toText())
     await t.objectStore('metadata').put(meta)
-    return
 }
 /**
  * Read avatar out
  */
-export async function queryAvatarDB(id: IdentityWithAvatar): Promise<ArrayBuffer | null> {
-    const t = (await db()).transaction('avatars')
+export async function queryAvatarDB(
+    t: IDBPSafeTransaction<AvatarDBSchema, ['avatars'], 'readonly'>,
+    id: IdentifierWithAvatar,
+): Promise<ArrayBuffer | null> {
     const result = await t.objectStore('avatars').get(id.toText())
-    if (result) {
-        updateAvatarMetaDB(id, { lastAccessTime: new Date() }).catch((error: unknown) => {
-            console.warn('Update last use record failed', error)
-        })
-    }
+    if (result) scheduleAvatarMetaUpdate(id, { lastAccessTime: new Date() })
     return result || null
 }
-/**
- * Store avatar metadata
- */
-export async function updateAvatarMetaDB(id: IdentityWithAvatar, newMeta: Partial<AvatarMetadataRecord>) {
-    const t = (await db()).transaction('metadata', 'readwrite')
-    const meta = await t.objectStore('metadata').get(id.toText())
-    const newRecord = Object.assign({}, meta, newMeta)
-    await t.objectStore('metadata').put(newRecord)
+function scheduleAvatarMetaUpdate(id: IdentifierWithAvatar, meta: Partial<AvatarMetadataRecord>) {
+    pendingUpdate.set(id, meta)
+
+    if (pendingUpdateTimer) return
+    const _1_minute = 60 * 1000
+    pendingUpdateTimer = setTimeout(async () => {
+        try {
+            const t = createTransaction(await createAvatarDBAccess(), 'readwrite')('metadata')
+            for (const [id, meta] of pendingUpdate) {
+                const old = await t.objectStore('metadata').get(id.toText())
+                await t.objectStore('metadata').put({ ...old, ...meta } as AvatarMetadataRecord)
+            }
+        } finally {
+            pendingUpdateTimer = null
+            pendingUpdate.clear()
+        }
+    }, _1_minute)
 }
+
 /**
  * Find avatar lastUpdateTime or lastAccessTime out-of-date
  * @param attribute - Which attribute want to query
  * @param deadline - Select all identifiers before a date
  * defaults to 14 days for lastAccessTime
  * defaults to 7 days for lastUpdateTime
+ * @internal
  */
 export async function queryAvatarOutdatedDB(
+    t: IDBPSafeTransaction<AvatarDBSchema, ['metadata'], 'readonly'>,
     attribute: 'lastUpdateTime' | 'lastAccessTime',
-    t?: IDBPSafeTransaction<AvatarDBSchema, ['metadata'], 'readonly'>,
     deadline: Date = new Date(Date.now() - 1000 * 60 * 60 * 24 * (attribute === 'lastAccessTime' ? 14 : 7)),
 ) {
-    t = createTransaction(await db(), 'readonly')('metadata')
-    const outdated: IdentityWithAvatar[] = []
+    const outdated: IdentifierWithAvatar[] = []
     for await (const { value } of t.objectStore('metadata')) {
         if (deadline > value[attribute]) {
             const id = Identifier.fromString(value.identifier)
             if (id.err) continue
-            outdated.push(id.val as IdentityWithAvatar)
+            outdated.push(id.val as IdentifierWithAvatar)
         }
     }
     return outdated
@@ -100,13 +110,14 @@ export async function queryAvatarOutdatedDB(
  * @param deadline - Select all identifiers before a date
  * defaults to 30 days for lastAccessTime
  * defaults to 7 days for lastUpdateTime
+ * @internal
  */
 export async function isAvatarOutdatedDB(
-    identifier: IdentityWithAvatar,
+    t: IDBPSafeTransaction<AvatarDBSchema, ['metadata'], 'readonly'>,
+    identifier: IdentifierWithAvatar,
     attribute: 'lastUpdateTime' | 'lastAccessTime',
     deadline: Date = new Date(Date.now() - 1000 * 60 * 60 * 24 * (attribute === 'lastAccessTime' ? 30 : 7)),
 ): Promise<boolean> {
-    const t = (await db()).transaction('metadata')
     const meta = await t.objectStore('metadata').get(identifier.toText())
     if (!meta) return true
     if (deadline > meta[attribute]) return true
@@ -114,15 +125,14 @@ export async function isAvatarOutdatedDB(
 }
 /**
  * Batch delete avatars
+ * @internal
  */
 export async function deleteAvatarsDB(
-    ids: IdentityWithAvatar[],
-    t?: IDBPSafeTransaction<AvatarDBSchema, ['metadata', 'avatars'], 'readwrite'>,
-) {
-    t = createTransaction(await db(), 'readwrite')('avatars', 'metadata')
+    t: IDBPSafeTransaction<AvatarDBSchema, ['metadata', 'avatars'], 'readwrite'>,
+    ids: IdentifierWithAvatar[],
+): Promise<void> {
     for (const id of ids) {
         t.objectStore('avatars').delete(id.toText())
         t.objectStore('metadata').delete(id.toText())
     }
-    return
 }
