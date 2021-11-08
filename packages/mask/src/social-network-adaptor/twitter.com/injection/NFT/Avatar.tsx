136ad50a92d78ada92c79098214faa0dfeaf2b9c
lelenei <72531217+lelenei@users.noreply.github.com>
2021-11-08 12:08:45 +0800

    fix: tweet avatar in home

--- packages/mask/src/social-network-adaptor/twitter.com/injection/NFT/Avatar.tsx
+++ packages/mask/src/social-network-adaptor/twitter.com/injection/NFT/Avatar.tsx
@@ -1,8 +1,8 @@
 import { DOMProxy, LiveSelector, MutationObserverWatcher } from '@dimensiondev/holoflows-kit'
 import { NFTBadgeTimeline } from '../../../../plugins/Avatar/SNSAdaptor/NFTBadgeTimeline'
 import { createReactRootShadowed, Flags, startWatch } from '../../../../utils'
+import { getInjectNodeInfo } from '../../utils/avatar'
 import { postAvatarsContentSelector } from '../../utils/selector'
-import { getAvatarId } from '../../utils/user'
 
 function getTwitterId(ele: HTMLElement) {
     const twitterIdNode = (ele.firstChild?.nextSibling as HTMLElement).querySelector(
@@ -23,29 +23,11 @@ function _(main: () => LiveSelector<HTMLElement, false>, signal: AbortSignal) {
                 const twitterId = getTwitterId(ele)
                 if (!twitterId) return
 
-                const imgEle = (ele.firstChild as HTMLElement).querySelector('img')
-                if (!imgEle) return
-
-                const avatarNodeParent = imgEle.parentNode?.parentNode?.parentNode?.parentNode
-                    ?.parentNode as HTMLElement
-                if (!avatarNodeParent || !avatarNodeParent.firstChild) return
-
-                const avatarImageNodeParent = avatarNodeParent.querySelector('div > :last-child > div')
-                if (!avatarImageNodeParent) return
-
-                const avatarIdNode = avatarImageNodeParent.querySelector('img') as HTMLImageElement
-                if (!avatarIdNode) return
-                const avatarId = getAvatarId(avatarIdNode.getAttribute('src') ?? '')
+                const info = getInjectNodeInfo(ele.firstChild as HTMLElement)
+                if (!info) return
 
                 const proxy = DOMProxy({ afterShadowRootInit: { mode: Flags.using_ShadowDOM_attach_mode } })
-                proxy.realCurrent = avatarNodeParent.firstChild as HTMLElement
-
-                const width = Number(window.getComputedStyle(avatarNodeParent).width.replace('px', '') ?? 0)
-                const height = Number(window.getComputedStyle(avatarNodeParent).height.replace('px', '') ?? 0)
-                avatarNodeParent.style.overflow = 'unset'
-
-                const imgNode = avatarImageNodeParent.querySelector('div') as HTMLImageElement
-                if (imgNode) imgNode.style.borderRadius = '100%'
+                proxy.realCurrent = info.element.firstChild as HTMLElement
 
                 const root = createReactRootShadowed(proxy.afterShadow, { signal })
                 root.render(
@@ -56,7 +38,12 @@ function _(main: () => LiveSelector<HTMLElement, false>, signal: AbortSignal) {
                             top: -2,
                             zIndex: -1,
                         }}>
-                        <NFTBadgeTimeline userId={twitterId} avatarId={avatarId} width={width} height={height} />
+                        <NFTBadgeTimeline
+                            userId={twitterId}
+                            avatarId={info.avatarId}
+                            width={info.width}
+                            height={info.height}
+                        />
                     </div>,
                 )
                 remover = root.destory
--- packages/mask/src/social-network-adaptor/twitter.com/injection/NFT/tweetNFTAvatar.tsx
+++ packages/mask/src/social-network-adaptor/twitter.com/injection/NFT/tweetNFTAvatar.tsx
@@ -1,8 +1,8 @@
 import { DOMProxy, LiveSelector, MutationObserverWatcher } from '@dimensiondev/holoflows-kit'
 import { NFTBadgeTweet } from '../../../../plugins/Avatar/SNSAdaptor/NFTBadgeTweet'
 import { createReactRootShadowed, Flags, startWatch } from '../../../../utils'
+import { getInjectNodeInfo } from '../../utils/avatar'
 import { searchRetweetAvatarSelector, searchTweetAvatarSelector } from '../../utils/selector'
-import { getAvatarId } from '../../utils/user'
 
 function _(main: () => LiveSelector<HTMLElement, false>, signal: AbortSignal) {
     startWatch(
@@ -11,25 +11,15 @@ function _(main: () => LiveSelector<HTMLElement, false>, signal: AbortSignal) {
             const remove = () => remover()
 
             const run = async () => {
-                const nftDom = ele.firstChild?.firstChild as HTMLElement
-                if (!nftDom || !nftDom.firstChild) return
-                nftDom.style.overflow = 'unset'
-                const width = Number(window.getComputedStyle(nftDom).width.replace('px', '') ?? 0)
-                const height = Number(window.getComputedStyle(nftDom).height.replace('px', '') ?? 0)
+                const info = getInjectNodeInfo(ele.firstChild as HTMLElement)
+                if (!info) return
 
-                const avatarIdNodeParent = nftDom.firstChild.firstChild?.firstChild?.nextSibling?.firstChild
-                const dom = avatarIdNodeParent?.firstChild as HTMLElement
-                if (dom) dom.style.borderRadius = '100%'
-                const img = avatarIdNodeParent?.firstChild?.nextSibling as HTMLImageElement
-                if (!img) return
-                const avatarId = getAvatarId(img.src)
-                if (!avatarId) return
                 const proxy = DOMProxy({ afterShadowRootInit: { mode: Flags.using_ShadowDOM_attach_mode } })
-                proxy.realCurrent = nftDom.firstChild as HTMLElement
+                proxy.realCurrent = info.element.firstChild as HTMLElement
                 const root = createReactRootShadowed(proxy.afterShadow, { signal })
                 root.render(
                     <div style={{ position: 'absolute', top: -2, left: -2, zIndex: -1 }}>
-                        <NFTBadgeTweet width={width} height={height} avatarId={avatarId} />
+                        <NFTBadgeTweet width={info.width} height={info.height} avatarId={info.avatarId} />
                     </div>,
                 )
                 remover = root.destory
