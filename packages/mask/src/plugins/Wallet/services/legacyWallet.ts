e0a9344e96faa6bdd3c23fa5b212bc4f6f63c866
BradGao <ugling88@hotmail.com>
2021-11-18 19:20:31 +0800

    feat: get legacy wallet records for mobile (#4808)
    * feat: add `persona_logout` and `persona_getOwnedPersonaInformation`
    
    * fix: type errror
    
    * fix: typo
    
    * fix: type error
    
    * fix: null check
    
    * fix: use identifier to filter ownedPersonaInformation
    
    * feat: add `getLegacyWalletRecords` for mobile apps to migrate wallets from JS
    
    * chore: refactor

--- packages/mask/src/plugins/Wallet/services/legacyWallet.ts
+++ packages/mask/src/plugins/Wallet/services/legacyWallet.ts
@@ -56,6 +56,20 @@ export async function getLegacyWallets(provider?: ProviderType) {
         return [createWalletRecord(address, 'Mask Network')]
     }
 
+    const wallets = await getAllWalletRecords()
+    if (provider === ProviderType.MaskWallet) return wallets.filter((x) => x._private_key_ || x.mnemonic.length)
+    if (provider === currentProviderSettings.value)
+        return wallets.filter(currySameAddress(currentAccountSettings.value))
+    if (provider) return []
+    return wallets
+}
+
+export async function getLegacyWalletRecords() {
+    const wallets = await getAllWalletRecords()
+    return wallets.filter((x) => x._private_key_ || x.mnemonic.length)
+}
+
+async function getAllWalletRecords() {
     const t = createTransaction(await createWalletDBAccess(), 'readonly')('Wallet')
     const records = await t.objectStore('Wallet').getAll()
     const wallets = (
@@ -69,19 +83,16 @@ export async function getLegacyWallets(provider?: ProviderType) {
             }),
         )
     ).sort(sortWallet)
-    if (provider === ProviderType.MaskWallet) return wallets.filter((x) => x._private_key_ || x.mnemonic.length)
-    if (provider === currentProviderSettings.value)
-        return wallets.filter(currySameAddress(currentAccountSettings.value))
-    if (provider) return []
     return wallets
-    async function makePrivateKey(record: LegacyWalletRecord) {
-        // not a managed wallet
-        if (!record._private_key_ && !record.mnemonic.length) return ''
-        const { privateKey } = record._private_key_
-            ? await recoverWalletFromPrivateKey(record._private_key_)
-            : await recoverWalletFromMnemonicWords(record.mnemonic, record.passphrase, record.path)
-        return `0x${buf2hex(privateKey)}`
-    }
+}
+
+async function makePrivateKey(record: LegacyWalletRecord) {
+    // not a managed wallet
+    if (!record._private_key_ && !record.mnemonic.length) return ''
+    const { privateKey } = record._private_key_
+        ? await recoverWalletFromPrivateKey(record._private_key_)
+        : await recoverWalletFromMnemonicWords(record.mnemonic, record.passphrase, record.path)
+    return `0x${buf2hex(privateKey)}`
 }
 
 export async function freezeLegacyWallet(address: string) {
