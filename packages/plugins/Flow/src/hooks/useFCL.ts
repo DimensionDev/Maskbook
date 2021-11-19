92d5ac526448a688b122abdf97d9d81188df754e
zhouhanseng <z308114274@gmail.com>
2021-11-19 11:45:19 +0800

    chore: flow TestNet

--- packages/plugins/Flow/src/hooks/useFCL.ts
+++ packages/plugins/Flow/src/hooks/useFCL.ts
@@ -1,10 +1,8 @@
 import { useMemo } from 'react'
 import * as SDK from '@masknet/web3-shared-flow'
-import { useChainId } from '@masknet/plugin-infra'
 
 export function useFCL() {
-    const chainId = useChainId()
     return useMemo(() => {
-        return SDK.createClient(chainId)
-    }, [chainId])
+        return SDK.createClient(SDK.ChainId.Testnet)
+    }, [])
 }
