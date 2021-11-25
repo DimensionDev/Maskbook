e238340462fbdc2be8f8123db2b86a1259c43cde
Lantt <laanfor@mask.io>
2021-11-25 17:45:40 +0800

    feat: mnemonic suppport auto paste

--- packages/dashboard/src/components/Mnemonic/DesktopMnemonicConfirm.tsx
+++ packages/dashboard/src/components/Mnemonic/DesktopMnemonicConfirm.tsx
@@ -1,14 +1,28 @@
 import { MaskTextField } from '@masknet/theme'
 import { Grid } from '@mui/material'
-import { memo } from 'react'
+import { memo, useCallback } from 'react'
+import { useDrop } from 'react-use'
 
 export interface DesktopMnemonicConfirmProps {
     puzzleWords: string[]
     indexes?: number[]
     onChange(word: string, index: number): void
+    setAll?(words: string[]): void
 }
+
 export const DesktopMnemonicConfirm = memo((props: DesktopMnemonicConfirmProps) => {
-    const { puzzleWords, indexes, onChange } = props
+    const { puzzleWords, indexes, onChange, setAll } = props
+    useDrop({ onText: (text) => handlePaster(text) })
+
+    const handlePaster = useCallback(
+        (text: string) => {
+            if (!setAll) return
+            const result = [...text.matchAll(/([a-z])+/g)]
+            if (result.length !== 12) return
+            setAll(result.map((x) => x[0]))
+        },
+        [setAll],
+    )
 
     return (
         <Grid container spacing={2}>
@@ -20,7 +34,10 @@ export const DesktopMnemonicConfirm = memo((props: DesktopMnemonicConfirmProps) 
                         value={word}
                         InputProps={{ disableUnderline: true }}
                         disabled={indexes && !indexes.includes(i)}
-                        onChange={(e) => onChange(e.target.value, indexes ? indexes.indexOf(i) : i)}
+                        onChange={(e) => {
+                            if ((e.nativeEvent as InputEvent).inputType === 'insertFromPaste') return
+                            onChange(e.target.value, indexes ? indexes.indexOf(i) : i)
+                        }}
                     />
                 </Grid>
             ))}
