d2c060eb9a6e3f6300c98c85b3075437a3962b37
nuanyang233 <nuanyang233@gmail.com>
2021-12-02 03:10:51 +0800

    fix: retry ens scan when chain id changed

--- packages/plugin-infra/src/hooks/useLookupDomain.ts
+++ packages/plugin-infra/src/hooks/useLookupDomain.ts
@@ -1,13 +1,23 @@
 import { useWeb3State } from '../web3'
 import { useAsync } from 'react-use'
 import type { NetworkPluginID } from '../web3-types'
+import { useSubscription } from 'use-subscription'
+import { noop } from 'lodash-unified'
 
 export function useLookupAddress(domain: string, pluginId?: NetworkPluginID) {
-    const { NameService, Utils } = useWeb3State(pluginId)
+    const { NameService, Utils, Shared } = useWeb3State(pluginId)
+
+    const chainId = useSubscription(
+        Shared?.chainId ?? {
+            getCurrentValue: () => null,
+            subscribe: () => noop,
+        },
+    )
+
     return useAsync(async () => {
         if (NameService?.lookup && Utils?.isValidDomain?.(domain)) {
             return NameService.lookup(domain)
         }
         return ''
-    }, [NameService, Utils, domain])
+    }, [NameService, Utils, domain, chainId])
 }
