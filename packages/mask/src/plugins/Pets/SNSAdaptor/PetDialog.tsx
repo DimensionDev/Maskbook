41ac8199e2aa51583e4b7895416e88de70a1a439
javaLuo <376693576@qq.com>
2021-12-04 16:53:07 +0800

    refactor:  react block <>

--- packages/mask/src/plugins/Pets/SNSAdaptor/PetDialog.tsx
+++ packages/mask/src/plugins/Pets/SNSAdaptor/PetDialog.tsx
@@ -153,85 +153,86 @@ export function PetDialog() {
 
     const [isTipShow, setTipShow] = useState(false)
 
-    return [
-        <InjectedDialog open={open} onClose={closeDialog} title="Set up your NFT pet" key="dialog">
-            <DialogContent>
-                <Typography className={classes.des}>Can customize pet image and language</Typography>
-                <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                        <PreviewBox message={msgValue} imageUrl={imageChose} />
+    return (
+        <>
+            <InjectedDialog open={open} onClose={closeDialog} title="Set up your NFT pet">
+                <DialogContent>
+                    <Typography className={classes.des}>Can customize pet image and language</Typography>
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}>
+                            <PreviewBox message={msgValue} imageUrl={imageChose} />
+                        </Grid>
+                        <Grid item xs={8}>
+                            <TextField
+                                className={classes.input}
+                                label="Collections"
+                                fullWidth
+                                select
+                                required
+                                value={collectionsValue}
+                                error={isCollectionsError}
+                                variant="outlined"
+                                onChange={(e) => onCollectionsChage(e.target.value)}>
+                                <MenuItem key="Traveloggers" value="Traveloggers">
+                                    Traveloggers
+                                </MenuItem>
+                                <MenuItem key="MIRROR" value="MIRROR">
+                                    MIRROR
+                                </MenuItem>
+                            </TextField>
+                            <TextField
+                                className={classes.input}
+                                label="Image"
+                                fullWidth
+                                select
+                                required
+                                value={imageValue}
+                                error={isImageError}
+                                variant="outlined"
+                                disabled={!collectionsValue}
+                                maxRows={2}
+                                onChange={(e) => onImageChange(e.target.value)}>
+                                {selectMock.map((item, index) => {
+                                    return (
+                                        <MenuItem key={item.value} value={item.value}>
+                                            <Typography className={classes.itemFix}>
+                                                <img
+                                                    className={classes.thumbnail}
+                                                    src="https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=164&h=164&fit=crop&auto=format"
+                                                />
+                                                {item.label}
+                                            </Typography>
+                                        </MenuItem>
+                                    )
+                                })}
+                            </TextField>
+                            <TextField
+                                className={classes.input}
+                                label="Pet preset copy (Optional)"
+                                fullWidth
+                                multiline
+                                rows={3}
+                                disabled={!collectionsValue}
+                                value={msgValue}
+                                onChange={(e) => setMsgValueCheck(e.target.value)}
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={8}>
-                        <TextField
-                            className={classes.input}
-                            label="Collections"
-                            fullWidth
-                            select
-                            required
-                            value={collectionsValue}
-                            error={isCollectionsError}
-                            variant="outlined"
-                            onChange={(e) => onCollectionsChage(e.target.value)}>
-                            <MenuItem key="Traveloggers" value="Traveloggers">
-                                Traveloggers
-                            </MenuItem>
-                            <MenuItem key="MIRROR" value="MIRROR">
-                                MIRROR
-                            </MenuItem>
-                        </TextField>
-                        <TextField
-                            className={classes.input}
-                            label="Image"
-                            fullWidth
-                            select
-                            required
-                            value={imageValue}
-                            error={isImageError}
-                            variant="outlined"
-                            disabled={!collectionsValue}
-                            maxRows={2}
-                            onChange={(e) => onImageChange(e.target.value)}>
-                            {selectMock.map((item, index) => {
-                                return (
-                                    <MenuItem key={item.value} value={item.value}>
-                                        <Typography className={classes.itemFix}>
-                                            <img
-                                                className={classes.thumbnail}
-                                                src="https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=164&h=164&fit=crop&auto=format"
-                                            />
-                                            {item.label}
-                                        </Typography>
-                                    </MenuItem>
-                                )
-                            })}
-                        </TextField>
-                        <TextField
-                            className={classes.input}
-                            label="Pet preset copy (Optional)"
-                            fullWidth
-                            multiline
-                            rows={3}
-                            disabled={!collectionsValue}
-                            value={msgValue}
-                            onChange={(e) => setMsgValueCheck(e.target.value)}
-                        />
-                    </Grid>
-                </Grid>
 
-                <Button className={classes.btn} variant="contained" size="large" fullWidth onClick={saveHandle}>
-                    Confirm Add
-                </Button>
-                <Typography className={classes.desBox}>
-                    <Typography className={classes.des}>Support By: Mask</Typography>
-                    <Typography className={classes.des}>RSS3</Typography>
-                </Typography>
-            </DialogContent>
-        </InjectedDialog>,
-        <Snackbar
-            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-            open={isTipShow}
-            message="Your new pet has been successFully set up"
-            key="tip"
-        />,
-    ]
+                    <Button className={classes.btn} variant="contained" size="large" fullWidth onClick={saveHandle}>
+                        Confirm Add
+                    </Button>
+                    <Typography className={classes.desBox}>
+                        <Typography className={classes.des}>Support By: Mask</Typography>
+                        <Typography className={classes.des}>RSS3</Typography>
+                    </Typography>
+                </DialogContent>
+            </InjectedDialog>
+            <Snackbar
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                open={isTipShow}
+                message="Your new pet has been successFully set up"
+            />
+        </>
+    )
 }
