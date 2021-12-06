c81ddb42a48b999af390627eddb25c5bf2332bda
Jack Works <jackworks@protonmail.com>
2021-12-06 11:49:19 +0800

    fix: try another fix

--- packages/mask/src/database/index.ts
+++ packages/mask/src/database/index.ts
@@ -3,3 +3,13 @@ export * from './Persona/helpers'
 export * from './Persona/types'
 export * from './post'
 export * from './Plugin'
+
+// https://developer.apple.com/forums/thread/681201
+if (process.env.engine === 'safari') {
+    try {
+        window.indexedDB
+        setInterval(() => {
+            window.indexedDB
+        }, 30 * 1000)
+    } catch {}
+}
