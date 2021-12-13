33f6027e212746132caffa8a4eed4d9c3a85608c
guanbinrui <guanbinrui@dimension.im>
2021-12-14 01:14:54 +0800

    fix: auto connect to c98 & fortmatic if page mounted

--- packages/mask/src/plugins/EVM/UI/components/InjectedProviderBridge.tsx
+++ packages/mask/src/plugins/EVM/UI/components/InjectedProviderBridge.tsx
@@ -1,15 +1,48 @@
-import { useEffect } from 'react'
+import { useEffect, useCallback } from 'react'
+import { useMount } from 'react-use'
+import { unreachable } from '@dimensiondev/kit'
 import { bridgedEthereumProvider } from '@masknet/injected-script'
-import { useValueRef } from '@masknet/shared'
-import { isInjectedProvider } from '@masknet/web3-shared-evm'
+import { ProviderType, isInjectedProvider, isFortmaticSupported, ChainId } from '@masknet/web3-shared-evm'
+import { NetworkPluginID, useChainId, useProviderType } from '@masknet/plugin-infra'
 import { EVM_Messages } from '../../messages'
-import { currentProviderSettings } from '../../../Wallet/settings'
 import Services from '../../../../extension/service'
+import { WalletRPC } from '../../../Wallet/messages'
 
 export interface InjectedProviderBridgeProps {}
 
 export function InjectedProviderBridge(props: InjectedProviderBridgeProps) {
-    const providerType = useValueRef(currentProviderSettings)
+    const chainId = useChainId<ChainId>(NetworkPluginID.PLUGIN_EVM)
+    const providerType = useProviderType<ProviderType>(NetworkPluginID.PLUGIN_EVM)
+
+    const onMounted = useCallback(async () => {
+        const updateAccount = (connected: { account?: string; chainId: ChainId }) =>
+            WalletRPC.updateAccount({
+                account: connected.account,
+                chainId: connected.chainId,
+                providerType,
+            })
+
+        switch (providerType) {
+            case ProviderType.MaskWallet:
+            case ProviderType.MetaMask:
+            case ProviderType.WalletConnect:
+            case ProviderType.MathWallet:
+            case ProviderType.WalletLink:
+                break
+            case ProviderType.Fortmatic:
+                await updateAccount(
+                    await Services.Ethereum.connectFortmatic(isFortmaticSupported(chainId) ? chainId : ChainId.Mainnet),
+                )
+                break
+            case ProviderType.Coin98:
+                await updateAccount(await Services.Ethereum.connectInjected())
+                break
+            case ProviderType.CustomNetwork:
+                break
+            default:
+                unreachable(providerType)
+        }
+    }, [chainId, providerType])
 
     useEffect(() => {
         return EVM_Messages.events.INJECTED_PROVIDER_RPC_REQUEST.on(async ({ payload }) => {
@@ -45,6 +78,10 @@ export function InjectedProviderBridge(props: InjectedProviderBridgeProps) {
             Services.Ethereum.notifyInjectedEvent('chainChanged', event, providerType)
         })
     }, [providerType])
+
+    useMount(() => {
+        onMounted()
+    })
 
     return null
 }
