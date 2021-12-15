4a5a85ec776011cbaa0b06cdf69d0ebb95af17ea
Jack Works <jackworks@protonmail.com>
2021-12-15 13:48:41 +0800

    refactor: remove unused code and move files (#5187)
    * refactor: remove qrcode version backup support
    
    * refactor: move files
    
    * chore: patch webpack
    
    * refactor: rename file
    
    * fix: build
    
    * chore: fix typo

---
+++ packages/shared-base/src/convert/index.ts
@@ -0,0 +1,15 @@
+import { Convert } from 'pvtsutils'
+
+export function fromBase64URL(x: string) {
+    return new Uint8Array(Convert.FromBase64Url(x))
+}
+export function toBase64URL(x: BufferSource) {
+    return Convert.ToBase64Url(x)
+}
+export function toHex(x: BufferSource) {
+    return Convert.ToHex(x)
+}
+export function fromHex(x: string) {
+    if (x.startsWith('0x')) x = x.slice(2)
+    return new Uint8Array(Convert.FromHex(x))
+}
