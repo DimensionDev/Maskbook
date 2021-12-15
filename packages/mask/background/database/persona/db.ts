8fc7f0a5eb9031a2d4b4424d3640158613982432
Lantt <laanfor@mask.io>
2021-12-15 21:03:14 +0800

    fix: should keep login status when restore persona which not have private key (#5182)
    * fix: should keep login status when restore persona which not have private key
    
    * fix: user can send nft to self and contract
    
    * feat: should check ens when send to self

--- packages/mask/background/database/persona/db.ts
+++ packages/mask/background/database/persona/db.ts
@@ -308,11 +308,16 @@ export async function createOrUpdatePersonaDB(
     t: PersonasTransaction<'readwrite'>,
 ) {
     const personaInDB = await t.objectStore('personas').get(record.identifier.toText())
+
+    if (howToMerge.protectPrivateKey && !!personaInDB?.privateKey && !record.privateKey) return
+
     if (howToMerge.protectPrivateKey && !!personaInDB?.privateKey) {
         const nextRecord = personaRecordOutDB(personaInDB)
         nextRecord.hasLogout = false
+
         return updatePersonaDB(nextRecord, howToMerge, t)
     }
+
     if (personaInDB) return updatePersonaDB(record, howToMerge, t)
     else
         return createPersonaDB(
