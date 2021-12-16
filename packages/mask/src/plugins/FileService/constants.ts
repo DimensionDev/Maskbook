44a59df99875cb54d47e8065a3b136f5e2d3fa81
fun <lljxx1@gmail.com>
2021-12-16 18:29:42 +0800

    refactor: provider agent

--- packages/mask/src/plugins/FileService/constants.ts
+++ packages/mask/src/plugins/FileService/constants.ts
@@ -1,6 +1,3 @@
-import type { ProviderConfig } from './types'
-import { Provider } from './types'
-
 export const FileServicePluginID = 'com.maskbook.fileservice'
 export const META_KEY_1 = 'com.maskbook.fileservice:1'
 export const META_KEY_2 = 'com.maskbook.fileservice:2'
