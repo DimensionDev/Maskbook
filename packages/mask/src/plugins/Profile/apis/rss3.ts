970ebe332fd4ec3fd537ee8f89236b94a7c064a3
guanbinrui <guanbinrui@dimension.im>
2021-12-19 17:56:42 +0800

    refactor: remove unused code

--- packages/mask/src/plugins/Profile/apis/rss3.ts
+++ packages/mask/src/plugins/Profile/apis/rss3.ts
@@ -1,10 +1,8 @@
 import RSS3 from 'rss3-next'
-import { hexlify } from '@ethersproject/bytes'
-import { toUtf8Bytes } from '@ethersproject/strings'
 import { fetch } from './middleware'
-import type { GitcoinResponse, GeneralAsset, NFTResponse, POAPResponse } from './types'
+import type { GeneralAsset } from './types'
 import config from './config'
-import { personalSign } from '../../../extension/background-script/EthereumService'
+import { EthereumAddress } from 'wallet.ts'
 
 const EMPTY_RSS3_DP: RSS3DetailPersona = {
     persona: null,
@@ -25,18 +23,6 @@ export interface RSS3DetailPersona {
     persona: RSS3 | null
     address: string
     isReady: boolean
-}
-
-async function connect(address: string, skipSignSync: boolean = false) {
-    RSS3LoginUser.persona = new RSS3({
-        endpoint: config.hubEndpoint,
-        address: address,
-        agentSign: true,
-        sign: (data: string) => personalSign(hexlify(toUtf8Bytes(data)), address.toLowerCase()),
-    })
-    await initUser(RSS3LoginUser, skipSignSync)
-
-    return RSS3LoginUser
 }
 
 async function initUser(user: RSS3DetailPersona, skipSignSync: boolean = false) {
@@ -61,32 +47,15 @@ function apiPersona(): RSS3 {
     )
 }
 
-export async function isValidRSS3() {
-    return !!RSS3LoginUser.persona
-}
-
-export async function connectUser(address: string) {
-    try {
-        if (await connect(address)) return RSS3LoginUser
-        return null
-    } catch {
-        return null
-    }
-}
-
 export async function getAPIUser(): Promise<RSS3DetailPersona> {
     const user = Object.create(EMPTY_RSS3_DP)
     user.persona = apiPersona()
     return user
 }
 
-export async function getLoginUser() {
-    return RSS3LoginUser
-}
-
 export async function setPageOwner(addrOrName: string) {
     let isReloadRequired = false
-    if (addrOrName.startsWith('0x') && addrOrName.length === 42) {
+    if (EthereumAddress.isValid(addrOrName)) {
         if (RSS3PageOwner.address !== addrOrName) {
             isReloadRequired = true
             RSS3PageOwner.address = addrOrName
@@ -121,76 +90,6 @@ export async function getAssetProfile(
         const data = res?.data
         if (data) assetsProfileCache.set(address + type, data)
         return data
-    } catch {
-        return null
-    }
-}
-
-export async function getNFTDetails(
-    address: string,
-    platform: string,
-    identity: string,
-    id: string,
-    type: string,
-): Promise<NFTResponse | null> {
-    try {
-        const res = await fetch(`/assets/details`, {
-            baseURL: config.hubEndpoint,
-            params: {
-                personaID: address,
-                platform: 'EVM+',
-                id,
-                identity,
-                type,
-            },
-        })
-        return res?.data || null
-    } catch {
-        return null
-    }
-}
-
-export async function getGitcoinDonation(
-    address: string,
-    platform: string,
-    identity: string,
-    id: string,
-): Promise<GitcoinResponse | null> {
-    try {
-        const res = await fetch(`/assets/details`, {
-            baseURL: config.hubEndpoint,
-            params: {
-                personaID: address,
-                platform: 'EVM+',
-                id: id,
-                identity: identity,
-                type: 'Gitcoin-Donation',
-            },
-        })
-        return res?.data || null
-    } catch {
-        return null
-    }
-}
-
-export async function getFootprintDetail(
-    address: string,
-    platform: string,
-    identity: string,
-    id: string,
-): Promise<POAPResponse | null> {
-    try {
-        const res = await fetch(`/assets/details`, {
-            baseURL: config.hubEndpoint,
-            params: {
-                personaID: address,
-                platform: 'EVM+',
-                id: id,
-                identity,
-                type: 'xDai-POAP',
-            },
-        })
-        return res?.data || null
     } catch {
         return null
     }
