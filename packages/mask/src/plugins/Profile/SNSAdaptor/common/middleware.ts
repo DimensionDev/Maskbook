4341acf01fc23d932c0c668415939c110017c53b
unclebill <billbill290@gmail.com>
2021-12-19 11:41:01 +0800

    fix: no need to check or request web request permission

--- packages/mask/src/plugins/Profile/SNSAdaptor/common/middleware.ts
+++ packages/mask/src/plugins/Profile/SNSAdaptor/common/middleware.ts
@@ -1,20 +1,5 @@
 import Services from '../../../../extension/service'
 
-const authorizedOrigins: string[] = []
-async function preFetchTestOrigin(origin: string) {
-    if (!authorizedOrigins.includes(origin)) {
-        try {
-            await Services.Helper.requestExtensionPermission({
-                origins: [origin + '/'],
-                permissions: ['webRequest'],
-            })
-            authorizedOrigins.push(origin)
-        } catch (error) {
-            throw error
-        }
-    }
-}
-
 export async function fetch(
     url: string,
     options: { baseURL: string; params?: { [key: string]: string } },
@@ -24,7 +9,6 @@ export async function fetch(
         urlObj.searchParams.set(key, options.params[key])
     }
     try {
-        await preFetchTestOrigin(urlObj.origin)
         const res = await Services.Helper.fetchJSON(urlObj.href)
         return {
             data: res,
