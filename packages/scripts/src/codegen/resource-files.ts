6cfad6e8f0aa60d0072da2e50ae856c21a02b81c
Jack Works <jackworks@protonmail.com>
2021-12-20 18:16:55 +0800

    fix: typo

--- packages/scripts/src/codegen/resource-files.ts
+++ packages/scripts/src/codegen/resource-files.ts
@@ -8,12 +8,12 @@ const Encryption = join(__dirname, '../../../encryption')
 
 const CopyShared = () => src('./src/**/*.png', { cwd: Shared }).pipe(dest('./dist', { cwd: Shared }))
 const CopyFlow = () => src('./src/**/*.png', { cwd: Flow }).pipe(dest('./dist', { cwd: Flow }))
-const ColfEncryption = () => src('./src/**/*.png', { cwd: Encryption }).pipe(dest('./dist', { cwd: Encryption }))
+const CopyEncryption = () => src('./src/**/*.png', { cwd: Encryption }).pipe(dest('./dist', { cwd: Encryption }))
 
-export const resourceCopy: TaskFunction = parallel(CopyShared, CopyFlow, ColfEncryption)
+export const resourceCopy: TaskFunction = parallel(CopyShared, CopyFlow, CopyEncryption)
 export function resourceCopyWatch() {
     watch('./src/**/*.png', { cwd: Flow }, CopyFlow)
-    watch('./src/**/*.png', { cwd: Encryption }, ColfEncryption)
+    watch('./src/**/*.png', { cwd: Encryption }, CopyEncryption)
     return watch('./src/**/*.png', { cwd: Shared }, CopyShared)
 }
 watchTask(resourceCopy, resourceCopyWatch, 'resource-copy', 'Copy resources')
