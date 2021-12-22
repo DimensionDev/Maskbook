05e264d2cabcfcd47ac95d9551c4b4f93ff3402f
Jack Works <jackworks@protonmail.com>
2021-12-22 23:26:55 +0800

    feat: support dts rollup

--- packages/provider-proxy/build.mjs
+++ packages/provider-proxy/build.mjs
@@ -8,53 +8,51 @@ const __file = fileURLToPath(import.meta.url)
 const __dirname = resolve(__file, '../')
 
 function awaitChildProcess(child) {
-    return new Promise((resolve, reject) => {
-        child.on('error', () => reject(child.exitCode || 0))
-        child.on('exit', (code) => resolve(code || 0))
-    })
+  return new Promise((resolve, reject) => {
+    child.on('error', () => reject(child.exitCode || 0))
+    child.on('exit', (code) => resolve(code || 0))
+  })
 }
 
 const rollup = awaitChildProcess(
-    spawn('pnpm run build:rollup', {
-        cwd: __dirname,
-        shell: true,
-        stdio: 'inherit',
-    }),
+  spawn('pnpm run build:rollup', {
+    cwd: __dirname,
+    shell: true,
+    stdio: 'inherit',
+  }),
 )
 await rollup
 const version = JSON.parse(await readFile(join(__dirname, './package.json'), 'utf-8')).version
 const now = new Date()
 const packageJSON = {
-    name: '@dimensiondev/provider-proxy',
-    version: `${version}-${now.getFullYear()}${String(now.getMonth() + 1).padStart(2)}${String(now.getDate()).padStart(
-        2,
-    )}-${String(now.getHours()).padStart(2)}${String(now.getMinutes()).padStart(2)}`.replace(' ', '0'),
-    dependencies: {
-        'wallet.ts': '1.0.1',
-        'bignumber.js': '9.0.1',
-        'socket.io-client': '2.4.0',
-    },
-    main: './output.js',
-    types: './index.d.ts',
+  name: '@dimensiondev/provider-proxy',
+  version: `${version}-${now.getFullYear()}${String(now.getMonth() + 1).padStart(2)}${String(now.getDate()).padStart(
+    2,
+  )}-${String(now.getHours()).padStart(2)}${String(now.getMinutes()).padStart(2)}`.replace(' ', '0'),
+  dependencies: {
+    'wallet.ts': '1.0.1',
+    'bignumber.js': '9.0.1',
+    'socket.io-client': '2.4.0',
+  },
+  main: './output.js',
+  types: './output.d.ts',
+  files: ['output.js', 'output.d.ts'],
 }
 await writeFile(join(__dirname, './dist/package.json'), JSON.stringify(packageJSON, undefined, 4))
 
 const out = join(__dirname, './dist')
 // validate if there is dependency missing
 await awaitChildProcess(
-    spawn('npm install', {
-        cwd: out,
-        shell: true,
-        stdio: 'inherit',
-    }),
+  spawn('npm install', {
+    cwd: out,
+    shell: true,
+    stdio: 'inherit',
+  }),
 )
 await awaitChildProcess(
-    spawn('node output.js', {
-        cwd: out,
-        shell: true,
-        stdio: 'inherit',
-    }),
+  spawn('node output.js', {
+    cwd: out,
+    shell: true,
+    stdio: 'inherit',
+  }),
 )
-
-await unlink(join(__dirname, './dist/index.ts'))
-await rmdir(join(__dirname, './dist/producers'), { recursive: true, force: true })
--- packages/provider-proxy/rollup.config.js
+++ packages/provider-proxy/rollup.config.js
@@ -3,39 +3,44 @@ import node from '@rollup/plugin-node-resolve'
 import sucrase from '@rollup/plugin-sucrase'
 import json from '@rollup/plugin-json'
 import alias from '@rollup/plugin-alias'
-import typescript from '@rollup/plugin-typescript'
-
-const typesBuildConfig = {
-    input: './src/index.ts',
-    output: [{ file: './dist/index.ts', format: 'cjs' }],
-    plugins: [typescript({ tsconfig: './tsconfig.json' })],
-}
+import dts from 'rollup-plugin-dts'
 
 const config = {
-    input: './src/index.ts',
-    output: {
-        file: 'dist/output.js',
-        format: 'cjs',
-    },
-    plugins: [
-        commonjs(),
-        node(),
-        json(),
-        alias({
-            entries: [
-                { find: '@masknet/web3-shared-evm', replacement: '../web3-shared/evm/index.ts' },
-                { find: '@masknet/web3-shared-base', replacement: '../web3-shared/base/index.ts' },
-            ],
-        }),
-        sucrase({ transforms: ['typescript', 'jsx'] }),
-    ],
-    external: (id) => {
-        if (id.startsWith('.')) return false
-        if (id.includes('@masknet')) return false
-        if (id.includes('node_modules')) return true
-        return false
-    },
-    treeshake: { moduleSideEffects: false, propertyReadSideEffects: false },
+  input: './src/index.ts',
+  output: {
+    file: 'dist/output.js',
+    format: 'cjs',
+  },
+  plugins: [
+    commonjs(),
+    node(),
+    json(),
+    alias({
+      entries: [
+        { find: '@masknet/web3-shared-evm', replacement: '../web3-shared/evm/index.ts' },
+        { find: '@masknet/web3-shared-base', replacement: '../web3-shared/base/index.ts' },
+      ],
+    }),
+    sucrase({ transforms: ['typescript', 'jsx'] }),
+  ],
+  external: (id) => {
+    if (id.startsWith('.')) return false
+    if (id.includes('@masknet')) return false
+    if (id.includes('node_modules')) return true
+    return false
+  },
+  treeshake: { moduleSideEffects: false, propertyReadSideEffects: false },
 }
 
-export default [config, typesBuildConfig]
+const dtsConfig = {
+  input: './dist/index.d.ts',
+  output: [{ file: './dist/output.d.ts', format: 'es' }],
+  plugins: [
+    dts({
+      respectExternal: true,
+    }),
+  ],
+  external: config.external,
+}
+
+export default [config, dtsConfig]
