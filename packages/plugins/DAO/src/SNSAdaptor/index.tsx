39855d2b37672b6e0e933482dc4146b5b01c0b8c
guanbinrui <guanbinrui@dimension.im>
2021-12-24 11:12:45 +0800

    chore: add the DAO tab

---
+++ packages/plugins/DAO/src/SNSAdaptor/index.tsx
@@ -0,0 +1,9 @@
+import type { Plugin } from '@masknet/plugin-infra'
+import { base } from '../base'
+
+const sns: Plugin.SNSAdaptor.Definition = {
+    ...base,
+    init(signal) {},
+}
+
+export default sns
