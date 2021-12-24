6e1ab7424aed892865c8da6366892616ede8a204
lelenei <zwpsky@gmail.com>
2021-12-24 14:56:34 +0800

    feat: nft page to web3 plugin

--- packages/mask/src/plugins/Collectible/SNSAdaptor/index.tsx
+++ packages/mask/src/plugins/Collectible/SNSAdaptor/index.tsx
@@ -5,8 +5,9 @@ import { PostInspector } from './PostInspector'
 import { base } from '../base'
 import type { CollectibleJSON_Payload } from '../types'
 import { checkUrl, getAssetInfoFromURL, getRelevantUrl } from '../utils'
-import { PLUGIN_NAME } from '../constants'
+import { PLUGIN_NAME, PLUGIN_PROFILE_TAB_NAME } from '../constants'
 import { getTypedMessageContent } from '../../../protocols/typed-message'
+import { NFTPage } from './NFTPage'
 
 const sns: Plugin.SNSAdaptor.Definition = {
     ...base,
@@ -24,6 +25,15 @@ const sns: Plugin.SNSAdaptor.Definition = {
         const asset = getAssetInfoFromURL(collectibleUrl)
         return asset ? renderPostInspector(asset) : null
     },
+
+    ProfileTabs: [
+        {
+            ID: PLUGIN_NAME,
+            label: PLUGIN_PROFILE_TAB_NAME,
+            priority: 1,
+            children: ({ addressNames = [] }) => <NFTPage address={addressNames[0].resolvedAddress} />,
+        },
+    ],
 }
 
 export default sns
