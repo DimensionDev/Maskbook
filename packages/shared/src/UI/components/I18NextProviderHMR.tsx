a1dce6b0827cb7d36a8d2d3e1315ee39024c0df8
Jack Works <jackworks@protonmail.com>
2021-12-26 09:56:36 +0800

    chore: apply review

--- packages/shared/src/UI/components/I18NextProviderHMR.tsx
+++ packages/shared/src/UI/components/I18NextProviderHMR.tsx
@@ -1,29 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { I18nextProvider, type I18nextProviderProps } from 'react-i18next'
 
-export function I18NextProviderHMR({ i18n, defaultNS, children }: React.PropsWithChildren<I18nextProviderProps>) {
-    if (process.env.NODE_ENV === 'development') {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [ns, setNS] = useState(defaultNS)
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        useEffect(() => {
-            const f = () => setNS('HMR')
-            globalThis.addEventListener('MASK_I18N_HMR', f)
-            return () => globalThis.removeEventListener('MASK_I18N_HMR', f)
-        }, [])
+export const I18NextProviderHMR =
+    process.env.NODE_ENV === 'development'
+        ? function I18NextProviderHMR({ i18n, defaultNS, children }: React.PropsWithChildren<I18nextProviderProps>) {
+              const [ns, setNS] = useState(defaultNS)
 
-        // Force trigger a re-render to apply HMR
-        if (ns === 'HMR') defaultNS = Math.random() + ''
+              useEffect(() => {
+                  const f = () => setNS('HMR')
+                  globalThis.addEventListener('MASK_I18N_HMR', f)
+                  return () => globalThis.removeEventListener('MASK_I18N_HMR', f)
+              }, [])
+              // Force trigger a re-render to apply HMR
+              if (ns === 'HMR') defaultNS = Math.random() + ''
 
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        useEffect(() => {
-            if (ns === 'HMR') setNS('')
-        }, [ns])
-    }
-    // deliberately call it as a function in order to skip a React component nesting level.
-    return I18nextProvider({
-        i18n,
-        defaultNS,
-        children,
-    })
-}
+              useEffect(() => {
+                  if (ns === 'HMR') setNS('')
+              }, [ns])
+              // deliberately call it as a function in order to skip a React component nesting level.
+              return I18nextProvider({
+                  i18n,
+                  defaultNS,
+                  children,
+              })
+          }
+        : I18nextProvider
