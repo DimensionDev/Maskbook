1ef6710842760ad397ec54c6091109524bfa5f2b
Lantt <laanfor@mask.io>
2021-12-26 14:12:54 +0800

    fix: ci

--- packages/provider-proxy/rollup.config.js
+++ packages/provider-proxy/rollup.config.js
@@ -6,41 +6,41 @@ import alias from '@rollup/plugin-alias'
 import dts from 'rollup-plugin-dts'
 
 const config = {
-  input: './src/index.ts',
-  output: {
-    file: 'dist/output.js',
-    format: 'cjs',
-  },
-  plugins: [
-    commonjs(),
-    node(),
-    json(),
-    alias({
-      entries: [
-        { find: '@masknet/web3-shared-evm', replacement: '../web3-shared/evm/index.ts' },
-        { find: '@masknet/web3-shared-base', replacement: '../web3-shared/base/index.ts' },
-      ],
-    }),
-    sucrase({ transforms: ['typescript', 'jsx'] }),
-  ],
-  external: (id) => {
-    if (id.startsWith('.')) return false
-    if (id.includes('@masknet')) return false
-    if (id.includes('node_modules')) return true
-    return false
-  },
-  treeshake: { moduleSideEffects: false, propertyReadSideEffects: false },
+    input: './src/index.ts',
+    output: {
+        file: 'dist/output.js',
+        format: 'cjs',
+    },
+    plugins: [
+        commonjs(),
+        node(),
+        json(),
+        alias({
+            entries: [
+                { find: '@masknet/web3-shared-evm', replacement: '../web3-shared/evm/index.ts' },
+                { find: '@masknet/web3-shared-base', replacement: '../web3-shared/base/index.ts' },
+            ],
+        }),
+        sucrase({ transforms: ['typescript', 'jsx'] }),
+    ],
+    external: (id) => {
+        if (id.startsWith('.')) return false
+        if (id.includes('@masknet')) return false
+        if (id.includes('node_modules')) return true
+        return false
+    },
+    treeshake: { moduleSideEffects: false, propertyReadSideEffects: false },
 }
 
 const dtsConfig = {
-  input: './dist/index.d.ts',
-  output: [{ file: './dist/output.d.ts', format: 'es' }],
-  plugins: [
-    dts({
-      respectExternal: true,
-    }),
-  ],
-  external: config.external,
+    input: './dist/index.d.ts',
+    output: [{ file: './dist/output.d.ts', format: 'es' }],
+    plugins: [
+        dts({
+            respectExternal: true,
+        }),
+    ],
+    external: config.external,
 }
 
 export default [config, dtsConfig]
