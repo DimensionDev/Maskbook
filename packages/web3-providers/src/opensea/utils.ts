e4489e21cb98bf5bd7440502a3a54f1a10bcac54
lelenei <zwpsky@gmail.com>
2021-12-27 20:13:55 +0800

    fix: use BigNumber

--- packages/web3-providers/src/opensea/utils.ts
+++ packages/web3-providers/src/opensea/utils.ts
@@ -1,16 +1,15 @@
 import BigNumber from 'bignumber.js'
-import { formatBalance } from '@masknet/web3-shared-evm'
+import { pow10 } from '@masknet/web3-shared-base'
 
 export function getOrderUnitPrice(currentPrice?: string, decimals?: number, quantity?: string) {
     if (!currentPrice || !decimals || !quantity) return
-    currentPrice = formatBalance(currentPrice, decimals)
-    quantity = formatBalance(quantity, new BigNumber(quantity).toString() !== '1' ? 8 : 0)
-    return new BigNumber(currentPrice).dividedBy(quantity).decimalPlaces(4, 1)
+    const _currentPrice = new BigNumber(currentPrice).div(pow10(decimals))
+    const _quantity = new BigNumber(quantity).div(pow10(new BigNumber(quantity).toString() !== '1' ? 8 : 0))
+    return _currentPrice.dividedBy(_quantity).decimalPlaces(4, 1)
 }
 
 export function getOrderUSDPrice(currentPrice?: string, usdPrice?: string, decimals?: number) {
     if (!currentPrice || !decimals) return
-    const price = formatBalance(usdPrice, 0)
-    const quantity = formatBalance(currentPrice, decimals)
-    return new BigNumber(price).multipliedBy(quantity).decimalPlaces(2, 1)
+    const quantity = new BigNumber(currentPrice).div(pow10(decimals))
+    return new BigNumber(usdPrice).multipliedBy(quantity).decimalPlaces(2, 1)
 }
