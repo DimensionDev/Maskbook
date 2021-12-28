89801d7762313b7124c9caabddc1d3ef5acc2b69
guanbinrui <guanbinrui@dimension.im>
2021-12-28 16:11:53 +0800

    refactor: move now out of hooks

--- packages/mask/src/plugins/MaskBox/hooks/useContext.ts
+++ packages/mask/src/plugins/MaskBox/hooks/useContext.ts
@@ -40,6 +40,7 @@ import { useIsWhitelisted } from './useIsWhitelisted'
 import { isGreaterThanOrEqualTo, isLessThanOrEqualTo, isZero, multipliedBy } from '@masknet/web3-shared-base'
 
 function useContext(initialState?: { boxId: string }) {
+    const now = new Date()
     const heartBit = useHeartBit()
     const account = useAccount()
     const chainId = useChainId()
@@ -139,7 +140,6 @@ function useContext(initialState?: { boxId: string }) {
         if (maskBoxInfo && !boxInfo) return BoxState.UNKNOWN
         if (!maskBoxInfo || !maskBoxStatus || !boxInfo) return BoxState.NOT_FOUND
         if (maskBoxStatus.canceled) return BoxState.CANCELED
-        const now = new Date()
         if (isGreaterThanOrEqualTo(boxInfo.tokenIdsPurchased.length, boxInfo.personalLimit)) return BoxState.DRAWED_OUT
         if (isLessThanOrEqualTo(boxInfo.remaining, 0)) return BoxState.SOLD_OUT
         if (boxInfo.startAt > now) return BoxState.NOT_READY
@@ -170,10 +170,10 @@ function useContext(initialState?: { boxId: string }) {
             case BoxState.EXPIRED:
                 return 'Ended'
             case BoxState.NOT_READY:
-                const now = Date.now()
+                const nowAt = now.getTime()
                 const startAt = boxInfo?.startAt.getTime() ?? 0
-                if (startAt <= now) return 'Loading...'
-                const countdown = formatCountdown(startAt, now)
+                if (startAt <= nowAt) return 'Loading...'
+                const countdown = formatCountdown(startAt, nowAt)
                 return countdown ? `Start sale in ${countdown}` : 'Loading...'
             case BoxState.SOLD_OUT:
                 return 'Sold Out'
