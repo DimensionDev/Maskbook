184200125130569524334e1d7a5c3c512e161ea4
etouyang <etouyang@gmail.com>
2021-12-29 19:54:55 +0800

    fix: remove not used props

--- packages/mask/src/plugins/Pets/SNSAdaptor/ImgLoader.tsx
+++ packages/mask/src/plugins/Pets/SNSAdaptor/ImgLoader.tsx
@@ -2,17 +2,11 @@ import { CircularProgress } from '@mui/material'
 import { ImgHTMLAttributes, useState } from 'react'
 
 export function ImgLoader(props: ImgHTMLAttributes<HTMLImageElement>) {
-    const { children, className, ...other } = props
     const [imgLoad, setImgLoad] = useState(false)
 
     return (
         <>
-            <img
-                {...other}
-                className={className}
-                onLoad={() => setImgLoad(true)}
-                style={{ display: imgLoad ? 'block' : 'none' }}
-            />
+            <img {...props} onLoad={() => setImgLoad(true)} style={{ display: imgLoad ? 'block' : 'none' }} />
             {!imgLoad ? <CircularProgress size={20} /> : null}
         </>
     )
