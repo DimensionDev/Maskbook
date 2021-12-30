d888cbd3b52751016e7d3c792dbfdb1b7e707622
zhouhanseng <z308114274@gmail.com>
2021-12-30 12:38:35 +0800

    fix: conflict

--- packages/mask/src/plugins/Trader/trader/openocean/useTrade.ts
+++ packages/mask/src/plugins/Trader/trader/openocean/useTrade.ts
@@ -1,6 +1,6 @@
 import {
     FungibleTokenDetailed,
-    isNative,
+    isNativeTokenAddress,
     useAccount,
     useBlockNumber,
     useChainId,
@@ -33,14 +33,14 @@ export function useTrade(
     return useAsyncRetry(async () => {
         if (!inputToken || !outputToken) return null
         if (inputAmount === '0') return null
-        const sellToken = isNative(inputToken.address)
+        const sellToken = isNativeTokenAddress(inputToken.address)
             ? { ...inputToken, address: OPENOCEAN_ETH_ADDRESS ?? '' }
             : inputToken
-        const buyToken = isNative(outputToken.address)
+        const buyToken = isNativeTokenAddress(outputToken.address)
             ? { ...outputToken, address: OPENOCEAN_ETH_ADDRESS ?? '' }
             : outputToken
         return PluginTraderRPC.swapOO({
-            isNativeSellToken: isNative(inputToken.address),
+            isNativeSellToken: isNativeTokenAddress(inputToken.address),
             fromToken: sellToken,
             toToken: buyToken,
             fromAmount: inputAmount,
