ba97f5a49412f3cb18ba463e1736f4af170cd6ad
codingsh <codingsh@pm.me>
2021-12-30 19:56:57 +0000

    feat(optimistc): add events optimistic

--- packages/shared-base/src/Messages/Mask.ts
+++ packages/shared-base/src/Messages/Mask.ts
@@ -35,6 +35,7 @@ export interface MaskSettingsEvents {
     xdaiNetworkTradeProviderSettings: TradeProvider
     fantomNetworkTradeProviderSettings: TradeProvider
     celoNetworkTradeProviderSettings: TradeProvider
+    optimisticNetworkTradeProviderSettings: TradeProvider
 }
 
 export interface MaskMobileOnlyEvents {
