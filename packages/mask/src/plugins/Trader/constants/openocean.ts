ad2eee58777222a87d97f12cb314be6e32529cfe
zhouhanseng <z308114274@gmail.com>
2021-12-30 12:40:48 +0800

    fix: conflict

--- packages/mask/src/plugins/Trader/constants/openocean.ts
+++ packages/mask/src/plugins/Trader/constants/openocean.ts
@@ -8,4 +8,6 @@ export const networkNames: Record<NetworkType, string> = {
     [NetworkType.Polygon]: 'polygon',
     [NetworkType.Arbitrum]: 'arbitrum',
     [NetworkType.xDai]: 'xdai',
+    [NetworkType.Celo]: 'celo',
+    [NetworkType.Fantom]: 'fantom',
 }
