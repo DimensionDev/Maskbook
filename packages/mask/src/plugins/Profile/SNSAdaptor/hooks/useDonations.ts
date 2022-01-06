d9877f7b84d1413f1d446db5eeb9d39124c12bba
lelenei <72531217+lelenei@users.noreply.github.com>
2022-01-06 23:02:02 +0800

    fix: check address (#5368)
    * fix: check address
    
    * fix: reply review

--- packages/mask/src/plugins/Profile/SNSAdaptor/hooks/useDonations.ts
+++ packages/mask/src/plugins/Profile/SNSAdaptor/hooks/useDonations.ts
@@ -3,6 +3,7 @@ import { PluginProfileRPC } from '../../messages'
 
 export function useDonations(address: string) {
     return useAsync(async () => {
+        if (!address) return []
         const response = await PluginProfileRPC.getDonations(address)
         return response.status ? response.assets : []
     }, [address])
--- packages/mask/src/plugins/Profile/SNSAdaptor/hooks/useFootprints.ts
+++ packages/mask/src/plugins/Profile/SNSAdaptor/hooks/useFootprints.ts
@@ -3,6 +3,7 @@ import { PluginProfileRPC } from '../../messages'
 
 export function useFootprints(address: string) {
     return useAsync(async () => {
+        if (!address) return []
         const response = await PluginProfileRPC.getFootprints(address)
         return response.status ? response.assets : []
     }, [address])
