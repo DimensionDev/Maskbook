3d2d0b7aac8686a03de8273300688fb8881465a0
Jack Works <jackworks@protonmail.com>
2022-01-06 14:09:25 +0800

    chore: change

--- packages/mask/src/components/InjectedComponents/DecryptedPost/DecryptPostAwaiting.tsx
+++ packages/mask/src/components/InjectedComponents/DecryptedPost/DecryptPostAwaiting.tsx
@@ -1,11 +1,10 @@
 import { memo } from 'react'
-import { ShadowRootTooltip, useI18N } from '../../../utils'
+import { useI18N } from '../../../utils'
 import { AdditionalContent } from '../AdditionalPostContent'
 import type { ProfileIdentifier } from '@masknet/shared-base'
 import { wrapAuthorDifferentMessage } from './authorDifferentMessage'
 import { DecryptIntermediateProgressKind, DecryptIntermediateProgress } from '@masknet/encryption'
 import { useTimeout } from 'react-use'
-import { Typography } from '@mui/material'
 export interface DecryptPostAwaitingProps {
     type?: DecryptIntermediateProgress
     /** The author in the payload */
@@ -24,16 +23,13 @@ export const DecryptPostAwaiting = memo(function DecryptPostAwaiting(props: Decr
         text = t('decrypted_postbox_decrypting_finding_post_key')
         if (isReady()) text = t('decrypted_postbox_decrypting_finding_post_key_long')
     }
-    // unused
-    t('decrypted_postbox_decrypting_finding_person_key')
 
-    const header = wrapAuthorDifferentMessage(
-        author,
-        postedBy,
-        <ShadowRootTooltip title="If this post is not shared with you, it will be like this.">
-            <Typography variant="caption">Can't decrypt?</Typography>
-        </ShadowRootTooltip>,
-    )
     const shouldShowProgress = type?.event !== DecryptIntermediateProgressKind.TryDecryptByE2E || !isReady()
-    return <AdditionalContent title={text} progress={shouldShowProgress} headerActions={header} />
+    return (
+        <AdditionalContent
+            title={text}
+            progress={shouldShowProgress}
+            headerActions={wrapAuthorDifferentMessage(author, postedBy, undefined)}
+        />
+    )
 })
