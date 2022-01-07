357c81d77e62455903b7cff3429310fdfc702581
zhouhanseng <z308114274@gmail.com>
2022-01-07 14:22:16 +0800

    chore: update maskbox and nft wall

--- packages/mask/src/extension/options-page/DashboardComponents/CollectibleList/CollectibleCard.tsx
+++ packages/mask/src/extension/options-page/DashboardComponents/CollectibleList/CollectibleCard.tsx
@@ -1,11 +1,9 @@
-import { useAsyncRetry } from 'react-use'
-import { Card, CircularProgress, Link } from '@mui/material'
+import { Card, Link } from '@mui/material'
 import { makeStyles } from '@masknet/theme'
 import { Wallet, ERC721TokenDetailed, resolveCollectibleLink, NonFungibleAssetProvider } from '@masknet/web3-shared-evm'
 import { MaskSharpIconOfSize } from '../../../../resources/MaskIcon'
+import { NFTCardStyledAssetPlayer } from '@masknet/shared'
 import { ActionsBarNFT } from '../ActionsBarNFT'
-import { Video } from '../../../../components/shared/Video'
-import { Image } from '../../../../components/shared/Image'
 
 const useStyles = makeStyles()((theme) => ({
     root: {
@@ -31,9 +29,15 @@ const useStyles = makeStyles()((theme) => ({
         height: 64,
         opacity: 0.1,
     },
-    video: {
-        width: 172,
-        height: 172,
+    loadingFailImage: {
+        minHeight: '0px !important',
+        maxWidth: 'none',
+        width: 64,
+        height: 64,
+    },
+    wrapper: {
+        width: '172px !important',
+        height: '172px !important',
     },
 }))
 
@@ -44,63 +48,34 @@ export interface CollectibleCardProps {
     readonly?: boolean
 }
 
-const videoTypeRe = /\.(mp4|mp3|m4v|ogg)$/i
-
 export function CollectibleCard(props: CollectibleCardProps) {
     const { wallet, token, provider, readonly } = props
     const { classes } = useStyles()
 
-    const mediaUrl = token.info.mediaUrl
-    const { loading, value } = useAsyncRetry(async () => {
-        if (!mediaUrl) return
-
-        const blob = await (await fetch(mediaUrl)).blob()
-        return blob
-    }, [mediaUrl])
-
-    const mimeType = value?.type || ''
-    // some video resources response content-type not video, e.g. application/octet-stream
-    const isVideo = mediaUrl ? videoTypeRe.test(mediaUrl) || mimeType.startsWith('video') : undefined
-    const isHtml = mimeType.startsWith('text')
-
     return (
-        <>
-            {loading ? (
-                <Card className={classes.root}>
-                    <CircularProgress />
-                </Card>
-            ) : (
-                <Link
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={resolveCollectibleLink(token.contractDetailed.chainId, provider, token)}>
-                    <Card className={classes.root}>
-                        {readonly || !wallet ? null : (
-                            <ActionsBarNFT classes={{ more: classes.icon }} wallet={wallet} token={token} />
-                        )}
-                        {token.info.mediaUrl ? (
-                            isVideo ? (
-                                <Video
-                                    src={value ?? token.info.mediaUrl}
-                                    VideoProps={{ className: classes.video, autoPlay: true, loop: true }}
-                                />
-                            ) : isHtml ? (
-                                <iframe src={token.info.mediaUrl} />
-                            ) : (
-                                <Image
-                                    component="img"
-                                    width={172}
-                                    height={172}
-                                    style={{ objectFit: 'cover' }}
-                                    src={value ?? token.info.mediaUrl}
-                                />
-                            )
-                        ) : (
-                            <MaskSharpIconOfSize classes={{ root: classes.placeholderIcon }} size={22} />
-                        )}
-                    </Card>
-                </Link>
-            )}
-        </>
+        <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href={resolveCollectibleLink(token.contractDetailed.chainId, provider, token)}>
+            <Card className={classes.root}>
+                {readonly || !wallet ? null : (
+                    <ActionsBarNFT classes={{ more: classes.icon }} wallet={wallet} token={token} />
+                )}
+                {token.info.mediaUrl ? (
+                    <NFTCardStyledAssetPlayer
+                        contractAddress={token.contractDetailed.address}
+                        chainId={token.contractDetailed.chainId}
+                        url={token.info.mediaUrl}
+                        tokenId={token.tokenId}
+                        classes={{
+                            loadingFailImage: classes.loadingFailImage,
+                            wrapper: classes.wrapper,
+                        }}
+                    />
+                ) : (
+                    <MaskSharpIconOfSize classes={{ root: classes.placeholderIcon }} size={22} />
+                )}
+            </Card>
+        </Link>
     )
 }
