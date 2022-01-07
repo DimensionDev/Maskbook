2e91df70fec109b462c6366f388ba0d0f561c708
etouyang <etouyang@gmail.com>
2022-01-07 17:26:31 +0800

    fix: add default icon for image

--- packages/mask/src/plugins/Pets/SNSAdaptor/ImgLoader.tsx
+++ packages/mask/src/plugins/Pets/SNSAdaptor/ImgLoader.tsx
@@ -3,12 +3,16 @@ import { ImgHTMLAttributes, useState } from 'react'
 
 export function ImgLoader(props: ImgHTMLAttributes<HTMLImageElement>) {
     const [loaded, setLoaded] = useState(false)
-
+    const defaultIcon = new URL('../assets/defaultIcon.png', import.meta.url).toString()
+    const onErrorHandle = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        e.currentTarget.src = defaultIcon
+    }
     return (
         <>
             <img
                 {...props}
                 onLoad={() => setLoaded(Boolean(props.src))}
+                onError={onErrorHandle}
                 style={{ display: loaded || !Boolean(props.src) ? 'block' : 'none' }}
             />
             {!loaded && Boolean(props.src) ? <CircularProgress size={20} /> : null}
