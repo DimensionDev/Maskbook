08f0397cf2ed18c623dd66c4120f24061a3d0556
zhouhanseng <z308114274@gmail.com>
2022-01-10 18:30:11 +0800

    chore: nft lucky drop create

--- packages/mask/src/plugins/Wallet/SNSAdaptor/WalletStatusDialog/contractMethodDescription/index.ts
+++ packages/mask/src/plugins/Wallet/SNSAdaptor/WalletStatusDialog/contractMethodDescription/index.ts
@@ -1,10 +1,12 @@
 import { getRedpacketDescription } from './getRedpacketDescription'
 import { getITO_Description } from './getITO_Description'
+import { getNFTRedpacketDescription } from './getNFTRedpacketDescription'
 import type { ContractMethodInfo, ComputedPayload } from '../type'
 
 export function getContractMethodDescription(contractMethodInfo: ContractMethodInfo, computedPayload: ComputedPayload) {
     return (
         getRedpacketDescription(contractMethodInfo, computedPayload) ??
-        getITO_Description(contractMethodInfo, computedPayload)
+        getITO_Description(contractMethodInfo, computedPayload) ??
+        getNFTRedpacketDescription(contractMethodInfo, computedPayload)
     )
 }
