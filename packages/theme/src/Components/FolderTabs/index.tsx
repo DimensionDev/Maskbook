c2006e85fc0a27d71ec098221f47f6605f96ed6a
unclebill <billbill290@gmail.com>
2022-01-13 13:39:36 +0800

    refactor: use makeStyles

--- packages/theme/src/Components/FolderTabs/index.tsx
+++ packages/theme/src/Components/FolderTabs/index.tsx
@@ -1,92 +1,94 @@
 import { styled } from '@mui/material'
-// import {
-// TabPanelUnstyled,
-// TabPanelUnstyledProps,
-// TabsListUnstyled,
-// TabsUnstyled,
-// TabsUnstyledProps,
-// TabUnstyled,
-// tabUnstyledClasses,
-// } from '@mui/base'
-import { Children, cloneElement, FC, useState, HTMLProps } from 'react'
+import { Children, cloneElement, FC, useState, HTMLProps, ReactElement } from 'react'
+import classnames from 'classnames'
+import { makeStyles } from '../../makeStyles'
 import { MaskColorVar } from '../../constants'
 
-const TabList = styled('div')`
-    display: flex;
-    gap: 8px;
-`
-
-const Tab = styled('button')(({ theme }) => {
-    const isDark = theme.palette.mode === 'dark'
+const useStyles = makeStyles<void, 'selected'>()((theme, _, refs) => {
+    const { palette } = theme
+    const isDark = palette.mode === 'dark'
     const inactiveColor = isDark ? theme.palette.grey['50'] : MaskColorVar.twitterBg
+    const selected = {}
     return {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexGrow: 1,
-        height: 40,
-        borderRadius: '8px 8px 0 0',
-        cursor: 'pointer',
-        backgroundColor: inactiveColor,
-        border: `1px solid ${inactiveColor}`,
-        borderBottomColor: 'transparent',
-        color: theme.palette.text.secondary,
-        position: 'relative',
-        boxSizing: 'border-box',
-        ['&.selected']: {
-            backgroundColor: theme.palette.background.paper,
-            borderColor: MaskColorVar.twitterBorderLine,
-            color: theme.palette.primary.main,
+        folderTabs: {},
+        selected,
+        tabList: {
+            display: 'flex',
+            gap: 8,
         },
-        // cover bottom border of the active tab
-        ['&.selected::after']: {
-            position: 'absolute',
-            content: '""',
-            height: 2,
-            borderLeft: `1px solid ${MaskColorVar.twitterBorderLine}`,
-            borderRight: `1px solid ${MaskColorVar.twitterBorderLine}`,
+        tab: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexGrow: 1,
+            height: 40,
+            borderRadius: '8px 8px 0 0',
+            cursor: 'pointer',
+            backgroundColor: inactiveColor,
+            border: `1px solid ${inactiveColor}`,
+            borderBottomColor: 'transparent',
+            color: theme.palette.text.secondary,
+            position: 'relative',
+            boxSizing: 'border-box',
+            [`&.${refs.selected}`]: {
+                backgroundColor: theme.palette.background.paper,
+                borderColor: MaskColorVar.twitterBorderLine,
+                color: theme.palette.primary.main,
+            },
+            // cover bottom border of the active tab
+            [`&.${refs.selected}::after`]: {
+                position: 'absolute',
+                content: '""',
+                height: 2,
+                borderLeft: `1px solid ${MaskColorVar.twitterBorderLine}`,
+                borderRight: `1px solid ${MaskColorVar.twitterBorderLine}`,
+                backgroundColor: theme.palette.background.paper,
+                bottom: '-2px',
+                left: -1,
+                right: -1,
+            },
+        },
+        tabPanel: {
             backgroundColor: theme.palette.background.paper,
-            bottom: '-2px',
-            left: -1,
-            right: -1,
+        },
+        tabContent: {
+            border: `1px solid ${MaskColorVar.twitterBorderLine}`,
+            padding: 12,
+            borderRadius: '0 0 8px 8px',
         },
     }
 })
-
-const PanelContent = styled('div')`
-    border: 1px solid ${MaskColorVar.twitterBorderLine};
-    padding: 12px;
-    border-radius: 0 0 8px 8px;
-`
 
 interface TabPanelProps extends HTMLProps<HTMLDivElement> {
     label: string
     value?: number | string
 }
 
-export const FolderTabPanel = styled('div')<TabPanelProps>(({ theme }) => ({
-    backgroundColor: theme.palette.background.paper,
-}))
+export const FolderTabPanel: FC<TabPanelProps> = ({ className, ...rest }) => {
+    const { classes } = useStyles()
+    return <div className={classnames(classes.tabPanel, className)} role="tabpanel" {...rest} />
+}
 
-type TabPanelReactElement = React.ReactElement<TabPanelProps, FC<TabPanelProps>>
+type TabPanelReactElement = ReactElement<TabPanelProps, FC<TabPanelProps>>
 
 interface FolderTabsProps extends HTMLProps<HTMLDivElement> {}
 
 export const FolderTabs: FC<FolderTabsProps> = ({ children: childNodes, defaultValue = 0, ...rest }) => {
+    const { classes } = useStyles()
     const [value, setValue] = useState(defaultValue)
     const tabs = Children.map(childNodes as TabPanelReactElement[], (child, index) => {
         const label = child.props.label
         const childValue = child.props.value ?? index
         const selected = value === childValue
         return (
-            <Tab
+            <button
                 key={label}
                 tabIndex={index === 0 ? 0 : -1}
                 role="tab"
-                className={selected ? 'selected' : ''}
+                className={classnames(classes.tab, selected ? classes.selected : null)}
                 onClick={() => setValue(childValue)}>
                 {label}
-            </Tab>
+            </button>
         )
     })
     const children = Children.map(childNodes as TabPanelReactElement[], (child, index) => {
@@ -97,8 +99,10 @@ export const FolderTabs: FC<FolderTabsProps> = ({ children: childNodes, defaultV
     if (!tabs.length) return null
     return (
         <div>
-            <TabList>{tabs}</TabList>
-            <PanelContent>{children}</PanelContent>
+            <div className={classes.tabList} role="tablist">
+                {tabs}
+            </div>
+            <div className={classes.tabContent}>{children}</div>
         </div>
     )
 }
