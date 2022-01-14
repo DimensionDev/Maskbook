24cfd30d316a7338a1bfee6559c8aa308eea58b7
UncleBill <billbill290@gmail.com>
2022-01-14 11:26:39 +0800

    feat: integrate solana chain (#4777)
    * wip: solana wallet
    
    * fixup! wip: solana wallet
    
    * fixup! wip: solana wallet
    
    * fix: remove hooks
    
    * fix: add useConnection
    
    * wip: upgrade wallet-adapter
    
    * fixup! wip: upgrade wallet-adapter
    
    * wip
    
    * fix: [wip]list assets in dashboard
    
    * fix: assets in dashboard
    
    * fix: revert unnecessary changes
    
    * fixup! fix: revert unnecessary changes
    
    * fix: resolve token details
    
    * fix: list nft on dashboard
    
    * fix: walkaround react-router@6.1
    
    * fix: network endpoint
    
    * fix: some issues
    
    * fix: missing logo of token
    
    * fix: color

--- packages/web3-constants/compile-constants.ts
+++ packages/web3-constants/compile-constants.ts
@@ -57,6 +57,6 @@ compileConstants(path.join(__dirname, 'evm'), [
     'Fantom',
 ])
 
-compileConstants(path.join(__dirname, 'solana'), ['mainnet-beta', 'testnet', 'devnet'])
+compileConstants(path.join(__dirname, 'solana'), ['Mainnet', 'Testnet', 'Devnet'])
 
 compileConstants(path.join(__dirname, 'flow'), ['Mainnet', 'Testnet'])
