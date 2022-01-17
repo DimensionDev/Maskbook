d28d0b1a7dcf77d8268574ca7c7d3a38042b176a
Septs <github@septs.pw>
2022-01-17 17:06:53 +0800

    fix: html entity

--- packages/mask/src/plugins/Trader/SNSAdaptor/trader/InputTokenPanel.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trader/InputTokenPanel.tsx
@@ -180,7 +180,7 @@ export const InputTokenPanel = memo<InputTokenPanelProps>(
                     ),
                     endAdornment: (
                         <Typography className={classes.price}>
-                            &ap; <FormattedCurrency value={tokenValueUSD} sign="$" formatter={formatCurrency} />
+                            &#x2248; <FormattedCurrency value={tokenValueUSD} sign="$" formatter={formatCurrency} />
                         </Typography>
                     ),
                 }}
