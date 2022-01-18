d5a24028bec154e4f534e61f3a1aa373de58f3fb
nuanyang233 <528944303@qq.com>
2022-01-18 13:19:02 +0800

    feat(wip): define native api type

--- packages/public-api/src/mobile.ts
+++ packages/public-api/src/mobile.ts
@@ -1,4 +1,4 @@
-import type { JsonRpcPayload, JsonRpcResponse } from './types'
+import type { JsonRpcPayload, JsonRpcResponse, PersonaRecord, PageOption, ProfileRecord } from './types'
 
 /**
  * APIs that both Android and iOS implements and have the same API signature
@@ -11,6 +11,45 @@ export interface SharedNativeAPIs {
     wallet_switchBlockChain(payload: { coinId?: number; networkId: number }): Promise<void>
     misc_openCreateWalletView(): Promise<void>
     misc_openDashboardView(): Promise<void>
+    /**
+     * DB JSON RPC
+     */
+    query_personas(params: {
+        identifier?: string
+        hasPrivateKey?: boolean
+        includeLogout?: boolean
+        nameContains?: string
+        pageOption?: PageOption
+    }): Promise<PersonaRecord>
+    update_persona(params: {
+        persona: PersonaRecord
+        options: {
+            linkedProfileMergePolicy?: 0 | 1
+            deleteUndefinedFields?: boolean
+            protectPrivateKey?: boolean
+            createWhenNotExist?: boolean
+        }
+    }): Promise<void>
+    delete_persona(params: { identifier: string; options: { safeDelete?: boolean } }): Promise<void>
+    query_profiles(params: {
+        identifier?: string
+        network?: string
+        nameContains?: string
+        pageOption: PageOption
+    }): Promise<ProfileRecord>
+    update_profile(params: {
+        profile: ProfileRecord
+        options: {
+            createWhenNotExist?: boolean
+        }
+    }): Promise<void>
+    delete_profile(params: { identifier: string }): Promise<void>
+    attachProfile(params: {
+        profileIdentifier: string
+        personaIdentifier: string
+        state: string // ?
+    }): Promise<void>
+    deattachProfile(params: { identifier: string }): Promise<void>
 }
 /**
  * APIs that only implemented by iOS Mask Network
--- packages/public-api/src/types/index.ts
+++ packages/public-api/src/types/index.ts
@@ -11,3 +11,34 @@ export interface JsonRpcResponse {
     result?: any
     error?: string
 }
+
+export interface PersonaRecord {
+    identifier: string
+    mnemonic?: {
+        words: string
+        parameter: { path: string; withPassword: string }
+    }
+    publicKey: string
+    privateKey?: string
+    localKey?: string // ?
+    nickname?: string
+    linkedProfiles: Map<string, { connectionConfirmState: 'confirmed' | 'pending' | 'denied' }> // ?
+    createAt: string
+    updateAt: string
+    hasLogout?: boolean
+    uninitialized?: boolean
+}
+
+export interface ProfileRecord {
+    identifier: string
+    nickname?: string
+    localKey?: string // ?
+    linkedPersona?: string
+    createAt?: string
+    updateAt?: string
+}
+
+export type PageOption = {
+    pageSize: number
+    pageOffset: number
+}
