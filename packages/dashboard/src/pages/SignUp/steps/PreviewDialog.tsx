9a930e99bf46dbf50d16b480b643ca8fb4435227
Hom Yan <yanzhihong23@qq.com>
2022-01-19 11:45:31 +0800

    fix: QR value

--- packages/dashboard/src/pages/SignUp/steps/PreviewDialog.tsx
+++ packages/dashboard/src/pages/SignUp/steps/PreviewDialog.tsx
@@ -137,7 +137,7 @@ const ComponentToPrint = forwardRef((props: PreviewDialogProps, ref: ForwardedRe
                             </Typography>
                         </Box>
                     </Box>
-                    <QRCode value={`MASK:[${privateKey}]`} ecLevel="L" size={120} quietZone={6} qrStyle="dots" />
+                    <QRCode value={`mask://${privateKey}`} ecLevel="L" size={120} quietZone={6} qrStyle="dots" />
                 </Box>
                 <Typography margin="24px 0" fontWeight={600}>
                     {t.create_account_identity_id()}
