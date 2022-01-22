b52c7c7429b4c5eca13d20911c18f26b41d05ec0
sebastianLF <slf.pro@gmail.com>
2022-01-22 13:47:07 -0800

    chore(artblocks): source logo locally

--- packages/mask/src/plugins/ArtBlocks/SNSAdaptor/ArtBlocksIcon.tsx
+++ packages/mask/src/plugins/ArtBlocks/SNSAdaptor/ArtBlocksIcon.tsx
@@ -1,3 +1,3 @@
 export default function ArtBlocksIcon() {
-    return <img src="https://www.artblocks.io/_next/image?url=%2Fsquig.png&w=48&q=75" alt="artblocks logo" />
+    return <img src="../icon/artblocks.png" alt="artblocks logo" />
 }
