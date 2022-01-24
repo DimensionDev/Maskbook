b5d771512ef0f9efced241e064759a78d4fe7457
lelenei <zwpsky@gmail.com>
2022-01-24 15:56:40 +0800

    fix: kv storage increase network identification

--- packages/mask/src/plugins/Avatar/hooks/useNFTAvatar.ts
+++ packages/mask/src/plugins/Avatar/hooks/useNFTAvatar.ts
@@ -1,11 +1,12 @@
 import { useAsync } from 'react-use'
 import type { AsyncState } from 'react-use/lib/useAsyncFn'
+import { activatedSocialNetworkUI } from '../../../social-network'
 import { PluginNFTAvatarRPC } from '../messages'
 import type { AvatarMetaDB } from '../types'
 
 export function useNFTAvatar(userId: string): AsyncState<AvatarMetaDB | undefined> {
     return useAsync(async () => {
         if (!userId || userId === '$unknown') return
-        return PluginNFTAvatarRPC.getNFTAvatar(userId)
-    }, [userId])
+        return PluginNFTAvatarRPC.getNFTAvatar(userId, activatedSocialNetworkUI.networkIdentifier)
+    }, [userId, activatedSocialNetworkUI.networkIdentifier])
 }
