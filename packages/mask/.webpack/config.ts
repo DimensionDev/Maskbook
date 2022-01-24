e9343411762bd28a19af032e281d2c26aca941ae
Jack Works <jackworks@protonmail.com>
2022-01-24 17:14:03 +0800

    fix: more crashes under trusted types

--- packages/mask/.webpack/config.ts
+++ packages/mask/.webpack/config.ts
@@ -328,7 +328,7 @@ function addHTMLEntry(options: HTMLPlugin.Options & { secp256k1?: boolean; sourc
     if (options.sourceMap) {
         templateContent = templateContent.replace(
             `<!-- CSP -->`,
-            `<meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-eval'; require-trusted-types-for 'script'; trusted-types webpack">`,
+            `<meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-eval'; require-trusted-types-for 'script'; trusted-types default webpack">`,
         )
     }
     return new HTMLPlugin({
