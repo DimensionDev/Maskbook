a9bc43a36d43d10185b67c997d6d63315bcb406d
lelenei <72531217+lelenei@users.noreply.github.com>
2022-01-24 16:24:18 +0800

    fix: kv storage increase network identification (#5512)

--- packages/mask/src/plugins/Avatar/hooks/useUserOwnerAddress.ts
+++ packages/mask/src/plugins/Avatar/hooks/useUserOwnerAddress.ts
@@ -1,5 +1,6 @@
 import { useAsync } from 'react-use'
 import type { AsyncState } from 'react-use/lib/useAsyncFn'
+import { activatedSocialNetworkUI } from '../../../social-network'
 import { PluginNFTAvatarRPC } from '../messages'
 
 const cache = new Map<string, Promise<string | undefined>>()
@@ -8,8 +9,8 @@ export function useUserOwnerAddress(userId: string): AsyncState<string | undefin
     return useAsync(async () => {
         if (!userId) return
         if (!cache.has(userId)) {
-            cache.set(userId, PluginNFTAvatarRPC.getAddress(userId))
+            cache.set(userId, PluginNFTAvatarRPC.getAddress(userId, activatedSocialNetworkUI.networkIdentifier))
         }
         return cache.get(userId)
-    }, [userId])
+    }, [userId, activatedSocialNetworkUI.networkIdentifier])
 }
