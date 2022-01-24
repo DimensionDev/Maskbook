ad7188718fdd025f14c0a331c0f3ba43577663d5
Jack Works <jackworks@protonmail.com>
2022-01-24 20:26:52 +0800

    docs: improve gun-utils comment

--- packages/gun-utils/src/utils.ts
+++ packages/gun-utils/src/utils.ts
@@ -6,7 +6,11 @@ function getGunNodeFromPath(path: string[]) {
     return resultNode
 }
 /**
- * A sugar for gun.get(path[0]).get(path[1])....get(path[n]).once()
+ * Get data from Gun. Equivalent as the following code:
+ *
+ * ```ts
+ * gun.get(path[0]).get(path[1])....get(path[n]).once()
+ * ```
  */
 export function getGunData(...path: string[]) {
     return new Promise<{ [x: string]: unknown } | undefined>((resolve) => {
@@ -14,17 +18,48 @@ export function getGunData(...path: string[]) {
     })
 }
 
+/**
+ * Set data on Gun. Equivalent as the following code:
+ *
+ * ```ts
+ * gun.get(path[0]).get(path[1])....get(path[n]).put(data)
+ * ```
+ * @param path graph path on Gun
+ * @param data data to be stored
+ */
 export function setGunData(path: string[], data: any) {
     getGunNodeFromPath(path).put(data)
 }
+
+/**
+ * Delete data on Gun. Equivalent as the following code:
+ *
+ * ```ts
+ * gun.get(path[0]).get(path[1])....get(path[n]).put(null!)
+ * ```
+ * @param path graph path on Gun
+ */
 export function deleteGunData(path: string[]) {
     getGunNodeFromPath(path).put(null!)
 }
 
+/**
+ * Push data to the Gun data Set (Mathematical Set)
+ * @param path graph path on Gun
+ * @param value the object to be stored
+ */
 export function pushToGunDataArray(path: string[], value: object) {
     getGunNodeFromPath(path).set(value)
 }
 
+/**
+ * Subscribe future data on Gun.
+ * When subscribing a Gun data Set (Mathematical Set), you will not get the immediate result back.
+ *
+ * @param path graph path on Gun
+ * @param isT is the data type T
+ * @param abortSignal the signal to stop subscribing
+ */
 export async function* subscribeGunMapData<T>(path: string[], isT: (x: unknown) => x is T, abortSignal: AbortSignal) {
     yield* new EventIterator<T>((queue) => {
         // gun.off() will remove ALL listener on it
