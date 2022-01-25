538a1f9b88ddb9a801ad3c672387aa343df10114
zhouhanseng <z308114274@gmail.com>
2022-01-25 11:18:31 +0800

    chore: reply code review

--- packages/mask/src/plugins/ITO/SNSAdaptor/hooks/useClaimAll.ts
+++ packages/mask/src/plugins/ITO/SNSAdaptor/hooks/useClaimAll.ts
@@ -1,8 +1,7 @@
 import { useAsyncRetry } from 'react-use'
 import { PluginITO_RPC } from '../../messages'
-import { useBlockNumberOfChain } from './useBlockNumberOfChain'
 import type { SwappedTokenType } from '../../types'
-import type { ChainId } from '@masknet/web3-shared-evm'
+import { ChainId, useBlockNumber } from '@masknet/web3-shared-evm'
 import { useRef, useEffect } from 'react'
 
 export function useClaimAll(swapperAddress: string, chainId: ChainId) {
@@ -12,7 +11,7 @@ export function useClaimAll(swapperAddress: string, chainId: ChainId) {
         allPoolsRef.current = []
     }, [chainId])
 
-    const { value: blockNumber = 0 } = useBlockNumberOfChain(chainId)
+    const { value: blockNumber = 0 } = useBlockNumber(chainId)
     const asyncResult = useAsyncRetry(async () => {
         if (allPoolsRef.current.length > 0) return allPoolsRef.current
         const results = await PluginITO_RPC.getClaimAllPools(chainId, blockNumber, swapperAddress)
