ce16957031519b1091ffe7f05ca31404ecb037f7
Jack Works <jackworks@protonmail.com>
2022-01-26 16:16:48 +0800

    fix: type error

--- packages/typed-message/dom/Renderer/MetadataRender.tsx
+++ packages/typed-message/dom/Renderer/MetadataRender.tsx
@@ -3,11 +3,11 @@ import type { MessageRenderProps } from './Entry'
 
 /** @internal */
 export function withMetadata(props: MessageRenderProps<TypedMessage>, jsx: React.ReactNode) {
-    const FooterMetadada = props.footerMetadataRenderer || DefaultMetadataRender
+    const FooterMetadata = props.footerMetadataRenderer || DefaultMetadataRender
     return (
         <>
             {jsx}
-            <FooterMetadada metadata={props.message.meta} message={props.message} />
+            <FooterMetadata metadata={props.message.meta} message={props.message} />
         </>
     )
 }
