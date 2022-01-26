820177712a079751e7281516754836ec839ee8ac
Jack Works <jackworks@protonmail.com>
2022-01-26 20:34:13 +0800

    fix: inject scripts failed in prod

--- packages/mask/.webpack/config.ts
+++ packages/mask/.webpack/config.ts
@@ -336,6 +336,7 @@ function addHTMLEntry(options: HTMLPlugin.Options & { secp256k1?: boolean; sourc
         templateContent,
         inject: 'body',
         scriptLoading: 'defer',
+        minify: false,
         ...options,
     })
 }
