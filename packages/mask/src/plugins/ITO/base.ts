18fcc27077ffa359d059de54a3b3ee4696f1ab32
codingsh <codingsh@pm.me>
2022-01-29 02:56:35 +0100

    feat(Metis): enable Metis on ITO base

--- packages/mask/src/plugins/ITO/base.ts
+++ packages/mask/src/plugins/ITO/base.ts
@@ -25,6 +25,7 @@ export const base: Plugin.Shared.Definition = {
                     ChainId.Aurora,
                     ChainId.Avalanche,
                     ChainId.Fantom,
+                    ChainId.Metis,
                 ],
             },
         },
