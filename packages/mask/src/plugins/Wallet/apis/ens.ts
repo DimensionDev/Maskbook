207559d182b6cc522021869fea8f58094052e879
unclebill <billbill290@gmail.com>
2022-01-29 15:07:27 +0800

    fix: improve web3 tab loading
    set timeout for fetchFromENSTextResolverSubgraph
    remove useless uns

--- packages/mask/src/plugins/Wallet/apis/ens.ts
+++ packages/mask/src/plugins/Wallet/apis/ens.ts
@@ -1,15 +1,20 @@
 import stringify from 'json-stable-stringify'
 
+const subgraphURL = 'https://api.thegraph.com/subgraphs/name/dimensiondev/ens-text-resolver-subgraph'
+const TIMEOUT = 3000
 async function fetchFromENSTextResolverSubgraph<T>(query: string) {
-    const subgraphURL = 'https://api.thegraph.com/subgraphs/name/dimensiondev/ens-text-resolver-subgraph'
+    const controller = new AbortController()
+    const timer = setTimeout(() => controller.abort(), TIMEOUT)
     const response = await fetch(subgraphURL, {
         method: 'POST',
         mode: 'cors',
         body: stringify({ query }),
+        signal: controller.signal,
     })
     const { data } = (await response.json()) as {
         data: T
     }
+    clearTimeout(timer)
     return data
 }
 
