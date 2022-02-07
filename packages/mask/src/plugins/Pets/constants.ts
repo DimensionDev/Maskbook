496b62c7f81b32fdaeeb100e002d947c3e048d68
etouyang <etouyang@gmail.com>
2022-02-07 17:05:07 +0800

    fix: change word notice

--- packages/mask/src/plugins/Pets/constants.ts
+++ packages/mask/src/plugins/Pets/constants.ts
@@ -6,7 +6,8 @@ export const PetsPluginID = 'com.maskbook.pets'
 export const TWITTER = 'twitter.com'
 export const MASK_TWITTER = 'realMaskNetwork'
 export const NFTS_CONFIG_ADDRESS = '0x500cF2ea6755ea35eAC8727A70D997ecc3d26258'
-export const DEFAULT_SET_WORD = 'Click the Mask icon to the left and set up your Non-Fungible Friend in "NFTs" tab!'
+export const DEFAULT_SET_WORD =
+    'Click the wallet icon on the left side of the page and set up your Non-Fungible Friend in "NFTs" tab!'
 export const DEFAULT_PUNK_MASK_WORD =
     "I'm CryptoPunk #6128... in 3D & with a body! Voyagers, welcome to the uncharted waters of WEB3!"
 
