9b1642ce9fd71eda3c6a8f4838b5a238c22c0ea7
zhouhanseng <z308114274@gmail.com>
2022-02-08 18:56:28 +0800

    chore: cspell

--- packages/plugin-infra/src/hooks/useDaoTabTwitterIdList.ts
+++ packages/plugin-infra/src/hooks/useDaoTabTwitterIdList.ts
@@ -2,24 +2,22 @@ import { useAsync } from 'react-use'
 
 const WEB3_DAO_TAB_TWITTER_ID_LIST_API = 'https://configuration.r2d2.to/twitter-supported-id-list.json'
 
+// cspell:disable
 export const DEFAULT_SUPPORTED_TWITTER_IDS = [
     'ConstitutionDAO',
     'juiceboxETH',
-    /* cspell:disable-next-line */
     'AssangeDAO',
     'OfficialMoonDAO',
     'TheSpiceDAO',
-    /* cspell:disable-next-line */
     'sharkdao',
     'CrayonFinance',
     'merge_dao',
-    /* cspell:disable-next-line */
     'DAOTaiFung',
     'Tile_DAO',
-    /* cspell:disable-next-line */
     'mountaindao',
     'OfficialMoonDAO',
 ]
+// cspell:enable
 
 export function useDaoTabTwitterIdList() {
     return useAsync(async () => {
