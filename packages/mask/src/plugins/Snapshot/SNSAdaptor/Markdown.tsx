5c804c896251ec4b089940449641e971a1a14ffc
zhouhanseng <z308114274@gmail.com>
2022-02-11 10:43:58 +0800

    chore: font size

--- packages/mask/src/plugins/Snapshot/SNSAdaptor/Markdown.tsx
+++ packages/mask/src/plugins/Snapshot/SNSAdaptor/Markdown.tsx
@@ -8,12 +8,13 @@ const useStyles = makeStyles()((theme) => ({
         fontFamily: 'sans-serif',
         '& p': {
             margin: 0,
+            fontSize: 12,
         },
         '& p + p': {
             marginTop: theme.spacing(0.5),
         },
         '& h1, & h2, & h3, & h4, & h5, & h6': {
-            fontSize: 14,
+            fontSize: 15,
         },
         '& img': {
             maxWidth: '100%',
