f82fe5a5c1c191456c293dcf6b7d18b8e03f59f7
adz <slf.pro@gmail.com>
2022-02-11 06:53:38 +0300

    fix(artblocks): modify config base texts

--- packages/mask/src/plugins/ArtBlocks/base.tsx
+++ packages/mask/src/plugins/ArtBlocks/base.tsx
@@ -2,12 +2,12 @@ import type { Plugin } from '@masknet/plugin-infra'
 import ArtBlocksIcon from './SNSAdaptor/ArtBlocksIcon'
 
 export const base: Plugin.Shared.Definition = {
-    ID: 'artblocks',
+    ID: 'artblocks.io',
     icon: <ArtBlocksIcon />,
-    name: { fallback: 'ArtBlocks Plugin' },
+    name: { fallback: 'ArtBlocks' },
     description: {
         fallback:
-            'Art Blocks is a first of its kind platform focused on genuinely programmable on demand generative content that is stored immutably on the Ethereum Blockchain. You pick a style that you like, pay for the work, and a randomly generated version of the content is created by an algorithm and sent to your Ethereum account. ',
+            'Artblocks allow you to pick a style that you like, pay for the work, and a randomly generated version of the content is created by an algorithm and sent to your Ethereum account.',
     },
     publisher: { name: { fallback: 'SebastianLF' }, link: 'https://github.com/sebastianLF' },
     enableRequirement: {
