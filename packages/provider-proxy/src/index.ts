d0e75c59351be81a6d4a910ec7091626aa9f91f1
Lantt <laanfor@gmail.com>
2022-02-14 16:45:00 +0800

    feat: should use preview image link in nft api

--- packages/provider-proxy/src/index.ts
+++ packages/provider-proxy/src/index.ts
@@ -2,9 +2,11 @@ import type { RPCMethodRegistrationValue } from './types'
 import fungibleTokenProducer from './producers/fungibleTokenAsset'
 import nonFungibleCollectionAsset from './producers/nonFungibleCollectionAsset'
 import nonFungibleCollectibleAsset from './producers/nonFungibleCollectibleAsset'
+import nonFungibleCollectibleAssetV2 from './producers/nonFungibleCollectibleAssetV2'
 
 export const producers: RPCMethodRegistrationValue[] = [
     fungibleTokenProducer,
     nonFungibleCollectibleAsset,
+    nonFungibleCollectibleAssetV2,
     nonFungibleCollectionAsset,
 ]
