19ba80e3fc7289ae9668ef8bc0d21babc4631b9e
unclebill <billbill290@gmail.com>
2022-02-17 13:18:18 +0800

    fix(rss3): new design for donations page

--- packages/plugins/RSS3/src/SNSAdaptor/pages/DonationsPage.tsx
+++ packages/plugins/RSS3/src/SNSAdaptor/pages/DonationsPage.tsx
@@ -1,7 +1,7 @@
 import urlcat from 'urlcat'
 import { makeStyles } from '@masknet/theme'
-import { Box, CircularProgress, Link, Typography } from '@mui/material'
-import type { AddressName } from '@masknet/web3-shared-evm'
+import { Box, CircularProgress, Link, List, ListItem, Typography } from '@mui/material'
+import { AddressName, EMPTY_LIST } from '@masknet/web3-shared-evm'
 import { useI18N } from '../../locales'
 import type { GeneralAssetWithTags } from '../../types'
 import { RSS3_DEFAULT_IMAGE } from '../../constants'
@@ -19,10 +19,30 @@ const getDonationLink = (label: string, donation: GeneralAssetWithTags) => {
 }
 
 const useStyles = makeStyles()((theme) => ({
+    statusBox: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: theme.spacing(6),
+    },
+    list: {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(2, 1fr)',
+        gridGap: theme.spacing(2),
+    },
+    listItem: {
+        overflow: 'auto',
+        padding: 0,
+    },
+    donationCard: {
+        width: '100%',
+        overflow: 'auto',
+    },
     address: {
         color: theme.palette.primary.main,
     },
     link: {
+        width: '100%',
         '&:hover': {
             textDecoration: 'none',
         },
@@ -36,42 +56,44 @@ export interface DonationPageProps {
 export function DonationPage(props: DonationPageProps) {
     const { addressName } = props
     const { classes } = useStyles()
-    const { value: donations = [], loading } = useDonations(addressName?.resolvedAddress ?? '')
+    const { value: donations = EMPTY_LIST, loading } = useDonations(addressName?.resolvedAddress ?? '')
     const t = useI18N()
 
     if (!addressName) return null
 
     if (loading) {
         return (
-            <Box display="flex" alignItems="center" justifyContent="center">
+            <Box className={classes.statusBox}>
                 <CircularProgress />
             </Box>
         )
     }
     if (!donations.length) {
         return (
-            <Box display="flex" alignItems="center" justifyContent="center">
+            <Box className={classes.statusBox}>
                 <Typography color="textPrimary">No data.</Typography>
             </Box>
         )
     }
     return (
-        <section className="grid grid-cols-1 gap-4 py-4">
+        <List className={classes.list}>
             {donations.map((donation) => (
-                <Link
-                    className={classes.link}
-                    href={getDonationLink(addressName.label, donation)}
-                    key={donation.id}
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <DonationCard
-                        imageUrl={donation.info.image_preview_url || RSS3_DEFAULT_IMAGE}
-                        name={donation.info.title || t.inactive_project()}
-                        contribCount={donation.info.total_contribs || 0}
-                        contribDetails={donation.info.token_contribs || []}
-                    />
-                </Link>
+                <ListItem key={donation.id} className={classes.listItem}>
+                    <Link
+                        className={classes.link}
+                        href={getDonationLink(addressName.label, donation)}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <DonationCard
+                            className={classes.donationCard}
+                            imageUrl={donation.info.image_preview_url || RSS3_DEFAULT_IMAGE}
+                            name={donation.info.title || t.inactive_project()}
+                            contribCount={donation.info.total_contribs || 0}
+                            contribDetails={donation.info.token_contribs || []}
+                        />
+                    </Link>
+                </ListItem>
             ))}
-        </section>
+        </List>
     )
 }
