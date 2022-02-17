76d362b5586d3d9bb4a45a84bc3e5d3dd3d77aea
lelenei <zwpsky@gmail.com>
2022-02-17 17:34:21 +0800

    refactor: remove log

--- packages/mask/src/plugins/Avatar/hooks/useNFTContainerAtTwitter.ts
+++ packages/mask/src/plugins/Avatar/hooks/useNFTContainerAtTwitter.ts
@@ -4,7 +4,6 @@ import type { AsyncState } from 'react-use/lib/useAsyncFn'
 
 export function useNFTContainerAtTwitter(screenName: string): AsyncState<{ address: string; token_id: string }> {
     return useAsyncRetry(async () => {
-        console.log(screenName)
         return UserNFTContainerAtTwitter.getUserNftContainer(screenName)
     }, [UserNFTContainerAtTwitter, screenName])
 }
