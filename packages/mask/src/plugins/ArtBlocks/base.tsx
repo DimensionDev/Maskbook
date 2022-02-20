ec915f7b1e99bb5888949b08c1a5a42bd0bea91b
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2022-02-20 14:20:14 +0800

    refactor: update the publisher

--- packages/mask/src/plugins/ArtBlocks/base.tsx
+++ packages/mask/src/plugins/ArtBlocks/base.tsx
@@ -10,7 +10,7 @@ export const base: Plugin.Shared.Definition = {
         fallback:
             'Artblocks allow you to pick a style that you like, pay for the work, and a randomly generated version of the content is created by an algorithm and sent to your Ethereum account.',
     },
-    publisher: { name: { fallback: 'SebastianLF' }, link: 'https://github.com/sebastianLF' },
+    publisher: { name: { fallback: 'Art Blocks' }, link: 'https://www.artblocks.io/' },
     enableRequirement: {
         architecture: { app: false, web: true },
         networks: { type: 'opt-out', networks: {} },
