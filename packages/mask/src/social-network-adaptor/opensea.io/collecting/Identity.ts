f8d7cebc848319f5fcdfe3079b14b3ab1a79835c
billy <Billy@mask.io>
2022-02-21 18:33:20 +0800

    feat: social-network-adaptor add  opensea.io

---
+++ packages/mask/src/social-network-adaptor/opensea.io/collecting/Identity.ts
@@ -0,0 +1,36 @@
+import { SocialNetworkUI, creator } from '../../../social-network'
+import { ProfileIdentifier } from '@masknet/shared-base'
+import { openseaBase } from '../base'
+import { openDB } from 'idb/with-async-ittr'
+export const IdentityProviderOpensea: SocialNetworkUI.CollectingCapabilities.IdentityResolveProvider = {
+    async start(signal) {
+        const ref = this.recognized
+        update()
+
+        async function update() {
+            if (signal.aborted) return
+            const val = await query()
+            if (val) ref.value = val
+            setTimeout(update, 10 * 1000)
+        }
+    },
+    recognized: creator.EmptyIdentityResolveProviderState(),
+}
+
+async function query(): Promise<null | SocialNetworkUI.CollectingCapabilities.IdentityResolved> {
+    const db = await openDB('redux', 1, {
+        upgrade: () => {
+            db.createObjectStore('paths')
+        },
+    })
+    const tx = db.transaction('paths', 'readonly')
+    const id = await tx.store.get('users.viewerId')
+    if (!id) return null
+    const detail = (await tx.store.get('users.users'))[id]
+    db.close()
+    return {
+        identifier: new ProfileIdentifier(openseaBase.networkIdentifier, detail.username),
+        avatar: detail.profilePictureUrl,
+        nickname: detail.fullName,
+    }
+}
---
+++ packages/mask/src/social-network-adaptor/opensea.io/ui-provider.ts
@@ -0,0 +1,33 @@
+import type { SocialNetworkUI } from '../../social-network'
+import { stateCreator } from '../../social-network/utils'
+import { createTaskStartSetupGuideDefault } from '../../social-network/defaults'
+import { InitAutonomousStateFriends } from '../../social-network/defaults/state/InitFriends'
+import { InitAutonomousStateProfiles } from '../../social-network/defaults/state/InitProfiles'
+import { IdentityProviderOpensea } from './collecting/Identity'
+import { openseaBase } from './base'
+import { openseaShared } from './shared'
+const define: SocialNetworkUI.Definition = {
+    ...openseaShared,
+    ...openseaBase,
+    automation: {},
+    collecting: {
+        identityProvider: IdentityProviderOpensea,
+    },
+    configuration: {
+        setupWizard: {
+            disableSayHello: true,
+        },
+    },
+    customization: {},
+    init(signal) {
+        const friends = stateCreator.friends()
+        const profiles = stateCreator.profiles()
+        InitAutonomousStateFriends(signal, friends, openseaShared.networkIdentifier)
+        InitAutonomousStateProfiles(signal, profiles, openseaShared.networkIdentifier)
+        return { friends, profiles }
+    },
+    injection: {
+        setupWizard: createTaskStartSetupGuideDefault(),
+    },
+}
+export default define
