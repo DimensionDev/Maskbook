4fde4d99dda4c2d944230919fe6d120b305da591
nuanyang233 <528944303@qq.com>
2022-02-25 00:21:38 -0800

    feat: add limit about create wallet and persona (#5701)
    * feat: add limit about create wallet and persona
    
    * chore: reply review
    
    * fix: bugfix
    
    * fix: bugfix

--- packages/shared/src/constants.tsx
+++ packages/shared/src/constants.tsx
@@ -9,3 +9,6 @@ export const SOCIAL_MEDIA_ICON_MAPPING: Record<string, ReactNode> = {
 }
 
 export const mediaViewerUrl = 'https://media-viewer.r2d2.to/index.html'
+
+export const MAX_WALLET_LIMIT = 100
+export const MAX_PERSONA_LIMIT = 10
