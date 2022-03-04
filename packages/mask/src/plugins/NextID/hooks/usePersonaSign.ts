f1def37c6ecc5f76b0c3435340a8379f718921d5
Lantt <laanfor@gmail.com>
2022-03-04 18:42:28 +0800

    fix: should show address selector when nft list is empty (#5782)
    * fix: should show address selector when nft list is empty
    
    * fix: collection icon size fix
    
    * chore: rename build steps
    
    * feat: wording

--- packages/mask/src/plugins/NextID/hooks/usePersonaSign.ts
+++ packages/mask/src/plugins/NextID/hooks/usePersonaSign.ts
@@ -1,8 +1,6 @@
 import { useAsyncFn } from 'react-use'
 import Services from '../../../extension/service'
 import type { ECKeyIdentifier } from '@masknet/shared-base'
-import { useI18N } from '../locales'
-import { useCustomSnackbar } from '@masknet/theme'
 import type { AsyncFnReturn } from 'react-use/lib/useAsyncFn'
 import type { SignRequestResult } from '../../../extension/background-script/IdentityService'
 
@@ -10,12 +8,9 @@ export function usePersonaSign(
     message?: string,
     currentIdentifier?: ECKeyIdentifier,
 ): AsyncFnReturn<() => Promise<SignRequestResult | undefined>> {
-    const t = useI18N()
-    const { showSnackbar } = useCustomSnackbar()
     return useAsyncFn(async () => {
         if (!message || !currentIdentifier) return
         try {
-            showSnackbar(t.notify_persona_sign(), { processing: true, message: t.notify_persona_sign_confirm() })
             const result = await Services.Identity.signWithPersona({
                 method: 'eth',
                 message: message,
@@ -23,7 +18,6 @@ export function usePersonaSign(
             })
             return result
         } catch {
-            showSnackbar(t.notify_persona_sign(), { variant: 'error', message: t.notify_persona_sign_cancel() })
             return
         }
     }, [message, currentIdentifier])
