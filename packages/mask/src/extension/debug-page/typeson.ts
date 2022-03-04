32cf580dce560ec5b8251bbdfb46ecb4551bde24
Jack Works <jackworks@protonmail.com>
2022-03-04 13:59:31 +0800

    fix: ci

--- packages/mask/src/extension/debug-page/typeson.ts
+++ packages/mask/src/extension/debug-page/typeson.ts
@@ -1,18 +1,13 @@
-import Typeson from 'typeson'
+import { Typeson } from 'typeson'
 
 // @ts-ignore
-import builtins from 'typeson-registry/dist/presets/builtin' // @ts-ignore
-import num from 'typeson-registry/dist/presets/special-numbers' // @ts-ignore
-import blob from 'typeson-registry/dist/types/blob' // @ts-ignore
-import file from 'typeson-registry/dist/types/file' // @ts-ignore
-import fileList from 'typeson-registry/dist/types/filelist' // @ts-ignore
-import imageBitMap from 'typeson-registry/dist/types/imagebitmap' // @ts-ignore
+import { builtin, blob, file, filelist, imagebitmap, specialNumbers } from 'typeson-registry'
 import * as CustomTypes from './typeson-custom-types'
 
 const typeson = new Typeson({})
-typeson.register(builtins)
-typeson.register(num)
-typeson.register([blob, file, fileList, imageBitMap, num])
+typeson.register(builtin)
+typeson.register(specialNumbers)
+typeson.register([blob, file, filelist, imagebitmap])
 typeson.register([CustomTypes])
 
 export default typeson
