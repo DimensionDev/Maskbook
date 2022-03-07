3f2b1153f8526a43e8607e51873e9c577a8930ee
Jack Works <jackworks@protonmail.com>
2022-03-07 16:16:00 +0800

    feat: add implementation space for speedy mode

--- packages/theme/src/ShadowRoot/ShadowRootStyleSheet.ts
+++ packages/theme/src/ShadowRoot/ShadowRootStyleSheet.ts
@@ -1,27 +1,30 @@
 const shadowHeadMap = new WeakMap<ShadowRoot, HTMLHeadElement>()
-/* https://github.com/emotion-js/emotion/blob/main/packages/sheet/src/index.js */
+const constructableStyleSheetEnabled = false
+const constructableStyleSheetOrder = new WeakMap<ShadowRoot, CSSStyleSheet[][]>()
 
 // There are 2 rendering mode of this ShadowRootStyleSheet.
 // 1. If the host supports ConstructableStyleSheet proposal:
 //      It is the fastest but only Chrome supports it.
 // 2. style tags that being synchronize between all ShadowRoot.
+//      See https://github.com/emotion-js/emotion/blob/main/packages/sheet/src/index.js
 //      Note: We cannot use .sheet.insertRule (so called "speedy mode") because in our app,
 //            the host of a ShadowRoot might be detached from the DOM and reattach to another place,
 //            when a <style> tag is disconnected from the DOM, it's CSSStyleSheet object will be dropped.
 export class StyleSheet {
     // Unlucky, emotion will create it's own StyleSheet and use isSpeedy, tags, keys and container for Global components.
-    tags: any = []
+    readonly tags: any = []
     container!: HTMLElement
-
-    private ctr = 0
-    private containers = new Map<ShadowRoot, HTMLDivElement>()
-    // Those global stuffs, see https://github.com/emotion-js/emotion/issues/2675
-    private globalContainers = new Map<ShadowRoot, HTMLDivElement>()
-    private _alreadyInsertedOrderInsensitiveRule = false
+    readonly isSpeedy: boolean
     constructor(public key: string, containerShadow: ShadowRoot) {
+        if (constructableStyleSheetEnabled && 'adoptedStyleSheets' in Document.prototype) {
+            this.implementation = new ConstructableStyleSheet(key, containerShadow)
+            this.isSpeedy = true
+        } else {
+            this.implementation = new SynchronizeStyleSheet(key, containerShadow)
+            this.isSpeedy = false
+        }
         this.addContainer(containerShadow)
-
-        // Otherwise it will dead loop
+        // To avoid dead loop.
         this.globalContainers.delete(containerShadow)
 
         const append = Node.prototype.appendChild
@@ -45,16 +48,65 @@ export class StyleSheet {
         }
     }
     addContainer(container: ShadowRoot) {
+        if (this.globalContainers.has(container)) return
+        this.addGlobalContainer(container)
+        this.implementation.addContainer(container)
+    }
+    hydrate(_nodes: HTMLStyleElement[]) {
+        throw new Error('Does not support SSR.')
+    }
+    insert(rule: string) {
+        if (this.isSpeedy) {
+            const isImportRule = rule.charCodeAt(0) === 64 && rule.charCodeAt(1) === 105
+            if (isImportRule && this._alreadyInsertedOrderInsensitiveRule) {
+                // this would only cause problem in speedy mode
+                // but we don't want enabling speedy to affect the observable behavior
+                // so we report this error at all times
+                console.error(
+                    "You're attempting to insert the following rule:\n" +
+                        rule +
+                        '\n\n`@import` rules must be before all other types of rules in a stylesheet but other rules have already been inserted. Please ensure that `@import` rules are before all other rules.',
+                )
+            }
+
+            this._alreadyInsertedOrderInsensitiveRule = this._alreadyInsertedOrderInsensitiveRule || !isImportRule
+        }
+        this.implementation.insert(rule)
+    }
+    flush() {
+        this.implementation.flush()
+        this._alreadyInsertedOrderInsensitiveRule = false
+    }
+    private implementation: ConstructableStyleSheet | SynchronizeStyleSheet
+    private _alreadyInsertedOrderInsensitiveRule = false
+    private globalContainers = new Map<ShadowRoot, HTMLDivElement>()
+    private addGlobalContainer(container: ShadowRoot) {
+        // setup tags
+        const head = getShadowRootHead(container)
+        const globalContainer = (this.container = document.createElement('div'))
+        globalContainer.dataset.globalKey = this.key
+        head.insertBefore(globalContainer, head.firstChild)
+        this.globalContainers.set(container, globalContainer)
+    }
+}
+class ConstructableStyleSheet {
+    constructor(public key: string, containerShadow: ShadowRoot) {}
+    addContainer(container: ShadowRoot) {}
+    insert(rule: string) {}
+    flush() {}
+}
+class SynchronizeStyleSheet {
+    container!: HTMLElement
+    private ctr = 0
+    private containers = new Map<ShadowRoot, HTMLDivElement>()
+    constructor(public key: string, containerShadow: ShadowRoot) {
+        this.addContainer(containerShadow)
+    }
+    addContainer(container: ShadowRoot) {
         if (this.containers.has(container)) return
 
         // setup tags
         const head = getShadowRootHead(container)
-        {
-            const globalContainer = (this.container = document.createElement('div'))
-            globalContainer.dataset.globalKey = this.key
-            head.insertBefore(globalContainer, head.firstChild)
-            this.globalContainers.set(container, globalContainer)
-        }
         const localContainer = document.createElement('div')
         localContainer.dataset.key = this.key
         head.appendChild(localContainer)
@@ -70,37 +122,9 @@ export class StyleSheet {
         existingStyles.forEach((x) => frag.appendChild(x.cloneNode(true)) as HTMLStyleElement)
         localContainer.appendChild(frag)
     }
-
-    private _insertTag = () => {
-        for (const container of this.containers.values()) {
-            container.appendChild(createStyleElement())
-        }
-    }
-
-    hydrate(_nodes: HTMLStyleElement[]) {
-        throw new Error('Does not support SSR.')
-    }
-
     insert(rule: string) {
         if (this.ctr % 25 === 0) {
             this._insertTag()
-        }
-
-        {
-            const isImportRule = rule.charCodeAt(0) === 64 && rule.charCodeAt(1) === 105
-
-            if (isImportRule && this._alreadyInsertedOrderInsensitiveRule) {
-                // this would only cause problem in speedy mode
-                // but we don't want enabling speedy to affect the observable behavior
-                // so we report this error at all times
-                console.error(
-                    "You're attempting to insert the following rule:\n" +
-                        rule +
-                        '\n\n`@import` rules must be before all other types of rules in a stylesheet but other rules have already been inserted. Please ensure that `@import` rules are before all other rules.',
-                )
-            }
-
-            this._alreadyInsertedOrderInsensitiveRule = this._alreadyInsertedOrderInsensitiveRule || !isImportRule
         }
 
         for (const container of this.containers.values()) {
@@ -109,7 +133,6 @@ export class StyleSheet {
         }
         this.ctr += 1
     }
-
     flush() {
         for (const container of this.containers.values()) {
             for (const tag of container.children) {
@@ -117,7 +140,11 @@ export class StyleSheet {
             }
         }
         this.ctr = 0
-        this._alreadyInsertedOrderInsensitiveRule = false
+    }
+    private _insertTag = () => {
+        for (const container of this.containers.values()) {
+            container.appendChild(createStyleElement())
+        }
     }
 }
 
