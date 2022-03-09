cd6431afa60c430d8fc2c47d468ca2849685cba7
zhouhanseng <z308114274@gmail.com>
2022-03-09 14:15:45 +0800

    Revert "chore: test provider proxy"
    This reverts commit a2e7227a113b5909cd6c1e7373d015b283bb5b5e.

--- packages/provider-proxy/src/producers/nonFungibleCollectibleAssetV2.ts
+++ packages/provider-proxy/src/producers/nonFungibleCollectibleAssetV2.ts
@@ -1,8 +1,8 @@
-import { getOpenSeaNFTList } from '@masknet/web3-providers'
+import { getOpenSeaNFTList, getRaribleNFTList, getNFTScanNFTs, getAlchemyNFTList } from '@masknet/web3-providers'
 import type { ERC721TokenDetailed } from '@masknet/web3-shared-base'
 import type { ProducerArgBase, ProducerKeyFunction, ProducerPushFunction, RPCMethodRegistrationValue } from '../types'
 import { collectAllPageData } from '../helper/request'
-import type { Web3Plugin } from '@masknet/plugin-infra'
+import { Web3Plugin, PluginId } from '@masknet/plugin-infra'
 
 export interface NonFungibleTokenAssetArgs extends ProducerArgBase {
     address: string
@@ -18,21 +18,21 @@ const nonFungibleCollectibleAsset = async (
     const openSeaApiKey = await getKeys('opensea')
 
     // Alchemy api is used for polygon and flow network.
-    // if (network) {
-    //     await collectAllPageData<ERC721TokenDetailed>(
-    //         async (page) => {
-    //             const r = (await getAlchemyNFTList(address, network, page, pageSize)) as {
-    //                 data: ERC721TokenDetailed[]
-    //                 hasNextPage: boolean
-    //             }
-    //             return r
-    //         },
-    //         pageSize,
-    //         push,
-    //     )
-    // }
+    if (network) {
+        await collectAllPageData<ERC721TokenDetailed>(
+            async (page) => {
+                const r = (await getAlchemyNFTList(address, network, page, pageSize)) as {
+                    data: ERC721TokenDetailed[]
+                    hasNextPage: boolean
+                }
+                return r
+            },
+            pageSize,
+            push,
+        )
+    }
 
-    // if (network && network.ID !== `${PluginId.EVM}_ethereum`) return
+    if (network && network.ID !== `${PluginId.EVM}_ethereum`) return
 
     try {
         await collectAllPageData<ERC721TokenDetailed>(
@@ -41,22 +41,25 @@ const nonFungibleCollectibleAsset = async (
             push,
         )
     } finally {
-        // const fromRarible = collectAllPageData<ERC721TokenDetailed>(
-        //     (page, pageInfo) => getRaribleNFTList(openSeaApiKey, address, page, pageSize, pageInfo),
-        //     pageSize,
-        //     push,
-        // )
-        // const formNFTScanERC721 = collectAllPageData<ERC721TokenDetailed>(
-        //     (page) => getNFTScanNFTs(address, 'erc721', page, pageSize),
-        //     pageSize,
-        //     push,
-        // )
-        // const fromNFTScanERC1155 = collectAllPageData<ERC721TokenDetailed>(
-        //     (page) => getNFTScanNFTs(address, 'erc1155', page, pageSize),
-        //     pageSize,
-        //     push,
-        // )
-        // await Promise.allSettled([fromRarible, formNFTScanERC721, fromNFTScanERC1155])
+        const fromRarible = collectAllPageData<ERC721TokenDetailed>(
+            (page, pageInfo) => getRaribleNFTList(openSeaApiKey, address, page, pageSize, pageInfo),
+            pageSize,
+            push,
+        )
+
+        const formNFTScanERC721 = collectAllPageData<ERC721TokenDetailed>(
+            (page) => getNFTScanNFTs(address, 'erc721', page, pageSize),
+            pageSize,
+            push,
+        )
+
+        const fromNFTScanERC1155 = collectAllPageData<ERC721TokenDetailed>(
+            (page) => getNFTScanNFTs(address, 'erc1155', page, pageSize),
+            pageSize,
+            push,
+        )
+
+        await Promise.allSettled([fromRarible, formNFTScanERC721, fromNFTScanERC1155])
     }
 }
 
