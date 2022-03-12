60573ba7cb4b4713a03a7a98a731bc69dd44e2c8
zhouhanseng <z308114274@gmail.com>
2022-03-12 21:48:35 +0800

    chore: remove unused param

--- packages/plugin-infra/src/web3/useNonFungibleAssets.ts
+++ packages/plugin-infra/src/web3/useNonFungibleAssets.ts
@@ -5,17 +5,13 @@ import { isSameAddress } from './utils/isSameAddress'
 import { uniqWith } from 'lodash-unified'
 import { useMemo } from 'react'
 
-export function useNonFungibleAssets(
-    address: string,
-    networkList: Web3Plugin.NetworkDescriptor[],
-    dependReady?: boolean,
-) {
+export function useNonFungibleAssets(address: string, networkList: Web3Plugin.NetworkDescriptor[]) {
     const id = useMemo(
         () => `mask.fetchNonFungibleCollectibleAssetV2_${address}_${Date.now()}`,
         [JSON.stringify(networkList)],
     )
     const message = {
-        id: dependReady === undefined ? id : dependReady ? id : '',
+        id,
         method: 'mask.fetchNonFungibleCollectibleAssetV2',
         params: {
             address,
@@ -23,7 +19,6 @@ export function useNonFungibleAssets(
             networkList,
         },
     }
-
     const result = useSocket<ERC721TokenDetailed>(message)
     const data = uniqWith(
         result.data,
