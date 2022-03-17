63e330ecac01c14b570ebc7ad61b67a870c11954
nuanyang233 <528944303@qq.com>
2022-03-17 17:36:09 +0800

    fix: add format ethereum address

--- packages/plugin-infra/src/web3/ReverseAddress.tsx
+++ packages/plugin-infra/src/web3/ReverseAddress.tsx
@@ -6,13 +6,15 @@ import { useWeb3State } from './useWeb3State'
 interface ReverseAddressProps {
     address: string
     pluginId?: NetworkPluginID
+    domainSize?: number
+    addressSize?: number
 }
 
-export const ReverseAddress = memo<ReverseAddressProps>(({ address, pluginId }) => {
+export const ReverseAddress = memo<ReverseAddressProps>(({ address, pluginId, domainSize, addressSize }) => {
     const { value: domain } = useReverseAddress(address, pluginId)
     const { Utils } = useWeb3State(pluginId)
 
-    if (!domain || !Utils?.formatDomainName) return null
+    if (!domain || !Utils?.formatDomainName) return <>{Utils?.formatAddress?.(address, addressSize)}</>
 
-    return <>{Utils.formatDomainName(domain)}</>
+    return <>{Utils.formatDomainName(domain, domainSize)}</>
 })
