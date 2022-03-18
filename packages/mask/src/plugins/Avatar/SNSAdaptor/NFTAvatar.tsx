45bc536faa56313d9bc3025bd024fbfaeac11f0c
nuanyang233 <528944303@qq.com>
2022-03-18 11:40:36 +0800

    fix: support ens name on ins

--- packages/mask/src/plugins/Avatar/SNSAdaptor/NFTAvatar.tsx
+++ packages/mask/src/plugins/Avatar/SNSAdaptor/NFTAvatar.tsx
@@ -17,6 +17,7 @@ import { EthereumChainBoundary } from '../../../web3/UI/EthereumChainBoundary'
 import { AddNFT } from './AddNFT'
 import { NFTImage } from './NFTImage'
 import { useAccount, useWeb3State } from '@masknet/plugin-infra'
+import { ReverseAddress } from '@masknet/shared'
 
 const useStyles = makeStyles()((theme) => ({
     root: {},
@@ -142,7 +143,7 @@ export function NFTAvatar(props: NFTAvatarProps) {
                     </Typography>
                     {account ? (
                         <Typography variant="body1" color="textPrimary" className={classes.account}>
-                            {t('nft_wallet_label')}: {Utils?.formatAddress?.(account, 4) || account}
+                            {t('nft_wallet_label')}: <ReverseAddress address={account} addressSize={4} />
                             {!hideWallet ? (
                                 <Button
                                     variant="text"
--- packages/shared/src/UI/components/ReverseAddress/index.tsx
+++ packages/shared/src/UI/components/ReverseAddress/index.tsx
@@ -1,7 +1,5 @@
 import { memo } from 'react'
-import type { NetworkPluginID } from '../web3-types'
-import { useReverseAddress } from '../hooks'
-import { useWeb3State } from './useWeb3State'
+import { NetworkPluginID, useReverseAddress, useWeb3State } from '@masknet/plugin-infra'
 
 interface ReverseAddressProps {
     address: string
@@ -14,7 +12,7 @@ export const ReverseAddress = memo<ReverseAddressProps>(({ address, pluginId, do
     const { value: domain } = useReverseAddress(address, pluginId)
     const { Utils } = useWeb3State(pluginId)
 
-    if (!domain || !Utils?.formatDomainName) return <>{Utils?.formatAddress?.(address, addressSize)}</>
+    if (!domain || !Utils?.formatDomainName) return <>{Utils?.formatAddress?.(address, addressSize) ?? address}</>
 
     return <>{Utils.formatDomainName(domain, domainSize)}</>
 })
