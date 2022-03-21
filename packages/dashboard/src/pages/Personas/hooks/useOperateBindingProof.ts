87762ff92b7e16187635f866589eb08d77aece99
billy <Billy@mask.io>
2022-03-22 01:56:45 +0800

    style: enum in proof hook

--- packages/dashboard/src/pages/Personas/hooks/useOperateBindingProof.ts
+++ packages/dashboard/src/pages/Personas/hooks/useOperateBindingProof.ts
@@ -1,4 +1,4 @@
-import type { ECKeyIdentifier } from '@masknet/shared-base'
+import { ECKeyIdentifier, NextIDPlatform } from '@masknet/shared-base'
 import { bindProof, createPersonaPayload } from '@masknet/web3-providers'
 import { useAsyncFn } from 'react-use'
 import { Services, Messages } from '../../../API'
@@ -7,7 +7,7 @@ export function useDeleteBound() {
     return useAsyncFn(async (identifier: ECKeyIdentifier, profile, network, action) => {
         const persona_ = await Services.Identity.queryPersona(identifier)
         const username = profile.userId.toLowerCase()
-        const platform = network.split('.')[0] || 'twitter'
+        const platform = network.split('.')[0] || NextIDPlatform.Twitter
         if (!persona_ || !persona_.publicHexKey) throw new Error('Failed to get person')
         const payload = await createPersonaPayload(persona_.publicHexKey, action, username, platform)
         if (!payload) throw new Error('Failed to create persona payload.')
