594b53161e19e24ea72e73fd8b90a618a66d8631
Lantt <laanfor@gmail.com>
2022-03-22 14:42:31 +0800

    style: interaction design improve

--- packages/plugins/GoPlusSecurity/src/SNSAdaptor/components/Searching.tsx
+++ packages/plugins/GoPlusSecurity/src/SNSAdaptor/components/Searching.tsx
@@ -1,3 +1,10 @@
+import { Stack } from '@mui/material'
+import { LoadingAnimation } from './Loading'
+
 export const Searching = () => {
-    return <div>Searching</div>
+    return (
+        <Stack>
+            <LoadingAnimation sx={{ fontSize: 48 }} />
+        </Stack>
+    )
 }
--- packages/plugins/GoPlusSecurity/src/SNSAdaptor/components/SecurityPanel.tsx
+++ packages/plugins/GoPlusSecurity/src/SNSAdaptor/components/SecurityPanel.tsx
@@ -2,11 +2,12 @@ import { Link, Stack, Typography } from '@mui/material'
 import { useI18N } from '../../locales'
 import { ExternalLink } from 'react-feather'
 import { makeStyles } from '@masknet/theme'
-import { memo, useMemo } from 'react'
+import { memo, useMemo, useState } from 'react'
 import { DefineMapping, SecurityMessageLevel, TokenSecurity } from './Common'
 import { SecurityMessages } from '../rules'
 import { RiskCard } from './RiskCard'
 import { formatEthereumAddress } from '@masknet/web3-shared-evm'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 interface TokenCardProps {
     tokenSecurity: TokenSecurity
@@ -43,6 +44,7 @@ const useStyles = makeStyles()((theme) => ({
 export const SecurityPanel = memo<TokenCardProps>(({ tokenSecurity }) => {
     const { classes } = useStyles()
     const t = useI18N()
+    const [isFold, setFold] = useState(false)
 
     const makeMessageList = SecurityMessages.filter((x) => x.condition(tokenSecurity))
 
@@ -56,9 +58,15 @@ export const SecurityPanel = memo<TokenCardProps>(({ tokenSecurity }) => {
         <Stack spacing={2}>
             <Stack spacing={1}>
                 <Stack direction="row" justifyContent="space-between">
-                    <Typography variant="h6" className={classes.header}>
-                        {t.token_info()}
-                    </Typography>
+                    <Stack display="inline-flex" direction="row" alignItems="center" spacing={0.6}>
+                        <Typography variant="h6" className={classes.header}>
+                            {t.token_info()}
+                        </Typography>
+                        <KeyboardArrowDownIcon
+                            onClick={() => setFold(!isFold)}
+                            sx={{ fontSize: 15, cursor: 'pointer' }}
+                        />
+                    </Stack>
                     <Stack direction="row" alignItems="center" spacing={1}>
                         <Typography component="span" lineHeight="14px" fontSize={12}>
                             {t.more_details()}
@@ -74,61 +82,67 @@ export const SecurityPanel = memo<TokenCardProps>(({ tokenSecurity }) => {
                         </Link>
                     </Stack>
                 </Stack>
-                <Stack>
-                    <Stack direction="row" className={classes.card} spacing={2}>
-                        <Stack
-                            spacing={1.5}
-                            borderRadius={0.5}
-                            sx={{ background: DefineMapping[securityMessageLevel].bgColor }}
-                            width={128}
-                            height={128}
-                            justifyContent="center"
-                            alignItems="center">
-                            {DefineMapping[securityMessageLevel].icon(33)}
-                            <Typography color={DefineMapping[securityMessageLevel].titleColor} fontSize={14}>
-                                {t[DefineMapping[securityMessageLevel].i18nKey]()}
-                            </Typography>
-                        </Stack>
-                        <Stack height={128} justifyContent="space-between" flex={1}>
-                            <Stack direction="row" justifyContent="space-between">
-                                <Typography className={classes.subtitle}>{t.token_info_token_name()}</Typography>
-                                <Typography className={classes.cardValue}>
-                                    {formatEthereumAddress(tokenSecurity.contract, 4)}
+                {!isFold && (
+                    <Stack>
+                        <Stack direction="row" className={classes.card} spacing={2}>
+                            <Stack
+                                spacing={1.5}
+                                borderRadius={0.5}
+                                sx={{ background: DefineMapping[securityMessageLevel].bgColor }}
+                                width={128}
+                                height={128}
+                                justifyContent="center"
+                                alignItems="center">
+                                {DefineMapping[securityMessageLevel].icon(33)}
+                                <Typography color={DefineMapping[securityMessageLevel].titleColor} fontSize={14}>
+                                    {t[DefineMapping[securityMessageLevel].i18nKey]()}
                                 </Typography>
                             </Stack>
-                            <Stack direction="row" justifyContent="space-between">
-                                <Typography className={classes.subtitle}>
-                                    {t.token_info_token_contract_address()}
-                                </Typography>
-                                <Typography className={classes.cardValue}>
-                                    {formatEthereumAddress(tokenSecurity.contract, 4)}
-                                </Typography>
-                            </Stack>
-                            <Stack direction="row" justifyContent="space-between">
-                                <Typography className={classes.subtitle}>{t.token_info_contract_creator()}</Typography>
-                                <Typography className={classes.cardValue}>
-                                    {formatEthereumAddress(tokenSecurity.creator_address ?? '', 4)}
-                                </Typography>
-                            </Stack>
-                            <Stack direction="row" justifyContent="space-between">
-                                <Typography className={classes.subtitle}>{t.token_info_contract_owner()}</Typography>
-                                <Typography className={classes.cardValue}>
-                                    {formatEthereumAddress(tokenSecurity.owner_address ?? '', 4)}
-                                </Typography>
-                            </Stack>
-                            <Stack direction="row" justifyContent="space-between">
-                                <Typography className={classes.subtitle}>{t.token_info_total_supply()}</Typography>
-                                <Typography className={classes.cardValue}>{tokenSecurity.total_supply}</Typography>
+                            <Stack height={128} justifyContent="space-between" flex={1}>
+                                <Stack direction="row" justifyContent="space-between">
+                                    <Typography className={classes.subtitle}>{t.token_info_token_name()}</Typography>
+                                    <Typography className={classes.cardValue}>
+                                        {formatEthereumAddress(tokenSecurity.contract, 4)}
+                                    </Typography>
+                                </Stack>
+                                <Stack direction="row" justifyContent="space-between">
+                                    <Typography className={classes.subtitle}>
+                                        {t.token_info_token_contract_address()}
+                                    </Typography>
+                                    <Typography className={classes.cardValue}>
+                                        {formatEthereumAddress(tokenSecurity.contract, 4)}
+                                    </Typography>
+                                </Stack>
+                                <Stack direction="row" justifyContent="space-between">
+                                    <Typography className={classes.subtitle}>
+                                        {t.token_info_contract_creator()}
+                                    </Typography>
+                                    <Typography className={classes.cardValue}>
+                                        {formatEthereumAddress(tokenSecurity.creator_address ?? '', 4)}
+                                    </Typography>
+                                </Stack>
+                                <Stack direction="row" justifyContent="space-between">
+                                    <Typography className={classes.subtitle}>
+                                        {t.token_info_contract_owner()}
+                                    </Typography>
+                                    <Typography className={classes.cardValue}>
+                                        {formatEthereumAddress(tokenSecurity.owner_address ?? '', 4)}
+                                    </Typography>
+                                </Stack>
+                                <Stack direction="row" justifyContent="space-between">
+                                    <Typography className={classes.subtitle}>{t.token_info_total_supply()}</Typography>
+                                    <Typography className={classes.cardValue}>{tokenSecurity.total_supply}</Typography>
+                                </Stack>
                             </Stack>
                         </Stack>
                     </Stack>
-                </Stack>
+                )}
             </Stack>
             <Stack spacing={2}>
                 <Typography variant="h6" className={classes.header}>
                     {t.security_detection()}
                 </Typography>
-                <Stack spacing={1} maxHeight={240} sx={{ overflowY: 'scroll' }}>
+                <Stack spacing={1} maxHeight={isFold ? 402 : 240} sx={{ overflowY: 'scroll' }}>
                     {makeMessageList.map((x, i) => (
                         <RiskCard info={x} key={i} />
                     ))}
