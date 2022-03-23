143e9ecb31f287915f8febc2110d5a68b2fa3c22
zhouhanseng <z308114274@gmail.com>
2022-03-23 13:05:41 +0800

    chore: remove debug code

--- packages/web3-shared/evm/hooks/useBalance.ts
+++ packages/web3-shared/evm/hooks/useBalance.ts
@@ -12,9 +12,5 @@ export function useBalance(expectedChainId?: ChainId, expectedAccount?: string) 
 
     const web3 = useWeb3({ chainId })
 
-    return useAsyncRetry(async () => {
-        const b = await web3.eth.getBalance(account)
-        console.log({ b })
-        return b
-    }, [web3, account])
+    return useAsyncRetry(async () => web3.eth.getBalance(account), [web3, account])
 }
