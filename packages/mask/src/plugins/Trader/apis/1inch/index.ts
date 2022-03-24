09310fac82cad44a9d9bbbb215b9410eb4e90750
codingsh <codingsh@pm.me>
2022-03-24 09:44:29 +0000

    chore(1inch): hot fix add conflux on compile-constants and rewrite params on swapOnQuote

--- packages/mask/src/plugins/Trader/apis/1inch/index.ts
+++ packages/mask/src/plugins/Trader/apis/1inch/index.ts
@@ -1,21 +1,15 @@
 import BigNumber from 'bignumber.js'
 import { first } from 'lodash-unified'
 import { BIPS_BASE, ONE_INCH_BASE_URL } from '../../constants'
-import type {
-    SwapOneErrorResponse,
-    SwapQuoteOneResponse,
-    SwapQuoteOneRequest,
-} from '../../types'
+import type { SwapOneErrorResponse, SwapQuoteOneResponse, SwapQuoteOneRequest } from '../../types'
 import urlcat from 'urlcat'
 
 import { getNetworkTypeFromChainId, NetworkType } from '@masknet/web3-shared-evm'
 import { fetchJSON } from '@masknet/web3-providers'
 
 export async function swapOneQuote(request: SwapQuoteOneRequest) {
-    const params: Record<string, string | number> = {}
-    Object.entries(request).map(([key, value]) => {
-        params[key] = value
-    })
+    const params: Record<string, string | number> = Object.fromEntries(Object.entries(request))
+
     if (request.slippage) params.slippage = new BigNumber(request.slippage).dividedBy(BIPS_BASE).toFixed()
 
     const netType: NetworkType = getNetworkTypeFromChainId(request.chainId)!
