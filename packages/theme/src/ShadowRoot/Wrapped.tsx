114a7a4e4d52f9e15881301c64febddb6de6435d
Jack Works <jackworks@protonmail.com>
2022-03-24 10:47:49 +0800

    fix: type error

--- packages/theme/src/ShadowRoot/Wrapped.tsx
+++ packages/theme/src/ShadowRoot/Wrapped.tsx
@@ -1,6 +1,6 @@
 import { Menu, Popper, Tooltip } from '@mui/material'
 import { createShadowRootForwardedComponent, createShadowRootForwardedPopperComponent } from './Portal'
 
-export const ShadowRootTooltip: typeof Tooltip = createShadowRootForwardedPopperComponent(Tooltip)
-export const ShadowRootMenu: typeof Menu = createShadowRootForwardedComponent(Menu)
-export const ShadowRootPopper: typeof Popper = createShadowRootForwardedComponent(Popper)
+export const ShadowRootTooltip: typeof Tooltip = createShadowRootForwardedPopperComponent(Tooltip) as any
+export const ShadowRootMenu: typeof Menu = createShadowRootForwardedComponent(Menu) as any
+export const ShadowRootPopper: typeof Popper = createShadowRootForwardedComponent(Popper) as any
