c10ddac0b8f3c2b5000b31716cdc43a66ae3c0f7
lelenei <zwpsky@gmail.com>
2022-03-28 11:42:56 +0800

    feat: check erc1155

--- packages/web3-shared/evm/hooks/useAddressCheck.ts
+++ packages/web3-shared/evm/hooks/useAddressCheck.ts
@@ -1,8 +1,13 @@
 import { useAsync } from 'react-use'
+import type { AbiItem } from 'web3-utils'
 import { useERC721TokenContract } from '../contracts'
+import { createContract } from './useContract'
 import { useWeb3 } from './useWeb3'
+import ERC1155 from '@masknet/web3-contracts/abis/ERC1155.json'
 
 const ERC721_ENUMERABLE_INTERFACE_ID = '0x780e9d63'
+const ERC1155_ENUMERABLE_INTERFACE_ID = '0xd9b67a26'
+                                                                                   '
 function useCheckContract(address: string) {
     const web3 = useWeb3()
     return useAsync(async () => {
@@ -18,22 +23,20 @@ function useCheckERC721(address: string) {
     }, [erc721TokenContract]).value
 }
 
-function useGetSymbol(address: string) {
-    const erc721TokenContract = useERC721TokenContract(address)
-    return useAsync(async () => {
-        return erc721TokenContract?.methods.symbol().call()
-    }, [erc721TokenContract]).value
+function useCheckERC1155(address: string) {
+    const web3 = useWeb3()
+    const erc1155Contract = createContract(web3, address, ERC1155 as AbiItem[])
+    return useAsync(async() => {
+        return erc1155Contract?.methods.supportsInterface(ERC1155_ENUMERABLE_INTERFACE_ID).call()
+    }, [erc1155Contract])
 }
 
 export function useAddressCheck(address: string) {
     const isContract = useCheckContract(address)
     const isERC721 = useCheckERC721(address)
-    const symbol = useGetSymbol(address)
+    const isERC1155 = useCheckERC1155(address)
 
-    if (isContract && isERC721 && symbol === 'ERC1155') {
-        return 'ERC1155'
-    } else if (isContract && isERC721 && symbol !== 'ERC1155') {
-        return 'ERC721'
-    }
+    if (isContract && isERC1155) return 'ERC1155'
+    if (isContract && isERC721) return 'ERC721'
     return 'ERC20'
 }
