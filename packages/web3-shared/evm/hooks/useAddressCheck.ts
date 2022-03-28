5459e7a54ba59219688e2fa792f3d319a6813b11
lelenei <zwpsky@gmail.com>
2022-03-28 13:39:17 +0800

    fix: check erc165

--- packages/web3-shared/evm/hooks/useAddressCheck.ts
+++ packages/web3-shared/evm/hooks/useAddressCheck.ts
@@ -4,6 +4,7 @@ import { useERC721TokenContract } from '../contracts'
 import { createContract } from './useContract'
 import { useWeb3 } from './useWeb3'
 import ERC1155 from '@masknet/web3-contracts/abis/ERC1155.json'
+import { useERC165 } from '.'
 
 const ERC721_ENUMERABLE_INTERFACE_ID = '0x780e9d63'
 const ERC1155_ENUMERABLE_INTERFACE_ID = '0xd9b67a26'
@@ -17,24 +18,21 @@ function useCheckContract(address: string) {
 }
 
 function useCheckERC721(address: string) {
-    const erc721TokenContract = useERC721TokenContract(address)
-    return useAsync(async () => {
-        return erc721TokenContract?.methods.supportsInterface(ERC721_ENUMERABLE_INTERFACE_ID).call()
-    }, [erc721TokenContract]).value
+    const erc721Contract = useERC721TokenContract(address)
+    return useERC165(erc721Contract, address, ERC721_ENUMERABLE_INTERFACE_ID)
 }
 
 function useCheckERC1155(address: string) {
     const web3 = useWeb3()
     const erc1155Contract = createContract(web3, address, ERC1155 as AbiItem[])
-    return useAsync(async () => {
-        return erc1155Contract?.methods.supportsInterface(ERC1155_ENUMERABLE_INTERFACE_ID).call()
-    }, [erc1155Contract])
+    return useERC165(erc1155Contract, address, ERC1155_ENUMERABLE_INTERFACE_ID)
 }
 
 export function useAddressCheck(address: string) {
     const isContract = useCheckContract(address)
-    const isERC721 = useCheckERC721(address)
-    const isERC1155 = useCheckERC1155(address)
+    const { value: isERC721, loading: loadingERC721 } = useCheckERC721(address)
+    const { value: isERC1155, loading: loadingERC1155 } = useCheckERC1155(address)
 
+    if (loadingERC1155 || loadingERC721) return
     return isERC1155 ? 'ERC1155' : isERC721 ? 'ERC721' : isContract ? 'ERC20' : ''
 }
