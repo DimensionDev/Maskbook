0a573ab5eadcf8a8371c06fc75e87be929f7b299
Septs <github@septs.pw>
2022-03-29 09:15:12 +0800

    fix: bool state

--- packages/dashboard/src/components/CreateWalletForm/index.tsx
+++ packages/dashboard/src/components/CreateWalletForm/index.tsx
@@ -1,6 +1,6 @@
 import { FormControl, ListItemIcon, MenuItem, Select, styled, Typography, FilledInput } from '@mui/material'
 import { makeStyles } from '@masknet/theme'
-import { useCallback, useState } from 'react'
+import { useState } from 'react'
 import { useDashboardI18N } from '../../locales'
 
 const useStyles = makeStyles()((theme) => ({
@@ -32,7 +32,7 @@ export interface CreateWalletFormProps {
 export function CreateWalletForm(props: CreateWalletFormProps) {
     const { options } = props
     const { classes } = useStyles()
-    const [selected, setSelected] = useState()
+    const [selected, setSelected] = useState<string | undefined>()
 
     const t = useDashboardI18N()
 
@@ -43,7 +43,7 @@ export function CreateWalletForm(props: CreateWalletFormProps) {
                     classes={{ filled: classes.filled }}
                     variant="filled"
                     value={selected}
-                    onChange={useCallback((event) => setSelected(event.target.value), [])}>
+                    onChange={(event) => setSelected(event.target.value)}>
                     {options.map(({ label, icon, value }) => (
                         <MenuItem value={value} key={label}>
                             <ListItemIcon>{icon}</ListItemIcon>
--- packages/mask/src/components/shared/SelectProfileUI/SelectProfileUI.tsx
+++ packages/mask/src/components/shared/SelectProfileUI/SelectProfileUI.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState } from 'react'
 import { ListItem, ListItemText, InputBase, Button, List, Box, Chip } from '@mui/material'
 import { makeStyles, useStylesExtends } from '@masknet/theme'
 import { useI18N } from '../../../utils'
@@ -98,7 +98,7 @@ export function SelectProfileUI(props: SelectProfileUIProps) {
                 <InputBase
                     className={classes.input}
                     value={disabled ? '' : search}
-                    onChange={useCallback((e) => setSearch(e.target.value), [])}
+                    onChange={(e) => setSearch(e.target.value)}
                     onKeyDown={(e) => {
                         if (search === '' && e.key === 'Backspace') {
                             onSetSelected(selected.slice(0, selected.length - 1) as Profile[])
