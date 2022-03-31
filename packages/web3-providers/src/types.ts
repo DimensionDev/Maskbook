9e3f61a8018bd3e023bd7b0cd5c0ea919c220174
Lantt <laanfor@gmail.com>
2022-03-31 12:35:55 +0800

    feat: create next kv server api (#5994)
    * feat: create next kv server api
    
    * fix: cspell
    
    * fix: cspell
    
    * refactor: expose proof method by class
    
    * feat: add mask plugin key
    
    * refactor: create constants
    
    * fix: rename kv payload to storage payload

--- packages/web3-providers/src/types.ts
+++ packages/web3-providers/src/types.ts
@@ -8,6 +8,8 @@ import type {
     NativeTokenDetailed,
 } from '@masknet/web3-shared-evm'
 import type { CurrencyType } from '@masknet/plugin-infra'
+import type { Result } from 'ts-results'
+import type { NextIDAction, NextIDStoragePayload, NextIDPayload, NextIDPlatform } from '@masknet/shared-base'
 
 export namespace ExplorerAPI {
     export type Transaction = Web3Transaction & {
@@ -305,6 +307,61 @@ export namespace StorageAPI {
     export interface Provider {
         createJSON_Storage?(key: string): Storage
         createBinaryStorage?(key: string): Storage
+    }
+}
+
+export namespace NextIDBaseAPI {
+    export interface Storage {
+        set<T extends {}>(
+            uuid: string,
+            personaPublicKey: string,
+            signature: string,
+            platform: NextIDPlatform,
+            identity: string,
+            createdAt: string,
+            patchData: unknown,
+        ): Promise<Result<void, string>>
+        get<T>(key: string): Promise<Result<T, string>>
+        getPayload(
+            personaPublicKey: string,
+            platform: NextIDPlatform,
+            identity: string,
+            patchData: unknown,
+        ): Promise<Result<NextIDStoragePayload, string>>
+    }
+    export interface Proof {
+        bindProof(
+            uuid: string,
+            personaPublicKey: string,
+            action: NextIDAction,
+            platform: string,
+            identity: string,
+            createdAt: string,
+            options?: {
+                walletSignature?: string
+                signature?: string
+                proofLocation?: string
+            },
+        ): Promise<Result<unknown, string>>
+
+        queryExistedBindingByPersona(personaPublicKey: string, enableCache?: boolean): Promise<any>
+
+        queryExistedBindingByPlatform(platform: NextIDPlatform, identity: string, page?: number): Promise<any>
+
+        queryIsBound(
+            personaPublicKey: string,
+            platform: NextIDPlatform,
+            identity: string,
+            enableCache?: boolean,
+        ): Promise<boolean>
+
+        createPersonaPayload(
+            personaPublicKey: string,
+            action: NextIDAction,
+            identity: string,
+            platform: NextIDPlatform,
+            language?: string,
+        ): Promise<NextIDPayload | null>
     }
 }
 
