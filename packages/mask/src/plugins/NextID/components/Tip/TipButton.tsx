3e6a7e7ab3dd360ac1397157ad2ca8375b1b2a12
Lantt <laanfor@gmail.com>
2022-04-06 12:27:22 +0800

    fix: should update tip status when next id verify status changed (#6020)
    * fix: should update tip status when next id verify status changed
    
    * refactor: code review feedback

--- packages/mask/src/plugins/NextID/components/Tip/TipButton.tsx
+++ packages/mask/src/plugins/NextID/components/Tip/TipButton.tsx
@@ -7,12 +7,13 @@ import { EMPTY_LIST } from '@masknet/web3-shared-evm'
 import type { TooltipProps } from '@mui/material'
 import classnames from 'classnames'
 import { uniq } from 'lodash-unified'
-import { FC, HTMLProps, MouseEventHandler, useCallback, useMemo } from 'react'
+import { FC, HTMLProps, MouseEventHandler, useCallback, useEffect, useMemo } from 'react'
 import { useAsync, useAsyncFn, useAsyncRetry } from 'react-use'
 import Services from '../../../../extension/service'
 import { activatedSocialNetworkUI } from '../../../../social-network'
 import { useI18N } from '../../locales'
 import { PluginNextIdMessages } from '../../messages'
+import { MaskMessages } from '../../../../../shared'
 
 interface Props extends HTMLProps<HTMLDivElement> {
     addresses?: string[]
@@ -72,7 +73,11 @@ export const TipButton: FC<Props> = ({
         return Services.Identity.queryPersonaByProfile(receiver)
     }, [receiver])
 
-    const { value: isAccountVerified, loading: loadingVerifyInfo } = useAsync(() => {
+    const {
+        value: isAccountVerified,
+        loading: loadingVerifyInfo,
+        retry: retryLoadVerifyInfo,
+    } = useAsyncRetry(() => {
         if (!receiverPersona?.publicHexKey || !receiver?.userId) return Promise.resolve(false)
         return NextIDProof.queryIsBound(receiverPersona.publicHexKey, platform, receiver.userId, true)
     }, [receiverPersona?.publicHexKey, platform, receiver?.userId])
@@ -91,6 +96,13 @@ export const TipButton: FC<Props> = ({
     }, [receiver])
 
     useAsync(queryBindings, [queryBindings])
+
+    useEffect(() => {
+        return MaskMessages.events.ownProofChanged.on(() => {
+            retryLoadVerifyInfo()
+            queryBindings()
+        })
+    }, [])
 
     const allAddresses = useMemo(() => {
         return uniq([...(walletsState.value || []), ...addresses])
