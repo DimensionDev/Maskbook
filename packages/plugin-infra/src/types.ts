84acaea46a8d9b233a5b1a1edf988b3a710f8846
Randolph314 <97870249+Randolph314@users.noreply.github.com>
2022-04-06 16:21:46 +0800

    feat: add cross-chain-bridge plugin (#6016)
    * feat: add cross-chain-bridge plugin
    
    * feat: add word ignored
    
    * feat: delete useless code
    
    * feat: add parameter type
    
    * feat: delete useless code and add ID for bridges
    
    Co-authored-by: Randolph <840094513@qq.com>

--- packages/plugin-infra/src/types.ts
+++ packages/plugin-infra/src/types.ts
@@ -689,6 +689,7 @@ export enum PluginId {
     FileService = 'com.maskbook.fileservice',
     CyberConnect = 'me.cyberconnect.app',
     GoPlusSecurity = 'io.gopluslabs.security',
+    CrossChainBridge = 'io.mask.cross-chain-bridge',
     // @masknet/scripts: insert-here
 }
 
