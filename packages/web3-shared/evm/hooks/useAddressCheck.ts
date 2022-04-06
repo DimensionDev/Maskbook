fbf09ff6f27342f05cf590d130e3b39cd212ede2
lelenei <zwpsky@gmail.com>
2022-04-06 11:37:49 +0800

    fix: reply review

--- packages/web3-shared/evm/hooks/useAddressCheck.ts
+++ packages/web3-shared/evm/hooks/useAddressCheck.ts
@@ -5,6 +5,7 @@ import { createContract } from './useContract'
 import { useWeb3 } from './useWeb3'
 import ERC1155 from '@masknet/web3-contracts/abis/ERC1155.json'
 import { useERC165 } from '.'
+import { EthereumTokenType } from '../types'
 
 const ERC721_ENUMERABLE_INTERFACE_ID = '0x780e9d63'
 const ERC1155_ENUMERABLE_INTERFACE_ID = '0xd9b67a26'
@@ -22,22 +23,27 @@ function useCheckERC721(address: string) {
     return useERC165(erc721Contract, address, ERC721_ENUMERABLE_INTERFACE_ID)
 }
 
-function useCreateERC1155TokenContract(address: string) {
+function useERC1155TokenContract(address: string) {
     const web3 = useWeb3()
-    const erc1155Contract = createContract(web3, address, ERC1155 as AbiItem[])
-    return erc1155Contract
+    return createContract(web3, address, ERC1155 as AbiItem[])
 }
 
 function useCheckERC1155(address: string) {
-    const erc1155Contract = useCreateERC1155TokenContract(address)
+    const erc1155Contract = useERC1155TokenContract(address)
     return useERC165(erc1155Contract, address, ERC1155_ENUMERABLE_INTERFACE_ID)
 }
 
-export function useAddressCheck(address: string) {
+export function useAddressCheck(address: string): EthereumTokenType {
     const { value: isContract, loading: loadingContract } = useCheckContract(address)
     const { value: isERC721, loading: loadingERC721 } = useCheckERC721(address)
     const { value: isERC1155, loading: loadingERC1155 } = useCheckERC1155(address)
 
     if (loadingERC1155 || loadingERC721 || loadingContract) return
-    return isERC1155 ? 'ERC1155' : isERC721 ? 'ERC721' : isContract ? 'ERC20' : ''
+    return isERC1155
+        ? EthereumTokenType.ERC1155
+        : isERC721
+        ? EthereumTokenType.ERC721
+        : isContract
+        ? EthereumTokenType.ERC20
+        : EthereumTokenType.Native
 }
