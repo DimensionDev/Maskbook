19213de207dc9664ec2757bcfa2d6458579e2729
Jack Works <jackworks@protonmail.com>
2022-04-10 15:10:11 +0800

    refactor: move typed-message package to esm

--- packages/typed-message/dom/index.ts
+++ packages/typed-message/dom/index.ts
@@ -1,23 +1,23 @@
 // Render
-export { TypedMessageRender, TypedMessageRenderInline, type RenderProps } from './Renderer/Entry'
-export { useMetadataRender, type MetadataRenderProps } from './Renderer/MetadataRender'
+export { TypedMessageRender, TypedMessageRenderInline, type RenderProps } from './Renderer/Entry.js'
+export { useMetadataRender, type MetadataRenderProps } from './Renderer/MetadataRender.js'
 
 // Render behavior
-export { TextResizeContext } from './Renderer/utils/TextResizerContext'
-export { TUPLE_RENDER_STYLE } from './Renderer/Core/Tuple'
+export { TextResizeContext } from './Renderer/utils/TextResizerContext.js'
+export { TUPLE_RENDER_STYLE } from './Renderer/Core/Tuple.js'
 
 // Render Registry
-export { type RenderConfig, createTypedMessageRenderRegistry } from './Renderer/registry'
+export { type RenderConfig, createTypedMessageRenderRegistry } from './Renderer/registry.js'
 export {
     RenderFragmentsContext,
     type RenderFragmentsContextType,
     DefaultRenderFragments,
-} from './Renderer/utils/RenderFragments'
-export { RegistryContext } from './Renderer/utils/RegistryContext'
+} from './Renderer/utils/RenderFragments.js'
+export { RegistryContext } from './Renderer/utils/RegistryContext.js'
 
 // Transformation
 export {
     TransformerProvider,
     useTransformedValue,
     TransformationContextProvider,
-} from './Renderer/utils/TransformContext'
+} from './Renderer/utils/TransformContext.js'
