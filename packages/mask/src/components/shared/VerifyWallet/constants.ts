6ba8c11a8b279154bc34d7501b991f4d16fdafee
billy <Billy@mask.io>
2022-04-12 16:09:27 +0800

    fix: comment

--- packages/mask/src/components/shared/VerifyWallet/constants.ts
+++ packages/mask/src/components/shared/VerifyWallet/constants.ts
@@ -1,17 +1,7 @@
-const step1ActiveIcon = new URL('../assets/stepAssets/step1Active.png', import.meta.url)
-const step2DisableIcon = new URL('../assets/stepAssets/step2Disable.png', import.meta.url)
-const step2ActiveIcon = new URL('../assets/stepAssets/step2Active.png', import.meta.url)
-const stepSuccessIcon = new URL('../assets/stepAssets/stepSuccess.png', import.meta.url)
-const dividerDisableIcon = new URL('../assets/stepAssets/dividerDisable.png', import.meta.url)
-const dividerActiveIcon = new URL('../assets/stepAssets/dividerActive.png', import.meta.url)
-const dividerSuccessIcon = new URL('../assets/stepAssets/dividerDone.png', import.meta.url)
-
-export {
-    step1ActiveIcon,
-    step2DisableIcon,
-    step2ActiveIcon,
-    stepSuccessIcon,
-    dividerDisableIcon,
-    dividerActiveIcon,
-    dividerSuccessIcon,
-}
+export const step1ActiveIcon = new URL('../assets/stepAssets/step1Active.png', import.meta.url)
+export const step2DisableIcon = new URL('../assets/stepAssets/step2Disable.png', import.meta.url)
+export const step2ActiveIcon = new URL('../assets/stepAssets/step2Active.png', import.meta.url)
+export const stepSuccessIcon = new URL('../assets/stepAssets/stepSuccess.png', import.meta.url)
+export const dividerDisableIcon = new URL('../assets/stepAssets/dividerDisable.png', import.meta.url)
+export const dividerActiveIcon = new URL('../assets/stepAssets/dividerActive.png', import.meta.url)
+export const dividerSuccessIcon = new URL('../assets/stepAssets/dividerDone.png', import.meta.url)
