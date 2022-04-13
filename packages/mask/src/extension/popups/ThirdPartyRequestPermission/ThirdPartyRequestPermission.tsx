5a4852d36205dc566d45948f118a4660eb188a87
septs <github@septs.pw>
2022-04-13 16:30:15 +0800

    fix: jsx-key (#6074)
    * fix: jsx-key
    
    * fix: human readable variable name

--- packages/mask/src/extension/popups/ThirdPartyRequestPermission/ThirdPartyRequestPermission.tsx
+++ packages/mask/src/extension/popups/ThirdPartyRequestPermission/ThirdPartyRequestPermission.tsx
@@ -16,8 +16,8 @@ export function ThirdPartyRequestPermission(props: ThirdPartyRequestPermissionPr
                 values={{ pluginName: props.pluginName, pluginURL: props.pluginURL }}
             />
             <ul>
-                {props.permissions.map((x) => (
-                    <li key={x}>{ThirdPartyPluginPermission[x]}</li>
+                {props.permissions.map((permission, key) => (
+                    <li key={key}>{ThirdPartyPluginPermission[permission]}</li>
                 ))}
             </ul>
             <button onClick={() => window.close()}>{t('cancel')}</button>
