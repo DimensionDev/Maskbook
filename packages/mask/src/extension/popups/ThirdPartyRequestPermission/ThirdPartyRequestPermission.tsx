510ef16dbfa2942e9607732f70b4d7d33538748f
Septs <github@septs.pw>
2022-04-13 15:17:09 +0800

    fix: human readable variable name

--- packages/mask/src/extension/popups/ThirdPartyRequestPermission/ThirdPartyRequestPermission.tsx
+++ packages/mask/src/extension/popups/ThirdPartyRequestPermission/ThirdPartyRequestPermission.tsx
@@ -16,8 +16,8 @@ export function ThirdPartyRequestPermission(props: ThirdPartyRequestPermissionPr
                 values={{ pluginName: props.pluginName, pluginURL: props.pluginURL }}
             />
             <ul>
-                {props.permissions.map((x, key) => (
-                    <li key={key}>{ThirdPartyPluginPermission[x]}</li>
+                {props.permissions.map((permission, key) => (
+                    <li key={key}>{ThirdPartyPluginPermission[permission]}</li>
                 ))}
             </ul>
             <button onClick={() => window.close()}>{t('cancel')}</button>
