d417f49cb88cd6a4778bb6dc935ab29d56b623e3
Jack Works <jackworks@protonmail.com>
2022-04-13 16:42:30 +0800

    chore: cleanup

--- packages/mask/.webpack/fix-regenerator-runtime.ts
+++ packages/mask/.webpack/fix-regenerator-runtime.ts
@@ -3,7 +3,7 @@ import type { LoaderContext } from 'webpack'
 const reg = /Gp\[iteratorSymbol\]\s*=\s*function\s*\(\)\s*{\n\s*return this;\n\s*};/
 export default function (this: LoaderContext<{}>, source: string) {
     if (reg.test(source)) {
-        this.emitWarning(new Error(' contains old version of regenerator runtime.'))
+        // this.emitWarning(new Error(' contains old version of regenerator runtime.'))
         return source.replace(reg, 'define(Gp, iteratorSymbol, function () { return this });')
     }
     return source
