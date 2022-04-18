757e5ad18543f5cba721069d6100557d43dc082a
lelenei <zwpsky@gmail.com>
2022-04-18 22:08:52 +0800

    refactor: remove unused code

--- packages/mask/src/plugins/Avatar/constants.ts
+++ packages/mask/src/plugins/Avatar/constants.ts
@@ -4,7 +4,6 @@ export const NFT_AVATAR_JSON_SERVER = 'https://configuration.r2d2.to/com.maskboo
 export const NFT_AVATAR_DB_NAME = 'com.maskbook.user'
 export const NFT_AVATAR_DB_NAME_STORAGE = 'com.maskbook.user.storage'
 
-export const NFT_CONTRACT_JSON_VERIFIED_SERVER = 'https://configuration.r2d2.to/com.maskbook.verified_nft.json'
 export const PLUGIN_ID = PluginId.Avatar
 export const PLUGIN_NAME = 'Avatar'
 export const PLUGIN_DESCRIPTION = 'NFT Avatar'
