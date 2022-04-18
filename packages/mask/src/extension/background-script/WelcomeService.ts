ee9eecb9755886ff96078790149ed1e49efb3ad6
Jack Works <jackworks@protonmail.com>
2022-04-18 20:45:17 +0800

    refactor: move openDashboard

--- packages/mask/src/extension/background-script/WelcomeService.ts
+++ packages/mask/src/extension/background-script/WelcomeService.ts
@@ -1,26 +1,4 @@
-import type { DashboardRoutes } from '@masknet/shared-base'
-
 import { assertEnvironment, Environment } from '@dimensiondev/holoflows-kit'
 import './legacy'
-
-export {
-    mobile_generateBackupJSON,
-    mobile_generateBackupJSONOnlyForPersona,
-    generateBackupPreviewInfo,
-    createBackupFile,
-} from '../../../background/services/backup/create'
-export {
-    addUnconfirmedBackup,
-    getUnconfirmedBackup,
-    restoreUnconfirmedBackup,
-    mobile_restoreFromBase64,
-} from '../../../background/services/backup/restore'
-
+export * from '../../../background/services/backup'
 assertEnvironment(Environment.ManifestBackground)
-
-export async function openOptionsPage(route?: DashboardRoutes, search?: string) {
-    return browser.tabs.create({
-        active: true,
-        url: browser.runtime.getURL(`/dashboard.html#${route}${search ? `?${search}` : ''}`),
-    })
-}
