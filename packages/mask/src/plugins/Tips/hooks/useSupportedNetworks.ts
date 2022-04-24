06c1facc3603e2363c8a8394f76ca325415c8c67
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2022-04-24 17:13:26 +0800

    refactor: typo

--- packages/mask/src/plugins/Tips/hooks/useSupportedNetworks.ts
+++ packages/mask/src/plugins/Tips/hooks/useSupportedNetworks.ts
@@ -3,7 +3,7 @@ import { NetworkPluginID } from '@masknet/plugin-infra/web3'
 export function useSupportedNetworks(keys: NetworkPluginID[]) {
     // todo support solana flow when NextID supported
     const networkMap = {
-        [NetworkPluginID.PLUGIN_EVM]: { name: 'Evm Chian', icon: new URL('../assets/evmChains.png', import.meta.url) },
+        [NetworkPluginID.PLUGIN_EVM]: { name: 'EVM Chian', icon: new URL('../assets/evmChains.png', import.meta.url) },
         [NetworkPluginID.PLUGIN_FLOW]: { name: 'Solana Chain', icon: new URL('../assets/solana.png', import.meta.url) },
         [NetworkPluginID.PLUGIN_SOLANA]: { name: 'Flow Chain', icon: new URL('../assets/flow.png', import.meta.url) },
     }
