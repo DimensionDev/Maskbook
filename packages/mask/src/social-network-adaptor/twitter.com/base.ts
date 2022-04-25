00e87778a016c305bc6219670916ddf14aadd501
nuanyang233 <nuanyang233@gmail.com>
2022-04-25 09:37:40 +0800

    chore: replace network id to enhanceable site (#6138)

--- packages/mask/src/social-network-adaptor/twitter.com/base.ts
+++ packages/mask/src/social-network-adaptor/twitter.com/base.ts
@@ -1,10 +1,10 @@
-import { TWITTER_ID } from '@masknet/shared'
 import { SocialNetworkEnum } from '@masknet/encryption'
 import type { SocialNetwork, SocialNetworkWorker } from '../../social-network/types'
+import { EnhanceableSite } from '@masknet/shared-base'
 
 const origins = ['https://mobile.twitter.com/*', 'https://twitter.com/*']
 export const twitterBase: SocialNetwork.Base = {
-    networkIdentifier: TWITTER_ID,
+    networkIdentifier: EnhanceableSite.Twitter,
     encryptionNetwork: SocialNetworkEnum.Twitter,
     declarativePermissions: { origins },
     shouldActivate(location) {
@@ -13,7 +13,7 @@ export const twitterBase: SocialNetwork.Base = {
 }
 
 export function isTwitter(ui: SocialNetwork.Base) {
-    return ui.networkIdentifier === TWITTER_ID
+    return ui.networkIdentifier === EnhanceableSite.Twitter
 }
 
 export const twitterWorkerBase: SocialNetworkWorker.WorkerBase & SocialNetwork.Base = {
