cafa08dacef7e0c78602192fd50e9c44bd990199
adz <slf.pro@gmail.com>
2022-04-28 10:30:23 +0300

    fix: disable twitter bearer token line

--- packages/mask/src/plugins/IdeaMarket/constants.ts
+++ packages/mask/src/plugins/IdeaMarket/constants.ts
@@ -11,4 +11,4 @@ export const ideaMarketPathnameRegexMatcher = /^\/i\/(\S+)\/(\S+)/
 
 /* cspell:disable-next-line */
 export const TWITTER_BEARER_TOKEN =
-    'AAAAAAAAAAAAAAAAAAAAAIakawEAAAAAFtDCZS2%2Fv8a8UsU3%2Bq1PdZYCY%2Bo%3DJNGDHi0EqYR11jkW9bhU27h0tXOq52BPaEYqJlA18wUBL5rydc'
+    'AAAAAAAAAAAAAAAAAAAAAIakawEAAAAAFtDCZS2%2Fv8a8UsU3%2Bq1PdZYCY%2Bo%3DJNGDHi0EqYR11jkW9bhU27h0tXOq52BPaEYqJlA18wUBL5rydc' /* cspell: disable-line */
