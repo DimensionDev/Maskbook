d92435df1142186983c27c1384b51debd7b08695
Jack Works <jackworks@protonmail.com>
2022-04-29 11:34:53 +0800

    refactor: move plugin database

--- packages/mask/src/database/index.ts
+++ packages/mask/src/database/index.ts
@@ -1,5 +1,2 @@
-export * from '../../background/database/avatar-cache/avatar'
-export * from '../../background/database/post'
 export * from './Persona/helpers'
 export * from './Persona/types'
-export * from './Plugin'
