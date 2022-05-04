450716b6228a9fb89d997a2a65bd8742154bcb8c
Jack Works <jackworks@protonmail.com>
2022-05-04 11:43:25 +0800

    chore: add patch-package

--- packages/scripts/src/codegen/index.ts
+++ packages/scripts/src/codegen/index.ts
@@ -5,17 +5,14 @@ import { typescript, typescriptWatch } from './typescript'
 import { isLocked } from '../utils'
 import { resourceCopy, resourceCopyWatch } from './resource-files'
 
-function patchPackage() {
-    return shell`pnpx patch-package`
-}
 export function codegen(cb: TaskFunctionCallback) {
     if (isLocked('codegen')) return cb()
-    const codegen = series(patchPackage, i18nCodegen, typescript, resourceCopy)
+    const codegen = series(i18nCodegen, typescript, resourceCopy)
     codegen(cb)
 }
 export const codegenWatch = series(
     getProcessLock.bind(null, 'codegen'),
-    parallel(patchPackage, resourceCopyWatch, i18nCodegenWatch, typescriptWatch),
+    parallel(resourceCopyWatch, i18nCodegenWatch, typescriptWatch),
 )
 watchTask(codegen, codegenWatch, 'codegen', 'All codegen tasks combined into one')
 
