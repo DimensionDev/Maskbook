7e95326c75d25a400266ab977e63d0879fecda0b
zhouhanseng <z308114274@gmail.com>
2022-05-05 12:39:08 +0800

    chore: transaction list ui

--- packages/mask/src/components/shared/WalletStatusBox/usePendingTransactions.tsx
+++ packages/mask/src/components/shared/WalletStatusBox/usePendingTransactions.tsx
@@ -1,16 +1,14 @@
 import { EMPTY_LIST } from '@masknet/shared-base'
-import { useAim } from '@masknet/shared-base-ui'
 import { makeStyles } from '@masknet/theme'
 import { TransactionStatusType } from '@masknet/web3-shared-evm'
 import { Typography } from '@mui/material'
 import classnames from 'classnames'
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import {
     useClearRecentTransactions,
     useRecentTransactions,
     useRemoveRecentTransaction,
 } from '../../../plugins/Wallet/hooks'
-import type { RecentTransaction } from '../../../plugins/Wallet/services'
 import { useI18N } from '../../../utils'
 import { TransactionList } from './TransactionList'
 
@@ -37,9 +35,6 @@ const useStyles = makeStyles()((theme) => ({
 export function usePendingTransactions() {
     const { classes } = useStyles()
     const { t } = useI18N()
-    const pendingSummaryRef = useRef<HTMLDivElement>(null)
-    const transactionsListRef = useRef<HTMLDivElement>(null)
-    const showRecentTransactions = useAim(pendingSummaryRef, transactionsListRef)
 
     // #region recent pending transactions
     const { value: pendingTransactions = EMPTY_LIST } = useRecentTransactions({
@@ -48,21 +43,17 @@ export function usePendingTransactions() {
 
     // frozenTxes would not be reactive to pendingTransactions,
     // it would be recreated then the list shows up.
-    const frozenTxes = useRef<RecentTransaction[]>([])
     const [meltedTxHashes, setMeltedTxHashes] = useState<string[]>([])
-    useEffect(() => {
-        frozenTxes.current = pendingTransactions.slice(0, 5)
-        setMeltedTxHashes([])
-    }, [showRecentTransactions])
+
     const clearRecentTxes = useClearRecentTransactions()
     const removeRecentTx = useRemoveRecentTransaction()
 
-    const transactions = frozenTxes.current.filter((tx) => !meltedTxHashes.includes(tx.hash))
+    const transactions = pendingTransactions.slice(0, 5).filter((tx) => !meltedTxHashes.includes(tx.hash))
     // #endregion
-
+    console.log({ transactions, pendingTransactions })
     const summary = pendingTransactions.length ? (
         <section className={classes.summaryWrapper}>
-            <div ref={pendingSummaryRef} className={classnames(pendingTransactions.length ? '' : classes.hide)}>
+            <div className={classnames(pendingTransactions.length ? '' : classes.hide)}>
                 {pendingTransactions.length ? (
                     <Typography className={classes.pendingSummary} variant="body2" mr={1} fontWeight={700}>
                         {pendingTransactions.length} {t('wallet_status_pending')}
@@ -78,15 +69,13 @@ export function usePendingTransactions() {
     ) : null
 
     const transactionList = (
-        <div ref={transactionsListRef} className={showRecentTransactions ? '' : classes.hide}>
-            <TransactionList
-                transactions={transactions}
-                onClear={(tx) => {
-                    setMeltedTxHashes((list) => [...list, tx.hash])
-                    removeRecentTx(tx.hash)
-                }}
-            />
-        </div>
+        <TransactionList
+            transactions={transactions}
+            onClear={(tx) => {
+                setMeltedTxHashes((list) => [...list, tx.hash])
+                removeRecentTx(tx.hash)
+            }}
+        />
     )
 
     return { summary, transactionList }
