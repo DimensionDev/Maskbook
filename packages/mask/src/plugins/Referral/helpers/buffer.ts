184221a6047e2a83eb8f315d5e030573f44b262b
Mariia <msynoverska@webinerds.com>
2022-05-10 12:17:46 +0200

    refactor: helpers restructure

---
+++ packages/mask/src/plugins/Referral/helpers/buffer.ts
@@ -0,0 +1,77 @@
+import { BigNumber } from '@ethersproject/bignumber'
+import type { Bytes } from '@ethersproject/bytes'
+import type { PrefixedHexString } from 'ethereumjs-util'
+
+// 32-byte buffer for hashes and 32-byte encoded strings
+export interface Buffer32 extends Buffer {}
+// 24-byte buffer for chain addresses and token definitions
+export interface Buffer24 extends Buffer {}
+// 24-byte buffer for chain addresses and token definitions
+export interface Buffer20 extends Buffer {}
+
+// Never used class to force ts compiler to hide the underlying alias type.
+class TypeStub {
+    constructor() {}
+}
+
+type Uint32 = bigint | TypeStub
+// 32-byte hex-encoded string
+type Hex32 = string | TypeStub
+// 24-byte hex-encoded string
+type Hex24 = string | TypeStub
+// 20-byte hex-encoded string
+type Hex20 = string | TypeStub
+
+export type BufferTypes = Buffer | Buffer32 | Buffer24 | Buffer20
+export type HexTypes = PrefixedHexString | Hex32 | Hex24 | Hex20
+export type BigIntTypes = bigint | Uint32
+
+function toEvenLength(str: string): string {
+    if (str.length % 2 !== 0) {
+        return '0' + str
+    }
+    return str
+}
+function isHexString(value: string): boolean {
+    return !!value.match(/^0x[\dA-Fa-f]*$/)
+}
+function buf(b: BufferTypes | Bytes | Uint8Array | HexTypes | number | BigIntTypes | BigNumber): Buffer {
+    if (b === null) return b
+
+    if (Buffer.isBuffer(b)) return b
+
+    if (b instanceof Uint8Array) return Buffer.from(b)
+
+    if (typeof b === 'number') {
+        return Buffer.from(BigNumber.from(b).toHexString().substring(2), 'hex')
+    }
+
+    if (typeof b === 'bigint') return Buffer.from(toEvenLength(b.toString(16)), 'hex')
+
+    if (typeof b === 'string') {
+        if (!b.startsWith('0x')) throw new Error('unsupported')
+        const hex = b.substring(2)
+        if (hex.length % 2 !== 0 || !isHexString(b)) {
+            // Buffer.from(hex, 'hex') will throw on invalid hex, should we do it?
+            throw new Error('invalid hexable string')
+        }
+
+        return Buffer.from(hex, 'hex')
+    }
+
+    // This should return the most compact buffer version without leading zeros, so safe for RLP encodings.
+    if (b instanceof BigNumber || (b as any)?._isBigNumber === true)
+        return Buffer.from((b as any).toHexString().substring(2), 'hex')
+
+    throw new Error('unsupported: ')
+}
+export function bi(b: BufferTypes | BigIntTypes | Uint8Array | HexTypes | number | BigNumber): bigint {
+    if (typeof b === 'bigint') return b
+    if (typeof b === 'number') return BigInt(b)
+    if (b instanceof Buffer) return BigInt('0x' + b.toString('hex'))
+    if (b instanceof Uint8Array) return bi(buf(b))
+    if (typeof b === 'string' && b.startsWith('0x')) return BigInt(b)
+    if (b instanceof BigNumber || (b as any)?._isBigNumber === true) return (b as any).toBigInt() as bigint
+
+    throw new Error('unsupported')
+}
---
+++ packages/mask/src/plugins/Referral/helpers/index.tsx
@@ -0,0 +1,62 @@
+import { padStart } from 'lodash-unified'
+import { BigNumber } from '@ethersproject/bignumber'
+import type { ChainId } from '@masknet/web3-shared-evm'
+import { createTypedMessageMetadataReader } from '@masknet/typed-message'
+
+import { META_KEY, supportedChainId } from '../constants'
+import type { ReferralMetaData, ChainAddress, ChainAddressProps, EvmAddress, Bytes32, Bytes24 } from '../types'
+import schema from '../schema.json'
+import { bi, Buffer20, Buffer24 } from './buffer'
+
+function toChainAddress(chainId: BigNumber | bigint, address: Buffer20): Buffer24 {
+    if (address.byteLength !== 20) throw new Error('invalid address')
+    const b = Buffer.alloc(24)
+    // Only numeric network id's are supported in the chain address, with max of uint32.
+    // TODO throw on this if wrong
+    b.writeUInt32BE(Number(bi(chainId)), 0)
+    b.fill(address, 4)
+    return b
+}
+export function toChainAddressEthers(chainId: number, address: string): string {
+    return '0x' + toChainAddress(BigNumber.from(chainId), Buffer.from(address.substring(2), 'hex')).toString('hex')
+}
+
+export function parseChainAddress(chainAddress: ChainAddress): ChainAddressProps {
+    const chainId = toChainId(chainAddress)
+    const address = toEvmAddress(chainAddress)
+    const isNative = chainId === Number.parseInt(address.substring(2 + 16 * 2), 16)
+    return {
+        chainId,
+        address,
+        isNative,
+    }
+}
+
+export function toNativeRewardTokenDefn(chainId: ChainId): string {
+    const nativeTokenAddr = '0x' + padStart(Number(chainId).toString(16), 40, '0')
+    return toChainAddressEthers(chainId, nativeTokenAddr)
+}
+
+export function expandEvmAddressToBytes32(addr: EvmAddress): Bytes32 {
+    return `0x000000000000000000000000${addr.substring(2)}`.toLowerCase()
+}
+export function expandBytes24ToBytes32(b24: Bytes24): Bytes32 {
+    return `0x${b24.substring(2)}0000000000000000`.toLowerCase()
+}
+export function toEvmAddress(addr: ChainAddress): EvmAddress {
+    return `0x${addr.substring(2 + 4 * 2)}`
+}
+export function toChainId(addr: ChainAddress): number {
+    return Number.parseInt(addr.substring(2, 2 + 4 * 2), 16)
+}
+
+export function roundValue(value: string | number, tokenDecimals?: number) {
+    const valueStr = tokenDecimals === 0 ? Math.ceil(Number(value)).toString() : Number(value).toFixed(5)
+    return Number.parseFloat(valueStr)
+}
+
+export function getRequiredChainId(currentChainId: ChainId) {
+    return supportedChainId === currentChainId ? currentChainId : supportedChainId
+}
+
+export const referralMetadataReader = createTypedMessageMetadataReader<ReferralMetaData>(META_KEY, schema)
