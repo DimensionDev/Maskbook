e9f3010c4131b8ab2cd5f57699ca39b3b2d99a91
guanbinrui <guanbinrui@dimension.im>
2022-05-11 20:44:00 +0800

    refactor: temp remove pooltogether

--- packages/mask/src/plugins/PoolTogether/contracts/usePoolTogetherPool.ts
+++
@@ -1,9 +0,0 @@
-import type { AbiItem } from 'web3-utils'
-import PoolTogetherPoolABI from '@masknet/web3-contracts/abis/PoolTogetherPool.json'
-import type { PoolTogetherPool } from '@masknet/web3-contracts/types/PoolTogetherPool'
-import type { ChainId } from '@masknet/web3-shared-evm'
-import { useContract } from '@masknet/plugin-infra/web3-evm'
-
-export function usePoolTogetherPoolContract(chainId?: ChainId, address?: string) {
-    return useContract<PoolTogetherPool>(chainId, address, PoolTogetherPoolABI as AbiItem[])
-}
--- packages/mask/src/plugins/PoolTogether/contracts/usePoolTogetherTicket.ts
+++
@@ -1,9 +0,0 @@
-import type { AbiItem } from 'web3-utils'
-import PoolTogetherTicketABI from '@masknet/web3-contracts/abis/PoolTogetherTicket.json'
-import type { PoolTogetherTicket } from '@masknet/web3-contracts/types/PoolTogetherTicket'
-import type { ChainId } from '@masknet/web3-shared-evm'
-import { useContracts } from '@masknet/plugin-infra/web3-evm'
-
-export function usePoolTogetherTicketContracts(chainId: ChainId, addresses: string[]) {
-    return useContracts<PoolTogetherTicket>(chainId, addresses, PoolTogetherTicketABI as AbiItem[])
-}
