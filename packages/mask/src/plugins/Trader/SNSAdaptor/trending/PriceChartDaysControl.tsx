23006da0b25605a7b1feb567f1ff0f1e0a749f27
Lantt <laanfor@gmail.com>
2022-05-11 23:24:08 +0800

    feat: change line days control style

--- packages/mask/src/plugins/Trader/SNSAdaptor/trending/PriceChartDaysControl.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trending/PriceChartDaysControl.tsx
@@ -1,18 +1,27 @@
 import { Link, Stack, Typography } from '@mui/material'
 import { makeStyles } from '@masknet/theme'
 import { resolveDaysName } from '../../pipes'
+import classNames from 'classnames'
 
 const useStyles = makeStyles()((theme) => ({
-    link: {
-        cursor: 'pointer',
-        marginRight: theme.spacing(1),
-        '&:last-child': {
-            marginRight: 0,
-        },
+    root: {
+        background: theme.palette.background.input,
+        borderRadius: 28,
+        fontSize: 14,
+        padding: theme.spacing(0.5),
     },
-    text: {
-        fontSize: 10,
-        fontWeight: 300,
+    active: {
+        boxShadow: '0px 2px 5px 1px rgba(0, 0, 0, 0.05)',
+        background: theme.palette.background.paper,
+        fontWeight: 700,
+    },
+    link: {
+        padding: theme.spacing(1),
+        width: '25%',
+        cursor: 'pointer',
+        borderRadius: 18,
+        textAlign: 'center',
+        color: theme.palette.text.primary,
     },
 }))
 
@@ -20,9 +29,7 @@ export enum Days {
     MAX = 0,
     ONE_DAY = 1,
     ONE_WEEK = 7,
-    TWO_WEEKS = 14,
     ONE_MONTH = 30,
-    THREE_MONTHS = 90,
     ONE_YEAR = 365,
 }
 
@@ -34,23 +41,13 @@ export interface PriceChartDaysControlProps {
 export function PriceChartDaysControl(props: PriceChartDaysControlProps) {
     const { classes } = useStyles()
     return (
-        <Stack direction="row" gap={2}>
-            {[
-                Days.ONE_DAY,
-                Days.ONE_WEEK,
-                Days.TWO_WEEKS,
-                Days.ONE_MONTH,
-                Days.THREE_MONTHS,
-                Days.ONE_YEAR,
-                Days.MAX,
-            ].map((days) => (
-                <Link className={classes.link} key={days} onClick={() => props.onDaysChange?.(days)}>
-                    <Typography
-                        className={classes.text}
-                        component="span"
-                        color={props.days === days ? 'primary' : 'textSecondary'}>
-                        {resolveDaysName(days)}
-                    </Typography>
+        <Stack className={classes.root} direction="row" gap={2}>
+            {[Days.ONE_DAY, Days.ONE_WEEK, Days.ONE_MONTH, Days.ONE_YEAR, Days.MAX].map((days) => (
+                <Link
+                    className={classNames(classes.link, props.days === days ? classes.active : '')}
+                    key={days}
+                    onClick={() => props.onDaysChange?.(days)}>
+                    <Typography component="span">{resolveDaysName(days)}</Typography>
                 </Link>
             ))}
         </Stack>
