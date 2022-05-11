e84aa94a4aaac1a0746a04c36690f848e48b05d5
nuanyang233 <nuanyang233@gmail.com>
2022-05-11 20:20:17 +0800

    fix: navigate to connect wallet when params has internal

--- packages/mask/src/extension/popups/pages/Personas/ConnectedWallets/index.tsx
+++ packages/mask/src/extension/popups/pages/Personas/ConnectedWallets/index.tsx
@@ -12,12 +12,15 @@ import type { ConnectedWalletInfo } from '../type'
 import { NextIDProof } from '@masknet/web3-providers'
 import Service from '../../../../service'
 import { usePopupCustomSnackbar } from '@masknet/theme'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const ConnectedWallets = memo(() => {
     const { t } = useI18N()
     const chainId = useChainId()
     const { NameService } = useWeb3State()
     const wallets = useWallets()
+    const navigate = useNavigate()
+    const location = useLocation()
     const { proofs, currentPersona, refreshProofs, fetchProofsLoading } = PersonaContext.useContainer()
 
     const { showSnackbar } = usePopupCustomSnackbar()
@@ -107,6 +110,13 @@ const ConnectedWallets = memo(() => {
     )
 
     const navigateToConnectWallet = async () => {
+        const params = new URLSearchParams(location.search)
+        const internal = params.get('internal')
+
+        if (internal) {
+            navigate(PopupRoutes.ConnectWallet)
+            return
+        }
         await Service.Helper.openPopupWindow(PopupRoutes.ConnectWallet)
         window.close()
     }
