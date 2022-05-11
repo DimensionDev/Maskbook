4525dafc24a10c509420c0d2191fdaf2b21aead8
guanbinrui <guanbinrui@dimension.im>
2022-05-11 21:21:53 +0800

    refactor: comment out ito

--- packages/mask/src/plugins/ITO/SNSAdaptor/hooks/usePoolPayload.ts
+++
@@ -1,9 +0,0 @@
-import { useAsyncRetry } from 'react-use'
-import { PluginITO_RPC } from '../../messages'
-
-export function usePoolPayload(pid: string) {
-    return useAsyncRetry(async () => {
-        if (!pid) return
-        return PluginITO_RPC.getPool(pid)
-    }, [pid])
-}
--- packages/mask/src/plugins/ITO/messages.ts
+++
@@ -1,6 +0,0 @@
-import { ITO_PluginID } from './constants'
-import { createPluginMessage, createPluginRPC } from '@masknet/plugin-infra'
-
-if (import.meta.webpackHot) import.meta.webpackHot.accept()
-const PluginITO_Messages = createPluginMessage(ITO_PluginID)
-export const PluginITO_RPC = createPluginRPC(ITO_PluginID, () => import('./Worker/services'), PluginITO_Messages.rpc)
