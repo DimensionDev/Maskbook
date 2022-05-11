7c2029bb4f23aa65c8a268798a9ae987d8a9110a
guanbinrui <guanbinrui@dimension.im>
2022-05-11 21:24:56 +0800

    refactor: comment out good dhedge

--- packages/mask/src/plugins/dHEDGE/hooks/useManager.ts
+++
@@ -1,6 +0,0 @@
-import { useBlockie } from '@masknet/plugin-infra/web3'
-import { BLOCKIES_OPTIONS } from '../constants'
-
-export function useAvatar(address: string) {
-    return useBlockie(address, BLOCKIES_OPTIONS)
-}
