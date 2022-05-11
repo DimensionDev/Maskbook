a85395ea98c8edb9352ee9ec48e9c32794f4a39c
guanbinrui <guanbinrui@dimension.im>
2022-05-11 20:42:36 +0800

    refactor: temp remove savings

--- packages/mask/src/plugins/Savings/SNSAdaptor/IconURL.tsx
+++
@@ -1,6 +0,0 @@
-import { ProtocolType } from '../types'
-
-export const ProviderIconURLs: Record<ProtocolType, string> = {
-    [ProtocolType.Lido]: new URL('./assets/lido.png', import.meta.url).toString(),
-    [ProtocolType.AAVE]: new URL('./assets/aave.png', import.meta.url).toString(),
-}
--- packages/mask/src/plugins/Savings/protocols/index.ts
+++
@@ -1,8 +0,0 @@
-import { AAVE_PAIRS, LDO_PAIRS } from '../constants'
-import { LidoProtocol } from './LDOProtocol'
-import { AAVEProtocol } from './AAVEProtocol'
-
-export const SavingsProtocols = [
-    ...LDO_PAIRS.map((pair) => new LidoProtocol(pair)),
-    ...AAVE_PAIRS.map((pair) => new AAVEProtocol(pair)),
-]
