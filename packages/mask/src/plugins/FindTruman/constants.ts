4b0ce9343dbf5c6c92fda07673b376476e8d4a8f
guanbinrui <guanbinrui@dimension.im>
2022-05-11 21:25:52 +0800

    refactor: comment out good findtrueman

--- packages/mask/src/plugins/FindTruman/constants.ts
+++
@@ -1,6 +0,0 @@
-import FindTrumanConstPromise from './SNSAdaptor/ConstPromise'
-
-export const FIND_TRUMAN_PLUGIN_NAME = 'FindTruman'
-export const FIND_TRUMAN_PLUGIN_ID = 'org.findtruman'
-
-export const FindTruman_Const: FindTrumanConstPromise = new FindTrumanConstPromise()
