f7aa319689ea12ccaf13016b2362a73ce7edce6b
guanbinrui <guanbinrui@dimension.im>
2022-05-11 21:29:07 +0800

    refactor: comment out popup

--- packages/mask/src/extension/popups/pages/Wallet/GasSetting/types.ts
+++
@@ -1,9 +0,0 @@
-import type { GasOptionType } from '@masknet/web3-shared-base'
-
-export interface GasSettingProps {
-    gasLimit?: number | string
-    onGasLimitChange?: (newVal: number | string) => void
-    gasOptionType?: GasOptionType
-    onGasOptionTypeChange?: (newOptionType: GasOptionType) => void
-    onConfirm?: (options: { gasPrice: number; gasLimit: number | string }) => void
-}
