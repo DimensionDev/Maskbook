b22f576156ac5d6f206febaed06e84844346ed7e
guanbinrui <guanbinrui@dimension.im>
2022-05-11 21:27:37 +0800

    refactor: comment out ArtBlock

--- packages/mask/src/plugins/ArtBlocks/SNSAdaptor/PurchaseDialog.tsx
+++ packages/mask/src/plugins/ArtBlocks/SNSAdaptor/PurchaseDialog.tsx
@@ -51,159 +51,160 @@ export interface ActionBarProps {
 }
 
 export function PurchaseDialog(props: ActionBarProps) {
-    const { t } = useI18N()
-    const { classes } = useStyles()
-    const { project, open, onClose } = props
+    return <></>
+    // const { t } = useI18N()
+    // const { classes } = useStyles()
+    // const { project, open, onClose } = props
 
-    const { token, balance } = useFungibleTokenWatched(
-        NetworkPluginID.PLUGIN_EVM,
-        project.currencyAddress ? project.currencyAddress : '',
-    )
+    // const { token, balance } = useFungibleTokenWatched(
+    //     NetworkPluginID.PLUGIN_EVM,
+    //     project.currencyAddress ? project.currencyAddress : '',
+    // )
 
-    const [ToS_Checked, setToS_Checked] = useState(false)
-    const [purchaseState, onCheckout, resetCallback] = usePurchaseCallback(
-        project.projectId,
-        project.pricePerTokenInWei,
-        token.value?.schema,
-    )
-    const { GEN_ART_721_MINTER: spender } = useArtBlocksConstants()
+    // const [ToS_Checked, setToS_Checked] = useState(false)
+    // const [purchaseState, onCheckout, resetCallback] = usePurchaseCallback(
+    //     project.projectId,
+    //     project.pricePerTokenInWei,
+    //     token.value?.schema,
+    // )
+    // const { GEN_ART_721_MINTER: spender } = useArtBlocksConstants()
 
-    const price = useMemo(
-        () => leftShift(project.pricePerTokenInWei, token.value?.decimals),
-        [project.pricePerTokenInWei, token.value?.decimals],
-    )
+    // const price = useMemo(
+    //     () => leftShift(project.pricePerTokenInWei, token.value?.decimals),
+    //     [project.pricePerTokenInWei, token.value?.decimals],
+    // )
 
-    const postLink = usePostLink()
-    const shareText = [
-        t(
-            isTwitter(activatedSocialNetworkUI) || isFacebook(activatedSocialNetworkUI)
-                ? 'plugin_artblocks_share'
-                : 'plugin_artblocks_share_no_official_account',
-            {
-                name: project.name,
-                price: price,
-                symbol: token.value?.symbol,
-            },
-        ),
-        '#mask_io #artblocks_io #nft',
-        postLink,
-    ].join('\n')
+    // const postLink = usePostLink()
+    // const shareText = [
+    //     t(
+    //         isTwitter(activatedSocialNetworkUI) || isFacebook(activatedSocialNetworkUI)
+    //             ? 'plugin_artblocks_share'
+    //             : 'plugin_artblocks_share_no_official_account',
+    //         {
+    //             name: project.name,
+    //             price: price,
+    //             symbol: token.value?.symbol,
+    //         },
+    //     ),
+    //     '#mask_io #artblocks_io #nft',
+    //     postLink,
+    // ].join('\n')
 
-    const { setDialog: setTransactionDialog } = useRemoteControlledDialog(
-        WalletMessages.events.transactionDialogUpdated,
-        useCallback(
-            (ev: { open: boolean }) => {
-                if (!ev.open) {
-                    const allowedTypes = [TransactionStateType.HASH, TransactionStateType.CONFIRMED]
-                    if (!allowedTypes.includes(purchaseState.type)) return
-                    onClose()
-                }
-                resetCallback()
-            },
-            [purchaseState, onClose],
-        ),
-    )
+    // const { setDialog: setTransactionDialog } = useRemoteControlledDialog(
+    //     WalletMessages.events.transactionDialogUpdated,
+    //     useCallback(
+    //         (ev: { open: boolean }) => {
+    //             if (!ev.open) {
+    //                 const allowedTypes = [TransactionStateType.HASH, TransactionStateType.CONFIRMED]
+    //                 if (!allowedTypes.includes(purchaseState.type)) return
+    //                 onClose()
+    //             }
+    //             resetCallback()
+    //         },
+    //         [purchaseState, onClose],
+    //     ),
+    // )
 
-    const isTransaction =
-        purchaseState.type === TransactionStateType.UNKNOWN ||
-        purchaseState.type === TransactionStateType.CONFIRMED ||
-        purchaseState.type === TransactionStateType.FAILED
+    // const isTransaction =
+    //     purchaseState.type === TransactionStateType.UNKNOWN ||
+    //     purchaseState.type === TransactionStateType.CONFIRMED ||
+    //     purchaseState.type === TransactionStateType.FAILED
 
-    useEffect(() => {
-        if (purchaseState.type === TransactionStateType.UNKNOWN) return
-        setTransactionDialog({
-            open: true,
-            shareText,
-            state: purchaseState,
-            summary: `Minting a new collectible from '${project.name}' collection.`,
-        })
-    }, [purchaseState])
+    // useEffect(() => {
+    //     if (purchaseState.type === TransactionStateType.UNKNOWN) return
+    //     setTransactionDialog({
+    //         open: true,
+    //         shareText,
+    //         state: purchaseState,
+    //         summary: `Minting a new collectible from '${project.name}' collection.`,
+    //     })
+    // }, [purchaseState])
 
-    const validationMessage = useMemo(() => {
-        const balance_ = leftShift(balance.value ?? '0', token.value?.decimals)
+    // const validationMessage = useMemo(() => {
+    //     const balance_ = leftShift(balance.value ?? '0', token.value?.decimals)
 
-        if (balance_.isZero() || price.isGreaterThan(balance_)) return t('plugin_collectible_insufficient_balance')
-        if (!ToS_Checked) return t('plugin_artblocks_check_tos_document')
+    //     if (balance_.isZero() || price.isGreaterThan(balance_)) return t('plugin_collectible_insufficient_balance')
+    //     if (!ToS_Checked) return t('plugin_artblocks_check_tos_document')
 
-        return ''
-    }, [price, balance.value, token.value?.decimals, ToS_Checked])
+    //     return ''
+    // }, [price, balance.value, token.value?.decimals, ToS_Checked])
 
-    return (
-        <InjectedDialog title={t('plugin_artblocks_purchase')} open={open} onClose={onClose}>
-            <DialogContent className={classes.content}>
-                <Card elevation={0}>
-                    <CardContent>
-                        <TokenAmountPanel
-                            label={t('plugin_artblocks_price_per_mint')}
-                            amount={price.toString()}
-                            balance={balance.value ?? '0'}
-                            token={token.value}
-                            onAmountChange={() => {}}
-                        />
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    color="primary"
-                                    checked={ToS_Checked}
-                                    onChange={(event) => setToS_Checked(event.target.checked)}
-                                />
-                            }
-                            label={
-                                <Typography variant="body2">
-                                    <Trans
-                                        i18nKey="plugin_artblocks_legal_text"
-                                        components={{
-                                            terms: (
-                                                <Link
-                                                    color="primary"
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                    href="https://www.artblocks.io/tos.pdf"
-                                                />
-                                            ),
-                                        }}
-                                    />
-                                </Typography>
-                            }
-                        />
-                    </CardContent>
-                    <CardActions>
-                        <EthereumWalletConnectedBoundary>
-                            {token.value?.schema === SchemaType.Native ? (
-                                <ActionButton
-                                    className={classes.button}
-                                    disabled={!!validationMessage}
-                                    color="primary"
-                                    variant="contained"
-                                    onClick={onCheckout}
-                                    fullWidth>
-                                    {validationMessage || isTransaction
-                                        ? t('plugin_artblocks_purchase')
-                                        : t('plugin_artblocks_purchasing')}
-                                </ActionButton>
-                            ) : null}
-                            {token.value?.schema === SchemaType.ERC20 ? (
-                                <EthereumERC20TokenApprovedBoundary
-                                    amount={project.pricePerTokenInWei}
-                                    spender={spender}
-                                    token={token.value}>
-                                    <ActionButton
-                                        className={classes.button}
-                                        disabled={!!validationMessage}
-                                        color="primary"
-                                        variant="contained"
-                                        onClick={onCheckout}
-                                        fullWidth>
-                                        {validationMessage || isTransaction
-                                            ? t('plugin_artblocks_purchase')
-                                            : t('plugin_artblocks_purchasing')}
-                                    </ActionButton>
-                                </EthereumERC20TokenApprovedBoundary>
-                            ) : null}
-                        </EthereumWalletConnectedBoundary>
-                    </CardActions>
-                </Card>
-            </DialogContent>
-        </InjectedDialog>
-    )
+    // return (
+    //     <InjectedDialog title={t('plugin_artblocks_purchase')} open={open} onClose={onClose}>
+    //         <DialogContent className={classes.content}>
+    //             <Card elevation={0}>
+    //                 <CardContent>
+    //                     <TokenAmountPanel
+    //                         label={t('plugin_artblocks_price_per_mint')}
+    //                         amount={price.toString()}
+    //                         balance={balance.value ?? '0'}
+    //                         token={token.value}
+    //                         onAmountChange={() => {}}
+    //                     />
+    //                     <FormControlLabel
+    //                         control={
+    //                             <Checkbox
+    //                                 color="primary"
+    //                                 checked={ToS_Checked}
+    //                                 onChange={(event) => setToS_Checked(event.target.checked)}
+    //                             />
+    //                         }
+    //                         label={
+    //                             <Typography variant="body2">
+    //                                 <Trans
+    //                                     i18nKey="plugin_artblocks_legal_text"
+    //                                     components={{
+    //                                         terms: (
+    //                                             <Link
+    //                                                 color="primary"
+    //                                                 target="_blank"
+    //                                                 rel="noopener noreferrer"
+    //                                                 href="https://www.artblocks.io/tos.pdf"
+    //                                             />
+    //                                         ),
+    //                                     }}
+    //                                 />
+    //                             </Typography>
+    //                         }
+    //                     />
+    //                 </CardContent>
+    //                 <CardActions>
+    //                     <EthereumWalletConnectedBoundary>
+    //                         {token.value?.schema === SchemaType.Native ? (
+    //                             <ActionButton
+    //                                 className={classes.button}
+    //                                 disabled={!!validationMessage}
+    //                                 color="primary"
+    //                                 variant="contained"
+    //                                 onClick={onCheckout}
+    //                                 fullWidth>
+    //                                 {validationMessage || isTransaction
+    //                                     ? t('plugin_artblocks_purchase')
+    //                                     : t('plugin_artblocks_purchasing')}
+    //                             </ActionButton>
+    //                         ) : null}
+    //                         {token.value?.schema === SchemaType.ERC20 ? (
+    //                             <EthereumERC20TokenApprovedBoundary
+    //                                 amount={project.pricePerTokenInWei}
+    //                                 spender={spender}
+    //                                 token={token.value}>
+    //                                 <ActionButton
+    //                                     className={classes.button}
+    //                                     disabled={!!validationMessage}
+    //                                     color="primary"
+    //                                     variant="contained"
+    //                                     onClick={onCheckout}
+    //                                     fullWidth>
+    //                                     {validationMessage || isTransaction
+    //                                         ? t('plugin_artblocks_purchase')
+    //                                         : t('plugin_artblocks_purchasing')}
+    //                                 </ActionButton>
+    //                             </EthereumERC20TokenApprovedBoundary>
+    //                         ) : null}
+    //                     </EthereumWalletConnectedBoundary>
+    //                 </CardActions>
+    //             </Card>
+    //         </DialogContent>
+    //     </InjectedDialog>
+    // )
 }
