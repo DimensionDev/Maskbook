6172eee7756753eb7304d3f727f08ff2eb497182
unclebill <billbill290@gmail.com>
2022-05-12 15:41:54 +0800

    fix(gas): don't arbitrarily fallback gas to zero if fails to estimate gas fee

--- packages/mask/src/plugins/Trader/SNSAdaptor/trader/hooks/useGasConfig.ts
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trader/hooks/useGasConfig.ts
@@ -5,16 +5,18 @@ import type { ChainId, GasOptionConfig } from '@masknet/web3-shared-evm'
 import { formatGweiToWei, isEIP1559Supported } from '@masknet/web3-shared-evm'
 import { WalletRPC } from '../../../../Wallet/messages'
 
+function formatGas(val?: BigNumber.Value) {
+    return typeof val !== 'undefined' ? formatGweiToWei(val).toFixed(0) : undefined
+}
+
 export function useGasConfig(chainId: ChainId) {
     const [gasConfig, setGasConfig] = useState<GasOptionConfig | undefined>()
     const { value: gasPrice } = useAsync(async () => {
         try {
             if (isEIP1559Supported(chainId)) {
                 const response = await WalletRPC.getEstimateGasFees(chainId)
-                const maxFeePerGas = formatGweiToWei(response?.medium?.suggestedMaxFeePerGas ?? 0).toFixed(0)
-                const maxPriorityFeePerGas = formatGweiToWei(
-                    response?.medium?.suggestedMaxPriorityFeePerGas ?? 0,
-                ).toFixed(0)
+                const maxFeePerGas = formatGas(response?.medium?.suggestedMaxFeePerGas)
+                const maxPriorityFeePerGas = formatGas(response?.medium?.suggestedMaxPriorityFeePerGas)
                 setGasConfig({
                     maxFeePerGas,
                     maxPriorityFeePerGas,
