227ccbea50163f602857c8c7ab12a9774e71c44b
zhouhanseng <z308114274@gmail.com>
2022-05-13 11:49:36 +0800

    chore: update red packet

--- packages/mask/src/plugins/RedPacket/SNSAdaptor/hooks/useAvailability.ts
+++ packages/mask/src/plugins/RedPacket/SNSAdaptor/hooks/useAvailability.ts
@@ -1,16 +1,15 @@
 import { useAsyncRetry } from 'react-use'
-import { ChainId, useChainId } from '@masknet/web3-shared-evm'
+import type { ChainId } from '@masknet/web3-shared-evm'
 import { useRedPacketContract } from './useRedPacketContract'
 
 export function useAvailability(version: number, from: string, id: string, redpacketChainId: ChainId) {
-    const redPacketContract = useRedPacketContract(version)
-    const currentChainId = useChainId()
+    const redPacketContract = useRedPacketContract(version, redpacketChainId)
     return useAsyncRetry(async () => {
-        if (!id || redpacketChainId !== currentChainId) return null
+        if (!id) return null
         if (!redPacketContract) return null
         return redPacketContract.methods.check_availability(id).call({
             // check availability is ok w/o account
             from,
         })
-    }, [id, from, redpacketChainId, currentChainId, redPacketContract])
+    }, [id, from, redpacketChainId, redPacketContract])
 }
