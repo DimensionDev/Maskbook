e0339b926d8d4468f36ed4893ae03c900c3d5f5c
Mariia <msynoverska@webinerds.com>
2022-05-13 13:57:21 +0100

    fix: change REFERRAL_FARMS_V1_ADDR and CONFIRMATION_V1_ADDR

--- packages/mask/src/plugins/Referral/constants.ts
+++ packages/mask/src/plugins/Referral/constants.ts
@@ -20,8 +20,8 @@ enum ORACLE_CHAIN_ID {
 // TODO: change to Mainnet before mainnet release
 export const supportedChainId = ChainId.Rinkeby
 export const SWAP_CHAIN_ID = ChainId.Mainnet
-export const REFERRAL_FARMS_V1_ADDR = '0xF657F4F0408AcEE64722483acf53Ae3fd7C85cB7'
-export const CONFIRMATION_V1_ADDR = '0x27B98395Ab405078387da866013fDabfcD2f0ee9'
+export const REFERRAL_FARMS_V1_ADDR = '0x5c057Fd6830fCAecA2BC76A8FBFf9b03D274Ce13'
+export const CONFIRMATION_V1_ADDR = '0x1d7216020f8FF3Fb32C7AD96F583a6D7b65E985A'
 export const supportedOracleChainId = ORACLE_CHAIN_ID.testnet
 
 export const APR = 'N/A'
