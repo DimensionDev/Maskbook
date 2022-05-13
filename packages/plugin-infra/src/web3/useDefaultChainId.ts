0105b292affce0c4f3b0006c84f9455d6f9b5578
guanbinrui <guanbinrui@dimension.im>
2022-05-13 17:44:12 +0800

    refactor: temp disable flow chain

--- packages/plugin-infra/src/web3/useDefaultChainId.ts
+++ packages/plugin-infra/src/web3/useDefaultChainId.ts
@@ -1,13 +1,14 @@
 import { NetworkPluginID } from '@masknet/web3-shared-base'
 import { ChainId as EVM_ChainId } from '@masknet/web3-shared-evm'
-import { ChainId as FlowChainId } from '@masknet/web3-shared-flow'
+// import { ChainId as FlowChainId } from '@masknet/web3-shared-flow'
 import { ChainId as SolanaChainId } from '@masknet/web3-shared-solana'
 import type { Web3Helper } from '../web3-helpers'
 import { useCurrentWeb3NetworkPluginID } from './Context'
 
 const DEFAULT_CHAIN_ID: Record<NetworkPluginID, Web3Helper.Definition[NetworkPluginID]['ChainId']> = {
     [NetworkPluginID.PLUGIN_EVM]: EVM_ChainId.Mainnet,
-    [NetworkPluginID.PLUGIN_FLOW]: FlowChainId.Mainnet,
+    [NetworkPluginID.PLUGIN_FLOW]: 1,
+    // [NetworkPluginID.PLUGIN_FLOW]: FlowChainId.Mainnet,
     [NetworkPluginID.PLUGIN_SOLANA]: SolanaChainId.Mainnet,
 }
 
