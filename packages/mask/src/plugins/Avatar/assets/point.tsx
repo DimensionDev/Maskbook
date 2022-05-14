0abb7b2295e1d5673e4f9df8f8caecf9cbfe2d3e
lelenei <zwpsky@gmail.com>
2022-05-14 11:00:24 +0800

    fix: typo

--- packages/mask/src/plugins/Avatar/assets/point.tsx
+++ packages/mask/src/plugins/Avatar/assets/point.tsx
@@ -1,6 +1,6 @@
 import { createIcon } from '@masknet/icons'
 export const PointIcon = createIcon(
-    'Pointicon',
+    'PointIcon',
     <g>
         <path
             d="M4.6001 8.29999C6.5331 8.29999 8.1001 6.73298 8.1001 4.79999C8.1001 2.86699 6.5331 1.29999 4.6001 1.29999C2.6671 1.29999 1.1001 2.86699 1.1001 4.79999C1.1001 6.73298 2.6671 8.29999 4.6001 8.29999Z"
