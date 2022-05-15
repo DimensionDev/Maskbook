80447c47025940d9a40b0147efaedad464795d7d
guanbinrui <guanbinrui@dimension.im>
2022-05-15 17:28:44 +0800

    refactor: code style

--- packages/plugins/EVM/src/state/TokenIcon.ts
+++ packages/plugins/EVM/src/state/TokenIcon.ts
@@ -1,15 +1,17 @@
 import type { Plugin } from '@masknet/plugin-infra'
 import SPECIAL_ICON_LIST from './TokenIcon/TokenIconSpecialIconList.json'
 import { currySameAddress, isSameAddress, TokenIconState } from '@masknet/web3-shared-base'
-import { ChainId, formatEthereumAddress, getTokenAssetBaseURLConstants, getTokenConstants } from '@masknet/web3-shared-evm'
+import {
+    ChainId,
+    formatEthereumAddress,
+    getTokenAssetBaseURLConstants,
+    getTokenConstants,
+} from '@masknet/web3-shared-evm'
 
 export class TokenIcon implements TokenIconState<ChainId> {
-    constructor(
-        context: Plugin.Shared.SharedContext,
-    ) {
-    }
+    constructor(context: Plugin.Shared.SharedContext) {}
     async getFungibleTokenIconURLs(chainId: ChainId, address: string) {
-        const {TOKEN_ASSET_BASE_URI = []} = getTokenAssetBaseURLConstants(chainId)
+        const { TOKEN_ASSET_BASE_URI = [] } = getTokenAssetBaseURLConstants(chainId)
         const checkSummedAddress = formatEthereumAddress(address)
 
         if (isSameAddress(getTokenConstants().NATIVE_TOKEN_ADDRESS, checkSummedAddress)) {
