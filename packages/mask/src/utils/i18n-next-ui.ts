986155b9dfd0c60b5fa551a0eb7bf2d0cfe9701c
unclebill <billbill290@gmail.com>
2022-05-16 17:20:28 +0800

    chore(locale-kit): scan for plural/context/nested i18n keys
    Set `contextSeparator` to `$`
    
    closes #6221

--- packages/mask/src/utils/i18n-next-ui.ts
+++ packages/mask/src/utils/i18n-next-ui.ts
@@ -3,11 +3,17 @@ import type en from '../../shared-ui/locales/en-US.json'
 import type { i18NextInstance, TranslateOptions } from '@masknet/shared-base'
 import { SupportedLanguages } from '@masknet/public-api'
 
-export type I18NFunction = <TKeys extends keyof typeof en>(
+type PluralsSuffix = 'zero' | 'one' | 'two' | 'few' | 'many' | 'other'
+
+type LocaleKeys = keyof typeof en
+type ExtendBaseKeys<K> = K extends `${infer B}$${string}` ? B | K : K extends `${infer B}_${PluralsSuffix}` ? B | K : K
+type AvailableLocaleKeys = ExtendBaseKeys<LocaleKeys>
+
+export type I18NFunction = <TKeys extends AvailableLocaleKeys>(
     key: TKeys | TKeys[],
     // defaultValue?: string,
     options?: TranslateOptions | string,
-) => typeof en[TKeys]
+) => string
 
 /**
  * Enhanced version of useTranslation
