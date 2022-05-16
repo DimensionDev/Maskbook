6b4017af867d4ef755cfd7b9e1fa2ee288fbca2a
billy <Billy@mask.io>
2022-05-16 16:50:50 +0800

    fix: lint

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,7 @@
 export default {
     '*': 'prettier --write --ignore-unknown',
     'packages/**/*.{ts,tsx,js,jsx}': 'eslint -c packages/.eslintrc.json --fix',
+    'packages/mask/**/*': () => 'gulp locale-kit --sync-keys --remove-unused-keys',
     'packages/web3-constants/**/*': () => 'pnpm --filter ./packages/web3-constants start',
     'cspell.json': () => 'gulp reorder-spellcheck',
 }
