a6c54cf3d215888a80c1efa15fc7c1e3c8670964
billy <Billy@mask.io>
2022-05-16 23:45:18 +0800

    fix: prettier

--- packages/mask/src/components/shared/SelectRecipients/useTwitterIdByWalletSearch.tsx
+++ packages/mask/src/components/shared/SelectRecipients/useTwitterIdByWalletSearch.tsx
@@ -14,18 +14,17 @@ export function useTwitterIdByWalletSearch(
 ) {
     if (!bindings || !type) return EMPTY_LIST
 
-    return bindings
-        .map((binding) => {
-            let proofs = binding.proofs.filter((x) => x.platform === NextIDPlatform.Twitter)
-            proofs = uniqBy(cur.proofs, (proof) => proof.identity)
-            const linkedTwitterNames = proofs.map((x) => x.identity)
-            return {
-                nickname: proofs[0].identity,
-                identifier: ProfileIdentifier.of('twitter.com', proofs[0].identity).unwrap(),
-                walletAddress: type === NextIDPlatform.Ethereum ? value : undefined,
-                fromNextID: true,
-                linkedTwitterNames,
-                linkedPersona: ECKeyIdentifier.fromHexPublicKeyK256(binding.persona).unwrap(),
-            }
-        })
+    return bindings.map((binding) => {
+        let proofs = binding.proofs.filter((x) => x.platform === NextIDPlatform.Twitter)
+        proofs = uniqBy(binding.proofs, (proof) => proof.identity)
+        const linkedTwitterNames = proofs.map((x) => x.identity)
+        return {
+            nickname: proofs[0].identity,
+            identifier: ProfileIdentifier.of('twitter.com', proofs[0].identity).unwrap(),
+            walletAddress: type === NextIDPlatform.Ethereum ? value : undefined,
+            fromNextID: true,
+            linkedTwitterNames,
+            linkedPersona: ECKeyIdentifier.fromHexPublicKeyK256(binding.persona).unwrap(),
+        }
+    })
 }
