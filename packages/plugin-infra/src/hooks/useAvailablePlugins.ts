a4014886b9d794283e89d5062f860105db0896b6
lelenei <zwpsky@gmail.com>
2022-05-19 22:18:31 +0800

    refactor: remove unused code

--- packages/plugin-infra/src/hooks/useAvailablePlugins.ts
+++ packages/plugin-infra/src/hooks/useAvailablePlugins.ts
@@ -1,20 +1,7 @@
-import { useMemo } from 'react'
 import type { Plugin } from '../types'
-import { useChainId, useCurrentWeb3NetworkPluginID } from '../web3'
-import type { NetworkPluginID } from '../web3-types'
 
 type HasRequirement = { enableRequirement: Plugin.Shared.Definition['enableRequirement'] }
 
-function checkPluginAvailable(plugin: HasRequirement, pluginId: NetworkPluginID, chainId: number) {
-    /* don't check web3 at timeline */
-    return true
-}
-
 export function useAvailablePlugins<T extends HasRequirement>(plugins: T[]) {
-    const networkPluginId = useCurrentWeb3NetworkPluginID()
-    const chainId = useChainId(networkPluginId)
-    return useMemo(
-        () => plugins.filter((plugin) => checkPluginAvailable(plugin, networkPluginId, chainId)),
-        [plugins, networkPluginId, chainId],
-    )
+    return plugins
 }
