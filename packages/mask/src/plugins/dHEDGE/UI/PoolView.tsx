017b859fa331d5acc4e7eba7fd5e8e6cd5f6aee1
lelenei <zwpsky@gmail.com>
2022-05-23 09:25:35 +0800

    fix: remove plugin border

--- packages/mask/src/plugins/dHEDGE/UI/PoolView.tsx
+++ packages/mask/src/plugins/dHEDGE/UI/PoolView.tsx
@@ -14,7 +14,6 @@ const useStyles = makeStyles()((theme) => ({
     root: {
         width: '100%',
         boxShadow: 'none',
-        border: `solid 1px ${theme.palette.divider}`,
         padding: 0,
     },
     message: {
