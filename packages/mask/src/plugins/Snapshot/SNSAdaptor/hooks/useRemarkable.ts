2be46fc5355c1d5cfab578ef3a31a209358e7521
zhouhanseng <z308114274@gmail.com>
2022-05-24 16:02:37 +0800

    fix: markdown ipfs parser

--- packages/mask/src/plugins/Snapshot/SNSAdaptor/hooks/useRemarkable.ts
+++ packages/mask/src/plugins/Snapshot/SNSAdaptor/hooks/useRemarkable.ts
@@ -1,9 +1,23 @@
 import { useMemo } from 'react'
 import { Remarkable } from 'remarkable'
+import { resolveIPFSLinkFromURL } from '@masknet/web3-shared-evm'
 
 export function useRemarkable(md: string) {
     return useMemo(() => {
         const remarkable = new Remarkable()
+        const defaultImageRender = remarkable.renderer.rules.image
+        remarkable.use(() => {
+            remarkable.renderer.rules.image = function (tokens: Remarkable.ImageToken[], idx: number, ...args: []) {
+                return defaultImageRender(
+                    tokens.map((token) => ({
+                        ...token,
+                        src: resolveIPFSLinkFromURL(token.src),
+                    })),
+                    idx,
+                    ...args,
+                )
+            }
+        })
         return remarkable.render(md)
     }, [md])
 }
