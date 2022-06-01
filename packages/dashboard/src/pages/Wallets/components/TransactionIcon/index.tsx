e451df8e4d293c6533aa12311b985ebf2dea4ad3
Lantt <laanfor@gmail.com>
2022-06-01 16:45:37 +0800

    fix: refactor error in dashboard (#6382)
    * fix: html tag wrong usage
    
    * feat: should loading owner non-fungible token after selected contact in dashboard
    
    * feat: support dashboard switch network
    
    * feat: update chain id type
    
    * feat: improve dashboard asset history transaction type

--- packages/dashboard/src/pages/Wallets/components/TransactionIcon/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/TransactionIcon/index.tsx
@@ -36,13 +36,18 @@ export interface TransactionIconProps {
 }
 
 export const TransactionIcon = memo<TransactionIconProps>(({ address, failed, type, transactionType }) => {
-    const { HAPPY_RED_PACKET_ADDRESS_V1, HAPPY_RED_PACKET_ADDRESS_V2, HAPPY_RED_PACKET_ADDRESS_V3 } =
-        useRedPacketConstants()
+    const {
+        HAPPY_RED_PACKET_ADDRESS_V1,
+        HAPPY_RED_PACKET_ADDRESS_V2,
+        HAPPY_RED_PACKET_ADDRESS_V3,
+        HAPPY_RED_PACKET_ADDRESS_V4,
+    } = useRedPacketConstants()
 
     const isRedPacket =
         isSameAddress(HAPPY_RED_PACKET_ADDRESS_V1, address) ||
         isSameAddress(HAPPY_RED_PACKET_ADDRESS_V2, address) ||
-        isSameAddress(HAPPY_RED_PACKET_ADDRESS_V3, address)
+        isSameAddress(HAPPY_RED_PACKET_ADDRESS_V3, address) ||
+        isSameAddress(HAPPY_RED_PACKET_ADDRESS_V4, address)
 
     return (
         <TransactionIconUI transactionType={transactionType} isRedPacket={isRedPacket} isFailed={failed} type={type} />
@@ -60,6 +65,7 @@ export const TransactionIconUI = memo<TransactionIconUIProps>(({ isFailed, isRed
     const { classes } = useStyles()
     const icon = useMemo(() => {
         if (isFailed) return <CloseIcon style={{ stroke: MaskColorVar.redMain }} className={classes.icon} />
+        if (isRedPacket) return <RedPacketIcon className={classes.icon} />
 
         switch (type) {
             case TransactionType.SEND:
@@ -69,6 +75,8 @@ export const TransactionIconUI = memo<TransactionIconUIProps>(({ isFailed, isRed
             case TransactionType.RECEIVE:
                 return <DownloadIcon style={{ stroke: MaskColorVar.greenMain }} className={classes.icon} />
             case TransactionType.CREATE_LUCKY_DROP:
+                return <RedPacketIcon className={classes.icon} />
+            case TransactionType.CREATE_RED_PACKET:
                 return <RedPacketIcon className={classes.icon} />
             case TransactionType.FILL_POOL:
                 return <ITOIcon className={classes.icon} />
