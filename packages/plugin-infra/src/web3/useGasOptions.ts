3adf38706c9eeb536761608cafd60571d6185c9e
guanbinrui <guanbinrui@dimension.im>
2022-06-02 18:26:17 +0800

    refactor: add Indicator to pageable hooks

--- packages/plugin-infra/src/web3/useGasOptions.ts
+++ packages/plugin-infra/src/web3/useGasOptions.ts
@@ -7,7 +7,6 @@ import { useWeb3Hub } from './useWeb3Hub'
 export function useGasOptions<T extends NetworkPluginID>(pluginID?: T, options?: Web3Helper.Web3HubOptions<T>) {
     type GetGasOptions = (
         chainId: Web3Helper.Definition[T]['ChainId'],
-        options?: Web3Helper.Web3HubOptions<T>,
     ) => Promise<Record<GasOptionType, Web3Helper.Definition[T]['GasOption']>>
 
     const chainId = useChainId(pluginID)
