e6779478f79e2ff9bcf850e942858ae565bed256
UncleBill <billbill290@gmail.com>
2022-06-03 16:15:04 +0800

    fix: fixup maskbox for evm migrate (#6395)

--- packages/mask/src/plugins/MaskBox/hooks/useMaskBoxContract.ts
+++ packages/mask/src/plugins/MaskBox/hooks/useMaskBoxContract.ts
@@ -1,10 +1,12 @@
 import type { AbiItem } from 'web3-utils'
 import type { MaskBox } from '@masknet/web3-contracts/types/MaskBox'
 import MASK_BOX_ABI from '@masknet/web3-contracts/abis/MaskBox.json'
-import { ChainId, useMaskBoxConstants } from '@masknet/web3-shared-evm'
+import { useMaskBoxConstants } from '@masknet/web3-shared-evm'
 import { useContract } from '@masknet/plugin-infra/web3-evm'
+import { TargetChainIdContext } from '../contexts'
 
-export function useMaskBoxContract(chainId: ChainId) {
-    const { MASK_BOX_CONTRACT_ADDRESS } = useMaskBoxConstants()
+export function useMaskBoxContract() {
+    const { targetChainId: chainId } = TargetChainIdContext.useContainer()
+    const { MASK_BOX_CONTRACT_ADDRESS } = useMaskBoxConstants(chainId)
     return useContract<MaskBox>(chainId, MASK_BOX_CONTRACT_ADDRESS, MASK_BOX_ABI as AbiItem[])
 }
