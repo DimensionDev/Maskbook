981b0353ae3e771f731bea26c2ad581371eb98f6
Mariia Synoverska <32291396+mariiaSynoverska@users.noreply.github.com>
2022-06-03 10:32:21 +0100

    Update packages/mask/src/plugins/Referral/types.ts
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>

--- packages/mask/src/plugins/Referral/types.ts
+++ packages/mask/src/plugins/Referral/types.ts
@@ -64,7 +64,7 @@ export interface DepositDialogInterface {
 }
 
 export enum TransactionStatus {
-    CONFIRMATION = 'Confirmation',
+    CONFIRMATION = 'CONFIRMATION',
     CONFIRMED = 'CONFIRMED',
     FAILED = 'FAILED',
 }
