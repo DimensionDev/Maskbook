42032705baa18ef7508c142a8f6535feab362740
Lantt <laanfor@gmail.com>
2022-06-09 11:56:09 +0800

    feat: restore add collectible dialog

--- packages/dashboard/src/pages/Wallets/components/AddCollectibleDialog/index.tsx
+++ packages/dashboard/src/pages/Wallets/components/AddCollectibleDialog/index.tsx
@@ -1,13 +1,19 @@
-import { FormEvent, memo, useEffect, useState } from 'react'
+import { FormEvent, memo, useCallback, useEffect, useState } from 'react'
 import { MaskDialog, MaskTextField } from '@masknet/theme'
 import { Box, Button, DialogActions, DialogContent } from '@mui/material'
-import { NetworkPluginID } from '@masknet/web3-shared-base'
+import { isSameAddress, NetworkPluginID } from '@masknet/web3-shared-base'
 import { EthereumAddress } from 'wallet.ts'
 import { useDashboardI18N } from '../../../../locales'
 import { z } from 'zod'
 import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useWeb3Connection, useWallet, useChainId } from '@masknet/plugin-infra/web3'
+import {
+    useWeb3Connection,
+    useChainId,
+    useNonFungibleTokenContract,
+    useAccount,
+    useWeb3Hub,
+} from '@masknet/plugin-infra/web3'
 
 export interface AddCollectibleDialogProps {
     open: boolean
@@ -25,48 +31,49 @@ enum FormErrorType {
 }
 
 export const AddCollectibleDialog = memo<AddCollectibleDialogProps>(({ open, onClose }) => {
-    const wallet = useWallet(NetworkPluginID.PLUGIN_EVM)
+    const wallet = useAccount(NetworkPluginID.PLUGIN_EVM)
     const [address, setAddress] = useState('')
     const [tokenId, setTokenId] = useState('')
     const connection = useWeb3Connection(NetworkPluginID.PLUGIN_EVM)
     const chainId = useChainId(NetworkPluginID.PLUGIN_EVM)
+    const hub = useWeb3Hub(NetworkPluginID.PLUGIN_EVM)
 
-    return null
+    const { value, loading, error, retry } = useNonFungibleTokenContract(NetworkPluginID.PLUGIN_EVM, address, tokenId)
 
-    // const [tokenId, setTokenId, erc721TokenDetailedCallback] = useERC721TokenDetailedCallback(contractDetailed)
+    const onSubmit = useCallback(async () => {
+        if (loading || !wallet || !value || !hub?.getNonFungibleAsset) return
 
-    // const onSubmit = useCallback(async () => {
-    //     if (contractDetailLoading || !wallet) return
+        // const tokenInDB = await PluginServices.Wallet.getToken(SchemaType.ERC721, address, tokenId)
+        // if (tokenInDB)
+        if (!value) throw new Error(FormErrorType.Added)
 
-    //     const tokenInDB = await PluginServices.Wallet.getToken(SchemaType.ERC721, address, tokenId)
-    //     if (tokenInDB) throw new Error(FormErrorType.Added)
+        const tokenDetailed = await hub?.getNonFungibleAsset?.(address ?? '', tokenId, {
+            chainId,
+        })
 
-    //     const tokenDetailed = await connection?.getNonFungibleToken(address ?? '', tokenId, {
-    //         chainId,
-    //     })
+        if (
+            (tokenDetailed && !isSameAddress(tokenDetailed?.owner?.address, wallet)) ||
+            !tokenDetailed ||
+            !tokenDetailed.owner
+        ) {
+            throw new Error(FormErrorType.NotExist)
+        } else {
+            console.log(tokenDetailed)
+            // await PluginServices.Wallet.addToken(tokenDetailed)
+            onClose()
+        }
+    }, [wallet, address, tokenId, value, loading, hub?.getNonFungibleAsset])
 
-    //     if (
-    //         (tokenDetailed && !isSameAddress(tokenDetailed.info.owner, wallet.address)) ||
-    //         !tokenDetailed ||
-    //         !tokenDetailed.info.owner
-    //     ) {
-    //         throw new Error(FormErrorType.NotExist)
-    //     } else {
-    //         await PluginServices.Wallet.addToken(tokenDetailed)
-    //         onClose()
-    //     }
-    // }, [contractDetailLoading, wallet, address, tokenId, erc721TokenDetailedCallback])
-
-    // return (
-    //     <AddCollectibleDialogUI
-    //         open={open}
-    //         onClose={onClose}
-    //         address={address}
-    //         onAddressChange={setAddress}
-    //         onTokenIdChange={setTokenId}
-    //         onSubmit={onSubmit}
-    //     />
-    // )
+    return (
+        <AddCollectibleDialogUI
+            open={open}
+            onClose={onClose}
+            address={address}
+            onAddressChange={setAddress}
+            onTokenIdChange={setTokenId}
+            onSubmit={onSubmit}
+        />
+    )
 })
 
 export interface AddCollectibleDialogUIProps {
