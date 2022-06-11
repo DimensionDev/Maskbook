3665e6c9a5f019fa0086a49a4a3ff1334c33db4c
billy <Billy@mask.io>
2022-06-11 18:21:39 +0800

    fix: temp ban flow && fix solana domain name error bug

--- packages/plugins/Solana/src/apis/getDomainName.ts
+++ packages/plugins/Solana/src/apis/getDomainName.ts
@@ -17,8 +17,12 @@ const connection = (chainId: ChainId) => {
 
 export async function lookup(chainId: ChainId, name: string) {
     const { domainKey } = await getKey(name.replace('.sol', ''))
-    const registry = await NameRegistryState.retrieve(connection(chainId), domainKey)
-    return registry.owner.toBase58()
+    try {
+        const registry = await NameRegistryState.retrieve(connection(chainId), domainKey)
+        return registry.owner.toBase58()
+    } catch (error) {
+        return
+    }
 }
 
 export async function reverse(chainId: ChainId, owner: string) {
