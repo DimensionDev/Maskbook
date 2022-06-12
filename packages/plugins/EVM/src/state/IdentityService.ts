3f5018118e3714433e18012b14fd03c447d6b90b
lelenei <zwpsky@gmail.com>
2022-06-12 09:44:10 +0800

    fix: get kv address by user id

--- packages/plugins/EVM/src/state/IdentityService.ts
+++ packages/plugins/EVM/src/state/IdentityService.ts
@@ -4,7 +4,7 @@ import { NetworkPluginID, SocialIdentity, SocialAddress, SocialAddressType } fro
 import { createPayload, createWeb3Provider, isValidAddress, isZeroAddress } from '@masknet/web3-shared-evm'
 import { IdentityServiceState } from '@masknet/plugin-infra/web3'
 import type { RequestArguments } from 'web3-core'
-import { RSS3, KeyValue } from '@masknet/web3-providers'
+import { KeyValue, RSS3 } from '@masknet/web3-providers'
 import { getSiteType } from '@masknet/shared-base'
 
 const ENS_RE = /\S{1,256}\.(eth|kred|xyz|luxe)\b/
@@ -56,10 +56,13 @@ export class IdentityService extends IdentityServiceState {
         const allSettled = await Promise.allSettled([
             web3.eth.ens.getAddress(ethereumName),
             RSS3.getNameInfo(RSS3Id).then((x) => x?.address ?? ''),
-            KeyValue.createJSON_Storage<Record<NetworkPluginID, string>>(`com.maskbook.user_${getSiteType()}`)
-                .get(identifier?.userId ?? '$unknown')
-                .then((x) => x?.[NetworkPluginID.PLUGIN_EVM] ?? ''),
         ])
+
+        const addressKVStorage = await KeyValue.createJSON_Storage<
+            Record<NetworkPluginID, { address: string; networkPluginID: NetworkPluginID }>
+        >(`com.maskbook.user_${getSiteType()}`)
+            .get(identifier?.userId ?? '$unknown')
+            .then((x) => x?.[NetworkPluginID.PLUGIN_EVM])
 
         const getSettledAddress = (result: PromiseSettledResult<string>) => {
             return result.status === 'fulfilled' ? result.value : ''
@@ -67,7 +70,7 @@ export class IdentityService extends IdentityServiceState {
 
         const addressENS = getSettledAddress(allSettled[0])
         const addressRSS3 = getSettledAddress(allSettled[1])
-        const addressKV = getSettledAddress(allSettled[2])
+        const addressKV = addressKVStorage?.address ?? ''
 
         return [
             isValidSocialAddress(address)
