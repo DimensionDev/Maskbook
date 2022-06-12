c2f81b77f450189ee37b09d1daf0a205e1562260
guanbinrui <guanbinrui@dimension.im>
2022-06-12 19:25:56 +0800

    fix: alway force to connect a wallet

--- packages/plugins/EVM/src/state/Connection/provider.ts
+++ packages/plugins/EVM/src/state/Connection/provider.ts
@@ -1,9 +1,9 @@
 import { ProviderType } from '@masknet/web3-shared-evm'
+import { BaseProvider } from './providers/Base'
 import { CustomNetworkProvider } from './providers/CustomNetwork'
 import { MaskWalletProvider } from './providers/MaskWallet'
 import { MetaMaskProvider } from './providers/MetaMask'
 import WalletConnectProvider from './providers/WalletConnect'
-import type { BaseProvider } from './providers/Base'
 import { WalletLinkProvider } from './providers/WalletLink'
 import { Coin98Provider } from './providers/Coin98'
 import { MathWalletProvider } from './providers/MathWallet'
@@ -14,6 +14,7 @@ import TorusProvider from './providers/Torus'
  * Register all supported providers
  */
 export const Providers: Record<ProviderType, BaseProvider> = {
+    [ProviderType.None]: new BaseProvider(),
     [ProviderType.MaskWallet]: new MaskWalletProvider(),
     [ProviderType.MetaMask]: new MetaMaskProvider(),
     [ProviderType.WalletConnect]: new WalletConnectProvider(),
