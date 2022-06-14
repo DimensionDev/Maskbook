583bb3a38604c24fc090c62a1dd6166f4edc1eb9
zhouhanseng <z308114274@gmail.com>
2022-06-15 01:27:46 +0800

    fix: avatar

--- packages/mask/src/plugins/Avatar/Application/NFTAvatarsDialog.tsx
+++ packages/mask/src/plugins/Avatar/Application/NFTAvatarsDialog.tsx
@@ -80,7 +80,7 @@ export function NFTAvatarDialog(props: NFTAvatarsDialogProps) {
                     ? t.application_edit_profile_dialog_title()
                     : t.application_dialog_title()
             }
-            isOnBack={step === CreateNFTAvatarStep.UploadAvatar}
+            isOnBack={step !== CreateNFTAvatarStep.Persona}
             open={props.open}
             onClose={onBack}>
             <DialogContent className={classes.root}>
