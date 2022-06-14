4e9f29d4abcdc8bdb59852bfee2f9bf4ae65f574
BillyS <Billy@mask.io>
2022-06-15 04:06:43 +0800

    fix: tips add wallet (#6563)
    * fix(lucky drop): bug && ui (#6553)
    
    * fix(lucky drop): bug && ui
    
    * fix: tips && avatar
    
    * refactor: none provider
    
    * fix: avatar
    
    * fix: provider middleware
    
    * fix: provider middleware (#6562)
    
    * refactor: none provider
    
    * fix: provider middleware
    
    Co-authored-by: guanbinrui <guanbinrui@dimension.im>
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>
    
    * fix: tips add wallet
    
    Co-authored-by: Hancheng Zhou <z308114274@gmail.com>
    Co-authored-by: guanbinrui <guanbinrui@dimension.im>
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>

--- packages/mask/src/plugins/Tips/SNSAdaptor/TipsEntranceDialog.tsx
+++ packages/mask/src/plugins/Tips/SNSAdaptor/TipsEntranceDialog.tsx
@@ -248,6 +248,7 @@ export function TipsEntranceDialog({ open, onClose }: TipsEntranceDialogProps) {
         <InjectedDialog
             open={open}
             onClose={clickBack}
+            isOnBack={bodyViewStep !== BodyViewStep.Main}
             title={bodyViewStep}
             titleTail={
                 <WalletButton
