fea741ccefd33322727ffd8da64a0ddbf3df3750
nuanyang233 <nuanyang233@gmail.com>
2022-06-14 23:01:22 +0800

    fix: provider

--- packages/mask/src/components/shared/VerifyWallet/CurrentWalletBox.tsx
+++ packages/mask/src/components/shared/VerifyWallet/CurrentWalletBox.tsx
@@ -2,7 +2,6 @@ import {
     useAccount,
     useNetworkDescriptor,
     useProviderDescriptor,
-    useProviderType,
     useReverseAddress,
     useWeb3State,
 } from '@masknet/plugin-infra/web3'
@@ -105,7 +104,7 @@ export function CurrentWalletBox(props: CurrentWalletBox) {
     const { t } = useI18N()
     const { classes } = useStyles()
     const { wallet, walletName, notInPop, changeWallet } = props
-    const providerType = useProviderType(NetworkPluginID.PLUGIN_EVM)
+    const { providerType } = wallet
     const providerDescriptor = useProviderDescriptor(NetworkPluginID.PLUGIN_EVM, providerType)
     const networkDescriptor = useNetworkDescriptor(NetworkPluginID.PLUGIN_EVM)
     const frontAccount = useAccount(NetworkPluginID.PLUGIN_EVM)
@@ -133,9 +132,6 @@ export function CurrentWalletBox(props: CurrentWalletBox) {
                             <Typography className={classes.accountName}>
                                 {walletName ?? providerDescriptor?.name}
                             </Typography>
-                            {domain && Others?.formatDomainName ? (
-                                <Typography className={classes.domain}>{Others.formatDomainName(domain)}</Typography>
-                            ) : null}
                         </>
                     )}
                 </div>
