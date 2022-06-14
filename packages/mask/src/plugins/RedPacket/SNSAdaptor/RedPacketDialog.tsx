550f05665fa880b5dd52cc23cef313be6585735c
zhouhanseng <z308114274@gmail.com>
2022-06-14 23:07:35 +0800

    fix(lucky drop): bug && ui

--- packages/mask/src/plugins/RedPacket/SNSAdaptor/RedPacketDialog.tsx
+++ packages/mask/src/plugins/RedPacket/SNSAdaptor/RedPacketDialog.tsx
@@ -187,6 +187,7 @@ export default function RedPacketDialog(props: RedPacketDialogProps) {
             open={props.open}
             title={title}
             onClose={isCreateStep ? onClose : () => setStep(CreateRedPacketPageStep.NewRedPacketPage)}
+            isOnBack={!isCreateStep}
             disableTitleBorder>
             <DialogContent className={classes.dialogContent}>
                 {step === CreateRedPacketPageStep.NewRedPacketPage ? (
