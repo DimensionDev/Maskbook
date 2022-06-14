ecc26414636f06004e003c9788cde1445c000b4b
zhouhanseng <z308114274@gmail.com>
2022-06-15 01:12:19 +0800

    fix: tips && avatar

--- packages/mask/src/plugins/Avatar/Application/NFTAvatarsDialog.tsx
+++ packages/mask/src/plugins/Avatar/Application/NFTAvatarsDialog.tsx
@@ -80,6 +80,7 @@ export function NFTAvatarDialog(props: NFTAvatarsDialogProps) {
                     ? t.application_edit_profile_dialog_title()
                     : t.application_dialog_title()
             }
+            isOnBack={step === CreateNFTAvatarStep.UploadAvatar}
             open={props.open}
             onClose={onBack}>
             <DialogContent className={classes.root}>
