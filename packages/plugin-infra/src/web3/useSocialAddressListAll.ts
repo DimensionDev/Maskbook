ec6fcb5e0f618f9053f2c41640f40c77e7bc8579
unclebill <billbill290@gmail.com>
2022-06-15 22:42:02 +0800

    fix: cache result of useSocialAddressListAll

--- packages/plugin-infra/src/web3/useSocialAddressListAll.ts
+++ packages/plugin-infra/src/web3/useSocialAddressListAll.ts
@@ -1,7 +1,16 @@
-import { useAsyncRetry } from 'react-use'
 import { EMPTY_LIST } from '@masknet/shared-base'
 import { NetworkPluginID, SocialAddress, SocialIdentity } from '@masknet/web3-shared-base'
+import LRUCache from 'lru-cache'
+import { useAsyncRetry } from 'react-use'
 import { useWeb3State } from './useWeb3State'
+
+type AddressList = Array<SocialAddress<NetworkPluginID>>
+type CacheValue = Promise<Array<PromiseSettledResult<AddressList>>>
+
+const addressCache = new LRUCache<string, CacheValue>({
+    max: 500,
+    ttl: 2 * 60 * 1000,
+})
 
 /**
  * Get all social addresses under of all networks.
@@ -18,10 +27,15 @@ export function useSocialAddressListAll(
         const userId = identity?.identifier?.userId
         if (!userId || userId === '$unknown') return EMPTY_LIST
 
-        const allSettled = await Promise.allSettled<Array<SocialAddress<NetworkPluginID>>>(
-            [EVM_IdentityService, SolanaIdentityService].map((x) => x?.lookup(identity) ?? []),
-        )
+        let cached = addressCache.get(userId)
+        if (!cached) {
+            cached = Promise.allSettled<AddressList>(
+                [EVM_IdentityService, SolanaIdentityService].map((x) => x?.lookup(identity) ?? []),
+            )
+            addressCache.set(userId, cached)
+        }
+        const allSettled = await cached
         const listOfAddress = allSettled.flatMap((x) => (x.status === 'fulfilled' ? x.value : []))
         return sorter && listOfAddress.length ? listOfAddress.sort(sorter) : listOfAddress
-    }, [identity, sorter, EVM_IdentityService?.lookup, SolanaIdentityService?.lookup])
+    }, [identity?.identifier?.toText(), sorter, EVM_IdentityService?.lookup, SolanaIdentityService?.lookup])
 }
