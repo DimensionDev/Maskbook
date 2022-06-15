a1137482253145c9fd379fea7dd45114b3fe3634
unclebill <billbill290@gmail.com>
2022-06-16 01:18:35 +0800

    fix(tips): share receiver status via storage

--- packages/mask/src/plugins/Tips/components/TipButton.tsx
+++ packages/mask/src/plugins/Tips/components/TipButton.tsx
@@ -10,6 +10,7 @@ import classnames from 'classnames'
 import { uniq } from 'lodash-unified'
 import { FC, HTMLProps, MouseEventHandler, useCallback, useEffect, useMemo } from 'react'
 import { useAsyncRetry } from 'react-use'
+import { useSubscription } from 'use-subscription'
 import { MaskMessages } from '../../../../shared'
 import { useCurrentVisitingIdentity } from '../../../components/DataSource/useActivatedUI'
 import Services from '../../../extension/service'
@@ -17,6 +18,7 @@ import { activatedSocialNetworkUI } from '../../../social-network'
 import { usePublicWallets } from '../hooks/usePublicWallets'
 import { useI18N } from '../locales'
 import { PluginNextIDMessages } from '../messages'
+import { getStorage, setAddresses, setAccountVerified, setPersonaPubkey } from '../storage'
 
 interface Props extends HTMLProps<HTMLDivElement> {
     addresses?: string[]
@@ -71,24 +73,45 @@ export const TipButton: FC<Props> = ({
     const t = useI18N()
 
     const platform = activatedSocialNetworkUI.configuration.nextIDConfig?.platform as NextIDPlatform
-    const { value: receiverPersona, loading: loadingPersona } = useAsyncRetry(async () => {
+    useAsyncRetry(async () => {
         if (!receiver) return
-        return Services.Identity.queryPersonaByProfile(receiver)
+        const persona = await Services.Identity.queryPersonaByProfile(receiver)
+        if (persona) {
+            setPersonaPubkey(receiver.userId, persona.identifier.publicKeyAsHex)
+        }
     }, [receiver])
 
+    const storage = getStorage()
+
+    const personaKey = receiver?.userId
+    const accountVerifiedSubscription = useSubscription(storage.accountVerifiedMap.subscription)
+    const addressesSubscription = useSubscription(storage.addressMap.subscription)
+    const personaPubkeySubscription = useSubscription(storage.personaPubkeyMap.subscription)
+
     const pluginId = useCurrentWeb3NetworkPluginID()
-    const {
-        value: isAccountVerified,
-        loading: loadingVerifyInfo,
-        retry: retryLoadVerifyInfo,
-    } = useAsyncRetry(() => {
-        if (pluginId !== NetworkPluginID.PLUGIN_EVM) return Promise.resolve(true)
-        if (!receiverPersona?.identifier.publicKeyAsHex || !receiver?.userId) return Promise.resolve(false)
-        return NextIDProof.queryIsBound(receiverPersona.identifier.publicKeyAsHex, platform, receiver.userId, true)
-    }, [pluginId, receiverPersona?.identifier.publicKeyAsHex, platform, receiver?.userId])
+    const personaPubkey = personaKey ? personaPubkeySubscription[personaKey] : null
+    const accountVerifiedKey = useMemo(() => {
+        if (!personaPubkey || !personaKey) return null
+        return `${personaPubkey}/${platform}/${receiver.userId}`
+    }, [personaPubkey, platform, personaKey])
+    const addressKey = personaKey ? `${pluginId}/${personaKey}` : null
+    const { retry: retryLoadVerifyInfo } = useAsyncRetry(async () => {
+        if (pluginId !== NetworkPluginID.PLUGIN_EVM) return
+        if (!personaPubkey || !personaKey) return
+        const accountVerified = await NextIDProof.queryIsBound(personaPubkey, platform, receiver.userId, true)
+
+        if (accountVerifiedKey) {
+            setAccountVerified(accountVerifiedKey, accountVerified)
+        }
+    }, [accountVerifiedKey, pluginId, personaPubkey, platform, personaKey])
+    const isAccountVerified = useMemo(() => {
+        if (pluginId !== NetworkPluginID.PLUGIN_EVM) return true
+        if (!accountVerifiedKey) return false
+        return accountVerifiedSubscription[accountVerifiedKey] ?? false
+    }, [accountVerifiedSubscription, accountVerifiedKey, pluginId])
     const visitingIdentity = useCurrentVisitingIdentity()
 
-    const isVisitingUser = visitingIdentity.identifier?.userId === receiver?.userId
+    const isVisitingUser = visitingIdentity.identifier?.userId === personaKey
     const isRuntimeAvailable = useMemo(() => {
         switch (pluginId) {
             case NetworkPluginID.PLUGIN_EVM:
@@ -106,40 +129,46 @@ export const TipButton: FC<Props> = ({
     }, [])
 
     const publicWallets = usePublicWallets(receiver || undefined)
+    useEffect(() => {
+        if (publicWallets.length && addressKey) {
+            setAddresses(addressKey, publicWallets)
+        }
+    }, [publicWallets, addressKey])
+
     const { value: socialAddressList = EMPTY_LIST } = useSocialAddressListAll(visitingIdentity)
+    const storedPublicWallets = addressKey ? addressesSubscription[addressKey] ?? EMPTY_LIST : EMPTY_LIST
     const allAddresses = useMemo(() => {
         switch (pluginId) {
             case NetworkPluginID.PLUGIN_EVM:
-                return uniq([...publicWallets, ...addresses])
+                return uniq([...storedPublicWallets, ...addresses])
             case NetworkPluginID.PLUGIN_SOLANA:
                 return socialAddressList.filter((x) => x.networkSupporterPluginID === pluginId).map((x) => x.address)
         }
         return EMPTY_LIST
-    }, [pluginId, publicWallets, addresses, socialAddressList])
+    }, [pluginId, storedPublicWallets, addresses, socialAddressList])
 
-    const isChecking = loadingPersona || loadingVerifyInfo
-    const disabled = isChecking || !isAccountVerified || allAddresses.length === 0 || !isRuntimeAvailable
+    const disabled = !isAccountVerified || allAddresses.length === 0 || !isRuntimeAvailable
 
     const createTipTask: MouseEventHandler<HTMLDivElement> = useCallback(
         async (evt) => {
             evt.stopPropagation()
             evt.preventDefault()
             if (disabled) return
-            if (!allAddresses.length || !receiver?.userId) return
+            if (!allAddresses.length || !personaKey) return
             PluginNextIDMessages.tipTask.sendToLocal({
                 recipientSnsId: receiver.userId,
                 addresses: allAddresses,
             })
         },
-        [disabled, allAddresses, receiver?.userId],
+        [disabled, allAddresses, personaKey],
     )
 
     useEffect(() => {
         PluginNextIDMessages.tipTaskUpdate.sendToLocal({
-            recipientSnsId: receiver?.userId,
+            recipientSnsId: personaKey,
             addresses: allAddresses,
         })
-    }, [receiver?.userId, allAddresses])
+    }, [personaKey, allAddresses])
 
     const dom = (
         <div
@@ -156,7 +185,7 @@ export const TipButton: FC<Props> = ({
         return (
             <ShadowRootTooltip
                 classes={{ tooltip: classes.tooltip }}
-                title={isChecking ? '' : t.tip_wallets_missed()}
+                title={t.tip_wallets_missed()}
                 placement="bottom"
                 arrow={false}
                 {...tooltipProps}>
--- packages/mask/src/plugins/Tips/storage/index.ts
+++ packages/mask/src/plugins/Tips/storage/index.ts
@@ -5,11 +5,17 @@ import type { ChainId, SchemaType } from '@masknet/web3-shared-evm'
 
 interface StorageValue {
     addedTokens: Array<NonFungibleToken<ChainId, SchemaType>>
+    addressMap: Record<string, string[]>
+    accountVerifiedMap: Record<string, boolean>
+    personaPubkeyMap: Record<string, string>
 }
 
 export const storageDefaultValue = {
     publicKey: null as null | string,
     addedTokens: [],
+    addressMap: {},
+    accountVerifiedMap: {},
+    personaPubkeyMap: {},
 }
 
 let storage: ScopedStorage<StorageValue> = null!
@@ -29,6 +35,29 @@ export function getTokens() {
 export function storeToken(token: NonFungibleToken<ChainId, SchemaType>) {
     const tokens = [token, ...getTokens()]
     storage.storage.addedTokens.setValue(tokens)
+}
+
+export function getAddresses(key: string) {
+    return storage.storage.addressMap.value[key] ?? []
+}
+export function setAddresses(key: string, addresses: string[]) {
+    storage.storage.addressMap.setValue({
+        ...storage.storage.addressMap.value,
+        [key]: addresses,
+    })
+}
+export function setAccountVerified(key: string, accountVerified: boolean) {
+    storage.storage.accountVerifiedMap.setValue({
+        ...storage.storage.accountVerifiedMap.value,
+        [key]: accountVerified,
+    })
+}
+
+export function setPersonaPubkey(key: string, pubkey: string) {
+    storage.storage.personaPubkeyMap.setValue({
+        ...storage.storage.personaPubkeyMap.value,
+        [key]: pubkey,
+    })
 }
 
 export function deleteToken(address: string, tokenId: string) {
