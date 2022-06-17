32843c39025c4e6dce161803183f393fc38acbda
Jack Works <jackworks@protonmail.com>
2022-06-17 13:58:07 +0800

    chore: upgrade infra deps (#6606)
    * chore: upgrade infra deps
    
    * chore: revert core-js

--- packages/mask-sdk/server/index.ts
+++ packages/mask-sdk/server/index.ts
@@ -1,8 +1,8 @@
 import { BridgeAPI, UserScriptAPI, createMaskSDKChannel, serializer } from '../shared'
-import { AsyncCall, _AsyncVersionOf } from 'async-call-rpc'
+import { AsyncCall, AsyncVersionOf } from 'async-call-rpc'
 
 export type { BridgeAPI, UserScriptAPI, InitInformation } from '../shared'
-export function createMaskSDKServer(api: BridgeAPI, signal?: AbortSignal): _AsyncVersionOf<UserScriptAPI> {
+export function createMaskSDKServer(api: BridgeAPI, signal?: AbortSignal): AsyncVersionOf<UserScriptAPI> {
     // TODO: support AbortSignal
     return AsyncCall<UserScriptAPI>(api, {
         serializer,
