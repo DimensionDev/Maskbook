f0484ff94787aa63fa3c34db4e92beb1062d672b
SebastienLF <slf.pro@gmail.com>
2022-06-20 07:50:21 +0200

    refactor: memoize markets and leagues list

--- packages/mask/src/plugins/Predict/Azuro/hooks/useLeagueList.ts
+++ packages/mask/src/plugins/Predict/Azuro/hooks/useLeagueList.ts
@@ -1,13 +1,16 @@
 import type { AzuroGame } from '@azuro-protocol/sdk'
 import { uniqBy } from 'lodash-unified'
+import { useMemo } from 'react'
 
 export function useLeagueList(games: AzuroGame[] | undefined) {
-    const leagues = games?.map((game) => {
-        return {
-            id: game.league,
-            label: `${game.country}: ${game.league}`,
-        }
-    })
+    const leagues = useMemo(() => {
+        return games?.map((game) => {
+            return {
+                id: game.league,
+                label: `${game.country}: ${game.league}`,
+            }
+        })
+    }, [games])
 
     return uniqBy(leagues, 'id').sort((a, b) => a.id.localeCompare(b.id))
 }
--- packages/mask/src/plugins/Predict/Azuro/hooks/useMarketList.ts
+++ packages/mask/src/plugins/Predict/Azuro/hooks/useMarketList.ts
@@ -1,9 +1,12 @@
 import type { AzuroGame } from '@azuro-protocol/sdk'
 import { uniqBy } from 'lodash-unified'
+import { useMemo } from 'react'
 import { marketRegistry } from '../helpers'
 
 export function useMarketList(games: AzuroGame[] | undefined) {
-    const markets = games?.map((game) => ({ id: game.marketRegistryId, label: marketRegistry[game.marketRegistryId] }))
+    const markets = useMemo(() => {
+        return games?.map((game) => ({ id: game.marketRegistryId, label: marketRegistry[game.marketRegistryId] }))
+    }, [games])
 
     return uniqBy(markets, 'id')
         .concat({ id: 0, label: 'All' })
