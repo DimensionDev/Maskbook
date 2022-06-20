3dbc7029b1011e3ec9e989d9069ccf27f5172c64
SebastienLF <slf.pro@gmail.com>
2022-06-20 09:17:08 +0200

    refactor: use bignumber to calculate actual odds

--- packages/mask/src/plugins/Predict/Azuro/hooks/useActualRate.ts
+++ packages/mask/src/plugins/Predict/Azuro/hooks/useActualRate.ts
@@ -1,14 +1,16 @@
 import { useAsync } from 'react-use'
 import { calculateActualOdds } from '@azuro-protocol/sdk'
 import type { Odds } from '../types'
+import BigNumber from 'bignumber.js'
 
 export function useActualRate(condition: Odds, amount: string) {
     return useAsync(async () => {
         if (!condition || !amount) return
+        const betAmount = new BigNumber(amount)
         return calculateActualOdds({
             conditionId: condition.conditionId,
             outcomeId: condition.outcomeId,
-            betAmount: Number(amount),
+            betAmount: betAmount.toNumber(),
         })
     }, [amount, condition])
 }
