5e9f3e274c872184e9cf24477642f35da77d9183
zhouhanseng <z308114274@gmail.com>
2022-06-21 14:13:53 +0800

    refactor: move useTargetChainIdContext to EVM hooks

--- packages/plugin-infra/src/web3/EVM/useTargetChainIdContext.ts
+++ packages/plugin-infra/src/web3/EVM/useTargetChainIdContext.ts
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import { NetworkPluginID } from '@masknet/web3-shared-base'
-import { ChainId } from '@masknet/web3-shared-evm'
+import type { ChainId } from '@masknet/web3-shared-evm'
 import { createContainer } from 'unstated-next'
 import { useChainId } from '../useChainId'
 
-export function useTargetChainIdContext(_targetChainId: ChainId = ChainId.Mainnet) {
+export function useTargetChainIdContext(_chainId?: ChainId) {
     const chainId = useChainId(NetworkPluginID.PLUGIN_EVM)
-    const [targetChainId, setTargetChainId] = useState<ChainId>(_targetChainId ?? chainId)
+    const [targetChainId, setTargetChainId] = useState<ChainId>(_chainId ?? chainId)
 
     return {
         targetChainId,
