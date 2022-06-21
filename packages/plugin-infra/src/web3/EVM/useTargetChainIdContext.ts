24d485ce456aa0530d835817f350eb3b78ca8aee
zhouhanseng <z308114274@gmail.com>
2022-06-21 11:05:24 +0800

    refactor: for maskbox

--- packages/plugin-infra/src/web3/EVM/useTargetChainIdContext.ts
+++ packages/plugin-infra/src/web3/EVM/useTargetChainIdContext.ts
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import { NetworkPluginID } from '@masknet/web3-shared-base'
-import type { ChainId } from '@masknet/web3-shared-evm'
+import { ChainId } from '@masknet/web3-shared-evm'
 import { createContainer } from 'unstated-next'
 import { useChainId } from '../useChainId'
 
-export function useTargetChainIdContext() {
+export function useTargetChainIdContext(_targetChainId: ChainId = ChainId.Mainnet) {
     const chainId = useChainId(NetworkPluginID.PLUGIN_EVM)
-    const [targetChainId, setTargetChainId] = useState<ChainId>(chainId)
+    const [targetChainId, setTargetChainId] = useState<ChainId>(_targetChainId ?? chainId)
 
     return {
         targetChainId,
