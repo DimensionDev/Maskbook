3241c4dfcd2acc9ebb164f068587b1b6ea123a89
unclebill <billbill290@gmail.com>
2022-06-23 21:30:31 +0800

    fix: remove tsx icons

--- packages/icons/utils/index.tsx
+++ packages/icons/utils/index.tsx
@@ -1,6 +1,5 @@
 import { useTheme, SvgIcon, SvgIconProps, Theme } from '@mui/material'
-import { forwardRef, memo, ForwardedRef, useContext } from 'react'
-import { MaskIconPaletteContext } from './MaskIconPaletteContext'
+import { forwardRef, memo, ForwardedRef } from 'react'
 
 /** @internal */
 export type Size = [width: number | undefined, height: number | undefined]
@@ -34,39 +33,4 @@ export function createIcon(name: string, svg: SvgIconRaw, viewBox?: string, defa
               }
     Icon.displayName = `Icon (${name})`
     return memo(forwardRef(Icon)) as unknown as typeof SvgIcon
-}
-
-/**
- * Create an icon from svg fragment
- * @internal
- * @param name Name of the Icon
- * @param light SVG content when the theme is light theme. Do not include <svg> tag
- * @param dark SVG content when the theme is light theme. Do not include <svg> tag
- * @param dim SVG content when the theme is dark, and the MaskIconPaletteContext is "dim". Do not include <svg> tag
- * @param viewBox The viewbox
- * @param defaultSize Only use this when the icon is not square. Unit: px
- * @returns A component that same type as icons from @mui/icons-material
- */
-export function createPaletteAwareIcon(
-    name: string,
-    light: JSX.Element,
-    dark: JSX.Element,
-    dim: JSX.Element | undefined,
-    viewBox?: string,
-    defaultSize?: Size,
-) {
-    return createIcon(
-        name,
-        (theme) => {
-            const palette = useContext(MaskIconPaletteContext)
-            const isDim = palette === 'dim'
-            if (theme.palette.mode === 'dark') {
-                if (isDim && dim) return dim
-                return dark
-            }
-            return light
-        },
-        viewBox,
-        defaultSize,
-    )
 }
