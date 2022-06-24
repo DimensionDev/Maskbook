e47d0f750e6ea00048344c7e06d2da41a1462688
guanbinrui <guanbinrui@dimension.im>
2022-06-24 12:25:04 +0800

    chore: scaffloding

--- packages/shared/src/contexts/common/index.tsx
+++ packages/shared/src/contexts/common/index.tsx
@@ -1,14 +1,18 @@
 import type { FC, PropsWithChildren } from 'react'
 import { ConfirmProvider } from './Confirm'
 import { SelectFungibleTokenProvider } from './SelectFungibleTokenDialog'
+import { SelectGasSettingsProvider } from './SelectGasSettingsDialog'
 
 export * from './Confirm'
 export * from './SelectFungibleTokenDialog'
+export * from './SelectGasSettingsDialog'
 
 export const CommonUIProvider: FC<PropsWithChildren<{}>> = ({ children }) => {
     return (
         <ConfirmProvider>
-            <SelectFungibleTokenProvider>{children}</SelectFungibleTokenProvider>
+            <SelectFungibleTokenProvider>
+                <SelectGasSettingsProvider>{children}</SelectGasSettingsProvider>
+            </SelectFungibleTokenProvider>
         </ConfirmProvider>
     )
 }
