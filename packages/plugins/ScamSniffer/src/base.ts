c390f28cb261df9a8bc0a7b4417169cc1bd863c7
fun <lljxx1@gmail.com>
2022-06-24 16:10:41 +0800

    chore: update target

--- packages/plugins/ScamSniffer/src/base.ts
+++ packages/plugins/ScamSniffer/src/base.ts
@@ -10,7 +10,7 @@ export const base: Plugin.Shared.Definition = {
     enableRequirement: {
         architecture: { app: false, web: true },
         networks: { type: 'opt-out', networks: {} },
-        target: 'insider',
+        target: 'stable',
     },
     experimentalMark: true,
     i18n: languages,
