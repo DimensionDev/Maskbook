ecaa73e77409d69a37b24c68f9265d88a654a836
Jack Works <jackworks@protonmail.com>
2022-06-25 23:58:11 +0800

    fix: build error

--- packages/injected-script/shared/index.ts
+++ packages/injected-script/shared/index.ts
@@ -1,5 +1,3 @@
-import type { RequestArguments } from 'web3-core'
-
 export const CustomEventId = 'c8a6c18e-f6a3-472a-adf3-5335deb80db6'
 export interface InternalEvents {
     /** Simulate a paste event on the activeElement */
@@ -40,6 +38,11 @@ export interface InternalEvents {
     rejectPromise: [req_id: number, error: unknown]
 }
 
+export interface RequestArguments {
+    method: string
+    params?: any
+    [key: string]: any
+}
 export interface EthereumProvider {
     /** Wait for ethereum object appears. */
     untilAvailable(): Promise<true>
