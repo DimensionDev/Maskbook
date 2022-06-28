517f882890c12d4edd6d8985a9315042a6c7e5e2
javaLuo <376693576@qq.com>
2022-06-28 17:02:55 +0800

    perf: update arrow control

--- packages/mask/src/plugins/Game/SNSAdaptor/GameList.tsx
+++ packages/mask/src/plugins/Game/SNSAdaptor/GameList.tsx
@@ -1,15 +1,18 @@
+import { useEffect, useState } from 'react'
 import { Button, List, Typography } from '@mui/material'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import { makeStyles, useStylesExtends } from '@masknet/theme'
 import { useI18N } from '../../../utils'
 import { useGameList } from '../hook'
 import type { GameInfo } from '../types'
+import classNames from 'classnames'
 
 const useStyles = makeStyles()(() => ({
     walletBar: {},
     title: {
         fontSize: '20px',
         fontWeight: '500',
-        padding: 0,
+        paddingBottom: '8px',
     },
     gameList: {
         padding: 0,
@@ -29,7 +32,8 @@ const useStyles = makeStyles()(() => ({
     },
     info: {
         flex: 'auto',
-        margin: '0 10px 0 20px',
+        margin: '0 4px 0 20px',
+        maxWidth: 'calc(100% - 214px)',
     },
     infoTitle: {
         fontSize: '18px',
@@ -40,6 +44,34 @@ const useStyles = makeStyles()(() => ({
         fontSize: '14px',
         color: '#888',
         marginTop: '10px',
+        textOverflow: 'ellipsis',
+        '-webkit-line-clamp': '2',
+        '-webkit-box-orient': 'vertical',
+        display: '-webkit-box',
+        maxHeight: '40px',
+        lineHeight: '20px',
+        overflow: 'hidden',
+        flex: 'auto',
+    },
+    isOpen: {
+        maxHeight: '200px',
+        '-webkit-line-clamp': 'inherit',
+        '-webkit-box-orient': 'inherit',
+        textOverflow: 'inherit',
+    },
+    isTurn: {
+        transform: 'rotate(180deg)',
+    },
+    introductionRow: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+
+    arrowBtn: {
+        flex: 'none',
+        cursor: 'pointer',
+        padding: '2px 0',
+        transition: '200ms',
     },
     rank: {
         backgroundColor: '#FAE4D5',
@@ -72,17 +104,44 @@ const GameList = (props: Props) => {
     const classes = useStylesExtends(useStyles(), {})
     const gameList = useGameList()
 
+    const [descTypes, setDescTypes] = useState<boolean[]>([])
+    useEffect(() => {
+        if (!gameList?.length) {
+            setDescTypes([])
+        } else {
+            setDescTypes(gameList.map(() => false))
+        }
+    }, [gameList?.length])
+
+    const toggleDescType = (index: number) => {
+        descTypes[index] = !descTypes[index]
+        setDescTypes([...descTypes])
+    }
+
     return (
         <List className={classes.walletBar}>
             <Typography className={classes.title}>{t('plugin_game_list_title')}</Typography>
             <ul className={classes.gameList}>
                 {gameList
-                    ? gameList.map((game: any) => (
+                    ? gameList.map((game: any, index: number) => (
                           <li className={classes.gameBar} key={game.id}>
                               <img className={classes.logo} src={game.image} alt="" />
                               <div className={classes.info}>
                                   <Typography className={classes.infoTitle}>{game.name}</Typography>
-                                  <Typography className={classes.introduction}>{game.description}</Typography>
+                                  <div className={classes.introductionRow}>
+                                      <Typography
+                                          className={classNames(classes.introduction, {
+                                              [classes.isOpen]: descTypes[index],
+                                          })}>
+                                          {game.description}
+                                      </Typography>
+                                      <ArrowDropDownIcon
+                                          className={classNames(classes.arrowBtn, {
+                                              [classes.isTurn]: descTypes[index],
+                                          })}
+                                          onClick={() => toggleDescType(index)}
+                                      />
+                                  </div>
                                   <Typography className={classes.rank}>
                                       {t('plugin_game_list_rank')} {game.rank}
                                   </Typography>
