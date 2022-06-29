34a78e8b2e20e1cc459f8f6a55e8a5b6e42ea7cf
Lantt <laanfor@gmail.com>
2022-06-29 15:25:24 +0800

    refactor: remove unused code

--- packages/mask/src/plugins/Trader/SNSAdaptor/trending/PluginHeader.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trending/PluginHeader.tsx
@@ -1,14 +1,10 @@
 import { TrendingIcon } from '@masknet/icons'
-import { useActivatedPlugin, PluginId } from '@masknet/plugin-infra/dom'
 import { Stack, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 
 interface PluginHeaderProps extends React.PropsWithChildren<{}> {}
 
 export const PluginHeader = ({ children }: PluginHeaderProps) => {
-    // TODO: should use definition info for icon
-    const trendingDefinition = useActivatedPlugin(PluginId.Trader, 'any')
-
     return (
         <Stack flexDirection="row" justifyContent="space-between" alignItems="center">
             <Stack flexDirection="row" justifyContent="space-between" gap={0.5} alignItems="center">
