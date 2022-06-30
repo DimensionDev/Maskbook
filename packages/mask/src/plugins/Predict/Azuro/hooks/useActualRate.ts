6b553afc8e494de6da020d0e3553f383856b3b6e
SebastienLF <slf.pro@gmail.com>
2022-06-30 19:08:39 +0300

    fix: update function signature for rate calc

--- packages/mask/src/plugins/Predict/Azuro/hooks/useActualRate.ts
+++ packages/mask/src/plugins/Predict/Azuro/hooks/useActualRate.ts
@@ -1,16 +1,25 @@
 import { useAsync } from 'react-use'
-import { calculateActualOdds } from '@azuro-protocol/sdk'
 import type { Odds } from '../types'
 import BigNumber from 'bignumber.js'
+import { calculateActualRate } from '../api'
+import { useChainId } from '@masknet/plugin-infra/web3'
+import { NetworkPluginID } from '@masknet/web3-shared-base'
 
-export function useActualRate(condition: Odds, amount: string) {
+export function useActualRate(condition: Odds | null, amount: string) {
+    const chainId = useChainId(NetworkPluginID.PLUGIN_EVM)
     return useAsync(async () => {
         if (!condition || !amount) return
-        const betAmount = new BigNumber(amount)
-        return calculateActualOdds({
-            conditionId: condition.conditionId,
-            outcomeId: condition.outcomeId,
-            betAmount: betAmount.toNumber(),
+        const conditionId = condition.conditionId
+        const outcomeId = condition.outcomeId
+        const betAmount = new BigNumber(amount).toNumber()
+
+        const calculation = await calculateActualRate({
+            chainId,
+            conditionId,
+            outcomeId,
+            betAmount,
         })
+
+        return calculation
     }, [amount, condition])
 }
