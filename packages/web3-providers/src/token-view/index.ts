f696ec67c836e2363ffd40d9ac57e877a66fb4e2
Randolph <840094513@qq.com>
2022-07-06 21:01:40 +0800

    fix: build error

--- packages/web3-providers/src/token-view/index.ts
+++ packages/web3-providers/src/token-view/index.ts
@@ -5,7 +5,7 @@ import { TOKEN_VIEW_ROOT_URL, API_KEY, INTERVAL } from './constants'
 
 export class TokenViewAPI implements TokenAPI.Provider {
     async getTokenInfo(tokenSymbol: string) {
-        const response = await fetchJSON<TokenAPI.tokenInfo[] | undefined>(
+        const response = await fetchJSON<TokenAPI.TokenInfo[] | undefined>(
             urlcat(TOKEN_VIEW_ROOT_URL, {
                 key: API_KEY,
                 ids: tokenSymbol,
