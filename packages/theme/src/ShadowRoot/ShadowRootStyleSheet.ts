d27311576e96c8ef8098107603318eea823066ad
Jack Works <jackworks@protonmail.com>
2022-07-06 17:31:33 +0800

    fix: firefox crash (#6701)

--- packages/theme/src/ShadowRoot/ShadowRootStyleSheet.ts
+++ packages/theme/src/ShadowRoot/ShadowRootStyleSheet.ts
@@ -1,3 +1,5 @@
+/// <reference types="@masknet/global-types/firefox" />
+
 const shadowHeadMap = new WeakMap<ShadowRoot, HTMLHeadElement>()
 const constructableStyleSheetEnabled = true
 
@@ -68,6 +70,7 @@ export class StyleSheet {
     private implementation!: ConstructableStyleSheet | SynchronizeStyleSheet
     private _alreadyInsertedOrderInsensitiveRule = false
 }
+
 class ConstructableStyleSheet {
     private sheet = new CSSStyleSheet()
     private globalSheet = new CSSStyleSheet()
@@ -75,7 +78,16 @@ class ConstructableStyleSheet {
     addContainer(container: ShadowRoot) {
         if (this.added.has(container)) return
         this.added.add(container)
-        container.adoptedStyleSheets = [this.globalSheet, ...(container.adoptedStyleSheets || []), this.sheet]
+
+        if (typeof XPCNativeWrapper === 'undefined') {
+            // push & unshift crashes Chrome 103. Not tested on other versions.
+            container.adoptedStyleSheets = [this.sheet, ...container.adoptedStyleSheets!, this.globalSheet]
+        } else {
+            // assignment does not work on Firefox 102. Not tested on other versions.
+            const unsafe = XPCNativeWrapper.unwrap(container.adoptedStyleSheets!)
+            Array.prototype.unshift.call(unsafe, XPCNativeWrapper.unwrap(this.globalSheet))
+            Array.prototype.push.call(unsafe, XPCNativeWrapper.unwrap(this.sheet))
+        }
     }
     insert(rule: string) {
         insertRuleSpeedy(this.sheet, rule)
