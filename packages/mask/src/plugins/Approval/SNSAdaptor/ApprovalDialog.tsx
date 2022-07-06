4283157c5a67f6d5713ea555e644328516b0c7ca
zhouhanseng <z308114274@gmail.com>
2022-07-06 17:05:29 +0800

    chore: for non evm

--- packages/mask/src/plugins/Approval/SNSAdaptor/ApprovalDialog.tsx
+++ packages/mask/src/plugins/Approval/SNSAdaptor/ApprovalDialog.tsx
@@ -6,6 +6,7 @@ import { NetworkTab } from '../../../components/shared/NetworkTab'
 import { useRemoteControlledDialog } from '@masknet/shared-base-ui'
 import { PluginWalletStatusBar } from '../../../utils/components/PluginWalletStatusBar'
 import { ChainId, chainResolver, NetworkType } from '@masknet/web3-shared-evm'
+import { NetworkPluginID } from '@masknet/web3-shared-base'
 import { useCurrentWeb3NetworkPluginID } from '@masknet/plugin-infra/web3'
 import { PluginId } from '@masknet/plugin-infra'
 import { useActivatedPlugin } from '@masknet/plugin-infra/dom'
@@ -14,6 +15,7 @@ import { useI18N as useBaseI18n } from '../../../utils'
 import { WalletMessages } from '../../../plugins/Wallet/messages'
 import { InjectedDialog } from '@masknet/shared'
 import { useStyles } from './useStyles'
+import { ApprovalEmptyContent } from './ApprovalEmptyContent'
 import { ApprovalTokenContent } from './ApprovalTokenContent'
 import { ApprovalNFTContent } from './ApprovalNFTContent'
 import { TabContext } from '@mui/lab'
@@ -67,7 +69,9 @@ function ApprovalWrapper(props: ApprovalWrapperProps) {
     const [networkTabChainId, setNetworkTabChainId] = useState<ChainId>(chainId)
     const approvalDefinition = useActivatedPlugin(PluginId.Approval, 'any')
     const pluginId = useCurrentWeb3NetworkPluginID()
-    const chainIdList = approvalDefinition?.enableRequirement.web3?.[pluginId]?.supportedChainIds ?? []
+    const chainIdList =
+        approvalDefinition?.enableRequirement.web3?.[NetworkPluginID.PLUGIN_EVM]?.supportedChainIds ?? []
+
     const { classes } = useStyles()
     const { setDialog: setSelectProviderDialog } = useRemoteControlledDialog(
         WalletMessages.events.selectProviderDialogUpdated,
@@ -75,21 +79,27 @@ function ApprovalWrapper(props: ApprovalWrapperProps) {
 
     return (
         <div className={classes.approvalWrapper}>
-            <div className={classes.abstractTabWrapper}>
-                <NetworkTab
-                    chainId={networkTabChainId}
-                    setChainId={setNetworkTabChainId}
-                    classes={classes}
-                    chains={chainIdList.filter(Boolean) as ChainId[]}
-                />
-            </div>
-            <section className={classes.contentWrapper}>
-                {tab === Tabs.tokens ? (
-                    <ApprovalTokenContent chainId={networkTabChainId} />
-                ) : (
-                    <ApprovalNFTContent chainId={networkTabChainId} />
-                )}
-            </section>
+            {pluginId === NetworkPluginID.PLUGIN_EVM ? (
+                <>
+                    <div className={classes.abstractTabWrapper}>
+                        <NetworkTab
+                            chainId={networkTabChainId}
+                            setChainId={setNetworkTabChainId}
+                            classes={classes}
+                            chains={chainIdList?.filter(Boolean) as ChainId[]}
+                        />
+                    </div>
+                    <section className={classes.contentWrapper}>
+                        {tab === Tabs.tokens ? (
+                            <ApprovalTokenContent chainId={networkTabChainId} />
+                        ) : (
+                            <ApprovalNFTContent chainId={networkTabChainId} />
+                        )}
+                    </section>
+                </>
+            ) : (
+                <ApprovalEmptyContent />
+            )}
             <PluginWalletStatusBar className={classes.footer}>
                 <Button
                     variant="contained"
@@ -98,7 +108,7 @@ function ApprovalWrapper(props: ApprovalWrapperProps) {
                         setSelectProviderDialog({
                             open: true,
                             supportedNetworkList: chainIdList
-                                .map((chainId) => {
+                                ?.map((chainId) => {
                                     const x = chainResolver.chainNetworkType(chainId)
                                     return x
                                 })
@@ -106,7 +116,9 @@ function ApprovalWrapper(props: ApprovalWrapperProps) {
                         })
                     }}
                     fullWidth>
-                    {tr('wallet_status_button_change')}
+                    {pluginId === NetworkPluginID.PLUGIN_EVM
+                        ? tr('wallet_status_button_change')
+                        : tr('wallet_status_button_change_to_evm')}
                 </Button>
             </PluginWalletStatusBar>
         </div>
