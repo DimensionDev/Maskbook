954b82834b6b7a0bbb398ebded681599063cbea6
etouyang <etouyang@gmail.com>
2022-07-07 14:34:39 +0800

    feat: Pet game (#6548)
    * feat: game
    
    * feat: game window logic
    
    * perf: game logo
    
    * perf: close wallet dialog when game beginning
    
    * feat: game list
    
    * pet owner contract
    
    * feat: right menu in pet
    
    * show video
    
    * add blacklist
    
    * update blocklist
    
    * change blacklist address
    
    * feat: add config game list
    
    * perf: window width/height/url
    
    * fix: add parameter for game window
    
    * feat: update game / rightMenu
    
    * perf: update
    
    * fix: check error of twitterId
    
    * perf: share btn / game window
    
    * perf: game and share
    
    * fix: change share for twitter
    
    * fix: add loading for request
    
    * fix: add PLUGIN_EVM
    
    * fix: reviewed
    
    * fix: reviewed
    
    * fix: close button click
    
    * fix: image error show
    
    * perf: update arrow control
    
    * feat: add error for wallet connect
    
    * fix: remove global settings
    
    * fix: collection name empty
    
    * fix: add notice for block nft
    
    * fix: wallet connect error
    
    * fix: logo style
    
    * fix: reviewed
    
    * fix: change wallet status bar
    
    * fix: reviewed
    
    * fix: reviewed
    
    * fix: constant twitter name
    
    * fix: change i18n
    
    * fix: reviewed
    
    Co-authored-by: javaLuo <376693576@qq.com>
    Co-authored-by: nprog <alwuyy@gmail.com>

--- packages/mask/src/plugins/Game/base.tsx
+++ packages/mask/src/plugins/Game/base.tsx
@@ -1,19 +1,27 @@
 import { CurrentSNSNetwork, Plugin } from '@masknet/plugin-infra'
 import { NetworkPluginID } from '@masknet/web3-shared-base'
 import { ChainId } from '@masknet/web3-shared-evm'
-import { PetsPluginID } from './constants'
+import { GamePluginID } from './constants'
 import { languages } from './locales/languages'
 
 export const base: Plugin.Shared.Definition = {
-    ID: PetsPluginID,
-    name: { fallback: 'Non-Fungible Friends' },
+    ID: GamePluginID,
+    name: { fallback: 'Game' },
     description: {
-        fallback: 'Discover the infinite possibilities of #NFTs.',
+        fallback: 'Play game with your NFT.',
     },
     publisher: { name: { fallback: '' }, link: 'https://github.com/HelloWeb3Team' },
     enableRequirement: {
         architecture: { app: false, web: true },
-        networks: { type: 'opt-in', networks: { [CurrentSNSNetwork.Twitter]: true } },
+        networks: {
+            type: 'opt-in',
+            networks: {
+                [CurrentSNSNetwork.Twitter]: true,
+                [CurrentSNSNetwork.Facebook]: true,
+                [CurrentSNSNetwork.Instagram]: true,
+                [CurrentSNSNetwork.Minds]: true,
+            },
+        },
         web3: {
             [NetworkPluginID.PLUGIN_EVM]: {
                 supportedChainIds: [ChainId.Mainnet],
---
+++ packages/mask/src/plugins/Game/constants.ts
@@ -0,0 +1,7 @@
+export const GamePluginID = 'com.maskbook.game'
+export const Share_Twitter = 'https://twitter.com/NonFFriend/status/1544559284075110400'
+
+export const IconClose = new URL('./assets/close.png', import.meta.url).toString()
+export const IconFull = new URL('./assets/full.png', import.meta.url).toString()
+export const IconShare = new URL('./assets/share.png', import.meta.url).toString()
+export const IconGame = new URL('./assets/game.png', import.meta.url).toString()
