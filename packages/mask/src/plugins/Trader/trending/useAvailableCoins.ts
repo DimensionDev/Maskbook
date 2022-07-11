23450160e7dc9ee99af4ea113f1aad3bfbbf0780
UncleBill <billbill290@gmail.com>
2022-07-11 12:07:53 +0800

    fix: mf-1440 coin menu selecting doesn't work (#6744)

--- packages/mask/src/plugins/Trader/trending/useAvailableCoins.ts
+++ packages/mask/src/plugins/Trader/trending/useAvailableCoins.ts
@@ -1,16 +1,17 @@
+import { useChainId } from '@masknet/plugin-infra/web3'
+import type { DataProvider } from '@masknet/public-api'
+import { EMPTY_LIST } from '@masknet/shared-base'
+import { NetworkPluginID } from '@masknet/web3-shared-base'
 import { useAsync } from 'react-use'
 import type { AsyncState } from 'react-use/lib/useAsyncFn'
 import { PluginTraderRPC } from '../messages'
 import type { Coin, TagType } from '../types'
-import type { DataProvider } from '@masknet/public-api'
-import { useChainId } from '@masknet/plugin-infra/web3'
-import { NetworkPluginID } from '@masknet/web3-shared-base'
 
 export function useAvailableCoins(type: TagType, keyword: string, dataProvider: DataProvider): AsyncState<Coin[]> {
     const chainId = useChainId(NetworkPluginID.PLUGIN_EVM)
 
     return useAsync(async () => {
-        if (!keyword) return []
+        if (!keyword) return EMPTY_LIST
         return PluginTraderRPC.getAvailableCoins(chainId, keyword, type, dataProvider)
     }, [dataProvider, type, keyword, chainId])
 }
