1c971b6aa12de0df6e7d846ed55ef1598701a69c
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2022-07-12 20:07:12 +0800

    fix: nonce confliction (#6771)
    * feat(lucky drop): pre gas minus (#6467)
    
    * feat(lucky drop): pre gas minus
    
    * chore: reply code review
    
    * chore: solve error
    
    * fix: solve conflict
    
    * chore: format balance
    
    * chore: apply confition
    
    apply only sum of gas and total greater than balance
    
    * chore: increase estimate gas
    
    * chore: format significant
    
    * chore: reply code review
    
    * chore: format significant
    
    * chore: i18n
    
    * chore: reply code review
    
    * chore: use big number
    
    * chore: add useTransactionValue hook
    
    * refactor: unlock nft
    
    Co-authored-by: Hancheng Zhou <z308114274@gmail.com>

--- packages/plugins/EVM/src/state/TransactionFormatter/descriptors/ERC721.ts
+++ packages/plugins/EVM/src/state/TransactionFormatter/descriptors/ERC721.ts
@@ -1,5 +1,6 @@
 import type { TransactionContext } from '@masknet/web3-shared-base'
 import type { ChainId, TransactionParameter } from '@masknet/web3-shared-evm'
+import { Web3StateSettings } from '../../../settings'
 import type { TransactionDescriptor } from '../types'
 
 export class ERC721Descriptor implements TransactionDescriptor {
@@ -10,6 +11,20 @@ export class ERC721Descriptor implements TransactionDescriptor {
             const parameters = method.parameters
 
             switch (method.name) {
+                case 'approve':
+                    if (parameters?.to === undefined || parameters?.tokenId === undefined) break
+
+                    const connection = await Web3StateSettings.value.Connection?.getConnection?.({
+                        chainId: context.chainId,
+                    })
+                    const contract = await connection?.getNonFungibleTokenContract(context.to)
+
+                    return {
+                        chainId: context.chainId,
+                        title: `Unlock ${contract?.symbol ?? 'token'} contract`,
+                        description: `Unlock ${contract?.symbol ?? 'token'} contract`,
+                        successfulDescription: `${contract?.symbol ?? 'token'} is unlocked successfully.`,
+                    }
                 case 'setApprovalForAll':
                     if (parameters?.operator === undefined || parameters?.approved === undefined) break
                     return {
