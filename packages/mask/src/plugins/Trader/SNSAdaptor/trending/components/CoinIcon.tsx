5d8dd55c96c5575b70e09381c85bda876e301bb9
unclebill <billbill290@gmail.com>
2022-07-13 12:45:36 +0800

    fix: replace TrendingCoinType with TokenType

--- packages/mask/src/plugins/Trader/SNSAdaptor/trending/components/CoinIcon.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trending/components/CoinIcon.tsx
@@ -1,10 +1,9 @@
 import { TokenIcon } from '@masknet/shared'
-import { TrendingCoinType } from '@masknet/web3-providers'
 import { NetworkPluginID, TokenType } from '@masknet/web3-shared-base'
 import type { FC } from 'react'
 
 interface CoinIconProps {
-    type: TrendingCoinType
+    type: TokenType
     address?: string
     logoUrl?: string
     name?: string
@@ -13,7 +12,7 @@ interface CoinIconProps {
 export const CoinIcon: FC<CoinIconProps> = ({ type, address, logoUrl, name, size }) => {
     return (
         <TokenIcon
-            tokenType={type === TrendingCoinType.NonFungible ? TokenType.NonFungible : TokenType.Fungible}
+            tokenType={type}
             pluginID={NetworkPluginID.PLUGIN_EVM}
             name={name}
             AvatarProps={{
