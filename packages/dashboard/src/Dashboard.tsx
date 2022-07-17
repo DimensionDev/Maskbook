2e308c2fcb6e91e83439691bca98daee29801639
Lantt <laanfor@gmail.com>
2022-07-17 19:41:17 +0800

    chore: setup sentry for dashboard

--- packages/dashboard/src/Dashboard.tsx
+++ packages/dashboard/src/Dashboard.tsx
@@ -1,4 +1,7 @@
+import { initLogs } from '@masknet/shared-base'
 import { lazy, Suspense } from 'react'
+
+initLogs('dashboard')
 
 const Dashboard = lazy(() => import(/* webpackPreload: true */ './initialization/Dashboard.js'))
 export function IntegratedDashboard() {
