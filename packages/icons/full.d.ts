84a5ef72c9670ebbd2dbad982d065f52bc2f6cb6
Jack Works <jackworks@protonmail.com>
2022-07-18 14:01:28 +0800

    chore: merge full and root

--- packages/icons/full.d.ts
+++
@@ -1,2 +0,0 @@
-export * as Icons from './icon-generated-as-jsx.js'
-export * as MaskIcons from './icon-generated-as-jsx.js'
--- packages/icons/index.js
+++ packages/icons/index.js
@@ -2,3 +2,7 @@ export * from './utils/index.js'
 export * from './utils/MaskIconPaletteContext.js'
 export * from './icon-generated-as-jsx.js'
 export * from './icon-generated-as-url.js'
+
+export * as Icons from './icon-generated-as-jsx.js'
+export * as MaskIcons from './icon-generated-as-jsx.js'
+export * as MaskIconURLs from './icon-generated-as-url.js'
