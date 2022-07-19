c8d6b081a802cbbef11426960024f781ef051b22
Hancheng Zhou <z308114274@gmail.com>
2022-07-19 11:35:57 +0800

    chore: persist lucky drop input state (#6814)
    * fix: ui
    
    * fix: text
    
    * fix: persist input state

--- packages/mask/src/plugins/RedPacket/SNSAdaptor/RedPacketInHistoryList.tsx
+++ packages/mask/src/plugins/RedPacket/SNSAdaptor/RedPacketInHistoryList.tsx
@@ -320,6 +320,8 @@ export function RedPacketInHistoryList(props: RedPacketInHistoryListProps) {
                                     {canSend
                                         ? t.send()
                                         : refunded
+                                        ? t.refund()
+                                        : isRefunding
                                         ? t.refunding()
                                         : listOfStatus.includes(RedPacketStatus.empty)
                                         ? t.empty()
