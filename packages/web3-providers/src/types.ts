07c32c2c8e8d7c6fd834c23f90048d5fdb281970
Randolph314 <97870249+Randolph314@users.noreply.github.com>
2022-07-19 14:19:40 +0800

    feat: rss3 api update (#6816)
    * feat: update RSS3 api
    
    * feat: modify the latest api call
    
    * fix: typo
    
    * fix: typo
    
    * feat: rename params
    
    * feat: rename type param
    
    * feat: rename type params
    
    * feat: rename type params
    
    Co-authored-by: Randolph <840094513@qq.com>

--- packages/web3-providers/src/types.ts
+++ packages/web3-providers/src/types.ts
@@ -113,6 +113,94 @@ export namespace RSS3BaseAPI {
         name: string
     }
 
+    export interface NFT_Contract {
+        address: string
+        name: string
+        symbol: string
+    }
+
+    export interface NFT_Trait {
+        trait_type: string
+        value: string
+    }
+    export interface NFT_Type {
+        asset_contract: NFT_Contract
+        chain: string
+        description: string
+        image_preview_url: string
+        image_preview_url_ct: string
+        image_thumbnail_url: string
+        image_thumbnail_url_ct: string
+        image_url: string
+        image_url_ct: string
+        name: string
+        received_at: string
+        token_id: string
+        traits: NFT_Trait[]
+    }
+
+    export interface NFT {
+        id: string
+        detail: NFT_Type
+    }
+
+    export interface DonationTx {
+        adminAddr: string
+        amount: string
+        approach: string
+        donor: string
+        formatedAmount: string
+        symbol: string
+        timeStamp: string
+        tokenAddr: string
+        txHash: string
+    }
+
+    export interface DonationGrant {
+        active: boolean
+        admin_address: string
+        contract_address: string
+        description: string
+        id: number
+        logo: string
+        reference_url: string
+        slug: string
+        title: string
+        token_address: string
+        token_symbol: string
+    }
+
+    export interface DonationType {
+        grant: DonationGrant
+        txs: DonationTx[]
+    }
+
+    export interface Donation {
+        id: string
+        detail: DonationType
+    }
+
+    export interface FootprintType {
+        id: number
+        fancy_id: string
+        name: string
+        event_url: string
+        image_url: string
+        country: string
+        city: string
+        description: string
+        year: number
+        start_date: string
+        end_date: string
+        expiry_date: string
+        supply: number
+    }
+
+    export interface Footprint {
+        id: string
+        detail: FootprintType
+    }
+
     export enum AssetType {
         GitcoinDonation = 'Gitcoin-Donation',
         POAP = 'POAP',
@@ -129,8 +217,8 @@ export namespace RSS3BaseAPI {
         createRSS3(address: string): RSS3
         getFileData<T>(rss3: RSS3, address: string, key: string): Promise<T | undefined>
         setFileData<T>(rss3: RSS3, address: string, key: string, data: T): Promise<T>
-        getDonations(address: string): Promise<GeneralAssetResponse | undefined>
-        getFootprints(address: string): Promise<GeneralAssetResponse | undefined>
+        getDonations(address: string): Promise<Donation[] | undefined>
+        getFootprints(address: string): Promise<Footprint[] | undefined>
         getNameInfo(id: string): Promise<NameInfo | undefined>
         getProfileInfo(address: string): Promise<ProfileInfo | undefined>
     }
