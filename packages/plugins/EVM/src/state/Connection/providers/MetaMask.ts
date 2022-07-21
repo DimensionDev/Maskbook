ac237d81096d3a6593851c35176c0109f4411fce
zhouhanseng <z308114274@gmail.com>
2022-07-21 18:03:31 +0800

    fix(firefox): connect metamask

--- packages/plugins/EVM/src/state/Connection/providers/MetaMask.ts
+++ packages/plugins/EVM/src/state/Connection/providers/MetaMask.ts
@@ -1,22 +1,25 @@
 import createMetaMaskProvider from '@dimensiondev/metamask-extension-provider'
 import { injectedMetaMaskProvider } from '@masknet/injected-script'
-import { isExtensionSiteType } from '@masknet/shared-base'
+import { isWindowEthereumUnableToInjected } from '@masknet/shared-base'
 import { ProviderType } from '@masknet/web3-shared-evm'
 import type { EVM_Provider } from '../types'
 import { BaseInjectedProvider } from './BaseInjected'
 
 export class MetaMaskProvider extends BaseInjectedProvider implements EVM_Provider {
     constructor() {
-        super(ProviderType.MetaMask, isExtensionSiteType() ? createMetaMaskProvider() : injectedMetaMaskProvider)
+        super(
+            ProviderType.MetaMask,
+            isWindowEthereumUnableToInjected() ? createMetaMaskProvider() : injectedMetaMaskProvider,
+        )
     }
 
     override get ready() {
-        if (isExtensionSiteType()) return true
+        if (isWindowEthereumUnableToInjected()) return true
         return super.ready
     }
 
     override get readyPromise() {
-        if (isExtensionSiteType()) return Promise.resolve(undefined)
+        if (isWindowEthereumUnableToInjected()) return Promise.resolve(undefined)
         return super.readyPromise
     }
 
