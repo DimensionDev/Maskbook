22aa1c7718fe4d1e50f6e0ed30518fa55d887bb5
lelenei <zwpsky@gmail.com>
2022-07-23 14:20:53 +0800

    refactor: use image component

--- packages/plugins/RSS3/src/SNSAdaptor/components/ImageHolder.tsx
+++ packages/plugins/RSS3/src/SNSAdaptor/components/ImageHolder.tsx
@@ -1,5 +1,4 @@
-import { CircularProgress } from '@mui/material'
-import { useAsync } from 'react-use'
+import { Image } from '@masknet/shared'
 
 export interface ImageProps {
     url: string
@@ -15,19 +14,9 @@ export const ImageHolder = ({ url, title, isFullRound, size }: ImageProps) => {
         height: `${size}px`,
     }
 
-    const { loading, value: image } = useAsync(async () => {
-        if (!url) return
-        const data = await globalThis.r2d2Fetch(url)
-        return URL.createObjectURL(await data.blob())
-    }, [url])
-
     return (
         <div className={`flex justify-around relative ${roundedClass}`} style={containerStyles}>
-            {loading ? (
-                <CircularProgress size="small" />
-            ) : (
-                <img className={`object-cover w-full h-full ${roundedClass}`} src={image} alt={title} />
-            )}
+            <Image className={`object-cover w-full h-full ${roundedClass}`} src={url} alt={title} />
         </div>
     )
 }
