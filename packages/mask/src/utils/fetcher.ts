b5e951afedb6309a63005637c9b7ddd7c5d73917
Jack Works <jackworks@protonmail.com>
2022-07-28 16:17:06 +0800

    chore: optimize same origin detection in r2d2

--- packages/mask/src/utils/fetcher.ts
+++ packages/mask/src/utils/fetcher.ts
@@ -4,7 +4,7 @@ const { fetch: original_fetch } = globalThis
 export function contentFetch(input: RequestInfo, init?: RequestInit) {
     const info = new Request(input, init)
 
-    if (isSameOrigin(info.url)) {
+    if (canAccessAsContent(info.url)) {
         if (process.env.engine === 'firefox' && process.env.manifest === '2' && typeof content === 'object') {
             return content.fetch(info)
         } else {
@@ -15,6 +15,17 @@ export function contentFetch(input: RequestInfo, init?: RequestInit) {
     return Services.Helper.r2d2Fetch(info)
 }
 
-function isSameOrigin(url: string) {
-    return new URL(url, location.href).origin === location.origin
+const extensionOrigin = (() => {
+    try {
+        return new URL(browser.runtime.getURL('')).origin
+    } catch (e) {
+        return null
+    }
+})()
+
+function canAccessAsContent(url: string) {
+    const target = new URL(url, location.href)
+    if (location.origin.endsWith('twitter.com') && target.origin === 'https://abs.twimg.com') return true
+    if (extensionOrigin === target.origin) return true
+    return target.origin === location.origin
 }
