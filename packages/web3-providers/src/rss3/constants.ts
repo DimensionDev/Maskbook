21e0eea0c6a87f459d4d203c47e6f93a3d2f7a57
Randolph314 <97870249+Randolph314@users.noreply.github.com>
2022-07-28 15:24:06 +0800

    feat: update rss3 api (#6906)
    * feat: update rss3 api
    
    * feat: change rss3 api call
    
    * fix: typo
    
    * feat: delete useless code
    
    * fix: code style
    
    Co-authored-by: Randolph <840094513@qq.com>

--- packages/web3-providers/src/rss3/constants.ts
+++ packages/web3-providers/src/rss3/constants.ts
@@ -1,8 +1,63 @@
 export const RSS3_ENDPOINT = 'https://hub.pass3.me'
-export const NEW_RSS3_ENDPOINT = 'https://prenode.rss3.dev'
+export const NEW_RSS3_ENDPOINT = 'https://pregod.rss3.dev/v1.1.0/notes/'
 
 export const CollectionType = {
     NFT: /Polygon.NFT|Ethereum.NFT|BSC.NFT/,
     donation: /Gitcoin.Donation/,
     footprint: /Mirror.XYZ|xDai.POAP/,
+}
+
+export enum NETWORK {
+    ethereum = 'ethereum',
+    ethereum_classic = 'ethereum_classic',
+    binance_smart_chain = 'binance_smart_chain',
+    polygon = 'polygon',
+    zksync = 'zksync',
+    xdai = 'xdai',
+    arweave = 'arweave',
+    arbitrum = 'arbitrum',
+    optimism = 'optimism',
+    fantom = 'fantom',
+    avalanche = 'avalanche',
+    crossbell = 'crossbell',
+}
+export enum PLATFORM {
+    mirror = 'mirror',
+    lens = 'lens',
+    gitcoin = 'gitcoin',
+    snapshot = 'snapshot',
+    uniswap = 'uniswap',
+    binance = 'binance',
+    crossbell = 'crossbell',
+}
+
+export enum TAG {
+    social = 'social',
+    transaction = 'transaction',
+    exchange = 'exchange',
+    collectible = 'collectible',
+    donation = 'donation',
+    governance = 'governance',
+}
+
+export enum TYPE {
+    transfer = 'transfer',
+    mint = 'mint',
+    burn = 'burn',
+    withdraw = 'withdraw',
+    deposit = 'deposit',
+    swap = 'swap',
+    trade = 'trade',
+    poap = 'poap',
+    post = 'post',
+    comment = 'comment',
+    share = 'share',
+    profile = 'profile',
+    follow = 'follow',
+    unfollow = 'unfollow',
+    like = 'like',
+    propose = 'propose',
+    vote = 'vote',
+    launch = 'launch',
+    donate = 'donate',
 }
