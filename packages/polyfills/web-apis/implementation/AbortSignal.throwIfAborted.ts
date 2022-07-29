d1fadd508d73e542eae3fa57b1afa7785d4a8c8f
Jack Works <jackworks@protonmail.com>
2022-07-29 16:03:37 +0800

    fix: eslint

--- packages/polyfills/web-apis/implementation/AbortSignal.throwIfAborted.ts
+++ packages/polyfills/web-apis/implementation/AbortSignal.throwIfAborted.ts
@@ -1,10 +1,9 @@
 try {
     if (!AbortSignal.prototype.throwIfAborted) {
         AbortSignal.prototype.throwIfAborted = function throwIfAborted(this: AbortSignal) {
-            if (this.aborted) {
-                if (this.reason) throw this.reason
-                throw new DOMException('signal is aborted without reason', 'AbortError')
-            }
+            if (!this.aborted) return
+            if (this.reason) throw this.reason
+            throw new DOMException('signal is aborted without reason', 'AbortError')
         }
     }
 } catch {}
