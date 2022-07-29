aa30402a94b52de1ff53a750d792538f1e2f64f5
Jack Works <jackworks@protonmail.com>
2022-07-29 16:05:42 +0800

    chore: optimize same origin detection in r2d2 (#6909)
    * chore: optimize same origin detection in r2d2
    
    * fix: eslint

--- packages/mask/src/utils/fetcher.ts
+++ packages/mask/src/utils/fetcher.ts
@@ -4,7 +4,7 @@ const { fetch: original_fetch } = globalThis
 export function contentFetch(input: RequestInfo, init?: RequestInit) {
     const info = new Request(input, init)
 
-    if (isSameOrigin(info.url)) {
+    if (canAccessAsContent(info.url)) {
         if (process.env.engine === 'firefox' && process.env.manifest === '2' && typeof content === 'object') {
             return content.fetch(info, init)
         } else {
@@ -20,6 +20,17 @@ export function contentFetch(input: RequestInfo, init?: RequestInit) {
     })
 }
 
-function isSameOrigin(url: string) {
-    return new URL(url, location.href).origin === location.origin
+const extensionOrigin = (() => {
+    try {
+        return new URL(browser.runtime.getURL('')).origin
+    } catch {
+        return null
+    }
+})()
+
+function canAccessAsContent(url: string) {
+    const target = new URL(url, location.href)
+    if (location.origin.endsWith('twitter.com') && target.origin === 'https://abs.twimg.com') return true
+    if (extensionOrigin === target.origin) return true
+    return target.origin === location.origin
 }
