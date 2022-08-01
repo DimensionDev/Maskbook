a7348fe55666e760fac1880ec2aace97b3a2cf6f
guanbinrui <guanbinrui@dimension.im>
2022-08-01 20:35:11 +0800

    refactor: zora api

--- packages/web3-providers/src/zora/queries.ts
+++ packages/web3-providers/src/zora/queries.ts
@@ -1,106 +1,149 @@
 import { gql } from 'graphql-request'
 
-export const getAssetQuery = gql`
-    query getAsset($address: String!, $tokenId: String!) {
-        Token(where: { address: { _eq: $address }, tokenId: { _eq: $tokenId } }) {
-            tokenId
-            name
-            address
-            owner
-            metadata {
-                json
+const CurrencyAmount = gql`
+    raw
+    decimal
+    currency {
+        name
+        address
+        decimals
+    }
+`
+
+const Page = gql`
+    limit
+    endCursor
+    hasNextPage
+`
+
+const Network = gql`
+    chain
+`
+
+const Transaction = gql`
+    blockNumber
+    blockTimestamp
+    transactionHash
+    logIndex
+`
+
+const Media = gql`
+    url
+    mimeType
+    size
+`
+
+const Attribute = gql`
+    traitType
+    displayType
+    value
+`
+
+const Token = gql`
+    tokenId
+    collectionAddress
+    collectionName
+    mintInfo {
+        mintContext ${Transaction}
+        originatorAddress
+        toAddress
+        price {
+            blockNumber
+            nativePrice {
+                ${CurrencyAmount}
             }
-            v3Ask {
-                askPrice
+            chainTokenPrice {
+                ${CurrencyAmount}
             }
-            currentAuction {
-                expiresAt
+            usdcPrice {
+                ${CurrencyAmount}
             }
-            symbol
-            tokenContract {
-                name
-            }
+        }
+    }
+    networkInfo {
+        ${Network}
+    }
+    tokenUrl
+    tokenUrlMimeType
+    content {
+        ${Media}
+    }
+    image {
+        ${Media}
+    }
+    owner
+    tokenContract {
+        collectionAddress
+        chain
+        name
+        symbol
+        totalSupply
+        description
+    }
+    name
+    description
+    metadata
+    attributes {
+        ${Attribute}
+    }
+`
+
+const Collection = gql`
+    address
+    description
+    name
+    symbol
+    totalSupply
+    networkInfo ${Network}
+`
+
+const Event = gql`
+    networkInfo ${Network}
+    transactionInfo ${Transaction}
+    eventType
+    collectionAddress
+    tokenId
+    properties
+`
+
+export const GetTokenQuery = gql`
+    query getToken($address: String!, $tokenId: String!) {
+        token(token: { address: $address, tokenId: $tokenId }) {
+            token ${Token}
         }
     }
 `
 
-export const getTokenHistoryQuery = gql`
-    query getTokenHistory($address: String!, $tokenId: String!) {
-        Token(where: { address: { _eq: $address }, tokenId: { _eq: $tokenId } }) {
-            transferEvents {
-                blockTimestamp
-                transaction {
-                    mediaMints {
-                        id
-                        blockTimestamp
-                        creator
-                        address
-                    }
-                    auctionCreatedEvents {
-                        id
-                        reservePrice
-                        tokenOwner
-                        blockTimestamp
-                        auctionCurrency
-                    }
-                    marketBidEvents(where: { status: { _eq: "FINALIZED" } }) {
-                        id
-                        blockTimestamp
-                        amount
-                        currencyAddress
-                        bidder
-                        recipient
-                    }
-                    auctionEndedEvents {
-                        id
-                        transaction {
-                            blockTimestamp
-                        }
-                        tokenOwner
-                        winner
-                        auction {
-                            lastBidAmount
-                            auctionCurrency
-                        }
-                    }
-                }
+export const GetTokensQuery = gql`
+    query getTokens($ownerAddresses: [String!], $size: Int = 20) {
+        tokens(pagination: { limit: $size }, where: { ownerAddresses: $ownerAddresses }) {
+            nodes {
+                Token ${Token}
             }
+            pageInfo ${Page}
         }
     }
 `
 
-export const getBidsQuery = gql`
-    query getBidEvents($tokenAddress: String!, $tokenId: String!) {
-        Token(where: { address: { _eq: $tokenAddress }, tokenId: { _eq: $tokenId } }) {
-            transferEvents {
-                transaction {
-                    marketBidEvents(where: { status: { _eq: "FINALIZED" } }) {
-                        blockTimestamp
-                        amount
-                        currencyAddress
-                        transactionHash
-                        recipient
-                    }
-                }
+export const GetCollectionsByKeywordQuery = gql`
+    query getCollections($keyword: String!, $size: Int = 20) {
+        search(pagination: { limit: $size }, query: { text: $keyword }, filter: { entityType: COLLECTION}) {
+            nodes {
+                entity ${Collection}
+                networkInfo ${Network}
             }
+            pageInfo ${Page}
         }
     }
 `
 
-export const getAsksQuery = gql`
-    query getAskEvents($tokenAddress: String!, $tokenId: String!) {
-        Token(where: { address: { _eq: $tokenAddress }, tokenId: { _eq: $tokenId } }) {
-            transferEvents {
-                transaction {
-                    auctionCreatedEvents {
-                        auctionCurrency
-                        reservePrice
-                        tokenOwner
-                        blockTimestamp
-                        transactionHash
-                    }
-                }
+export const GetEventsQuery = gql`
+    query getEvents($address: String!, $tokenId: String!, $size: Int = 20) {
+        events(pagination: { limit: $size }, where: { tokens: [ { address: $address, tokenId: $tokenId } ] }) {
+            nodes {
+                ${Event}
             }
+            pageInfo ${Page}
         }
     }
 `
--- packages/web3-providers/src/zora/types.ts
+++ packages/web3-providers/src/zora/types.ts
@@ -1,98 +1,313 @@
-export interface ZoraHistory {
+export interface Page {
+    limit: number
+    endCursor?: string
+    hasNextPage: boolean
+}
+
+export interface CurrencyAmount {
+    currency: {
+        name: string
+        address: string
+        decimals: string
+    }
+    raw: string
+    decimals: number
+}
+
+export interface PriceAtTime {
+    nativePrice: CurrencyAmount
+    chainTokenPrice?: CurrencyAmount
+    usdcPrice?: CurrencyAmount
+    blockNumber: number
+}
+
+export interface Network {
+    network: 'ETHEREUM'
+    chain: 'MAINNET' | 'GOERLI' | 'RINKEBY'
+}
+
+export interface Transaction {
+    blockNumber: number
+    // e.g., 2021-09-07T11:48:32+00:00
     blockTimestamp: string
-    transaction: ZoraTransactionEvent
+    transactionHash?: string
+    logIndex?: number
 }
 
-export interface ZoraTransactionEvent {
-    mediaMints?: MediaMint[]
-    auctionCreatedEvents?: AuctionCreatedEvent[]
-    marketBidEvents?: MarketBidEvent[]
-    auctionEndedEvents?: AuctionEndedEvent[]
+export interface Media {
+    url?: string
+    mimeType?: string
+    size?: string
 }
 
-export interface MediaMint {
-    id: string
-    blockTimestamp: string
-    creator: string
-    address: string
+export interface Attribute {
+    value?: string
+    traitType?: string
+    displayType?: string
 }
 
-export interface AuctionCreatedEvent {
-    id: string
-    blockTimestamp: string
-    reservePrice: string
-    auctionCurrency: string
-    tokenOwner: string
-    transactionHash: string
+export interface CollectionAttribute {
+    traitType?: string
+    valueMetrics: Array<{
+        value: string
+        count: string
+        percent: string
+    }>
 }
 
-export interface MarketBidEvent {
-    id: string
-    blockTimestamp: string
-    amount: string
-    currencyAddress: string
-    bidder: string
-    recipient: string
-    transactionHash: string
-}
-
-export interface AuctionEndedEvent {
-    id: string
-    tokenOwner: string
-    winner: string
-    blockTimestamp: string
-    auction: AuctionInfo
-}
-
-export interface AuctionInfo {
-    lastBidAmount: string
-    auctionCurrency: string
-    expiresAt: string
-}
-
-export interface ZoraBid {
-    transaction: ZoraTransactionEvent
-}
-
-export interface ZoraAsk {
-    transaction: ZoraTransactionEvent
-}
-
-export interface ZoraToken {
+export interface Token<T = unknown> {
+    collectionAddress: string
+    collectionName?: string
     tokenId: string
-    name: string
+    mimtInfo?: {
+        mintContext: Transaction
+        originatorAddress: string
+        toAddress: string
+        price: PriceAtTime
+    }
+    networkInfo: Network
+    tokenUrl?: string
+    tokenUrlMimeType?: string
+    content?: Media
+    image?: Media
+    owner?: string
+    tokenContract?: {
+        chain: number
+        name?: string
+        symbol?: string
+        description?: string
+        totalSupply?: number
+        network?: string
+        collectionAddress: string
+    }
+    name?: string
+    description?: string
+    metadata?: T
+    attributes?: Attribute[]
+    lastRefreshTime?: string
+}
+
+export interface Collection {
     address: string
-    currentAuction: AuctionInfo
-    metadata: TokenMetadata
-    tokenContract: TokenContract
-    v3Ask: V3TokenAsk
-    symbol: string
-    owner: string
-}
-
-export interface TokenMetadata {
-    json: MetadataInfo
-}
-
-export interface MetadataInfo {
-    animation_url: string
-    image_url: string
-    created_by: string
-    attributes: TokenAttributes[]
-    name: string
     description: string
-    image: string
+    name?: string
+    symbol?: string
+    totalSupply?: number
+    networkInfo: Network
+    attributes: CollectionAttribute[]
 }
 
-export interface TokenContract {
-    name: string
+export enum EventType {
+    APPROVAL_EVENT = 'APPROVAL_EVENT',
+    LIL_NOUNS_AUCTION_EVENT = 'LIL_NOUNS_AUCTION_EVENT',
+    NOUNS_AUCTION_EVENT = 'NOUNS_AUCTION_EVENT',
+    SALE_EVENT = 'SALE_EVENT',
+    SEAPORT_EVENT = 'SEAPORT_EVENT',
+    MINT_EVENT = 'MINT_EVENT',
+    TRANSFER_EVENT = 'TRANSFER_EVENT',
+    V1_MARKET_EVENT = 'V1_MARKET_EVENT',
+    V2_AUCTION_EVENT = 'V2_AUCTION_EVENT',
+    V3_ASK_EVENT = 'V3_ASK_EVENT',
+    V3_RESERVE_AUCTION_EVENT = 'V3_RESERVE_AUCTION_EVENT',
 }
 
-export interface V3TokenAsk {
-    askPrice: string
+export enum ApprovalEventType {
+    APPROVAL = 'APPROVAL',
+    APPROVAL_FOR_ALL = 'APPROVAL_FOR_ALL',
 }
 
-export interface TokenAttributes {
-    trait_type: string
-    value: string
+export enum LilNounsAuctionEventType {
+    LIL_NOUNS_AUCTION_HOUSE_AUCTION_CREATED_EVENT = 'LIL_NOUNS_AUCTION_HOUSE_AUCTION_CREATED_EVENT',
+    LIL_NOUNS_AUCTION_HOUSE_AUCTION_BID_EVENT = 'LIL_NOUNS_AUCTION_HOUSE_AUCTION_BID_EVENT',
+    LIL_NOUNS_AUCTION_HOUSE_AUCTION_EXTENDED_EVENT = 'LIL_NOUNS_AUCTION_HOUSE_AUCTION_EXTENDED_EVENT',
+    LIL_NOUNS_AUCTION_HOUSE_AUCTION_SETTLED_EVENT = 'LIL_NOUNS_AUCTION_HOUSE_AUCTION_SETTLED_EVENT',
+    LIL_NOUNS_AUCTION_HOUSE_AUCTION_TIME_BUFFER_UPDATED_EVENT = 'LIL_NOUNS_AUCTION_HOUSE_AUCTION_TIME_BUFFER_UPDATED_EVENT',
+    LIL_NOUNS_AUCTION_HOUSE_AUCTION_RESERVE_PRICE_UPDATED_EVENT = 'LIL_NOUNS_AUCTION_HOUSE_AUCTION_RESERVE_PRICE_UPDATED_EVENT',
+    LIL_NOUNS_AUCTION_HOUSE_AUCTION_MIN_BID_INCREMENT_PERCENTAGE_UPDATED = 'LIL_NOUNS_AUCTION_HOUSE_AUCTION_MIN_BID_INCREMENT_PERCENTAGE_UPDATED',
+}
+
+export enum NounsAuctionEventType {
+    NOUNS_AUCTION_HOUSE_AUCTION_CREATED_EVENT = 'NOUNS_AUCTION_HOUSE_AUCTION_CREATED_EVENT',
+    NOUNS_AUCTION_HOUSE_AUCTION_BID_EVENT = 'NOUNS_AUCTION_HOUSE_AUCTION_BID_EVENT',
+    NOUNS_AUCTION_HOUSE_AUCTION_EXTENDED_EVENT = 'NOUNS_AUCTION_HOUSE_AUCTION_EXTENDED_EVENT',
+    NOUNS_AUCTION_HOUSE_AUCTION_SETTLED_EVENT = 'NOUNS_AUCTION_HOUSE_AUCTION_SETTLED_EVENT',
+    NOUNS_AUCTION_HOUSE_AUCTION_TIME_BUFFER_UPDATED_EVENT = 'NOUNS_AUCTION_HOUSE_AUCTION_TIME_BUFFER_UPDATED_EVENT',
+    NOUNS_AUCTION_HOUSE_AUCTION_RESERVE_PRICE_UPDATED_EVENT = 'NOUNS_AUCTION_HOUSE_AUCTION_RESERVE_PRICE_UPDATED_EVENT',
+    NOUNS_AUCTION_HOUSE_AUCTION_MIN_BID_INCREMENT_PERCENTAGE_UPDATED = 'NOUNS_AUCTION_HOUSE_AUCTION_MIN_BID_INCREMENT_PERCENTAGE_UPDATED',
+}
+
+export enum SaleType {
+    FOUNDATION_SALE = 'FOUNDATION_SALE',
+    NOUNS_AUCTION_SALE = 'NOUNS_AUCTION_SALE',
+    LIL_NOUNS_AUCTION_SALE = 'LIL_NOUNS_AUCTION_SALE',
+    CRYPTOPUNKS_SALE = 'CRYPTOPUNKS_SALE',
+    LOOKS_RARE_SALE = 'LOOKS_RARE_SALE',
+    OPENSEA_SINGLE_SALE = 'OPENSEA_SINGLE_SALE',
+    OPENSEA_BUNDLE_SALE = 'OPENSEA_BUNDLE_SALE',
+    RARIBLE_SALE = 'RARIBLE_SALE',
+    SEAPORT_SALE = 'SEAPORT_SALE',
+    SUPERRARE_SALE = 'SUPERRARE_SALE',
+    ZEROX_SALE = 'ZEROX_SALE',
+    ZORA_V2_AUCTION_SALE = 'ZORA_V2_AUCTION_SALE',
+    ZORA_V3_ASK_SALE = 'ZORA_V3_ASK_SALE',
+}
+
+export enum SeaportEventType {
+    SEAPORT_COUNTER_INCREMENTED_EVENT = 'SEAPORT_COUNTER_INCREMENTED_EVENT',
+    SEAPORT_ORDER_CANCELLED_EVENT = 'SEAPORT_ORDER_CANCELLED_EVENT',
+    SEAPORT_ORDER_FULFILLED_EVENT = 'SEAPORT_ORDER_FULFILLED_EVENT',
+    SEAPORT_ORDER_VALIDATED_EVENT = 'SEAPORT_ORDER_VALIDATED_EVENT',
+}
+
+export enum V1MarketEventType {
+    V1_MARKET_BID_CREATED = 'V1_MARKET_BID_CREATED',
+    V1_MARKET_BID_REMOVED = 'V1_MARKET_BID_REMOVED',
+    V1_MARKET_BID_FINALIZED = 'V1_MARKET_BID_FINALIZED',
+    V1_MARKET_ASK_CREATED = 'V1_MARKET_ASK_CREATED',
+    V1_MARKET_ASK_REMOVED = 'V1_MARKET_ASK_REMOVED',
+    V1_MARKET_BID_SHARE_UPDATED = 'V1_MARKET_BID_SHARE_UPDATED',
+}
+
+export enum V2AuctionEventType {
+    V2_AUCTION_CREATED = 'V2_AUCTION_CREATED',
+    V2_AUCTION_CANCELED = 'V2_AUCTION_CANCELED',
+    V2_AUCTION_RESERVE_PRICE_UPDATED = 'V2_AUCTION_RESERVE_PRICE_UPDATED',
+    V2_AUCTION_BID = 'V2_AUCTION_BID',
+    V2_AUCTION_DURATION_EXTENDED = 'V2_AUCTION_DURATION_EXTENDED',
+    V2_AUCTION_APPROVAL_UPDATED = 'V2_AUCTION_APPROVAL_UPDATED',
+    V2_AUCTION_ENDED = 'V2_AUCTION_ENDED',
+}
+
+export enum V3AskEventType {
+    V3_ASK_CREATED = 'V3_ASK_CREATED',
+    V3_ASK_CANCELED = 'V3_ASK_CANCELED',
+    V3_ASK_PRICE_UPDATED = 'V3_ASK_PRICE_UPDATED',
+    V3_ASK_FILLED = 'V3_ASK_FILLED',
+    V3_PRIVATE_ASK_CREATED = 'V3_PRIVATE_ASK_CREATED',
+    V3_PRIVATE_ASK_CANCELED = 'V3_PRIVATE_ASK_CANCELED',
+    V3_PRIVATE_ASK_PRICE_UPDATED = 'V3_PRIVATE_ASK_PRICE_UPDATED',
+    V3_PRIVATE_ASK_FILLED = 'V3_PRIVATE_ASK_FILLED',
+}
+
+export enum V3ReserveAuctionEventType {
+    V3_RESERVE_AUCTION_CREATED = 'V3_RESERVE_AUCTION_CREATED',
+    V3_RESERVE_AUCTION_CANCELED = 'V3_RESERVE_AUCTION_CANCELED',
+    V3_RESERVE_AUCTION_RESERVE_PRICE_UPDATED = 'V3_RESERVE_AUCTION_RESERVE_PRICE_UPDATED',
+    V3_RESERVE_AUCTION_BID = 'V3_RESERVE_AUCTION_BID',
+    V3_RESERVE_AUCTION_ENDED = 'V3_RESERVE_AUCTION_ENDED',
+}
+
+export interface ApprovalEventProperty {
+    approvalEventType: ApprovalEventType
+    collectionAddress: string
+    ownerAddress: string
+    approvedAddress: string
+    tokenid?: string
+    approved?: boolean
+}
+
+export interface LilNounsAuctionEventProperty<T = unknown> {
+    lilNounsAuctionEventType: LilNounsAuctionEventType
+    address: string
+    collectionAddress: string
+    tokenId: string
+    properties: T[]
+}
+
+export interface MintEventProperty {
+    tokenId: string
+    collectionAddress: string
+    originatorAddress: string
+    toAddress: string
+    price: PriceAtTime
+}
+
+export interface NounsAuctionEventProperty<T = unknown> {
+    nounsAuctionEventType: NounsAuctionEventType
+    address: string
+    collectionAddress: string
+    tokenId: string
+    properties: T[]
+}
+
+export interface SaleEventProperty {
+    saleContractAddress?: String
+    transactionInfo: Transaction
+    networkInfo: Network
+    buyerAddress: string
+    collectionAddress: string
+    saleType: SaleType
+    price: PriceAtTime
+    sellerAddress: string
+    tokenId: string
+}
+
+export interface SeaportEventProperty<T = unknown> {
+    address: string
+    eventType: SeaportEventType
+    offerer: string
+    orderHash?: string
+    zone?: string
+    properties?: T[]
+}
+
+export interface TransferEventProperty {
+    fromAddress: string
+    toAddress: string
+    collectionAddress: string
+    tokenId: string
+}
+
+export interface V1MarketEventProperty<T = unknown> {
+    v1MarketEventType: V1MarketEventType
+    address: string
+    collectionAddress: string
+    tokenId: string
+    properties: T[]
+}
+
+export interface V2AuctionEventProperty<T = unknown> {
+    v2AuctionEventType: V2AuctionEventType
+    address: string
+    auctionId: number
+    collectionAddress: string
+    tokenId: string
+    properties: T[]
+}
+
+export interface V3AskEventProperty<T = unknown> {
+    v3AskEventType: V3AskEventType
+    address: string
+    collectionAddress: string
+    tokenId: string
+    properties: T[]
+}
+
+export interface V3ReserveAuctionEventProperty<T = unknown> {
+    eventType: V3ReserveAuctionEventType
+    address: string
+    tokenContract: string
+    tokenId: string
+    properties: T[]
+}
+
+export interface Event {
+    networkInfo: Network
+    transactionInfo: Transaction
+    eventType: EventType
+    collectionAddress?: string
+    tokenId?: string
+    properties?: Array<
+        | ApprovalEventProperty
+        | LilNounsAuctionEventProperty
+        | MintEventProperty
+        | NounsAuctionEventProperty
+        | SaleEventProperty
+        | SeaportEventProperty
+        | TransferEventProperty
+        | V1MarketEventProperty
+        | V2AuctionEventProperty
+        | V3AskEventProperty
+        | V3ReserveAuctionEventProperty
+    >
 }
