6a070a886e96dbe2c0899392e63933dfdca0ded8
Hancheng Zhou <z308114274@gmail.com>
2022-08-01 21:01:28 +0800

    feat: update app board settings (#6936)
    * feat: update app board settings
    
    * fix: lucky drop history ui

--- packages/mask/src/components/shared/ApplicationBoardDialog.tsx
+++ packages/mask/src/components/shared/ApplicationBoardDialog.tsx
@@ -1,7 +1,9 @@
-import { DialogContent } from '@mui/material'
+import { DialogContent, Tab } from '@mui/material'
+import { TabContext, TabPanel } from '@mui/lab'
 import { useState, useCallback } from 'react'
-import { ApplicationSettingDialog } from './ApplicationSettingDialog'
-import { makeStyles } from '@masknet/theme'
+import { ApplicationSettingPluginSwitch } from './ApplicationSettingPluginSwitch'
+import { ApplicationSettingPluginList } from './ApplicationSettingPluginList'
+import { makeStyles, MaskTabList, useTabs } from '@masknet/theme'
 import { InjectedDialog } from '@masknet/shared'
 import { CrossIsolationMessages } from '@masknet/shared-base'
 import { useRemoteControlledDialog } from '@masknet/shared-base-ui'
@@ -10,12 +12,12 @@ import { WalletMessages } from '../../plugins/Wallet/messages'
 import { useI18N } from '../../utils'
 import { Icons } from '@masknet/icons'
 
-const useStyles = makeStyles()((theme) => {
+const useStyles = makeStyles<{ openSettings: boolean }>()((theme, { openSettings }) => {
     return {
         content: {
             padding: theme.spacing(1.5, 2, '6px'),
-            height: 470,
-            overflow: 'hidden',
+            height: openSettings ? 'auto' : 470,
+            overflow: 'scroll',
         },
         settingIcon: {
             cursor: 'pointer',
@@ -23,10 +25,16 @@ const useStyles = makeStyles()((theme) => {
     }
 })
 
+export enum ApplicationSettingTabs {
+    pluginList = 'pluginList',
+    pluginSwitch = 'pluginSwitch',
+}
+
 export function ApplicationBoardDialog() {
-    const { classes } = useStyles()
-    const { t } = useI18N()
     const [openSettings, setOpenSettings] = useState(false)
+    const { classes } = useStyles({ openSettings })
+    const { t } = useI18N()
+    const [currentTab, onChange, tabs] = useTabs(ApplicationSettingTabs.pluginList, ApplicationSettingTabs.pluginSwitch)
 
     const { open, closeDialog: _closeDialog } = useRemoteControlledDialog(
         WalletMessages.events.ApplicationDialogUpdated,
@@ -41,18 +49,40 @@ export function ApplicationBoardDialog() {
     }, [])
 
     return open ? (
-        <InjectedDialog
-            open={open}
-            maxWidth="sm"
-            onClose={closeDialog}
-            title={t('applications')}
-            titleTail={<Icons.Gear size={24} className={classes.settingIcon} onClick={() => setOpenSettings(true)} />}>
-            <DialogContent className={classes.content}>
-                <ApplicationBoard closeDialog={closeDialog} />
-                {openSettings ? (
-                    <ApplicationSettingDialog open={openSettings} onClose={() => setOpenSettings(false)} />
-                ) : null}
-            </DialogContent>
-        </InjectedDialog>
+        <TabContext value={currentTab}>
+            <InjectedDialog
+                open={open}
+                maxWidth="sm"
+                onClose={openSettings ? () => setOpenSettings(false) : closeDialog}
+                titleTabs={
+                    openSettings ? (
+                        <MaskTabList variant="base" onChange={onChange} aria-label="ApplicationBoard">
+                            <Tab label={t('application_settings_tab_app_list')} value={tabs.pluginList} />
+                            <Tab label={t('application_settings_tab_plug_in_switch')} value={tabs.pluginSwitch} />
+                        </MaskTabList>
+                    ) : null
+                }
+                title={t('applications')}
+                titleTail={
+                    openSettings ? null : (
+                        <Icons.Gear size={24} className={classes.settingIcon} onClick={() => setOpenSettings(true)} />
+                    )
+                }>
+                <DialogContent className={classes.content}>
+                    {openSettings ? (
+                        <>
+                            <TabPanel value={tabs.pluginList} style={{ padding: 0 }}>
+                                <ApplicationSettingPluginList />
+                            </TabPanel>
+                            <TabPanel value={tabs.pluginSwitch} style={{ padding: 0 }}>
+                                <ApplicationSettingPluginSwitch />
+                            </TabPanel>
+                        </>
+                    ) : (
+                        <ApplicationBoard closeDialog={closeDialog} />
+                    )}
+                </DialogContent>
+            </InjectedDialog>
+        </TabContext>
     ) : null
 }
