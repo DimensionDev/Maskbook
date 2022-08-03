27ad7e950a8fc2df54d4fd45a9dd13b3f71668e1
billy <Billy@mask.io>
2022-08-04 01:49:10 +0800

    fix: typo

--- packages/web3-providers/src/opensea/index.ts
+++ packages/web3-providers/src/opensea/index.ts
@@ -334,7 +334,7 @@ export class OpenSeaAPI implements NonFungibleTokenAPI.Provider<ChainId, SchemaT
             urlcat('/api/v1/asset/:address/:tokenId', { address, tokenId }),
             chainId,
         )
-        console.log(response, 'respon')
+        console.log(response, 'response')
         if (!response) return
         return createNFTAsset(chainId, response)
     }
