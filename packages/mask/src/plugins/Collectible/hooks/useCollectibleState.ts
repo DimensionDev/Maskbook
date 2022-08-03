652bef85f188149d29357950908334b0ee15e539
billy <Billy@mask.io>
2022-08-04 01:39:03 +0800

    feat: orders events hook

--- packages/mask/src/plugins/Collectible/hooks/useCollectibleState.ts
+++ packages/mask/src/plugins/Collectible/hooks/useCollectibleState.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { createContainer } from 'unstated-next'
 import type { CollectibleToken } from '../types'
-import { useNonFungibleAsset } from '@masknet/plugin-infra/web3'
+import { useNonFungibleAsset, useNonFungibleOrders, useNonFungibleEvents } from '@masknet/plugin-infra/web3'
 import { NetworkPluginID, SourceType } from '@masknet/web3-shared-base'
 import { ChainId } from '@masknet/web3-shared-evm'
 
@@ -13,9 +13,31 @@ function useCollectibleState(token?: CollectibleToken) {
         chainId: ChainId.Mainnet,
     })
 
+    const orders = useNonFungibleOrders(
+        NetworkPluginID.PLUGIN_EVM,
+        token?.contractAddress ?? '',
+        token?.tokenId ?? '',
+        {
+            sourceType: provider,
+            chainId: ChainId.Mainnet,
+        },
+    )
+
+    const events = useNonFungibleEvents(
+        NetworkPluginID.PLUGIN_EVM,
+        token?.contractAddress ?? '',
+        token?.tokenId ?? '',
+        {
+            sourceType: provider,
+            chainId: ChainId.Mainnet,
+        },
+    )
+
     return {
         token,
         asset,
+        orders,
+        events,
         provider,
         setProvider,
     }
---
+++ packages/plugin-infra/src/web3/useNonFungibleOrders.ts
@@ -0,0 +1,23 @@
+import { useAsyncRetry } from 'react-use'
+import { NetworkPluginID, OrderSide } from '@masknet/web3-shared-base'
+import type { Web3Helper } from '../web3-helpers'
+import { useAccount } from '../entry-web3'
+import { useWeb3Hub } from './useWeb3Hub'
+
+export function useNonFungibleOrders<S extends 'all' | void = void, T extends NetworkPluginID = NetworkPluginID>(
+    pluginID?: T,
+    address?: string,
+    id?: string,
+    options?: Web3Helper.Web3HubOptionsScope<S, T>,
+) {
+    const account = useAccount(pluginID, options?.account)
+    const hub = useWeb3Hub(pluginID, {
+        account,
+        ...options,
+    })
+
+    return useAsyncRetry(async () => {
+        if (!address || !id || !hub) return
+        return hub.getNonFungibleTokenOrders?.(address, id, OrderSide.Buy, options)
+    }, [address, id, hub])
+}
