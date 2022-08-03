bcb376aef438d6a549cb88c6e81361da38ff92f4
nuanyang233 <nuanyang233@gmail.com>
2022-08-03 23:38:52 +0800

    feat: profile cover realm (#6954)
    * feat: introduce cover realm for plugin
    
    * feat: twitter profile cover selector
    
    * feat: facebook profile cover
    
    * feat: minds profile cover
    
    * feat: example

--- packages/mask/src/social-network-adaptor/twitter.com/ui-provider.ts
+++ packages/mask/src/social-network-adaptor/twitter.com/ui-provider.ts
@@ -33,6 +33,7 @@ import { injectOpenNFTAvatarEditProfileButton, openNFTAvatarSettingDialog } from
 import { injectUserNFTAvatarAtTweet } from './injection/NFT/TweetNFTAvatar'
 import { injectNFTAvatarClipInTwitter } from './injection/NFT/NFTAvatarClip'
 import { TwitterRenderFragments } from './customization/render-fragments'
+import { injectProfileCover } from './injection/ProfileCover'
 
 const useInjectedDialogClassesOverwriteTwitter = makeStyles()((theme) => {
     const smallQuery = `@media (max-width: ${theme.breakpoints.values.sm}px)`
@@ -161,6 +162,7 @@ const twitterUI: SocialNetworkUI.Definition = {
         toolbox: injectToolboxHintAtTwitter,
         searchResult: injectSearchResultBoxAtTwitter,
         profileTab: injectProfileTabAtTwitter,
+        profileCover: injectProfileCover,
         profileTabContent: injectProfileTabContentAtTwitter,
         enhancedPostRenderer: injectPostReplacerAtTwitter,
         pageInspector: injectPageInspectorDefault(),
