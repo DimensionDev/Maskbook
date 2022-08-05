19c393a7f12114a562d4cb9d511b4e88bb78aaf2
Randolph <840094513@qq.com>
2022-08-05 14:56:02 +0800

    feat: change Skeleton for Next ID page

--- packages/mask/src/plugins/NextID/components/NextIdPage.tsx
+++ packages/mask/src/plugins/NextID/components/NextIdPage.tsx
@@ -83,8 +83,6 @@ const useStyles = makeStyles()((theme) => ({
         color: theme.palette.grey[700],
     },
     skeleton: {
-        borderRadius: 8,
-        margin: theme.spacing(1),
         marginTop: 0,
         backgroundColor: theme.palette.background.default,
         height: '196px',
@@ -261,7 +259,7 @@ export function NextIdPage({ persona }: NextIdPageProps) {
     if (loadingBindings || loadingPersona || loadingVerifyInfo) {
         return (
             <>
-                {Array.from({ length: 2 })
+                {Array.from({ length: 1 })
                     .fill(0)
                     .map((_, i) => (
                         <div key={i}>
