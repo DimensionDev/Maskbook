63afa52ff40d65d1bd8c9a3a3b98db8c95db9fa3
Hancheng Zhou <z308114274@gmail.com>
2022-08-05 14:04:06 +0800

    refactor: transaction descriptions (#6933)
    * refactor: transaction descriptions
    
    * fix: check is erc721 contract
    
    * chore: reply code review
    
    * chore: rename

--- packages/web3-shared/base/src/specs/index.ts
+++ packages/web3-shared/base/src/specs/index.ts
@@ -510,6 +510,8 @@ export interface TransactionContext<ChainId, Parameter = string | undefined> {
     value: string
     /** code to deploy */
     code?: string
+    /** transaction hash */
+    hash?: string
     /** methods */
     methods?: Array<{
         /** name */
@@ -1170,13 +1172,12 @@ export interface TransactionFormatterState<ChainId, Parameters, Transaction> {
         chainId: ChainId,
         transaction: Transaction,
         context: TransactionContext<ChainId, Parameters>,
-        isError?: boolean
     ) => Promise<TransactionDescriptor<ChainId, Transaction>>
     /** Elaborate a transaction in a human-readable format. */
     formatTransaction: (
         chainId: ChainId,
         transaction: Transaction,
-        isError?: boolean
+        txHash?: string
     ) => Promise<TransactionDescriptor<ChainId, Transaction>>
 }
 export interface TransactionWatcherState<ChainId, Transaction> {
