f6c430ff676708d0ddc21b651604f6a3a6442ee8
UncleBill <billbill290@gmail.com>
2022-08-09 16:45:05 +0800

    fix: mf-1652 add link to Next.ID (#6996)

--- packages/web3-providers/src/NFTScan/constants.ts
+++ packages/web3-providers/src/NFTScan/constants.ts
@@ -5,4 +5,3 @@ export const NFTSCAN_API = 'https://webapi.nftscan.com'
 export const NFTSCAN_LOGO_BASE = 'https://logo.nftscan.com/logo'
 export const NFTSCAN_URL = 'https://restapi.nftscan.com'
 export const NFTSCAN_BASE_API = urlcat(NFTSCAN_URL, '/api/v2')
-export const NFTSCAN_ACCESS_TOKEN_URL = urlcat(NFTSCAN_URL, '/gw/token')
