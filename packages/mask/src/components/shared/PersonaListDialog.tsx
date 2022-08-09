cb4314351c02c008e5b0b1b4e00d06abc5a48fc4
Lantt <laanfor@gmail.com>
2022-08-09 14:15:40 +0800

    refactor: create sub component for persona list item

--- packages/mask/src/components/shared/PersonaListDialog.tsx
+++ packages/mask/src/components/shared/PersonaListDialog.tsx
@@ -1,14 +1,10 @@
-import { Icons } from '@masknet/icons'
-import { Avatar, Box, Button, DialogContent, Stack, Typography } from '@mui/material'
+import { Button, DialogContent, Stack, Typography } from '@mui/material'
 import {
-    BindingProof,
     EMPTY_LIST,
-    formatPersonaFingerprint,
     isSamePersona,
     isSameProfile,
     NextIDPlatform,
     PersonaIdentifier,
-    PersonaInformation,
     ProfileIdentifier,
 } from '@masknet/shared-base'
 import { useAsync, useAsyncFn, useCopyToClipboard } from 'react-use'
@@ -24,6 +20,8 @@ import { useRemoteControlledDialog } from '@masknet/shared-base-ui'
 import { makeStyles, useCustomSnackbar } from '@masknet/theme'
 import { activatedSocialNetworkUI } from '../../social-network'
 import { PluginNextIDMessages } from '../../plugins/NextID/messages'
+import type { PersonaNextIDMixture } from './PersonaListUI/PersonaItemUI'
+import { PersonaItemUI } from './PersonaListUI/PersonaItemUI'
 
 const useStyles = makeStyles()((theme) => {
     return {
@@ -43,47 +41,12 @@ const useStyles = makeStyles()((theme) => {
         header: {
             background: theme.palette.maskColor.bottom,
         },
-        nickname: {
-            fontSize: 16,
-            lineHeight: '20px',
-            color: theme.palette.maskColor.main,
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            whiteSpace: 'nowrap',
-        },
-        fingerprint: {
-            fontSize: 12,
-            lineHeight: '16px',
-            color: theme.palette.maskColor.second,
-        },
-        copyIcon: {
-            color: 'red',
-        },
-        dot: {
-            display: 'inline-block',
-            background: '#2DDF00',
-            borderRadius: '50%',
-            width: 7,
-            height: 7,
-            position: 'absolute',
-            left: '77.62%',
-            right: '5.84%',
-            top: '5.84%',
-            bottom: '77.62',
-            border: `1px solid ${theme.palette.maskColor.bottom}`,
-        },
     }
 })
 
 interface PersonaListProps {
     open: boolean
     onClose(): void
-}
-
-interface PersonaNextIDMixture {
-    persona: PersonaInformation
-    proof: BindingProof[]
-    avatar?: string
 }
 
 export const PersonaListDialog = ({ open, onClose }: PersonaListProps) => {
@@ -226,71 +189,15 @@ export const PersonaListDialog = ({ open, onClose }: PersonaListProps) => {
                 <Stack gap={1.5}>
                     {personas.map((x) => {
                         return (
-                            <Stack
+                            <PersonaItemUI
                                 key={x.persona.identifier.toText()}
-                                direction="row"
-                                alignItems="center"
-                                gap={1}
-                                onClick={() => onSelectPersona(x)}>
-                                <Box flexGrow={0} position="relative">
-                                    {x.avatar && (
-                                        <Avatar
-                                            src={x.avatar}
-                                            sx={{
-                                                width: 30,
-                                                height: 30,
-                                                display: 'inline-block',
-                                                borderRadius: '50%',
-                                            }}
-                                        />
-                                    )}
-                                    {!x.avatar && <Icons.MenuPersonasActive size={30} />}
-                                    {isSamePersona(currentPersonaIdentifier, x.persona) && (
-                                        <Box className={classes.dot} />
-                                    )}
-                                </Box>
-                                <Stack flexGrow={1}>
-                                    <Typography className={classes.nickname}>
-                                        <Stack
-                                            component="span"
-                                            display="inline-flex"
-                                            direction="row"
-                                            alignItems="center"
-                                            gap={0.25}>
-                                            {x.persona.nickname}
-                                            <>
-                                                {!!x.proof.find(
-                                                    (p) =>
-                                                        p.identity.toLowerCase() ===
-                                                        currentProfileIdentify.identifier?.userId.toLowerCase(),
-                                                ) && <Icons.NextIDMini width={32} height={18} />}
-                                            </>
-                                        </Stack>
-                                    </Typography>
-                                    <Typography className={classes.fingerprint}>
-                                        <Stack
-                                            component="span"
-                                            display="inline-flex"
-                                            direction="row"
-                                            alignItems="center"
-                                            gap={0.25}>
-                                            {formatPersonaFingerprint(x.persona.identifier.rawPublicKey, 4)}
-                                            <Icons.Copy
-                                                style={{ cursor: 'pointer' }}
-                                                size={14}
-                                                onClick={(e) => onCopyPersons(e, x)}
-                                            />
-                                        </Stack>
-                                    </Typography>
-                                </Stack>
-                                <Stack flexGrow={0}>
-                                    {isSamePersona(selectedPersona?.persona, x.persona) ? (
-                                        <Icons.CheckCircle size={20} />
-                                    ) : (
-                                        <Icons.RadioNo size={20} />
-                                    )}
-                                </Stack>
-                            </Stack>
+                                data={x}
+                                onCopy={(e) => onCopyPersons(e, x)}
+                                onClick={() => onSelectPersona(x)}
+                                currentPersona={selectedPersona}
+                                currentPersonaIdentifier={currentPersonaIdentifier}
+                                currentProfileIdentify={currentProfileIdentify}
+                            />
                         )
                     })}
                 </Stack>
