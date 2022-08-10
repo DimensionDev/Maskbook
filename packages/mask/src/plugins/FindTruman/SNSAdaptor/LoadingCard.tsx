95d9fc19993b98897338b298f1d2b5a50adfab08
UncleBill <billbill290@gmail.com>
2022-08-10 13:10:54 +0800

    chore: simplify some code (#7008)

--- packages/mask/src/plugins/FindTruman/SNSAdaptor/LoadingCard.tsx
+++ packages/mask/src/plugins/FindTruman/SNSAdaptor/LoadingCard.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react'
 import { Skeleton } from '@mui/material'
 import { makeStyles } from '@masknet/theme'
 import { FindTrumanCard } from './FindTrumanCard'
+import { range } from 'lodash-unified'
 
 const useStyles = makeStyles()((theme) => {
     return {
@@ -20,18 +21,16 @@ export function LoadingCard(props: React.PropsWithChildren<{ title: string }>) {
         <Suspense
             fallback={
                 <FindTrumanCard title={props.title}>
-                    {Array.from({ length: 2 })
-                        .fill(0)
-                        .map((_, i) => (
-                            <Skeleton
-                                key={i}
-                                className={classes.skeleton}
-                                animation="wave"
-                                variant="rectangular"
-                                width={i === 0 ? '80%' : '60%'}
-                                height={15}
-                            />
-                        ))}
+                    {range(2).map((i) => (
+                        <Skeleton
+                            key={i}
+                            className={classes.skeleton}
+                            animation="wave"
+                            variant="rectangular"
+                            width={i === 0 ? '80%' : '60%'}
+                            height={15}
+                        />
+                    ))}
                 </FindTrumanCard>
             }>
             {props.children}
