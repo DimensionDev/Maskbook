b0ca8b3bf93450c456129f9c9e8fe6eb2ff18bf2
Lantt <laanfor@gmail.com>
2022-08-11 18:08:57 +0800

    Revert "feat: enable switch for market trend"
    This reverts commit 593d5909b1917bb9281e9132f06952aab0fb6f5d.

--- packages/mask/src/plugins/Trader/SNSAdaptor/trending/TagInspector.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trending/TagInspector.tsx
@@ -5,20 +5,12 @@ import type { DataProvider } from '@masknet/public-api'
 import { TraderView } from './TraderView'
 import { useAvailableDataProviders } from '../../trending/useAvailableDataProviders'
 import { TargetChainIdContext } from '@masknet/plugin-infra/web3-evm'
-import { useIsMinimalMode } from '@masknet/plugin-infra/content-script'
-import { useAsync } from 'react-use'
-import Services from '../../../../extension/service'
 
 export interface TagInspectorProps {}
 
 export function TagInspector(props: TagInspectorProps) {
     // build availability cache in the background page
     useAvailableDataProviders(TagType.CASH, 'BTC')
-    const isMarketTrendingMinimalMode = useIsMinimalMode('io.mask.market-trend')
-
-    const { value } = useAsync(() => {
-        return Services.Settings.getPluginMinimalModeEnabled('io.mask.market-trend')
-    })
 
     const createTrendingView = useCallback(
         (name: string, type: TagType, dataProviders: DataProvider[], reposition?: () => void) => {
