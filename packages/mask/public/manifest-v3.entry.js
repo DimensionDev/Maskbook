5eb16baec6ef2185bd621a35c696a8977ff196e3
Jack Works <jackworks@protonmail.com>
2022-08-16 15:45:59 +0800

    fix: missing ext

--- packages/mask/public/manifest-v3.entry.js
+++ packages/mask/public/manifest-v3.entry.js
@@ -2,6 +2,6 @@ importScripts(
     './worker_env.js',
     './gun.js',
     './polyfill/browser-polyfill.js',
-    './sandboxed-modules/mv3-preload',
+    './sandboxed-modules/mv3-preload.js',
     './js/background.js',
 )
