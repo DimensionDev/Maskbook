2817c7d2e676c4430b3bd1b214b09545d5bd477f
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2022-08-17 16:47:50 +0800

    refactor: update gem api (#7100)

--- packages/web3-providers/src/gem/constants.ts
+++ packages/web3-providers/src/gem/constants.ts
@@ -1,5 +1,5 @@
 import { SourceType } from '@masknet/web3-shared-base'
 
-export const GEM_API_URL = 'https://api-6.gemlabs.xyz/'
+export const GEM_API_URL = 'https://gem-proxy.r2d2.to/'
 
 export const RARITY_SOURCE_TYPE = [SourceType.Gem, SourceType.RaritySniper, SourceType.TraitSniper] as const
