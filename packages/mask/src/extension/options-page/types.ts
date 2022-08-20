10aa7d205d5afef11a815fdf7d0b68c48797e174
nuanyang233 <nuanyang233@gmail.com>
2022-08-20 13:42:48 +0800

    feat: hidden tab content when wallet be hidden

--- packages/mask/src/extension/options-page/types.ts
+++ packages/mask/src/extension/options-page/types.ts
@@ -1,4 +1,6 @@
 import type { NextIDPlatform } from '@masknet/shared-base'
+import type { NetworkPluginID } from '@masknet/web3-shared-base'
+import type { ChainId } from '@masknet/web3-shared-evm'
 
 export enum COLLECTION_TYPE {
     NFTs = 'NFTs',
@@ -11,8 +13,32 @@ export interface CollectionKeys {
     Donations: string[]
     Footprints: string[]
 }
+export interface WalletsCollection {
+    NFTs?: WalletTypes[]
+    donations?: WalletTypes[]
+    footprints?: WalletTypes[]
+}
+
+export interface CollectionTypes {
+    platform: NetworkPluginID
+    address: string // take id as address if collection is a poap
+    key: string // address + tokenId as unique key of NFT, id as unique key of poap
+    tokenId?: string
+    iconURL?: string
+    hidden?: boolean
+    name?: string
+    chainId?: ChainId
+}
+export interface WalletTypes {
+    address: string
+    platform?: NetworkPluginID
+    updateTime?: string
+    collections?: CollectionTypes[]
+}
+
 export interface Patch {
     unListedCollections: Record<string, CollectionKeys>
+    hiddenAddresses: WalletsCollection
 }
 export interface KVType {
     persona: string
