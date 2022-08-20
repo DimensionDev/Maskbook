ed788d191a7aec58fae5d800aa97c1004006c14b
BillyS <Billy@mask.io>
2022-08-21 02:35:24 +0800

    Update packages/web3-shared/base/src/utils/timestamp.ts
    Co-authored-by: UncleBill <billbill290@gmail.com>

--- packages/web3-shared/base/src/utils/timestamp.ts
+++ packages/web3-shared/base/src/utils/timestamp.ts
@@ -2,5 +2,5 @@ const errorTriggerTimestamp = 1262304000000 // 2010-01-01
 
 export function isValidTimestamp(time?: number) {
     if (!time) return
-    return time >= new Date(errorTriggerTimestamp).getTime()
+    return time >= ETHEREUM_GENESIS_BLOCK_DATE
 }
