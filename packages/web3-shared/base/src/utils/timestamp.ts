4ca2185fa0c9f43c742d6af68fd95cec3f4e27fb
guanbinrui <guanbinrui@dimension.im>
2022-08-21 02:49:46 +0800

    fix: build error

--- packages/web3-shared/base/src/utils/timestamp.ts
+++ packages/web3-shared/base/src/utils/timestamp.ts
@@ -1,6 +1,6 @@
-const errorTriggerTimestamp = 1262304000000 // 2010-01-01
+const ETHEREUM_GENESIS_BLOCK_DATE = 1262304000000 // 2010-01-01
 
 export function isValidTimestamp(time?: number) {
-    if (!time) return
+    if (!time) return false
     return time >= ETHEREUM_GENESIS_BLOCK_DATE
 }
