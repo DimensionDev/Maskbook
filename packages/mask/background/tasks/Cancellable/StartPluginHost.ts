d51f1d14882f945cada0781e2f59bd31edcd242b
Jack Works <jackworks@protonmail.com>
2022-08-24 12:33:02 +0800

    feat: initial impl

--- packages/mask/background/tasks/Cancellable/StartPluginHost.ts
+++ packages/mask/background/tasks/Cancellable/StartPluginHost.ts
@@ -3,9 +3,11 @@ import { createPluginDatabase } from '../../database/plugin-db'
 import { createPluginHost, createSharedContext } from '../../../shared/plugin-infra/host'
 import { getPluginMinimalModeEnabled } from '../../services/settings/old-settings-accessor'
 import { hmr } from '../../../utils-pure'
+import { startBackgroundHost } from '@masknet/sandboxed-plugin-runtime/background'
 
 const { signal } = hmr(import.meta.webpackHot)
 startPluginWorker(createPluginHost(signal, createWorkerContext, getPluginMinimalModeEnabled))
+startBackgroundHost(signal, process.env.NODE_ENV === 'development')
 
 function createWorkerContext(pluginID: string, signal: AbortSignal): Plugin.Worker.WorkerContext {
     let storage: Plugin.Worker.DatabaseStorage<any> = undefined!
