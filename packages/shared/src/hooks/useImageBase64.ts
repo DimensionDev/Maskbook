bf794d7411ad53bc932961285f3376ee0016d951
nuanyang233 <nuanyang233@gmail.com>
2022-08-26 15:17:34 +0800

    fix: token icon

--- packages/shared/src/hooks/useImageBase64.ts
+++ packages/shared/src/hooks/useImageBase64.ts
@@ -46,11 +46,18 @@ export function useAccessibleUrl(
         }
 
         if (!url) return
-        const fetchingTask = globalThis.r2d2Fetch(`https://cors.r2d2.to/?${url}`)
+        const fetchingTask = fetch(url, {
+            headers: {
+                accept: url.includes('imagedelivery.net')
+                    ? 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8'
+                    : '',
+            },
+        })
         cache.set(key, fetchingTask)
         const response = await fetchingTask
         if (!response.ok) {
             cache.delete(key)
+            setAvailableUrl('')
             return
         }
 
