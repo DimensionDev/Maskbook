9964c556fc1a00cbae1fc674f3c23bfe23fce6ef
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2022-08-30 21:36:34 +0800

    refacotr: adjust test cases for resolveIPFSLink (#7250)

--- packages/web3-shared/base/tests/resolver.ts
+++ packages/web3-shared/base/tests/resolver.ts
@@ -1,8 +1,12 @@
+/* cspell:disable */
 import { describe, test, expect } from 'vitest'
 import { resolveCORSLink, resolveIPFSLink } from '../src/utils/resolver'
 
 const MASK_URL = 'https://mask.io'
-const HASH = 'QmYE5ZAofUqGV7ph3owYBDTmGkZU44teCVAzLrKWwHCURM'
+const CHROME_EXTENSION_URL = 'chrome-extension://jkoeaghipilijlahjplgbfiocjhldnap/logo.png'
+const MOZ_EXTENSION_URL = 'moz-extension://jkoeaghipilijlahjplgbfiocjhldnap/logo.png'
+const CIDv0 = 'QmYE5ZAofUqGV7ph3owYBDTmGkZU44teCVAzLrKWwHCURM'
+const CIDv1 = 'bafkreiewefwtzgekhxv5r5hnvjisypqq5jwd2lqbex4a33ahhz3663qngy'
 const IMAGE_DATA_PARTIAL = 'data:image/png;base64,iVB'
 
 describe('resolveCORSLink', () => {
@@ -10,42 +14,31 @@ describe('resolveCORSLink', () => {
         { give: '', expected: '' },
         { give: MASK_URL, expected: `https://cors.r2d2.to?${encodeURIComponent(MASK_URL)}` },
         { give: MASK_URL, expected: `https://cors.r2d2.to?${encodeURIComponent(MASK_URL)}` },
-        // cspell:ignore jkoeaghipilijlahjplgbfiocjhldnap
-        {
-            give: 'chrome-extension://jkoeaghipilijlahjplgbfiocjhldnap/logo.png',
-            expected: 'chrome-extension://jkoeaghipilijlahjplgbfiocjhldnap/logo.png',
-        },
-        {
-            give: 'moz-extension://jkoeaghipilijlahjplgbfiocjhldnap/logo.png',
-            expected: 'moz-extension://jkoeaghipilijlahjplgbfiocjhldnap/logo.png',
-        },
+        { give: CHROME_EXTENSION_URL, expected: CHROME_EXTENSION_URL },
+        { give: MOZ_EXTENSION_URL, expected: MOZ_EXTENSION_URL },
+        { give: IMAGE_DATA_PARTIAL, expected: IMAGE_DATA_PARTIAL },
     ])('.resolveCORSLink', ({ give, expected }) => {
         expect(resolveCORSLink(give)).toBe(expected)
     })
 })
 
 describe('resolveIPFSLink', () => {
-    test.each(
-        [
+    const cases = [CIDv0, CIDv1].flatMap((cid) => {
+        return [
             { give: '', expected: '' },
-            { give: HASH, expected: `https://ipfs.io/ipfs/${HASH}` },
-            { give: `${HASH}/image`, expected: `https://ipfs.io/ipfs/${HASH}/image` },
-            // cspell:disable
-            {
-                give: 'bafkreiewefwtzgekhxv5r5hnvjisypqq5jwd2lqbex4a33ahhz3663qngy',
-                expected: 'https://ipfs.io/ipfs/bafkreiewefwtzgekhxv5r5hnvjisypqq5jwd2lqbex4a33ahhz3663qngy',
-            },
-            // cspell:disable
-            { give: `ipfs://${HASH}`, expected: `https://ipfs.io/ipfs/${HASH}` },
-            { give: `ipfs://ipfs/${HASH}`, expected: `https://ipfs.io/ipfs/${HASH}` },
+            { give: cid, expected: `https://ipfs.io/ipfs/${cid}` },
+            { give: `${cid}/image`, expected: `https://ipfs.io/ipfs/${cid}/image` },
+            { give: `ipfs://${cid}`, expected: `https://ipfs.io/ipfs/${cid}` },
+            { give: `ipfs://ipfs/${cid}`, expected: `https://ipfs.io/ipfs/${cid}` },
             { give: `https://ipfs.io/ipfs/${IMAGE_DATA_PARTIAL}`, expected: IMAGE_DATA_PARTIAL },
-            { give: `https://ipfs.io/ipfs/${HASH}`, expected: `https://ipfs.io/ipfs/${HASH}` },
+            { give: `https://ipfs.io/ipfs/${cid}`, expected: `https://ipfs.io/ipfs/${cid}` },
             { give: MASK_URL, expected: MASK_URL },
-        ].flatMap(({ give, expected }) => [
+        ].map(({ give, expected }) => [
             { give, expected },
             { give: `https://cors.r2d2.to?${encodeURIComponent(give)}`, expected },
-        ]),
-    )('.resolveIPFSLink($give)', ({ give, expected }) => {
+        ])
+    })
+    test.each(cases)('.resolveIPFSLink($give)', ({ give, expected }) => {
         expect(resolveIPFSLink(give)).toBe(expected)
     })
 })
