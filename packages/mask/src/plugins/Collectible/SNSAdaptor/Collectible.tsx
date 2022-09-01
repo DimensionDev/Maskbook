f45db145ebefc5189163d5cd027e554d7e579079
guanbinrui <guanbinrui@dimension.im>
2022-09-01 11:15:21 +0800

    refactor: i18n

--- packages/mask/src/plugins/Collectible/SNSAdaptor/Collectible.tsx
+++ packages/mask/src/plugins/Collectible/SNSAdaptor/Collectible.tsx
@@ -173,7 +173,7 @@ export function Collectible(props: CollectibleProps) {
                     <Box sx={{ flex: 1, padding: 1 }}> {CollectibleProviderSwitcher}</Box>
                     <Box sx={{ flex: 1, padding: 1 }}>
                         <Button fullWidth onClick={() => asset.retry()} variant="roundedDark">
-                            Refresh
+                            {t('refresh')}
                         </Button>
                     </Box>
                 </Box>
