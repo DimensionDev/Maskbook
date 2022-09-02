e30b94834ee39132ca100c5c84e0b68fefc37408
UncleBill <billbill290@gmail.com>
2022-09-02 18:18:33 +0800

    fix(ProfileCard): show transaction activities (#7279)
    * fix(ProfileCard): show transaction activities
    
    * fixup! fix(ProfileCard): show transaction activities

--- packages/plugins/RSS3/src/SNSAdaptor/hooks/useNormalizedFeed.ts
+++ packages/plugins/RSS3/src/SNSAdaptor/hooks/useNormalizedFeed.ts
@@ -13,7 +13,7 @@ export function useNormalizeFeed(feed: RSS3BaseAPI.Activity): RSS3Feed {
                 image: metadata?.image || RSS3_DEFAULT_IMAGE,
                 title: metadata?.name,
                 relatedURLs: action.related_urls,
-                description: metadata?.description ?? metadata?.name,
+                description: metadata?.description ?? '',
                 network: feed.network,
                 attributes: metadata && 'attributes' in metadata ? metadata.attributes : [],
                 metadata: {
@@ -42,7 +42,7 @@ export function useNormalizeFeed(feed: RSS3BaseAPI.Activity): RSS3Feed {
                 image: metadata?.image || RSS3_DEFAULT_IMAGE,
                 title: action.type,
                 relatedURLs: action.related_urls,
-                description: metadata?.description,
+                description: metadata?.value_display ? `${metadata?.value_display} ${metadata?.symbol}` : '',
                 network: feed.network,
                 attributes: [],
                 metadata: {
