2d1ceb936403d1fc478ba8322911043c62e30140
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2022-09-02 11:31:20 +0800

    refactor: CORS replacer (#7272)

--- packages/web3-shared/base/src/utils/resolver.ts
+++ packages/web3-shared/base/src/utils/resolver.ts
@@ -242,7 +242,7 @@ export function resolveIPFSLink(cidOrURL?: string): string | undefined {
 
     // eliminate cors proxy
     if (cidOrURL.startsWith(CORS_HOST)) {
-        return resolveIPFSLink(decodeURIComponent(cidOrURL.replace(`${CORS_HOST}?`, '')))
+        return resolveIPFSLink(decodeURIComponent(cidOrURL.replace(new RegExp(`^${CORS_HOST}\??`), '')))
     }
 
     // a ipfs protocol
