2c8cad551d4ee523a3619302b95abb883fe34a7e
unclebill <billbill290@gmail.com>
2022-09-05 15:27:58 +0800

    fix: don't add tips button alongside follow button for now

--- packages/mask/src/social-network-adaptor/twitter.com/injection/Tips/index.tsx
+++ packages/mask/src/social-network-adaptor/twitter.com/injection/Tips/index.tsx
@@ -1,7 +1,5 @@
 import { injectOpenTipsButtonOnProfile } from './ProfileTipButton'
-import { injectTipsButtonOnFollowButton } from './FollowTipsButton'
 
 export function injectTips(signal: AbortSignal) {
     injectOpenTipsButtonOnProfile(signal)
-    injectTipsButtonOnFollowButton(signal)
 }
