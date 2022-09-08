85f7a21f9768739593bec0902e62a56114d880ea
unclebill <billbill290@gmail.com>
2022-09-08 13:41:44 +0800

    fix: mf-1987 can not predict csp blocking, manually check accessible origins

--- packages/shared/src/hooks/useImageURL.ts
+++ packages/shared/src/hooks/useImageURL.ts
@@ -1,6 +1,14 @@
 import { useAsync } from 'react-use'
 import { hasIPFS_CID, isLocaleResource, resolveIPFS_URL, resolveLocalResource } from '@masknet/web3-shared-base'
 
+// From manifest.json.
+const ACCESSIBLE_ORIGINS = [
+    'https://gateway.ipfscdn.io',
+    'https://logo.nftscan.com',
+    'https://imagedelivery.net',
+    'https://raw.githubusercontent.com/dimensiondev',
+]
+
 export function useImageURL(url?: string) {
     return useAsync(async () => {
         if (!url) return
@@ -9,8 +17,19 @@ export function useImageURL(url?: string) {
             const startsWithHTTPS = /^https?:\/\//.test(url)
             const hasCID = hasIPFS_CID(url)
 
-            // Unknown type, fallback to fetch, aka. r2d2Fetch.
-            if (!startsWithHTTPS && !hasCID) throw new TypeError('Invalid url.')
+            let fixedUrl = url
+            if (!startsWithHTTPS) {
+                if (!hasCID) {
+                    // Unknown type, fallback to fetch, aka r2d2Fetch.
+                    throw new TypeError('Invalid url.')
+                }
+                fixedUrl = resolveIPFS_URL(fixedUrl)!
+            }
+            if (!ACCESSIBLE_ORIGINS.some((x) => fixedUrl.startsWith(x))) {
+                throw new TypeError(
+                    `${origin} is not accessible origin, consider add it to web_accessible_resources field of manifest.json`,
+                )
+            }
 
             const img = document.createElement('img')
             const cleanup = () => {
@@ -28,7 +47,7 @@ export function useImageURL(url?: string) {
             img.decoding = 'async'
             img.addEventListener('load', onload)
             img.addEventListener('error', onerror)
-            img.src = hasCID ? resolveIPFS_URL(url)! : url
+            img.src = fixedUrl
         }).catch(async () => {
             const response = await fetch(url, {
                 cache: 'force-cache',
