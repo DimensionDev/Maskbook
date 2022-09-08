ef6227f676dd8417b4867b86d27f23706ecde5a0
nuanyang233 <nuanyang233@gmail.com>
2022-09-08 14:19:32 +0800

    chore: fomatter display

--- packages/mask/src/plugins/Trader/SNSAdaptor/trader/ConfirmDialog.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trader/ConfirmDialog.tsx
@@ -2,7 +2,7 @@ import { useValueRef } from '@masknet/shared-base-ui'
 import type { TradeComputed } from '../../types'
 import { createNativeToken, formatUSD, formatWeiToEther, GasOptionConfig } from '@masknet/web3-shared-evm'
 import { useCallback, useMemo, useState } from 'react'
-import { formatBalance, FungibleToken, multipliedBy, NetworkPluginID } from '@masknet/web3-shared-base'
+import { formatBalance, FungibleToken, multipliedBy, NetworkPluginID, scale10 } from '@masknet/web3-shared-base'
 import { TargetChainIdContext } from '@masknet/plugin-infra/web3-evm'
 import { currentSlippageSettings } from '../../settings'
 import { useNativeTokenPrice, useFungibleTokenPrice, Web3Helper } from '@masknet/plugin-infra/web3'
@@ -63,7 +63,9 @@ export function ConfirmDialog(props: ConfirmDialogProps) {
         trade.inputToken?.decimals ?? 0,
     )
 
-    const lostValue = multipliedBy(inputTokenPrice ?? 0, lostToken).toFixed(2)
+    const lostValue = multipliedBy(inputTokenPrice ?? 0, lostToken).lt(scale10(-2))
+        ? '< 0.01'
+        : multipliedBy(inputTokenPrice ?? 0, lostToken).toFixed(2)
 
     const handleOpenPriceImpactDialog = useCallback(() => {
         setPriceImpactDialogOpen(true)
