235dba360f04aa93f7ff2e0e6915bf51bb645d4a
UncleBill <billbill290@gmail.com>
2022-09-13 11:45:30 +0800

    fix: mf-2025 avoid recreating new function each time when access (#7371)

--- packages/shared-base/src/utils/mixin.ts
+++ packages/shared-base/src/utils/mixin.ts
@@ -10,13 +10,21 @@ export function mixin<T, S1, S2, S3, S4>(
 ): T & S1 & S2 & S3 & S4
 export function mixin<R>(target: any, ...others: any[]): R {
     const all = [target, ...others]
+    const map = new WeakMap()
     return new Proxy(
         {},
         {
             get(_, prop, receiver) {
                 for (const source of all) {
-                    if (typeof source[prop] === 'function') return Reflect.get(source, prop, source).bind(source)
-                    if (typeof source[prop] !== 'undefined') return Reflect.get(source, prop, source)
+                    const property = source[prop]
+                    if (typeof property === 'function') {
+                        if (map.has(property)) return map.get(property)
+
+                        const fn = Reflect.get(source, prop, source).bind(source)
+                        map.set(property, fn)
+                        return fn
+                    }
+                    if (typeof property !== 'undefined') return Reflect.get(source, prop, source)
                     continue
                 }
                 return
