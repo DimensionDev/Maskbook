dcfba5a4e73ff5fd0c2cdeb78629f72132fbede1
guanbinrui <guanbinrui@dimension.im>
2022-09-14 21:12:43 +0800

    refactor: rename api

--- packages/plugin-infra/src/components/RemoteView.ts
+++ packages/plugin-infra/src/components/RemoteView.ts
@@ -2,20 +2,20 @@ import React, { createElement, Fragment, useMemo } from 'react'
 import { useActivatedPluginsSNSAdaptor } from '../manager/sns-adaptor'
 import type { Plugin, PluginId } from '../types'
 
-export interface RemoteViewProps<Name extends keyof Plugin.SNSAdaptor.ContributedViewRegistry> {
+export interface WidgetProps<Name extends keyof Plugin.SNSAdaptor.WidgetRegistry> {
     name: Name
     pluginID?: PluginId
     fallback?: React.ReactElement | null
 }
-export const RemoteView: <Name extends keyof Plugin.SNSAdaptor.ContributedViewRegistry>(
-    props: RemoteViewProps<Name> &
-        (Plugin.SNSAdaptor.ContributedViewRegistry[Name] extends infer U extends object ? U : never),
+
+export const Widget: <Name extends keyof Plugin.SNSAdaptor.WidgetRegistry>(
+    props: WidgetProps<Name> & (Plugin.SNSAdaptor.WidgetRegistry[Name] extends infer U extends object ? U : never),
 ) => React.ReactElement = (props) => {
     const { name, pluginID, fallback, ...rest } = props
     const plugins = useActivatedPluginsSNSAdaptor(false)
     const TargetView: any = useMemo(() => {
-        if (pluginID) return plugins.find((x) => x.ID === pluginID)?.ContributedView?.[name]
-        return plugins.find((x) => x.ContributedView && name in x.ContributedView)?.ContributedView![name].component
+        if (pluginID) return plugins.find((x) => x.ID === pluginID)?.Widgets?.[name]
+        return plugins.find((x) => x.Widgets && name in x.Widgets)?.Widgets![name].component
     }, [plugins])
 
     if (!TargetView) return fallback || createElement(Fragment, {})
