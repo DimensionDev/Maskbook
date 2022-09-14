266e587915b720f77698c5fbf972e13750b95ecd
guanbinrui <guanbinrui@dimension.im>
2022-09-14 21:47:31 +0800

    feat: scaffloding of widget dialogs

---
+++ packages/plugins/Debugger/src/SNSAdaptor/components/WidgetContent.tsx
@@ -0,0 +1,7 @@
+export interface WidgetContentProps {
+    onClose?: () => void
+}
+
+export function WidgetContent(props: WidgetContentProps) {
+    return <h1>Widget Content</h1>
+}
---
+++ packages/plugins/Debugger/src/SNSAdaptor/components/WidgetDialog.tsx
@@ -0,0 +1,29 @@
+import { InjectedDialog } from '@masknet/shared'
+import { DialogContent } from '@mui/material'
+import { ChainId } from '@masknet/web3-shared-evm'
+import { NetworkPluginID } from '@masknet/web3-shared-base'
+import { PluginIDContextProvider, PluginWeb3ContextProvider } from '@masknet/plugin-infra/web3'
+import { WidgetContent } from './WidgetContent.js'
+import { useRemoteControlledDialog } from '../../../../../shared-base-ui/src/hooks/index.js'
+import { PluginDebuggerMessages } from '../../messages.js'
+
+export interface WidgetDialogProps {}
+
+export function WidgetDialog(props: WidgetDialogProps) {
+    const { open, closeDialog } = useRemoteControlledDialog(PluginDebuggerMessages.connectionDialogUpdated)
+    return (
+        <InjectedDialog title="Connection" fullWidth open={open} onClose={closeDialog}>
+            <DialogContent>
+                <PluginIDContextProvider value={NetworkPluginID.PLUGIN_EVM}>
+                    <PluginWeb3ContextProvider
+                        pluginID={NetworkPluginID.PLUGIN_EVM}
+                        value={{
+                            chainId: ChainId.Mainnet,
+                        }}>
+                        <WidgetContent onClose={closeDialog} />
+                    </PluginWeb3ContextProvider>
+                </PluginIDContextProvider>
+            </DialogContent>
+        </InjectedDialog>
+    )
+}
