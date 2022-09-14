7e93e45434e0686559e06128d8594d1b38f70cd0
nuanyang233 <nuanyang233@gmail.com>
2022-09-14 15:02:21 +0800

    fix: unescape

--- packages/mask/src/plugins/Trader/SNSAdaptor/trader/components/PriceImpactDialogUI.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trader/components/PriceImpactDialogUI.tsx
@@ -92,6 +92,7 @@ export const PriceImpactDialogUI = memo<PriceImpactDialogProps>(
         const isDashboard = isDashboardPage()
         const { classes } = useStyles({ isDashboard })
 
+        console.log(`${lostToken} ${symbol}`, `${lostValue.replace('<', '&lt;')} USD`)
         return (
             <InjectedDialog open={open} onClose={onClose} title={t.impact_warning()} className={classes.dialog}>
                 <DialogContent className={classes.content}>
@@ -102,7 +103,7 @@ export const PriceImpactDialogUI = memo<PriceImpactDialogProps>(
                             components={{ span: <span /> }}
                             values={{
                                 impact: formatPercentage(priceImpact ?? 0),
-                                lostSymbol: `${lostToken} ${symbol}`,
+                                lostSymbol: `${lostToken.replace('<', '&lt;')} ${symbol}`,
                                 lostValue: `${lostValue.replace('<', '&lt;')} USD`,
                             }}
                             shouldUnescape
