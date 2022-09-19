84d54ad0528ed9bd39a3ef0d3f9f79b3c62b1628
zhouhanseng <z308114274@gmail.com>
2022-09-19 17:24:56 +0800

    feat(ens card): show multi social accounts

---
+++ packages/plugins/ENS/src/SNSAdaptor/NextIdBadge.tsx
@@ -0,0 +1,36 @@
+import { Icons } from '@masknet/icons'
+import { ShadowRootTooltip } from '@masknet/theme'
+import { Typography, Link } from '@mui/material'
+import { Trans } from 'react-i18next'
+import useStyles from './useStyles'
+import { useI18N } from '../locales'
+
+export function NextIdBadge() {
+    const t = useI18N()
+    const { classes } = useStyles()
+
+    return (
+        <ShadowRootTooltip
+            arrow
+            placement="top"
+            title={
+                <Typography style={{ padding: '6px 2px', whiteSpace: 'nowrap' }} fontSize={14}>
+                    <Trans
+                        i18nKey="data_source_from_nextid"
+                        components={{
+                            nextIdLink: (
+                                <Link
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className={classes.nextIdLink}
+                                    href="https://next.id/"
+                                />
+                            ),
+                        }}
+                    />
+                </Typography>
+            }>
+            <Icons.NextIDMini variant="light" width={32} />
+        </ShadowRootTooltip>
+    )
+}
