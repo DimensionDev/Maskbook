63502351b39d26d701c86bfc4961e8bdca0a1c11
Jack Works <jackworks@protonmail.com>
2022-09-20 15:58:20 +0800

    chore: fix

--- packages/mask/src/social-network-adaptor/mirror.xyz/base.ts
+++ packages/mask/src/social-network-adaptor/mirror.xyz/base.ts
@@ -5,7 +5,7 @@ import { EnhanceableSite } from '@masknet/shared-base'
 const origins = ['https://mirror.xyz/*']
 export const mirrorBase: SocialNetwork.Base = {
     networkIdentifier: EnhanceableSite.Mirror,
-    encryptionNetwork: SocialNetworkEnum.Mirror,
+    encryptionNetwork: SocialNetworkEnum.Unknown,
     declarativePermissions: { origins },
     shouldActivate(location) {
         return location.host.endsWith(EnhanceableSite.Mirror)
