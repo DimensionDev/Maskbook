db7850b86eb14564591ceaeab54c69d1f965234a
lelenei <zwpsky@gmail.com>
2022-09-21 08:52:45 +0800

    fix: vcent icon color

--- packages/mask/src/plugins/VCent/SNSAdaptor/index.tsx
+++ packages/mask/src/plugins/VCent/SNSAdaptor/index.tsx
@@ -20,7 +20,7 @@ const sns: Plugin.SNSAdaptor.Definition = {
             name: <Trans i18nKey="plugin_valuables_name" />,
             marketListSortingPriority: 10,
             tutorialLink: 'https://realmasknetwork.notion.site/27424923ee454a4a9b0ed16fc5cb93d0',
-            icon: <Icons.Valuables size={36} variant="light" />,
+            icon: <Icons.Valuables size={36} variant="dark" />,
         },
     ],
     wrapperProps: {
