4a5ef3c9176b0b7a860dc2b2de6447e0b6029b9a
Lantt <laanfor@gmail.com>
2022-09-28 14:01:03 +0800

    fix: ci

--- packages/mask/src/social-network-adaptor/mirror.xyz/collecting/posts.ts
+++ packages/mask/src/social-network-adaptor/mirror.xyz/collecting/posts.ts
@@ -16,7 +16,9 @@ export function queryInjectPoint(node: HTMLElement) {
         [
             // workaround for case: only have one contributor in entry detail page
             // Not find better selector
+            /* cspell:disable-next-line */
             ':scope > div:nth-child(3) > div > div > div:has(div._1sjywpl0._1sjywpl1.bc5nci3lg.bc5nci3rz):not(svg)',
+            /* cspell:disable-next-line */
             ':scope > div:nth-child(4) > div > div > div:has(div._1sjywpl0._1sjywpl1.bc5nci3lg.bc5nci3rz):not(svg)',
 
             // if have header image is 4, either 3
