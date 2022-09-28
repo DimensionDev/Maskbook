cd584860be63ff59726b65a18182ef4f8e333352
guanbinrui <guanbinrui@dimension.im>
2022-09-28 16:55:59 +0800

    refactor: remove r2d2Fetch

--- packages/web3-providers/src/debank/index.ts
+++ packages/web3-providers/src/debank/index.ts
@@ -1,4 +1,6 @@
 import urlcat from 'urlcat'
+import { unionWith } from 'lodash-unified'
+import { EMPTY_LIST } from '@masknet/shared-base'
 import {
     GasOptionType,
     Transaction,
@@ -19,8 +21,6 @@ import { formatAssets, formatTransactions } from './format.js'
 import type { WalletTokenRecord, HistoryResponse, GasPriceDictResponse } from './type.js'
 import type { FungibleTokenAPI, HistoryAPI, GasOptionAPI } from '../types/index.js'
 import { getAllEVMNativeAssets } from '../helpers.js'
-import { unionWith } from 'lodash-unified'
-import { EMPTY_LIST } from '@masknet/shared-base'
 
 const DEBANK_API = 'https://api.debank.com'
 const DEBANK_OPEN_API = 'https://debank-proxy.r2d2.to'
@@ -50,7 +50,7 @@ export class DeBankAPI
         const { CHAIN_ID = '' } = getDeBankConstants(chainId)
         if (!CHAIN_ID) throw new Error('Failed to get gas price.')
 
-        const response = await global.r2d2Fetch(urlcat(DEBANK_API, '/chain/gas_price_dict_v2', { chain: CHAIN_ID }))
+        const response = await fetch(urlcat(DEBANK_API, '/chain/gas_price_dict_v2', { chain: CHAIN_ID }))
         const result = (await response.json()) as GasPriceDictResponse
         if (result.error_code !== 0) throw new Error('Failed to get gas price.')
 
@@ -75,11 +75,10 @@ export class DeBankAPI
     }
 
     async getAssets(address: string, options?: HubOptions<ChainId>) {
-        const response = await global.r2d2Fetch(
-            urlcat(DEBANK_OPEN_API, '/v1/user/token_list', {
-                id: address.toLowerCase(),
+        const response = await fetch(
+            urlcat(DEBANK_OPEN_API, '/v1/user/all_token_list', {
+                id: address,
                 is_all: false,
-                has_balance: true,
             }),
         )
         const result = (await response.json()) as WalletTokenRecord[] | undefined
@@ -114,9 +113,7 @@ export class DeBankAPI
         const { CHAIN_ID = '' } = getDeBankConstants(chainId)
         if (!CHAIN_ID) return createPageable(EMPTY_LIST, createIndicator(indicator))
 
-        const response = await global.r2d2Fetch(
-            `${DEBANK_API}/history/list?user_addr=${address.toLowerCase()}&chain=${CHAIN_ID}`,
-        )
+        const response = await fetch(`${DEBANK_API}/history/list?user_addr=${address.toLowerCase()}&chain=${CHAIN_ID}`)
         const { data, error_code } = (await response.json()) as HistoryResponse
         if (error_code !== 0) throw new Error('Fail to load transactions.')
 
