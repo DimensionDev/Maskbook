ad4f379d65276a16cfdc40959967e2422ea03cc5
guanbinrui <guanbinrui@dimension.im>
2022-10-04 17:33:29 +0800

    refactor: rpc api returning in wei

--- packages/web3-providers/src/astar/index.ts
+++ packages/web3-providers/src/astar/index.ts
@@ -17,19 +17,19 @@ export class AstarAPI implements GasOptionAPI.Provider<ChainId, GasOption> {
             [GasOptionType.FAST]: {
                 estimatedBaseFee: toFixed(result.eip1559.baseFeePerGas),
                 estimatedSeconds: 15,
-                suggestedMaxFeePerGas: toFixed(result.fast ?? 0).toString(),
+                suggestedMaxFeePerGas: toFixed(result.fast ?? 0),
                 suggestedMaxPriorityFeePerGas: toFixed(priorityFeePerGas.fast),
             },
             [GasOptionType.NORMAL]: {
                 estimatedBaseFee: toFixed(result.eip1559.baseFeePerGas),
                 estimatedSeconds: 30,
-                suggestedMaxFeePerGas: toFixed(result.average ?? 0).toString(),
+                suggestedMaxFeePerGas: toFixed(result.average ?? 0),
                 suggestedMaxPriorityFeePerGas: toFixed(priorityFeePerGas.average),
             },
             [GasOptionType.SLOW]: {
                 estimatedBaseFee: toFixed(result.eip1559.baseFeePerGas),
                 estimatedSeconds: 60,
-                suggestedMaxFeePerGas: toFixed(result.slow ?? 0).toString(),
+                suggestedMaxFeePerGas: toFixed(result.slow ?? 0),
                 suggestedMaxPriorityFeePerGas: toFixed(priorityFeePerGas.slow),
             },
         }
