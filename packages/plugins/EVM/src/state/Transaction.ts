320106e5d7ea0299326b2ed9410404b0f58881c6
nuanyang233 <nuanyang233@gmail.com>
2022-10-08 15:43:30 +0800

    fix: bugfix for crash (#7595)
    * fix: bugfix for crash
    
    * fix: add patch (#7598)
    
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>

--- packages/plugins/EVM/src/state/Transaction.ts
+++ packages/plugins/EVM/src/state/Transaction.ts
@@ -2,7 +2,12 @@ import type { Subscription } from 'use-subscription'
 import { getEnumAsArray } from '@dimensiondev/kit'
 import type { Plugin } from '@masknet/plugin-infra'
 import { TransactionState } from '@masknet/plugin-infra/web3'
-import { ChainId, Transaction as EVM_Transaction, formatEthereumAddress } from '@masknet/web3-shared-evm'
+import {
+    ChainId,
+    Transaction as EVM_Transaction,
+    formatEthereumAddress,
+    isValidChainId,
+} from '@masknet/web3-shared-evm'
 
 export class Transaction extends TransactionState<ChainId, EVM_Transaction> {
     constructor(
@@ -18,6 +23,7 @@ export class Transaction extends TransactionState<ChainId, EVM_Transaction> {
             subscriptions,
             {
                 formatAddress: formatEthereumAddress,
+                isValidChainId,
             },
         )
     }
