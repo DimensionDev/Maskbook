2d98861ac1c550c4d78b7467a2b4ab3de5d9c47a
Lantt <laanfor@gmail.com>
2022-10-08 13:43:29 +0800

    feat: should switch to web3 tab when href have web3 hash

--- packages/mask/src/components/InjectedComponents/ProfileTab.tsx
+++ packages/mask/src/components/InjectedComponents/ProfileTab.tsx
@@ -4,6 +4,7 @@ import { Typography } from '@mui/material'
 import { MaskMessages, useMatchXS, useLocationChange } from '../../utils/index.js'
 import { isTwitter } from '../../social-network-adaptor/twitter.com/base.js'
 import { activatedSocialNetworkUI } from '../../social-network/index.js'
+import { useMount } from 'react-use'
 
 export interface ProfileTabProps extends withClasses<'tab' | 'button' | 'selected'> {
     clear(): void
@@ -20,13 +21,22 @@ export function ProfileTab(props: ProfileTabProps) {
     const [active, setActive] = useState(false)
     const isMobile = useMatchXS()
 
-    const onClick = useCallback(() => {
-        // Change the url hashtag to trigger `locationchange` event from e.g. 'hostname/medias#web3 => hostname/medias'
-        isTwitter(activatedSocialNetworkUI) && location.assign('#web3')
+    const switchToTab = useCallback(() => {
         MaskMessages.events.profileTabUpdated.sendToLocal({ show: true })
         setActive(true)
         clear()
     }, [clear])
+
+    const onClick = useCallback(() => {
+        // Change the url hashtag to trigger `locationchange` event from e.g. 'hostname/medias#web3 => hostname/medias'
+        isTwitter(activatedSocialNetworkUI) && location.assign('#web3')
+        switchToTab()
+    }, [switchToTab])
+
+    useMount(() => {
+        if (location.hash !== '#web3' || !active) return
+        switchToTab()
+    })
 
     useLocationChange(() => {
         MaskMessages.events.profileTabUpdated.sendToLocal({ show: false })
