6042d240179f88d30e411574298b25e966f94c77
lelenei <zwpsky@gmail.com>
2022-10-09 18:06:40 +0800

    fix: get minds user

---
+++ packages/web3-providers/src/minds/index.ts
@@ -0,0 +1,11 @@
+import urlcat from 'urlcat'
+import type { MindBaseAPI } from '../types/minds.js'
+
+export class MindsAPI implements MindBaseAPI.Provider {
+    async getUserByScreenName(screenName?: string) {
+        if (!screenName) return
+        const response = await fetch(urlcat('https://www.minds.com/api/v1/channel/', screenName))
+        const data = await response.json()
+        return data.channel
+    }
+}
