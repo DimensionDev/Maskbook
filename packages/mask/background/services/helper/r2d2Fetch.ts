111bb99e35bf3e67ab1f65b9ce78af7550032169
nuanyang233 <nuanyang233@gmail.com>
2022-10-11 19:35:34 +0800

    chore: reply review

--- packages/mask/background/services/helper/r2d2Fetch.ts
+++ packages/mask/background/services/helper/r2d2Fetch.ts
@@ -84,9 +84,7 @@ export async function r2d2Fetch(input: RequestInfo, init?: RequestInit): Promise
             [async () => (await originalFetch(url, info)).blob(), async () => fetchImageViaDOM(url)],
             null,
         )
-        if (!blob?.type.includes('image')) {
-            return new Response(null)
-        }
+
         return new Response(blob, {
             headers: {
                 'Content-Type': 'image/png',
