d532a643bbb4bec3781e8a6cb6433c7512062876
lelenei <zwpsky@gmail.com>
2022-10-11 23:16:13 +0800

    fix: clean unused tss style

--- packages/mask/src/plugins/Collectible/src/SNSAdaptor/CardDialog/CardDialogContent.tsx
+++ packages/mask/src/plugins/Collectible/src/SNSAdaptor/CardDialog/CardDialogContent.tsx
@@ -1,13 +1,12 @@
 import { useCallback } from 'react'
 import { openWindow } from '@masknet/shared-base-ui'
 import { Button, Typography } from '@mui/material'
-import { LoadingBase } from '@masknet/theme'
+import { LoadingBase, makeStyles } from '@masknet/theme'
 import { Icons } from '@masknet/icons'
 import { PluginID, NetworkPluginID, CrossIsolationMessages } from '@masknet/shared-base'
 import { resolveSourceTypeName } from '@masknet/web3-shared-base'
 import { PluginIDContextProvider, PluginWeb3ContextProvider } from '@masknet/web3-hooks-base'
 import { PluginWalletStatusBar, useI18N as useBaseI18n } from '../../../../../utils/index.js'
-import { useStyles } from './hooks/useStyles.js'
 import { AboutTab } from './tabs/AboutTab.js'
 import { OffersTab } from './tabs/OffersTab.js'
 import { ActivitiesTab } from './tabs/ActivitiesTab.js'
@@ -16,6 +15,54 @@ import { FigureCard } from '../Shared/FigureCard.js'
 import { Context } from '../Context/index.js'
 import { useCurrentVisitingIdentity, useIsOwnerIdentity } from '../../../../../components/DataSource/useActivatedUI.js'
 import { ConnectPersonaBoundary } from '../../../../../components/shared/ConnectPersonaBoundary.js'
+
+const useStyles = makeStyles<{ listItemBackground?: string; listItemBackgroundIcon?: string } | void>()(
+    (theme, props) => ({
+        contentWrapper: {
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        loadingPlaceholder: {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+        },
+        mediaBox: {
+            width: 300,
+        },
+        contentLayout: {
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            padding: 24,
+            boxSizing: 'border-box',
+            paddingBottom: 72,
+        },
+        tabWrapper: {
+            width: 'calc( 100% - 336px)',
+            marginLeft: 36,
+            overflowY: 'auto',
+        },
+        emptyText: {
+            color: theme.palette.text.secondary,
+        },
+        footer: {
+            boxShadow:
+                theme.palette.mode === 'dark'
+                    ? '0px 0px 20px rgba(255, 255, 255, 0.12)'
+                    : '0px 0px 20px rgba(0, 0, 0, 0.05)',
+            position: 'sticky',
+            bottom: 0,
+        },
+        buttonText: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+        },
+    }),
+)
 
 export interface CardDialogContentProps {
     currentTab: TabType
