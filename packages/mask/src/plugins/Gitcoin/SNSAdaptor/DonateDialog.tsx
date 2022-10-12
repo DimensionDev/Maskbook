4940b5952b842175a3b9bdf0cad50b90a2c76797
lelenei <72531217+lelenei@users.noreply.github.com>
2022-10-12 14:10:50 +0800

    fix: share text at gitcoin (#7644)

--- packages/mask/src/plugins/Gitcoin/SNSAdaptor/DonateDialog.tsx
+++ packages/mask/src/plugins/Gitcoin/SNSAdaptor/DonateDialog.tsx
@@ -100,11 +100,13 @@ export function DonateDialog(props: DonateDialogProps) {
     const donate = useCallback(async () => {
         const hash = await donateCallback()
         if (typeof hash !== 'string') return
+
         const cashTag = isTwitter(activatedSocialNetworkUI) ? '$' : ''
         const shareText = token
             ? t.share_text({
                   balance: formatBalance(amount, token?.decimals),
                   symbol: `${cashTag}${token?.symbol || ''}`,
+                  promote: t.promote(),
               })
             : ''
         await openShareTxDialog({
