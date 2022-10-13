c69e6d45c330a65aec768f0f098794352204549d
guanbinrui <guanbinrui@dimension.im>
2022-10-13 12:14:57 +0800

    feat: define mask-x provider

--- packages/web3-providers/src/types/MaskX.ts
+++ packages/web3-providers/src/types/MaskX.ts
@@ -1,3 +1,52 @@
 export namespace MaskX_BaseAPI {
-    export interface Provider {}
+    export enum PlatformType {
+        Ethereum = 'eth',
+        Twitter = 'twitter',
+    }
+
+    export enum SourceType {
+        CyberConnect = 'cyberconnect',
+        Sybil = 'sybil',
+        Leaderboard = 'ethLeaderboard',
+        RSS3 = 'rss3',
+    }
+
+    export interface Identity {
+        // internal uuid
+        id: string
+        sns_handle: string
+        sns_platform: PlatformType
+        // an EVM EOA
+        web3_addr: string
+        web3_platform: PlatformType
+        // data source
+        source: SourceType
+        ens?: string
+        // a js like timestamp
+        create_timestamp: string
+        // a js like timestamp
+        modify_timestamp: string
+    }
+
+    export interface Options {
+        size: number
+        page: number
+    }
+
+    export interface Response {
+        pagination: {
+            current: number
+            next: number
+        }
+        records: Identity[]
+    }
+
+    export interface Provider {
+        /** Get identities by precisely matching the identity id. */
+        getIdentitiesExact(handle: string, platform: PlatformType, options?: Options): Promise<Response>
+        /** Get identities by fuzzy searcching the identity id. */
+        getIdentitiesFuzzy(handle: string, platform: PlatformType, options?: Options): Promise<Response>
+        /** Get all included identities. */
+        getAllIdentities(options?: Options): Promise<Response>
+    }
 }
