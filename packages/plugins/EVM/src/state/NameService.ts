911133dfd1ce54ccf3123cc15c8af73a02df1909
zhouhanseng <z308114274@gmail.com>
2022-10-13 23:17:42 +0800

    chore: no bsc

--- packages/plugins/EVM/src/state/NameService.ts
+++ packages/plugins/EVM/src/state/NameService.ts
@@ -4,7 +4,6 @@ import { NameServiceID } from '@masknet/shared-base'
 import { NameServiceState } from '@masknet/web3-state'
 import { ChainId, formatEthereumAddress, isValidAddress, isZeroAddress } from '@masknet/web3-shared-evm'
 import { ENS_Resolver } from './NameService/ENS.js'
-import { BSC_Resolver } from './NameService/BSC.js'
 
 export class NameService extends NameServiceState<ChainId> {
     constructor(
@@ -13,15 +12,10 @@ export class NameService extends NameServiceState<ChainId> {
             chainId?: Subscription<ChainId>
         },
     ) {
-        super(
-            context,
-            subscriptions.chainId?.getCurrentValue() === ChainId.BSC ? new BSC_Resolver() : new ENS_Resolver(),
-            subscriptions.chainId?.getCurrentValue() === ChainId.BSC ? NameServiceID.BSC : NameServiceID.EVM,
-            {
-                isValidName: (x) => x !== '0x',
-                isValidAddress: (x) => isValidAddress(x) && !isZeroAddress(x),
-                formatAddress: formatEthereumAddress,
-            },
-        )
+        super(context, new ENS_Resolver(), NameServiceID.EVM, {
+            isValidName: (x) => x !== '0x',
+            isValidAddress: (x) => isValidAddress(x) && !isZeroAddress(x),
+            formatAddress: formatEthereumAddress,
+        })
     }
 }
