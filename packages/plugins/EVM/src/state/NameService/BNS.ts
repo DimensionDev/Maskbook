533fc44bb66ee1e4dd8d459954f154c59f92375d
zhouhanseng <z308114274@gmail.com>
2022-10-14 10:42:39 +0800

    fix: typo

--- packages/plugins/EVM/src/state/NameService/BNS.ts
+++ packages/plugins/EVM/src/state/NameService/BNS.ts
@@ -1,6 +1,7 @@
 import type { NameServiceResolver } from '@masknet/web3-state'
 
-export class BSC_Resolver implements NameServiceResolver {
+// TODO: implement
+export class BNS_Resolver implements NameServiceResolver {
     async lookup(name: string) {
         return ''
     }
