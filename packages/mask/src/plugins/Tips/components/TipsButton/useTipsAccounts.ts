5c86464f699f0426e244256db68b679e18a30b80
guanbinrui <guanbinrui@dimension.im>
2022-10-14 15:22:47 +0800

    feat: add more address source

--- packages/mask/src/plugins/Tips/components/TipsButton/useTipsAccounts.ts
+++ packages/mask/src/plugins/Tips/components/TipsButton/useTipsAccounts.ts
@@ -1,11 +1,11 @@
 import { useMemo } from 'react'
+import { groupBy, uniq } from 'lodash-unified'
 import { useCurrentWeb3NetworkPluginID, useSocialAddressListAll } from '@masknet/web3-hooks-base'
 import { isSameAddress, SocialAddressType } from '@masknet/web3-shared-base'
 import type { IdentityResolved } from '@masknet/plugin-infra'
 import { EMPTY_LIST, NetworkPluginID } from '@masknet/shared-base'
 import { usePublicWallets } from '../../hooks/usePublicWallets.js'
 import type { TipsAccount } from '../../types/index.js'
-import { uniqBy } from 'lodash-unified'
 import { useTipsSetting } from '../../hooks/useTipsSetting.js'
 
 const ENABLE_PLUGINS = [NetworkPluginID.PLUGIN_EVM, NetworkPluginID.PLUGIN_SOLANA]
@@ -24,7 +24,14 @@ export function useTipsAccounts(
         const fromSocialAddresses = socialAddressList
             .filter((x) => ENABLE_PLUGINS.includes(x.networkSupporterPluginID))
             .map((x): TipsAccount => {
-                const name = [SocialAddressType.ENS, SocialAddressType.SOL, SocialAddressType.RSS3].includes(x.type)
+                const name = [
+                    SocialAddressType.ENS,
+                    SocialAddressType.SOL,
+                    SocialAddressType.RSS3,
+                    SocialAddressType.CyberConnect,
+                    SocialAddressType.Sybil,
+                    SocialAddressType.Leaderboard,
+                ].includes(x.type)
                     ? x.label
                     : undefined
                 return {
@@ -32,15 +39,22 @@ export function useTipsAccounts(
                     address: x.address,
                     isSocialAddress: true,
                     name,
-                    type: x.type,
+                    supportedTypes: [x.type],
                 }
             })
-
         const list = [...publicWallets, ...addresses, ...fromSocialAddresses].filter(
             (x) => !TipsSetting?.hiddenAddresses?.some((y) => isSameAddress(y, x.address)),
         )
-
-        return uniqBy(list, (v) => v.pluginId + v.address.toLowerCase())
+        const listGrouped = groupBy(list, (x) => `${x.pluginId}_${x.address.toLowerCase()}`)
+        return Object.entries(listGrouped).map(([, group]) => {
+            return group.reduce(
+                (accumulator, account) => ({
+                    ...account,
+                    supportedTypes: uniq([...(account.supportedTypes ?? []), ...(accumulator?.supportedTypes ?? [])]),
+                }),
+                group[0],
+            )
+        })
     }, [pluginId, publicWallets, addresses, socialAddressList, TipsSetting?.hiddenAddresses])
 
     return tipsAccounts
