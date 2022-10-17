d8399283ae5314f443b7c6bb7d5b1d179ec2fd0f
Jack Works <jackworks@protonmail.com>
2022-10-17 21:43:35 +0800

    chore: remove polls plugin (#7699)

--- packages/mask/src/plugins/Polls/constants.ts
+++
@@ -1,8 +0,0 @@
-import { PluginID } from '@masknet/shared-base'
-
-export const PollGunRootNode = 'com.maskbook.plugin.polls'
-
-export const PLUGIN_META_KEY = `${PluginID.Poll}:1`
-export const PLUGIN_ID = PluginID.Poll
-export const PLUGIN_NAME = 'Poll'
-export const PLUGIN_DESCRIPTION = 'Easily create a Poll for receiving public votes.'
