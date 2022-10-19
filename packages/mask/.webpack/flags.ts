2c9fc4e4017e31fa77fa6138e835a69d11616a98
Jack Works <jackworks@protonmail.com>
2022-10-19 19:47:31 +0800

    chore: remove source map

--- packages/mask/.webpack/flags.ts
+++ packages/mask/.webpack/flags.ts
@@ -39,19 +39,8 @@ export function normalizeBuildFlags(flags: BuildFlags) {
 
 export function computedBuildFlags(flags: ReturnType<typeof normalizeBuildFlags>) {
     const { runtime, mode } = flags
-    let sourceMapKind: Configuration['devtool'] = false
-    let lockdown = runtime.engine === 'chromium'
+    const sourceMapKind: Configuration['devtool'] = false
+    const lockdown = runtime.engine === 'chromium'
 
-    if (runtime.engine === 'safari' && runtime.architecture === 'app') {
-        // Due to webextension-polyfill, eval on iOS is async.
-        sourceMapKind = false
-    } else if (runtime.manifest === 3 && mode === 'development') {
-        // MV3 does not allow eval even in production
-        sourceMapKind = 'inline-cheap-source-map'
-    } else if (mode === 'development') {
-        sourceMapKind = 'eval-cheap-source-map'
-    } else {
-        sourceMapKind = false
-    }
     return { sourceMapKind, lockdown } as const
 }
