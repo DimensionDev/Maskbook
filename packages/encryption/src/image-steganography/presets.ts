8134449de7444fd05a1f7c04aea2608462d89f1c
Jack Works <jackworks@protonmail.com>
2022-10-20 19:09:21 +0800

    feat: support image encryption for payload 37 (#7682)
    * chore: upgrade stego to 0.14
    
    * refactor: cleanup unused code path
    
    * refactor: move files
    
    * refactor: add preset for stego and let each preset bring their own parameters
    
    * feat: let stego return u8 array directly
    
    * chore: return null for stego preset not found
    
    * feat: support v37 image encoding
    
    * feat: support image stego
    
    * fix: typo
    
    * fix: typo

---
+++ packages/encryption/src/image-steganography/presets.ts
@@ -0,0 +1,113 @@
+import { unreachable } from '@dimensiondev/kit'
+import { AlgorithmVersion, GrayscaleAlgorithm, TransformAlgorithm, type EncodeOptions } from '@dimensiondev/stego-js'
+import { SteganographyPreset } from './index.js'
+
+/** @internal */
+export interface Dimension {
+    width: number
+    height: number
+}
+
+/** @internal */
+export interface Preset extends Dimension {
+    mask: string | null
+    type: 'string' | 'raw'
+    description: string
+    options: Omit<EncodeOptions, 'text'>
+}
+
+const libV1AlgrDefaults: Omit<EncodeOptions, 'text'> = {
+    size: 8,
+    narrow: 0,
+    copies: 3,
+    tolerance: 128,
+    exhaustPixels: true,
+    cropEdgePixels: false,
+    fakeMaskPixels: false,
+    grayscaleAlgorithm: GrayscaleAlgorithm.NONE,
+    transformAlgorithm: TransformAlgorithm.FFT1D,
+    version: AlgorithmVersion.V1,
+}
+const libV2AlgrDefaults: Omit<EncodeOptions, 'text'> = {
+    size: 8,
+    narrow: 0,
+    copies: 3,
+    tolerance: 400,
+    exhaustPixels: false,
+    cropEdgePixels: false,
+    fakeMaskPixels: false,
+    grayscaleAlgorithm: GrayscaleAlgorithm.NONE,
+    transformAlgorithm: TransformAlgorithm.FFT2D,
+    version: AlgorithmVersion.V2,
+}
+
+const Preset2021: Preset = {
+    type: 'string',
+    description: 'the preset we used for payload V38',
+    width: 1200,
+    height: 681,
+    mask: new URL('./masks/mask-v2.png', import.meta.url).toString(),
+    options: libV1AlgrDefaults,
+}
+
+const Preset2022: Preset = {
+    type: 'raw',
+    description: 'the preset we used for payload V37',
+    width: 1200,
+    height: 671,
+    mask: null,
+    options: libV2AlgrDefaults,
+}
+
+const dimensionPreset: readonly Preset[] = [
+    Preset2021,
+    Preset2022,
+    {
+        type: 'string',
+        description: 'legacy post',
+        width: 1024,
+        height: 1240,
+        mask: new URL('./masks/mask-v1.png', import.meta.url).toString(),
+        options: libV1AlgrDefaults,
+    },
+    {
+        type: 'string',
+        description: 'legacy post',
+        width: 1200,
+        height: 680,
+        mask: new URL('./masks/mask-transparent.png', import.meta.url).toString(),
+        options: {
+            ...libV1AlgrDefaults,
+            cropEdgePixels: true,
+        },
+    },
+    {
+        type: 'string',
+        description: 'used in event election 2020',
+        width: 1000,
+        height: 558,
+        mask: new URL('./masks/mask-transparent.png', import.meta.url).toString(),
+        options: libV1AlgrDefaults,
+    },
+    {
+        type: 'string',
+        description: 'old NFT',
+        width: 1000,
+        height: 560,
+        mask: new URL('./masks/mask-v4.png', import.meta.url).toString(),
+        options: libV1AlgrDefaults,
+    },
+]
+
+/** @internal */
+export function findPreset(dimension: Dimension) {
+    return dimensionPreset.find((d) => isSameDimension(d, dimension))
+}
+export function getPreset(preset: SteganographyPreset): Preset {
+    if (preset === SteganographyPreset.Preset2021) return Preset2021
+    if (preset === SteganographyPreset.Preset2022) return Preset2022
+    unreachable(preset)
+}
+function isSameDimension(dimension: Dimension, otherDimension: Dimension) {
+    return dimension.width === otherDimension.width && dimension.height === otherDimension.height
+}
