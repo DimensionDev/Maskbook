38c7f42a60ec29e76a48db2a621ce059fc0294dc
Lantt <laanfor@gmail.com>
2022-10-21 15:50:33 +0800

    fix: release bug (#7765)
    * fix: nft details goto contract address
    
    * fix: nft details row value style

--- packages/mask/src/plugins/Collectible/src/SNSAdaptor/Shared/DetailsCard.tsx
+++ packages/mask/src/plugins/Collectible/src/SNSAdaptor/Shared/DetailsCard.tsx
@@ -37,7 +37,7 @@ const useStyles = makeStyles()((theme) => ({
         color: theme.palette.maskColor.second,
     },
     content: {
-        maxWidth: '40%',
+        maxWidth: '60%',
         overflow: 'hidden',
         textOverflow: 'ellipsis',
         whiteSpace: 'nowrap',
@@ -105,13 +105,7 @@ export function DetailsCard(props: DetailsCardProps) {
                             {x.link && (
                                 <Link
                                     className={classes.link}
-                                    href={
-                                        Others?.explorerResolver.nonFungibleTokenLink?.(
-                                            asset.chainId,
-                                            asset.address,
-                                            asset.tokenId,
-                                        ) ?? ''
-                                    }
+                                    href={Others?.explorerResolver.addressLink?.(asset.chainId, asset.address) ?? ''}
                                     target="_blank"
                                     rel="noopener noreferrer">
                                     <Icons.LinkOut size={16} />
