8051a24e48896c4ab045923e86300e46bbae136a
UncleBill <billbill290@gmail.com>
2022-10-25 20:52:28 +0800

    fix(Tips): mf-2557 reverse address (#7811)

--- packages/shared/src/UI/components/ReversedAddress/index.tsx
+++ packages/shared/src/UI/components/ReversedAddress/index.tsx
@@ -3,6 +3,7 @@ import type { NetworkPluginID } from '@masknet/shared-base'
 import { useReverseAddress, useWeb3State } from '@masknet/web3-hooks-base'
 import { Typography, TypographyProps } from '@mui/material'
 import { ShadowRootTooltip } from '@masknet/theme'
+import { isSameAddress } from '@masknet/web3-shared-base'
 
 export interface ReverseAddressProps {
     address: string
@@ -12,17 +13,19 @@ export interface ReverseAddressProps {
     isInline?: boolean
 }
 
-export const ReversedAddress = memo<ReverseAddressProps>(
-    ({ address, pluginID, size = 4, TypographyProps = { fontSize: '14px', fontWeight: 700 } }) => {
-        const { value: domain } = useReverseAddress(pluginID, address)
-        const { Others } = useWeb3State(pluginID)
+export const ReversedAddress = memo<ReverseAddressProps>(({ address, pluginID, size = 4, TypographyProps = {} }) => {
+    const { value: domain } = useReverseAddress(pluginID, address)
+    const { Others } = useWeb3State(pluginID)
 
-        const showDomain = !!domain && !!Others?.formatDomainName
-        const uiLabel = showDomain ? Others.formatDomainName(domain) : Others?.formatAddress?.(address, size) ?? address
-        const hasEllipsis = showDomain ? uiLabel !== domain : uiLabel !== address
+    const showDomain = !!domain && !!Others?.formatDomainName
+    const uiLabel = showDomain ? Others.formatDomainName(domain) : Others?.formatAddress?.(address, size) ?? address
+    const hasEllipsis = showDomain ? uiLabel !== domain : !isSameAddress(uiLabel, address)
 
-        const node = <Typography {...TypographyProps}>{uiLabel}</Typography>
+    const node = (
+        <Typography fontSize="14px" fontWeight={700} {...TypographyProps}>
+            {uiLabel}
+        </Typography>
+    )
 
-        return hasEllipsis ? <ShadowRootTooltip title={domain || address}>{node}</ShadowRootTooltip> : node
-    },
-)
+    return hasEllipsis ? <ShadowRootTooltip title={domain || address}>{node}</ShadowRootTooltip> : node
+})
