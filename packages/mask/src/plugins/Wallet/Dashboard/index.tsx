1a48503029b4c0975b2247fee4f27c0b6737a3a3
nuanyang233 <nuanyang233@gmail.com>
2022-10-25 16:43:40 +0800

    refactor: move global dialog

--- packages/mask/src/plugins/Wallet/Dashboard/index.tsx
+++ packages/mask/src/plugins/Wallet/Dashboard/index.tsx
@@ -2,7 +2,6 @@ import type { Plugin } from '@masknet/plugin-infra'
 import { base } from '@masknet/plugin-wallet'
 import { ExtensionSite, getSiteType, NetworkPluginID } from '@masknet/shared-base'
 import { SelectNftContractDialog } from '../SNSAdaptor/SelectNftContractDialog.js'
-import { WalletRiskWarningDialog } from '../SNSAdaptor/RiskWarningDialog/index.js'
 import { GasSettingDialog } from '../SNSAdaptor/GasSettingDialog/index.js'
 import { TransactionSnackbar } from '../SNSAdaptor/TransactionSnackbar/index.js'
 import { WalletConnectQRCodeDialog } from '../SNSAdaptor/WalletConnectQRCodeDialog/index.js'
@@ -17,7 +16,7 @@ const dashboard: Plugin.Dashboard.Definition = {
                 <SelectNftContractDialog />
                 {/* <WalletStatusDialog /> */}
                 {/* <ConnectWalletDialog /> */}
-                <WalletRiskWarningDialog />
+                {/* <WalletRiskWarningDialog /> */}
                 <GasSettingDialog />
                 {getSiteType() !== ExtensionSite.Popup ? (
                     <TransactionSnackbar pluginID={NetworkPluginID.PLUGIN_EVM} />
