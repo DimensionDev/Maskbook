5120647b21f8bcf37e1773af3a785fe660d39bb5
lelenei <72531217+lelenei@users.noreply.github.com>
2022-10-25 20:37:13 +0800

    fix: inject mask at minds (#7804)
    * fix: minds ui
    
    * fix: compose can not send post
    
    * fix: cspell error

--- packages/mask/src/social-network-adaptor/minds.com/collecting/post.ts
+++ packages/mask/src/social-network-adaptor/minds.com/collecting/post.ts
@@ -31,13 +31,13 @@ function collectPostsMindsInner(store: Next.CollectingCapabilities.PostsProvider
         new MutationObserverWatcher(postContentSelector()).useForeach((node, key, metadata) => {
             const activitySelector = new LiveSelector()
                 .replace(() => [metadata.realCurrent])
-                .closest('m-activity, m-activity__modal')
+                .closest('m-activityv2, m-activity__modal')
             const activityNode = activitySelector.evaluate()[0]! as HTMLElement
 
             // ? inject after comments
             const commentsSelector = activitySelector
                 .clone()
-                .querySelectorAll<HTMLElement>('m-comment .m-comment__message')
+                .querySelectorAll<HTMLElement>('m-activityv2__content .m-comment__message')
 
             // ? inject comment text field
             const commentBoxSelector = activitySelector
