fc1397a1e53fdecd9a6c4e004b00b0a3f6cec611
UncleBill <billbill290@gmail.com>
2022-10-25 20:33:09 +0800

    fix: mf-2555 don't list maskx in tips (#7808)

--- packages/mask/src/plugins/Tips/components/TipsButton/useTipsAccounts.ts
+++ packages/mask/src/plugins/Tips/components/TipsButton/useTipsAccounts.ts
@@ -1,10 +1,17 @@
 import { useMemo } from 'react'
 import type { IdentityResolved } from '@masknet/plugin-infra'
 import { EMPTY_LIST } from '@masknet/shared-base'
-import { isSameAddress } from '@masknet/web3-shared-base'
+import { isSameAddress, SocialAddressType } from '@masknet/web3-shared-base'
 import { useSocialAccountsAll } from '@masknet/web3-hooks-base'
 import { useTipsSetting } from '../../hooks/useTipsSetting.js'
 
+// In Tips, we don't list address from MaskX.
+const supportSources: SocialAddressType[] = [
+    SocialAddressType.Address,
+    SocialAddressType.ENS,
+    SocialAddressType.NEXT_ID,
+    SocialAddressType.TwitterBlue,
+]
 export function useTipsAccounts(identity: IdentityResolved | undefined, personaPubkey: string | undefined) {
     const { value: TipsSetting } = useTipsSetting(personaPubkey)
     const { value: socialAccounts = EMPTY_LIST } = useSocialAccountsAll(identity)
@@ -12,6 +19,11 @@ export function useTipsAccounts(identity: IdentityResolved | undefined, personaP
     const hiddenAddresses = TipsSetting?.hiddenAddresses
     return useMemo(() => {
         if (!hiddenAddresses?.length) return socialAccounts
-        return socialAccounts.filter((x) => !hiddenAddresses.some((y) => isSameAddress(y, x.address)))
+        return socialAccounts.filter((x) => {
+            const hidden = hiddenAddresses.some((y) => isSameAddress(y, x.address))
+            return (
+                !hidden && (!x.supportedAddressTypes || x.supportedAddressTypes.some((y) => supportSources.includes(y)))
+            )
+        })
     }, [socialAccounts, hiddenAddresses])
 }
