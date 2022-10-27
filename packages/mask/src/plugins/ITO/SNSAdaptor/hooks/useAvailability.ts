5fe694579e293bc335fbd85dfab9c3650fb5ed14
zhouhanseng <z308114274@gmail.com>
2022-10-27 13:03:00 +0800

    fix: ito

--- packages/mask/src/plugins/ITO/SNSAdaptor/hooks/useAvailability.ts
+++ packages/mask/src/plugins/ITO/SNSAdaptor/hooks/useAvailability.ts
@@ -1,8 +1,8 @@
 import { useAsyncRetry } from 'react-use'
 import { NetworkPluginID } from '@masknet/shared-base'
 import { isSameAddress } from '@masknet/web3-shared-base'
+import { useChainContext, useWeb3State, useWeb3Connection } from '@masknet/web3-hooks-base'
 import { useITOConstants } from '@masknet/web3-shared-evm'
-import { useChainContext, useWeb3Connection } from '@masknet/web3-hooks-base'
 import type { Web3Helper } from '@masknet/web3-helpers'
 import { checkAvailability } from '../utils/checkAvailability.js'
 
@@ -15,13 +15,14 @@ export function useAvailability(
         account: options?.account,
         chainId: options?.chainId,
     })
+    const { Others } = useWeb3State(NetworkPluginID.PLUGIN_EVM)
     const connection = useWeb3Connection(NetworkPluginID.PLUGIN_EVM, { chainId })
 
     const { ITO_CONTRACT_ADDRESS } = useITOConstants(chainId)
     const isV1 = isSameAddress(contractAddress ?? '', ITO_CONTRACT_ADDRESS)
 
     return useAsyncRetry(async () => {
-        if (!id || !contractAddress || !connection) return null
+        if (!id || !contractAddress || !connection || !Others?.chainResolver.isValid(chainId)) return null
         return checkAvailability(id, account, contractAddress, chainId, connection, isV1)
     }, [id, account, chainId, connection, isV1, contractAddress])
 }
--- packages/mask/src/plugins/ITO/SNSAdaptor/hooks/useQualification.ts
+++ packages/mask/src/plugins/ITO/SNSAdaptor/hooks/useQualification.ts
@@ -1,14 +1,16 @@
 import { useAsyncRetry } from 'react-use'
-import { useChainContext } from '@masknet/web3-hooks-base'
+import { useChainContext, useWeb3State } from '@masknet/web3-hooks-base'
 import type { NetworkPluginID } from '@masknet/shared-base'
 import { useQualificationContract } from './useQualificationContract.js'
 
 export function useQualification(qualification_address: string, ito_address: string) {
     const { account, chainId } = useChainContext<NetworkPluginID.PLUGIN_EVM>()
+    const { Others } = useWeb3State()
     const { contract: qualificationContract } = useQualificationContract(chainId, qualification_address, ito_address)
 
     return useAsyncRetry(async () => {
+        if (!Others?.chainResolver.isValid(chainId)) return null
         const startTime = await qualificationContract!.methods.get_start_time().call({ from: account })
         return Number(startTime) * 1000
-    }, [account, qualificationContract])
+    }, [account, qualificationContract, chainId])
 }
