62d1b54e5f6b50a0bead6c56d883e3068d3b71b3
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2022-10-28 15:44:21 +0800

    refactor: cleanup (#7834)

--- packages/mask/src/plugins/ITO/SNSAdaptor/hooks/useAvailability.ts
+++ packages/mask/src/plugins/ITO/SNSAdaptor/hooks/useAvailability.ts
@@ -1,7 +1,7 @@
 import { useAsyncRetry } from 'react-use'
 import { NetworkPluginID } from '@masknet/shared-base'
 import { isSameAddress } from '@masknet/web3-shared-base'
-import { useChainContext, useWeb3State, useWeb3Connection } from '@masknet/web3-hooks-base'
+import { useChainContext, useWeb3Connection } from '@masknet/web3-hooks-base'
 import { useITOConstants } from '@masknet/web3-shared-evm'
 import type { Web3Helper } from '@masknet/web3-helpers'
 import { checkAvailability } from '../utils/checkAvailability.js'
@@ -15,14 +15,13 @@ export function useAvailability(
         account: options?.account,
         chainId: options?.chainId,
     })
-    const { Others } = useWeb3State(NetworkPluginID.PLUGIN_EVM)
     const connection = useWeb3Connection(NetworkPluginID.PLUGIN_EVM, { chainId })
 
     const { ITO_CONTRACT_ADDRESS } = useITOConstants(chainId)
     const isV1 = isSameAddress(contractAddress ?? '', ITO_CONTRACT_ADDRESS)
 
     return useAsyncRetry(async () => {
-        if (!id || !contractAddress || !connection || !Others?.chainResolver.isValid(chainId)) return null
+        if (!id || !contractAddress || !connection) return null
         return checkAvailability(id, account, contractAddress, chainId, connection, isV1)
     }, [id, account, chainId, connection, isV1, contractAddress])
 }
