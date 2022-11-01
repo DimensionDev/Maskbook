c90ac3e000abed8140b659ea2f54006e35decc10
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2022-11-01 14:48:39 +0800

    [Release] Hotfix 2.15.1 => 2.15.2 (patch) (#7850)
    * chore: bump version to 2.15.2
    
    * fix: mf-2581 don't build trending cache from TagInspector component (#7851)
    
    Co-authored-by: UncleBill <billbill290@gmail.com>

--- packages/mask/src/plugins/Trader/trending/useAvailableDataProviders.ts
+++ packages/mask/src/plugins/Trader/trending/useAvailableDataProviders.ts
@@ -1,14 +1,15 @@
-import { useAsync } from 'react-use'
+import type { DataProvider } from '@masknet/public-api'
+import { EMPTY_LIST, NetworkPluginID } from '@masknet/shared-base'
 import { useChainId } from '@masknet/web3-hooks-base'
-import { NetworkPluginID } from '@masknet/shared-base'
+import { useAsync } from 'react-use'
+import type { AsyncState } from 'react-use/lib/useAsyncFn'
 import { PluginTraderRPC } from '../messages.js'
 import type { TagType } from '../types/index.js'
-import type { DataProvider } from '@masknet/public-api'
-import type { AsyncState } from 'react-use/lib/useAsyncFn'
 
 export function useAvailableDataProviders(type?: TagType, keyword?: string): AsyncState<DataProvider[]> {
     const chainId = useChainId(NetworkPluginID.PLUGIN_EVM)
     return useAsync(async () => {
+        if (!keyword) return EMPTY_LIST
         return PluginTraderRPC.getAvailableDataProviders(chainId, type, keyword)
     }, [chainId, type, keyword])
 }
