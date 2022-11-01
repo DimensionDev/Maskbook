0c0680f85cb9b2a51c9bbcf44bcc6bb4131db56c
UncleBill <billbill290@gmail.com>
2022-11-01 11:54:40 +0800

    fix: mf-2581 don't build trending cache from TagInspector component (#7851)

--- packages/mask/src/plugins/Trader/SNSAdaptor/trending/TagInspector.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trending/TagInspector.tsx
@@ -1,17 +1,13 @@
-import { useCallback } from 'react'
-import { TrendingPopper } from './TrendingPopper.js'
-import { TagType } from '../../types/index.js'
 import type { DataProvider } from '@masknet/public-api'
-import { TrendingView } from './TrendingView.js'
-import { useAvailableDataProviders } from '../../trending/useAvailableDataProviders.js'
 import { TargetChainIdContext } from '@masknet/web3-hooks-evm'
+import { useCallback } from 'react'
+import type { TagType } from '../../types/index.js'
+import { TrendingPopper } from './TrendingPopper.js'
+import { TrendingView } from './TrendingView.js'
 
 export interface TagInspectorProps {}
 
 export function TagInspector(props: TagInspectorProps) {
-    // build availability cache in the background page
-    useAvailableDataProviders(TagType.CASH, 'BTC')
-
     const createTrendingView = useCallback(
         (name: string, type: TagType, dataProviders: DataProvider[], reposition?: () => void) => {
             return <TrendingView name={name} tagType={type} dataProviders={dataProviders} onUpdate={reposition} />
