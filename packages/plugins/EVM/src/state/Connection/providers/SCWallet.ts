883bc580c869716d928362bb535994ab6b003efd
guanbinrui <guanbinrui@dimension.im>
2022-11-02 20:00:36 +0800

    chore: refine SCWallet implementation

--- packages/plugins/EVM/src/state/Connection/providers/SCWallet.ts
+++ packages/plugins/EVM/src/state/Connection/providers/SCWallet.ts
@@ -1,4 +1,5 @@
 import type { RequestArguments } from 'web3-core'
+import { encodePacked, keccak256 } from 'web3-utils'
 import type { ProviderOptions } from '@masknet/web3-shared-base'
 import type { ChainId, ProviderType, Web3Provider } from '@masknet/web3-shared-evm'
 import type { EVM_Provider } from '../types.js'
@@ -24,6 +25,32 @@ export class SCWalletProvider extends BaseProvider implements EVM_Provider {
         return provider.request(requestArguments) as Promise<T>
     }
 
+    /**
+     * Get SC wallet address (a.k.a. the sender address) at given nonce
+     * @param nonce
+     * @returns
+     */
+    protected getAccount(chainId: ChainId, nonce = 0): string {
+        const initCode = this.createInitCode(chainId)
+        const EP = this.getCreate2FactoryContractAddress(chainId)
+        if (!EP) return ''
+        const hash = encodePacked('0xff', EP, nonce, keccak256(initCode)) ?? ''
+        // TODO: cast to a EVM address
+        return hash
+    }
+
+    protected createInitCode(chainId: ChainId): string {
+        throw new Error('To be implemented')
+    }
+
+    protected getEPContractAddress(chainId: ChainId): string | undefined {
+        throw new Error('To be implemented')
+    }
+
+    protected getCreate2FactoryContractAddress(chainId: ChainId): string | undefined {
+        throw new Error('To be implemented')
+    }
+
     override async connect(): Promise<{ chainId: ChainId; account: string }> {
         throw new Error('To be implemented')
     }
@@ -32,5 +59,7 @@ export class SCWalletProvider extends BaseProvider implements EVM_Provider {
         throw new Error('To be implemented')
     }
 
-    protected async deploy(): Promise<void> {}
+    protected async deploy(nonce: number): Promise<void> {
+        throw new Error('To be implemented')
+    }
 }
--- packages/plugins/EVM/src/state/Connection/providers/SmartPay.ts
+++ packages/plugins/EVM/src/state/Connection/providers/SmartPay.ts
@@ -1,4 +1,4 @@
-import { ProviderType } from '@masknet/web3-shared-evm'
+import { ChainId, getSmartPayConstant, ProviderType } from '@masknet/web3-shared-evm'
 import { SCWalletProvider } from './SCWallet.js'
 import type { EVM_Provider } from '../types.js'
 
@@ -9,5 +9,13 @@ import type { EVM_Provider } from '../types.js'
 export class SmartPayProvider extends SCWalletProvider implements EVM_Provider {
     constructor() {
         super(ProviderType.SmartPay)
+    }
+
+    protected override getEPContractAddress(chainId: ChainId) {
+        return getSmartPayConstant(chainId, 'EP_CONTRACT_ADDRESS')
+    }
+
+    protected override getCreate2FactoryContractAddress(chainId: ChainId) {
+        return getSmartPayConstant(chainId, 'CREATE2_FACTORY_CONTRACT_ADDRESS')
     }
 }
