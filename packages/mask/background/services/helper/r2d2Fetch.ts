97e84c12b221383a7a2efc6626fe2cbbd4c83e70
guanbinrui <guanbinrui@dimension.im>
2022-11-04 06:59:19 +0800

    refactor: add cache rules

--- packages/mask/background/services/helper/r2d2Fetch.ts
+++ packages/mask/background/services/helper/r2d2Fetch.ts
@@ -36,26 +36,76 @@ const HOTFIX_RPC_URLS = [
     'evm.confluxrpc.com',
 ]
 
+const CACHE_RULES = {
+    'https://proof-service.nextnext.id/v1/proof': 1000 * 5,
+    // twitter shorten links
+    'https://t.co': 1000 * 60 * 30,
+    'https://gitcoin.co/grants/v1/api/grant': 1000 * 60,
+    'https://vcent-agent.r2d2.to': 1000 * 60,
+    'https://rss3.domains/name': 1000 * 60,
+    // avatar on RSS3 kv queries
+    'https://kv.r2d2.to/api/com.maskbook.user_twitter.com': 1000 * 60,
+    'https://discovery.attrace.com': 1000 * 60,
+    // mask-x
+    '7x16bogxfb.execute-api.us-east-1.amazonaws.com': 1000 * 60,
+}
+const CACHE_URLS = Object.keys(CACHE_RULES) as unknown as Array<keyof typeof CACHE_RULES>
+
 const { fetch: originalFetch } = globalThis
 
-async function squashedFetch(request: RequestInfo, init?: RequestInit): Promise<Response> {
-    console.log('DEBUG: squashedFetch')
-    console.log(request)
+// #region debugging
+const set = new Set()
+const hitSet = new Set()
 
-    const cache = await caches.open('r2d2')
+// @ts-ignore
+window.dumpSet = () => {
+    return set
+}
+
+// @ts-ignore
+window.dumpHitSet = () => {
+    return hitSet
+}
+// #endregion
+
+async function squashedFetch(request: RequestInfo, init?: RequestInit): Promise<Response> {
+    set.add({
+        request,
+        timestamp: Date.now(),
+    })
+
+    // skip all side effect requests
+    if (typeof request !== 'string' && request.method !== 'GET') return originalFetch(request, init)
+
+    // skip all non-http requests
+    const url = typeof request === 'string' ? request : request.url
+    if (!url.startsWith('http')) return originalFetch(request, init)
+
+    // no need to cache
+    const rule = CACHE_URLS.find((x) => url.includes(x))
+    if (!rule) return originalFetch(request, init)
+
+    // hit a cached request
+    const cache = await caches.open(rule)
     const hit = await cache.match(request)
     if (hit) {
-        console.log('DEBUG: hit')
+        hitSet.add({
+            request,
+            timestamp: Date.now(),
+        })
+        return hit
     }
 
+    // send the request & cache the response
     const response = await originalFetch(request, init)
+
     if (response.ok && response.status === 200) {
         await cache.put(request, response)
 
-        // keep cache for 1s
+        // stale the cache
         setTimeout(async () => {
             await cache.delete(request)
-        }, 1000)
+        }, CACHE_RULES[rule])
     }
     return response
 }
@@ -73,7 +123,7 @@ export async function r2d2Fetch(input: RequestInfo, init?: RequestInit): Promise
     // hotfix image requests
     if (request.method === 'GET' && request.headers.get('accept')?.includes('image/')) {
         const blob = await attemptUntil<Blob | null>(
-            [async () => (await originalFetch(url, request)).blob(), async () => fetchImageViaDOM(url)],
+            [async () => (await squashedFetch(url, request)).blob(), async () => fetchImageViaDOM(url)],
             null,
         )
 
@@ -85,17 +135,17 @@ export async function r2d2Fetch(input: RequestInfo, init?: RequestInit): Promise
     }
 
     // r2d2
-    if (url.includes(R2D2_ROOT_URL)) return originalFetch(request, init)
+    if (url.includes(R2D2_ROOT_URL)) return squashedFetch(request, init)
 
     // infura ipfs
-    if (url.includes(INFURA_IPFS_ROOT_URL)) return originalFetch(request, init)
+    if (url.includes(INFURA_IPFS_ROOT_URL)) return squashedFetch(request, init)
 
     // hotfix rpc requests lost content-type header
     if (request.method === 'POST' && HOTFIX_RPC_URLS.some((x) => url.includes(x)))
-        return originalFetch(request, { ...init, headers: { ...request?.headers, 'Content-Type': 'application/json' } })
+        return squashedFetch(request, { ...init, headers: { ...request?.headers, 'Content-Type': 'application/json' } })
 
     // fallback
-    return originalFetch(request, init)
+    return squashedFetch(request, init)
 }
 
 Reflect.set(globalThis, 'r2d2Fetch', r2d2Fetch)
