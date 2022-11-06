6077480cadff89870cce982fffa73e5002660e98
zhouhanseng <z308114274@gmail.com>
2022-11-06 10:11:43 +0800

    chore: migrate getTabContent to plugin infra

--- packages/plugin-infra/src/entry.ts
+++ packages/plugin-infra/src/entry.ts
@@ -1,6 +1,7 @@
 export * from './types.js'
 export { createPluginMessage, type DefaultPluginMessage, type PluginMessageEmitter } from './utils/message.js'
 export { createPluginRPC, createPluginRPCGenerator } from './utils/rpc.js'
+export { getProfileTabContent, getSettingsTabContent } from './utils/getTabContent.js'
 export {
     getPluginDefine,
     getRegisteredWeb3Networks,
