82cb7b9cb77304cc526aee06362f96b08f7332e6
zhouhanseng <z308114274@gmail.com>
2022-11-07 11:41:30 +0800

    chore: not replace trending this time

--- packages/mask/src/components/InjectedComponents/SearchResultBox.tsx
+++ packages/mask/src/components/InjectedComponents/SearchResultBox.tsx
@@ -1,26 +1,24 @@
 import { useMemo } from 'react'
-import { PluginID } from '@masknet/shared-base'
-import { createInjectHooksRenderer, useActivatedPluginSNSAdaptor } from '@masknet/plugin-infra/content-script'
+import { createInjectHooksRenderer, useActivatedPluginsSNSAdaptor } from '@masknet/plugin-infra/content-script'
 import { useSearchedKeyword } from '../DataSource/useSearchedKeyword.js'
+
+function getSearchResultContent(keyword: string) {
+    return createInjectHooksRenderer(useActivatedPluginsSNSAdaptor.visibility.useNotMinimalMode, (x) => {
+        const shouldDisplay = x.SearchResultBox?.Utils?.shouldDisplay?.(keyword) ?? true
+        if (!shouldDisplay) return
+        return x.SearchResultBox?.UI?.Content
+    })
+}
 
 export interface SearchResultBoxProps {}
 
-// Todo: migrate to @masknet/decentralized-search, requires @masknet/social-network.
 export function SearchResultBox(props: SearchResultBoxProps) {
     const keyword = useSearchedKeyword()
-    const DSearchPlugin = useActivatedPluginSNSAdaptor(PluginID.DecentralizedSearch, 'any')
+
     const component = useMemo(() => {
-        if (!DSearchPlugin) return
-        const Component = createInjectHooksRenderer(
-            () => [DSearchPlugin],
-            (x) => {
-                const shouldDisplay = x.SearchResultBox?.Utils?.shouldDisplay?.(keyword) ?? true
-                if (!shouldDisplay) return
-                return x.SearchResultBox?.UI?.Content
-            },
-        )
+        const Component = getSearchResultContent(keyword)
         return <Component keyword={keyword} />
-    }, [keyword, DSearchPlugin])
+    }, [keyword])
 
     if (!keyword) return null
     return <>{component}</>
