9e9277f8e1237d298f154822712e75432258cfa9
zhouhanseng <z308114274@gmail.com>
2022-11-08 18:29:42 +0800

    chore: little fix

--- packages/mask/src/plugins/Trader/SNSAdaptor/trending/SearchResultInspector.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trending/SearchResultInspector.tsx
@@ -1,6 +1,6 @@
 import { EMPTY_LIST, NetworkPluginID } from '@masknet/shared-base'
-import { Web3ContextProvider, useWeb3State } from '@masknet/web3-hooks-base'
-import { ChainId } from '@masknet/web3-shared-evm'
+import { Web3ContextProvider, useWeb3State, useAddressType } from '@masknet/web3-hooks-base'
+import { AddressType, ChainId } from '@masknet/web3-shared-evm'
 import { useFungibleTokenBaseOnChainIdList } from '@masknet/web3-hooks-evm'
 import { useAvailableDataProviders } from '../../trending/useAvailableDataProviders.js'
 import { TagType } from '../../types/index.js'
@@ -14,6 +14,7 @@ export function SearchResultInspector({ keyword }: SearchResultInspectorProps) {
     const regexResult = keyword.match(/([#$])(\w+)/) ?? []
     const { Others } = useWeb3State(NetworkPluginID.PLUGIN_EVM)
     const type = Others?.isValidAddress(keyword) ? '$' : regexResult[1]
+    const { value: addressType } = useAddressType(NetworkPluginID.PLUGIN_EVM, keyword, { chainId: ChainId.Mainnet })
 
     const [_, _type, name = ''] = keyword.match(/([#$])(\w+)/) ?? []
     const { value: fungibleToken } = useFungibleTokenBaseOnChainIdList(
@@ -23,10 +24,11 @@ export function SearchResultInspector({ keyword }: SearchResultInspectorProps) {
     )
     const type_ = type === '$' ? TagType.CASH : TagType.HASH
     const name_ = Others?.isValidAddress(keyword) ? fungibleToken?.symbol ?? '' : name
-    console.log({ type_, name_ })
+
     const { value: dataProviders = EMPTY_LIST } = useAvailableDataProviders(type_, name_)
 
-    if (!name_ || !dataProviders?.length) return null
+    if (!name_ || name_ === 'UNKNOWN' || addressType === AddressType.ExternalOwned || !dataProviders?.length)
+        return null
     return (
         <Web3ContextProvider value={{ pluginID: NetworkPluginID.PLUGIN_EVM, chainId: ChainId.Mainnet }}>
             <TrendingView isPopper={false} name={name_} tagType={type_} dataProviders={dataProviders} />
