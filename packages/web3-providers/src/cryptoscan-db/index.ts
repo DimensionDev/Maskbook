ef9118b2be764d33f40aa9aa0c538cc836688579
Lantt <laanfor@gmail.com>
2022-11-10 21:07:21 +0800

    refactor: rename

--- packages/web3-providers/src/cryptoscan-db/index.ts
+++ packages/web3-providers/src/cryptoscan-db/index.ts
@@ -1,18 +1,7 @@
 import type { ScamWarningAPI } from '../types/ScamWarning.js'
-import LRUCache from 'lru-cache'
 import urlcat from 'urlcat'
 import { fetchJSON } from '../helpers.js'
 import { ScalableBloomFilter } from 'bloom-filters'
-
-const cache = new LRUCache<string, JSON | ScamWarningAPI.Info>({
-    max: 100,
-    ttl: 300_000,
-})
-
-interface OnlineCategory {
-    id: number
-    value: string
-}
 
 export interface OnlineInfo {
     name: string
@@ -23,34 +12,23 @@ export interface OnlineInfo {
     description: string
 }
 
-const baseURL = 'https://raw.githubusercontent.com/DimensionDev/Mask-Scam-List/main/providers/cryptoscan-db/'
+const baseURL = 'https://raw.githubusercontent.com/DimensionDev/Mask-Scam-List/main/providers/cryptoscam-db/'
 
-export class CryptoScanDbAPI implements ScamWarningAPI.Provider {
+export class CryptoScamDBAPI implements ScamWarningAPI.Provider {
     bloomFilter?: ScalableBloomFilter
 
     async getBloomFilter() {
         if (this.bloomFilter) return this.bloomFilter
-        const categoriesCacheKey = 'filter-config'
-        const filterConfigCache = cache.get<JSON>(categoriesCacheKey)
-
-        if (filterConfigCache) {
-            this.bloomFilter = ScalableBloomFilter.fromJSON(filterConfigCache) as ScalableBloomFilter
-            return this.bloomFilter
-        }
-
         const filter = await fetchJSON<JSON>(urlcat(baseURL, 'filter/config.json'))
-        cache.set(categoriesCacheKey, filter)
 
         this.bloomFilter = ScalableBloomFilter.fromJSON(filter)
         return this.bloomFilter as ScalableBloomFilter
     }
 
     async getScamWarning(key: string): Promise<ScamWarningAPI.Info | undefined> {
-        const cacheKey = `site-${key}`
-        const cacheSite = cache.get<ScamWarningAPI.Info>(cacheKey)
-        if (cacheSite) return cacheSite
-
         const filter = await this.getBloomFilter()
+        if (!filter) return
+
         if (!filter.has(key)) return
 
         return fetchJSON<ScamWarningAPI.Info>(urlcat(baseURL, `${key}.json`))
