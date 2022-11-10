2c17b55f7da5deb52c77002ff835643cb4a9b927
zhouhanseng <z308114274@gmail.com>
2022-11-10 14:38:24 +0800

    fix: mf-2713 add icons

--- packages/plugins/ENS/src/SNSAdaptor/SearchResultInspector.tsx
+++ packages/plugins/ENS/src/SNSAdaptor/SearchResultInspector.tsx
@@ -1,6 +1,10 @@
 import { useContext, forwardRef, useImperativeHandle } from 'react'
 import { ChainId } from '@masknet/web3-shared-evm'
+import { useCopyToClipboard } from 'react-use'
 import { SourceType } from '@masknet/web3-shared-base'
+import { useWeb3State } from '@masknet/web3-hooks-base'
+import { NetworkPluginID } from '@masknet/shared-base'
+import { useSnackbarCallback } from '@masknet/shared'
 import { Box, Typography, Link, alpha } from '@mui/material'
 import { ENSProvider, ENSContext, SearchResultInspectorProps } from './context.js'
 import { CollectibleState } from './hooks/useCollectibleState.js'
@@ -8,6 +12,7 @@ import { SocialAccountList } from './SocialAccountList.js'
 import { SocialTooltip } from './SocialTooltip.js'
 import { makeStyles } from '@masknet/theme'
 import { Icons } from '@masknet/icons'
+import { useI18N } from '../locales/index.js'
 
 interface StyleProps {
     isMenuScroll?: boolean
@@ -43,6 +48,8 @@ const useStyles = makeStyles<StyleProps>()((theme) => {
             borderRadius: 999,
         },
         reversedAddress: {
+            display: 'flex',
+            alignItems: 'center',
             color: theme.palette.maskColor.secondaryDark,
             fontSize: 14,
             lineHeight: '18px',
@@ -86,6 +93,12 @@ const useStyles = makeStyles<StyleProps>()((theme) => {
             background: alpha(theme.palette.common.white, 0.5),
             borderRadius: 8,
         },
+        reversedAddressIcon: {
+            marginRight: 2,
+            marginBottom: 1,
+            cursor: 'pointer',
+            color: theme.palette.maskColor.secondaryDark,
+        },
     }
 })
 
@@ -93,7 +106,9 @@ export const SearchResultInspectorContent = forwardRef(function (
     props: SearchResultInspectorProps,
     ref: React.ForwardedRef<unknown>,
 ) {
+    const t = useI18N()
     const { classes, cx } = useStyles({})
+    const { Others } = useWeb3State(NetworkPluginID.PLUGIN_EVM)
     const {
         isLoading,
         isError,
@@ -113,6 +128,12 @@ export const SearchResultInspectorContent = forwardRef(function (
         tokenId,
         retry,
     ])
+    const [, copyToClipboard] = useCopyToClipboard()
+    const copyWalletAddress = useSnackbarCallback({
+        executor: async (address: string) => copyToClipboard(address),
+        deps: [],
+        successText: t.wallets_address_copied(),
+    })
 
     return (
         <CollectibleState.Provider
@@ -127,7 +148,25 @@ export const SearchResultInspectorContent = forwardRef(function (
                     <Icons.ETH size={30} className={classes.ensIcon} />
                     <div>
                         <Typography className={classes.ensDomain}>{domain}</Typography>
-                        <Typography className={classes.reversedAddress}>{reversedAddress}</Typography>
+                        {reversedAddress ? (
+                            <Typography className={classes.reversedAddress}>
+                                {reversedAddress}{' '}
+                                <Icons.Copy
+                                    size={20}
+                                    className={classes.reversedAddressIcon}
+                                    onClick={() => copyWalletAddress(reversedAddress ?? '')}
+                                />
+                                <Link
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className={classes.link}
+                                    href={
+                                        Others?.explorerResolver.addressLink?.(ChainId.Mainnet, reversedAddress) ?? ''
+                                    }>
+                                    <Icons.LinkOut size={20} className={classes.reversedAddressIcon} />
+                                </Link>
+                            </Typography>
+                        ) : null}
                     </div>
                 </section>
                 {firstValidNextIdTwitterBinding?.identity ? (
