9738118ab75754761828fcd19c7b78d37a1c915f
slfweb <slf.pro@gmail.com>
2022-11-12 13:07:56 +0100

    fix: resolve merge conflicts

--- packages/mask/background/setup.ts
+++ packages/mask/background/setup.ts
@@ -1,4 +1,4 @@
-import { polyfill } from '@dimensiondev/secp256k1-webcrypto'
+import { polyfill } from '@masknet/secp256k1-webcrypto'
 import { setupMaskKVStorageBackend } from '../shared/kv-storage.js'
 import { inMemory_KVStorageBackend, indexedDB_KVStorageBackend } from './database/kv-storage.js'
 
@@ -9,6 +9,12 @@ import './services/setup.js'
 import './tasks/setup.js' // Setup Tasks
 import '../shared/site-adaptors/index.js'
 import '../shared/native-rpc/index.js' // setup Android and iOS API server
+
+if (process.env.architecture === 'app') {
+    // Note: mobile (Android and iOS does not return a correct MINE type, therefore we can not use streaming to initialize the WASM module).
+    WebAssembly.instantiateStreaming = undefined!
+    WebAssembly.compileStreaming = undefined!
+}
 
 polyfill()
 setupMaskKVStorageBackend(indexedDB_KVStorageBackend, inMemory_KVStorageBackend)
