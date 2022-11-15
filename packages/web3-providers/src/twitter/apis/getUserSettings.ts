69923a3c0a6c69c7d1fed908c65a7464ace678c6
guanbinrui <guanbinrui@dimension.im>
2022-11-16 01:32:32 +0800

    fix: cspell

--- packages/web3-providers/src/twitter/apis/getUserSettings.ts
+++ packages/web3-providers/src/twitter/apis/getUserSettings.ts
@@ -1,24 +1,28 @@
 import { TwitterBaseAPI } from '../../types/index.js'
 
+/* cspell:disable-next-line */
 const DB_NAME = 'localforage'
 const DB_VERSION = 2
+/* cspell:disable-next-line */
+const STORE_NAME = 'keyvaluepairs'
+/* cspell:disable-next-line */
+const KEY_NAME = 'device:rweb.settings'
 
 export async function getUserSettings() {
     const databases = await indexedDB.databases()
     if (!databases.some((x) => x.name === DB_NAME && x.version === DB_VERSION)) return
 
     return new Promise<TwitterBaseAPI.UserSettings | undefined>(async (resolve, reject) => {
-        /* cspell:disable-next-line */
         const request = indexedDB.open(DB_NAME, DB_VERSION)
 
         request.addEventListener('success', () => {
             const db = request.result
             /* cspell:disable-next-line */
-            const transaction = db.transaction(['keyvaluepairs'], 'readonly')
+            const transaction = db.transaction([STORE_NAME], 'readonly')
             /* cspell:disable-next-line */
-            const objectStore = transaction.objectStore('keyvaluepairs')
+            const objectStore = transaction.objectStore(STORE_NAME)
             /* cspell:disable-next-line */
-            const query = objectStore.get('device:rweb.settings')
+            const query = objectStore.get(KEY_NAME)
 
             query.addEventListener('success', (event) => {
                 if (!event.target) reject('Failed to get user settings.')
