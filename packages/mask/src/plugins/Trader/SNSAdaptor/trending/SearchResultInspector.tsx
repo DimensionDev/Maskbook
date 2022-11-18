27095a872ebf8557111ad6327fb9a6135082c503
Hancheng Zhou <z308114274@gmail.com>
2022-11-18 16:01:57 +0800

    fix: trending data providers (#8054)

--- packages/mask/src/plugins/Trader/SNSAdaptor/trending/SearchResultInspector.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trending/SearchResultInspector.tsx
@@ -1,5 +1,6 @@
 import { EMPTY_LIST, NetworkPluginID } from '@masknet/shared-base'
 import { Web3ContextProvider, useAddressType } from '@masknet/web3-hooks-base'
+import { DataProvider } from '@masknet/public-api'
 import { AddressType, ChainId } from '@masknet/web3-shared-evm'
 import { useAvailableDataProviders } from '../../trending/useAvailableDataProviders.js'
 import { TrendingView } from './TrendingView.js'
@@ -10,14 +11,15 @@ export interface SearchResultInspectorProps {
 }
 
 export function SearchResultInspector({ keyword }: SearchResultInspectorProps) {
-    const { name, type, presetDataProviders, chainId, searchedContractAddress, asset } =
-        usePayloadFromTokenSearchKeyword(NetworkPluginID.PLUGIN_EVM, keyword)
+    const { name, type, isNFT, chainId, searchedContractAddress, asset } = usePayloadFromTokenSearchKeyword(
+        NetworkPluginID.PLUGIN_EVM,
+        keyword,
+    )
     const { value: addressType } = useAddressType(NetworkPluginID.PLUGIN_EVM, keyword, {
         chainId: chainId ?? ChainId.Mainnet,
     })
 
-    const { value: dataProviders_ = EMPTY_LIST } = useAvailableDataProviders(type, name)
-    const dataProviders = presetDataProviders ?? dataProviders_
+    const { value: dataProviders = EMPTY_LIST } = useAvailableDataProviders(type, name)
     if (!name || name === 'UNKNOWN' || addressType === AddressType.ExternalOwned || !dataProviders?.length) return null
     return (
         <Web3ContextProvider value={{ pluginID: NetworkPluginID.PLUGIN_EVM, chainId: ChainId.Mainnet }}>
@@ -28,7 +30,13 @@ export function SearchResultInspector({ keyword }: SearchResultInspectorProps) {
                 tagType={type}
                 expectedChainId={chainId}
                 searchedContractAddress={searchedContractAddress}
-                dataProviders={dataProviders}
+                dataProviders={
+                    isNFT
+                        ? [DataProvider.NFTScan]
+                        : dataProviders.filter((x) => {
+                              return x !== DataProvider.NFTScan
+                          })
+                }
             />
         </Web3ContextProvider>
     )
