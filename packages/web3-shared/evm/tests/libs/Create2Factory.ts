b387b94d9083fae4b4ab3925e92fbac27f535fc2
guanbinrui <guanbinrui@dimension.im>
2022-11-25 18:08:58 +0800

    fix: test Create2Factory

--- packages/web3-shared/evm/tests/libs/Create2Factory.ts
+++ packages/web3-shared/evm/tests/libs/Create2Factory.ts
@@ -1,12 +1,11 @@
 import { describe, expect, test } from 'vitest'
+import { isValidAddress } from '../../src/utils/address.js'
 import { Create2Factory } from '../../src/libs/Create2Factory.js'
 
 describe('Create2Factory', () => {
     const create2Factory = new Create2Factory('0x0000000000000000000000000000000000000000')
 
     test('getDeployedAddress', () => {
-        expect(create2Factory.getDeployedAddress('0x0000000000000000000000000000000000000000', '0')).toBe(
-            '0xc8086641b2f1e8af5b8a6d07d565a65086a451df',
-        )
+        expect(create2Factory.derive('0x0000000000000000000000000000000000000000').every(isValidAddress)).toBeTruthy()
     })
 })
