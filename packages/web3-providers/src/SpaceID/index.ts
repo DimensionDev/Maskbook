065714b03b64b5495afb74ee655b8299e8f06666
guanbinrui <guanbinrui@dimension.im>
2022-11-25 13:06:51 +0800

    refactor: naming

---
+++ packages/web3-providers/src/SpaceID/index.ts
@@ -0,0 +1,11 @@
+import type { ChainId } from '@masknet/web3-shared-evm'
+import type { DomainAPI } from '../index.js'
+
+export class SpaceIDAPI implements DomainAPI.Provider<ChainId> {
+    lookup(name: string, chainId: ChainId): Promise<string | undefined> {
+        throw new Error('Method not implemented.')
+    }
+    reverse(address: string, chainId: ChainId): Promise<string | undefined> {
+        throw new Error('Method not implemented.')
+    }
+}
