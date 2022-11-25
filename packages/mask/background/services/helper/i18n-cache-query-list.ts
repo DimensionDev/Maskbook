f08e64ea2cf62917254d8332ec100fb45d756dd6
guanbinrui <guanbinrui@dimension.im>
2022-11-25 16:29:25 +0800

    refactor: renaming

--- packages/mask/background/services/helper/i18n-cache-query-list.ts
+++ packages/mask/background/services/helper/i18n-cache-query-list.ts
@@ -7,7 +7,7 @@ export default {
     '/[DimensionDev.Maskbook] develop/packages/shared/src/locales/%locale%.json': 'shared',
     '/[DimensionDev.Maskbook] develop/packages/shared-base-ui/src/locales/%locale%.json': 'shareBase',
     '/[DimensionDev.Maskbook] develop/packages/dashboard/src/locales/%locale%.json': 'dashboard',
-    '/[DimensionDev.Maskbook] develop/packages/plugins/example/src/locales/%locale%.json': 'io.mask.example',
+    '/[DimensionDev.Maskbook] develop/packages/plugins/Example/src/locales/%locale%.json': 'io.mask.example',
     '/[DimensionDev.Maskbook] develop/packages/plugins/Debugger/src/locales/%locale%.json': 'io.mask.debugger',
     '/[DimensionDev.Maskbook] develop/packages/plugins/Wallet/src/locales/%locale%.json': 'com.maskbook.wallet',
     '/[DimensionDev.Maskbook] develop/packages/plugins/Flow/src/locales/%locale%.json': 'com.mask.flow',
