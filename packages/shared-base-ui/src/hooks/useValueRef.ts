d628800ce7fa61fa6b1ea5b5e072e8e1c3d4d27b
Jack Works <jackworks@protonmail.com>
2022-11-28 18:01:01 +0800

    refactor: move ValueRef from holowflows-kit to mask (#8141)
    * chore: move ValueRef from holowflows-kit to mask
    
    * fix: lint
    
    * prettier

--- packages/shared-base-ui/src/hooks/useValueRef.ts
+++ packages/shared-base-ui/src/hooks/useValueRef.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { Subscription, useSubscription } from 'use-subscription'
-import type { ValueRef } from '@dimensiondev/holoflows-kit'
+import type { ValueRef } from '@masknet/shared-base'
 
 export function useValueRef<T>(ref: ValueRef<T>) {
     const subscription = useMemo<Subscription<T>>(
