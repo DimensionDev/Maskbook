dad0c4a81446c67084b10e1bc35e316ae60a4597
Jack Works <jackworks@protonmail.com>
2022-12-02 18:40:52 +0800

    chore: revert a change

--- packages/shared-base-ui/src/hooks/useValueRef.ts
+++ packages/shared-base-ui/src/hooks/useValueRef.ts
@@ -2,7 +2,7 @@ import { useMemo } from 'react'
 import { Subscription, useSubscription } from 'use-subscription'
 import type { ValueRef } from '@masknet/shared-base'
 
-export function useValueRef<T>(ref: ValueRef<T>): Readonly<T> {
+export function useValueRef<T>(ref: ValueRef<T>) {
     const subscription = useMemo<Subscription<T>>(
         () => ({
             getCurrentValue: () => ref.value,
