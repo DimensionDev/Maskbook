717d8393971c7d273be509eb9d2cffb0fb89acc5
guanbinrui <guanbinrui@dimension.im>
2022-12-03 16:52:35 +0800

    fix: hooks

--- packages/web3-hooks/base/src/useWallet.ts
+++ packages/web3-hooks/base/src/useWallet.ts
@@ -6,15 +6,9 @@ import { useWallets } from './useWallets.js'
 
 export function useWallet<T extends NetworkPluginID>(pluginID?: T) {
     const { account } = useChainContext()
+    const wallets = useWallets()
 
-    const { value: wallets, loading } = useWallets()
-
-    const wallet = useMemo(() => {
+    return useMemo(() => {
         return account ? wallets?.find((x) => isSameAddress?.(x.address, account)) ?? null : null
     }, [account, wallets?.map((x) => x.address.toLowerCase()).join()])
-
-    return {
-        wallet,
-        loading,
-    }
 }
--- packages/web3-hooks/base/src/useWallets.ts
+++ packages/web3-hooks/base/src/useWallets.ts
@@ -1,15 +1,8 @@
-import { EMPTY_ARRAY, NetworkPluginID } from '@masknet/shared-base'
-import { useAsync } from 'react-use'
 import { useSubscription } from 'use-subscription'
-import { useWeb3Connection } from './useWeb3Connection.js'
+import { EMPTY_ARRAY, NetworkPluginID } from '@masknet/shared-base'
 import { useWeb3State } from './useWeb3State.js'
 
 export function useWallets<T extends NetworkPluginID>(pluginID?: T) {
     const { Wallet } = useWeb3State(pluginID)
-    // const connection = useWeb3Connection(pluginID)
-    // const { Wallet } = useWeb3State(pluginID)
-    // const wallets = useSubscription(Wallet?.wallets ?? EMPTY_ARRAY)
-    // return useAsync(async () => {
-    //     return connection?.getWallets?.() ?? wallets
-    // }, [wallets, connection])
+    return useSubscription(Wallet?.wallets ?? EMPTY_ARRAY)
 }
