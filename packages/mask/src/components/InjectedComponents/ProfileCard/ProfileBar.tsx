b22e428e3291ceb0143b4b1542c3bc2c41ec442f
unclebill <billbill290@gmail.com>
2022-12-05 11:11:49 +0800

    fix: mf-2837 avatar image might break in profile card

--- packages/mask/src/components/InjectedComponents/ProfileCard/ProfileBar.tsx
+++ packages/mask/src/components/InjectedComponents/ProfileCard/ProfileBar.tsx
@@ -4,7 +4,7 @@ import { v4 as uuid } from 'uuid'
 import { Icons } from '@masknet/icons'
 import { Box, Link, MenuItem, Typography } from '@mui/material'
 import { useWeb3State, useChainContext } from '@masknet/web3-hooks-base'
-import { AccountIcon, AddressItem, useSnackbarCallback } from '@masknet/shared'
+import { AccountIcon, AddressItem, Image, useSnackbarCallback } from '@masknet/shared'
 import { makeStyles, ShadowRootMenu } from '@masknet/theme'
 import { isSameAddress, SocialAccount, SocialIdentity } from '@masknet/web3-shared-base'
 import { ChainId } from '@masknet/web3-shared-evm'
@@ -43,6 +43,9 @@ const useStyles = makeStyles<void, 'avatarDecoration'>()((theme, _, refs) => ({
             height: '100%',
             transform: 'scale(1)',
         },
+    },
+    avatarImageContainer: {
+        borderRadius: '50%',
     },
     avatarDecoration: {},
     description: {
@@ -147,14 +150,17 @@ export const ProfileBar = memo<ProfileBarProps>(
 
         return (
             <Box className={cx(classes.root, className)} {...rest} ref={containerRef}>
-                <div className={classes.avatar}>
-                    <img
+                <div className={classes.avatar} data-id={identity.identifier?.userId}>
+                    <Image
                         src={identity.avatar}
                         height={40}
                         width={40}
                         alt={identity.nickname}
-                        style={{
-                            WebkitClipPath: `url(#${avatarClipPathId}-clip-path)`,
+                        containerProps={{
+                            className: classes.avatarImageContainer,
+                            style: {
+                                WebkitClipPath: `url(#${avatarClipPathId}-clip-path)`,
+                            },
                         }}
                     />
                     <AvatarDecoration
--- packages/shared/src/UI/components/Image/index.tsx
+++ packages/shared/src/UI/components/Image/index.tsx
@@ -1,44 +1,37 @@
-import { ImgHTMLAttributes, useState } from 'react'
+import { HTMLProps, ImgHTMLAttributes, useState } from 'react'
 import { LoadingBase, makeStyles } from '@masknet/theme'
-import { Box, useTheme } from '@mui/material'
+import { useTheme } from '@mui/material'
 import { useImageURL } from '../../../hooks/useImageURL.js'
 
-const useStyles = makeStyles<Pick<ImageProps, 'size' | 'rounded'>, 'floatingContainer'>()(
-    (theme, { size, rounded }, refs) => ({
-        container: {
-            width: size ?? '100%',
-            height: size ?? '100%',
-            position: 'relative',
-            borderRadius: rounded ? '50%' : undefined,
-            overflow: rounded ? 'hidden' : undefined,
+const useStyles = makeStyles<Pick<ImageProps, 'size' | 'rounded'>, 'center'>()((theme, { size, rounded }, refs) => ({
+    container: {
+        width: size ?? '100%',
+        height: size ?? '100%',
+        position: 'relative',
+        borderRadius: rounded ? '50%' : undefined,
+        overflow: rounded ? 'hidden' : undefined,
+    },
+    image: {
+        display: 'block',
+    },
+    failImage: {
+        width: 30,
+        height: 30,
+    },
+    center: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    failed: {
+        [`&.${refs.center}`]: {
+            background:
+                theme.palette.mode === 'light'
+                    ? 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.9) 100%), linear-gradient(90deg, rgba(98, 152, 234, 0.2) 1.03%, rgba(98, 152, 234, 0.2) 1.04%, rgba(98, 126, 234, 0.2) 100%)'
+                    : 'linear-gradient(180deg, #202020 0%, #181818 100%)',
         },
-        image: {
-            display: 'block',
-        },
-        failImage: {
-            width: 30,
-            height: 30,
-        },
-        floatingContainer: {
-            width: '100%',
-            height: '100%',
-            inset: 0,
-            margin: 'auto',
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        failed: {
-            [`.${refs.floatingContainer}`]: {
-                background:
-                    theme.palette.mode === 'light'
-                        ? 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.9) 100%), linear-gradient(90deg, rgba(98, 152, 234, 0.2) 1.03%, rgba(98, 152, 234, 0.2) 1.04%, rgba(98, 126, 234, 0.2) 100%)'
-                        : 'linear-gradient(180deg, #202020 0%, #181818 100%)',
-            },
-        },
-    }),
-)
+    },
+}))
 
 export interface ImageProps
     extends ImgHTMLAttributes<HTMLImageElement>,
@@ -47,6 +40,7 @@ export interface ImageProps
     rounded?: boolean
     fallback?: URL | string | JSX.Element
     disableSpinner?: boolean
+    containerProps?: HTMLProps<HTMLDivElement>
 }
 
 export function Image({
@@ -56,6 +50,7 @@ export function Image({
     disableSpinner,
     classes: extraClasses,
     onClick,
+    containerProps,
     ...rest
 }: ImageProps) {
     const { classes, cx } = useStyles({ size, rounded }, { props: { classes: extraClasses } })
@@ -66,17 +61,15 @@ export function Image({
 
     if (loadingImageURL && !disableSpinner) {
         return (
-            <Box className={classes.container}>
-                <Box className={classes.floatingContainer}>
-                    <LoadingBase />
-                </Box>
-            </Box>
+            <div {...containerProps} className={cx(classes.container, classes.center, containerProps?.className)}>
+                <LoadingBase />
+            </div>
         )
     }
 
     if (imageURL && !failed) {
         return (
-            <Box className={classes.container} onClick={onClick}>
+            <div {...containerProps} className={cx(classes.container, containerProps?.className)}>
                 <img
                     className={classes.image}
                     loading="lazy"
@@ -87,14 +80,16 @@ export function Image({
                     src={imageURL}
                     onError={() => setFailed(true)}
                 />
-            </Box>
+            </div>
         )
     }
     if (fallback && !(fallback instanceof URL) && typeof fallback !== 'string') {
         return (
-            <Box className={cx(classes.container, classes.failed)}>
-                <Box className={classes.floatingContainer}>{fallback}</Box>
-            </Box>
+            <div
+                {...containerProps}
+                className={cx(classes.container, classes.failed, classes.center, containerProps?.className)}>
+                {fallback}
+            </div>
         )
     }
 
@@ -105,18 +100,18 @@ export function Image({
             : new URL('./mask-light.png', import.meta.url).toString())
 
     return (
-        <Box className={cx(classes.container, classes.failed)} onClick={onClick}>
-            <Box className={classes.floatingContainer}>
-                <img
-                    loading="lazy"
-                    decoding="async"
-                    width={size}
-                    height={size}
-                    {...rest}
-                    src={fallbackImageURL}
-                    className={cx(classes.image, classes.failImage, classes.fallbackImage)}
-                />
-            </Box>
-        </Box>
+        <div
+            {...containerProps}
+            className={cx(classes.container, classes.failed, classes.center, containerProps?.className)}>
+            <img
+                loading="lazy"
+                decoding="async"
+                width={size}
+                height={size}
+                {...rest}
+                src={fallbackImageURL}
+                className={cx(classes.image, classes.failImage, classes.fallbackImage)}
+            />
+        </div>
     )
 }
