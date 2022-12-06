47bc02c0efc452a3470db172e0f5bb95cb8de43e
guanbinrui <guanbinrui@dimension.im>
2022-12-06 17:33:39 +0800

    fix: gitcoin donations

--- packages/plugins/Gitcoin/src/hooks/useDonateCallback.ts
+++ packages/plugins/Gitcoin/src/hooks/useDonateCallback.ts
@@ -7,6 +7,8 @@ import { ChainId, ContractTransaction, SchemaType, useGitcoinConstants } from '@
 import { useChainContext, useWeb3Connection } from '@masknet/web3-hooks-base'
 import { useBulkCheckoutContract } from './useBulkCheckoutWallet.js'
 
+type Donation = [string, string, string]
+
 /**
  * A callback for donate gitcoin grant
  * @param address the donor address
@@ -14,29 +16,34 @@ import { useBulkCheckoutContract } from './useBulkCheckoutWallet.js'
  * @param token
  */
 export function useDonateCallback(address: string, amount: string, token?: FungibleToken<ChainId, SchemaType>) {
-    const { GITCOIN_ETH_ADDRESS, GITCOIN_TIP_PERCENTAGE } = useGitcoinConstants()
+    const { GITCOIN_ETH_ADDRESS, GITCOIN_TIP_PERCENTAGE, GITCOIN_MAINTAINER_ADDRESS } = useGitcoinConstants()
 
     const { account, chainId } = useChainContext<NetworkPluginID.PLUGIN_EVM>()
     const bulkCheckoutContract = useBulkCheckoutContract(chainId)
     const connection = useWeb3Connection(NetworkPluginID.PLUGIN_EVM)
 
-    const donations = useMemo((): Array<[string, string, string]> => {
+    const donations = useMemo((): Donation[] => {
         if (!connection || !address || !token || !GITCOIN_ETH_ADDRESS || !GITCOIN_TIP_PERCENTAGE) return EMPTY_LIST
         const tipAmount = new BigNumber(GITCOIN_TIP_PERCENTAGE / 100).multipliedBy(amount)
         const grantAmount = new BigNumber(amount).minus(tipAmount)
-        return [
-            [
-                token.schema === SchemaType.Native ? GITCOIN_ETH_ADDRESS : token.address,
-                tipAmount.toFixed(0),
-                address, // dest
-            ],
+        const result: Donation[] = [
             [
                 token.schema === SchemaType.Native ? GITCOIN_ETH_ADDRESS : token.address,
                 grantAmount.toFixed(0),
                 address, // dest
             ],
         ]
-    }, [address, amount, token])
+
+        if (GITCOIN_MAINTAINER_ADDRESS && tipAmount.isGreaterThan(0)) {
+            result.push([
+                token.schema === SchemaType.Native ? GITCOIN_ETH_ADDRESS : token.address,
+                tipAmount.toFixed(0),
+                GITCOIN_MAINTAINER_ADDRESS, // dest
+            ])
+        }
+
+        return result
+    }, [address, amount, token, GITCOIN_MAINTAINER_ADDRESS])
 
     return useAsyncFn(async () => {
         if (!connection || !token || !bulkCheckoutContract || !donations.length) {
