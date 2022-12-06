dcf7c6fa1b08856660b9f25a9530bce958226fcf
Lantt <laanfor@gmail.com>
2022-12-06 16:45:31 +0800

    fix: scam db bugs (#8192)
    * fix: should compare the scam url when not pathname
    
    * feat: enabel mind collect post links
    
    * feat: collect post link in facebook
    
    * refactor: review feedback

--- packages/mask/src/social-network-adaptor/facebook.com/collecting/posts.tsx
+++ packages/mask/src/social-network-adaptor/facebook.com/collecting/posts.tsx
@@ -106,10 +106,29 @@ function collectPostsFacebookInner(store: Next.CollectingCapabilities.PostsProvi
                 }
                 info.postMessage.value = makeTypedMessageTuple(nextTypedMessage)
             }
-            collectPostInfo()
+
+            function collectLinks() {
+                if (metadata.destroyed) return
+                const linkElements = metadata.current.querySelectorAll<HTMLLinkElement>('[role=article] [id] a')
+                const links = [...Array.from(linkElements).filter((x) => x.href)]
+
+                const seen = new Set<string>()
+                for (const x of links) {
+                    if (seen.has(x.href)) continue
+                    seen.add(x.href)
+                    info.postMetadataMentionedLinks.set(x, x.href)
+                }
+            }
+
+            function run() {
+                collectPostInfo()
+                collectLinks()
+            }
+
+            run()
             return {
-                onNodeMutation: collectPostInfo,
-                onTargetChanged: collectPostInfo,
+                onNodeMutation: run,
+                onTargetChanged: run,
                 onRemove: () => store.delete(metadata),
             }
         }),
