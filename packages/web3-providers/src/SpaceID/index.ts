24d698a9470f8d67a57752f61e61221bd8eeb87a
Jack Works <jackworks@protonmail.com>
2022-12-09 15:00:04 +0800

    refactor: defer import of sid

--- packages/web3-providers/src/SpaceID/index.ts
+++ packages/web3-providers/src/SpaceID/index.ts
@@ -1,17 +1,28 @@
-import SID, { getSidAddress } from '@siddomains/sidjs'
+import type SID from '@siddomains/sidjs'
 import { ChainId } from '@masknet/web3-shared-evm'
 import { Web3API } from '../EVM/index.js'
 import type { DomainAPI } from '../entry-types.js'
+import type Web3 from 'web3'
 
 export class SpaceID_API implements DomainAPI.Provider<ChainId> {
-    private web3 = new Web3API().createSDK(ChainId.BSC)
-    private sid = new SID({ provider: this.web3.currentProvider, sidAddress: getSidAddress(ChainId.BSC) })
+    private _web3: Web3 | undefined
+    private get web3() {
+        return (this._web3 ??= new Web3API().createSDK(ChainId.Mainnet))
+    }
+    private sid: SID | undefined
+    private async SID() {
+        if (this.sid) return this.sid
+        const { default: SID, getSidAddress } = await import('@siddomains/sidjs')
+        return (this.sid ??= new SID({ provider: this.web3.currentProvider, sidAddress: getSidAddress(ChainId.BSC) }))
+    }
 
     async lookup(chainId: ChainId, name: string): Promise<string | undefined> {
         if (!name.endsWith('.bnb')) return
+        await this.SID()
         return this.sid?.name(name).getAddress()
     }
     async reverse(chainId: ChainId, address: string): Promise<string | undefined> {
+        await this.SID()
         const result = await this.sid?.getName(address)
         return result?.name
     }
