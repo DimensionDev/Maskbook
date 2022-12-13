7de488f00ff8189f194142eaba49dc126b765f8c
lelenei <zwpsky@gmail.com>
2022-12-13 18:32:03 +0800

    fix: reply review

--- packages/web3-state/src/Identity.ts
+++ packages/web3-state/src/Identity.ts
@@ -51,7 +51,7 @@ export class IdentityServiceState<ChainId> implements Web3SocialIdentityState<Ch
     }
 
     __mergeSocialAddressesAll__(socialAddresses: Array<SocialAddress<ChainId>>) {
-        const accountsGrouped = groupBy(socialAddresses, (x) => `${x.pluginID}_${x.address.toLowerCase()}`)
+        const accountsGrouped = groupBy(socialAddresses, (x) => `${x.pluginID}_${x.address.toLowerCase()}_${x.type}`)
         return Object.entries(accountsGrouped).map<SocialAccount<ChainId>>(([, group]) => {
             return {
                 pluginID: group[0].pluginID,
