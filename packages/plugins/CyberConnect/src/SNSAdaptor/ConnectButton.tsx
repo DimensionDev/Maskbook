68abbaa0477a04ff69a5d0c3651c40f81aec28b3
lelenei <zwpsky@gmail.com>
2022-12-15 17:09:17 +0800

    fix: follow failed

--- packages/plugins/CyberConnect/src/SNSAdaptor/ConnectButton.tsx
+++ packages/plugins/CyberConnect/src/SNSAdaptor/ConnectButton.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useState, useCallback, useMemo } from 'react'
 import { useAsync } from 'react-use'
 import { makeStyles } from '@masknet/theme'
 import { isSameAddress } from '@masknet/web3-shared-base'
 import { NetworkPluginID } from '@masknet/shared-base'
 import { useChainContext, useWeb3, useNetworkContext } from '@masknet/web3-hooks-base'
 import CyberConnect, { Env } from '@cyberlab/cyberconnect'
-import { Button } from '@mui/material'
 import { PluginCyberConnectRPC } from '../messages.js'
-import { WalletConnectedBoundary } from '@masknet/shared'
 import { useI18N } from '../locales/i18n_generated.js'
+import { Button } from '@mui/material'
+import { WalletConnectedBoundary } from '@masknet/shared'
 
 const useStyles = makeStyles()((theme) => ({
     button: {
@@ -51,7 +51,6 @@ export default function ConnectButton({
     const web3 = useWeb3(NetworkPluginID.PLUGIN_EVM)
     const { pluginID } = useNetworkContext()
     const { account } = useChainContext()
-    const [cc, setCC] = useState<CyberConnect | null>(null)
     const [isFollowing, setFollowing] = useState(false)
     const [isLoading, setLoading] = useState(false)
 
@@ -61,36 +60,36 @@ export default function ConnectButton({
         setFollowing(res.isFollowing)
     }, [address, account])
 
-    useEffect(() => {
+    const ccInstance = useMemo(() => {
         if (!web3?.eth.currentProvider) return
-        const ccInstance = new CyberConnect({
+        return new CyberConnect({
             provider: web3.eth.currentProvider,
             namespace: 'Mask',
             env: process.env.NODE_ENV === 'production' ? Env.PRODUCTION : Env.STAGING,
         })
-
-        setCC(ccInstance)
     }, [web3, account])
 
     const handleClick = useCallback(() => {
-        if (!cc) return
+        if (!ccInstance) return
         setLoading(true)
         if (!isFollowing) {
-            cc.connect(address)
+            ccInstance
+                .connect(address)
                 .then(() => {
                     setFollowing(true)
                     refreshFollowList()
                 })
                 .finally(() => setLoading(false))
         } else {
-            cc.disconnect(address)
+            ccInstance
+                .disconnect(address)
                 .then(() => {
                     setFollowing(false)
                     refreshFollowList()
                 })
                 .finally(() => setLoading(false))
         }
-    }, [cc, account, isFollowing])
+    }, [ccInstance, address, isFollowing])
 
     if (!isSameAddress(account, address)) {
         return (
