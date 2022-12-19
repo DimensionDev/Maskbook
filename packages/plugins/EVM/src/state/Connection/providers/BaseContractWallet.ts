7669ea5b8d28d6e086c7b8c1c1e9681828661937
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2022-12-19 23:04:09 +0800

    fix: 4337 OP (#8314)
    * chore: update lock file
    
    * fix: setup storage

--- packages/plugins/EVM/src/state/Connection/providers/BaseContractWallet.ts
+++ packages/plugins/EVM/src/state/Connection/providers/BaseContractWallet.ts
@@ -1,6 +1,6 @@
 import { ECKeyIdentifier, StorageItem } from '@masknet/shared-base'
 import { isSameAddress } from '@masknet/web3-shared-base'
-import { isValidAddress } from '@masknet/web3-shared-evm'
+import { ProviderType, isValidAddress } from '@masknet/web3-shared-evm'
 import type { EVM_Provider } from '../types.js'
 import { BaseHostedProvider } from './BaseHosted.js'
 import { SharedContextSettings } from '../../../settings/index.js'
@@ -11,32 +11,36 @@ const EMPTY_IDENTIFIER = new ECKeyIdentifier('secp256k1', 'EMPTY')
  * EIP-4337 compatible smart contract based wallet.
  */
 export class BaseContractWalletProvider extends BaseHostedProvider implements EVM_Provider {
-    private _ownerStorage?: StorageItem<{
-        account: string
-        identifier: ECKeyIdentifier
-    }>
+    private ownerStorage:
+        | StorageItem<{
+              account: string
+              identifier: ECKeyIdentifier
+          }>
+        | undefined
 
-    get ownerStorage() {
-        const { storage } = SharedContextSettings.value
-            .createKVStorage('memory', {})
-            .createSubScope(`${this.providerType}_hosted`, {
+    constructor(protected override providerType: ProviderType) {
+        super(providerType)
+
+        // setup storage
+        SharedContextSettings.readyPromise.then((context) => {
+            const { storage } = context.createKVStorage('memory', {}).createSubScope(`${this.providerType}_hosted`, {
                 value: {
                     account: this.options.getDefaultAccount(),
                     // empty string means EOA signer
                     identifier: EMPTY_IDENTIFIER,
                 },
             })
-        this._ownerStorage = storage.value
-        return this._ownerStorage
+            this.ownerStorage = storage.value
+        })
     }
 
     get owner() {
-        return this.ownerStorage.value.account
+        return this.ownerStorage?.value.account ?? this.options.getDefaultAccount()
     }
 
     get identifier() {
-        const identifier = this.ownerStorage.value.identifier
-        return identifier.rawPublicKey === 'EMPTY' ? undefined : identifier
+        const identifier = this.ownerStorage?.value.identifier
+        return identifier?.rawPublicKey === 'EMPTY' ? undefined : identifier
     }
 
     override async switchAccount(account?: string, owner?: string, identifier = EMPTY_IDENTIFIER) {
@@ -46,7 +50,7 @@ export class BaseContractWalletProvider extends BaseHostedProvider implements EV
 
         // ensure account switching is successful
         if (isSameAddress(this.account, account)) {
-            await this.ownerStorage.setValue({
+            await this.ownerStorage?.setValue({
                 account: owner,
                 identifier,
             })
