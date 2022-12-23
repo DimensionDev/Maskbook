b3aa25b6bfd4f51a72c803e54dc8f42a30aa5a2c
nuanyang233 <nuanyang233@gmail.com>
2022-12-23 15:19:55 +0800

    feat: logout tip

--- packages/dashboard/src/pages/Personas/components/LogoutPersonaDialog/index.tsx
+++ packages/dashboard/src/pages/Personas/components/LogoutPersonaDialog/index.tsx
@@ -1,9 +1,11 @@
 import { Icons } from '@masknet/icons'
 import { DashboardRoutes, PersonaIdentifier } from '@masknet/shared-base'
 import { MaskDialog } from '@masknet/theme'
+import { useWallets } from '@masknet/web3-hooks-base'
+import { formatEthereumAddress } from '@masknet/web3-shared-evm'
 import { LoadingButton } from '@mui/lab'
 import { Box, Button, DialogActions, DialogContent, Typography } from '@mui/material'
-import { memo, useCallback } from 'react'
+import { memo, useCallback, useMemo } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Services } from '../../../../API.js'
 import { useDashboardI18N } from '../../../../locales/index.js'
@@ -11,13 +13,15 @@ import { PersonaContext } from '../../hooks/usePersonaContext.js'
 
 export interface LogoutPersonaDialogProps {
     open: boolean
+    nickname?: string
     onClose: () => void
     identifier: PersonaIdentifier
 }
 
-export const LogoutPersonaDialog = memo<LogoutPersonaDialogProps>(({ open, onClose, identifier }) => {
+export const LogoutPersonaDialog = memo<LogoutPersonaDialogProps>(({ open, onClose, identifier, nickname }) => {
     const t = useDashboardI18N()
     const navigate = useNavigate()
+    const wallets = useWallets()
     const { changeCurrentPersona } = PersonaContext.useContainer()
     const handleLogout = useCallback(async () => {
         await Services.Identity.logoutPersona(identifier)
@@ -32,6 +36,10 @@ export const LogoutPersonaDialog = memo<LogoutPersonaDialogProps>(({ open, onClo
         }
     }, [identifier, onClose])
 
+    const manageWallets = useMemo(() => {
+        return wallets.filter((x) => x.identifier?.toText() === identifier.toText())
+    }, [wallets, identifier])
+
     return (
         <MaskDialog open={open} title={t.personas_logout()} onClose={onClose} maxWidth="xs">
             <DialogContent>
@@ -43,6 +51,14 @@ export const LogoutPersonaDialog = memo<LogoutPersonaDialogProps>(({ open, onClo
                 <Typography color="error" variant="body2" fontSize={13}>
                     {t.personas_logout_warning()}
                 </Typography>
+                {manageWallets.length ? (
+                    <Typography color="error" variant="body2" fontSize={13}>
+                        {t.personas_logout_manage_wallet_warning({
+                            persona: nickname ?? '',
+                            addresses: manageWallets.map((x) => formatEthereumAddress(x.address, 4)).join(','),
+                        })}
+                    </Typography>
+                ) : null}
             </DialogContent>
             <DialogActions>
                 <Button color="secondary" onClick={onClose} sx={{ minWidth: 150 }}>
