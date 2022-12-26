bb92f7bda38f22a6f6c01d71084adf8adda6926e
guanbinrui <guanbinrui@dimension.im>
2022-12-26 15:47:23 +0800

    fix: test

--- packages/web3-shared/evm/tests/libs/UserTransaction.ts
+++ packages/web3-shared/evm/tests/libs/UserTransaction.ts
@@ -1,31 +1,25 @@
-import Web3 from 'web3'
 import { describe, test, expect } from 'vitest'
 import { UserTransaction } from '../../src/libs/UserTransaction.js'
 import { ChainId } from '../../src/types/index.js'
 
 describe('UserTransaction', async () => {
-    const userTransaction = await UserTransaction.fromUserOperation(
-        ChainId.Matic,
-        new Web3(),
-        '0x0000000000000000000000000000000000000000',
-        {
-            sender: '0x0000000000000000000000000000000000000000',
-            nonce: 0,
-            initCode: '0x',
-            callData: '0x',
-            callGas: '0',
-            verificationGas: '100000',
-            preVerificationGas: '100000',
-            maxFeePerGas: '100000',
-            maxPriorityFeePerGas: '100000',
-            paymaster: '0x0000000000000000000000000000000000000000',
-            paymasterData: '0x',
-            signature: '0x',
-        },
-    )
+    const userTransaction = new UserTransaction(ChainId.Matic, '0x0000000000000000000000000000000000000000', {
+        sender: '0x0000000000000000000000000000000000000000',
+        nonce: 0,
+        initCode: '0x',
+        callData: '0x',
+        callGas: '0',
+        verificationGas: '100000',
+        preVerificationGas: '100000',
+        maxFeePerGas: '100000',
+        maxPriorityFeePerGas: '100000',
+        paymaster: '0x0000000000000000000000000000000000000000',
+        paymasterData: '0x',
+        signature: '0x',
+    })
 
     test('hasPaymaster', () => {
-        expect(userTransaction.hasPaymaster).toBe(true)
+        expect(userTransaction.hasPaymaster).toBe(false)
     })
 
     test('pack', () => {
@@ -37,12 +31,12 @@ describe('UserTransaction', async () => {
     })
 
     test('hash', () => {
-        const hash = '0xa291d506adc38a60c6539bd4eef7dcd01a04b84d5196a12d91bd39d8903c5c30'
+        const hash = '0x2714ac199129eaf48570adf18ce5c6849ac1203a6b92d72bdb52ae816cfc72a2'
         expect(userTransaction.hash).toBe(hash)
     })
 
     test('requestId', () => {
-        const id = '0x4306460833ca3f38b308db101c2c411671cdbe9f2b511b00e56df9288702a5f4'
+        const id = '0x4c8c9017e6e601e9e23fd988e0ae770f12c719844b981948352ee04fe373f7f3'
         expect(userTransaction.requestId).toBe(id)
     })
 })
