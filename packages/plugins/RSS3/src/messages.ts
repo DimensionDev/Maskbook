486e3b4b5ce13701b65a12c2b200705d871f88b8
unclebill <billbill290@gmail.com>
2023-01-06 12:47:43 +0800

    chore: clean up rss3

--- packages/plugins/RSS3/src/messages.ts
+++
@@ -1,9 +0,0 @@
-import { createPluginMessage, PluginMessageEmitter, createPluginRPC } from '@masknet/plugin-infra'
-import { PLUGIN_ID } from './constants.js'
-
-interface ProfileMessages {
-    rpc: unknown
-}
-
-export const PluginProfileMessages: PluginMessageEmitter<ProfileMessages> = createPluginMessage(PLUGIN_ID)
-export const PluginProfileRPC = createPluginRPC(PLUGIN_ID, () => import('./services.js'), PluginProfileMessages.rpc)
