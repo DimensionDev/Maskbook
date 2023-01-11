14014de19c8500dcef1ae534be6463f5f4d6fd94
lelenei <zwpsky@gmail.com>
2023-01-11 20:31:09 +0800

    fix: bugfix

--- packages/mask/src/plugins/Trader/SNSAdaptor/trending/TrendingView.tsx
+++ packages/mask/src/plugins/Trader/SNSAdaptor/trending/TrendingView.tsx
@@ -164,7 +164,7 @@ export function TrendingView(props: TrendingViewProps) {
         !isNFT &&
         !!trending?.coin.contract_address &&
         chainIdValid &&
-        trending?.coin.pluginID === NetworkPluginID.PLUGIN_EVM
+        trending.contracts?.[0].pluginID === NetworkPluginID.PLUGIN_EVM
     // #endregion
 
     // #region expected chainId
@@ -176,6 +176,7 @@ export function TrendingView(props: TrendingViewProps) {
                       {
                           chainId: trending.coin.chainId,
                           address: trending.coin.contract_address,
+                          pluginID: NetworkPluginID.PLUGIN_EVM,
                       },
                   ]
                 : []
