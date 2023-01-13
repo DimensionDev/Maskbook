c6678b85987cfaf78b906df527a6916ee2aa3c56
nuanyang233 <nuanyang233@gmail.com>
2023-01-13 13:16:00 +0800

    fix: bugfix for incorrect network in trader

--- packages/mask/src/plugins/Trader/Dashboard/index.tsx
+++ packages/mask/src/plugins/Trader/Dashboard/index.tsx
@@ -2,12 +2,18 @@ import type { Plugin } from '@masknet/plugin-infra'
 import type { ChainId } from '@masknet/web3-shared-evm'
 import { TraderDialog } from '../SNSAdaptor/trader/TraderDialog.js'
 import { base } from '../base.js'
+import { Web3ContextProvider } from '@masknet/web3-hooks-base'
+import { NetworkPluginID } from '@masknet/shared-base'
 
 const dashboard: Plugin.Dashboard.Definition<ChainId> = {
     ...base,
     init(signal) {},
     GlobalInjection() {
-        return <TraderDialog />
+        return (
+            <Web3ContextProvider value={{ pluginID: NetworkPluginID.PLUGIN_EVM }}>
+                <TraderDialog />
+            </Web3ContextProvider>
+        )
     },
 }
 
