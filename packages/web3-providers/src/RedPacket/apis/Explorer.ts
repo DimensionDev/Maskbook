3d74a71bed9eab10df5882b5c995f7125ecad1d7
zhouhanseng <z308114274@gmail.com>
2023-02-01 16:32:44 +0800

    chore: reply code review

--- packages/web3-providers/src/RedPacket/apis/Explorer.ts
+++ packages/web3-providers/src/RedPacket/apis/Explorer.ts
@@ -4,6 +4,7 @@ import { isSameAddress, Transaction } from '@masknet/web3-shared-base'
 import { getExplorerConstants } from '@masknet/web3-shared-evm'
 import type { ChainId, SchemaType } from '@masknet/web3-shared-evm'
 import type { RedPacketBaseAPI } from '../../types/RedPacket.js'
+import { fetchJSON } from '../../entry-helpers.js'
 
 export class ExplorerAPI implements RedPacketBaseAPI.DataSourceProvider<ChainId, SchemaType> {
     async getHistories(
@@ -17,28 +18,26 @@ export class ExplorerAPI implements RedPacketBaseAPI.DataSourceProvider<ChainId,
         const { EXPLORER_API, API_KEYS = [] } = getExplorerConstants(chainId)
         if (!EXPLORER_API || !senderAddress || !contractAddress || !startBlock || !endBlock || !methodId) return
 
-        const response = await fetch(
-            urlcat(EXPLORER_API, {
-                apikey: first(API_KEYS),
-                action: 'txlist',
-                module: 'account',
-                sort: 'desc',
-                startBlock,
-                endBlock,
-                address: contractAddress,
-            }),
-        )
+        try {
+            const { result } = await fetchJSON<{ result: Array<Transaction<ChainId, SchemaType>> }>(
+                urlcat(EXPLORER_API, {
+                    apikey: first(API_KEYS),
+                    action: 'txlist',
+                    module: 'account',
+                    sort: 'desc',
+                    startBlock,
+                    endBlock,
+                    address: contractAddress,
+                }),
+            )
 
-        if (!response.ok) return
+            if (!result) return
 
-        const {
-            result,
-        }: {
-            result: Array<Transaction<ChainId, SchemaType>>
-        } = await response.json()
-
-        return result.filter(
-            (x) => x.methodId?.toLowerCase() === methodId.toLowerCase() && isSameAddress(x.from, senderAddress),
-        )
+            return result.filter(
+                (x) => x.methodId?.toLowerCase() === methodId.toLowerCase() && isSameAddress(x.from, senderAddress),
+            )
+        } catch {
+            return
+        }
     }
 }
