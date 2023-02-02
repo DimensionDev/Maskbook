eeeb8bb801214b626d2802813c7fd8bf66b616f5
zhouhanseng <z308114274@gmail.com>
2023-02-02 20:42:21 +0800

    refactor: migrate transaction confirm modal

--- packages/mask/src/plugins/Tips/types/tip.ts
+++ packages/mask/src/plugins/Tips/types/tip.ts
@@ -1,11 +1,6 @@
 import type { Web3Helper } from '@masknet/web3-helpers'
 import type { SocialAccount } from '@masknet/web3-shared-base'
 
-export enum TipsType {
-    Tokens = 'tokens',
-    Collectibles = 'collectibles',
-}
-
 export interface TipTask {
     recipient?: string
     recipientSnsId?: string
