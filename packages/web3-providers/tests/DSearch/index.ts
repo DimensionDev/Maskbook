a895a6339fbdb583ac8023970138f6d04123968f
UncleBill <billbill290@gmail.com>
2023-02-07 17:56:30 +0800

    fixup! feat: remove dsearch hash tag check (#8607) (#8632)
    * fixup! feat: remove dsearch hash tag check (#8607)
    
    lower priority of searching token
    
    * refactor: remove logs
    
    ---------
    
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>

--- packages/web3-providers/tests/DSearch/index.ts
+++ packages/web3-providers/tests/DSearch/index.ts
@@ -1,11 +1,11 @@
-import { describe, expect, test } from 'vitest'
-import { NonFungibleCollectionResult, NonFungibleTokenResult } from '@masknet/web3-shared-base'
+import { describe, expect, it, test } from 'vitest'
+import { NonFungibleCollectionResult, NonFungibleTokenResult, DomainResult } from '@masknet/web3-shared-base'
 import type { Web3Helper } from '@masknet/web3-helpers'
 import { DSearchAPI } from '../../src/DSearch/index.js'
 
 /* cspell:disable */
 describe('DSearch test', () => {
-    test('should return from specific list only', async () => {
+    it('should return from specific list only', async () => {
         const DSearch = new DSearchAPI()
         const result = await DSearch.search('eth')
 
@@ -19,7 +19,7 @@ describe('DSearch test', () => {
         })
     })
 
-    test('should return by name', async () => {
+    it('should return by name', async () => {
         const DSearch = new DSearchAPI()
         const result = await DSearch.search('eth1')
 
@@ -33,7 +33,7 @@ describe('DSearch test', () => {
             pluginID: 'com.mask.evm',
         })
     })
-    test('should return by fuzzy search', async () => {
+    it('should return by fuzzy search', async () => {
         const DSearch = new DSearchAPI()
         const result = await DSearch.search('efuzzy')
 
@@ -46,7 +46,7 @@ describe('DSearch test', () => {
             pluginID: 'com.mask.evm',
         })
     })
-    test('should return by fuzzy search without empty string', async () => {
+    it('should return by fuzzy search without empty string', async () => {
         const DSearch = new DSearchAPI()
         const result = await DSearch.search('searchempty')
 
@@ -60,7 +60,7 @@ describe('DSearch test', () => {
         })
     })
 
-    test('should return collection by twitter handle', async () => {
+    it('should return collection by twitter handle', async () => {
         const DSearch = new DSearchAPI()
         const result = (await DSearch.search('mathcastles')) as Array<
             NonFungibleCollectionResult<Web3Helper.ChainIdAll, Web3Helper.SchemaTypeAll>
@@ -70,7 +70,7 @@ describe('DSearch test', () => {
         expect(result[0]?.name).toBe('Terraforms')
     })
 
-    test('should return all the data with tag prefix', async () => {
+    it('should return all the data with tag prefix', async () => {
         const DSearch = new DSearchAPI()
         const result = (await DSearch.search('$eth')) as Array<
             NonFungibleTokenResult<Web3Helper.ChainIdAll, Web3Helper.SchemaTypeAll>
@@ -78,5 +78,12 @@ describe('DSearch test', () => {
 
         expect(result.length).toBe(1)
         expect(result[0]?.name).toBe('eth1')
+    })
+
+    test('searching lens profile', async () => {
+        const DSearch = new DSearchAPI()
+        const result = (await DSearch.search('sujiyan.lens')) as Array<DomainResult<Web3Helper.ChainIdAll>>
+        expect(result.length).toBe(1)
+        expect(result[0].domain).toBe('sujiyan.lens')
     })
 })
