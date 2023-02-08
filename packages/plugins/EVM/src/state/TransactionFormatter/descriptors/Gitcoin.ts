2fdbb32dc4cfb49ba2f9afdca9f0a268cf915057
unclebill <billbill290@gmail.com>
2023-02-08 19:20:31 +0800

    fix: mf-3326 token amout in notification

--- packages/plugins/EVM/src/state/TransactionFormatter/descriptors/Gitcoin.ts
+++ packages/plugins/EVM/src/state/TransactionFormatter/descriptors/Gitcoin.ts
@@ -1,6 +1,7 @@
 import { i18NextInstance } from '@masknet/shared-base'
 import { TransactionContext, formatBalance } from '@masknet/web3-shared-base'
 import { ChainId, getGitcoinConstant, getNativeTokenAddress } from '@masknet/web3-shared-evm'
+import { BigNumber } from 'bignumber.js'
 import { Web3StateSettings } from '../../../settings/index.js'
 import type { TransactionDescriptor } from '../types.js'
 
@@ -26,25 +27,27 @@ export class GitcoinDescriptor implements TransactionDescriptor {
 
         const GITCOIN_ETH_ADDRESS = getGitcoinConstant(context.chainId, 'GITCOIN_ETH_ADDRESS')
         const nativeTokenAddress = getNativeTokenAddress(context.chainId)
-        const value = context_.value
 
         for (const { name, parameters } of context.methods) {
             if (name === 'donate' && parameters) {
-                const tokenAddress = parameters?._donations[0].token
+                const tokenAddress = parameters._donations[0].token
                 const address = tokenAddress === GITCOIN_ETH_ADDRESS ? nativeTokenAddress : tokenAddress
                 const token = await hub?.getFungibleToken?.(address, { chainId: context.chainId })
+                const amount = new BigNumber(parameters._donations[0].amount)
+                    .plus(parameters._donations[1].amount)
+                    .toFixed()
                 return {
                     chainId: context.chainId,
                     tokenInAddress: address,
-                    tokenInAmount: value,
+                    tokenInAmount: amount,
                     title: i18NextInstance.t('plugin_infra_descriptor_gitcoin_donate_title'),
                     description: i18NextInstance.t('plugin_infra_descriptor_gitcoin_submitted', {
-                        amount: formatBalance(value, token?.decimals, 6),
+                        amount: formatBalance(amount, token?.decimals, 6),
                         symbol: token?.symbol,
                     }),
                     snackbar: {
                         successfulDescription: i18NextInstance.t('plugin_infra_descriptor_gitcoin_donate_success', {
-                            amount: formatBalance(value, token?.decimals, 6),
+                            amount: formatBalance(amount, token?.decimals, 6),
                             symbol: token?.symbol,
                         }),
                         failedDescription: i18NextInstance.t('plugin_infra_descriptor_gitcoin_donate_fail'),
