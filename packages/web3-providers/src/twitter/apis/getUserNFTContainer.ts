0212a9d04a328b8acaf6fe84d7dbb8303b30ae70
lelenei <zwpsky@gmail.com>
2023-02-13 21:10:50 +0800

    fix: update twitter user api

---
+++ packages/web3-providers/src/twitter/apis/getUserNFTContainer.ts
@@ -0,0 +1,38 @@
+import urlcat from 'urlcat'
+import { fetchJSON } from '../../helpers'
+import type { TwitterBaseAPI } from '../../types'
+
+export async function getUserNFTContainer(
+    screenName: string,
+    queryToken: string,
+    bearerToken: string,
+    csrfToken: string,
+) {
+    return fetchJSON<{
+        data: {
+            user: {
+                result: TwitterBaseAPI.NFTContainer
+            }
+        }
+    }>(
+        urlcat('https://twitter.com/i/api/graphql/:queryToken/userNftContainer_Query', {
+            queryToken,
+            variables: JSON.stringify({
+                screenName,
+            }),
+            features: JSON.stringify({
+                responsive_web_twitter_blue_verified_badge_is_enabled: false,
+            }),
+        }),
+        {
+            headers: {
+                authorization: `Bearer ${bearerToken}`,
+                'x-csrf-token': csrfToken,
+                'content-type': 'application/json',
+                'x-twitter-auth-type': 'OAuth2Session',
+                'x-twitter-active-user': 'yes',
+                referer: `https://twitter.com/${screenName}/nft`,
+            },
+        },
+    )
+}
