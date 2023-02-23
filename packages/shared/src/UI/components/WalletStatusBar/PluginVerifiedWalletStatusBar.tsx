c3875b09bb500c964fdd698c2bce492de3e72065
lelenei <72531217+lelenei@users.noreply.github.com>
2023-02-23 20:03:56 +0800

    fix: add shadow in wallet menu (#8783)

--- packages/shared/src/UI/components/WalletStatusBar/PluginVerifiedWalletStatusBar.tsx
+++ packages/shared/src/UI/components/WalletStatusBar/PluginVerifiedWalletStatusBar.tsx
@@ -53,6 +53,10 @@ const useStyles = makeStyles()((theme) => ({
     },
     menu: {
         background: theme.palette.maskColor.bottom,
+        boxShadow:
+            theme.palette.mode === 'dark'
+                ? '0px 4px 30px rgba(255, 255, 255, 0.15)'
+                : '0px 4px 30px rgba(0, 0, 0, 0.1)',
     },
 }))
 
