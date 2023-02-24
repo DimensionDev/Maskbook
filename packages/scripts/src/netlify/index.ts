a9f3b0f303025ef22a8509cdd39bf19324d1c7c9
Jack Works <jackworks@protonmail.com>
2023-02-24 20:08:09 +0800

    fix: build script

--- packages/scripts/src/netlify/index.ts
+++ packages/scripts/src/netlify/index.ts
@@ -1,17 +1,17 @@
 import { series, TaskFunction } from 'gulp'
-import { createBuildStorybook6, fromNPMTask, PKG_PATH, task } from '../utils/index.js'
+import { createBuildStorybook7, fromNPMTask, PKG_PATH, task } from '../utils/index.js'
 import { codegen } from '../codegen/index.js'
 
 const STATIC_PATH = new URL('netlify/storybook-static/', PKG_PATH)
 
 // prettier-ignore
-const dashboardSB = createBuildStorybook6(
+const dashboardSB = createBuildStorybook7(
     new URL('dashboard/', PKG_PATH),
     new URL('dashboard/', STATIC_PATH),
     'dashboard-storybook',
 )
 // prettier-ignore
-const themeSB = createBuildStorybook6(
+const themeSB = createBuildStorybook7(
     new URL('theme/', PKG_PATH),
     new URL('theme/', STATIC_PATH),
     'theme',
--- packages/scripts/src/utils/storybook.ts
+++ packages/scripts/src/utils/storybook.ts
@@ -1,8 +1,8 @@
 import { shell } from './run.js'
 import { fileURLToPath } from 'url'
-export function createBuildStorybook6(cwd: URL, output: URL, name: string) {
+export function createBuildStorybook7(cwd: URL, output: URL, name: string) {
     const fn = () => {
-        return shell.cwd(cwd)`npx build-storybook -o ${fileURLToPath(output)} --quiet`
+        return shell.cwd(cwd)`npx storybook build -o ${fileURLToPath(output)} --quiet`
     }
     fn.displayName = `${name}-storybook`
     fn.description = `Build storybook of ${name} to ${output}`
