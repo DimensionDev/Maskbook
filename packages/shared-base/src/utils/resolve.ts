b8c4930e865fc2f493d882f2c31abc3ba88fc39c
guanbinrui <guanbinrui@dimension.im>
2023-03-02 22:21:27 +0800

    fix: typo

--- packages/shared-base/src/utils/resolve.ts
+++ packages/shared-base/src/utils/resolve.ts
@@ -2,6 +2,6 @@ export type Unresolved<T, P = never> = T | ((options?: P) => T)
 
 export function resolve<T, P>(unresolved: Unresolved<T, P>, options?: P): T {
     if (typeof unresolved !== 'function') return unresolved
-    const unresolveFunction = unresolved as (options?: P) => T
-    return unresolveFunction(options)
+    const unresolvedFunction = unresolved as (options?: P) => T
+    return unresolvedFunction(options)
 }
