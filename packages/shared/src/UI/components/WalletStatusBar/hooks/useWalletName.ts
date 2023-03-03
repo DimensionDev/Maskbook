113a84da754107dfd1f2c5e5635eeaa2a55b07f5
nuanyang233 <nuanyang233@gmail.com>
2023-03-02 22:59:17 -0800

    fix: bugfix for wallet status bar (#8867)
    * fix: bugfix for wallet status bar
    
    * fix: deps

--- packages/shared/src/UI/components/WalletStatusBar/hooks/useWalletName.ts
+++ packages/shared/src/UI/components/WalletStatusBar/hooks/useWalletName.ts
@@ -1,14 +1,14 @@
 import { useMemo } from 'react'
 import type { NetworkPluginID } from '@masknet/shared-base'
 import {
-    useWallet,
     useChainContext,
     useReverseAddress,
     useWeb3State,
     useProviderDescriptor,
+    useWallets,
 } from '@masknet/web3-hooks-base'
 import { ProviderType } from '@masknet/web3-shared-evm'
-import { resolveNetworkWalletName } from '@masknet/web3-shared-base'
+import { isSameAddress, resolveNetworkWalletName } from '@masknet/web3-shared-base'
 
 export const useWalletName = (
     expectedAccount?: string,
@@ -18,20 +18,22 @@ export const useWalletName = (
     const { Others } = useWeb3State(expectedPluginId)
     const { account, providerType } = useChainContext({ account: expectedAccount })
     const { value: domain } = useReverseAddress(expectedPluginId, account)
-    const wallet = useWallet(expectedPluginId)
+    const wallets = useWallets()
     const providerDescriptor = useProviderDescriptor(expectedPluginId)
 
     return useMemo(() => {
         // Binding Wallet Just display domain and network name
         if (domain) return domain
         if (isNextIdWallet && expectedPluginId) return resolveNetworkWalletName(expectedPluginId)
+        const wallet = wallets.find((x) => isSameAddress(x.address, expectedAccount ?? account))
         if (providerType === ProviderType.MaskWallet && wallet?.name) return wallet?.name
 
         return providerDescriptor?.name || Others?.formatAddress(account, 4)
     }, [
+        wallets,
+        expectedAccount,
         providerType,
         domain,
-        wallet?.name,
         providerDescriptor?.name,
         Others?.formatAddress,
         account,
