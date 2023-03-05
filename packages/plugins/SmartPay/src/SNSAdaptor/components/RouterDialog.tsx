1269bd742aadf3e3494cc12483cbf396ddbb58aa
nuanyang233 <nuanyang233@gmail.com>
2023-03-05 09:11:27 -0800

    fix: smartpay verify next flow (#8925)

--- packages/plugins/SmartPay/src/SNSAdaptor/components/RouterDialog.tsx
+++ packages/plugins/SmartPay/src/SNSAdaptor/components/RouterDialog.tsx
@@ -9,13 +9,12 @@ import { useRemoteControlledDialog } from '@masknet/shared-base-ui'
 import { makeStyles } from '@masknet/theme'
 import { useWallets } from '@masknet/web3-hooks-base'
 import { SmartPayBundler, SmartPayFunder, SmartPayOwner } from '@masknet/web3-providers'
-import { isSameAddress } from '@masknet/web3-shared-base'
 import { DialogContent, CircularProgress } from '@mui/material'
 import { Box } from '@mui/system'
 import { compact } from 'lodash-es'
 import { useCallback, useMemo } from 'react'
 import { matchPath, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
-import { useAsync, useUpdateEffect } from 'react-use'
+import { useAsync } from 'react-use'
 import { RoutePaths } from '../../constants.js'
 import { SmartPayContext } from '../../hooks/useSmartPayContext.js'
 import { useI18N } from '../../locales/i18n_generated.js'
@@ -96,16 +95,6 @@ export function RouterDialog() {
         if (state?.canBack) return navigate(-1)
         closeDialog()
     }, [state])
-
-    useUpdateEffect(() => {
-        if (
-            (signer?.signPersona &&
-                currentPersona?.identifier.publicKeyAsHex !== signer.signPersona.identifier.publicKeyAsHex) ||
-            (signer?.signWallet && !wallets.some((x) => isSameAddress(x.address, signer.signWallet?.address)))
-        ) {
-            closeDialog()
-        }
-    }, [currentPersona, signer, wallets])
 
     return (
         <InjectedDialog
