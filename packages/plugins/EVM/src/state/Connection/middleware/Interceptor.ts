2d4934c1c9639599e5d60cfde23e846d6ef8d74e
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-03-06 16:00:25 +0800

    refactor: composer (#8938)

--- packages/plugins/EVM/src/state/Connection/middleware/Interceptor.ts
+++ packages/plugins/EVM/src/state/Connection/middleware/Interceptor.ts
@@ -10,20 +10,20 @@ import { Popups } from '../interceptors/Popups.js'
 
 export class Interceptor implements Middleware<ConnectionContext> {
     private composers: Partial<Record<ProviderType, Composer<ConnectionContext>>> = {
-        [ProviderType.None]: Composer.from([new NoneWallet()]),
-        [ProviderType.MaskWallet]: Composer.from([
+        [ProviderType.None]: Composer.from(new NoneWallet()),
+        [ProviderType.MaskWallet]: Composer.from(
             new Popups(),
             new ContractWallet(ProviderType.MaskWallet, SmartPayAccount, SmartPayBundler, SmartPayFunder),
             new MaskWallet(),
-        ]),
-        [ProviderType.MetaMask]: Composer.from([new MetaMask()]),
-        [ProviderType.WalletConnect]: Composer.from([new WalletConnect()]),
-        [ProviderType.Coin98]: Composer.from([new MetaMask()]),
-        [ProviderType.WalletLink]: Composer.from([new MetaMask()]),
-        [ProviderType.MathWallet]: Composer.from([new MetaMask()]),
-        [ProviderType.Fortmatic]: Composer.from([new Fortmatic()]),
-        [ProviderType.Opera]: Composer.from([new MetaMask()]),
-        [ProviderType.Clover]: Composer.from([new MetaMask()]),
+        ),
+        [ProviderType.MetaMask]: Composer.from(new MetaMask()),
+        [ProviderType.WalletConnect]: Composer.from(new WalletConnect()),
+        [ProviderType.Coin98]: Composer.from(new MetaMask()),
+        [ProviderType.WalletLink]: Composer.from(new MetaMask()),
+        [ProviderType.MathWallet]: Composer.from(new MetaMask()),
+        [ProviderType.Fortmatic]: Composer.from(new Fortmatic()),
+        [ProviderType.Opera]: Composer.from(new MetaMask()),
+        [ProviderType.Clover]: Composer.from(new MetaMask()),
     }
 
     async fn(context: ConnectionContext, next: () => Promise<void>) {
