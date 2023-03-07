ef1d84db3adb430618e4b413839344a879605463
guanbinrui <guanbinrui@dimension.im>
2023-03-07 11:33:58 +0800

    refactor: captureFetchException

--- packages/web3-providers/src/helpers/captureFetchException.ts
+++ packages/web3-providers/src/helpers/captureFetchException.ts
@@ -1,30 +1,51 @@
 export async function captureFetchException(request: Request, response?: Response) {
-    Sentry.captureException(new Error(`Failed to fetch: ${request.url}`), {
-        tags: {
-            source: new URL(request.url).host,
-            method: request.method.toUpperCase(),
-            url: request.url,
-            response_type: response?.type,
-            status_code: response?.status,
-            status_text: response?.statusText,
-            request_headers: JSON.stringify(Object.fromEntries(request.headers.entries())),
-            request_body: await getRequestJSON(request.clone()),
-            request_text: await getRequestText(request.clone()),
+    const requestHeaders = getHeaders(request.clone())
+    const responseHeaders = getHeaders(response?.clone())
+    const requestBody = (await getBodyAsJSON(request.clone())) || (await getBodyAsText(request.clone()))
+    const responseBody = (await getBodyAsJSON(response?.clone())) || (await getBodyAsText(response?.clone()))
+    Sentry.captureException(
+        new Error(
+            [
+                `Failed to fetch: ${request.url}`,
+                `  with Request Headers: ${requestHeaders}`,
+                `  with Request Body: ${requestBody}`,
+                `  with Response Headers: ${responseHeaders}`,
+                `  with Response Body: ${responseBody}`,
+            ].join('\n'),
+        ),
+        {
+            tags: {
+                source: new URL(request.url).host,
+                method: request.method.toUpperCase(),
+                url: request.url,
+                response_type: response?.type,
+                status_code: response?.status,
+                status_text: response?.statusText,
+            },
         },
-    })
+    )
 }
-function getRequestJSON(request: Request) {
+
+function getHeaders(requestOrResponse?: Request | Response) {
     try {
-        return request.json()
+        return JSON.stringify(Object.fromEntries(requestOrResponse?.headers.entries() ?? []))
     } catch {
-        return
+        return 'N/A'
     }
 }
 
-function getRequestText(request: Request) {
+function getBodyAsJSON(requestOrResponse?: Request | Response) {
     try {
-        return request.text()
+        return requestOrResponse?.json()
     } catch {
-        return
+        return 'N/A'
+    }
+}
+
+function getBodyAsText(requestOrResponse?: Request | Response) {
+    try {
+        return requestOrResponse?.text()
+    } catch {
+        return 'N/A'
     }
 }
--- packages/web3-providers/src/helpers/fetchJSON.ts
+++ packages/web3-providers/src/helpers/fetchJSON.ts
@@ -1,13 +1,9 @@
 import { fetch } from './fetch.js'
 import { fetchSquashed } from './fetchSquashed.js'
 import { fetchCached } from './fetchCached.js'
-import { captureFetchException } from './captureFetchException.js'
 
 export async function fetchJSON<T = unknown>(input: RequestInfo | URL, init?: RequestInit): Promise<T> {
     const response = await fetch(input, init, [fetchSquashed, fetchCached])
-    if (!response.ok) {
-        await captureFetchException(new Request(input, init), response)
-        throw new Error('Failed to fetch as JSON.')
-    }
+    if (!response.ok) throw new Error('Failed to fetch as JSON.')
     return response.json()
 }
