9754ae5a3918977806b89f2780dbb9646202afc1
lelenei <zwpsky@gmail.com>
2023-03-07 14:17:19 +0800

    refactor: prettier error

--- packages/web3-shared/evm/src/libs/PayloadEditor.ts
+++ packages/web3-shared/evm/src/libs/PayloadEditor.ts
@@ -1,19 +1,19 @@
+import CREATE2_FACTORY_ABI from '@masknet/web3-contracts/abis/Create2Factory.json'
+import type { ECKeyIdentifier, Proof, ProofPayload, Wallet } from '@masknet/web3-shared-base'
 import { first, isUndefined, omitBy } from 'lodash-es'
 import Web3 from 'web3'
-import { type AbiItem, hexToNumber, hexToNumberString, toHex } from 'web3-utils'
 import type { JsonRpcPayload } from 'web3-core-helpers'
-import type { Wallet , ECKeyIdentifier, Proof, ProofPayload } from '@masknet/web3-shared-base'
-import CREATE2_FACTORY_ABI from '@masknet/web3-contracts/abis/Create2Factory.json'
+import { hexToNumber, hexToNumberString, toHex, type AbiItem } from 'web3-utils'
+import { formatEthereumAddress } from '../helpers/formatter.js'
+import { createJsonRpcPayload } from '../helpers/provider.js'
+import { ZERO_ADDRESS, getSmartPayConstant } from '../index.js'
 import {
-    type ChainId,
     EthereumMethodType,
+    type ChainId,
     type Transaction,
     type TransactionOptions,
     type UserOperation,
 } from '../types/index.js'
-import { formatEthereumAddress } from '../helpers/formatter.js'
-import { createJsonRpcPayload } from '../helpers/provider.js'
-import { ZERO_ADDRESS, getSmartPayConstant } from '../index.js'
 
 type Options = Pick<TransactionOptions, 'account' | 'chainId'>
 
