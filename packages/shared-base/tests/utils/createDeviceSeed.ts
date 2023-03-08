2f55d28a4f949cec038a2710e17925cf43f9cf6e
guanbinrui <guanbinrui@dimension.im>
2023-03-08 23:44:29 +0800

    test: isDeviceOnWhitelist

--- packages/shared-base/tests/utils/createDeviceSeed.ts
+++ packages/shared-base/tests/utils/createDeviceSeed.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest'
 import { createDeviceSeed } from '../../src/utils/createDeviceSeed.js'
 
 describe('create device seed', () => {
-    it('should create a in range', () => {
+    it('should create a seed in range', () => {
         const seed = createDeviceSeed(3)
 
         expect(seed >= 0).toBeTruthy()
