896c2f4acc89c061d9067da1a800febd3374fe3b
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-03-08 23:57:54 +0800

    refactor: update comment

--- packages/shared-base/tests/utils/isDeviceOnWhitelist.ts
+++ packages/shared-base/tests/utils/isDeviceOnWhitelist.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 import { isDeviceOnWhitelist } from '../../src/utils/isDeviceOnWhitelist.js'
 
-describe('detect a device whether is in the grayscale range', () => {
+describe('detect a device whether it is in the grayscale range', () => {
     it('grayscale', () => {
         expect(isDeviceOnWhitelist(100)).toBeTruthy()
         expect(isDeviceOnWhitelist(0)).toBeFalsy()
