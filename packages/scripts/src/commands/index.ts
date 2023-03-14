fdfd3c8dbeb1fa24eea54b61957a3dad354fc672
Jack Works <jackworks@protonmail.com>
2023-03-14 12:55:11 +0800

    fix: ci

--- packages/scripts/src/commands/index.ts
+++ packages/scripts/src/commands/index.ts
@@ -1,6 +1,5 @@
 export * from './clean.js'
 export * from './help.js'
 export * from './new-package.js'
-export * from './fix-lockfile.js'
 export * from './sync-develop.js'
 export * from './changeset-release.js'
