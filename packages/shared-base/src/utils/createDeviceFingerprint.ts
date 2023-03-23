cd30248ce0c8e56a49ab2bf84cf165e8f7850101
zhouhanseng <z308114274@gmail.com>
2023-03-23 18:41:07 +0800

    fix: type

--- packages/shared-base/src/utils/createDeviceFingerprint.ts
+++ packages/shared-base/src/utils/createDeviceFingerprint.ts
@@ -26,7 +26,6 @@ function createCanvasFingerprint() {
     context.textBaseline = 'alphabetic'
     context.fillStyle = 'rgb(255,5,5)'
     context.rotate(0.03)
-    // eslint-disable-next-line @dimensiondev/unicode-specific-set
     context.fillText('mask9\u00E90#\u30DE\u30B9\u30AFfbz1\uB9C8\uC2A4\uD06C$%^@\u00A3\u00E9\u00FA\u9762\u5177', 4, 17)
     context.fillStyle = 'rgb(155,255,5)'
     context.shadowBlur = 8
