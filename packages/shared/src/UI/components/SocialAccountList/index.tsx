3fc7d3d2f6452a123e10d6a8206d82affda8be68
unclebill <billbill290@gmail.com>
2023-03-24 13:38:04 +0800

    fix(Lens): mf-3780 mf-3791 show the first three platform icons only

--- packages/shared/src/UI/components/SocialAccountList/index.tsx
+++ packages/shared/src/UI/components/SocialAccountList/index.tsx
@@ -6,8 +6,8 @@ import { openWindow } from '@masknet/shared-base-ui'
 import { makeStyles } from '@masknet/theme'
 import { resolveNextIDPlatformLink } from '@masknet/web3-shared-base'
 import { Button, MenuItem, Typography, alpha } from '@mui/material'
-import { debounce } from 'lodash-es'
-import { type HTMLProps, useEffect, useRef, useState } from 'react'
+import { debounce, uniqBy } from 'lodash-es'
+import { type HTMLProps, useEffect, useMemo, useRef, useState } from 'react'
 import { useWindowScroll } from 'react-use'
 import { SocialTooltip } from './SocialTooltip.js'
 import { resolveNextIDPlatformIcon } from './utils.js'
@@ -26,6 +26,7 @@ const useStyles = makeStyles()((theme) => {
             boxSizing: 'border-box',
             backgroundColor: alpha(theme.palette.common.white, 0.4),
             borderRadius: 8,
+            minWidth: 'auto',
             '&:hover': {
                 backgroundColor: alpha(theme.palette.common.white, 0.4),
             },
@@ -37,6 +38,7 @@ const useStyles = makeStyles()((theme) => {
             marginLeft: '-3.5px',
             ':nth-of-type(1)': {
                 zIndex: 3,
+                marginLeft: 0,
             },
             ':nth-of-type(2)': {
                 zIndex: 2,
@@ -198,17 +200,22 @@ export function SocialAccountList({ nextIdBindings, ...rest }: SocialAccountList
 
     useEffect(closeMenu, [position])
 
+    const platformIcons = useMemo(() => {
+        return uniqBy(nextIdBindings, (x) => x.platform)
+            .sort((x) => (x.platform === NextIDPlatform.LENS ? -1 : 0))
+            .map((x) => resolveNextIDPlatformIcon(x.platform))
+            .filter(isNonNull)
+            .slice(0, 3)
+    }, [nextIdBindings])
+
+    if (!platformIcons.length) return null
+
     return (
         <div {...rest}>
             <Button variant="text" onClick={openMenu} className={classes.iconStack} disableRipple>
-                {nextIdBindings
-                    .sort((x) => (x.platform === NextIDPlatform.LENS ? -1 : 0))
-                    .map((x) => resolveNextIDPlatformIcon(x.platform))
-                    .filter(isNonNull)
-                    .slice(0, 3)
-                    .map((Icon, index) => (
-                        <Icon key={index} className={classes.icon} size={20} />
-                    ))}
+                {platformIcons.map((Icon, index) => (
+                    <Icon key={index} className={classes.icon} size={20} />
+                ))}
             </Button>
             {menu}
         </div>
