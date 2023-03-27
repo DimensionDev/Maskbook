0acab641518eaf5c94404821a0f08ec97f90b5d8
UncleBill <billbill290@gmail.com>
2023-03-27 22:42:28 +0800

    fix(ArtBlocks): mf-3793 fix pathname regexp (#9165)

--- packages/mask/src/plugins/ArtBlocks/tests/check.ts
+++ packages/mask/src/plugins/ArtBlocks/tests/check.ts
@@ -3,7 +3,11 @@ import { artBlocksHostnames, artBlocksPathnameRegex } from '../constants.js'
 import { checkUrl, getAssetInfoFromURL } from '../utils.js'
 
 const protocols = ['', 'http://', 'https://']
-const pathnames = ['/project/410', '/collections/presents/projects/0x99a9b7c1116f9ceeb1652de04d5969cce509b069/410']
+const pathnames = [
+    '/project/410',
+    '/collections/presents/projects/0x99a9b7c1116f9ceeb1652de04d5969cce509b069/410',
+    '/collections/curated/projects/0x99a9b7c1116f9ceeb1652de04d5969cce509b069/426',
+]
 
 describe('ArtBlocks', () => {
     it.each(pathnames)('matches pathname %s', (pathname) => {
@@ -23,10 +27,14 @@ describe('ArtBlocks', () => {
     })
 
     it.each([
-        'https://www.artblocks.io/project/410',
-        'https://www.artblocks.io/collections/presents/projects/0x99a9b7c1116f9ceeb1652de04d5969cce509b069/410',
-    ])('gets assets from url %s', (url) => {
+        ['https://www.artblocks.io/project/410', '410'],
+        [
+            'https://www.artblocks.io/collections/presents/projects/0x99a9b7c1116f9ceeb1652de04d5969cce509b069/410',
+            '410',
+        ],
+        ['https://www.artblocks.io/collections/curated/projects/0x99a9b7c1116f9ceeb1652de04d5969cce509b069/426', '426'],
+    ])('gets assets from url %s', (url, id) => {
         const result = getAssetInfoFromURL(url)
-        expect(result?.project_id).toBe('410')
+        expect(result?.project_id).toBe(id)
     })
 })
