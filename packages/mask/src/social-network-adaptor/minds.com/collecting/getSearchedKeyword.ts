81dc6b334f43c5b68f5065e93bdddb059ca141f7
unclebill <billbill290@gmail.com>
2023-04-02 11:14:44 +0800

    fix: no need to decodeURIComponent value from result of URLSearchParams

--- packages/mask/src/social-network-adaptor/minds.com/collecting/getSearchedKeyword.ts
+++ packages/mask/src/social-network-adaptor/minds.com/collecting/getSearchedKeyword.ts
@@ -1,7 +1,7 @@
 export default function getSearchedKeywordAtMinds() {
     const params = new URLSearchParams(location.search)
     if (location.pathname === '/discovery/search') {
-        return decodeURIComponent(params.get('q') ?? '')
+        return params.get('q') ?? ''
     }
 
     return ''
