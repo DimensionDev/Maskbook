cb5e1fe2531bd564984016475eb4e48d6d6e6ec4
lelenei <zwpsky@gmail.com>
2023-04-03 16:20:43 +0800

    fix: i18n

--- packages/plugins/Web3Profile/src/SNSAdaptor/index.tsx
+++ packages/plugins/Web3Profile/src/SNSAdaptor/index.tsx
@@ -73,10 +73,13 @@ const sns: Plugin.SNSAdaptor.Definition = {
                 nextIdRequired: true,
                 category: 'dapp',
                 recommendFeature,
-                description: {
-                    i18nKey: '__plugin_description',
-                    fallback: 'Choose and showcase your Web3 footprints on Twitter.',
-                },
+                description: (
+                    <Trans
+                        i18nKey="__plugin_description"
+                        defaults="Choose and showcase your Web3 footprints on Twitter."
+                        ns={base.ID}
+                    />
+                ),
                 features: [
                     {
                         name: <Trans i18nKey="plugin_web3_profile_feature_general_user_name" />,
