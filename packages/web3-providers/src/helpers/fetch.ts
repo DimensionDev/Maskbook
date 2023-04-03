5f0318bc00a6831907313dc14d02ddf7e4b2f20c
guanbinrui <guanbinrui@dimension.im>
2023-04-03 16:57:15 +0800

    refactor: add global fetch timeout

--- packages/web3-providers/src/helpers/fetch.ts
+++ packages/web3-providers/src/helpers/fetch.ts
@@ -1,4 +1,5 @@
 import { captureFetchTransaction } from './captureFetchTransaction.js'
+import { createTimeoutController } from './createTimeoutController.js'
 
 const { fetch: originalFetch } = globalThis
 
@@ -7,7 +8,15 @@ export type Fetcher<T = Response> = (input: RequestInfo | URL, init?: RequestIni
 export async function fetch(input: RequestInfo | URL, init?: RequestInit, fetchers: Fetcher[] = []): Promise<Response> {
     if (!fetchers.length) throw new Error('No fetcher found.')
 
-    const fetcher = fetchers.reduceRight<Fetcher>((ff, f) => (r, i) => f(r, i, ff), originalFetch)
+    const fetcher = fetchers.reduceRight<Fetcher>(
+        (ff, f) => (r, i) => f(r, i, ff),
+        (input: RequestInfo | URL, init?: RequestInit | undefined) => {
+            return originalFetch(input, {
+                signal: createTimeoutController(3 * 60 * 1000 /* 3 mins */).signal,
+                ...init,
+            })
+        },
+    )
 
     // capture exception if bad response or any error occurs
     let hasError = false
