5ac219bed8f3374114d9cc4abc8409bd01f66128
Jack Works <jackworks@protonmail.com>
2023-04-11 00:11:22 +0800

    chore: remove encrypted text when using image encryption (#9361)

--- packages/plugins/FileService/src/base.ts
+++ packages/plugins/FileService/src/base.ts
@@ -5,10 +5,9 @@ import { META_KEY_1, META_KEY_2 } from './constants.js'
 
 export const base: Plugin.Shared.Definition = {
     ID: PluginID.FileService,
-    name: { fallback: 'File Service', i18nKey: '__display_name' },
+    name: { fallback: 'File Service' },
     description: {
         fallback: 'Upload and share files on top of Arweave network. Store data, permanently.',
-        i18nKey: '__description',
     },
     publisher: { name: { fallback: 'Mask Network' }, link: 'https://mask.io' },
     enableRequirement: {
