975f326b8794c979823ac887c048515e450f3009
Hancheng Zhou <z308114274@gmail.com>
2023-04-10 13:20:30 +0800

    fix: close the duplicated dialog (#9346)

--- packages/mask/src/plugins/RedPacket/SNSAdaptor/openComposition.ts
+++ packages/mask/src/plugins/RedPacket/SNSAdaptor/openComposition.ts
@@ -1,6 +1,8 @@
 import { CrossIsolationMessages } from '@masknet/shared-base'
 
 export function openComposition(metadataKey: string, payload: unknown) {
+    // Close the duplicated dialog if already opened by clicking the mask compose icon.
+    CrossIsolationMessages.events.compositionDialogEvent.sendToLocal({ reason: 'popup', open: false })
     CrossIsolationMessages.events.compositionDialogEvent.sendToLocal({
         reason: 'timeline',
         open: true,
