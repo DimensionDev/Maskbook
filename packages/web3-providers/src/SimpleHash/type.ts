e6ebb4eb62a84ba478db8fa5b56c30186d7081f1
lelenei <zwpsky@gmail.com>
2023-04-10 18:45:40 +0800

    fix: type error

--- packages/web3-providers/src/SimpleHash/type.ts
+++ packages/web3-providers/src/SimpleHash/type.ts
@@ -49,8 +49,8 @@ export interface Asset {
         from_address: string
         to_address: string
         marketplace_id: string
-        total_price: BigNumber.Value
-        unit_price: BigNumber.Value
+        total_price: string
+        unit_price: string
         transaction: string // tx hash
         marketplace_name: string
         payment_token?: PaymentToken
