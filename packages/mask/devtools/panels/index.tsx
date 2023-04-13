e8979ab7cf534a6bb57f0deaf97a8af3a960d0f2
Jack Works <jackworks@protonmail.com>
2023-04-13 16:56:49 +0800

    fix: devtools not response to click

--- packages/mask/devtools/panels/index.tsx
+++ packages/mask/devtools/panels/index.tsx
@@ -1,9 +1,16 @@
-import { startReactDevTools } from './react.js'
 import { attachListener } from './utils.js'
 
 function init() {
     const controller = new AbortController()
-    startReactDevTools(controller.signal)
+
+    // this code runs in a invisible page, therefore it's requestAnimationFrame is not being called.
+    // some UI libraries (like react-window) require that to work.
+    globalThis.requestAnimationFrame = function (callback) {
+        return requestIdleCallback((x) => callback(x.timeRemaining()))
+    }
+    globalThis.cancelAnimationFrame = cancelIdleCallback
+
+    import(/* webpackMode: 'eager' */ './react.js').then((m) => m.startReactDevTools(controller.signal))
 
     function onRefresh() {
         // let's be the last task to run in the event loop. but is it ordered?
