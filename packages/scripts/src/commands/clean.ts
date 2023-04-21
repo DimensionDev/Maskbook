dbab72a9cb68e916b675f96a9c7987346def73bc
Jack Works <jackworks@protonmail.com>
2023-04-21 15:53:36 +0800

    fix: rimraf

--- packages/scripts/src/commands/clean.ts
+++ packages/scripts/src/commands/clean.ts
@@ -1,6 +1,4 @@
 import { awaitChildProcess, printShell, ROOT_PATH, shell, task } from '../utils/index.js'
-import { join } from 'path'
-import { fileURLToPath } from 'url'
 
 export async function clean() {
     const { rimraf: rm } = await import('rimraf')
@@ -10,7 +8,9 @@ export async function clean() {
         )`git clean -xdf -e node_modules -e plugins-local.json -e i18n_generated.* -e icon-generated-as-* ./packages/`,
     )
     printShell`rm -rf **/node_modules/.cache`
-    await rm(join(fileURLToPath(ROOT_PATH), '**/node_modules/.cache'))
+    await rm('**/node_modules/.cache', {
+        glob: { cwd: ROOT_PATH },
+    })
     await awaitChildProcess(shell.cwd(ROOT_PATH)`pnpm install --frozen-lockfile --prefer-offline`)
 }
 
