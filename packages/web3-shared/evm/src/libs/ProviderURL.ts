1bc4b0ff445ab369dc2d15da27a673b29cb71dc1
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-05-10 16:49:57 +0800

    feat: blockpi (#9568)
    * feat: blockpi rpc grayscale
    
    * fix: cspell
    
    * fix: type errors

--- packages/web3-shared/evm/src/libs/ProviderURL.ts
+++ packages/web3-shared/evm/src/libs/ProviderURL.ts
@@ -1,6 +1,7 @@
-import { createDeviceSeed } from '@masknet/shared-base'
+import { Flags } from '@masknet/flags'
+import { createDeviceSeed, isDeviceOnWhitelist } from '@masknet/shared-base'
 import { getRPCConstants } from '../constants/constants.js'
-import type { ChainId } from '../types/index.js'
+import { ChainId } from '../types/index.js'
 
 const DEVICE_SEED = createDeviceSeed()
 
@@ -12,16 +13,19 @@ export class ProviderURL {
     constructor() {}
 
     static from(chainId: ChainId) {
-        const { RPC_URLS, RPC_WEIGHTS } = getRPCConstants(chainId)
-        if (!RPC_URLS || !RPC_WEIGHTS) throw new Error(`No RPC presets at chainId: ${chainId}.`)
+        const { RPC_URLS, RPC_WEIGHTS, RPC_BLOCKPI_URL } = getRPCConstants(chainId)
 
+        // a grayscale user will be served the blockpi API
+        if (chainId !== ChainId.Mainnet && RPC_BLOCKPI_URL && isDeviceOnWhitelist(Flags.blockpi_grayscale))
+            return RPC_BLOCKPI_URL
+
+        if (!RPC_URLS || !RPC_WEIGHTS) throw new Error(`No RPC presets at chainId: ${chainId}.`)
         return RPC_URLS[RPC_WEIGHTS[DEVICE_SEED % RPC_URLS.length]]
     }
 
     static fromOfficial(chainId: ChainId) {
         const { RPC_URLS_OFFICIAL } = getRPCConstants(chainId)
         if (!RPC_URLS_OFFICIAL?.length) throw new Error(`No RPC presets at chainId: ${chainId}.`)
-
         return RPC_URLS_OFFICIAL[0]
     }
 }
