1914473cc0b398bca19d9ac9a545d8f9d52cc589
Jack Works <jackworks@protonmail.com>
2023-05-10 23:33:07 +0800

    fix: typo

--- packages/shared-base/src/utils/ABTest.ts
+++ packages/shared-base/src/utils/ABTest.ts
@@ -2,7 +2,7 @@ import { TelemetryID } from './TelemetryID.js'
 
 /**
  * Return a seed (0~maximum) based on the TelemetryID.
- * @param maximum The maximum seed expected (default 2 ** 23, which is 0~4294967296)
+ * @param maximum The maximum seed expected (default 2 ** 32, which is 0~4294967296)
  */
 export function getABTestSeed(maximum = 2 ** 32): number {
     const number = Number.parseInt(TelemetryID.value.slice(0, 16), 16)
