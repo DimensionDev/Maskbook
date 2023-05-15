16e4a3e9abb7f820698ed86f48fe122e26a8a42a
Jack Works <jackworks@protonmail.com>
2023-05-15 22:52:37 +0800

    fix: joinsABTest 0 result

--- packages/shared-base/src/utils/ABTest.ts
+++ packages/shared-base/src/utils/ABTest.ts
@@ -15,6 +15,8 @@ export function getABTestSeed(maximum = 2 ** 32): number {
  * @param percent 0 - 100
  */
 export function joinsABTest(percent = 50): boolean {
+    if (percent === 0) return false
+    if (percent === 100) return true
     if (percent < 0 || percent > 100) throw new RangeError()
     return getABTestSeed(100) <= percent
 }
