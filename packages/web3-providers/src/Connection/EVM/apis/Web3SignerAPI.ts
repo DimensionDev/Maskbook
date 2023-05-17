177ff11c2a496487b19a7626606a968eb1d241bc
Jack Works <jackworks@protonmail.com>
2023-05-17 21:26:05 +0800

    chore: defer @metamask/eth-sig-util

--- packages/web3-providers/src/Connection/EVM/apis/Web3SignerAPI.ts
+++ packages/web3-providers/src/Connection/EVM/apis/Web3SignerAPI.ts
@@ -1,4 +1,4 @@
-import { SignTypedDataVersion, personalSign, signTypedData } from '@metamask/eth-sig-util'
+import * as _metamask_eth_sig_util from /* webpackDefer: true */ '@metamask/eth-sig-util'
 import type { Transaction } from '@masknet/web3-shared-evm'
 import { SignType, toHex } from '@masknet/shared-base'
 import { Web3API } from './Web3API.js'
@@ -10,15 +10,15 @@ export class Web3SignerAPI implements SignerAPI.Provider {
     async sign<T>(type: SignType, key: Buffer, message: T): Promise<string> {
         switch (type) {
             case SignType.Message:
-                return personalSign({
+                return _metamask_eth_sig_util.personalSign({
                     privateKey: key,
                     data: message as string,
                 })
             case SignType.TypedData:
-                return signTypedData({
+                return _metamask_eth_sig_util.signTypedData({
                     privateKey: key,
                     data: JSON.parse(message as string),
-                    version: SignTypedDataVersion.V4,
+                    version: _metamask_eth_sig_util.SignTypedDataVersion.V4,
                 })
             case SignType.Transaction:
                 const transaction = message as Transaction
