ac386b7707d2363e0cfd08dbe77fb9b2bbcd3bf2
zhouhanseng <z308114274@gmail.com>
2023-05-19 17:55:28 +0800

    fix: avatar realm render conflict

--- packages/mask/src/components/shared/AvatarBadge/AvatarBadge.tsx
+++ packages/mask/src/components/shared/AvatarBadge/AvatarBadge.tsx
@@ -1,15 +1,17 @@
 import type { FC } from 'react'
 import type { IconButtonProps } from '@mui/material'
-import type { SocialIdentity } from '@masknet/shared-base'
+import { NetworkPluginID, type SocialAccount, type SocialIdentity } from '@masknet/shared-base'
 import { useCollectionByTwitterHandler } from '../../../plugins/Trader/trending/useTrending.js'
 import { CollectionProjectAvatarBadge } from './CollectionProjectAvatarBadge.js'
 import { ProfileAvatarBadge } from './ProfileAvatarBadge.js'
+import type { Web3Helper } from '@masknet/web3-helpers'
 
 interface Props extends IconButtonProps {
     userId: string
     identity?: SocialIdentity
+    socialAccounts?: Array<SocialAccount<Web3Helper.ChainIdAll>>
 }
-export const AvatarBadge: FC<Props> = ({ userId, identity }) => {
+export const AvatarBadge: FC<Props> = ({ userId, identity, socialAccounts }) => {
     const { value: collectionList } = useCollectionByTwitterHandler(userId)
     if (collectionList?.[0]) {
         return (
@@ -21,5 +23,7 @@ export const AvatarBadge: FC<Props> = ({ userId, identity }) => {
         )
     }
 
-    return <ProfileAvatarBadge userId={userId} />
+    return socialAccounts?.filter((x) => x.pluginID === NetworkPluginID.PLUGIN_EVM).length ? (
+        <ProfileAvatarBadge userId={userId} />
+    ) : null
 }
--- packages/mask/src/plugins/ProfileCard/SNSAdaptor/index.tsx
+++ packages/mask/src/plugins/ProfileCard/SNSAdaptor/index.tsx
@@ -1,7 +1,7 @@
 import { Trans } from 'react-i18next'
 import { Icons } from '@masknet/icons'
 import type { Plugin } from '@masknet/plugin-infra'
-import { type EnhanceableSite, NetworkPluginID } from '@masknet/shared-base'
+import { type EnhanceableSite } from '@masknet/shared-base'
 import { Box } from '@mui/material'
 import { base } from '../base.js'
 import { PLUGIN_ID, SNS_RSS3_FIELD_KEY_MAP } from '../constants.js'
@@ -41,23 +41,26 @@ const sns: Plugin.SNSAdaptor.Definition = {
         label: 'Web3 Profile Card',
         priority: 99999,
         UI: {
-            Decorator({ identity }) {
+            Decorator: function ProfileAvatarRealmDecorator({ identity, socialAccounts }) {
                 if (!identity?.identifier?.userId) return null
                 const rss3Key = SNS_RSS3_FIELD_KEY_MAP[identity.identifier.network as EnhanceableSite]
                 if (!rss3Key) return null
                 return (
                     <Box display="flex" alignItems="top" justifyContent="center">
                         <div style={{ display: 'flex', alignItems: 'top', justifyContent: 'center' }}>
-                            <AvatarBadge userId={identity.identifier.userId} identity={identity} />
+                            <AvatarBadge
+                                userId={identity.identifier.userId}
+                                identity={identity}
+                                socialAccounts={socialAccounts}
+                            />
                         </div>
                     </Box>
                 )
             },
         },
         Utils: {
-            shouldDisplay(_, socialAccounts) {
-                if (!socialAccounts?.length) return false
-                return !!socialAccounts.filter((x) => x.pluginID === NetworkPluginID.PLUGIN_EVM).length
+            shouldDisplay() {
+                return true
             },
         },
     },
