4655324294e04e751c01f260b969b8318866aed0
guanbinrui <guanbinrui@mask.io>
2023-05-19 12:24:33 +0800

    fix: ts errors

--- packages/web3-hooks/base/src/useFireflyLensAccounts.ts
+++ packages/web3-hooks/base/src/useFireflyLensAccounts.ts
@@ -1,9 +1,10 @@
-import { Firefly } from '@masknet/web3-providers'
-import { useQuery } from '@tanstack/react-query'
 import { useId } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { Firefly } from '@masknet/web3-providers'
+import type { FireflyBaseAPI } from '@masknet/web3-providers/types'
 
 export function useFireflyLensAccounts(userId?: string) {
-    return useQuery({
+    return useQuery<FireflyBaseAPI.LensAccount[]>({
         queryKey: ['firefly', 'lens', useId],
         enabled: !!useId,
         queryFn: () => Firefly.getLensByTwitterId(userId),
