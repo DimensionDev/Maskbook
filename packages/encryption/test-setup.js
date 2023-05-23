693d6ce007e35f9a74aec94866f5639bb33475eb
Jack Works <jackworks@protonmail.com>
2023-05-23 13:56:53 +0800

    chore: support node 20 as build env (#9644)

--- packages/encryption/test-setup.js
+++ packages/encryption/test-setup.js
@@ -1,8 +1,4 @@
-import { atob, btoa } from 'buffer'
 import { polyfill } from '@masknet/secp256k1-webcrypto/node'
 import '../shared-base/node_modules/tiny-secp256k1'
 
-if (!Reflect.get(globalThis, 'crypto')) {
-    polyfill()
-    Object.assign(globalThis, { atob, btoa })
-}
+polyfill()
