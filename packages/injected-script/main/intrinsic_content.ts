b8aa37869d81f33029ec4de6ab069b087fa658a2
Jack Works <jackworks@protonmail.com>
2023-05-24 23:06:04 +0800

    chore: update

--- packages/injected-script/main/intrinsic_content.ts
+++ packages/injected-script/main/intrinsic_content.ts
@@ -38,6 +38,7 @@ if (!hasOwn) {
 }
 export const StringSplit = takeThisF(globalThis.String.prototype.split)<string>
 export const StringToLowerCase = takeThisF(globalThis.String.prototype.toLowerCase)<string>
+export const StringStartsWith = takeThisF(globalThis.String.prototype.startsWith)<string>
 export const StringInclude = takeThisF(globalThis.String.prototype.includes)<string>
 export const ArrayFilter = takeThisF(globalThis.Array.prototype.filter)<readonly unknown[]>
 export const ArrayIncludes = takeThisF(globalThis.Array.prototype.includes)<readonly unknown[]>
