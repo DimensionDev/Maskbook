3e7bd8fe777531f1ca7ac4b0e03654e321f95843
zhouhanseng <z308114274@gmail.com>
2023-05-26 19:53:39 +0800

    chore: done

--- packages/mask/src/components/InjectedComponents/DecryptedPost/DecryptedPostSuccess.tsx
+++ packages/mask/src/components/InjectedComponents/DecryptedPost/DecryptedPostSuccess.tsx
@@ -5,17 +5,17 @@ import { SelectProfileDialog } from '../SelectPeopleDialog.js'
 import { makeStyles } from '@masknet/theme'
 import { Typography } from '@mui/material'
 import type { TypedMessage } from '@masknet/typed-message'
-import { EMPTY_LIST, type ProfileIdentifier } from '@masknet/shared-base'
+import { EMPTY_LIST, type ProfileIdentifier, type ProfileInformation } from '@masknet/shared-base'
 import { wrapAuthorDifferentMessage } from './authorDifferentMessage.js'
 import { DecryptedUI_PluginRendererWithSuggestion } from '../DecryptedPostMetadataRender.js'
 import { PostInfoContext, usePostInfoDetails } from '@masknet/plugin-infra/content-script'
 import { useRecipientsList } from '../../CompositionDialog/useRecipientsList.js'
 import { useSelectedRecipientsList } from '../../CompositionDialog/useSelectedRecipientsList.js'
-import { useAsyncRetry } from 'react-use'
 import Services from '../../../extension/service.js'
 import type { LazyRecipients } from '../../CompositionDialog/CompositionUI.js'
 import { delay } from '@masknet/kit'
 import { activatedSocialNetworkUI } from '../../../social-network/index.js'
+import { RecipientsToolTip } from './RecipientsToolTip.js'
 
 export interface DecryptPostSuccessProps {
     message: TypedMessage
@@ -73,25 +73,29 @@ export const DecryptPostSuccess = memo(function DecryptPostSuccess(props: Decryp
     const { t } = useI18N()
     const [showDialog, setShowDialog] = useState(false)
     const recipients = useRecipientsList()
-    const { value: alreadySelectedPreviously } = useSelectedRecipientsList()
+    const { value: alreadySelectedPreviously = EMPTY_LIST, retry } = useSelectedRecipientsList()
 
     const rightActions =
         props.author?.userId === props.whoAmI?.userId ? (
             canAppendShareTarget && props.whoAmI ? (
                 <>
-                    {!alreadySelectedPreviously?.length ? (
+                    {alreadySelectedPreviously?.length ? (
+                        <RecipientsToolTip
+                            recipients={alreadySelectedPreviously}
+                            openDialog={() => setShowDialog(true)}
+                        />
+                    ) : (
                         <section className={classes.visibilityBox} onClick={() => setShowDialog(true)}>
                             <Typography color="textPrimary" fontSize={12} fontWeight={500}>
                                 {t('decrypted_postbox_only_visible_to_yourself')}
                             </Typography>
                         </section>
-                    ) : (
-                        <section className={classes.visibilityBox} onClick={() => setShowDialog(true)}>
-                            {1234}
-                        </section>
                     )}
+
                     {showDialog ? (
                         <AppendShareDetail
+                            alreadySelectedPreviously={alreadySelectedPreviously}
+                            retry={retry}
                             whoAmI={props.whoAmI}
                             onClose={() => setShowDialog(false)}
                             recipients={recipients}
@@ -109,19 +113,22 @@ export const DecryptPostSuccess = memo(function DecryptPostSuccess(props: Decryp
     return <DecryptPostSuccessBase {...props}>{rightActions}</DecryptPostSuccessBase>
 })
 
-function AppendShareDetail(props: { onClose(): void; recipients: LazyRecipients; whoAmI: ProfileIdentifier }) {
+function AppendShareDetail(props: {
+    onClose(): void
+    recipients: LazyRecipients
+    whoAmI: ProfileIdentifier
+    alreadySelectedPreviously: ProfileInformation[]
+    retry(): void
+}) {
     const info = useContext(PostInfoContext)!
     const iv = usePostInfoDetails.postIVIdentifier()!
-    const { value: alreadySelectedPreviously = EMPTY_LIST, retry } = useAsyncRetry(
-        () => Services.Crypto.getIncompleteRecipientsOfPost(iv),
-        [iv],
-    )
+
     useEffect(props.recipients.request, [])
 
     return (
         <SelectProfileDialog
             open
-            alreadySelectedPreviously={alreadySelectedPreviously}
+            alreadySelectedPreviously={props.alreadySelectedPreviously}
             profiles={props.recipients.recipients || EMPTY_LIST}
             onClose={props.onClose}
             onSelect={async (profiles) => {
@@ -133,7 +140,7 @@ function AppendShareDetail(props: { onClose(): void; recipients: LazyRecipients;
                     activatedSocialNetworkUI.encryptionNetwork,
                 )
                 await delay(1500)
-                retry()
+                props.retry()
             }}
         />
     )
