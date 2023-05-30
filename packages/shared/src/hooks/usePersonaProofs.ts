06508421a014dec39d5d9aa28401fbcdca85697e
unclebill <billbill290@gmail.com>
2023-05-30 09:10:33 +0800

    fixup! fix: mf-4224 connect and verify persona

--- packages/shared/src/hooks/usePersonaProofs.ts
+++ packages/shared/src/hooks/usePersonaProofs.ts
@@ -2,12 +2,11 @@ import type { WebExtensionMessage } from '@dimensiondev/holoflows-kit'
 import { EMPTY_LIST, type BindingProof, type MaskEvents } from '@masknet/shared-base'
 import { NextIDProof } from '@masknet/web3-providers'
 import { useQuery } from '@tanstack/react-query'
-import { useEffect, useMemo } from 'react'
+import { useEffect } from 'react'
 
 export function usePersonaProofs(publicKey?: string, message?: WebExtensionMessage<MaskEvents>) {
-    const queryKey = useMemo(() => ['next-id', 'bindings-by-persona', publicKey], [publicKey])
     const result = useQuery<BindingProof[], Error>({
-        queryKey,
+        queryKey: ['next-id', 'bindings-by-persona', publicKey],
         enabled: !!publicKey,
         queryFn: async () => {
             const binding = await NextIDProof.queryExistedBindingByPersona(publicKey!)
