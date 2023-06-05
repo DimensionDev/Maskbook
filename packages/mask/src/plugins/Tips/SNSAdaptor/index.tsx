bcbb086f3f63aaf3e2da0c4575a1ceafed7a3e5f
unclebill <billbill290@gmail.com>
2023-06-05 18:20:50 +0800

    chore(Tips): mf-4270 remove tips entry from application settings

--- packages/mask/src/plugins/Tips/SNSAdaptor/index.tsx
+++ packages/mask/src/plugins/Tips/SNSAdaptor/index.tsx
@@ -1,10 +1,5 @@
 import { SNSAdaptorContext } from '@masknet/plugin-infra/content-script'
-import { Trans } from 'react-i18next'
-import { Icons } from '@masknet/icons'
 import type { Plugin } from '@masknet/plugin-infra'
-import { PluginID } from '@masknet/shared-base'
-import { MaskColorVar } from '@masknet/theme'
-import { Link } from '@mui/material'
 import { SNSAdaptorPluginContext } from '@masknet/web3-providers'
 import { base } from '../base.js'
 import { TipTaskManager } from '../contexts/index.js'
@@ -20,39 +15,6 @@ const sns: Plugin.SNSAdaptor.Definition = {
         const guideStorage = context.createKVStorage('persistent', guideStorageDefaultValue)
         setupStorage(storage, guideStorage)
     },
-    ApplicationEntries: [
-        (() => {
-            const name = base.name
-            const icon = <Icons.Tips size={36} />
-            const iconFilterColor = 'rgba(247, 147, 30, 0.3)'
-            return {
-                category: 'dapp',
-                description: (
-                    <Trans
-                        ns={PluginID.Tips}
-                        i18nKey="description"
-                        components={{
-                            Link: (
-                                <Link
-                                    href="https://next.id/"
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-                                    style={{ color: MaskColorVar.primary }}
-                                />
-                            ),
-                        }}
-                    />
-                ),
-                ApplicationEntryID: base.ID,
-                icon,
-                name,
-                iconFilterColor,
-                appBoardSortingDefaultPriority: 5,
-                nextIdRequired: true,
-                entryWalletConnectedNotRequired: true,
-            }
-        })(),
-    ],
     GlobalInjection() {
         return <TipTaskManager />
     },
