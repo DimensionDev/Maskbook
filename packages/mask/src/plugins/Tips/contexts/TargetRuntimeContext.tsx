ef630c1b5f140d65f3a8b9fafaf036a04eb22fa9
unclebill <billbill290@gmail.com>
2023-06-08 17:09:38 +0800

    fix(Tips): mf-4306 automaticly switch to current chain

--- packages/mask/src/plugins/Tips/contexts/TargetRuntimeContext.tsx
+++ packages/mask/src/plugins/Tips/contexts/TargetRuntimeContext.tsx
@@ -1,5 +1,11 @@
 import type { NetworkPluginID } from '@masknet/shared-base'
-import { useAccount, useDefaultChainId, useNetworkContext, Web3ContextProvider } from '@masknet/web3-hooks-base'
+import {
+    useAccount,
+    useChainContext,
+    useDefaultChainId,
+    useNetworkContext,
+    Web3ContextProvider,
+} from '@masknet/web3-hooks-base'
 import { type FC, type PropsWithChildren, useCallback, useState, useMemo } from 'react'
 import { createContainer } from 'unstated-next'
 
@@ -8,8 +14,11 @@ function useTargetRuntime(initPluginID?: NetworkPluginID) {
     const [targetPluginID, setTargetPluginID] = useState<NetworkPluginID>(pluginID)
 
     const [chainIdMap, setChainIdMap] = useState<Partial<Record<NetworkPluginID, number>>>({})
+
+    const { chainId } = useChainContext()
     const defaultChainId = useDefaultChainId(targetPluginID)
-    const targetChainId = chainIdMap[targetPluginID] ?? defaultChainId
+    const fallbackChainId = targetPluginID === pluginID ? chainId : defaultChainId
+    const targetChainId = chainIdMap[targetPluginID] ?? fallbackChainId
     const setTargetChainId = useCallback(
         (id: number) => {
             setChainIdMap((map) => ({ ...map, [targetPluginID]: id }))
