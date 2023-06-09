cefd601659433a832bc634ff67fe6ee521098106
Jack Works <jackworks@protonmail.com>
2023-06-09 09:45:27 +0800

    fix: normal payload cannot be decrypted (#9823)

--- packages/encryption/src/social-network-encode-decode/shared.ts
+++ packages/encryption/src/social-network-encode-decode/shared.ts
@@ -7,7 +7,7 @@ export function sharedEncoder(input: Uint8Array): string {
 
 /** @internal */
 export function sharedDecoder(input: string): Option<Uint8Array> {
-    const [, payload] = input.match(/\u{1F3BC}6\/8\|(\w+):\|\|/giu) || []
+    const [[, payload] = []] = input.matchAll(/\u{1F3BC}6\/8\|([\w+/=]+):\|\|/giu)
     if (!payload) return None
     try {
         return Some(new Uint8Array(decodeArrayBuffer(payload)))
