b14e0746beb15556403f1596026b948ce9c3494b
guanbinrui <guanbinrui@dimension.im>
2023-06-15 22:31:44 +0800

    refactor: telemetry settings

--- packages/web3-telemetry/src/settings.ts
+++
@@ -1,7 +0,0 @@
-import { createGlobalSettings } from '@masknet/shared-base'
-import { SentryAPI } from './providers/Sentry.js'
-
-const Sentry = new SentryAPI()
-
-export const telemetrySettings = createGlobalSettings<boolean>('log', false)
-telemetrySettings.addListener((x) => (x ? Sentry.enable() : Sentry.disable()))
