0d7e8b8ac51eea6fb8e190b377a3e4c9a51796b8
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-06-16 16:20:18 +0800

    fix: sns adaptor context (#9858)
    * chore: update lock file
    
    * refactor: code style
    
    * refactor: naming
    
    * refactor: remove bridge
    
    * refactor: remove logs
    
    * refactor: send should be readonly

--- packages/plugins/Solana/src/UI/SNSAdaptor/index.tsx
+++ packages/plugins/Solana/src/UI/SNSAdaptor/index.tsx
@@ -7,7 +7,7 @@ import type {
     Transaction,
     TransactionParameter,
 } from '@masknet/web3-shared-solana'
-import { SharedUIPluginContext, SolanaWeb3State } from '@masknet/web3-providers'
+import { SharedPluginContext, SolanaWeb3State } from '@masknet/web3-providers'
 import { base } from '../../base.js'
 
 const sns: Plugin.SNSAdaptor.Definition<
@@ -20,7 +20,7 @@ const sns: Plugin.SNSAdaptor.Definition<
 > = {
     ...base,
     async init(signal, context) {
-        SharedUIPluginContext.setup(context)
+        SharedPluginContext.setup(context)
 
         const state = await SolanaWeb3State.create(context)
 
