f91e11df066d86a762ae1a4b9da5c3a8e13b655f
Jack Works <jackworks@protonmail.com>
2023-06-19 18:16:02 +0800

    feat: enable trustedTypes in prod

--- packages/polyfills/types/dom.d.ts
+++ packages/polyfills/types/dom.d.ts
@@ -1,27 +1,31 @@
-interface Permissions {
-    request(permissionDesc: PermissionDescriptor): Promise<PermissionStatus>
-}
-
 interface ImportMeta {
     url: string
 }
 
 // #region Trusted Types
+interface Element {
+    insertAdjacentHTML(position: InsertPosition, text: string | TrustedHTML): void
+}
 interface TrustedHTML {
     toString(): string
 }
 interface TrustedScript {}
 interface TrustedScriptURL {}
+interface TrustedTypePolicyOptions {
+    createHTML?(input: string, ...args: any): string
+    createScript?(input: string, ...args: any): string
+    createScriptURL?(input: string, ...args: any): string
+}
 interface TrustedTypePolicy {
-    createHTML?(input: string, ...args: any): TrustedHTML
-    createScript?(input: string, ...args: any): TrustedScript
-    createScriptURL?(input: string, ...args: any): TrustedScript
+    createHTML(input: string, ...args: any): TrustedHTML
+    createScript(input: string, ...args: any): TrustedScript
+    createScriptURL(input: string, ...args: any): TrustedScript
 }
 interface TrustedTypePolicyFactory {
     readonly emptyHTML: TrustedHTML
     readonly emptyScript: TrustedScript
     readonly defaultPolicy: TrustedTypePolicy
-    createPolicy(policyName: string, policyOptions: TrustedTypePolicy): TrustedTypePolicy
+    createPolicy(policyName: string, policyOptions: TrustedTypePolicyOptions): TrustedTypePolicy
     isHTML(value: unknown): value is TrustedHTML
     isScript(value: unknown): value is TrustedScript
     isScriptURL(value: unknown): value is TrustedScriptURL
