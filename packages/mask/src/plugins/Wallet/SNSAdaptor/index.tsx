6d13b7f2f299dccc06ecb88d6883c8aa4af25930
lelenei <zwpsky@gmail.com>
2023-06-20 17:12:09 +0800

    refactor: transactionsnakebar

--- packages/mask/src/plugins/Wallet/SNSAdaptor/index.tsx
+++ packages/mask/src/plugins/Wallet/SNSAdaptor/index.tsx
@@ -1,21 +1,12 @@
-import { getEnumAsArray } from '@masknet/kit'
-import { NetworkPluginID } from '@masknet/shared-base'
 import type { Plugin } from '@masknet/plugin-infra'
 import { base } from '@masknet/plugin-wallet'
-import { Modals, TransactionSnackbar } from '@masknet/shared'
+import { Modals } from '@masknet/shared'
 
 const sns: Plugin.SNSAdaptor.Definition = {
     ...base,
     init(signal) {},
     GlobalInjection() {
-        return (
-            <>
-                {getEnumAsArray(NetworkPluginID).map(({ key, value: pluginID }) =>
-                    TransactionSnackbar.open({ pluginID }),
-                )}
-                <Modals />
-            </>
-        )
+        return <Modals />
     },
 }
 
