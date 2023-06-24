4657e355ad0d22c25622088159ed9155003711df
guanbinrui <guanbinrui@mask.io>
2023-06-24 23:52:48 +0800

    refactor: useDecrypt

--- packages/app/src/main/index.tsx
+++ packages/app/src/main/index.tsx
@@ -1,4 +1,5 @@
-import { lazy, Suspense, useEffect, useState } from 'react'
+import { lazy, Suspense } from 'react'
+import { useAsyncRetry } from 'react-use'
 import { DecryptError, DecryptErrorReasons } from '@masknet/encryption'
 import type { TypedMessage } from '@masknet/typed-message'
 import { RegistryContext, TypedMessageRender } from '@masknet/typed-message-react'
@@ -9,7 +10,8 @@ import { text } from './mockData.js'
 const PluginRender = lazy(() => import('./plugin-render.js'))
 
 export function DecryptUI() {
-    const [error, isE2E, message] = useDecryption()
+    const [error, isE2E, message] = useDecrypt(text)
+
     if (isE2E) {
         return <>This message is a end-to-end encrypted message. We can not decrypt it here.</>
     }
@@ -32,23 +34,20 @@ export function DecryptUI() {
     )
 }
 
-function useDecryption() {
-    const [message, setMessage] = useState<TypedMessage | null>(null)
-    const [isE2E, setIsE2E] = useState<boolean>(false)
-    const [error, setError] = useState<DecryptError | null>(null)
-    const version = '2'
-    useEffect(() => {
-        main(version, text).then((result) => {
-            if (typeof result === 'boolean')
-                return setError(new DecryptError(DecryptErrorReasons.PayloadBroken, undefined))
-            else if (result instanceof DecryptError) {
-                if (result.message === DecryptErrorReasons.PrivateKeyNotFound) setIsE2E(true)
-                else setError(result)
-            } else setMessage(result)
-        })
+function useDecrypt(text: string, version = '2') {
+    type Decrpytion = [DecryptError | null, boolean, TypedMessage | null]
+
+    const { value = [null, false, null] } = useAsyncRetry<Decrpytion>(async () => {
+        const result = await main(version, text)
+
+        if (typeof result === 'boolean')
+            return [new DecryptError(DecryptErrorReasons.PayloadBroken, undefined), false, null]
+        if (result instanceof DecryptError)
+            return [result, result.message === DecryptErrorReasons.PrivateKeyNotFound, null]
+        return [null, false, result]
     }, [version, text])
 
-    return [error, isE2E, message] as const
+    return value
 }
 
 /**
