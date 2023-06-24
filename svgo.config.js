ca61fb78243f3f4573bb82afa77639548f5ae1c1
UncleBill <billbill290@gmail.com>
2023-06-25 00:32:35 +0800

    chore: quote svg url to preview svg with Raycast (#9893)
    * chore: quote svg url to preview svg with Raycast
    
    * Update cspell.json

--- svgo.config.js
+++ svgo.config.js
@@ -5,10 +5,55 @@ export default {
         pretty: true,
     },
     plugins: [
-        'preset-default',
+        {
+            name: 'preset-default',
+            params: {
+                overrides: {
+                    // cleanupIds plugin can not recognize quoted url,
+                    // we disable it for `pnpm svgo`.
+                    cleanupIds: false,
+                },
+            },
+        },
         'prefixIds',
-        'sortAttrs',
         // Easy to preview with Finder
         'removeDimensions',
+        {
+            // Raycast can not resolve reference elements without quote,
+            // with this plugin, we add quote for reference urls.
+            name: 'quoteUrl',
+            fn: () => {
+                const nodes = []
+                const unquotedUrlRe = /^url\([^'"].*?\)/
+                const ignoreAttributes = ['fill']
+                return {
+                    element: {
+                        enter: (node) => {
+                            for (const key of Object.keys(node.attributes)) {
+                                const value = node.attributes[key]
+                                if (value?.match(unquotedUrlRe)) {
+                                    nodes.push(node)
+                                }
+                            }
+                        },
+                    },
+                    root: {
+                        exit() {
+                            nodes.forEach((node) => {
+                                for (const key of Object.keys(node.attributes)) {
+                                    if (ignoreAttributes.includes(key)) continue
+                                    const value = node.attributes[key]
+                                    if (value?.match(unquotedUrlRe)) {
+                                        node.attributes[key] = value.replace(/^url\((.*?)\)/, (_, url) => {
+                                            return `url('${url}')`
+                                        })
+                                    }
+                                }
+                            })
+                        },
+                    },
+                }
+            },
+        },
     ],
 }
