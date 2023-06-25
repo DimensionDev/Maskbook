e8e8a8519c06effd8b92e9c24fe437ce8023b671
guanbinrui <guanbinrui@dimension.im>
2023-06-25 20:50:27 +0800

    refactor: restore provider

--- packages/app/src/main/plugin-render.tsx
+++ packages/app/src/main/plugin-render.tsx
@@ -2,6 +2,7 @@ import '../plugin-host/enable.js'
 
 import { useActivatedPluginsSNSAdaptor } from '@masknet/plugin-infra/content-script'
 import { createInjectHooksRenderer } from '@masknet/plugin-infra/dom'
+import { PageUIProvider } from '@masknet/shared'
 import { MaskLightTheme } from '@masknet/theme'
 import type { TypedMessage } from '@masknet/typed-message'
 
@@ -15,6 +16,5 @@ const Decrypted = createInjectHooksRenderer(
 )
 
 export default function PluginRender(props: { message: TypedMessage }) {
-    // return PageUIProvider(useTheme, <Decrypted message={props.message} />)
-    return <Decrypted message={props.message} />
+    return PageUIProvider(useTheme, <Decrypted message={props.message} />)
 }
