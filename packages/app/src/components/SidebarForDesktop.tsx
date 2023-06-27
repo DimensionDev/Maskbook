816b0de2a0b7a56cd84f9c38bc1e5c4e7af11f0a
guanbinrui <guanbinrui@mask.io>
2023-06-27 21:07:09 +0800

    refactor: code style

--- packages/app/src/components/SidebarForDesktop.tsx
+++ packages/app/src/components/SidebarForDesktop.tsx
@@ -78,13 +78,15 @@ export function SidebarForDesktop(props: SidebarForDesktopProps) {
                                 href="#"
                                 className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800"
                                 onClick={() => {
+                                    console.log('DEBUG: open')
+
                                     ConnectWalletModal.open({
                                         pluginID: NetworkPluginID.PLUGIN_EVM,
                                     })
                                 }}>
                                 <img
                                     className="h-8 w-8 rounded-full bg-gray-800"
-                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                    src="https://github.com/DimensionDev/Mask-VI/raw/master/assets/Logo/MB--Logo--Geo--ForceCircle--Blue.svg"
                                     alt=""
                                 />
                                 <span className="sr-only">Your profile</span>
--- packages/app/src/plugin-host/rpc.ts
+++ packages/app/src/plugin-host/rpc.ts
@@ -1,15 +1,17 @@
-import { AsyncCall } from 'async-call-rpc/full'
+import { AsyncCall, type EventBasedChannel } from 'async-call-rpc/full'
 import { serializer } from '@masknet/shared-base'
 import { addListener, postMessage } from './message.js'
 
-export const PluginWorker = AsyncCall<typeof import('../plugin-worker/service.js')>(
-    {
-        on(listener) {
-            return addListener('rpc', (message) => listener(message))
-        },
-        send(data) {
-            postMessage('rpc', data)
-        },
+const channel: EventBasedChannel = {
+    on(listener) {
+        return addListener('rpc', (message) => listener(message))
     },
+    send(data) {
+        postMessage('rpc', data)
+    },
+}
+
+export const PluginWorker = AsyncCall<typeof import('../plugin-worker/service.js')>(
+    {},
     { channel, log: true, serializer },
 )
