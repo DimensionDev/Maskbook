b8738e2d5f9664471f30a98fb098a8f3ed298c5e
Jack Works <jackworks@protonmail.com>
2023-06-29 13:05:34 +0800

    chore: set limit to 500ms

--- packages/mask/src/initialization/telemetry.ts
+++ packages/mask/src/initialization/telemetry.ts
@@ -1,8 +1,14 @@
 import { TelemetryID } from '../../../shared-base/src/Telemetry/index.js'
 import Services from '../extension/service.js'
-import { timeout } from '@masknet/kit'
+import { delay } from '@masknet/kit'
 
+const timeStart = Date.now()
 const task = Services.Helper.getTelemetryID().then((id) => {
     TelemetryID.value = id
+    const timeEnd = Date.now()
+    if (timeEnd - timeStart > 500) {
+        console.warn(`Services.Helper.getTelemetryID took ${timeEnd - timeStart}ms.`)
+    }
 })
-await timeout(task, 1000, 'Services.Helper.getTelemetryID timed out').catch(console.error)
+
+await Promise.race([delay(500), task])
