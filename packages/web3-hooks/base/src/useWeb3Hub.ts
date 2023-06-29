2fb91356f4d1bbd11fb578dd3bb8d02d4ce98cd4
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-06-29 11:28:00 +0800

    fix: read others is undefined (#9954)
    * fix: web3 use
    
    * chore: update lock file
    
    * fix: deps

--- packages/web3-hooks/base/src/useWeb3Hub.ts
+++ packages/web3-hooks/base/src/useWeb3Hub.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import type { NetworkPluginID } from '@masknet/shared-base'
 import { HubAll } from '@masknet/web3-providers'
 import type { Hub, HubOptions } from '@masknet/web3-providers/types'
@@ -5,5 +6,5 @@ import { useNetworkContext } from './useContext.js'
 
 export function useWeb3Hub<T extends NetworkPluginID = NetworkPluginID>(expectedPluginID?: T, options?: HubOptions<T>) {
     const { pluginID } = useNetworkContext(expectedPluginID)
-    return HubAll.use(pluginID, options) as Hub<T>
+    return useMemo(() => HubAll.use(pluginID, options) as Hub<T>, [pluginID, JSON.stringify(options)])
 }
--- packages/web3-hooks/base/src/useWeb3Others.ts
+++ packages/web3-hooks/base/src/useWeb3Others.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import type { NetworkPluginID } from '@masknet/shared-base'
 import { OthersAll } from '@masknet/web3-providers'
 import type { Others } from '@masknet/web3-providers/types'
@@ -5,5 +6,5 @@ import { useNetworkContext } from './useContext.js'
 
 export function useWeb3Others<T extends NetworkPluginID = NetworkPluginID>(expectedPluginID?: T) {
     const { pluginID } = useNetworkContext(expectedPluginID)
-    return OthersAll.use(pluginID) as Others<T>
+    return useMemo(() => OthersAll.use(pluginID) as Others<T>, [pluginID])
 }
