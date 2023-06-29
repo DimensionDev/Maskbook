5c6e9a2e4da24223ad397e63f92e8b3b62f45cff
Jack Works <jackworks@protonmail.com>
2023-06-29 12:58:51 +0800

    chore: set getTelemetryID longer

--- packages/mask/src/initialization/telemetry.ts
+++ packages/mask/src/initialization/telemetry.ts
@@ -2,7 +2,7 @@ import { TelemetryID } from '../../../shared-base/src/Telemetry/index.js'
 import Services from '../extension/service.js'
 import { timeout } from '@masknet/kit'
 
-await timeout(
-    Services.Helper.getTelemetryID().then((id) => (TelemetryID.value = id)),
-    100,
-).catch(console.error)
+const task = Services.Helper.getTelemetryID().then((id) => {
+    TelemetryID.value = id
+})
+await timeout(task, 1000, 'Services.Helper.getTelemetryID timed out').catch(console.error)
