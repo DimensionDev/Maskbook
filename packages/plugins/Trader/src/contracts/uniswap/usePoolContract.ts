9c8e67d1f2fddb08b8c2a2a82563fcae2291684c
guanbinrui <guanbinrui@dimension.im>
2023-07-03 11:40:06 +0800

    refactor: trader plugins

--- packages/plugins/Trader/src/contracts/uniswap/usePoolContract.ts
+++ packages/plugins/Trader/src/contracts/uniswap/usePoolContract.ts
@@ -1,8 +1,8 @@
+import type { AbiItem } from 'web3-utils'
 import { useContracts } from '@masknet/web3-hooks-evm'
 import PoolStateV3ABI from '@masknet/web3-contracts/abis/PoolStateV3.json'
 import type { PoolStateV3 } from '@masknet/web3-contracts/types/PoolStateV3.js'
 import type { ChainId } from '@masknet/web3-shared-evm'
-import type { AbiItem } from 'web3-utils'
 
 export function usePoolContracts(chainId?: ChainId, listOfAddress: string[] = []) {
     return useContracts<PoolStateV3>(chainId, listOfAddress, PoolStateV3ABI as AbiItem[])
