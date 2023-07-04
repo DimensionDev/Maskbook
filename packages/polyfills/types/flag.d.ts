765e6ed1f3b2f0434de7ec6882a686bcf310755f
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-07-04 11:29:02 +0800

    refactor: remove flag (#9985)

--- packages/polyfills/types/flag.d.ts
+++ packages/polyfills/types/flag.d.ts
@@ -8,7 +8,6 @@ declare module NodeJS {
          * Debug flags
          */
         readonly BUILD_DATE: string
-        readonly APP: boolean
         readonly VERSION: string
         readonly COMMIT_HASH: string
         readonly COMMIT_DATE: string
