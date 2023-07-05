b520ce5f40ea4aa7e9001017b6a3b02b99ee9298
zhouhanseng <z308114274@gmail.com>
2023-07-05 17:02:57 +0800

    chore: reply code review

--- packages/dashboard/src/pages/CreateMaskWallet/Recovery/index.tsx
+++ packages/dashboard/src/pages/CreateMaskWallet/Recovery/index.tsx
@@ -37,10 +37,7 @@ const useStyles = makeStyles()((theme) => ({
         overflow: 'hidden',
     },
     tabList: {
-        background:
-            theme.palette.mode === 'light'
-                ? 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.9) 100%), linear-gradient(90deg, rgba(98, 152, 234, 0.2) 1.03%, rgba(98, 152, 234, 0.2) 1.04%, rgba(98, 126, 234, 0.2) 100%)'
-                : 'linear-gradient(180deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0.06) 100%)',
+        background: theme.palette.maskColor.modalTitleBg,
         padding: theme.spacing('14px', 2, 0),
     },
     tab: {
