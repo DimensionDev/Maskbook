41a233c3391c035275b2b6c9a967938a40b26f8a
guanbinrui <guanbinrui@dimension.im>
2023-07-11 15:11:00 +0800

    ci: add build-spa

--- packages/scripts/src/projects/app.ts
+++ packages/scripts/src/projects/app.ts
@@ -1,7 +1,5 @@
-import { fromNPMTask, PKG_PATH } from '../utils/index.js'
-export const [buildSPA, startSPA] = fromNPMTask(
-    //
-    new URL('app/', PKG_PATH),
-    'spa',
-    'Build SPA of Mask Network.',
-)
+import { fromNPMTask, PKG_PATH, task } from '../utils/index.js'
+
+export const [buildSPA, startSPA] = fromNPMTask(new URL('app/', PKG_PATH), 'spa', 'Build SPA of Mask Network.')
+
+task(buildSPA, 'build-spa', 'Build SPA')
