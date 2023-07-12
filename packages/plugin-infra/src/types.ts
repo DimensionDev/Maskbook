68f18e5868509cb289dddec362828682becdf699
nuanyang233 <nuanyang233@gmail.com>
2023-07-12 16:55:34 +0800

    revert: upgrade tslint (#10054)
    * revert: upgrade tslint
    
    * fix: eslint
    
    ---------
    
    Co-authored-by: guanbinrui <guanbinrui@dimension.im>

--- packages/plugin-infra/src/types.ts
+++ packages/plugin-infra/src/types.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-qualifier */
 // https://github.com/typescript-eslint/typescript-eslint/issues/7192
 import type React from 'react'
 import type { Option, Result } from 'ts-results-es'
