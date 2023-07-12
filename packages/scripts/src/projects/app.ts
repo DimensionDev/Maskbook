e2a500797bc0d6d85a248bbc7e1a94d26f04d84d
guanbinrui <guanbinrui@dimension.im>
2023-07-12 11:30:59 +0800

    ci: code gen

--- packages/scripts/src/projects/app.ts
+++ packages/scripts/src/projects/app.ts
@@ -1,5 +1,9 @@
+import { series, type TaskFunction } from 'gulp'
 import { fromNPMTask, PKG_PATH, task } from '../utils/index.js'
+import { codegen } from '../codegen/index.js'
 
-export const [buildSPA, startSPA] = fromNPMTask(new URL('app/', PKG_PATH), 'spa', 'Build SPA of Mask Network.')
+const [build] = fromNPMTask(new URL('app/', PKG_PATH), 'spa', 'Build SPA of Mask Network.')
 
-task(buildSPA, 'build-spa', 'Build SPA')
+export const buildSPA: TaskFunction = series(codegen, build)
+
+task(buildSPA, 'build-ci-spa', 'Build SPA')
