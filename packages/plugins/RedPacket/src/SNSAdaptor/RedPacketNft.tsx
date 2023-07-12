7b75be1ae83cf423a46816e94aa47f268b6bb337
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-07-12 16:40:58 +0800

    ci: add build-ci-spa (#10043)
    * ci: add build-spa
    
    * ci: change dist path
    
    * ci: trigger ci
    
    * ci: trigger ci
    
    * ci: .npmrc.pages
    
    * ci: add NPM_TOKEN
    
    * Revert "ci: .npmrc.pages"
    
    This reverts commit a1cbb381b2bb4c10c1669a1a8fc0d633436dad99.
    
    * chore: trigger CI
    
    * fix: env
    
    * ci: code gen
    
    * chore: trigger CI
    
    * ci: output dir
    
    * chore: revert output path
    
    * refactor: update match rules
    
    * refactor: detect CF env
    
    * chore: add newline
    
    * chore: use prepare
    
    * fix: lint errors
    
    * chore: revert
    
    * chore: add .npmrc.pages
    
    * chore: clean
    
    * chore: lock file
    
    * chore: update clean
    
    * chore: add newline

--- packages/plugins/RedPacket/src/SNSAdaptor/RedPacketNft.tsx
+++ packages/plugins/RedPacket/src/SNSAdaptor/RedPacketNft.tsx
@@ -13,13 +13,13 @@ import {
 import { useChainContext, useNetworkContext, useNonFungibleAsset } from '@masknet/web3-hooks-base'
 import { Web3 } from '@masknet/web3-providers'
 import { TokenType } from '@masknet/web3-shared-base'
+import { usePostLink, useSNSAdaptorContext } from '@masknet/plugin-infra/content-script'
 import { NetworkPluginID, CrossIsolationMessages, isTwitter, isFacebook } from '@masknet/shared-base'
 import { Icons } from '@masknet/icons'
 import { Stack } from '@mui/system'
 import { useI18N } from '../locales/index.js'
 import { useClaimNftRedpacketCallback } from './hooks/useClaimNftRedpacketCallback.js'
 import { useAvailabilityNftRedPacket } from './hooks/useAvailabilityNftRedPacket.js'
-import { usePostLink, useSNSAdaptorContext } from '@masknet/plugin-infra/content-script'
 
 const useStyles = makeStyles<{ claimed: boolean; outdated: boolean }>()((theme, { claimed, outdated }) => ({
     root: {
