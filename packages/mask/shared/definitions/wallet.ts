410a8d7bdb66edff0ef4f3df24a6253fa69b1aa5
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-07-12 19:09:23 +0800

    refactor: add comments (#10055)
    * refactor: add comments
    
    * refactor: update comment

--- packages/mask/shared/definitions/wallet.ts
+++ packages/mask/shared/definitions/wallet.ts
@@ -1,14 +1,17 @@
 import type { api } from '@dimensiondev/mask-wallet-core/proto'
 
 export interface WalletRecord {
+    type: 'wallet'
     id: string
     /** User define wallet name. Default address.prefix(6) */
     name: string
     /** The address of wallet */
     address: string
-    type: 'wallet'
+    /** The initial derivation path of mnemonic words  */
     derivationPath?: string
+    /** the latest local derivation path of mnemonic words */
     latestDerivationPath?: string
+    /** Mask Wallet key info */
     storedKeyInfo?: api.IStoredKeyInfo
     createdAt: Date
     updatedAt: Date
