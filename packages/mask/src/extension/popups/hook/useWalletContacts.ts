4bdb95de843cb730350708e0a35acfa14789835d
guanbinrui <guanbinrui@dimension.im>
2023-07-14 20:10:12 +0800

    fix: type errors

--- packages/mask/src/extension/popups/hook/useWalletContacts.ts
+++ packages/mask/src/extension/popups/hook/useWalletContacts.ts
@@ -1,6 +1,6 @@
 import { useSubscription } from 'use-subscription'
 import { Web3State } from '@masknet/web3-providers'
-import { EMPTY_ARRAY, EMPTY_LIST } from '@masknet/shared-base'
+import { EMPTY_ARRAY } from '@masknet/shared-base'
 
 export function useWalletContacts() {
     return useSubscription(Web3State.state.AddressBook?.addressBook ?? EMPTY_ARRAY)
