0f6151538b220ccd5d9aba4e98d67636f534b7ad
guanbinrui <guanbinrui@dimension.im>
2023-07-19 10:45:54 +0800

    refactor: code style

--- packages/mask/src/plugins/WalletService/UI/Dashboard/index.ts
+++ packages/mask/src/plugins/WalletService/UI/Dashboard/index.ts
@@ -1,50 +1,12 @@
 import type { Plugin } from '@masknet/plugin-infra'
-import { base } from '../../base.js'
 import { WalletServiceRef } from '@masknet/plugin-infra/dom'
-import { resolveMaskAccount } from '../../services/account.js'
-import {
-    changePassword,
-    createMnemonicWords,
-    createWalletFromMnemonicWords,
-    exportMnemonicWords,
-    exportPrivateKey,
-    generateAddressFromMnemonicWords,
-    getDerivableAccounts,
-    getWallets,
-    hasPassword,
-    recoverWalletFromKeyStoreJSON,
-    recoverWalletFromMnemonicWords,
-    recoverWalletFromPrivateKey,
-    resetPassword,
-    setDefaultPassword,
-    setPassword,
-    verifyPassword,
-} from '../../services/index.js'
-import { getLegacyWallets } from '../../services/legacyWallet.js'
+import { createWalletService } from '../../helpers/index.js'
+import { base } from '../../base.js'
 
 const dashboard: Plugin.Dashboard.Definition = {
     ...base,
     init(signal, context) {
-        WalletServiceRef.value = {
-            changePassword,
-            createMnemonicWords: async () => createMnemonicWords(),
-            createWalletFromMnemonicWords,
-            exportMnemonicWords,
-            exportPrivateKey,
-            generateAddressFromMnemonicWords,
-            getDerivableAccounts,
-            getLegacyWallets,
-            getWallets,
-            hasPassword,
-            recoverWalletFromKeyStoreJSON,
-            recoverWalletFromMnemonicWords,
-            recoverWalletFromPrivateKey,
-            resetPassword,
-            resolveMaskAccount,
-            setDefaultPassword,
-            setPassword,
-            verifyPassword,
-        }
+        WalletServiceRef.value = createWalletService()
     },
 }
 
--- packages/mask/src/plugins/WalletService/Worker/index.ts
+++ packages/mask/src/plugins/WalletService/Worker/index.ts
@@ -1,55 +1,17 @@
 import type { Plugin } from '@masknet/plugin-infra'
 import { MaskMessages } from '@masknet/shared-base'
 import { WalletServiceRef } from '@masknet/plugin-infra/dom'
-import '../messages.js'
-import {
-    hasPassword,
-    changePassword,
-    setPassword,
-    verifyPassword,
-    resetPassword,
-    exportMnemonicWords,
-    exportPrivateKey,
-    getDerivableAccounts,
-    getLegacyWallets,
-    getWallets,
-    isLocked,
-    recoverWalletFromMnemonicWords,
-    generateAddressFromMnemonicWords,
-    setDefaultPassword,
-    recoverWalletFromPrivateKey,
-    recoverWalletFromKeyStoreJSON,
-    createWalletFromMnemonicWords,
-    createMnemonicWords,
-    resolveMaskAccount,
-} from '../services/index.js'
+import { isLocked } from '../services/index.js'
 import { setupDatabase } from '../database/Plugin.db.js'
+import { createWalletService } from '../helpers/index.js'
+import '../messages.js'
 import { base } from '../base.js'
 
 const worker: Plugin.Worker.Definition = {
     ...base,
     init(signal, context) {
         setupDatabase(context.getDatabaseStorage())
-        WalletServiceRef.value = {
-            changePassword,
-            createMnemonicWords: async () => createMnemonicWords(),
-            createWalletFromMnemonicWords,
-            exportMnemonicWords,
-            exportPrivateKey,
-            generateAddressFromMnemonicWords,
-            getDerivableAccounts,
-            getLegacyWallets,
-            getWallets,
-            hasPassword,
-            recoverWalletFromKeyStoreJSON,
-            recoverWalletFromMnemonicWords,
-            recoverWalletFromPrivateKey,
-            resetPassword,
-            resolveMaskAccount,
-            setDefaultPassword,
-            setPassword,
-            verifyPassword,
-        }
+        WalletServiceRef.value = createWalletService()
         MaskMessages.events.wallet_is_locked.on(
             async ([type]) => {
                 if (type === 'request') {
