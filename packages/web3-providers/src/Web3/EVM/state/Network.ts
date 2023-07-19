1da78ccbe39bf89f701ed30286dc70808b1ad351
guanbinrui <guanbinrui@dimension.im>
2023-07-19 11:56:21 +0800

    fix: lint errors

--- packages/web3-providers/src/Web3/EVM/state/Network.ts
+++ packages/web3-providers/src/Web3/EVM/state/Network.ts
@@ -3,17 +3,12 @@ import { NetworkPluginID } from '@masknet/shared-base'
 import type { ChainId, NetworkType, SchemaType } from '@masknet/web3-shared-evm'
 import type { TransferableNetwork } from '@masknet/web3-shared-base'
 import { NetworkState } from '../../Base/state/Network.js'
-import { ChainDescriptorSchema } from '../schemas/ChainDescriptor.js'
 
 export class Network extends NetworkState<ChainId, SchemaType, NetworkType> {
     constructor(context: Plugin.Shared.SharedUIContext) {
         super(context, {
             getNetworkPluginID: () => NetworkPluginID.PLUGIN_EVM,
         })
-    }
-
-    private get chainDescriptorSchema() {
-        return new ChainDescriptorSchema().schema
     }
 
     protected override async validateNetwork(
