eb4ec04cc4b444130c481bab8cb3fd8c64899ffd
unclebill <billbill290@gmail.com>
2023-07-19 16:50:34 +0800

    fixup! fix: mf-4561 click on shadowdom cause ui jumping (#10114)

--- packages/mask/src/components/GuideStep/index.tsx
+++ packages/mask/src/components/GuideStep/index.tsx
@@ -139,7 +139,7 @@ export default function GuideStep({ total, step, tip, children, arrow = true, on
     useLayoutEffect(() => {
         let stopped = false
         requestAnimationFrame(function fn() {
-            if (stopped || !stepVisible) return
+            if (stopped) return
             requestAnimationFrame(fn)
             if (!childrenRef.current) return
             const cr = childrenRef.current.getBoundingClientRect()
@@ -169,7 +169,7 @@ export default function GuideStep({ total, step, tip, children, arrow = true, on
             }
         })
         return () => void (stopped = true)
-    }, [stepVisible])
+    }, [])
 
     return (
         <>
