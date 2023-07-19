513b068051d59f8615d24f8e2d6d17c42d0c3357
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-07-19 16:05:23 +0800

    refactor: createWalletService (#10124)

--- packages/mask/src/plugins/WalletService/UI/Dashboard/index.ts
+++ packages/mask/src/plugins/WalletService/UI/Dashboard/index.ts
@@ -1,31 +1,12 @@
 import type { Plugin } from '@masknet/plugin-infra'
 import { WalletServiceRef } from '@masknet/plugin-infra/dom'
 import { base } from '../../base.js'
-import { WalletRPC } from '../../messages.js'
+import { createWalletService } from '../../helpers/index.js'
 
 const dashboard: Plugin.Dashboard.Definition = {
     ...base,
     init(signal, context) {
-        WalletServiceRef.value = {
-            changePassword: WalletRPC.changePassword,
-            createMnemonicWords: WalletRPC.createMnemonicWords,
-            createWalletFromMnemonicWords: WalletRPC.createWalletFromMnemonicWords,
-            exportMnemonicWords: WalletRPC.exportMnemonicWords,
-            generateAddressFromMnemonicWords: WalletRPC.generateAddressFromMnemonicWords,
-            getDerivableAccounts: WalletRPC.getDerivableAccounts,
-            getLegacyWallets: WalletRPC.getLegacyWallets,
-            getWallets: WalletRPC.getWallets,
-            hasPassword: WalletRPC.hasPassword,
-            recoverWalletFromKeyStoreJSON: WalletRPC.recoverWalletFromKeyStoreJSON,
-            recoverWalletFromMnemonicWords: WalletRPC.recoverWalletFromMnemonicWords,
-            recoverWalletFromPrivateKey: WalletRPC.recoverWalletFromPrivateKey,
-            resetPassword: WalletRPC.resetPassword,
-            resolveMaskAccount: WalletRPC.resolveMaskAccount,
-            setDefaultPassword: WalletRPC.setDefaultPassword,
-            setPassword: WalletRPC.setPassword,
-            verifyPassword: WalletRPC.verifyPassword,
-            exportPrivateKey: WalletRPC.exportPrivateKey,
-        }
+        WalletServiceRef.value = createWalletService()
     },
 }
 
