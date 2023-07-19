f5422f6bee918e8d063d57b1d954daad7f267714
nuanyang233 <nuanyang233@gmail.com>
2023-07-19 16:53:58 +0800

    fix: dashboard wallet service ref (#10123)

--- packages/mask/src/plugins/WalletService/UI/Dashboard/index.ts
+++ packages/mask/src/plugins/WalletService/UI/Dashboard/index.ts
@@ -1,7 +1,7 @@
 import type { Plugin } from '@masknet/plugin-infra'
 import { WalletServiceRef } from '@masknet/plugin-infra/dom'
-import { createWalletService } from '../../helpers/index.js'
 import { base } from '../../base.js'
+import { createWalletService } from '../../helpers/index.js'
 
 const dashboard: Plugin.Dashboard.Definition = {
     ...base,
