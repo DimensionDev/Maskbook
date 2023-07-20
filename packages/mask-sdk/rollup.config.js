bc03974b60931312d72698ad09260ee909d4d310
Jack Works <jackworks@protonmail.com>
2023-07-20 15:40:48 +0800

    fix: rollup error

--- packages/mask-sdk/rollup.config.js
+++ packages/mask-sdk/rollup.config.js
@@ -1,5 +1,7 @@
-import { swc } from 'rollup-plugin-swc3'
 import node from '@rollup/plugin-node-resolve'
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
+const { swc } = require('rollup-plugin-swc3')
 
 export default {
     input: 'main/index.ts',
