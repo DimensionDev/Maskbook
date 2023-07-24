f426f7549905951e29bfdad8a662d4f7cc9917d0
lelenei <zwpsky@gmail.com>
2023-07-24 10:13:57 +0800

    fix: feed owner context crash

--- packages/plugins/RSS3/src/SNSAdaptor/modals/DetailsModal/DetailDialog.tsx
+++ packages/plugins/RSS3/src/SNSAdaptor/modals/DetailsModal/DetailDialog.tsx
@@ -2,11 +2,14 @@ import { Icons } from '@masknet/icons'
 import { InjectedDialog, type InjectedDialogProps, Linking } from '@masknet/shared'
 import { makeStyles } from '@masknet/theme'
 import { DialogContent, Typography } from '@mui/material'
-import type { PropsWithChildren } from 'react'
+import { useMemo, type PropsWithChildren } from 'react'
 import type { FeedCardProps } from '../../components/base.js'
 import { hostIconMap, type CardType, hostNameMap } from '../../components/share.js'
 import { useI18N } from '../../../locales/index.js'
 import { FeedCard } from '../../components/index.js'
+import { ScopedDomainsContainer, useReverseAddress } from '@masknet/web3-hooks-base'
+import { FeedOwnerContext, type FeedOwnerOptions } from '../../contexts/index.js'
+import { Others } from '@masknet/web3-providers'
 
 const useStyles = makeStyles()((theme) => ({
     detailsDialog: {
@@ -65,41 +68,58 @@ export function FeedDetailsDialog({ type, feed, onClose, actionIndex, ...rest }:
     const t = useI18N()
     const { classes } = useStyles()
     const links = feed.actions[0].related_urls
+
+    const { data: reversedName, isLoading: loadingENS } = useReverseAddress(undefined, feed.owner)
+    const { getDomain } = ScopedDomainsContainer.useContainer()
+
+    const name = feed.owner ? getDomain(feed.owner) || reversedName : reversedName
+    const feedOwner = useMemo((): FeedOwnerOptions | undefined => {
+        if (!feed.owner) return
+        return {
+            address: feed.owner,
+            name,
+            ownerDisplay: name ? Others.formatDomainName(name) : Others.formatAddress(feed.owner, 4) ?? feed.owner,
+        }
+    }, [feed.owner, name, Others.formatDomainName, Others.formatAddress])
+
+    if (!feedOwner) return null
     return (
-        <InjectedDialog
-            classes={{
-                paper: classes.detailsDialog,
-            }}
-            {...rest}
-            title={t.details()}
-            onClose={() => {
-                onClose?.()
-            }}>
-            <DialogContent className={classes.content}>
-                <div className={classes.details}>
-                    <FeedCard className={classes.card} feed={feed} actionIndex={actionIndex} verbose />
-                    {links?.length ? (
-                        <div className={classes.links}>
-                            {links.map((link, index) => {
-                                let host = ''
-                                try {
-                                    const url = new URL(link)
-                                    if (!['http:', 'https:'].includes(url.protocol)) return null
-                                    host = url.host
-                                } catch {}
-                                const Icon = hostIconMap[host] ?? Icons.SettingsLanguage
-                                const name = hostNameMap[host] ?? host
-                                return (
-                                    <Linking key={index} LinkProps={{ className: classes.link }} href={link}>
-                                        <Icon size={24} />
-                                        <Typography className={classes.linkLabel}>{name}</Typography>
-                                    </Linking>
-                                )
-                            })}
-                        </div>
-                    ) : null}
-                </div>
-            </DialogContent>
-        </InjectedDialog>
+        <FeedOwnerContext.Provider value={feedOwner}>
+            <InjectedDialog
+                classes={{
+                    paper: classes.detailsDialog,
+                }}
+                {...rest}
+                title={t.details()}
+                onClose={() => {
+                    onClose?.()
+                }}>
+                <DialogContent className={classes.content}>
+                    <div className={classes.details}>
+                        <FeedCard className={classes.card} feed={feed} actionIndex={actionIndex} verbose />
+                        {links?.length ? (
+                            <div className={classes.links}>
+                                {links.map((link, index) => {
+                                    let host = ''
+                                    try {
+                                        const url = new URL(link)
+                                        if (!['http:', 'https:'].includes(url.protocol)) return null
+                                        host = url.host
+                                    } catch {}
+                                    const Icon = hostIconMap[host] ?? Icons.SettingsLanguage
+                                    const name = hostNameMap[host] ?? host
+                                    return (
+                                        <Linking key={index} LinkProps={{ className: classes.link }} href={link}>
+                                            <Icon size={24} />
+                                            <Typography className={classes.linkLabel}>{name}</Typography>
+                                        </Linking>
+                                    )
+                                })}
+                            </div>
+                        ) : null}
+                    </div>
+                </DialogContent>
+            </InjectedDialog>
+        </FeedOwnerContext.Provider>
     )
 }
