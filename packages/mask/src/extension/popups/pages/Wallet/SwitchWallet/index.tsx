14bbdecfdbe768224ae3cddfbd8daf5a325eadad
UncleBill <billbill290@gmail.com>
2023-08-01 16:47:42 +0800

    fix: lens icon crash on conversation group (#10233)

--- packages/mask/src/extension/popups/pages/Wallet/SwitchWallet/index.tsx
+++ packages/mask/src/extension/popups/pages/Wallet/SwitchWallet/index.tsx
@@ -143,7 +143,6 @@ const SwitchWallet = memo(function SwitchWallet() {
         </Box>
     )
 
-    console.log(wallets)
     return (
         <ActionModal header={t('wallet_account')} action={action}>
             <div className={classes.content}>
