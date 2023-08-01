0be489ee3af661a1d1e28b525de8651fafaf9962
lelenei <72531217+lelenei@users.noreply.github.com>
2023-08-01 17:07:19 +0800

    fix: remove double export (#10234)
    * fix: remove double export
    
    * fix: remove unused code
    
    * refactor: code style
    
    ---------
    
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>

--- packages/app/src/pages/ComposePage.tsx
+++ packages/app/src/pages/ComposePage.tsx
@@ -1,6 +1,5 @@
 import { DisableShadowRootContext, ShadowRootIsolation } from '@masknet/theme'
 import { DashboardContainer } from '../components/DashboardContainer.js'
-import { StickySearchHeader } from '../components/StickySearchBar.js'
 import { DashboardHeader } from '../components/DashboardHeader.js'
 import { CompositionDialogUI, ShareSelectNetworkModal } from '@masknet/shared'
 import { useCallback } from 'react'
@@ -32,10 +31,8 @@ export default function ComposePage(props: ComposePageProps) {
 
     return (
         <DashboardContainer>
-            <StickySearchHeader />
-
             <main>
-                <DashboardHeader title="Swap" />
+                <DashboardHeader title="Message" />
 
                 <div className="bg-white p-5">
                     <div className="border pt-3 rounded-lg">
