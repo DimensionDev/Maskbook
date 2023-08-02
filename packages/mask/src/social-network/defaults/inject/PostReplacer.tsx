ef9367f69bb93e0646b522774440fc64305b5b4f
guanbinrui <guanbinrui@dimension.im>
2023-08-02 14:05:08 +0800

    refactor: injectPostReplacer

--- packages/mask/src/social-network/defaults/inject/PostReplacer.tsx
+++ packages/mask/src/social-network/defaults/inject/PostReplacer.tsx
@@ -1,11 +1,15 @@
 import { memo } from 'react'
-import { attachReactTreeWithContainer } from '../../../utils/shadow-root/renderInShadowRoot.js'
-import { PostInfoProvider, type PostInfo } from '@masknet/plugin-infra/content-script'
-import { PostReplacer, type PostReplacerProps } from '../../../components/InjectedComponents/PostReplacer.js'
 import type { DOMProxy } from '@dimensiondev/holoflows-kit'
-import { noop } from 'lodash-es'
+import { PostInfoProvider, type PostInfo } from '@masknet/plugin-infra/content-script'
+import { attachReactTreeWithContainer } from '../../../utils/shadow-root/renderInShadowRoot.js'
+import { PostReplacer, type PostReplacerProps } from '../../../components/InjectedComponents/PostReplacer.js'
 
-export function injectPostReplacer(config: injectPostReplacerConfig = {}) {
+interface InjectPostReplacerConfig {
+    zipPost(node: DOMProxy): void
+    unzipPost(node: DOMProxy): void
+}
+
+export function injectPostReplacer(config: InjectPostReplacerConfig) {
     const PostReplacerDefault = memo(function PostReplacerDefault(props: {
         zipPost: PostReplacerProps['zip']
         unZipPost: PostReplacerProps['unzip']
@@ -14,10 +18,9 @@ export function injectPostReplacer(config: injectPostReplacerConfig = {}) {
     })
 
     const { zipPost, unzipPost } = config
-    const zipPostF = zipPost || noop
-    const unzipPostF = unzipPost || noop
+
     return function injectPostReplacer(current: PostInfo, signal: AbortSignal) {
-        signal.addEventListener('abort', unzipPostF)
+        signal.addEventListener('abort', unzipPost as () => void)
         attachReactTreeWithContainer(current.rootElement.afterShadow, {
             key: 'post-replacer',
             untilVisible: true,
@@ -25,16 +28,11 @@ export function injectPostReplacer(config: injectPostReplacerConfig = {}) {
         }).render(
             <PostInfoProvider post={current}>
                 <PostReplacerDefault
-                    zipPost={() => zipPostF(current.rootElement)}
-                    unZipPost={() => unzipPostF(current.rootElement)}
+                    zipPost={() => zipPost(current.rootElement)}
+                    unZipPost={() => unzipPost(current.rootElement)}
                     {...current}
                 />
             </PostInfoProvider>,
         )
     }
-}
-
-interface injectPostReplacerConfig {
-    zipPost?(node: DOMProxy): void
-    unzipPost?(node: DOMProxy): void
 }
