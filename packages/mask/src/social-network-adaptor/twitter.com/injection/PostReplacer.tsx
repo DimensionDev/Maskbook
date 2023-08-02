1469d9eb0f672660877a1f871bb5e6ab01d15bd7
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-08-02 18:32:32 +0800

    refactor: do not replace post includes videos (#10251)

--- packages/mask/src/social-network-adaptor/twitter.com/injection/PostReplacer.tsx
+++ packages/mask/src/social-network-adaptor/twitter.com/injection/PostReplacer.tsx
@@ -12,6 +12,9 @@ export function injectPostReplacerAtTwitter(signal: AbortSignal, current: PostIn
     const isCollapsedPost = !!current.rootNode?.querySelector('[data-testid="tweet-text-show-more-link"]')
     if (isPromotionPost || isCollapsedPost) return
 
+    const hasVideo = !!current.rootNode?.closest('[data-testid="tweet"]')?.querySelector('video')
+    if (hasVideo) return
+
     const hasCashOrHashTag = !!current.rootNode?.querySelector(
         ['a[role="link"][href*="cashtag_click"]', 'a[role="link"][href*="hashtag_click"]'].join(','),
     )
