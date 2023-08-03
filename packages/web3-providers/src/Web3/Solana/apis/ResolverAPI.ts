e1add6dd2c06e9fc02a0f96891c7a5881859f37f
guanbinrui <guanbinrui@mask.io>
2023-08-03 17:17:25 +0800

    fix: ts errors

--- packages/web3-providers/src/Web3/Solana/apis/ResolverAPI.ts
+++ packages/web3-providers/src/Web3/Solana/apis/ResolverAPI.ts
@@ -1,10 +1,40 @@
-import { PROVIDER_DESCRIPTORS, NETWORK_DESCRIPTORS, CHAIN_DESCRIPTORS } from '@masknet/web3-shared-solana'
+import {
+    PROVIDER_DESCRIPTORS,
+    NETWORK_DESCRIPTORS,
+    CHAIN_DESCRIPTORS,
+    type ChainId,
+    type SchemaType,
+    type ProviderType,
+    type NetworkType,
+    type MessageRequest,
+    type MessageResponse,
+    type Transaction,
+    type TransactionParameter,
+} from '@masknet/web3-shared-solana'
 import { ChainResolverAPI_Base } from '../../Base/apis/ChainResolverAPI.js'
 import { ExplorerResolverAPI_Base } from '../../Base/apis/ExplorerResolverAPI.js'
 import { ProviderResolverAPI_Base } from '../../Base/apis/ProviderResolverAPI.js'
 import { NetworkResolverAPI_Base } from '../../Base/apis/NetworkExplorerAPI.js'
 
-export const SolanaChainResolver = new ChainResolverAPI_Base(CHAIN_DESCRIPTORS)
-export const SolanaExplorerResolver = new ExplorerResolverAPI_Base(CHAIN_DESCRIPTORS)
+export const SolanaChainResolver = new ChainResolverAPI_Base<
+    ChainId,
+    SchemaType,
+    ProviderType,
+    NetworkType,
+    MessageRequest,
+    MessageResponse,
+    Transaction,
+    TransactionParameter
+>(CHAIN_DESCRIPTORS)
+export const SolanaExplorerResolver = new ExplorerResolverAPI_Base<
+    ChainId,
+    SchemaType,
+    ProviderType,
+    NetworkType,
+    MessageRequest,
+    MessageResponse,
+    Transaction,
+    TransactionParameter
+>(CHAIN_DESCRIPTORS)
 export const SolanaProviderResolver = new ProviderResolverAPI_Base(PROVIDER_DESCRIPTORS)
 export const SolanaNetworkResolver = new NetworkResolverAPI_Base(NETWORK_DESCRIPTORS)
