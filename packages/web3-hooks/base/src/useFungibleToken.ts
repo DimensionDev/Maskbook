7c50158e9f77eb083cbc12b0c6bcb04166838f23
UncleBill <billbill290@gmail.com>
2023-08-03 19:25:01 +0800

    fix: mf-4655 wallet name of recovery wallet (#10269)
    * fix: mf-4655 wallet name of recovery wallet
    
    * fix: mf-4650 default name of creating wallet
    
    * fix: mf-4761 set chain context
    
    * chore: rename formatBalance2 to formatTokenBalance
    
    * fix: mf-4697 recovery persona wallets overflow

--- packages/web3-hooks/base/src/useFungibleToken.ts
+++ packages/web3-hooks/base/src/useFungibleToken.ts
@@ -3,8 +3,8 @@ import type { Web3Helper } from '@masknet/web3-helpers'
 import type { HubOptions } from '@masknet/web3-providers/types'
 import { attemptUntil } from '@masknet/web3-shared-base'
 import { useQuery } from '@tanstack/react-query'
-import { useChainId } from './useChainId.js'
 import { useWeb3Hub } from './useWeb3Hub.js'
+import { useChainContext } from './useContext.js'
 
 export function useFungibleToken<S extends 'all' | void = void, T extends NetworkPluginID = NetworkPluginID>(
     pluginID?: T,
@@ -12,7 +12,7 @@ export function useFungibleToken<S extends 'all' | void = void, T extends Networ
     fallbackToken?: Web3Helper.FungibleTokenScope<S, T>,
     options?: HubOptions<T>,
 ) {
-    const chainId = useChainId(pluginID, options?.chainId)
+    const { chainId } = useChainContext({ chainId: options?.chainId })
     const Hub = useWeb3Hub(pluginID, {
         chainId,
         ...options,
