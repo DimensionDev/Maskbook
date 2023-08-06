1d2cd84891daa6b9aeaee52ded137122a1551f7a
unclebill <billbill290@gmail.com>
2023-08-06 15:55:13 +0800

    refactor: narrow down WalletContext scope

--- packages/mask/src/extension/popups/pages/Wallet/hooks/useWalletContext.ts
+++ packages/mask/src/extension/popups/pages/Wallet/hooks/useWalletContext.ts
@@ -1,24 +1,14 @@
+import { type Wallet } from '@masknet/shared-base'
+import { useWallets } from '@masknet/web3-hooks-base'
+import { isSameAddress } from '@masknet/web3-shared-base'
 import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { createContainer } from 'unstated-next'
-import { useChainContext, useFungibleAssets, useWallets } from '@masknet/web3-hooks-base'
-import { EMPTY_LIST, NetworkPluginID, type Wallet } from '@masknet/shared-base'
-import { type FungibleAsset, isSameAddress } from '@masknet/web3-shared-base'
-import type { ChainId, SchemaType } from '@masknet/web3-shared-evm'
 
 function useWalletContext() {
     const location = useLocation()
     const wallets = useWallets()
-    const { chainId } = useChainContext<NetworkPluginID.PLUGIN_EVM>()
-    const {
-        data: assets = EMPTY_LIST,
-        isLoading,
-        refetch,
-    } = useFungibleAssets(NetworkPluginID.PLUGIN_EVM, undefined, { chainId })
-    const [currentToken, setCurrentToken] = useState<FungibleAsset<ChainId, SchemaType>>()
     const [selectedWallet, setSelectedWallet] = useState<Wallet | null>()
-
-    const [assetsIsExpand, setAssetsIsExpand] = useState(false)
 
     useEffect(() => {
         const contractAccount = new URLSearchParams(location.search).get('contractAccount')
@@ -28,11 +18,6 @@ function useWalletContext() {
     }, [location.search, wallets, selectedWallet])
 
     return {
-        currentToken,
-        setCurrentToken,
-        assets,
-        refreshAssets: refetch,
-        assetsLoading: isLoading,
         /**
          * @deprecated
          * Avoid using this, pass wallet as a router parameter instead
@@ -43,8 +28,6 @@ function useWalletContext() {
          * pass wallet as a router parameter instead
          */
         setSelectedWallet,
-        assetsIsExpand,
-        setAssetsIsExpand,
     }
 }
 
