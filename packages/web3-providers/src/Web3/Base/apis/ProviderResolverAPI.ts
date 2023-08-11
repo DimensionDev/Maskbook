0c18074120592d2bd14e1fdc755aab28b3e06ad2
guanbinrui <guanbinrui@dimension.im>
2023-08-11 13:31:40 +0800

    refactor: type

--- packages/web3-providers/src/Web3/Base/apis/ProviderResolverAPI.ts
+++ packages/web3-providers/src/Web3/Base/apis/ProviderResolverAPI.ts
@@ -4,14 +4,14 @@ export class ProviderResolverAPI_Base<ChainId, ProviderType> {
     constructor(private getDescriptors: () => Array<ProviderDescriptor<ChainId, ProviderType>>) {}
 
     private getDescriptor(providerType: ProviderType) {
-        return this.getDescriptors().find((x) => x.type === providerType)!
+        return this.getDescriptors().find((x) => x.type === providerType)
     }
 
-    providerName = (providerType: ProviderType) => this.getDescriptor(providerType).name
+    providerName = (providerType: ProviderType) => this.getDescriptor(providerType)?.name
 
-    providerHomeLink = (providerType: ProviderType) => this.getDescriptor(providerType).homeLink
+    providerHomeLink = (providerType: ProviderType) => this.getDescriptor(providerType)?.homeLink
 
-    providerShortenLink = (providerType: ProviderType) => this.getDescriptor(providerType).shortenLink
+    providerShortenLink = (providerType: ProviderType) => this.getDescriptor(providerType)?.shortenLink
 
-    providerDownloadLink = (providerType: ProviderType) => this.getDescriptor(providerType).downloadLink
+    providerDownloadLink = (providerType: ProviderType) => this.getDescriptor(providerType)?.downloadLink
 }
