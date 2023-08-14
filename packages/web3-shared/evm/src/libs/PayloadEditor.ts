602f85ddd7b97b79c149aa28f4c63a769d8da38f
guanbinrui <guanbinrui@mask.io>
2023-08-14 19:37:33 +0800

    fix: mask only methods

--- packages/web3-shared/evm/src/libs/PayloadEditor.ts
+++ packages/web3-shared/evm/src/libs/PayloadEditor.ts
@@ -16,6 +16,8 @@ import {
     EthereumMethodType,
 } from '../types/index.js'
 import { abiCoder } from '../helpers/abiCoder.js'
+import { isReadonlyMethodType } from '../helpers/isReadonlyMethodType.js'
+import { isRiskyMethodType } from '../helpers/isRiskyMethodType.js'
 
 type Options = Pick<TransactionOptions, 'account' | 'chainId'>
 
@@ -220,37 +222,11 @@ export class PayloadEditor {
     }
 
     get risky() {
-        const { method } = this.payload
-        return [
-            EthereumMethodType.ETH_SIGN,
-            EthereumMethodType.PERSONAL_SIGN,
-            EthereumMethodType.ETH_SIGN_TYPED_DATA,
-            EthereumMethodType.ETH_DECRYPT,
-            EthereumMethodType.ETH_GET_ENCRYPTION_PUBLIC_KEY,
-            EthereumMethodType.ETH_SEND_TRANSACTION,
-            EthereumMethodType.ETH_SIGN_TRANSACTION,
-            EthereumMethodType.MASK_REPLACE_TRANSACTION,
-        ].includes(method as EthereumMethodType)
+        return isRiskyMethodType(this.payload.method as EthereumMethodType)
     }
 
     get readonly() {
-        const { method } = this.payload
-        return [
-            EthereumMethodType.ETH_GET_CODE,
-            EthereumMethodType.ETH_GAS_PRICE,
-            EthereumMethodType.ETH_BLOCK_NUMBER,
-            EthereumMethodType.ETH_GET_BALANCE,
-            EthereumMethodType.ETH_GET_BLOCK_BY_NUMBER,
-            EthereumMethodType.ETH_GET_BLOCK_BY_HASH,
-            EthereumMethodType.ETH_GET_TRANSACTION_BY_HASH,
-            EthereumMethodType.ETH_GET_TRANSACTION_RECEIPT,
-            EthereumMethodType.ETH_GET_TRANSACTION_COUNT,
-            EthereumMethodType.ETH_GET_FILTER_CHANGES,
-            EthereumMethodType.ETH_NEW_PENDING_TRANSACTION_FILTER,
-            EthereumMethodType.ETH_ESTIMATE_GAS,
-            EthereumMethodType.ETH_CALL,
-            EthereumMethodType.ETH_GET_LOGS,
-        ].includes(method as EthereumMethodType)
+        return isReadonlyMethodType(this.payload.method as EthereumMethodType)
     }
 
     fill() {
