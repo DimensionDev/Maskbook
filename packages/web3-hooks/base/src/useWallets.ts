959193687b4e1aa11f844c54733be93df6c3c260
zhouhanseng <z308114274@gmail.com>
2023-08-16 17:39:50 +0800

    chore: code style

--- packages/web3-hooks/base/src/useWallets.ts
+++ packages/web3-hooks/base/src/useWallets.ts
@@ -1,13 +1,9 @@
 import { useSubscription } from 'use-subscription'
-import { EMPTY_ARRAY, type NetworkPluginID } from '@masknet/shared-base'
+import { EMPTY_ARRAY } from '@masknet/shared-base'
 import { Providers } from '@masknet/web3-providers'
-import type { Web3Helper } from '@masknet/web3-helpers'
 import { ProviderType } from '@masknet/web3-shared-evm'
 
-export function useWallets<T extends NetworkPluginID>(
-    pluginID?: T,
-    providerType?: Web3Helper.Definition[T]['ProviderType'],
-) {
+export function useWallets() {
     // We got stored Mask wallets only.
     const wallets = useSubscription(Providers[ProviderType.MaskWallet].subscription.wallets ?? EMPTY_ARRAY)
     return wallets.sort((a, b) => {
