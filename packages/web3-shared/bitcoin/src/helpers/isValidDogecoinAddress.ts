12146db0f16b4c60420c0894a2438923d7c27f8d
guanbinrui <guanbinrui@mask.io>
2023-08-17 11:12:22 +0800

    refactor: code style

--- packages/web3-shared/bitcoin/src/helpers/isValidDogecoinAddress.ts
+++ packages/web3-shared/bitcoin/src/helpers/isValidDogecoinAddress.ts
@@ -1,9 +1,6 @@
-/* cspell:disable */
-import { ProtocolType, decodeBase58Address, decodeBech32Address } from './decodeBitcoinAddress.js'
+import { ProtocolType, decodeBase58Address } from './decodeBitcoinAddress.js'
 
 export function isValidDogecoinAddress(address?: string): address is string {
     if (!address) return false
-    const prefix = address.slice(0, 2).toLowerCase()
-    if (prefix === 'bc' || prefix === 'tb') return !!decodeBech32Address(ProtocolType.Dogecoin, address)
     return !!decodeBase58Address(ProtocolType.Dogecoin, address)
 }
