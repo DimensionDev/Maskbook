5a5db877dec77f624da31cb4584f37a1def2a90a
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-08-18 17:38:57 +0800

    refactor: nonce is required (#10472)

--- packages/web3-shared/evm/src/helpers/signTransaction.ts
+++ packages/web3-shared/evm/src/helpers/signTransaction.ts
@@ -2,6 +2,7 @@ import Accounts from 'web3-eth-accounts'
 import type { Transaction } from '../types/index.js'
 
 export function signTransaction(transaction: Transaction, privateKey: string) {
+    if (typeof transaction.nonce === 'undefined') throw new Error('Nonce is required.')
     const accounts = new (Accounts as unknown as new () => Accounts.Accounts)()
     return accounts.signTransaction(transaction, privateKey)
 }
