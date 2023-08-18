6da610b95cb2eb8025d5c91c16e8f5c4c631fd7e
zhouhanseng <z308114274@gmail.com>
2023-08-18 11:16:00 +0800

    chore: add test

--- packages/web3-shared/base/tests/helpers/handleDuplicatedWalletName.ts
+++ packages/web3-shared/base/tests/helpers/handleDuplicatedWalletName.ts
@@ -8,7 +8,7 @@ describe('handleDuplicatedWalletName util test', () => {
         { wallets: [{ name: 'b' }] as Wallet[], name: 'a', expected: 'a' },
         { wallets: [{ name: 'b' }] as Wallet[], name: 'a', expected: 'a' },
         { wallets: [{ name: 'a (1)' }, { name: 'a (5)' }] as Wallet[], name: 'a', expected: 'a (6)' },
-        { wallets: [{ name: 'b' }] as Wallet[], name: 'a', expected: 'a' },
+        { wallets: [{ name: 'a (3)' }] as Wallet[], name: 'a (1)', expected: 'a (4)' },
     ])('.format($give)', ({ wallets, name, expected }) => {
         expect(handleDuplicatedWalletName(wallets, name)).toBe(expected)
     })
