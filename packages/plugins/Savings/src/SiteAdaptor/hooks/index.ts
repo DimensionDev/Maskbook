1586e91e17f908168cf3e16cf0de009a6be233ff
lelenei <zwpsky@gmail.com>
2023-08-18 22:20:35 +0800

    fix: token balance

--- packages/plugins/Savings/src/SiteAdaptor/hooks/index.ts
+++ packages/plugins/Savings/src/SiteAdaptor/hooks/index.ts
@@ -16,11 +16,11 @@ export function useApr(protocol: SavingsProtocol, enabled: boolean) {
 }
 
 export function useBalance(protocol: SavingsProtocol, enabled: boolean) {
-    const { chainId, account } = useChainContext<NetworkPluginID.PLUGIN_EVM>()
-    const web3 = useWeb3(NetworkPluginID.PLUGIN_EVM, { chainId })
+    const { account } = useChainContext<NetworkPluginID.PLUGIN_EVM>()
+    const web3 = useWeb3(NetworkPluginID.PLUGIN_EVM, { chainId: protocol.bareToken.chainId })
     return useQuery({
-        queryKey: ['savings', 'balance', chainId, protocol.bareToken.address, account],
+        queryKey: ['savings', 'balance', protocol.bareToken.chainId, protocol.bareToken.address, account],
         enabled: enabled && !!web3,
-        queryFn: () => protocol.getBalance(chainId, web3!, account),
+        queryFn: () => protocol.getBalance(protocol.bareToken.chainId, web3!, account),
     })
 }
