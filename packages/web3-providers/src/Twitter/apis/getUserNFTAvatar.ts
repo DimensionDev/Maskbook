e9e27a1816c181afce60eeb7a8f473349711ad3f
lelenei <zwpsky@gmail.com>
2023-08-18 11:40:04 +0800

    fix: migrate firefly api

--- packages/web3-providers/src/Twitter/apis/getUserNFTAvatar.ts
+++ packages/web3-providers/src/Twitter/apis/getUserNFTAvatar.ts
@@ -4,6 +4,6 @@ import type { TwitterBaseAPI } from '../../entry-types.js'
 
 export async function getUserNFTAvatar(screenName: string) {
     return fetchCachedJSON<TwitterBaseAPI.UserNFTAvatarResponse>(
-        urlcat('https://yb0w3z63oa.execute-api.us-east-1.amazonaws.com/prod/twitter-nft-avatar', { screenName }),
+        urlcat('https://a8fq5hs9nk.execute-api.us-east-1.amazonaws.com/prod/twitter', { screenName }),
     )
 }
