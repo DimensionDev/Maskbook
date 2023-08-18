7fc5b921cf749bb33948a3910db128bc3d71edd7
zhouhanseng <z308114274@gmail.com>
2023-08-18 16:19:11 +0800

    chore: reply code review

--- packages/web3-shared/base/src/helpers/generateNewWalletName.ts
+++ packages/web3-shared/base/src/helpers/generateNewWalletName.ts
@@ -3,12 +3,8 @@ import type { Wallet } from '@masknet/shared-base'
 
 export function generateNewWalletName(wallets: Wallet[], index = 0, preIndex?: number) {
     const indexList = wallets.filter((x) => !x.owner).map((x) => x.name.split('Wallet ')[1])
-
     const maxIndex = max(indexList.filter((x) => x && !Number.isNaN(x)).map(Number)) ?? 0
-
     const preIndexNotExists = !indexList.some((x) => Number(x) === preIndex)
-
     const finalIndex = preIndex && (preIndex > maxIndex || preIndexNotExists) ? preIndex : maxIndex + index + 1
-
     return `Wallet ${finalIndex}`
 }
--- packages/web3-shared/base/tests/helpers/generateNewWalletName.ts
+++ packages/web3-shared/base/tests/helpers/generateNewWalletName.ts
@@ -7,7 +7,7 @@ describe('generateNewWalletName util test', () => {
         { wallets: [{ name: 'Wallet 1' }] as Wallet[], index: 0, expected: 'Wallet 2' },
         { wallets: [{ name: 'Wallet 3' }] as Wallet[], index: 0, expected: 'Wallet 4' },
         { wallets: [{ name: 'Wallet 2' }, { name: 'Wallet 3' }] as Wallet[], index: 1, expected: 'Wallet 5' },
-        { wallets: [{ name: 'Wallet 3' }] as Wallet[], index: 0, preIndex: 1, expected: 'Wallet 4' },
+        { wallets: [{ name: 'Wallet 3' }] as Wallet[], index: 0, preIndex: 1, expected: 'Wallet 1' },
         { wallets: [{ name: 'Wallet 5' }] as Wallet[], index: 1, preIndex: 10, expected: 'Wallet 10' },
     ])('.format($wallets $index $preIndex)', ({ wallets, index, preIndex, expected }) => {
         expect(generateNewWalletName(wallets, index, preIndex)).toBe(expected)
