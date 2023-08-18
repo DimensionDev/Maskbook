e2dcde4050d3c522b84063bb80c1031b382d4d44
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-08-18 13:19:29 +0800

    refactor: code style (#10462)

--- packages/mask/shared-ui/TypedMessageRender/context.tsx
+++ packages/mask/shared-ui/TypedMessageRender/context.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react'
+import { useSubscription } from 'use-subscription'
 import { emptyTransformationContext, type TransformationContext } from '@masknet/typed-message'
 import {
     RenderFragmentsContext,
@@ -10,8 +12,6 @@ import {
     type TextResizer,
 } from '@masknet/typed-message-react'
 import { TypedMessageRenderRegistry } from './registry.js'
-import { useSubscription } from 'use-subscription'
-import { useMemo } from 'react'
 import { Container, Link } from './Components/Text.js'
 import { TypedMessageTransformers } from './transformer.js'
 
