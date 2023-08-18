9c7dbac0e5f4d382587b70027e7525fc18549a5a
zhouhanseng <z308114274@gmail.com>
2023-08-18 14:23:39 +0800

    fix: naming

--- packages/web3-shared/base/src/helpers/generateNewWalletName.ts
+++ packages/web3-shared/base/src/helpers/generateNewWalletName.ts
@@ -2,16 +2,13 @@ import { max } from 'lodash-es'
 import type { Wallet } from '@masknet/shared-base'
 
 export function generateNewWalletName(wallets: Wallet[], index = 0, preIndex?: number) {
-    const maxIndex =
-        max(
-            wallets
-                .filter((x) => !x.owner)
-                .map((x) => x.name.split('Wallet ')[1])
-                .filter((x) => x && !Number.isNaN(x))
-                .map(Number),
-        ) ?? 0
+    const indexList = wallets.filter((x) => !x.owner).map((x) => x.name.split('Wallet ')[1])
 
-    const finalIndex = preIndex && preIndex > maxIndex ? preIndex : maxIndex + index + 1
+    const maxIndex = max(indexList.filter((x) => x && !Number.isNaN(x)).map(Number)) ?? 0
+
+    const preIndexNotExists = !indexList.some((x) => Number(x) === preIndex)
+
+    const finalIndex = preIndex && (preIndex > maxIndex || preIndexNotExists) ? preIndex : maxIndex + index + 1
 
     return `Wallet ${finalIndex}`
 }
