4004ff0632595fd912a4df52bc25f1970695ddc3
Harlan Sun <1249413181@qq.com>
2023-08-20 18:09:07 +0800

    fix: self persona in search list

--- packages/mask/src/extension/popups/hook/useFriendsFromSearch.tsx
+++ packages/mask/src/extension/popups/hook/useFriendsFromSearch.tsx
@@ -5,6 +5,7 @@ import { uniqBy } from 'lodash-es'
 import type { Friend } from './useFriends.js'
 import { NextIDPlatform } from '@masknet/shared-base'
 import { PlatformSort } from './useFriends.js'
+import { useCurrentLinkedPersona } from '@masknet/shared'
 
 export type NextIDPersonaBindingsWithIdentifier = NextIDPersonaBindings & { linkedPersona: ECKeyIdentifier } & {
     isLocal?: boolean
@@ -15,35 +16,38 @@ export function useFriendsFromSearch(
     localList?: Friend[],
     searchValue?: string,
 ): AsyncStateRetry<NextIDPersonaBindingsWithIdentifier[]> {
+    const currentIdentifier = useCurrentLinkedPersona()
     return useAsyncRetry(async () => {
         if (!searchResult?.length) return EMPTY_LIST
-        const profiles: NextIDPersonaBindingsWithIdentifier[] = searchResult.map((item, index) => {
-            const filtered = item.proofs.filter(
-                (x) =>
-                    (x.platform === NextIDPlatform.ENS && x.name.endsWith('.eth')) ||
-                    (x.platform !== NextIDPlatform.Bit &&
-                        x.platform !== NextIDPlatform.CyberConnect &&
-                        x.platform !== NextIDPlatform.REDDIT &&
-                        x.platform !== NextIDPlatform.SYBIL &&
-                        x.platform !== NextIDPlatform.EthLeaderboard &&
-                        x.platform !== NextIDPlatform.NextID),
-            )
-            const identifier = ECKeyIdentifier.fromHexPublicKeyK256(item.persona).expect(
-                `${item.persona} should be a valid hex public key in k256`,
-            )
-            filtered.sort((a, b) => PlatformSort[a.platform] - PlatformSort[b.platform])
-            const searchItem = filtered.findIndex((x) => x.identity === searchValue || x.name === searchValue)
-            if (searchItem !== -1) filtered.unshift(filtered.splice(searchItem, 1)[0])
-            return {
-                proofs: uniqBy(filtered, ({ identity }) => identity),
-                linkedPersona: identifier,
-                activated_at: item.activated_at,
-                persona: item.persona,
-                isLocal: localList
-                    ? localList.some((x) => x.persona.publicKeyAsHex === identifier.publicKeyAsHex)
-                    : false,
-            }
-        })
+        const profiles: NextIDPersonaBindingsWithIdentifier[] = searchResult
+            .filter((x) => x.persona !== currentIdentifier?.identifier.publicKeyAsHex)
+            .map((item) => {
+                const filtered = item.proofs.filter(
+                    (x) =>
+                        (x.platform === NextIDPlatform.ENS && x.name.endsWith('.eth')) ||
+                        (x.platform !== NextIDPlatform.Bit &&
+                            x.platform !== NextIDPlatform.CyberConnect &&
+                            x.platform !== NextIDPlatform.REDDIT &&
+                            x.platform !== NextIDPlatform.SYBIL &&
+                            x.platform !== NextIDPlatform.EthLeaderboard &&
+                            x.platform !== NextIDPlatform.NextID),
+                )
+                const identifier = ECKeyIdentifier.fromHexPublicKeyK256(item.persona).expect(
+                    `${item.persona} should be a valid hex public key in k256`,
+                )
+                filtered.sort((a, b) => PlatformSort[a.platform] - PlatformSort[b.platform])
+                const searchItem = filtered.findIndex((x) => x.identity === searchValue || x.name === searchValue)
+                if (searchItem !== -1) filtered.unshift(filtered.splice(searchItem, 1)[0])
+                return {
+                    proofs: uniqBy(filtered, ({ identity }) => identity),
+                    linkedPersona: identifier,
+                    activated_at: item.activated_at,
+                    persona: item.persona,
+                    isLocal: localList
+                        ? localList.some((x) => x.persona.publicKeyAsHex === identifier.publicKeyAsHex)
+                        : false,
+                }
+            })
         return uniqBy(profiles, ({ linkedPersona }) => linkedPersona.publicKeyAsHex)
-    }, [searchResult, localList])
+    }, [searchResult, localList, currentIdentifier])
 }
