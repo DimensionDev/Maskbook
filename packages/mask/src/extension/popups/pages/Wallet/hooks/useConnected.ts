f435e05b426f91436e941d4e6f26c2b0a7d24e70
UncleBill <billbill290@gmail.com>
2023-08-21 12:59:11 +0800

    fix: mf-5088 get connected status (#10525)

--- packages/mask/src/extension/popups/pages/Wallet/hooks/useConnected.ts
+++ packages/mask/src/extension/popups/pages/Wallet/hooks/useConnected.ts
@@ -1,20 +1,15 @@
 import { getSiteType } from '@masknet/shared-base'
 import { useQuery } from '@tanstack/react-query'
 import Services from '../../../../service.js'
-
-/**
- * Get source page from hash
- */
-const getSourceUrl = () => {
-    const search = location.hash.slice(location.hash.indexOf('?'))
-    return new URLSearchParams(search).get('source') ?? ''
-}
+import { useSearchParams } from 'react-router-dom'
 
 export function useConnected() {
-    return useQuery(['connected-status'], async () => {
+    const [params] = useSearchParams()
+    const source = params.get('source') ?? ''
+    return useQuery(['connected-status', source], async () => {
         const result = await Services.Helper.queryCurrentActiveTab()
         if (!result) return
-        const url = result.url || getSourceUrl()
+        const url = result.url || source
         const siteType = getSiteType(url)
 
         if (!siteType) return { connected: false, url }
