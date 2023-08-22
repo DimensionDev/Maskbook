2423558f9e6f28a6ae24fcfcb5fe76ee00e65fa1
zhouhanseng <z308114274@gmail.com>
2023-08-22 18:57:48 +0800

    chore: support base lucky drop

--- packages/plugins/RedPacket/src/base.ts
+++ packages/plugins/RedPacket/src/base.ts
@@ -25,6 +25,7 @@ export const base: Plugin.Shared.Definition = {
                     ChainId.BSC,
                     ChainId.Matic,
                     ChainId.Arbitrum,
+                    ChainId.Base,
                     ChainId.xDai,
                     ChainId.Fantom,
                     ChainId.Optimism,
