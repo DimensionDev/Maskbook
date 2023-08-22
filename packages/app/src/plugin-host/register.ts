c5231cff10d20e043ff63a5083d3399c9becd51a
guanbinrui <guanbinrui@dimension.im>
2023-08-22 19:04:17 +0800

    refactor: spinner

--- packages/app/src/plugin-host/register.ts
+++ packages/app/src/plugin-host/register.ts
@@ -1,27 +1,24 @@
 import '@masknet/plugin-wallet/register'
 import '@masknet/plugin-evm/register'
+import '@masknet/plugin-flow/register'
+import '@masknet/plugin-solana/register'
+
 import '@masknet/plugin-gitcoin/register'
 import '@masknet/plugin-collectible/register'
 import '@masknet/plugin-artblocks/register'
 import '@masknet/plugin-snapshot/register'
 import '@masknet/plugin-cyberconnect/register'
 import '@masknet/plugin-vcent/register'
-
 import '@masknet/plugin-handle/register'
 import '@masknet/plugin-ito/register'
 import '@masknet/plugin-redpacket/register'
 import '@masknet/plugin-file-service/register'
-
 import '@masknet/plugin-maskbox/register'
-
-import '@masknet/plugin-flow/register'
-import '@masknet/plugin-solana/register'
 import '@masknet/plugin-rss3/register'
 import '@masknet/plugin-go-plus-security/register'
 import '@masknet/plugin-cross-chain-bridge/register'
 import '@masknet/plugin-scamsniffer/register'
 import '@masknet/plugin-web3-profile/register'
-
 import '@masknet/plugin-approval/register'
 import '@masknet/plugin-scam-warning/register'
 import '@masknet/plugin-transak/register'
@@ -29,10 +26,8 @@ import '@masknet/plugin-external/register'
 import '@masknet/plugin-nextid/register'
 import '@masknet/plugin-trader/register'
 import '@masknet/plugin-profilecard/register'
-
 import '@masknet/plugin-claim/register'
 import '@masknet/plugin-savings/register'
 import '@masknet/plugin-pets/register'
 import '@masknet/plugin-game/register'
-
 import '@masknet/plugin-smart-pay/register'
