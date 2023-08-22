db12db3011ef545b802a46393a26b2f545f7bb61
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-08-22 15:22:34 +0800

    refactor: compression (#10552)
    * refactor: compression
    
    * refactor: code style
    
    * refactor: add comments

--- packages/web3-constants/README.md
+++ packages/web3-constants/README.md
@@ -1,3 +1,60 @@
-# constants
+# @masknet/web3-constants
 
-The subrepo provide constant dataset
+## Usage
+
+The script offers two functions: `compressConstants` and `completeConstants`.
+
+### Complete Constants
+
+The `completeConstants` function ensures that all constant sets have the same fields, even if some fields are absent in certain sets. It fills in missing fields with default values based on their types.
+
+```bash
+npm run complete
+```
+
+To specify the names of constants for which fields need to be completed, open the script file and locate the following sections:
+
+```typescript
+completeConstants(join(__dirname, 'evm'), [
+  // List of names for completion
+])
+```
+
+Add the desired constant names within the array.
+
+### Compress Constants
+
+The `compressConstants` function reduces the size of constants JSON files by removing blank values or values that are repeated across constant sets.
+
+```bash
+npm run compress
+```
+
+To specify the names of constants to be compressed, open the script file and locate the following sections:
+
+```typescript
+compressConstants(join(__dirname, 'evm'), [
+  // List of names for compression
+])
+```
+
+Add the desired constant names within the array.
+
+### Running the Script
+
+To run the script, open your terminal and navigate to the project directory. Use the following command format:
+
+```bash
+node constants.js [options]
+```
+
+Available Options
+
+- `--compress`: Run the `compressConstants` function.
+- `--complete`: Run the `completeConstants` function.
+
+You can combine multiple options. For example, to compress and complete constants, use:
+
+```bash
+node script.js --compress --complete
+```
---
+++ packages/web3-constants/constants.ts
@@ -0,0 +1,149 @@
+import { promises as fs } from 'fs'
+import { resolve, join } from 'path'
+import { run } from '@masknet/web3-contracts/utils'
+
+type Primitive = string | number | boolean
+
+interface ConstantsConfig {
+    [key: string]: Record<string, Primitive | Primitive[]>
+}
+
+// Function to read the constants from a file
+async function readConstantsFile(filePath: string): Promise<ConstantsConfig> {
+    const data = await fs.readFile(filePath, 'utf-8')
+    return JSON.parse(data)
+}
+
+// Function to write constants to a file
+async function writeConstantsFile(filePath: string, constants: ConstantsConfig): Promise<void> {
+    await fs.writeFile(filePath, JSON.stringify(constants, null, 4), 'utf-8')
+}
+
+// Function to get the default value for a given value type
+function getDefaultPrimitiveValue(value: Primitive | Primitive[]): Primitive | Primitive[] {
+    if (typeof value === 'string') {
+        return ''
+    } else if (typeof value === 'number') {
+        return 0
+    } else if (typeof value === 'boolean') {
+        return false
+    } else if (Array.isArray(value)) {
+        return []
+    }
+    return ''
+}
+
+// Function to filter out non-blank pairs from the constants
+function filterNonBlankPairs(
+    values: Record<string, Primitive | Primitive[]>,
+    names: string[],
+): Array<[string, Primitive | Primitive[]]> {
+    return Object.entries(values).filter(([key, value]) => value !== '' || !names.includes(key) || key === 'Mainnet')
+}
+
+// Function to process constants based on the provided action
+async function processConstants(
+    folderPath: string,
+    keys: string[],
+    action: (constants: ConstantsConfig, names: string[]) => ConstantsConfig,
+): Promise<void> {
+    for (const file of await fs.readdir(folderPath)) {
+        const filePath = resolve(folderPath, file)
+        const constants = await readConstantsFile(filePath)
+        const updatedConstants = action(constants, keys)
+        await writeConstantsFile(filePath, updatedConstants)
+    }
+
+    // Run the prettier tool after processing
+    run(undefined, 'npx', 'prettier', '--write', folderPath)
+}
+
+// Action to compress constants
+function compressAction(constants: ConstantsConfig, names: string[]): ConstantsConfig {
+    const updatedConstants: ConstantsConfig = {}
+    for (const name of Object.keys(constants)) {
+        const values = constants[name]
+        const nonBlankPairs = filterNonBlankPairs(values, names)
+        updatedConstants[name] = Object.fromEntries(nonBlankPairs)
+    }
+    return updatedConstants
+}
+
+// Action to complete constants
+function completeAction(constants: ConstantsConfig, names: string[]): ConstantsConfig {
+    const updatedConstants: ConstantsConfig = {}
+    for (const name of Object.keys(constants)) {
+        const values = constants[name]
+        const updatedValues: Record<string, Primitive | Primitive[]> = {}
+
+        for (const key of names) {
+            if (key in values) {
+                updatedValues[key] = values[key]
+            } else {
+                updatedValues[key] = getDefaultPrimitiveValue(values[Object.keys(values)[0]])
+            }
+        }
+
+        for (const key of Object.keys(values)) {
+            if (!names.includes(key)) {
+                updatedValues[key] = values[key]
+            }
+        }
+
+        updatedConstants[name] = updatedValues
+    }
+    return updatedConstants
+}
+
+const EVM_KEYS = [
+    'Mainnet',
+    'Ropsten',
+    'Rinkeby',
+    'Kovan',
+    'Gorli',
+    'BSC',
+    'BSCT',
+    'Base',
+    'Base_Goerli',
+    'Matic',
+    'Mumbai',
+    'Arbitrum',
+    'Arbitrum_Rinkeby',
+    'xDai',
+    'Optimism',
+    'Optimism_Kovan',
+    'Optimism_Goerli',
+    'Avalanche',
+    'Avalanche_Fuji',
+    'Celo',
+    'Fantom',
+    'Aurora',
+    'Aurora_Testnet',
+    'Conflux',
+    'Astar',
+]
+
+const SOLANA_KEYS = ['Mainnet', 'Testnet', 'Devnet']
+
+const FLOW_KEYS = ['Mainnet', 'Testnet']
+
+// Main function to parse command line arguments and perform actions
+async function main() {
+    const args = process.argv.slice(2)
+
+    if (args.includes('--compress')) {
+        await processConstants(join(__dirname, 'evm'), EVM_KEYS, compressAction)
+        await processConstants(join(__dirname, 'solana'), SOLANA_KEYS, compressAction)
+        await processConstants(join(__dirname, 'flow'), FLOW_KEYS, compressAction)
+    }
+
+    if (args.includes('--complete')) {
+        await processConstants(join(__dirname, 'evm'), EVM_KEYS, completeAction)
+        await processConstants(join(__dirname, 'solana'), SOLANA_KEYS, completeAction)
+        await processConstants(join(__dirname, 'flow'), FLOW_KEYS, completeAction)
+    }
+}
+
+if (process.env.NODE_ENV !== 'test') {
+    main()
+}
