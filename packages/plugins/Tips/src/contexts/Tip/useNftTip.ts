c81e1b700a5a4fe6a3b9a7fb028b1f84dc56cd79
nuanyang233 <nuanyang233@gmail.com>
2023-08-23 18:18:55 +0800

    chore: remove useless notification

--- packages/plugins/Tips/src/contexts/Tip/useNftTip.ts
+++ packages/plugins/Tips/src/contexts/Tip/useNftTip.ts
@@ -3,7 +3,6 @@ import { useChainContext, useWeb3Connection, useWeb3State } from '@masknet/web3-
 import type { ConnectionOptions } from '@masknet/web3-providers/types'
 import { NetworkPluginID } from '@masknet/shared-base'
 import type { TipTuple } from './type.js'
-import { useCustomSnackbar } from '@masknet/theme'
 import { useI18N } from '../../locales/i18n_generated.js'
 
 export function useNftTip<T extends NetworkPluginID>(
@@ -21,24 +20,19 @@ export function useNftTip<T extends NetworkPluginID>(
         ...options,
         overrides: {},
     })
-    const { showSnackbar } = useCustomSnackbar()
+
     const [{ loading: isTransferring }, sendTip] = useAsyncFn(async () => {
-        try {
-            if (!contractAddress) return
-            if (pluginID === NetworkPluginID.PLUGIN_EVM && !tokenId) return
-            const txid = await Web3.transferNonFungibleToken(contractAddress, tokenId ?? '', recipient, '1')
-            const tokenDetailed = await Web3.getNonFungibleToken(contractAddress, tokenId ?? '', undefined, {
-                chainId,
-                account,
-            })
-            if (tokenDetailed) {
-                await Token?.removeToken?.(account, tokenDetailed)
-            }
-            return txid
-        } catch {
-            showSnackbar(t.failed_to_transfer_nft(), { variant: 'error' })
-            return
+        if (!contractAddress) return
+        if (pluginID === NetworkPluginID.PLUGIN_EVM && !tokenId) return
+        const txid = await Web3.transferNonFungibleToken(contractAddress, tokenId ?? '', recipient, '1')
+        const tokenDetailed = await Web3.getNonFungibleToken(contractAddress, tokenId ?? '', undefined, {
+            chainId,
+            account,
+        })
+        if (tokenDetailed) {
+            await Token?.removeToken?.(account, tokenDetailed)
         }
+        return txid
     }, [account, tokenId, pluginID, contractAddress, recipient, Web3])
 
     return [isTransferring, sendTip]
