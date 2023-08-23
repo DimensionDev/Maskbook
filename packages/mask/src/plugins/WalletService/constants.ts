099f0bb36bd8d588a9028b1a86ccdf00a8586e9c
zhouhanseng <z308114274@gmail.com>
2023-08-23 12:21:55 +0800

    revert: plugin id

--- packages/mask/src/plugins/WalletService/constants.ts
+++ packages/mask/src/plugins/WalletService/constants.ts
@@ -1,7 +1,7 @@
 import { PluginID } from '@masknet/shared-base'
 
 // #region plugin definitions
-export const PLUGIN_ID = PluginID.WalletService
+export const PLUGIN_ID = PluginID.Wallet
 export const PLUGIN_NAME = 'Wallet Service'
 export const PLUGIN_DESCRIPTION = 'Mask Wallet Service'
 // #endregion
