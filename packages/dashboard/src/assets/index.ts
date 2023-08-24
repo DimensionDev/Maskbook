05c22e43dbfb8f5014fb6489178faa4fe2a8261f
guanbinrui <guanbinrui@dimension.im>
2023-08-24 16:33:05 +0800

    chore: comment

--- packages/dashboard/src/assets/index.ts
+++ packages/dashboard/src/assets/index.ts
@@ -4,6 +4,7 @@
 export const ABOUT_DIALOG_BACKGROUND = new URL('./images/AboutDialogBackground.png', import.meta.url)
 export const SetupTutorialURL = new URL('./images/SetupTutorial.svg', import.meta.url)
 export const WatermarkURL = new URL('./images/MaskWatermark.png', import.meta.url)
+// A workaround for Opera file name validation
 export const Welcome = new URL('./Welcome.splinecode.png', import.meta.url)
 export const PrintBackground = new URL('./images/PrintBackground.png', import.meta.url)
 export const Trend = new URL('./images/Trend.png', import.meta.url)
