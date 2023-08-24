4e1683294d376435460558dcaabec586c355459b
guanbinrui <guanbinrui@dimension.im>
2023-08-24 16:28:49 +0800

    refactor: splinecode

--- packages/dashboard/src/assets/index.ts
+++ packages/dashboard/src/assets/index.ts
@@ -4,7 +4,7 @@
 export const ABOUT_DIALOG_BACKGROUND = new URL('./images/AboutDialogBackground.png', import.meta.url)
 export const SetupTutorialURL = new URL('./images/SetupTutorial.svg', import.meta.url)
 export const WatermarkURL = new URL('./images/MaskWatermark.png', import.meta.url)
-export const Welcome = new URL('./Welcome.splinecode', import.meta.url)
+export const Welcome = new URL('./Welcome.splinecode.png', import.meta.url)
 export const PrintBackground = new URL('./images/PrintBackground.png', import.meta.url)
 export const Trend = new URL('./images/Trend.png', import.meta.url)
 export const MaskWallet = new URL('./images/MaskWallet.png', import.meta.url)
