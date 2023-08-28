47d1b87b0b673d014a4fcf30030ff07a1ef4cc3f
UncleBill <billbill290@gmail.com>
2023-08-28 15:23:33 +0800

    fix: mf-5161 style of popup navigation (#10637)

--- packages/mask/src/extension/popups/components/Navigator/index.tsx
+++ packages/mask/src/extension/popups/components/Navigator/index.tsx
@@ -3,7 +3,7 @@
 import urlcat from 'urlcat'
 import { createContext, memo, use, useCallback, useContext, useMemo, useRef } from 'react'
 import { NavLink, type LinkProps } from 'react-router-dom'
-import { BottomNavigation, BottomNavigationAction, Box, type BoxProps } from '@mui/material'
+import { BottomNavigationAction, Box, type BoxProps } from '@mui/material'
 import { Icons } from '@masknet/icons'
 import { makeStyles } from '@masknet/theme'
 import { DashboardRoutes, PopupRoutes } from '@masknet/shared-base'
@@ -15,28 +15,27 @@ import Services from '../../../service.js'
 import { useCurrentPersona } from '../../../../components/DataSource/useCurrentPersona.js'
 
 const useStyle = makeStyles()((theme) => ({
-    navigation: {
-        height: 72,
-        padding: 0,
+    container: {
+        padding: theme.spacing(2, 1.5),
+        backgroundColor: theme.palette.maskColor.bottom,
+        width: '100%',
+        backdropFilter: 'blur(8px)',
         background: theme.palette.maskColor.secondaryBottom,
         boxShadow: theme.palette.maskColor.bottomBg,
-        backdropFilter: 'blur(8px)',
+        borderRadius: '8px',
     },
-    iconOnly: {
+    action: {
         color: theme.palette.maskColor.third,
         height: '100%',
         minWidth: 94,
+        padding: 6,
+        boxSizing: 'border-box',
     },
     selected: {
         '& > button': {
             color: theme.palette.maskColor.highlight,
             filter: 'drop-shadow(0px 4px 10px rgba(0, 60, 216, 0.2))',
         },
-    },
-    container: {
-        backgroundColor: theme.palette.maskColor.bottom,
-        width: '100%',
-        backdropFilter: 'blur(8px)',
     },
 }))
 
@@ -67,42 +66,43 @@ export const Navigator = memo(function Navigator({ className, ...rest }: BoxProp
             window.close()
         }
         await Services.Helper.removePopupWindow()
-    }, [currentPersona])
+    }, [!currentPersona])
 
     useContext(HydrateFinished)()
 
     return (
         <Box className={cx(classes.container, className)} {...rest}>
-            <BottomNavigation classes={{ root: classes.navigation }}>
-                <BottomNavLink to={PopupRoutes.Personas}>
-                    <BottomNavigationAction
-                        showLabel={false}
-                        icon={<Icons.Me size={28} />}
-                        className={classes.iconOnly}
-                    />
-                </BottomNavLink>
-                <BottomNavLink to={walletLink}>
-                    <BottomNavigationAction
-                        showLabel={false}
-                        icon={<Icons.WalletNav size={28} />}
-                        className={classes.iconOnly}
-                    />
-                </BottomNavLink>
-                <BottomNavLink to={PopupRoutes.Friends}>
-                    <BottomNavigationAction
-                        showLabel={false}
-                        icon={<Icons.Contacts size={28} />}
-                        className={classes.iconOnly}
-                    />
-                </BottomNavLink>
-
+            <BottomNavLink to={PopupRoutes.Personas}>
                 <BottomNavigationAction
-                    onClick={onOpenDashboardSettings}
+                    tabIndex={-1}
                     showLabel={false}
-                    icon={<Icons.Settings2 size={28} />}
-                    className={classes.iconOnly}
+                    icon={<Icons.Me size={28} />}
+                    className={classes.action}
                 />
-            </BottomNavigation>
+            </BottomNavLink>
+            <BottomNavLink to={walletLink}>
+                <BottomNavigationAction
+                    tabIndex={-1}
+                    showLabel={false}
+                    icon={<Icons.WalletNav size={28} />}
+                    className={classes.action}
+                />
+            </BottomNavLink>
+            <BottomNavLink to={PopupRoutes.Friends}>
+                <BottomNavigationAction
+                    tabIndex={-1}
+                    showLabel={false}
+                    icon={<Icons.Contacts size={28} />}
+                    className={classes.action}
+                />
+            </BottomNavLink>
+
+            <BottomNavigationAction
+                onClick={onOpenDashboardSettings}
+                showLabel={false}
+                icon={<Icons.Settings2 size={28} />}
+                className={classes.action}
+            />
         </Box>
     )
 })
