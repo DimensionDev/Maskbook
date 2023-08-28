ef4cb1a731d72c8e11651a8cc377e3013f335560
Jack Works <jackworks@protonmail.com>
2023-08-28 20:42:44 +0800

    chore: let shadowRootInit be open when there is no browser API

--- packages/flags/src/flags/index.ts
+++ packages/flags/src/flags/index.ts
@@ -1,17 +1,24 @@
 import { env } from './buildInfo.js'
+import { Environment, isEnvironment } from '@dimensiondev/holoflows-kit'
 
-const testOnly = process.env.NODE_ENV === 'test'
-const devOnly = process.env.NODE_ENV === 'development'
-const prodOnly = process.env.NODE_ENV === 'production'
-const insiderOnly = env.channel === 'insider' || devOnly
-const betaOrInsiderOnly = insiderOnly || env.channel === 'beta'
+const isTest = process.env.NODE_ENV === 'test'
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = process.env.NODE_ENV === 'production'
+const isInsider = env.channel === 'insider' || isDev
+const isBeta = isInsider || env.channel === 'beta'
 
 export const flags = {
-    mask_SDK_ready: betaOrInsiderOnly,
-    support_testnet_switch: betaOrInsiderOnly,
+    mask_SDK_ready: isBeta,
+    support_testnet_switch: isBeta,
 
     shadowRootInit: {
-        mode: '__REACT_DEVTOOLS_GLOBAL_HOOK__' in globalThis || betaOrInsiderOnly || testOnly ? 'open' : 'closed',
+        mode:
+            '__REACT_DEVTOOLS_GLOBAL_HOOK__' in globalThis ||
+            isBeta ||
+            isTest ||
+            !isEnvironment(Environment.HasBrowserAPI)
+                ? 'open'
+                : 'closed',
         delegatesFocus: true,
     } as const satisfies ShadowRootInit,
 
@@ -20,32 +27,32 @@ export const flags = {
     sandboxedPluginRuntime: false,
 
     // sentry
-    sentry_earliest_version: env.VERSION || env.VERSION,
+    sentry_earliest_version: env.VERSION,
     sentry_sample_rate: 0.05,
-    sentry_enabled: prodOnly,
-    sentry_event_enabled: prodOnly,
-    sentry_exception_enabled: prodOnly,
-    sentry_fetch_transaction_enabled: prodOnly,
-    sentry_async_transaction_enabled: devOnly,
+    sentry_enabled: isProd,
+    sentry_event_enabled: isProd,
+    sentry_exception_enabled: isProd,
+    sentry_fetch_transaction_enabled: isProd,
+    sentry_async_transaction_enabled: isDev,
 
     // mixpanel
-    mixpanel_earliest_version: env.VERSION || env.VERSION,
+    mixpanel_earliest_version: env.VERSION,
     mixpanel_sample_rate: 1,
-    mixpanel_enabled: prodOnly,
-    mixpanel_event_enabled: prodOnly,
-    mixpanel_exception_enabled: prodOnly,
+    mixpanel_enabled: isProd,
+    mixpanel_event_enabled: isProd,
+    mixpanel_exception_enabled: isProd,
     mixpanel_project_token: 'b815b822fd131650e92ff8539eb5e793',
 
     // wallet connect
     wc_v1_bridge_url: 'https://bridge.walletconnect.org',
     wc_v2_relay_url: 'wss://relay.walletconnect.com',
     wc_v2_project_id: '8f1769933420afe8873860925fcca14f',
-    wc_v2_mode: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
+    wc_v2_mode: isProd ? 'error' : 'debug',
     wc_v1_enabled: true,
     wc_v2_enabled: false,
 } as const
 
 Object.freeze(flags.shadowRootInit)
 if (process.env.NODE_ENV === 'development') {
-    console.log('Mask Network starts with flags:', flags)
+    console.log('[mask] flags:', flags)
 }
