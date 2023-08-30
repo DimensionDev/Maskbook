649ff37ec014855cf214f08b0070e1d71c259f17
UncleBill <billbill290@gmail.com>
2023-08-30 12:36:25 +0800

    perf: lazy render drawer content (#10665)

--- packages/mask/src/extension/popups/hooks/useGasRatio.ts
+++ packages/mask/src/extension/popups/hooks/useGasRatio.ts
@@ -1,19 +1,18 @@
-import { useAsync } from 'react-use'
-import { useContainer } from 'unstated-next'
 import { DepositPaymaster } from '@masknet/web3-providers'
 import { isNativeTokenAddress } from '@masknet/web3-shared-evm'
+import { useQuery } from '@tanstack/react-query'
+import { useContainer } from 'unstated-next'
 import { PopupContext } from './usePopupContext.js'
 
 export function useGasRatio(paymentToken?: string) {
     const { smartPayChainId } = useContainer(PopupContext)
 
-    const { value: smartPayRatio } = useAsync(async () => {
+    const { data: smartPayRatio } = useQuery(['smart-pay', 'gas-ratio', smartPayChainId], async () => {
         if (!smartPayChainId) return
         const depositPaymaster = new DepositPaymaster(smartPayChainId)
         const ratio = await depositPaymaster.getRatio()
-
         return ratio
-    }, [smartPayChainId])
+    })
 
     return paymentToken && !isNativeTokenAddress(paymentToken) ? smartPayRatio : undefined
 }
