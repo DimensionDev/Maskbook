32928dbeffb4832e2ff312b7c61c49167f875c00
Jack Works <jackworks@protonmail.com>
2023-09-01 14:54:49 +0800

    refactor: remove unused file

--- packages/mask/dashboard/API.tsx
+++
@@ -1,9 +0,0 @@
-import type { MaskEvents } from '@masknet/shared-base'
-import type { WebExtensionMessage } from '@dimensiondev/holoflows-kit'
-
-export let MaskMessages: WebExtensionMessage<MaskEvents> = null!
-
-export function setMessages(MaskMessage: any) {
-    MaskMessages = MaskMessage
-    Object.assign(globalThis, { Messages: MaskMessage })
-}
