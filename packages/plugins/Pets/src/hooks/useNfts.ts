f319f10619a8450851125a115a0af6214f21ffeb
UncleBill <billbill290@gmail.com>
2023-09-05 00:17:25 +0800

    fix: mf-5185 do not list lens in pet dialog (#10701)

--- packages/plugins/Pets/src/hooks/useNfts.ts
+++ packages/plugins/Pets/src/hooks/useNfts.ts
@@ -1,38 +1,58 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import { EMPTY_LIST, NetworkPluginID } from '@masknet/shared-base'
 import { resolveIPFS_URL } from '@masknet/web3-shared-base'
 import type { NonFungibleContract, OwnerERC721TokenInfo } from '../types.js'
 import { useChainContext, useNonFungibleAssets } from '@masknet/web3-hooks-base'
+import { isLens, isLensCollect, isLensComment, isLensFollower, isLensPost } from '@masknet/web3-shared-evm'
 
 export function useNFTs() {
     const { chainId } = useChainContext<NetworkPluginID.PLUGIN_EVM>()
-    const { value: assets = EMPTY_LIST, loading: state } = useNonFungibleAssets(NetworkPluginID.PLUGIN_EVM, undefined, {
+    const {
+        value: assets = EMPTY_LIST,
+        loading: isLoading,
+        done,
+        next,
+        dataUpdatedAt,
+    } = useNonFungibleAssets(NetworkPluginID.PLUGIN_EVM, undefined, {
         chainId,
     })
+    useEffect(() => {
+        if (done) return
+        next()
+    }, [isLoading, done, next, dataUpdatedAt])
+
     const nfts = useMemo(() => {
         const map: Record<string, NonFungibleContract> = {}
-        if (assets?.length) {
-            for (const NFT of assets) {
-                const glbSupport = NFT.metadata?.imageURL?.endsWith('.glb') ?? false
-                if (NFT.metadata?.imageURL) {
-                    NFT.metadata.imageURL = resolveIPFS_URL(NFT.metadata.imageURL)
-                }
-                const tokens: Record<string, OwnerERC721TokenInfo> = {
-                    ...map[NFT.address]?.tokens,
-                    [NFT.tokenId]: { ...NFT, tokenId: NFT.tokenId, glbSupport },
-                }
-                map[NFT.address] = {
-                    name: (NFT.collection?.name || NFT.contract?.name) ?? '',
-                    contract: NFT.address,
-                    icon: (NFT.collection?.iconURL || NFT.metadata?.imageURL) ?? '',
-                    tokens,
-                    chainId: NFT.chainId,
-                }
+        if (!assets.length) {
+            return EMPTY_LIST
+        }
+        const normalAssets = assets.filter((x) => {
+            const name = x.collection?.name || x.contract?.name || x.metadata?.name
+            if (!name) return true
+            if (isLensFollower(name) || isLensComment(name) || isLensPost(name) || isLensCollect(name) || isLens(name))
+                return false
+            return true
+        })
+        for (const asset of normalAssets) {
+            const glbSupport = asset.metadata?.imageURL?.endsWith('.glb') ?? false
+            if (asset.metadata?.imageURL) {
+                asset.metadata.imageURL = resolveIPFS_URL(asset.metadata.imageURL)
+            }
+            const tokens: Record<string, OwnerERC721TokenInfo> = {
+                ...map[asset.address]?.tokens,
+                [asset.tokenId]: { ...asset, tokenId: asset.tokenId, glbSupport },
+            }
+            map[asset.address] = {
+                name: (asset.collection?.name || asset.contract?.name) ?? '',
+                contract: asset.address,
+                icon: (asset.collection?.iconURL || asset.metadata?.imageURL) ?? '',
+                tokens,
+                chainId: asset.chainId,
             }
         }
         const nfts = Object.values(map).map((v) => ({ ...v, tokens: Object.values(v.tokens) }))
         return nfts
     }, [assets])
 
-    return { nfts, state }
+    return { nfts, isLoading }
 }
