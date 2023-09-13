e036335b853590e31a74553db0c0db4a4337f60a
guanbinrui <guanbinrui@users.noreply.github.com>
2023-09-13 05:15:14 +0000

    fix: markdownlint

--- packages/plugins/CyberConnect/src/base.tsx
+++ packages/plugins/CyberConnect/src/base.tsx
@@ -1,5 +1,5 @@
 import type { Plugin } from '@masknet/plugin-infra'
-import { DEFAULT_PLUGIN_PUBLISHER, EnhanceableSite, ExtensionSite } from '@masknet/shared-base'
+import { DEFAULT_PLUGIN_PUBLISHER } from '@masknet/shared-base'
 import { CYBERCONNECT_PLUGIN_ID } from './constants.js'
 import { languages } from './locales/languages.js'
 
