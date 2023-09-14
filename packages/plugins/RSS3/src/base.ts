bb35ee5f5d5f9dcbfaac0c4d1d3665da34164e3b
Jack-Works <Jack-Works@users.noreply.github.com>
2023-09-14 10:35:58 +0000

    fix: markdownlint

--- packages/plugins/RSS3/src/base.ts
+++ packages/plugins/RSS3/src/base.ts
@@ -1,5 +1,4 @@
 import type { Plugin } from '@masknet/plugin-infra'
-import type { ChainId } from '@masknet/web3-shared-evm'
 import { EnhanceableSite } from '@masknet/shared-base'
 import { languages } from './locales/languages.js'
 import { PLUGIN_NAME, PLUGIN_ID, PLUGIN_DESCRIPTION } from './constants.js'
