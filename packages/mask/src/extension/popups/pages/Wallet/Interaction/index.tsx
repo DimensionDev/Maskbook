842328cafb776c10a9ec59742ea80fa70e5db687
unclebill <billbill290@gmail.com>
2023-09-18 10:04:46 +0800

    fix: follow up comment

--- packages/mask/src/extension/popups/pages/Wallet/Interaction/index.tsx
+++ packages/mask/src/extension/popups/pages/Wallet/Interaction/index.tsx
@@ -10,6 +10,7 @@ import {
     PayloadEditor,
     formatEthereumAddress,
     ChainId,
+    ErrorEditor,
 } from '@masknet/web3-shared-evm'
 import { toHex, toUtf8 } from 'web3-utils'
 import { useNavigate, useSearchParams } from 'react-router-dom'
@@ -222,8 +223,9 @@ const Interaction = memo(function Interaction() {
                     paymentToken,
                 },
             })
-            if (response?.error) {
-                throw new Error(response.error.message)
+            const editor = response ? ErrorEditor.from(null, response) : undefined
+            if (editor?.presence) {
+                throw editor.error
             }
             if (source) await Services.Helper.removePopupWindow()
             navigate(urlcat(PopupRoutes.Wallet, { tab: WalletAssetTabs.Activity }), { replace: true })
