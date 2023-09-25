2c46f767cd81d39bb1f09e414b877d3c4b2a69c4
UncleBill <billbill290@gmail.com>
2023-09-25 17:13:27 +0800

    fix: mf-5058 adjust wrapped article info bar (#10848)

---
+++ packages/mask/src/site-adaptors/mirror.xyz/injection/Tips/adjustArticleInfoBar.ts
@@ -0,0 +1,15 @@
+function selector() {
+    const authorWallet = location.pathname.split('/')[1].toLowerCase()
+    return `#__next div:has(> div > a[href$="mirror.xyz/${authorWallet}" i] button[data-state] img[alt^="0x" i]) + div`
+}
+
+export function adjustArticleInfoBar(signal: AbortSignal) {
+    const node = document.querySelector<HTMLDivElement>(selector())
+    if (!node) return
+    const timer = setInterval(() => {
+        if (node.offsetWidth !== node.parentElement?.offsetWidth) return
+        node.style.justifyContent = 'flex-start'
+        clearInterval(timer)
+    }, 250)
+    signal.addEventListener('abort', () => clearInterval(timer))
+}
