049a654928dc504182011dee9a9db60be37e6870
guanbinrui <guanbinrui@dimension.im>
2023-09-25 18:05:31 +0800

    refactor: nomore auto open switch logo dialog

--- packages/mask/src/components/GuideStep/index.tsx
+++ packages/mask/src/components/GuideStep/index.tsx
@@ -1,15 +1,7 @@
 import { cloneElement, useRef, useState, type ReactElement, useLayoutEffect } from 'react'
 import { makeStyles, usePortalShadowRoot } from '@masknet/theme'
 import { Box, Modal, styled, Typography } from '@mui/material'
-import {
-    CrossIsolationMessages,
-    EnhanceableSite,
-    SwitchLogoDialogStatus,
-    sayHelloShowed,
-    switchLogoOpenedState,
-    userGuideFinished,
-    userGuideStatus,
-} from '@masknet/shared-base'
+import { sayHelloShowed, switchLogoOpenedState, userGuideFinished, userGuideStatus } from '@masknet/shared-base'
 import { useValueRef } from '@masknet/shared-base-ui'
 import { activatedSiteAdaptorUI } from '../../site-adaptor-infra/index.js'
 import { useI18N } from '../../utils/index.js'
@@ -131,7 +123,6 @@ export default function GuideStep({ total, step, tip, children, arrow = true, on
     const onSkip = () => {
         sayHelloShowed[networkIdentifier].value = true
         userGuideFinished[networkIdentifier].value = true
-        CrossIsolationMessages.events.switchLogoDialogUpdated.sendToLocal({ open: true })
     }
 
     const onNext = () => {
@@ -146,18 +137,7 @@ export default function GuideStep({ total, step, tip, children, arrow = true, on
     const onTry = () => {
         userGuideFinished[networkIdentifier].value = true
         onComplete?.()
-        CrossIsolationMessages.events.switchLogoDialogUpdated.sendToLocal({ open: true })
     }
-
-    useLayoutEffect(() => {
-        if (
-            !finished ||
-            state === SwitchLogoDialogStatus.PreviouslyOpened ||
-            networkIdentifier !== EnhanceableSite.Twitter
-        )
-            return
-        CrossIsolationMessages.events.switchLogoDialogUpdated.sendToLocal({ open: true })
-    }, [finished, state, networkIdentifier])
 
     useLayoutEffect(() => {
         let stopped = false
