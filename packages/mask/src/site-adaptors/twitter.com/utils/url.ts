a07c140a8b90a6dee2e7bef0f66f74c9b6b9db3f
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-09-26 21:55:42 +0800

    fix: reduce twitter image requests (#10867)
    * fix: reduce twitter image requests
    
    * fix: storage type
    
    * fix: cspell

--- packages/mask/src/site-adaptors/twitter.com/utils/url.ts
+++ packages/mask/src/site-adaptors/twitter.com/utils/url.ts
@@ -13,9 +13,7 @@ export const hostLeadingUrlAutoTwitter = (isMobile: boolean) =>
 // https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/entities-object#photo_format
 export const canonifyImgUrl = (url: string) => {
     const parsed = new URL(url)
-    if (parsed.hostname !== 'pbs.twimg.com') {
-        return url
-    }
+    if (parsed.hostname !== 'pbs.twimg.com') return url
     const { searchParams } = parsed
     searchParams.set('name', 'orig')
     // we can't understand original image format when given url labeled as webp
@@ -24,7 +22,7 @@ export const canonifyImgUrl = (url: string) => {
         const pngURL = parsed.href
         searchParams.set('format', 'jpg')
         const jpgURL = parsed.href
-        return [pngURL, jpgURL]
+        return [jpgURL, pngURL]
     }
     return parsed.href
 }
--- packages/mask/src/utils/fetcher.ts
+++ packages/mask/src/utils/fetcher.ts
@@ -38,7 +38,7 @@ function canAccessAsContent(url: string) {
     const target = new URL(url, location.href)
     if (
         location.origin.endsWith('twitter.com') &&
-        ['https://abs.twimg.com', 'https://upload.twitter.com', 'https://api.twitter.com'].includes(target.origin)
+        (target.origin.endsWith('twitter.com') || target.origin.endsWith('twimg.com'))
     )
         return true
 
