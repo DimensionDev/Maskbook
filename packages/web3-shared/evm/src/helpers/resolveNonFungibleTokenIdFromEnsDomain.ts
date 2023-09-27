66dcb07c5ed61eba9e8b12da1ce0cfef9b7f37e2
guanbinrui <guanbinrui@mask.io>
2023-09-27 23:47:54 +0800

    refactor: remove more pkgs

--- packages/web3-shared/evm/src/helpers/resolveNonFungibleTokenIdFromEnsDomain.ts
+++ packages/web3-shared/evm/src/helpers/resolveNonFungibleTokenIdFromEnsDomain.ts
@@ -1,7 +1,6 @@
+import { keccak256 } from 'web3-utils'
 import { BigNumber } from 'bignumber.js'
-import { keccak256 } from '@ethersproject/keccak256'
-import { toUtf8Bytes } from '@ethersproject/strings'
 
 export function resolveNonFungibleTokenIdFromEnsDomain(domain: string): string {
-    return new BigNumber(keccak256(toUtf8Bytes(domain.replace(/\.\w+$/, '')))).toFixed()
+    return new BigNumber(keccak256(domain.replace(/\.\w+$/, ''))).toFixed()
 }
