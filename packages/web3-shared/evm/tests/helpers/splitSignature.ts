7110ffe7e74f75a6532e4f18919fc363c3bad119
guanbinrui <guanbinrui@dimension.im>
2023-10-08 12:26:51 +0800

    chore: impl the case

--- packages/web3-shared/evm/tests/helpers/splitSignature.ts
+++ packages/web3-shared/evm/tests/helpers/splitSignature.ts
@@ -1,5 +1,15 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+import { splitSignature } from '../../src/helpers/splitSignature.js'
+
+const signed =
+    '0x670f45faac741b2c2d742007482790ed1bb87ca9b7e7680a8314c1cf96eabbb55f98114c10ade72b2fcdd6fdaa10811a337919e77862976355ea63d34cd2699f1c'
 
 describe('splitSignature', () => {
-    test.skip('should split signature', () => {})
+    test('should split signature', () => {
+        const splited = splitSignature(signed)
+
+        expect(splited.r).toBe('0x670f45faac741b2c2d742007482790ed1bb87ca9b7e7680a8314c1cf96eabbb5')
+        expect(splited.s).toBe('0x5f98114c10ade72b2fcdd6fdaa10811a337919e77862976355ea63d34cd2699f')
+        expect(splited.v).toBe(28)
+    })
 })
