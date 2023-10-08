bb2b8a3ff2fc3fdc47cec0318e1a4856c39ce663
Harlan Sun <33143119+beyond009@users.noreply.github.com>
2023-10-08 17:04:43 +0700

    fix: local profiles load slowly (#10913)

--- packages/mask/src/extension/popups/hooks/useFriendProfiles.ts
+++ packages/mask/src/extension/popups/hooks/useFriendProfiles.ts
@@ -9,6 +9,7 @@ import { NextIDProof } from '@masknet/web3-providers'
 import { useQuery } from '@tanstack/react-query'
 import { useCurrentPersona } from '../../../components/DataSource/useCurrentPersona.js'
 import { PlatformSort, UnsupportedPlatforms } from '../pages/Friends/common.js'
+import { useMemo } from 'react'
 
 export const profilesFilter = (x: BindingProof) => {
     return (x.platform === NextIDPlatform.ENS && x.name.endsWith('.eth')) || !UnsupportedPlatforms.includes(x.platform)
@@ -17,7 +18,7 @@ export const profilesFilter = (x: BindingProof) => {
 export function useFriendProfiles(seen: boolean, nextId?: string, profile?: ProfileIdentifier) {
     const currentPersona = useCurrentPersona()
 
-    const { data: profiles } = useQuery(
+    const { data: profiles = EMPTY_LIST } = useQuery(
         ['profiles', currentPersona?.identifier.publicKeyAsHex, nextId],
         async () => {
             if (!nextId) return EMPTY_LIST
@@ -31,26 +32,26 @@ export function useFriendProfiles(seen: boolean, nextId?: string, profile?: Prof
             enabled: seen && !!nextId,
         },
     )
-    if (!profiles) return EMPTY_LIST
-    if (profiles.length === 0) {
-        if (profile?.userId) {
-            return [
-                {
-                    platform: profile?.network as
-                        | EnhanceableSite.Twitter
-                        | EnhanceableSite.Facebook
-                        | EnhanceableSite.Instagram,
-                    identity: profile.userId,
-                    is_valid: true,
-                    last_checked_at: '',
-                    name: profile.userId,
-                    created_at: '',
-                },
-            ]
-        } else {
-            return EMPTY_LIST
+    return useMemo(() => {
+        if (profiles.length === 0) {
+            if (profile?.userId) {
+                return [
+                    {
+                        platform: profile?.network as
+                            | EnhanceableSite.Twitter
+                            | EnhanceableSite.Facebook
+                            | EnhanceableSite.Instagram,
+                        identity: profile.userId,
+                        is_valid: true,
+                        last_checked_at: '',
+                        name: profile.userId,
+                        created_at: '',
+                    },
+                ]
+            } else {
+                return EMPTY_LIST
+            }
         }
-    }
-    const filtered = profiles.filter(profilesFilter).sort((a, b) => PlatformSort[a.platform] - PlatformSort[b.platform])
-    return filtered
+        return profiles.filter(profilesFilter).sort((a, b) => PlatformSort[a.platform] - PlatformSort[b.platform])
+    }, [profiles])
 }
