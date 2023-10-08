3629829ae18b60859828ce496fccdadc62af7dc7
Harlan Sun <1249413181@qq.com>
2023-10-08 16:45:18 +0700

    chore: code review

--- packages/mask/src/extension/popups/hooks/useFriendProfiles.ts
+++ packages/mask/src/extension/popups/hooks/useFriendProfiles.ts
@@ -9,6 +9,7 @@ import { NextIDProof } from '@masknet/web3-providers'
 import { useQuery } from '@tanstack/react-query'
 import { useCurrentPersona } from '../../../components/DataSource/useCurrentPersona.js'
 import { PlatformSort, UnsupportedPlatforms } from '../pages/Friends/common.js'
+import { useMemo } from 'react'
 
 export const profilesFilter = (x: BindingProof) => {
     return (x.platform === NextIDPlatform.ENS && x.name.endsWith('.eth')) || !UnsupportedPlatforms.includes(x.platform)
@@ -31,25 +32,26 @@ export function useFriendProfiles(seen: boolean, nextId?: string, profile?: Prof
             enabled: seen && !!nextId,
         },
     )
-    if (profiles.length === 0) {
-        if (profile?.userId) {
-            return [
-                {
-                    platform: profile?.network as
-                        | EnhanceableSite.Twitter
-                        | EnhanceableSite.Facebook
-                        | EnhanceableSite.Instagram,
-                    identity: profile.userId,
-                    is_valid: true,
-                    last_checked_at: '',
-                    name: profile.userId,
-                    created_at: '',
-                },
-            ]
-        } else {
-            return EMPTY_LIST
+    return useMemo(() => {
+        if (profiles.length === 0) {
+            if (profile?.userId) {
+                return [
+                    {
+                        platform: profile?.network as
+                            | EnhanceableSite.Twitter
+                            | EnhanceableSite.Facebook
+                            | EnhanceableSite.Instagram,
+                        identity: profile.userId,
+                        is_valid: true,
+                        last_checked_at: '',
+                        name: profile.userId,
+                        created_at: '',
+                    },
+                ]
+            } else {
+                return EMPTY_LIST
+            }
         }
-    }
-    const filtered = profiles.filter(profilesFilter).sort((a, b) => PlatformSort[a.platform] - PlatformSort[b.platform])
-    return filtered
+        return profiles.filter(profilesFilter).sort((a, b) => PlatformSort[a.platform] - PlatformSort[b.platform])
+    }, [profiles])
 }
