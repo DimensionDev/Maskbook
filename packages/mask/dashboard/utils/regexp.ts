08a389b68f4f45eafd76125efa9b351d762af66c
nuanyang233 <nuanyang233@gmail.com>
2023-10-08 14:51:58 +0800

    fix: email validate regex

--- packages/mask/dashboard/utils/regexp.ts
+++ packages/mask/dashboard/utils/regexp.ts
@@ -1,5 +1,5 @@
 export const passwordRegexp = /^(?=.{8,20}$)(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^\dA-Za-z]).*/
 
-export const emailRegexp = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
+export const emailRegexp = /^([\w!#$%&*+./=?^`{|}~’-]{1,64}@([\dA-Za-z-]{1,255}.[\dA-Za-z-]{2,}))$/
 
 export const phoneRegexp = /^(\+?([ .-])?\d{1,2}([ .-])?)?(\(?\d{3}\)?|\d{3})([ .-])?(\d{3}([ .-])?\d{4})/
