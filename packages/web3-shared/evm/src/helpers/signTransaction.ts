dcc699134b5f1c4458804a35e6dfc1c30838a90f
guanbinrui <guanbinrui@dimension.im>
2023-10-08 14:18:54 +0800

    fix: package errors

--- packages/web3-shared/evm/src/helpers/signTransaction.ts
+++ packages/web3-shared/evm/src/helpers/signTransaction.ts
@@ -1,8 +1,9 @@
-import { Accounts } from 'web3-eth-accounts'
+import Web3Accounts, { type Accounts } from 'web3-eth-accounts'
 import type { Transaction } from '../types/index.js'
 
 export function signTransaction(transaction: Transaction, privateKey: string) {
     if (typeof transaction.nonce === 'undefined') throw new Error('Nonce is required.')
-    const accounts = new Accounts()
+    const Accounts_ = Web3Accounts as unknown as typeof Accounts
+    const accounts = new Accounts_()
     return accounts.signTransaction(transaction, privateKey)
 }
