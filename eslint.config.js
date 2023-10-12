3a7e4f9999cef9e604fa12005bf586badb0fde14
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-10-12 19:39:57 +0800

    refactor: code style

--- eslint.config.js
+++ eslint.config.js
@@ -507,7 +507,6 @@ export default [
             'import/resolver': {
                 typescript: {},
             },
-            // 'import/extensions': ['.ts', '.tsx'],
         },
     },
     {
