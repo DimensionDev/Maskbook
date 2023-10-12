0139d9339e1cc5a04fa1bab1cc451fb3f2b233cb
Jack Works <jackworks@protonmail.com>
2023-10-12 11:16:01 +0800

    fix: circular reference

--- packages/web3-providers/src/Manager/io.ts
+++ packages/web3-providers/src/Manager/io.ts
@@ -1,6 +1,3 @@
-import { createEVMState } from '../Web3/EVM/apis/Web3StateAPI.js'
-import { createFlowState } from '../Web3/Flow/apis/Web3StateAPI.js'
-import { createSolanaState } from '../Web3/Solana/apis/Web3StateAPI.js'
 import type { WalletAPI } from '../entry-types.js'
 import { evm, flow, solana } from './registry.js'
 
@@ -9,8 +6,14 @@ export async function initWallet(_io: WalletAPI.IOContext) {
     io = _io
 
     await Promise.all([
-        createEVMState(io).then((state) => (evm.state = state)),
-        createSolanaState(io).then((state) => (solana.state = state)),
-        createFlowState(io).then((state) => (flow.state = state)),
+        import(/* webpackMode: 'eager' */ '../Web3/EVM/apis/Web3StateAPI.js').then((mod) =>
+            mod.createEVMState(io).then((state) => (evm.state = state)),
+        ),
+        import(/* webpackMode: 'eager' */ '../Web3/Flow/apis/Web3StateAPI.js').then((mod) =>
+            mod.createFlowState(io).then((state) => (flow.state = state)),
+        ),
+        import(/* webpackMode: 'eager' */ '../Web3/Solana/apis/Web3StateAPI.js').then((mod) =>
+            mod.createSolanaState(io).then((state) => (solana.state = state)),
+        ),
     ])
 }
