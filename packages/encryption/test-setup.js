ce142edcffd8e4198d19ab51b1526ee867103283
Jack Works <jackworks@protonmail.com>
2023-10-13 18:32:39 +0800

    refactor: remove unused deps (#10963)

--- packages/encryption/test-setup.js
+++ packages/encryption/test-setup.js
@@ -1,6 +1,6 @@
 import { expect } from 'vitest'
 import { polyfill } from '@masknet/secp256k1-webcrypto/node'
-import '../shared-base/node_modules/tiny-secp256k1'
+import 'tiny-secp256k1'
 import { testSerializer } from '../test-serializer/index.js'
 
 expect.addSnapshotSerializer(testSerializer)
--- packages/shared/src/UI/components/ActionButton/index.tsx
+++ packages/shared/src/UI/components/ActionButton/index.tsx
@@ -1,82 +1,12 @@
-/* eslint-disable tss-unused-classes/unused-classes */
-import { useState, useCallback } from 'react'
-import { Button, CircularProgress } from '@mui/material'
+import { useState } from 'react'
+import { CircularProgress } from '@mui/material'
 import { makeStyles, ActionButton } from '@masknet/theme'
 import type { ButtonProps } from '@mui/material/Button'
 import { Check as CheckIcon, Error as ErrorIcon } from '@mui/icons-material'
 import { red, green } from '@mui/material/colors'
-import { useDebounce, useAsyncFn, useUpdateEffect } from 'react-use'
-
-const useErrorStyles = makeStyles()((theme) => {
-    const dark = theme.palette.mode === 'dark'
-    return {
-        containedPrimary: {
-            backgroundColor: dark ? red[500] : red[900],
-            '&:hover': {
-                backgroundColor: dark ? red[900] : red[700],
-            },
-        },
-        outlinedPrimary: {
-            borderColor: dark ? red[500] : red[900],
-            color: dark ? red[500] : red[900],
-            '&:hover': {
-                borderColor: dark ? red[900] : red[700],
-            },
-        },
-    }
-})
+import { useUpdateEffect } from 'react-use'
 
 const circle = <CircularProgress color="inherit" size={18} />
-
-interface DebounceButtonProps extends Omit<ButtonProps, 'color' | 'onClick'> {
-    color?: ButtonProps['color'] | 'danger'
-    onClick: (e: React.MouseEvent<HTMLButtonElement>) => unknown
-}
-
-function useDebounceAsync<T extends any[]>(
-    asyncFn: (...args: T) => unknown,
-): {
-    loading: boolean
-    disabled: boolean
-    f(...args: T): void
-} {
-    // useAsyncFn use T | [] as it's parameter where is conflict with our usage.
-    // We should ensure always call startAsyncFn with parameters.
-    const [state, startAsyncFn] = useAsyncFn(asyncFn as any, [asyncFn], { loading: false, value: undefined })
-    // Sync the debounce state after 500ms
-    const [debounceLoading, setDebounceLoading] = useState(false)
-    useDebounce(() => setDebounceLoading(state.loading), 500, [state])
-    const f = useCallback(
-        (...args: T) => {
-            if (state.loading) return
-            setDebounceLoading(false)
-            startAsyncFn(...args)
-        },
-        [startAsyncFn, state.loading],
-    )
-    // loading 0ms to 500ms: disabled, !loading
-    // loading 500ms+: disabled, loading
-    if (state.loading) return { f, disabled: true, loading: debounceLoading }
-    // The debounceLoading is invalidated, refresh it now (instead of waiting for 500ms)
-    if (debounceLoading) setDebounceLoading(false)
-    // If the task is not running, ignore the throttledLoading
-    return { disabled: false, loading: false, f }
-}
-export function DebounceButton(_props: DebounceButtonProps) {
-    const { onClick, color, ...props } = _props
-    const classes = useErrorStyles()
-    const { f, loading } = useDebounceAsync(onClick)
-    return (
-        <Button
-            startIcon={loading ? circle : undefined}
-            disabled={loading}
-            onClick={f}
-            classes={color === 'danger' ? classes : undefined}
-            color={color === 'danger' ? 'primary' : color}
-            {...props}
-        />
-    )
-}
 
 export interface ActionButtonPromiseProps extends ButtonProps {
     executor: () => Promise<ActionButtonPromiseState | undefined | void>
