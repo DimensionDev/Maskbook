a6dbd7237cb845161d3681e150a753f544c359d3
unclebill <billbill290@gmail.com>
2023-10-16 12:49:39 +0800

    fix: only verify on Twitter

--- packages/mask/src/components/InjectedComponents/SetupGuide/useSetupGuideStepInfo.ts
+++ packages/mask/src/components/InjectedComponents/SetupGuide/useSetupGuideStepInfo.ts
@@ -31,11 +31,12 @@ export function useSetupGuideStepInfo(destinedPersona: PersonaIdentifier) {
     const { data: persona, refetch } = useQuery(['query-persona-info', destinedPersona.publicKeyAsHex], async () => {
         return Services.Identity.queryPersona(destinedPersona)
     })
+    const { networkIdentifier, configuration } = activatedSiteAdaptorUI!
 
     useEffect(() => MaskMessages.events.ownPersonaChanged.on(() => refetch()), [])
 
     useEffect(() => {
-        if (username || activatedSiteAdaptorUI!.networkIdentifier !== EnhanceableSite.Twitter) return
+        if (username || networkIdentifier !== EnhanceableSite.Twitter) return
         // In order to collect user info after login, need to reload twitter once
         let reloaded = false
         const handler = () => {
@@ -64,7 +65,8 @@ export function useSetupGuideStepInfo(destinedPersona: PersonaIdentifier) {
 
     const { data: proofs } = usePersonaProofs(destinedPersona.publicKeyAsHex)
 
-    if (!persona || !username) return composeInfo(SetupGuideStep.Close, 'close')
+    if (!persona) return composeInfo(SetupGuideStep.Close, 'close')
+    if (!username) return composeInfo(SetupGuideStep.FindUsername, 'doing')
 
     // Not set status
     if (!lastSettingState.status) {
@@ -80,9 +82,7 @@ export function useSetupGuideStepInfo(destinedPersona: PersonaIdentifier) {
     const personaConnectedProfile = persona?.linkedProfiles.find((x) =>
         isSameProfile(
             x.identifier,
-            ProfileIdentifier.of(activatedSiteAdaptorUI!.networkIdentifier, username).expect(
-                `${username} should be a valid user id`,
-            ),
+            ProfileIdentifier.of(networkIdentifier, username).expect(`${username} should be a valid user id`),
         ),
     )
     // Should verified persona
@@ -94,14 +94,14 @@ export function useSetupGuideStepInfo(destinedPersona: PersonaIdentifier) {
             ) && x.is_valid
         )
     })
-    if (personaConnectedProfile && !verifiedProfile && lastSettingState.status === SetupGuideStep.VerifyOnNextID)
+    if (personaConnectedProfile && !verifiedProfile && lastSettingState.status === SetupGuideStep.VerifyOnNextID) {
+        // NextID is available on this site.
+        // Should show pin extension when not set
+        if (!configuration.nextIDConfig?.platform) return composeInfo(SetupGuideStep.Close, 'close')
         return composeInfo(SetupGuideStep.VerifyOnNextID, 'doing')
+    }
     if (!personaConnectedProfile || !confirmConnected) return composeInfo(SetupGuideStep.FindUsername, 'doing')
 
     // NextID is available on this site.
-    // Should show pin extension when not set
-    if (!activatedSiteAdaptorUI!.configuration.nextIDConfig?.platform) return composeInfo(SetupGuideStep.Close, 'close')
-
-    // Default
-    return composeInfo(SetupGuideStep.Close, 'done')
+    return composeInfo(SetupGuideStep.Close, configuration.nextIDConfig?.platform ? 'close' : 'done')
 }
