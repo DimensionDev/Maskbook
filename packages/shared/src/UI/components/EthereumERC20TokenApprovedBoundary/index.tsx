a070dc9973113e9be6890221affcd5df1e2c178d
UncleBill <billbill290@gmail.com>
2023-10-18 18:21:17 +0800

    fixup! feat: mf-5475 add scroll chain (#11001)

--- packages/shared/src/UI/components/EthereumERC20TokenApprovedBoundary/index.tsx
+++ packages/shared/src/UI/components/EthereumERC20TokenApprovedBoundary/index.tsx
@@ -1,14 +1,14 @@
+import React, { useCallback } from 'react'
 import { unreachable } from '@masknet/kit'
 import { TokenIcon } from '@masknet/shared'
-import { ActionButton, type ActionButtonProps, makeStyles, ShadowRootTooltip } from '@masknet/theme'
+import { NetworkPluginID } from '@masknet/shared-base'
+import { ActionButton, ShadowRootTooltip, makeStyles, type ActionButtonProps } from '@masknet/theme'
+import { useChainContext, useFungibleTokenSpenders } from '@masknet/web3-hooks-base'
 import { ApproveStateType, useERC20TokenApproveCallback } from '@masknet/web3-hooks-evm'
-import { isSameAddress, type FungibleToken, isGte } from '@masknet/web3-shared-base'
+import { isGte, isSameAddress, type FungibleToken } from '@masknet/web3-shared-base'
 import type { ChainId, SchemaType } from '@masknet/web3-shared-evm'
 import { HelpOutline } from '@mui/icons-material'
-import React, { useCallback } from 'react'
 import { useSharedTrans } from '../../../locales/index.js'
-import { useChainContext, useFungibleTokenSpenders } from '@masknet/web3-hooks-base'
-import { NetworkPluginID } from '@masknet/shared-base'
 
 const useStyles = makeStyles<void, 'icon'>()((theme, _, refs) => ({
     icon: {},
@@ -78,12 +78,10 @@ export function EthereumERC20TokenApprovedBoundary(props: EthereumERC20TokenAppr
         },
         token?.chainId,
     )
-    console.log('allowance', allowance)
 
     const approved =
-        spenders?.some(
-            (x) => isSameAddress(x.tokenInfo.address, token?.address) && isSameAddress(x.address, spender),
-        ) || isGte(allowance, amount)
+        isGte(allowance, amount) ||
+        spenders?.some((x) => isSameAddress(x.tokenInfo.address, token?.address) && isSameAddress(x.address, spender))
 
     const loading =
         spendersLoading ||
