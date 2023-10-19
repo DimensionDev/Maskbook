d3f07925320feefe444f981dfa2c084e8bc96deb
Jack Works <jackworks@protonmail.com>
2023-10-19 15:41:29 +0800

    fix: tsconfig

--- packages/web3-providers/src/env.d.ts
+++ packages/web3-providers/src/env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="@masknet/global-types/webpack" />
-
-declare module 'twitter-text' {
-    const parser: {
-        parseTweet(tweet: string): {
-            weightedLength: number
-        }
-    }
-
-    export default parser
-}
