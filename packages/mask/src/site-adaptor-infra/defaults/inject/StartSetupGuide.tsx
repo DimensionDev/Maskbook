e412fa4fda5aec50ac7395547fd9a39cefe8a43d
unclebill <billbill290@gmail.com>
2023-10-19 19:39:39 +0800

    refactor: refactor setup guide
    MF-5508
    MF-5509
    MF-5516
    MF-5527

--- packages/mask/src/site-adaptor-infra/defaults/inject/StartSetupGuide.tsx
+++ packages/mask/src/site-adaptor-infra/defaults/inject/StartSetupGuide.tsx
@@ -1,16 +1,9 @@
-import { combineAbortSignal } from '@masknet/kit'
 import type { PersonaIdentifier } from '@masknet/shared-base'
 import { attachReactTreeWithoutContainer } from '../../../utils/shadow-root/renderInShadowRoot.js'
 import { SetupGuide } from '../../../components/InjectedComponents/SetupGuide/index.js'
 
 export function createTaskStartSetupGuideDefault() {
     return (signal: AbortSignal, persona: PersonaIdentifier) => {
-        const controller = new AbortController()
-        const combinedSignal = combineAbortSignal(controller.signal, signal)
-        attachReactTreeWithoutContainer(
-            'setup-guide',
-            <SetupGuide persona={persona} onClose={() => controller.abort()} />,
-            combinedSignal,
-        )
+        attachReactTreeWithoutContainer('setup-guide', <SetupGuide persona={persona} />, signal)
     }
 }
