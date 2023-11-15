000a2429d380ec467cf93723ed4e5a633f430d25
guanbinrui <guanbinrui@mask.io>
2023-11-15 14:02:40 +0800

    refactor: testing friendly

--- packages/shared-base/src/Sniffings/index.ts
+++ packages/shared-base/src/Sniffings/index.ts
@@ -3,26 +3,21 @@ enum SiteHost {
     Facebook = 'facebook.com',
 }
 
-const isChromium =
-    typeof navigator === 'object' &&
-    (navigator.userAgent.includes('Chrome') || navigator.userAgent.includes('Chromium'))
+const navigator_ = process.env.NODE_ENV === 'test' ? null : navigator
+const location_ = process.env.NODE_ENV === 'test' ? null : location
+
+const isChromium = navigator_?.userAgent.includes('Chrome') || navigator_?.userAgent.includes('Chromium')
 
 export const Sniffings = {
-    is_dashboard_page:
-        typeof location === 'object' &&
-        location.protocol.includes('extension') &&
-        location.href.includes('dashboard.html'),
-    is_popup_page:
-        typeof location === 'object' &&
-        location.protocol.includes('extension') &&
-        location.href.includes('popups.html'),
+    is_dashboard_page: location_?.protocol.includes('extension') && location_?.href.includes('dashboard.html'),
+    is_popup_page: location_?.protocol.includes('extension') && location_?.href.includes('popups.html'),
 
-    is_twitter_page: typeof location === 'object' && location.href.includes(SiteHost.Twitter),
-    is_facebook_page: typeof location === 'object' && location.href.includes(SiteHost.Facebook),
+    is_twitter_page: location_?.href.includes(SiteHost.Twitter),
+    is_facebook_page: location_?.href.includes(SiteHost.Facebook),
 
-    is_opera: typeof navigator === 'object' && navigator.userAgent.includes('OPR/'),
-    is_edge: typeof navigator === 'object' && navigator.userAgent.includes('Edg'),
-    is_firefox: typeof navigator === 'object' && navigator.userAgent.includes('Firefox'),
+    is_opera: navigator_?.userAgent.includes('OPR/'),
+    is_edge: navigator_?.userAgent.includes('Edg'),
+    is_firefox: navigator_?.userAgent.includes('Firefox'),
     is_chromium: isChromium,
-    is_safari: !isChromium && navigator.userAgent.includes('Safari'),
+    is_safari: !isChromium && navigator_?.userAgent.includes('Safari'),
 }
