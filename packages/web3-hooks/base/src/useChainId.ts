b0c96a2fc6f6a5565e84992b4a0969b28bbc0dbe
unclebill <billbill290@gmail.com>
2023-11-17 16:21:43 +0800

    fix: mock for firefly

--- packages/web3-hooks/base/src/useChainId.ts
+++ packages/web3-hooks/base/src/useChainId.ts
@@ -3,14 +3,11 @@ import { UNDEFINED, type NetworkPluginID } from '@masknet/shared-base'
 import type { Web3Helper } from '@masknet/web3-helpers'
 import { useWeb3State } from './useWeb3State.js'
 import { useDefaultChainId } from './useDefaultChainId.js'
+import { ChainId } from '@masknet/web3-shared-evm'
 
 export function useChainId<S extends 'all' | void = void, T extends NetworkPluginID = NetworkPluginID>(
     pluginID?: T,
     expectedChainId?: Web3Helper.ChainIdScope<S, T>,
 ) {
-    const { Provider } = useWeb3State<S, T>(pluginID)
-    const defaultChainId = useDefaultChainId(pluginID)
-    const actualChainId = useSubscription(Provider?.chainId ?? UNDEFINED)
-
-    return (expectedChainId ?? actualChainId ?? defaultChainId) as Web3Helper.ChainIdScope<S, T>
+  return ChainId.Mainnet
 }
--- packages/web3-hooks/base/src/useProviderType.ts
+++ packages/web3-hooks/base/src/useProviderType.ts
@@ -2,11 +2,10 @@ import { useSubscription } from 'use-subscription'
 import type { Web3Helper } from '@masknet/web3-helpers'
 import { UNDEFINED, type NetworkPluginID } from '@masknet/shared-base'
 import { useWeb3State } from './useWeb3State.js'
+import { ProviderType } from '@masknet/web3-shared-evm'
 
 export function useProviderType<S extends 'all' | void = void, T extends NetworkPluginID = NetworkPluginID>(
     pluginID?: T,
 ) {
-    const { Provider } = useWeb3State(pluginID)
-    const defaultProviderType = useSubscription(Provider?.providerType ?? UNDEFINED)
-    return defaultProviderType as Web3Helper.ProviderTypeScope<S, T>
+  return ProviderType.MetaMask
 }
