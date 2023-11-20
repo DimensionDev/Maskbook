acdc61444b9085be70d495a2ba2c07eb6996bb94
UncleBill <billbill290@gmail.com>
2023-11-20 10:39:09 +0800

    fix(NFTScan): mf-5609 data struct gets changed for solana nft (#11144)

--- packages/web3-providers/src/types/NonFungibleToken.ts
+++ packages/web3-providers/src/types/NonFungibleToken.ts
@@ -55,7 +55,7 @@ export namespace NonFungibleTokenAPI {
         owners_total: number
         royalty: number
         opensea_floor_price: number
-        floor_price: number
+        floor_price: number | undefined
         price_symbol: string
     }
     export interface Provider<ChainId, SchemaType, Indicator = PageIndicator> {
