69ddb4bf7ae6129a25a1974589a8106a6c52aace
unclebill <billbill290@gmail.com>
2023-11-20 16:12:59 +0800

    fix: i18n

--- packages/plugins/Calendar/src/SiteAdaptor/RenderCalendar.tsx
+++ packages/plugins/Calendar/src/SiteAdaptor/RenderCalendar.tsx
@@ -1,31 +1,35 @@
-import { i18NextInstance } from '@masknet/shared-base';
-import { queryClient } from "@masknet/shared-base-ui";
-import { CSSVariableInjector, MaskLightTheme, MaskThemeProvider, DisableShadowRootContext } from '@masknet/theme';
-import { RootWeb3ContextProvider } from '@masknet/web3-hooks-base';
-import { StyledEngineProvider } from '@mui/material';
-import { QueryClientProvider } from "@tanstack/react-query";
-import { I18nextProvider } from "react-i18next";
-import { CalendarContent } from "./CalendarContent.js";
+import { useEffect } from 'react'
+import { i18NextInstance, addI18NBundle } from '@masknet/shared-base'
+import { queryClient } from '@masknet/shared-base-ui'
+import { CSSVariableInjector, MaskLightTheme, MaskThemeProvider, DisableShadowRootContext } from '@masknet/theme'
+import { StyledEngineProvider } from '@mui/material'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { I18nextProvider } from 'react-i18next'
+import { CalendarContent } from './CalendarContent.js'
+import { languages } from '../locales/languages.js'
+import { PLUGIN_ID } from '../constants.js'
 
 function useTheme() {
     return MaskLightTheme
 }
 
 export function RenderCalendar() {
-  return (
-    <DisableShadowRootContext.Provider value>
-      <I18nextProvider i18n={i18NextInstance}>
-        <StyledEngineProvider injectFirst>
-          <MaskThemeProvider useMaskIconPalette={(theme) => theme.palette.mode} useTheme={useTheme}>
-            <QueryClientProvider client={queryClient}>
-              <RootWeb3ContextProvider>
-                <CSSVariableInjector />
-                <CalendarContent />
-              </RootWeb3ContextProvider>
-            </QueryClientProvider>
-          </MaskThemeProvider>
-        </StyledEngineProvider>
-      </I18nextProvider>
-    </DisableShadowRootContext.Provider>
-  )
+    useEffect(() => {
+        addI18NBundle(i18NextInstance, PLUGIN_ID, languages)
+    }, [])
+
+    return (
+        <DisableShadowRootContext.Provider value>
+            <I18nextProvider i18n={i18NextInstance}>
+                <StyledEngineProvider injectFirst>
+                    <MaskThemeProvider useMaskIconPalette={(theme) => theme.palette.mode} useTheme={useTheme}>
+                        <QueryClientProvider client={queryClient}>
+                            <CSSVariableInjector />
+                            <CalendarContent />
+                        </QueryClientProvider>
+                    </MaskThemeProvider>
+                </StyledEngineProvider>
+            </I18nextProvider>
+        </DisableShadowRootContext.Provider>
+    )
 }
