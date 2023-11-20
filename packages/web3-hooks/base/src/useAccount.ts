aa5448d5cf8b7611a08fc4be259f96d692b220bb
unclebill <billbill290@gmail.com>
2023-11-20 16:12:59 +0800

    fix: i18n

--- packages/web3-hooks/base/src/useAccount.ts
+++ packages/web3-hooks/base/src/useAccount.ts
@@ -4,5 +4,8 @@ import { useWeb3State } from './useWeb3State.js'
 import { useWeb3Utils } from './useWeb3Utils.js'
 
 export function useAccount<T extends NetworkPluginID>(pluginID?: T, expectedAccount?: string) {
-  return '0x790116d0685eB197B886DAcAD9C247f785987A4a'
+    const Utils = useWeb3Utils(pluginID)
+    const { Provider } = useWeb3State(pluginID)
+    const defaultAccount = useSubscription(Provider?.account ?? UNDEFINED)
+    return Utils.formatAddress(expectedAccount ?? defaultAccount ?? '')
 }
--- packages/web3-hooks/base/src/useProviderType.ts
+++ packages/web3-hooks/base/src/useProviderType.ts
@@ -2,10 +2,11 @@ import { useSubscription } from 'use-subscription'
 import type { Web3Helper } from '@masknet/web3-helpers'
 import { UNDEFINED, type NetworkPluginID } from '@masknet/shared-base'
 import { useWeb3State } from './useWeb3State.js'
-import { ProviderType } from '@masknet/web3-shared-evm'
 
 export function useProviderType<S extends 'all' | void = void, T extends NetworkPluginID = NetworkPluginID>(
     pluginID?: T,
 ) {
-  return ProviderType.MetaMask
+    const { Provider } = useWeb3State(pluginID)
+    const defaultProviderType = useSubscription(Provider?.providerType ?? UNDEFINED)
+    return defaultProviderType as Web3Helper.ProviderTypeScope<S, T>
 }
