db82a9c009ace47e20a25fc98750bb0be52c1c64
unclebill <billbill290@gmail.com>
2024-01-05 12:11:14 +0800

    fixup! fix: mf-5937 update collectible

--- packages/plugins/Collectible/src/SiteAdaptor/hooks/getNFTXAssetAddress.ts
+++ packages/plugins/Collectible/src/SiteAdaptor/hooks/getNFTXAssetAddress.ts
@@ -3,29 +3,28 @@ import type { AbiItem } from 'web3-utils'
 import { ChainId } from '@masknet/web3-shared-evm'
 import { EVMContractReadonly } from '@masknet/web3-providers'
 
-const ABI = [
-    {
-        constant: true,
-        inputs: [],
-        name: 'assetAddress',
-        outputs: [
-            {
-                name: '',
-                type: 'address',
-            },
-        ],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-    },
-]
-
 /** address in url is not the one of the contract */
 export function useNFTXAssetAddress(address: string, enabled: boolean) {
     return useQuery({
         enabled,
         queryKey: ['nftx', 'asset-address', address],
         queryFn: async () => {
+            const ABI = [
+                {
+                    constant: true,
+                    inputs: [],
+                    name: 'assetAddress',
+                    outputs: [
+                        {
+                            name: '',
+                            type: 'address',
+                        },
+                    ],
+                    payable: false,
+                    stateMutability: 'view',
+                    type: 'function',
+                },
+            ]
             const contract = EVMContractReadonly.getWeb3Contract(address, ABI as AbiItem[], {
                 chainId: ChainId.Mainnet,
             })
