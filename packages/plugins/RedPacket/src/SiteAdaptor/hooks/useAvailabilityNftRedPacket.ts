88c895a7fee1dfbc150789c3808d97d7676a6fd9
UncleBill <billbill290@gmail.com>
2024-01-11 17:56:46 +0800

    refactor: adjust get nft avalibility (#11279)

--- packages/plugins/RedPacket/src/SiteAdaptor/hooks/useAvailabilityNftRedPacket.ts
+++ packages/plugins/RedPacket/src/SiteAdaptor/hooks/useAvailabilityNftRedPacket.ts
@@ -1,42 +1,47 @@
-import { useAsyncRetry } from 'react-use'
 import { BigNumber } from 'bignumber.js'
 import type { ChainId } from '@masknet/web3-shared-evm'
 import { useNftRedPacketContract } from './useNftRedPacketContract.js'
+import { useQuery, type UseQueryResult } from '@tanstack/react-query'
 
+type T = UseQueryResult
 export function useAvailabilityNftRedPacket(id: string, from: string, chainId?: ChainId) {
     const nftRedPacketContract = useNftRedPacketContract(chainId)
-    return useAsyncRetry(async () => {
-        if (!id || !nftRedPacketContract) return null
-        const availability = await nftRedPacketContract.methods.check_availability(id).call({
-            // check availability is ok w/o account
-            from,
-        })
+    return useQuery({
+        queryKey: ['nft-redpacket', 'availability', chainId, from, id],
+        enabled: !!nftRedPacketContract,
+        queryFn: async () => {
+            if (!id || !nftRedPacketContract) return null
+            const availability = await nftRedPacketContract.methods.check_availability(id).call({
+                // check availability is ok w/o account
+                from,
+            })
 
-        const result = await nftRedPacketContract.methods.check_erc721_remain_ids(id).call({
-            // check availability is ok w/o account
-            from,
-        })
+            const result = await nftRedPacketContract.methods.check_erc721_remain_ids(id).call({
+                // check availability is ok w/o account
+                from,
+            })
 
-        const isClaimed = availability.claimed_id !== '0'
-        const totalAmount = result.erc721_token_ids.length
-        const bits = new BigNumber(result.bit_status).toString(2).split('')
-        const claimedAmount = bits.filter((bit) => bit === '1').length
-        const isClaimedAll = totalAmount === claimedAmount
-        const isCompleted = isClaimedAll && !isClaimed
-        const isEnd = isCompleted || availability.expired
+            const isClaimed = availability.claimed_id !== '0'
+            const totalAmount = result.erc721_token_ids.length
+            const bits = new BigNumber(result.bit_status).toString(2).split('')
+            const claimedAmount = bits.filter((bit) => bit === '1').length
+            const isClaimedAll = totalAmount === claimedAmount
+            const isCompleted = isClaimedAll && !isClaimed
+            const isEnd = isCompleted || availability.expired
 
-        const bitStatusList = bits.reverse().map((bit) => bit === '1')
+            const bitStatusList = bits.reverse().map((bit) => bit === '1')
 
-        return {
-            isClaimed,
-            totalAmount,
-            claimedAmount,
-            remaining: totalAmount - claimedAmount,
-            isClaimedAll,
-            isCompleted,
-            isEnd,
-            bitStatusList,
-            ...availability,
-        }
-    }, [id, from, nftRedPacketContract])
+            return {
+                isClaimed,
+                totalAmount,
+                claimedAmount,
+                remaining: totalAmount - claimedAmount,
+                isClaimedAll,
+                isCompleted,
+                isEnd,
+                bitStatusList,
+                ...availability,
+            }
+        },
+    })
 }
