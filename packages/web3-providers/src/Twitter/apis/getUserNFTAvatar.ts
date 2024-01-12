686db0db2ede3347f43e3bd01f45593b7c42aaac
nuanyang233 <nuanyang233@gmail.com>
2024-01-12 14:47:25 +0800

    fix: create wallet with mnemonic

--- packages/web3-providers/src/Twitter/apis/getUserNFTAvatar.ts
+++
@@ -1,9 +0,0 @@
-import urlcat from 'urlcat'
-import { fetchCachedJSON } from '../../helpers/fetchJSON.js'
-import type { TwitterBaseAPI } from '../../entry-types.js'
-
-export async function getUserNFTAvatar(screenName: string) {
-    return await fetchCachedJSON<TwitterBaseAPI.UserNFTAvatarResponse>(
-        urlcat('https://a8fq5hs9nk.execute-api.us-east-1.amazonaws.com/prod/twitter', { screenName }),
-    )
-}
