bc6f6922fbde450a280a9b633f9f0dc046ff171b
guanbinrui <guanbinrui@mask.io>
2024-01-23 21:01:56 +0800

    fix: infer

--- packages/web3-hooks/base/src/useFireflyLensAccounts.ts
+++ packages/web3-hooks/base/src/useFireflyLensAccounts.ts
@@ -1,6 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { FireflyConfig } from '@masknet/web3-providers'
 import type { FireflyConfigAPI } from '@masknet/web3-providers/types'
+import type { UseQueryResult } from '@tanstack/react-query'
+
+// The inferred type of 'useFireflyLensAccounts' cannot be named without a reference to @tanstack/react-query
+type T = UseQueryResult
 
 export function useFireflyLensAccounts(twitterId?: string, isVerified?: boolean) {
     return useQuery<FireflyConfigAPI.LensAccount[]>({
