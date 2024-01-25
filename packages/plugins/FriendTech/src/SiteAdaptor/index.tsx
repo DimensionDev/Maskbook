222342d5099fb9bf1f119056265444979311abf8
UncleBill <billbill290@gmail.com>
2024-01-25 20:33:44 +0800

    fix: remove app icon from application board only (#11315)
    * Revert "fix: mf-6007 remove friend tech from application board (#11312)"
    
    This reverts commit 106717bbd203731cc88597c9f61a0a0f0028cb15.
    
    * fix: remove app icon from application board only

--- packages/plugins/FriendTech/src/SiteAdaptor/index.tsx
+++ packages/plugins/FriendTech/src/SiteAdaptor/index.tsx
@@ -1,4 +1,7 @@
+import { Icons } from '@masknet/icons'
 import type { Plugin } from '@masknet/plugin-infra'
+import { PluginID } from '@masknet/shared-base'
+import { Trans } from 'react-i18next'
 import { base } from '../base.js'
 import { FriendTechInjection } from './FriendTechInjection.js'
 import { FriendTechNameWidget } from './FriendTechNameWidget.js'
@@ -8,6 +11,21 @@ const site: Plugin.SiteAdaptor.Definition = {
     GlobalInjection() {
         return <FriendTechInjection />
     },
+    ApplicationEntries: [
+        (() => {
+            const icon = <Icons.FriendTech size={36} />
+            const name = <Trans ns={PluginID.FriendTech} i18nKey="name" />
+            const iconFilterColor = 'rgba(1, 186, 250, 0.20)'
+            return {
+                ApplicationEntryID: base.ID,
+                icon,
+                category: 'dapp',
+                description: <Trans ns={PluginID.FriendTech} i18nKey="description" />,
+                name,
+                iconFilterColor,
+            }
+        })(),
+    ],
     NameWidget: {
         ID: `${base.ID}_name_widget`,
         priority: 2,
