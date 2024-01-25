dcbaf9e7dec83362fd7f269b5db3e5c034a80ba1
UncleBill <billbill290@gmail.com>
2024-01-25 15:00:35 +0800

    fix: mf-6007 remove friend tech from application board (#11312)

--- packages/plugins/FriendTech/src/SiteAdaptor/index.tsx
+++ packages/plugins/FriendTech/src/SiteAdaptor/index.tsx
@@ -1,14 +1,6 @@
-import { Icons } from '@masknet/icons'
 import type { Plugin } from '@masknet/plugin-infra'
-import { PluginTransFieldRender } from '@masknet/plugin-infra/content-script'
-import { ApplicationEntry } from '@masknet/shared'
-import { PluginID } from '@masknet/shared-base'
-import { Telemetry } from '@masknet/web3-telemetry'
-import { EventID, EventType } from '@masknet/web3-telemetry/types'
-import { Trans } from 'react-i18next'
 import { base } from '../base.js'
 import { FriendTechInjection } from './FriendTechInjection.js'
-import { openDialog } from './emitter.js'
 import { FriendTechNameWidget } from './FriendTechNameWidget.js'
 
 const site: Plugin.SiteAdaptor.Definition = {
@@ -16,37 +8,6 @@ const site: Plugin.SiteAdaptor.Definition = {
     GlobalInjection() {
         return <FriendTechInjection />
     },
-    ApplicationEntries: [
-        (() => {
-            const icon = <Icons.FriendTech size={36} />
-            const name = <Trans ns={PluginID.FriendTech} i18nKey="name" />
-            const iconFilterColor = 'rgba(1, 186, 250, 0.20)'
-            return {
-                ApplicationEntryID: base.ID,
-                RenderEntryComponent(EntryComponentProps) {
-                    return (
-                        <ApplicationEntry
-                            title={<PluginTransFieldRender field={name} pluginID={base.ID} />}
-                            {...EntryComponentProps}
-                            icon={icon}
-                            iconFilterColor={iconFilterColor}
-                            onClick={() => {
-                                EntryComponentProps.onClick ? EntryComponentProps.onClick(openDialog) : openDialog()
-                                Telemetry.captureEvent(EventType.Access, EventID.EntryAppFileOpen)
-                            }}
-                        />
-                    )
-                },
-                appBoardSortingDefaultPriority: 1,
-                marketListSortingPriority: 3,
-                icon,
-                category: 'dapp',
-                description: <Trans ns={PluginID.FriendTech} i18nKey="description" />,
-                name,
-                iconFilterColor,
-            }
-        })(),
-    ],
     NameWidget: {
         ID: `${base.ID}_name_widget`,
         priority: 2,
