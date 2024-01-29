1518dcf14a85e78439119b03f421ffe895978bec
unclebill <billbill290@gmail.com>
2024-01-29 10:40:12 +0800

    chore: run svgo

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ export default {
     '*': 'prettier --write --ignore-unknown',
     'packages/**/*.{ts,tsx,js,jsx}': 'eslint --fix',
     'cspell.json': () => 'gulp reorder-spellcheck',
-    'packages/**/*.svg': 'npx svgo',
+    'packages/**/*.svg': 'pnpm svgo',
 }
