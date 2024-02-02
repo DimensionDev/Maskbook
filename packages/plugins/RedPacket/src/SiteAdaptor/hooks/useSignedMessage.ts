4d83b04b66c0a96b863640310c1a15b4a6538b41
UncleBill <billbill290@gmail.com>
2024-02-02 19:10:26 +0800

    fix: claim firefly redpacket (#11350)

--- packages/plugins/RedPacket/src/SiteAdaptor/hooks/useSignedMessage.ts
+++ packages/plugins/RedPacket/src/SiteAdaptor/hooks/useSignedMessage.ts
@@ -1,11 +1,12 @@
-import { FireflyRedPacket } from '@masknet/web3-providers'
-import { signMessage } from '@masknet/web3-shared-evm'
-import { useQuery } from '@tanstack/react-query'
-import { usePlatformType } from './usePlatformType.js'
-import { RedPacketRPC } from '../../messages.js'
 import { useLastRecognizedIdentity, usePostInfoDetails } from '@masknet/plugin-infra/content-script'
+import { FireflyRedPacket } from '@masknet/web3-providers'
 import type { RedPacketJSONPayload, RedPacketNftJSONPayload } from '@masknet/web3-providers/types'
+import { signMessage } from '@masknet/web3-shared-evm'
+import { useQuery, type UseQueryResult } from '@tanstack/react-query'
+import { RedPacketRPC } from '../../messages.js'
+import { usePlatformType } from './usePlatformType.js'
 
+type T = UseQueryResult
 export function useSignedMessage(
     account: string,
     payload: RedPacketJSONPayload | RedPacketNftJSONPayload = {} as RedPacketJSONPayload,
@@ -16,28 +17,31 @@ export function useSignedMessage(
     const author = usePostInfoDetails.author()
     const platform = usePlatformType()
     const me = useLastRecognizedIdentity()
-    const profile = platform ? { platform, profileId: author?.userId || '', lensToken: me?.lensToken } : undefined
+    const profile =
+        platform ?
+            {
+                needLensAndFarcasterHandle: true,
+                platform,
+                profileId: author?.userId || '',
+                lensToken: me?.lensToken,
+            }
+        :   undefined
 
-    const { data: storedPassword, isFetching: gettingStoredPassword } = useQuery({
-        enabled: !password,
-        queryKey: ['red-packet', 'password', payload.txid],
+    return useQuery({
+        queryKey: ['red-packet', 'signed-message', rpid, version, password, account, profile],
         queryFn: async () => {
             const record = await RedPacketRPC.getRedPacketRecord(payload.txid)
-            return record?.password
-        },
-    })
-
-    const { data: signature } = useQuery({
-        enabled: !password && !gettingStoredPassword,
-        queryKey: ['red-packet', 'signed-message', rpid, account, profile],
-        queryFn: async () => {
+            if (record?.password) return record.password
+            if (version <= 3) return password
+            if (password) return signMessage(account, password).signature
             if (!profile) return ''
-            return FireflyRedPacket.createClaimSignature(rpid, account as HexString, profile)
+            return FireflyRedPacket.createClaimSignature({
+                rpid,
+                profile: profile,
+                wallet: {
+                    address: account,
+                },
+            })
         },
     })
-
-    if (storedPassword) return storedPassword
-    if (version <= 3) return password
-    if (password) return signMessage(account, password).signature
-    return signature
 }
