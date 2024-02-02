18f91e63c47e2084774ca8406bd9a9b8aa24c0cd
guanbinrui <guanbinrui@dimension.im>
2024-02-02 21:02:34 +0800

    chore: remove unused dev deps

--- packages/mask/shared-ui/initialization/fetchRemoteFlags.ts
+++
@@ -1,7 +0,0 @@
-import { startFetchRemoteFlag } from '@masknet/flags'
-import { extensionRemoteFlagIO } from '../../shared/helpers/remoteFlagIO.js'
-import { hmr } from '../../utils-pure/index.js'
-
-const { signal } = hmr(import.meta.webpackHot)
-
-startFetchRemoteFlag(extensionRemoteFlagIO, signal)
