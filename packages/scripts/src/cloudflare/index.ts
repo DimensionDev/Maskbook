65a5664a25c77ae8b4af8c58fe2682c1e629192b
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-02-02 21:12:26 +0800

    fix: cp build (#11352)

--- packages/scripts/src/cloudflare/index.ts
+++ packages/scripts/src/cloudflare/index.ts
@@ -1,12 +1,9 @@
 import { dest, parallel, series, src, type TaskFunction } from 'gulp'
-import { awaitChildProcess, PKG_PATH, ROOT_PATH, shell, task } from '../utils/index.js'
+import { PKG_PATH, ROOT_PATH, task } from '../utils/index.js'
 import { codegen } from '../codegen/index.js'
 import { fileURLToPath } from 'url'
 
 const DIST_PATH = new URL('dist/cloudflare', ROOT_PATH)
-function buildApp() {
-    return awaitChildProcess(shell.cwd(new URL('app', PKG_PATH))`pnpm run build:ci`)
-}
 
 function copyLocalesJSON() {
     return src('**/locales/*.json', {
@@ -14,5 +11,5 @@ function copyLocalesJSON() {
         ignore: ['**/node_modules/**', '**/dist/**'],
     }).pipe(dest(fileURLToPath(DIST_PATH)))
 }
-export const buildCloudflare: TaskFunction = series(codegen, parallel(copyLocalesJSON, buildApp))
+export const buildCloudflare: TaskFunction = series(codegen, parallel(copyLocalesJSON))
 task(buildCloudflare, 'build-ci-cloudflare', 'Build for Cloudflare')
