e1ac67991d1c295fd2ab925ccd36753ad11b3700
guanbinrui <guanbinrui@dimension.im>
2024-02-02 18:29:11 +0800

    chore: update build script

--- packages/scripts/src/projects/app.ts
+++
@@ -1,9 +0,0 @@
-import { series, type TaskFunction } from 'gulp'
-import { fromNPMTask, PKG_PATH, task } from '../utils/index.js'
-import { codegen } from '../codegen/index.js'
-
-const [build] = fromNPMTask(new URL('app/', PKG_PATH), 'spa', 'Build Web App of Mask Network.')
-
-export const buildSPA: TaskFunction = series(codegen, build)
-
-task(buildSPA, 'build-app', 'Build App')
