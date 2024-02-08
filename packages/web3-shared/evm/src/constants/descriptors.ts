4f0507cb94fc529f368984944053f8195577aee6
unclebill <billbill290@gmail.com>
2024-02-08 11:17:04 +0800

    fix: add background for okx wallet in connect modal

--- packages/web3-shared/evm/src/constants/descriptors.ts
+++ packages/web3-shared/evm/src/constants/descriptors.ts
@@ -470,6 +470,8 @@ export const PROVIDER_DESCRIPTORS: ReadonlyArray<ProviderDescriptor<ChainId, Pro
         homeLink: 'https://www.okx.com/web3',
         shortenLink: 'okx.com',
         downloadLink: 'https://www.okx.com/web3',
+        backgroundGradient:
+            'linear-gradient(90deg, rgba(98, 152, 234, 0.2) 1.03%, rgba(98, 152, 234, 0.2) 1.04%, rgba(98, 126, 234, 0.2) 100%), linear-gradient(0deg, #FFFFFF, #FFFFFF)',
     },
     {
         ID: `${PLUGIN_ID}_opera`,
