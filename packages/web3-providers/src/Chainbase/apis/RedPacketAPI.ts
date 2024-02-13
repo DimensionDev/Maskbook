eed128e97f34765913d8707b57830b00c1008d98
unclebill <billbill290@gmail.com>
2024-02-13 13:54:42 +0800

    fix: do not discard exceptions of getting redpacket histories

--- packages/web3-providers/src/Chainbase/apis/RedPacketAPI.ts
+++ packages/web3-providers/src/Chainbase/apis/RedPacketAPI.ts
@@ -20,45 +20,41 @@ class ChainbaseRedPacketAPI implements RedPacketBaseAPI.Provider<ChainId, Schema
         contractAddress: string,
         methodId: string,
     ): Promise<Array<Transaction<ChainId, SchemaType>> | undefined> {
-        try {
-            const txes = await asyncIteratorToArray(
-                pageableToIterator(async (indicator) => {
-                    const { records } = await fetchJSON<{ records: { data: { result: Tx[] } } }>(
-                        urlcat(TRANSACTIONS_BY_CONTRACT_METHOD_ENDPOINT, {
-                            senderAddress,
-                            contractAddress,
-                            chainId,
-                            methodId: `${methodId}%`, // '%' for sql string match.
-                            size: MAX_SIZE_PER_PAGE,
-                            offset: Number(indicator?.id ?? 0) * MAX_SIZE_PER_PAGE,
-                        }),
-                    )
-
-                    return createPageable(
-                        records.data.result,
-                        createIndicator(indicator),
-                        records.data.result.length === 0 ? undefined : createNextIndicator(indicator),
-                    )
-                }),
-            )
-
-            if (!txes?.length) return
-
-            return txes
-                .sort((a, b) => new Date(b.block_timestamp).getTime() - new Date(a.block_timestamp).getTime())
-                .map((x) => {
-                    return {
-                        input: x.input,
-                        to: x.to_address,
-                        from: x.from_address,
-                        hash: x.transaction_hash,
+        const txes = await asyncIteratorToArray(
+            pageableToIterator(async (indicator) => {
+                const { records } = await fetchJSON<{ records: { data: { result: Tx[] } } }>(
+                    urlcat(TRANSACTIONS_BY_CONTRACT_METHOD_ENDPOINT, {
+                        senderAddress,
+                        contractAddress,
                         chainId,
-                        blockNumber: Number(x.block_number),
-                    } as Transaction<ChainId, SchemaType>
-                })
-        } catch {
-            return
-        }
+                        methodId: `${methodId}%`, // '%' for sql string match.
+                        size: MAX_SIZE_PER_PAGE,
+                        offset: Number(indicator?.id ?? 0) * MAX_SIZE_PER_PAGE,
+                    }),
+                )
+
+                return createPageable(
+                    records.data.result,
+                    createIndicator(indicator),
+                    records.data.result.length === 0 ? undefined : createNextIndicator(indicator),
+                )
+            }),
+        )
+
+        if (!txes?.length) return
+
+        return txes
+            .sort((a, b) => new Date(b.block_timestamp).getTime() - new Date(a.block_timestamp).getTime())
+            .map((x) => {
+                return {
+                    input: x.input,
+                    to: x.to_address,
+                    from: x.from_address,
+                    hash: x.transaction_hash,
+                    chainId,
+                    blockNumber: Number(x.block_number),
+                } as Transaction<ChainId, SchemaType>
+            })
     }
 }
 export const ChainbaseRedPacket = new ChainbaseRedPacketAPI()
