cc3393c6c65dc5dfd0fb1009943f4b071afd3751
UncleBill <billbill290@gmail.com>
2024-02-18 17:19:31 +0800

    fix: compatible with legacy red packet (#11399)

--- packages/plugins/RedPacket/src/SiteAdaptor/helpers.ts
+++ packages/plugins/RedPacket/src/SiteAdaptor/helpers.ts
@@ -1,10 +1,33 @@
 import { createRenderWithMetadata, createTypedMessageMetadataReader } from '@masknet/typed-message-react'
-import { RedPacketMetaKey, RedPacketNftMetaKey } from '../constants.js'
+import { EVMChainResolver } from '@masknet/web3-providers'
 import type { RedPacketJSONPayload, RedPacketNftJSONPayload } from '@masknet/web3-providers/types'
-import schema from '../schema.json'
+import { ChainId } from '@masknet/web3-shared-evm'
+import { Ok, type Result } from 'ts-results-es'
+import { RedPacketMetaKey, RedPacketNftMetaKey } from '../constants.js'
 import schemaNtf from '../schema-nft.json'
+import schema from '../schema.json'
 
-export const RedPacketMetadataReader = createTypedMessageMetadataReader<RedPacketJSONPayload>(RedPacketMetaKey, schema)
+export const reader = createTypedMessageMetadataReader<RedPacketJSONPayload>(RedPacketMetaKey, schema)
+export function RedPacketMetadataReader(
+    metadata: ReadonlyMap<string, unknown> | undefined,
+): Result<RedPacketJSONPayload, void> {
+    const result = reader(metadata)
+    if (result.isOk()) {
+        const payload = result.value
+        // Hard code for legacy RedPacket
+        if (!payload.token && payload.contract_version === 1 && payload.token_type === 0) {
+            const chainId = payload.network === 'Mainnet' ? ChainId.Mainnet : undefined
+            if (!chainId) return result
+
+            return Ok({
+                ...payload,
+                token: EVMChainResolver.nativeCurrency(chainId),
+            })
+        }
+        return result
+    }
+    return result
+}
 export const renderWithRedPacketMetadata = createRenderWithMetadata(RedPacketMetadataReader)
 
 export const RedPacketNftMetadataReader = createTypedMessageMetadataReader<RedPacketNftJSONPayload>(
