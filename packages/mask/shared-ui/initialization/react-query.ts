3e4affd6a8abd0875f3a40dd09898175038047f7
swkatmask <swkatmask@gmail.com>
2024-02-21 10:40:52 +0800

    fix: stop broadcasting react-query cache changes

--- packages/mask/shared-ui/initialization/react-query.ts
+++ packages/mask/shared-ui/initialization/react-query.ts
@@ -1,8 +1,4 @@
-import { queryClient } from '@masknet/shared-base-ui'
-import { broadcastQueryClient } from '../utils/broadcastQueryClient.js'
 import { setInitData } from '../utils/persistOptions.js'
-
-broadcastQueryClient({ queryClient })
 
 browser.storage.local.get('react-query').then(({ 'react-query': data }) => {
     setInitData('react-query', data)
