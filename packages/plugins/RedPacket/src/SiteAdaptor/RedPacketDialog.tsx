cf8d8ca7772e6ab577e39ba0ae4c40056245d65a
Harlan Sun <1249413181@qq.com>
2024-02-27 16:17:48 +0800

    chore: revert debug

--- packages/plugins/RedPacket/src/SiteAdaptor/RedPacketDialog.tsx
+++ packages/plugins/RedPacket/src/SiteAdaptor/RedPacketDialog.tsx
@@ -127,7 +127,7 @@ export default function RedPacketDialog(props: RedPacketDialogProps) {
     const [openSelectNFTDialog, setOpenSelectNFTDialog] = useState(false)
     // #endregion
 
-    const isFirefly = true
+    const isFirefly = !!props.fireflyContext
 
     const handleClose = useCallback(() => {
         setStep(CreateRedPacketPageStep.NewRedPacketPage)
