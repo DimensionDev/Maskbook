a820e479af2247224ac68cf29784e3354a31cac6
Jack Works <jackworks@protonmail.com>
2024-03-04 21:49:52 +0800

    Revert "fix: mf-6071 don't persistent cache my persona info (#11478)"
    This reverts commit 4f430244e5bc537127e325849d3e591ef3a9dfc6.

--- packages/shared/src/hooks/usePersonaContext.ts
+++ packages/shared/src/hooks/usePersonaContext.ts
@@ -25,7 +25,7 @@ function usePersonaInformation(
     queryOwnedPersonaInformation?: (initializedOnly: boolean) => Promise<PersonaInformation[]>,
 ) {
     const { data: personas = EMPTY_LIST, refetch } = useQuery({
-        queryKey: ['my-own-persona-info'],
+        queryKey: ['@@queryOwnedPersonaInformation(false)'],
         queryFn: () => queryOwnedPersonaInformation?.(false),
     })
     useEffect(() => MaskMessages.events.ownPersonaChanged.on(() => refetch()), [])
