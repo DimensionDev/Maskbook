c082c4ee00be4ac0b30231374194eb4b11518605
Wukong Sun <158803171+swkatmask@users.noreply.github.com>
2024-04-25 16:40:01 +0800

    fix: invoke redpacket parsing api on Firefly only (#11594)

--- packages/plugins/RedPacket/src/SiteAdaptor/hooks/useParseRedPacket.ts
+++ packages/plugins/RedPacket/src/SiteAdaptor/hooks/useParseRedPacket.ts
@@ -1,5 +1,5 @@
 import { useLastRecognizedIdentity, usePostInfoDetails } from '@masknet/plugin-infra/content-script'
-import { NetworkPluginID } from '@masknet/shared-base'
+import { EnhanceableSite, NetworkPluginID } from '@masknet/shared-base'
 import { useChainContext, useNetworkContext } from '@masknet/web3-hooks-base'
 import { FireflyRedPacket } from '@masknet/web3-providers'
 import type { FireflyRedPacketAPI } from '@masknet/web3-providers/types'
@@ -8,7 +8,8 @@ import { useQuery, type UseQueryResult } from '@tanstack/react-query'
 
 type T = UseQueryResult
 /**
- * Parse RedPacket with post info
+ * Parse RedPacket with post info.
+ * Firefly only.
  */
 export function useParseRedPacket(chainId: ChainId) {
     const images = usePostInfoDetails.postMetadataImages()
@@ -20,9 +21,11 @@ export function useParseRedPacket(chainId: ChainId) {
     const source = usePostInfoDetails.source()
     const me = useLastRecognizedIdentity()
     const myProfileId = me?.profileId
+    const site = usePostInfoDetails.site()
+    const isOnFirefly = site === EnhanceableSite.Firefly
 
     const query = useQuery({
-        enabled: images.length > 0,
+        enabled: images.length > 0 && isOnFirefly,
         queryKey: ['red-packet', 'parse', source, images[0], account],
         queryFn: async () => {
             const platform = source?.toLowerCase() as FireflyRedPacketAPI.PlatformType
