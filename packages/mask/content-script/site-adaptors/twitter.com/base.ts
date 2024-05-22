94e5d0b23a3d82f209a369c98e41d6203cc9ba29
nuanyang233 <nuanyang233@gmail.com>
2024-05-22 22:56:14 +0800

    fix: origins

--- packages/mask/content-script/site-adaptors/twitter.com/base.ts
+++ packages/mask/content-script/site-adaptors/twitter.com/base.ts
@@ -2,7 +2,7 @@ import { EncryptPayloadNetwork } from '@masknet/encryption'
 import type { SiteAdaptor } from '@masknet/types'
 import { EnhanceableSite, isDomainOrSubdomainOf } from '@masknet/shared-base'
 
-const origins = ['https://mobile.twitter.com/*', 'https://twitter.com/*', 'https://mobile.x.com/*', 'https://x.com/*']
+const origins = ['https://mobile.twitter.com/*', 'https://twitter.com/*', 'https://www.x.com/*', 'https://x.com/*']
 export const twitterBase: SiteAdaptor.Base = {
     networkIdentifier: EnhanceableSite.Twitter,
     encryptPayloadNetwork: EncryptPayloadNetwork.Twitter,
