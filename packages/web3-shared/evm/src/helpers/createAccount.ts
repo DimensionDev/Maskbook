f4d4263044e1765d17d4a789d70c625af572e581
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2024-07-01 22:13:40 +0800

    fix: lockfile

--- packages/web3-shared/evm/src/helpers/createAccount.ts
+++ packages/web3-shared/evm/src/helpers/createAccount.ts
@@ -1,7 +1,8 @@
+import type { Account } from 'web3-core'
 import * as Web3Accounts from /* webpackDefer: true */ 'web3-eth-accounts'
 import type { Accounts } from 'web3-eth-accounts'
 
-export function createAccount() {
+export function createAccount(): Account {
     const Accounts_ = Web3Accounts.default as unknown as typeof Accounts
     const accounts = new Accounts_()
     return accounts.create()
