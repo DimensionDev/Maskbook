9a814648e6f229c66207c69648c98da0a27d1dc8
guanbinrui <guanbinrui@mask.io>
2024-07-16 01:21:02 +0800

    refactor: remove domain api

--- packages/web3-providers/src/Web3/Solana/state/IdentityService.ts
+++ packages/web3-providers/src/Web3/Solana/state/IdentityService.ts
@@ -2,7 +2,6 @@ import { compact } from 'lodash-es'
 import { NetworkPluginID, type SocialIdentity, type SocialAddress, SocialAddressType } from '@masknet/shared-base'
 import { type ChainId, isValidAddress } from '@masknet/web3-shared-solana'
 import { IdentityServiceState } from '../../Base/state/IdentityService.js'
-import { SolanaDomain } from '../apis/DomainAPI.js'
 
 const SOL_RE = /\S{1,256}\.sol\b/i
 
@@ -18,17 +17,11 @@ function getSolanaDomain(nickname: string, bio: string) {
     return matched
 }
 
-function getSolanaDomainAddress(domain: string) {
-    if (!domain) return
-    return SolanaDomain.lookup(domain)
-}
-
 export class SolanaIdentityService extends IdentityServiceState<ChainId> {
     protected override async getFromRemote(identity: SocialIdentity) {
         const { bio = '', nickname = '' } = identity
         const address = getSolanaAddress(bio)
         const domain = getSolanaDomain(nickname, bio)
-        const domainAddress = domain ? await getSolanaDomainAddress(domain) : undefined
 
         return compact<SocialAddress<ChainId>>([
             address ?
@@ -37,14 +30,6 @@ export class SolanaIdentityService extends IdentityServiceState<ChainId> {
                     type: SocialAddressType.Address,
                     label: '',
                     address,
-                }
-            :   undefined,
-            domainAddress ?
-                {
-                    pluginID: NetworkPluginID.PLUGIN_SOLANA,
-                    type: SocialAddressType.SOL,
-                    label: domain ?? domainAddress,
-                    address: domainAddress,
                 }
             :   undefined,
         ])
