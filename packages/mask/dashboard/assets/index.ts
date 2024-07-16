fc9e99ebdec8cc3080e1f6783130e0bfe2e45c15
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2024-07-16 16:39:33 +0800

    refactor: remove unused files

--- packages/mask/dashboard/assets/index.ts
+++ packages/mask/dashboard/assets/index.ts
@@ -2,7 +2,6 @@
  * Manage local static resource
  */
 export const ABOUT_DIALOG_BACKGROUND = new URL('./images/AboutDialogBackground.png', import.meta.url).href
-export const WatermarkURL = new URL('./images/MaskWatermark.png', import.meta.url).href
 // A workaround for Opera file name validation
 export const Welcome = new URL('./Welcome.splinecode.png', import.meta.url).href
 export const PrintBackground = new URL('./images/PrintBackground.png', import.meta.url).href
--- packages/mask/dashboard/pages/SignUp/routePath.ts
+++ packages/mask/dashboard/pages/SignUp/routePath.ts
@@ -1,6 +1,3 @@
 export enum SignUpRoutePath {
-    MnemonicReveal = 'mnemonic-reveal',
-    PersonaCreate = 'persona-create',
     PersonaRecovery = 'persona-recovery',
-    ConnectSocialMedia = 'connect-social-media',
 }
