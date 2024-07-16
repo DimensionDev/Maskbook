d096a02b85594ea5be691df47b7c9ddcf64b370b
Wukong Sun <swkatmask@gmail.com>
2024-07-16 17:36:36 +0800

    fixup! refactor: slim maskbook for firefly

--- packages/backup-format/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { encryptBackup, decryptBackup } from './version-3/index.js'
-export { BackupErrors } from './BackupErrors.js'
-export {
-    normalizeBackup,
-    type NormalizedBackup,
-    createEmptyNormalizedBackup,
-    generateBackupRAW,
-} from './normalize/index.js'
-export { getBackupSummary, type BackupSummary } from './utils/backupPreview.js'
