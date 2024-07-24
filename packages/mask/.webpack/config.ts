57c707566f78d5f2d6d863ed596781f022355297
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-07-24 11:26:10 +0800

    [Release] Hotfix 2.26.4 => 2.26.5 (patch) (#11703)
    * chore: bump version to 2.26.5
    
    * fix: fw-6308 celo icon doesn't show in collectible card, enable zora (#11704)
    
    * fix: fw-6308 celo icon doesn't show in collectible card
    
    * fix: enable zora for simplehash
    
    * fix: disable celo for simplehash (#11705)
    
    * fix: lazy load calendar data (#11706)
    
    ---------
    
    Co-authored-by: Wukong Sun <swkatmask@gmail.com>

--- packages/mask/.webpack/config.ts
+++ packages/mask/.webpack/config.ts
@@ -363,7 +363,7 @@ export async function createConfiguration(_inputFlags: BuildFlags): Promise<webp
             path: flags.outputPath,
             filename: 'entry/[name].js',
             chunkFilename: productionLike ? 'bundled/[id].js' : 'bundled/chunk-[name].js',
-            assetModuleFilename: 'assets/[hash][ext][query]',
+            assetModuleFilename: productionLike ? 'assets/[hash][ext][query]' : 'assets/[name]-[hash][ext][query]',
             webassemblyModuleFilename: 'assets/[hash].wasm',
             hotUpdateMainFilename: 'hot/[runtime].[fullhash].json',
             hotUpdateChunkFilename: 'hot/[id].[fullhash].js',
