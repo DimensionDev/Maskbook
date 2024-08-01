6858762db8d12d062071a4aeb228eac18afbb028
nuanyang233 <nuanyang233@gmail.com>
2024-08-01 11:56:43 +0800

    feat: lido withdraw (#11709)
          * feat: lido withdraw
    
    * chore: cspell
    
    * chore: reply review

--- packages/web3-providers/src/Lido/constants.ts
+++ packages/web3-providers/src/Lido/constants.ts
@@ -1,3 +1,5 @@
 import { resolveCrossOriginURL } from '@masknet/web3-shared-base'
 
 export const LIDO_STETH_APR_API = resolveCrossOriginURL('https://stake.lido.fi/api/sma-steth-apr')
+
+export const LIDO_REQUEST_TIME_API = resolveCrossOriginURL('https://wq-api.lido.fi/v2/request-time/calculate')
