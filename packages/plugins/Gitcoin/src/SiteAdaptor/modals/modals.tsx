fb5c965815c17aeb11424f589e35eb67b71f6dd6
nuanyang233 <nuanyang233@gmail.com>
2024-08-05 16:58:43 +0800

    feat: new gitcoin project card (#11714) (#11720)
    * feat: new gitcoin project card
    
    * fix: run codegen
    
    * chore: lint
    
    ---------
    
    Co-authored-by: nuanyang233 <nuanyang233@users.noreply.github.com>

--- packages/plugins/Gitcoin/src/SiteAdaptor/modals/modals.tsx
+++
@@ -1,6 +0,0 @@
-import { SingletonModal } from '@masknet/shared-base'
-import type { DonateModalOpenProps } from './DonateModal/index.js'
-import type { ResultModalOpenProps } from './ResultModal/index.js'
-
-export const DonateModal = new SingletonModal<DonateModalOpenProps>()
-export const ResultModal = new SingletonModal<ResultModalOpenProps>()
--- packages/plugins/Gitcoin/src/constants.ts
+++ packages/plugins/Gitcoin/src/constants.ts
@@ -4,7 +4,7 @@ import { ChainId } from '@masknet/web3-shared-evm'
 
 export const PLUGIN_META_KEY = `${PluginID.Gitcoin}:1`
 export { PLUGIN_NAME } from './base.js'
-export const GITCOIN_API_GRANTS_V1 = 'https://gitcoin-agent.r2d2.to/grants/v1/api/grant/:id/'
+export const GITCOIN_API_GRANTS_V1 = 'https://grants-stack-indexer-v2.gitcoin.co/graphql'
 const TENANTS = ['ALGORAND', 'BINANCE', 'CELO', 'COSMOS', 'ETH', 'KUSAMA', 'POLKADOT', 'RSK', 'ZIL'] as const
 export const SUPPORTED_TENANTS = ['ETH']
 export type TenantTypes = (typeof TENANTS)[number]
