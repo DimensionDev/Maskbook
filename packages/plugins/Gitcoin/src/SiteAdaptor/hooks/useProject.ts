0358e1ccc60e5513a5d3bef71aca596a1ade36ab
nuanyang233 <nuanyang233@gmail.com>
2024-08-05 20:32:40 +0800

    fix: infinite loop (#11721)

--- packages/plugins/Gitcoin/src/SiteAdaptor/hooks/useProject.ts
+++ packages/plugins/Gitcoin/src/SiteAdaptor/hooks/useProject.ts
@@ -1,17 +1,13 @@
-import { PluginID } from '@masknet/shared-base'
-import { useQuery } from '@tanstack/react-query'
 import { fetchApplications, fetchProjectById } from '../../apis/index.js'
+import { useAsyncRetry } from 'react-use'
 
 export function useProject(id: string) {
-    return useQuery({
-        queryKey: [PluginID.Gitcoin, 'project', 'id'],
-        queryFn: async () => {
-            const project = await fetchProjectById(id)
-            const applications = await fetchApplications(id)
-            return {
-                project,
-                applications,
-            }
-        },
-    })
+    return useAsyncRetry(async () => {
+        const project = await fetchProjectById(id)
+        const applications = await fetchApplications(id)
+        return {
+            project,
+            applications,
+        }
+    }, [id])
 }
