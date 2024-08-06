b7a2d39ceb15432a442538d7f8358e86597c9311
guanbinrui <guanbinrui@mask.io>
2024-08-06 16:09:53 +0800

    refactor: remove unused hooks

--- packages/web3-hooks/evm/src/useERC721TokenContract.ts
+++
@@ -1,9 +0,0 @@
-import type { AbiItem } from 'web3-utils'
-import ERC721ABI from '@masknet/web3-contracts/abis/ERC721.json'
-import type { ERC721 } from '@masknet/web3-contracts/types/ERC721.js'
-import type { ChainId } from '@masknet/web3-shared-evm'
-import { useContract } from './useContract.js'
-
-export function useERC721TokenContract(chainId?: ChainId, address?: string) {
-    return useContract<ERC721>(chainId, address, ERC721ABI as AbiItem[])
-}
