c5a4303c253992e7f1a0fd58d71e97181114fbac
nuanyang233 <nuanyang233@gmail.com>
2024-08-08 17:10:37 +0800

    fix: read nonces from contract

--- packages/web3-providers/src/Lido/index.ts
+++ packages/web3-providers/src/Lido/index.ts
@@ -4,8 +4,9 @@ import { LIDO_REQUEST_TIME_API, LIDO_STETH_APR_API } from './constants.js'
 
 export class Lido {
     static async getStEthAPR() {
-        const apr = await fetchJSON<number>(LIDO_STETH_APR_API, { mode: 'cors' })
-        return apr.toString()
+        const apr = await fetchJSON<{ data: { smaApr: string } }>(LIDO_STETH_APR_API, { mode: 'cors' })
+
+        return apr.data.smaApr
     }
 
     static async getLidoWaitingTime(amount: string) {
