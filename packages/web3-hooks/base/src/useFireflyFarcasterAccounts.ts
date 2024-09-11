80451d6184bacafbc83826149af7cab1453a1a57
Wukong Sun <swkatmask@gmail.com>
2024-09-11 21:12:08 +0800

    fix: improve query user by user identity (#11787)

--- packages/web3-hooks/base/src/useFireflyFarcasterAccounts.ts
+++ packages/web3-hooks/base/src/useFireflyFarcasterAccounts.ts
@@ -2,21 +2,27 @@ import { skipToken, useQuery } from '@tanstack/react-query'
 import { FireflyConfig, Twitter } from '@masknet/web3-providers'
 import { EMPTY_LIST } from '@masknet/shared-base'
 
-export function useFireflyFarcasterAccounts(userId?: string) {
+export function useFireflyFarcasterAccounts(identity?: string) {
+    identity = identity?.toLowerCase()
     const { data: user } = useQuery({
-        queryKey: ['twitter', 'profile', userId],
-        queryFn: userId ? () => Twitter.getUserByScreenName(userId) : skipToken,
+        queryKey: ['twitter', 'profile', identity],
+        staleTime: 300_000,
+        queryFn: identity ? () => Twitter.getUserByScreenName(identity) : skipToken,
     })
+    const id = user?.userId.toLowerCase()
+
     return useQuery({
-        queryKey: ['union-profile', 'by-twitter-id', userId],
+        queryKey: ['union-profile', 'by-twitter-id', id],
         retry: 0,
         staleTime: 60_000,
-        queryFn: async () => {
-            if (!user?.userId) return
-            const unionProfile = await FireflyConfig.getUnionProfile({
-                twitterId: user?.userId,
-            })
-            return unionProfile.farcasterProfiles ?? EMPTY_LIST
-        },
+        queryFn:
+            id ?
+                async () => {
+                    const unionProfile = await FireflyConfig.getUnionProfile({
+                        twitterId: id,
+                    })
+                    return unionProfile.farcasterProfiles ?? EMPTY_LIST
+                }
+            :   skipToken,
     })
 }
