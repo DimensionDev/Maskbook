b73d06ebb675a1ea55884c377ac80111a190e55e
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2024-09-21 19:51:35 +0800

    fix: lingui warning

--- packages/mask/dashboard/initialization/i18n.ts
+++ packages/mask/dashboard/initialization/i18n.ts
@@ -5,6 +5,6 @@ import { initReactI18next } from 'react-i18next'
 import { i18n } from '@lingui/core'
 
 initReactI18next.init(i18NextInstance)
-i18n.activate('en-US')
 addSharedI18N(i18NextInstance, i18n)
 addDashboardI18N(i18NextInstance, i18n)
+i18n.activate('en')
