161a12bd0708042fd1ce2133e225d3e413fec99a
Wukong Sun <swkatmask@gmail.com>
2024-10-14 18:09:24 +0800

    feat: mf-6335 refine nft detail dialog

--- packages/plugins/Collectible/src/SiteAdaptor/CardDialog/CardDialogContent.tsx
+++ packages/plugins/Collectible/src/SiteAdaptor/CardDialog/CardDialogContent.tsx
@@ -1,13 +1,12 @@
 import { useCallback } from 'react'
 import { openWindow } from '@masknet/shared-base-ui'
 import { useSubscription } from 'use-subscription'
-import { Button } from '@mui/material'
+import { alpha, Button } from '@mui/material'
 import { makeStyles } from '@masknet/theme'
 import { Icons } from '@masknet/icons'
-import { ConnectPersonaBoundary, LoadingStatus, PluginWalletStatusBar, ReloadStatus } from '@masknet/shared'
+import { ConnectPersonaBoundary, LoadingStatus, ReloadStatus } from '@masknet/shared'
 import { PluginID, NetworkPluginID, CrossIsolationMessages } from '@masknet/shared-base'
 import { SourceType, resolveSourceTypeName } from '@masknet/web3-shared-base'
-import { Web3ContextProvider } from '@masknet/web3-hooks-base'
 import {
     useAllPersonas,
     useCurrentVisitingIdentity,
@@ -33,6 +32,7 @@ const useStyles = makeStyles<{ listItemBackground?: string; listItemBackgroundIc
         },
         mediaBox: {
             width: 300,
+            flexShrink: 0,
         },
         contentLayout: {
             width: '100%',
@@ -40,6 +40,7 @@ const useStyles = makeStyles<{ listItemBackground?: string; listItemBackgroundIc
             display: 'flex',
             padding: 24,
             boxSizing: 'border-box',
+            gap: theme.spacing(4),
             paddingBottom: 72,
             overflow: 'auto',
             scrollbarWidth: 'none',
@@ -48,21 +49,30 @@ const useStyles = makeStyles<{ listItemBackground?: string; listItemBackgroundIc
             },
         },
         tabWrapper: {
-            width: 'calc(100% - 336px)',
-            marginLeft: 36,
             overflowY: 'auto',
             scrollbarWidth: 'none',
+            flexGrow: 1,
             '&::-webkit-scrollbar': {
                 display: 'none',
             },
         },
         footer: {
+            position: 'sticky',
+            bottom: 0,
+            boxSizing: 'content-box',
+            display: 'flex',
+            backgroundColor: alpha(theme.palette.maskColor.bottom, 0.8),
             boxShadow:
                 theme.palette.mode === 'dark' ?
                     '0px 0px 20px rgba(255, 255, 255, 0.12)'
                 :   '0px 0px 20px rgba(0, 0, 0, 0.05)',
-            position: 'sticky',
-            bottom: 0,
+            backdropFilter: 'blur(16px)',
+            padding: theme.spacing(2),
+            borderRadius: '0 0 12px 12px',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flex: 1,
+            maxHeight: 40,
         },
         buttonText: {
             marginLeft: theme.spacing(1),
@@ -81,15 +91,7 @@ export function CardDialogContent(props: CardDialogContentProps) {
     const { currentTab } = props
     const { classes } = useStyles()
     const t = useCollectibleTrans()
-    const {
-        asset,
-        orders,
-        offers,
-        origin,
-        parentPluginID = NetworkPluginID.PLUGIN_EVM,
-        pluginID,
-        chainId,
-    } = Context.useContainer()
+    const { asset, orders, offers, origin, pluginID } = Context.useContainer()
     const currentVisitingIdentity = useCurrentVisitingIdentity()
     const lastRecognized = useLastRecognizedIdentity()
     const currentIdentifier = useSubscription(currentPersonaIdentifier)
@@ -141,48 +143,48 @@ export function CardDialogContent(props: CardDialogContentProps) {
                 </div>
             </div>
 
-            <Web3ContextProvider network={parentPluginID}>
-                <PluginWalletStatusBar className={classes.footer} expectedPluginID={pluginID} expectedChainId={chainId}>
-                    {origin === 'pfp' && currentVisitingIdentity?.isOwner ?
-                        <ConnectPersonaBoundary
-                            personas={personas}
-                            identity={lastRecognized}
-                            currentPersonaIdentifier={currentIdentifier}
-                            openDashboard={openDashboard}
-                            handlerPosition="top-right"
-                            customHint
-                            directTo={PluginID.Avatar}
-                            beforeAction={onBeforeAction}>
-                            <Button
-                                sx={{ display: 'flex', alignItems: 'center' }}
-                                variant="contained"
-                                size="medium"
-                                onClick={onPFPButtonClick}
-                                fullWidth>
-                                <Icons.Avatar size={20} />
-                                <span className={classes.buttonText}>{t.plugin_collectibles_pfp_button()}</span>
-                            </Button>
-                        </ConnectPersonaBoundary>
-                    : externalLink ?
+            {origin === 'pfp' && currentVisitingIdentity?.isOwner ?
+                <div className={classes.footer}>
+                    <ConnectPersonaBoundary
+                        personas={personas}
+                        identity={lastRecognized}
+                        currentPersonaIdentifier={currentIdentifier}
+                        openDashboard={openDashboard}
+                        handlerPosition="top-right"
+                        customHint
+                        directTo={PluginID.Avatar}
+                        beforeAction={onBeforeAction}>
                         <Button
                             sx={{ display: 'flex', alignItems: 'center' }}
                             variant="contained"
                             size="medium"
-                            onClick={onMoreButtonClick}
+                            onClick={onPFPButtonClick}
                             fullWidth>
-                            <span className={classes.buttonText}>
-                                {t.plugin_collectibles_more_on_button({
-                                    provider:
-                                        asset.data.source === SourceType.NFTScan ?
-                                            resolveSourceTypeName(asset.data.source)
-                                        :   'platform',
-                                })}
-                            </span>
-                            <Icons.LinkOut size={16} />
+                            <Icons.Avatar size={20} />
+                            <span className={classes.buttonText}>{t.plugin_collectibles_pfp_button()}</span>
                         </Button>
-                    :   <div />}
-                </PluginWalletStatusBar>
-            </Web3ContextProvider>
+                    </ConnectPersonaBoundary>
+                </div>
+            : externalLink ?
+                <div className={classes.footer}>
+                    <Button
+                        sx={{ display: 'flex', alignItems: 'center' }}
+                        variant="contained"
+                        size="medium"
+                        onClick={onMoreButtonClick}
+                        fullWidth>
+                        <span className={classes.buttonText}>
+                            {t.plugin_collectibles_more_on_button({
+                                provider:
+                                    asset.data.source === SourceType.NFTScan ?
+                                        resolveSourceTypeName(asset.data.source)
+                                    :   'platform',
+                            })}
+                        </span>
+                        <Icons.LinkOut size={16} />
+                    </Button>
+                </div>
+            :   null}
         </div>
     )
 }
