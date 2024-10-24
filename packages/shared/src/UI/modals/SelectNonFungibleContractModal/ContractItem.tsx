5ebcb37bb28fee952236e640f940b74116e60ec0
nuanyang233 <nuanyang233@gmail.com>
2024-10-24 16:14:31 +0800

    feat: support all network collection in rp

--- packages/shared/src/UI/modals/SelectNonFungibleContractModal/ContractItem.tsx
+++ packages/shared/src/UI/modals/SelectNonFungibleContractModal/ContractItem.tsx
@@ -1,8 +1,9 @@
 import { Icons } from '@masknet/icons'
+import { ImageIcon } from '@masknet/shared'
 import { type NetworkPluginID } from '@masknet/shared-base'
 import { makeStyles } from '@masknet/theme'
 import type { Web3Helper } from '@masknet/web3-helpers'
-import { useWeb3Utils } from '@masknet/web3-hooks-base'
+import { useNetworkDescriptor, useWeb3Utils } from '@masknet/web3-hooks-base'
 import { type NonFungibleCollection } from '@masknet/web3-shared-base'
 import { Avatar, Link, ListItem, Typography, type ListItemProps } from '@mui/material'
 import { memo } from 'react'
@@ -25,11 +26,16 @@ const useStyles = makeStyles()((theme) => ({
     logo: {
         borderRadius: '100%',
         overflow: 'hidden',
-        flexShrink: 0,
         width: 36,
         height: 36,
-        marginRight: 8,
         boxShadow: theme.palette.mode === 'light' ? '0 6px 12px rgba(0,0,0,0.2)' : undefined,
+    },
+    badgeIcon: {
+        position: 'absolute',
+        right: -6,
+        bottom: -4,
+        border: `1px solid ${theme.palette.common.white}`,
+        borderRadius: '50%',
     },
     contractName: {
         fontSize: 14,
@@ -51,6 +57,13 @@ const useStyles = makeStyles()((theme) => ({
     balance: {
         marginLeft: 'auto',
     },
+    icon: {
+        position: 'relative',
+        width: 36,
+        height: 36,
+        marginRight: 8,
+        flexShrink: 0,
+    },
 }))
 
 interface ContractItemProps extends Omit<ListItemProps, 'onSelect'> {
@@ -69,12 +82,16 @@ export const ContractItem = memo(function ContractItem({
 }: ContractItemProps) {
     const { classes, cx } = useStyles()
     const Utils = useWeb3Utils(pluginID)
+    const networkProvider = useNetworkDescriptor(pluginID, collection.chainId)
 
     return (
         <ListItem className={cx(classes.listItem, className)} onClick={() => onSelect?.(collection)} {...rest}>
-            <Avatar className={classes.logo} src={collection.iconURL || ''}>
-                <Icons.MaskAvatar size={36} />
-            </Avatar>
+            <div className={classes.icon}>
+                <Avatar className={classes.logo} src={collection.iconURL || ''}>
+                    <Icons.MaskAvatar size={36} />
+                </Avatar>
+                <ImageIcon className={classes.badgeIcon} size={12} icon={networkProvider?.icon} />
+            </div>
             <div>
                 <Typography className={classes.contractSymbol}>{collection.symbol}</Typography>
                 <Typography className={classes.contractName}>
@@ -92,9 +109,6 @@ export const ContractItem = memo(function ContractItem({
                     </Link>
                 </Typography>
             </div>
-            {collection.balance ?
-                <Typography className={classes.balance}>{collection.balance}</Typography>
-            :   null}
         </ListItem>
     )
 })
