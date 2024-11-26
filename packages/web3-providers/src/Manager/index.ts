f63ed94377de4d41c653a2f99ee6bc12f114a80a
guanbinrui <guanbinrui@mask.io>
2024-11-26 22:21:43 +0800

    fix: getter

--- packages/web3-providers/src/Manager/index.ts
+++ packages/web3-providers/src/Manager/index.ts
@@ -3,10 +3,17 @@ import type { Web3Helper } from '@masknet/web3-helpers'
 import { evm, solana } from './registry.js'
 
 export { initWallet } from './io.js'
-const all = {
-    [NetworkPluginID.PLUGIN_EVM]: evm,
-    [NetworkPluginID.PLUGIN_SOLANA]: solana,
-} as const
+
+function getRegistry(ID: NetworkPluginID) {
+    switch (ID) {
+        case NetworkPluginID.PLUGIN_EVM:
+            return evm
+        case NetworkPluginID.PLUGIN_SOLANA:
+            return solana
+        default:
+            throw new Error('Not supported network')
+    }
+}
 /**
  * Get Web3 providers, for example, WalletConnect, MetaMask, etc.
  * @param ID Network name
@@ -17,7 +24,7 @@ export function getRegisteredWeb3Providers<T extends NetworkPluginID>(
 export function getRegisteredWeb3Providers(
     ID: NetworkPluginID,
 ): ReadonlyArray<Web3Helper.Web3ProviderDescriptor<NetworkPluginID>> {
-    return all[ID].provider
+    return getRegistry(ID).provider
 }
 
 /**
@@ -30,13 +37,13 @@ export function getRegisteredWeb3Networks<T extends NetworkPluginID>(
 export function getRegisteredWeb3Networks(
     ID: NetworkPluginID,
 ): ReadonlyArray<Web3Helper.Web3NetworkDescriptor<NetworkPluginID>> {
-    return all[ID].network
+    return getRegistry(ID).network
 }
 
 export function getRegisteredWeb3Chains<T extends NetworkPluginID>(
     ID: T,
 ): ReadonlyArray<Web3Helper.ChainDescriptorScope<void, T>> {
-    return all[ID].chain
+    return getRegistry(ID).chain
 }
 export function getAllPluginsWeb3State(): {
     [key in NetworkPluginID]: Web3Helper.Web3State<key>
@@ -49,5 +56,5 @@ export function getAllPluginsWeb3State(): {
 
 export function getActivatedPluginWeb3State<T extends NetworkPluginID>(pluginID: T): Web3Helper.Web3State<T>
 export function getActivatedPluginWeb3State(pluginID: NetworkPluginID): Web3Helper.Web3State<NetworkPluginID> {
-    return all[pluginID].state!
+    return getRegistry(pluginID).state!
 }
