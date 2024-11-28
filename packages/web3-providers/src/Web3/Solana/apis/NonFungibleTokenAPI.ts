cf3ffa0be8a6591aac990170d391501fc7aba28f
Wukong Sun <swkatmask@gmail.com>
2024-11-28 14:58:17 +0800

    fix: upgrade solana deps

--- packages/web3-providers/src/Web3/Solana/apis/NonFungibleTokenAPI.ts
+++ packages/web3-providers/src/Web3/Solana/apis/NonFungibleTokenAPI.ts
@@ -1,94 +1,45 @@
 // cspell:ignore metaplex
-import { Connection } from '@metaplex/js'
-import { Metadata } from '@metaplex-foundation/mpl-token-metadata'
-import { EMPTY_LIST, createIndicator, createPageable, type Pageable } from '@masknet/shared-base'
+import { createIndicator, createPageable, type Pageable } from '@masknet/shared-base'
 import { type NonFungibleAsset, TokenType } from '@masknet/web3-shared-base'
 import { ChainId, SchemaType } from '@masknet/web3-shared-solana'
-import type { GetProgramAccountsResponse, SolanaHubOptions } from '../types/index.js'
-import { requestRPC } from '../helpers/index.js'
-import { fetchJSON } from '../../../helpers/fetchJSON.js'
-import { getAssetFullName } from '../../../helpers/getAssetFullName.js'
+import { fetchAllDigitalAssetByOwner } from '@metaplex-foundation/mpl-token-metadata'
+import { publicKey } from '@metaplex-foundation/umi'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
+import * as solanaWeb3 from /* webpackDefer: true */ '@solana/web3.js'
 import type { NonFungibleTokenAPI } from '../../../entry-types.js'
-
-interface ExternalMetadata {
-    name: string
-    symbol: string
-    description: string
-    image?: string
-    animation?: string
-    properties: {
-        files: Array<{
-            uri: string
-            type: LiteralUnion<'image/jpeg', 'image/png'>
-        }>
-        category: string
-        creators: Array<{
-            address: string
-            share: 100
-        }>
-    }
-}
+import type { SolanaHubOptions } from '../types/index.js'
 
 async function getNonFungibleAssets(
     chainId: ChainId,
     account: string,
 ): Promise<Array<NonFungibleAsset<ChainId, SchemaType>>> {
-    const data = await requestRPC<GetProgramAccountsResponse>(chainId, {
-        method: 'getProgramAccounts',
-        params: [
-            'https://api.raydium.io/v2/sdk/token/raydium.mainnet.json',
-            {
-                encoding: 'jsonParsed',
-                filters: [
-                    {
-                        dataSize: 165,
-                    },
-                    {
-                        memcmp: {
-                            offset: 32,
-                            bytes: account,
-                        },
-                    },
-                ],
-            },
-        ],
-    })
-    if (!data.result?.length) return EMPTY_LIST
-    const connection = new Connection('mainnet-beta')
-    const nftTokens = data.result.filter((x) => x.account.data.parsed.info.tokenAmount.decimals === 0)
-    const promises = nftTokens.map(async (x): Promise<NonFungibleAsset<ChainId, SchemaType> | null> => {
-        const pda = await Metadata.getPDA(x.account.data.parsed.info.mint)
-        const metadata = await Metadata.load(connection, pda)
-        if (!metadata) return null
-        const externalMeta = await fetchJSON<ExternalMetadata>(metadata.data.data.uri).catch(() => null)
-        const pubkey = pda.toBase58()
+    const umi = createUmi(solanaWeb3.clusterApiUrl('mainnet-beta'))
+    const ownerPublicKey = publicKey(account)
+    const allNFTs = await fetchAllDigitalAssetByOwner(umi, ownerPublicKey)
+    return allNFTs.map((asset) => {
         return {
-            id: pubkey,
-            tokenId: pubkey,
+            id: asset.mint.publicKey.toString(),
+            tokenId: asset.mint.publicKey.toString(),
             chainId,
             type: TokenType.NonFungible,
             schema: SchemaType.NonFungible,
             address: '',
             contract: {
                 chainId,
-                name: metadata.data.data.name,
-                symbol: metadata.data.data.symbol,
-                address: pubkey,
+                name: asset.metadata.name,
+                symbol: asset.metadata.symbol,
+                address: asset.metadata.publicKey,
                 schema: SchemaType.NonFungible,
             },
             metadata: {
                 chainId,
-                name: getAssetFullName(pubkey, metadata.data.data.name, metadata.data.data.name),
-                symbol: metadata.data.data.symbol,
-                description: externalMeta?.description,
-                mediaURL: externalMeta?.animation ?? externalMeta?.image ?? '',
-                mediaType: externalMeta?.properties.category || 'Unknown',
+                name: asset.metadata.name,
+                symbol: asset.metadata.symbol,
+                description: '',
+                mediaURL: asset.metadata.uri,
             },
         }
     })
-
-    const allSettled = await Promise.allSettled(promises)
-    return allSettled.flatMap((x) => (x.status === 'fulfilled' ? x.value ?? [] : []))
 }
 
 class SolanaNonFungibleTokenAPI implements NonFungibleTokenAPI.Provider<ChainId, SchemaType> {
