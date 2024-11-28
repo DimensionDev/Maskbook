10f8bc6ecd4c25efc268d287b802086580b1aab4
guanbinrui <guanbinrui@mask.io>
2024-11-28 16:32:03 +0800

    chore: cleanup plugin id

--- packages/shared-base/src/types/PluginID.ts
+++ packages/shared-base/src/types/PluginID.ts
@@ -12,14 +12,5 @@ export enum NetworkPluginID {
 export enum PluginID {
     EVM = 'com.mask.evm',
     Solana = 'com.mask.solana',
-    Avatar = 'com.maskbook.avatar',
-    Collectible = 'com.maskbook.collectibles',
-    Handle = 'com.maskbook.handle',
-    NextID = 'com.mask.next_id',
     RedPacket = 'com.maskbook.red_packet',
-    Wallet = 'com.maskbook.wallet',
-    Web3Profile = 'io.mask.web3-profile',
-    Web3ProfileCard = 'io.mask.web3-profile-card',
-    SmartPay = 'com.mask.smart-pay',
-    WalletService = 'com.mask.WalletService',
 }
