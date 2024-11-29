de93523147a2e2073b08a1d8a75b51b21974a0c7
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-11-29 22:23:03 +0800

    fix: flags (#11965)
    * fix: twitter handle
    
    * refactor: flags
    
    ---------
    
    Co-authored-by: Wukong Sun <swkatmask@gmail.com>

--- packages/flags/src/flags/buildInfo.ts
+++ packages/flags/src/flags/buildInfo.ts
@@ -1,4 +1,3 @@
-import { Environment, isEnvironment } from '@dimensiondev/holoflows-kit'
 import { defer } from '@masknet/kit'
 
 export interface BuildInfoFile {
@@ -11,30 +10,15 @@ export interface BuildInfoFile {
     readonly REACT_DEVTOOLS_EDITOR_URL?: string
     readonly channel: 'stable' | 'beta' | 'insider'
 }
-
-export async function getBuildInfo(): Promise<BuildInfoFile> {
-    try {
-        const hasBrowserAPI = isEnvironment(Environment.HasBrowserAPI)
-        const b = (globalThis as any).browser
-        const manifestVersion = hasBrowserAPI ? b.runtime.getManifest().version : undefined
-        const response = await fetch(hasBrowserAPI ? b.runtime.getURL('/build-info.json') : '/build-info.json')
-        const env: BuildInfoFile = await response.json()
-        if (manifestVersion) Object.assign(env, { VERSION: manifestVersion })
-        Object.freeze(env)
-        return env
-    } catch {
-        return {
-            channel: 'stable',
-        }
-    }
-}
 export let env: BuildInfoFile = {
     channel: 'stable',
 }
 const [_promise, resolve] = defer<void>()
 export const buildInfoReadyPromise = _promise
 export async function setupBuildInfo(): Promise<void> {
-    return setupBuildInfoManually(await getBuildInfo())
+    return setupBuildInfoManually({
+        channel: 'stable',
+    })
 }
 export function setupBuildInfoManually(_env: BuildInfoFile) {
     resolve()
