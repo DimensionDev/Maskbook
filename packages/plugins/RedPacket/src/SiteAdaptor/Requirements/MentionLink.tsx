e5d8b57514933daad8fdd9d70539c605e038ccda
Wukong Sun <swkatmask@gmail.com>
2024-11-29 21:20:57 +0800

    fix: twitter handle

--- packages/plugins/RedPacket/src/SiteAdaptor/Requirements/MentionLink.tsx
+++ packages/plugins/RedPacket/src/SiteAdaptor/Requirements/MentionLink.tsx
@@ -33,23 +33,22 @@ export function MentionLink({ platform, profileId, handle }: MentionLinkProps) {
     const { data: twitterHandle, isLoading } = useQuery({
         enabled: isTwitter && !handle,
         queryKey: ['twitter-user-info', profileId],
-        queryFn: async () => {
-            return FireflyTwitter.getUserInfoById(profileId)
-        },
+        queryFn: () => FireflyTwitter.getUserInfoById(profileId),
         select(data) {
             return data?.username
         },
     })
 
+    const screenName = isTwitter ? twitterHandle || handle : handle
     if (isLoading) return <LoadingBase size={12} />
 
-    if (!handle) return <span>the creator</span>
+    if (!screenName) return <span>the creator</span>
 
     return (
         <Link
             href={resolveProfileUrl(
                 platform,
-                platform === FireflyRedPacketAPI.PlatformType.farcaster ? profileId : handle,
+                platform === FireflyRedPacketAPI.PlatformType.farcaster ? profileId : screenName,
             )}
             target="_blank"
             className={classes.textLink}>
