5086f1b0b578c15039731845d7391f0c31b0908c
guanbinrui <guanbinrui@mask.io>
2025-01-07 22:03:53 +0800

    chore: chain id

--- packages/web3-shared/solana/src/sdk/index.ts
+++ packages/web3-shared/solana/src/sdk/index.ts
@@ -2,13 +2,11 @@ import { ChainId } from '../types.js'
 
 const Endpoints: Record<ChainId, string> = {
     [ChainId.Mainnet]: 'https://solana-mainnet.phantom.app/YBPpkkN4g91xDiAnTE9r0RcMkjg0sKUIWvAfoFVJ',
+    [ChainId.Testnet]: '',
+    [ChainId.Devnet]: '',
     [ChainId.Invalid]: '',
 }
 
 export function createClientEndpoint(chainId = ChainId.Mainnet) {
     return Endpoints[chainId]
-}
-
-export function createClient(chainId = ChainId.Mainnet) {
-    throw new Error('Not implemented')
 }
