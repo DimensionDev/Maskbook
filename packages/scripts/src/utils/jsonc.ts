195cf97f1452dfd7d40f6bb65ad9984ad930c108
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2025-02-17 18:25:20 +0800

    refactor(firefly): drop unused deps (#12114)

--- packages/scripts/src/utils/jsonc.ts
+++
@@ -1,9 +0,0 @@
-export function parseJSONc(data: string) {
-    if (data.includes('/*')) throw new TypeError('Only // comments are supported.')
-    data = data
-        .split('\n')
-        .filter((line) => !line.trim().startsWith('//'))
-        .join('\n')
-
-    return JSON.parse(data)
-}
--- packages/scripts/src/utils/paths.ts
+++
@@ -1,2 +0,0 @@
-export const ROOT_PATH = new URL('../../../../', import.meta.url)
-export const PKG_PATH = new URL('packages/', ROOT_PATH)
