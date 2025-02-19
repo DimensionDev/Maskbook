d78764cdd2cc646336f4d48d1302f00d186b20b4
Wukong Sun <swkatmask@gmail.com>
2025-02-19 15:54:58 +0800

    chore: add api key header for web3bio requests (#12128)

--- packages/web3-providers/src/Web3Bio/constants.ts
+++ packages/web3-providers/src/Web3Bio/constants.ts
@@ -1,2 +1,5 @@
 export const WEB3_BIO_HOST = 'https://api.web3.bio'
 export const WEB3_BIO_ROOT_URL = 'https://cors-next.r2d2.to?https://web3.bio/api/profile'
+// cspell: disable
+export const WEB3_BIO_JWT =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiMDkyM0YxIiwiZXhwIjoyMjA1OTA5MjkwLCJyb2xlIjo2fQ.slM96fUUOUs0ZiZAZSP6qClBCjDpIYqm6gzT73OADKA'
--- packages/web3-providers/src/Web3Bio/index.ts
+++ packages/web3-providers/src/Web3Bio/index.ts
@@ -1,7 +1,7 @@
 import urlcat from 'urlcat'
 import { NextIDPlatform, type Web3BioProfile } from '@masknet/shared-base'
 import { fetchCachedJSON } from '../helpers/fetchJSON.js'
-import { WEB3_BIO_HOST } from './constants.js'
+import { WEB3_BIO_HOST, WEB3_BIO_JWT } from './constants.js'
 
 type Response<T> =
     | T
@@ -24,25 +24,30 @@ function patchProfile(profile: Web3BioProfile): Web3BioProfile {
 
 export class Web3Bio {
     static fetchFromWeb3Bio<T>(request: Request | RequestInfo, init?: RequestInit) {
-        return fetchCachedJSON<T>(request, init)
+        return fetchCachedJSON<T>(request, {
+            ...init,
+            headers: {
+                'X-API-KEY': `Bearer ${WEB3_BIO_JWT}`,
+            },
+        })
     }
 
     static async getProfilesByTwitterId(handle: string) {
         const url = urlcat(WEB3_BIO_HOST, `/profile/twitter,${handle.toLowerCase()}`)
-        const res = await fetchCachedJSON<Response<Web3BioProfile[]>>(url)
+        const res = await Web3Bio.fetchFromWeb3Bio<Response<Web3BioProfile[]>>(url)
         return Array.isArray(res) ? res.map(patchProfile) : []
     }
 
     /** Get profiles by address or domain */
     static async getProfilesBy(domainOrAddress: string) {
         const url = urlcat(WEB3_BIO_HOST, '/profile/:id', { id: domainOrAddress })
-        const res = await fetchCachedJSON<Response<Web3BioProfile[]>>(url)
+        const res = await Web3Bio.fetchFromWeb3Bio<Response<Web3BioProfile[]>>(url)
         return Array.isArray(res) ? res.map(patchProfile) : []
     }
 
     static async getProfilesByNextId(pubkey: string) {
         const url = urlcat(WEB3_BIO_HOST, '/profile/nextid,:pubkey', { pubkey })
-        const res = await fetchCachedJSON<Response<Web3BioProfile[]>>(url)
+        const res = await Web3Bio.fetchFromWeb3Bio<Response<Web3BioProfile[]>>(url)
         return Array.isArray(res) ? res.map(patchProfile) : []
     }
 
