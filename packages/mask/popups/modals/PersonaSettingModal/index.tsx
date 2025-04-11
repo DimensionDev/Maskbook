dfa11fdc707e459ca3cf7eb1d6df5ef9def60d5d
Wukong Sun <swkatmask@gmail.com>
2025-04-11 15:04:50 +0800

    fix: mf-6690 modal naviate (#12199)

--- packages/mask/popups/modals/PersonaSettingModal/index.tsx
+++ packages/mask/popups/modals/PersonaSettingModal/index.tsx
@@ -120,7 +120,6 @@ export const PersonaSettingModal = memo<ActionModalBaseProps>(function PersonaSe
                             !user.backupPassword ?
                                 PopupModalRoutes.SetBackupPassword
                             :   PopupModalRoutes.verifyBackupPassword,
-                            { to: PopupRoutes.ExportPrivateKey },
                         )
                     }}
                 />
