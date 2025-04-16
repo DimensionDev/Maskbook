64c06d555e892a1e4eed36149fe392da56b285f2
Wukong Sun <swkatmask@gmail.com>
2025-04-16 16:06:41 +0800

    chore: versioning coingecko tokens

--- packages/web3-providers/src/CoinGecko/apis/DSearchAPI.ts
+++ packages/web3-providers/src/CoinGecko/apis/DSearchAPI.ts
@@ -20,7 +20,7 @@ export interface NonFungibleToken {
 export class CoinGeckoSearchAPI<ChainId, SchemaType> implements DSearchBaseAPI.DataSourceProvider<ChainId, SchemaType> {
     async get(): Promise<Array<FungibleTokenResult<ChainId, SchemaType>>> {
         return fetchFromDSearch<Array<FungibleTokenResult<ChainId, SchemaType>>>(
-            urlcat(DSEARCH_BASE_URL, '/fungible-tokens/coingecko.json'),
+            urlcat(DSEARCH_BASE_URL, '/fungible-tokens/coingecko.json?update=1'),
             { mode: 'cors' },
         )
     }
