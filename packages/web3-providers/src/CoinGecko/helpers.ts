d8ec84d4983eddfe52cfde70bd81231a70f11eb5
Wukong Sun <swkatmask@gmail.com>
2025-06-06 10:48:38 +0800

    chore(welcome): welcome-error handle permission request errors
    add custom snackbar to show error message when permission request fails.

--- packages/web3-providers/src/CoinGecko/helpers.ts
+++ packages/web3-providers/src/CoinGecko/helpers.ts
@@ -3,6 +3,7 @@ import type { Web3Helper } from '@masknet/web3-helpers'
 import { ChainId } from '@masknet/web3-shared-evm'
 import { ChainId as ChainIdSolana } from '@masknet/web3-shared-solana'
 
+// https://docs.coingecko.com/reference/networks-list
 export const resolveCoinGeckoChainId = createLookupTableResolver<string, Web3Helper.ChainIdAll | undefined>(
     {
         ethereum: ChainId.Mainnet,
