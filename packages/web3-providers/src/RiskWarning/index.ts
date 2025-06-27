f2d88f31bcdecdaaa9665f0afb0322be9deadebe
Wukong Sun <swkatmask@gmail.com>
2025-06-27 19:22:44 +0800

    fix(risk-warning): handle approve promise rejection

--- packages/web3-providers/src/RiskWarning/index.ts
+++ packages/web3-providers/src/RiskWarning/index.ts
@@ -8,12 +8,14 @@ const BASE_URL = BASE_URL_MAP[process.env.NODE_ENV]
 
 export class RiskWarning {
     static async approve(address: string, pluginID = '') {
-        await fetch(`${BASE_URL}/v1/risk_warning/confirm`, {
-            method: 'POST',
-            body: JSON.stringify({
-                address,
-                plugin_id: pluginID,
-            }),
-        })
+        try {
+            await fetch(`${BASE_URL}/v1/risk_warning/confirm`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    address,
+                    plugin_id: pluginID,
+                }),
+            })
+        } catch {}
     }
 }
