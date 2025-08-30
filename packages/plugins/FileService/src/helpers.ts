aaeba6146ca8f6cf06ad02a61e5375ff573ddf93
guanbinrui <guanbinrui@mask.io>
2025-08-30 09:39:39 +0800

    fix: u flag

--- packages/plugins/FileService/src/helpers.ts
+++ packages/plugins/FileService/src/helpers.ts
@@ -14,7 +14,7 @@ import schemaV2 from './schema-v2.json' with { type: 'json' }
 import schemaV3 from './schema-v3.json' with { type: 'json' }
 
 // Load legacy detection + gateway
-export const LOAD_LEGACY_ID_REGEX = /^0x[a-f0-9]{64}$/i
+export const LOAD_LEGACY_ID_REGEX = /^0x[a-f0-9]{64}$/iu
 export const LOAD_LEGACY_GATEWAY_URL = 'https://load0.network/download'
 
 // Note: if the latest version has been changed, please update packages/mask/content-script/components/CompositionDialog/useSubmit.ts
