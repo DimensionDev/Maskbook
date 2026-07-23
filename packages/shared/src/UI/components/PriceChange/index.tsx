290d03c5caab391c127d77a26836bf4a75dd33cf
Jack-Works <Jack-Works@users.noreply.github.com>
2026-07-23 03:55:23 +0000

    fix: eslint

--- packages/shared/src/UI/components/PriceChange/index.tsx
+++ packages/shared/src/UI/components/PriceChange/index.tsx
@@ -39,7 +39,7 @@ export const PriceChange = memo(function PriceChange({ change, loading, ...rest 
             {change ?
                 <Icons.ArrowDrop size={16} style={{ color, transform: change > 0 ? 'rotate(180deg)' : '' }} />
             :   null}
-            <Typography className={classes.value} sx={{ color: color }}>
+            <Typography className={classes.value} sx={{ color }}>
                 {change ? `${change.toFixed(2)}%` : '--'}
             </Typography>
         </Box>
