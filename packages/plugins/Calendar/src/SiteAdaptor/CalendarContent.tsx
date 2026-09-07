df66b646cc98f05ce477a4b5efe066d93ed17538
Wukong Sun <swkatmask@gmail.com>
2026-09-07 18:03:55 +0800

    fix(calendar): use white feed background

--- packages/plugins/Calendar/src/SiteAdaptor/CalendarContent.tsx
+++ packages/plugins/Calendar/src/SiteAdaptor/CalendarContent.tsx
@@ -16,7 +16,7 @@ const useStyles = makeStyles()((theme) => {
         calendar: {
             display: 'flex',
             flexDirection: 'column',
-            backgroundColor: theme.vars.palette.background.default,
+            backgroundColor: theme.vars.palette.maskColor.bottom,
             borderRadius: '12px',
             border: `1px solid ${theme.vars.palette.maskColor.line}`,
             position: 'relative',
