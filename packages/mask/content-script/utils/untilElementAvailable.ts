206d27745eb73227bd595f78c5a3d72345b79ce3
Wukong Sun <swkatmask@gmail.com>
2026-09-08 11:34:25 +0800

    fix: mf-6800 stop IntervalWatcher when untilElementAvailable times out

--- packages/mask/content-script/utils/untilElementAvailable.ts
+++ packages/mask/content-script/utils/untilElementAvailable.ts
@@ -4,9 +4,13 @@ export function untilElementAvailable(ls: LiveSelector<HTMLElement, boolean>, ti
     return new Promise<void>((resolve, reject) => {
         const w = new IntervalWatcher(ls)
         // eslint-disable-next-line @eslint-react/purity
-        setTimeout(() => reject(), timeout)
+        const timer = setTimeout(() => {
+            w.stopWatch()
+            reject()
+        }, timeout)
         w.useForeach(() => {
             w.stopWatch()
+            clearTimeout(timer)
             resolve()
         }).startWatch(500)
     })
