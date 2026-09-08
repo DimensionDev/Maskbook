268c3742387d6701d88e97b9bf5a3313cf402565
Wukong Sun <swkatmask@gmail.com>
2026-09-08 11:21:52 +0800

    fix: mf-6796 use rule-compliant minimal ternary for gas fields

--- packages/plugins/Trader/src/SiteAdaptor/trader/views/BridgeConfirm.tsx
+++ packages/plugins/Trader/src/SiteAdaptor/trader/views/BridgeConfirm.tsx
@@ -272,11 +272,10 @@ export const BridgeConfirm = memo(function BridgeConfirm() {
                 value: transaction.value,
                 gasPrice: gasConfig.gasPrice ?? transaction.gasPrice,
                 gas: gas ? addGasMargin(gas, fromChainId === ChainId.Arbitrum ? 1 : 0.3) : undefined,
-                maxFeePerGas: 'maxFeePerGas' in gasConfig ? gasConfig.maxFeePerGas : undefined,
+                maxFeePerGas: ('maxFeePerGas' in gasConfig && gasConfig.maxFeePerGas) || undefined,
                 maxPriorityFeePerGas:
-                    'maxPriorityFeePerGas' in gasConfig && gasConfig.maxPriorityFeePerGas ?
-                        gasConfig.maxPriorityFeePerGas
-                    :   transaction.maxPriorityFeePerGas,
+                    ('maxPriorityFeePerGas' in gasConfig && gasConfig.maxPriorityFeePerGas ? gasConfig : transaction)
+                        .maxPriorityFeePerGas,
                 _disableSnackbar: true,
             },
             {
